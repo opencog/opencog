c13c61b291238ff3dab6d868faf6846669ca1bbb
linas <>
2008-01-27 21:17:30 +0000

    cleanup several buffer overflows, plus some bizarre conceptions about string allocation.

--- src/AtomSpace/Node.cc
+++ src/AtomSpace/Node.cc
@@ -14,65 +14,70 @@
 #include "AtomSpaceDefinitions.h"
 
 
-Node::Node(Type type, const std::string& name, const TruthValue& tv) throw (InvalidParamException, AssertionException) : Atom(type, std::vector<Handle>(), tv) {
-
+Node::Node(Type type, 
+           const std::string& cname, 
+           const TruthValue& tv) 
+throw (InvalidParamException, AssertionException) 
+    : Atom(type, std::vector<Handle>(), tv) 
+{
     if (!ClassServer::isAssignableFrom(NODE, type)) {
         throw InvalidParamException(TRACE_INFO, "Node - Invalid node type '%d'.", type);
     }
     
     cassert(TRACE_INFO, &name, "Node - name parameter should not be NULL.");
-    
-    // If name is empty let it unitialized for saving memory...
-    if (!name.empty()) {
-        this->name = name;
-    }
+    name = cname;
 }
 
-Node::~Node() throw () {
-//    fprintf(stdout, "Deleting Node:\n%s\n", this->toString().c_str());
+Node::~Node() throw ()
+{
+//    fprintf(stdout, "Deleting Node:\n%s\n", toString().c_str());
 //    fflush(stdout);
 }
 
-const std::string& Node::getName() const{
+const std::string& Node::getName() const
+{
     return name;
 }
 
-void Node::setName(const std::string& name) throw (RuntimeException) {
+void Node::setName(const std::string& cname) throw (RuntimeException)
+{
     
     if (atomTable != NULL){
         throw RuntimeException(TRACE_INFO, 
               "Node - Cannot change the name of a node already inserted into an AtomTable.");
     }
-
-    if (!name.empty()) {
-        this->name = name;
-    } else if (!(this->name.empty())) {
-        this->name = name;
-    }
+    name = cname;
 }
 
-void Node::merge(Atom* atom) throw (InconsistenceException) {
-
-    if (!equals(atom)){
+void Node::merge(Atom* atom) throw (InconsistenceException)
+{
+    if (!equals(atom)) {
         throw InconsistenceException(TRACE_INFO, "Node - Different nodes cannot be merged");
     }
 
     Atom::merge(atom);
 }
 
-std::string Node::toShortString() {
-    char buf[1024];
-    sprintf(buf, "node[%d:%s%s]", type, name.c_str(), (getFlag(HYPOTETHICAL_FLAG)?":h":""));
+std::string Node::toShortString()
+{
+#define BUFSZ 1024
+    char buf[BUFSZ];
+    snprintf(buf, BUFSZ, "node[%d:%s%s]", 
+        type, name.c_str(), (getFlag(HYPOTETHICAL_FLAG)?":h":""));
     return buf;
 }
 
-std::string Node::toString() {
-    char buf[1024];
-    sprintf(buf, "node[%d:%s] (%d,%f)", type, name.c_str(), (int)getAttentionValue().getSTI(), 0.0); //activation here at 0: can be replace with LTI
+std::string Node::toString()
+{
+    char buf[BUFSZ];
+    //activation here at 0: can be replace with LTI
+    snprintf(buf, BUFSZ, "node[%d:%s] (%d,%f)", 
+        type, name.c_str(), (int)getAttentionValue().getSTI(), 0.0);
     return buf;
 }
 
-bool Node::equals(Atom *other){
+bool Node::equals(Atom *other)
+{
     bool result = false;
     if (other != NULL && (type == other->getType())){
         Node *otherNode = (Node *) other;
@@ -81,10 +86,10 @@ bool Node::equals(Atom *other){
     return(result);
 }
 
-int Node::hashCode(){
+int Node::hashCode()
+{
     int result = Atom::hashCode();
     result += Util::hash<const char*>()(name.c_str());
     return result;
 }
-
 
