7d6a25ef9acc4bbd8f08bd1301abe9a04c0331e4
linas <>
2008-03-20 00:21:01 +0000

    install the query processor into the cog server.

--- src/CogServer/CogServerMain.cc
+++ src/CogServer/CogServerMain.cc
@@ -1,8 +1,15 @@
 #include "CogServer.h"
+#include "QueryProcessor.h"
 
 using namespace opencog;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     CogServer server;
+
+    // Cheapo hack to get the query processory up and running.
+    // XXX fix me with some more permanent, appropriate solution.
+    QueryProcessor *qp = new QueryProcessor();
+    server.plugInMindAgent(qp, 1);
     server.serverLoop();
 }
