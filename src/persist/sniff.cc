3b339a0c49894a3b07b0dbc7732113f1f39e6b8a
linas <>
2008-04-05 03:51:18 +0000

    add a bit of insertion code.

--- src/persist/sniff.cc
+++ src/persist/sniff.cc
@@ -35,6 +35,7 @@ class AtomStorage
 };
 
 #include "Node.h"
+#include "TLB.h"
 #include "type_codes.h"
 
 AtomStorage::AtomStorage(void)
@@ -47,9 +48,35 @@ AtomStorage::~AtomStorage()
 	delete db_conn;
 }
 
-void AtomStorage::storeAtom(Atom *)
+void AtomStorage::storeAtom(Atom *atom)
 {
-	
+	std::string cols = "INSERT INTO Atoms (";
+	std::string vals = ") VALUES (";
+
+	char buff[24];
+	Handle h = TLB::getHandle(atom);
+	snprintf(buff, 24, "%lu", (unsigned long) h);
+	cols += "uuid"; 
+	vals += buff;
+
+	Type t = atom->getType();
+	snprintf(buff, 24, "%u", t);
+	cols += ", type"; 
+	vals += ", "; 
+	vals += buff;
+
+	Node *n = dynamic_cast<Node *>(atom);
+	if (n)
+	{
+		cols += ", name";
+		vals += ", '";
+		vals += n->getName();
+		vals += "'";
+	}
+
+	std::string qry = cols + vals + ");";
+printf ("duude its %s\n", qry.c_str());
+	db_conn->exec(qry.c_str());
 }
 
 Atom * AtomStorage::getAtom(Handle h)
