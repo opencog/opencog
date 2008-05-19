df8ca33ce67d7d0773011cb6ef907b00503e1cce
linas <>
2008-05-19 22:53:40 +0000

    initial checkin

---
+++ src/nlp/wsd/NNAdjust.cc
@@ -0,0 +1,167 @@
+/*
+ * NNAdjust.cc
+ *
+ * Bumps up the strength of the edges connecting two nouns in a  
+ * noun-modifier (nn) relationship. Basically, if there is a 
+ * noun-modifier phrase, then make a stronger tie to make sure 
+ * that the two word senses corellate with each other.
+ *
+ * Copyright (c) 2008 Linas Vepstas <linas@linas.org>
+ */
+#include <stdio.h>
+
+#include "ForeachWord.h"
+#include "NNAdjust.h"
+#include "SimpleTruthValue.h"
+
+#define DEBUG
+
+using namespace opencog;
+
+NNAdjust::NNAdjust(void)
+{
+}
+
+NNAdjust::~NNAdjust()
+{
+}
+
+/** Loop over all parses for this sentence. */
+void NNAdjust::adjust_sentence(Handle h)
+{
+	foreach_parse(h, &NNAdjust::adjust_parse, this);
+}
+
+/**
+ * For each parse, loop over all word-instances
+ */
+bool NNAdjust::adjust_parse(Handle h)
+{
+	foreach_word_instance(h, &NNAdjust::adjust_word, this);
+	return false;
+}
+
+/**
+ * For each word-instance loop over all syntactic relationships.
+ * (i.e. _subj, _obj, _nn, _amod, and so on). Discard all but the _nn.
+ */
+bool NNAdjust::adjust_word(Handle h)
+{
+	foreach_relex_relation(h, &NNAdjust::adjust_relation, this);
+	return false;
+}
+
+/**
+ * This routine is called for every relation between word-instances in
+ * a parse. 
+ */
+bool NNAdjust::adjust_relation(const std::string &relname, Handle first, Handle second)
+{
+	if (relname.compare("_nn")) return false;
+xxx
+	return false;
+}
+
+/**
+ * Create edges between all senses of a pair of words.
+ *
+ * This routine implements a doubley-nested foreach loop, iterating
+ * over all senses of each word, and creating an edge between them.
+ *
+ * All of the current word-sense similarity algorithms report zero
+ * similarity when the two words are different parts of speech. 
+ * Therefore, in order to improve performance, this routine does not 
+ * create any edges between words of differing parts-of-speech.
+ */
+bool NNAdjust::annotate_word_pair(Handle first, Handle second)
+{
+#ifdef DEBUG
+	Node *f = dynamic_cast<Node *>(TLB::getAtom(first));
+	Node *s = dynamic_cast<Node *>(TLB::getAtom(second));
+	const std::string &fn = f->getName();
+	const std::string &sn = s->getName();
+	printf("(%s, %s)\n", fn.c_str(), sn.c_str());
+#endif
+
+	// Don't bother linking words with different parts-of-speech;
+	// the similarity measures don't support these.
+	std::string first_pos = get_pos_of_word_instance(first);
+	std::string second_pos = get_pos_of_word_instance(second);
+	if (0 != first_pos.compare(second_pos))
+	{
+		return false;
+	}
+
+	second_word_inst = second;
+	foreach_word_sense_of_inst(first, &NNAdjust::sense_of_first_inst, this);
+	
+	return false;
+}
+
+/**
+ * Called for every pair (word-instance,word-sense) of the first
+ * word-instance of a relex relationship. This, in turn iterates
+ * over the second word-instance of the relex relationship.
+ */
+bool NNAdjust::sense_of_first_inst(Handle first_word_sense_h,
+                                       Handle first_sense_link_h)
+{
+	first_word_sense = first_word_sense_h;
+
+	// Rule out relations that aren't actual word-senses.
+	Node *sense = dynamic_cast<Node *>(TLB::getAtom(first_word_sense_h));
+	if (!sense || sense->getType() != WORD_SENSE_NODE) return false;
+
+	// printf("first sense %s\n", sense->getName().c_str());
+	// Get the handle of the link itself .. 
+	first_sense_link = first_sense_link_h;
+
+	foreach_word_sense_of_inst(second_word_inst,
+	                           &NNAdjust::sense_of_second_inst, this);
+	return false;
+}
+
+/**
+ * Called for every pair (word-instance,word-sense) of the second
+ * word-instance of a relex relationship. This routine is the last,
+ * most deeply nested loop of all of this set of nested loops.  This
+ * routine now has possession of both pairs, and can now create a 
+ * Mihalcea-graph edge between these pairs.
+ *
+ * As discussed in the README file, the resulting structure is:
+ *
+ *    <!-- the word "tree" occured in the sentence -->
+ *    <CosenseLink strength=0.49 confidence=0.3>
+ *       <InheritanceLink strength=0.9 confidence=0.6>
+ *          <ConceptNode name="tree_99" />
+ *          <WordSenseNode name="tree_sense_12" />
+ *       </InheritanceLink>
+ *       
+ *       <InheritanceLink strength=0.9 confidence=0.1>
+ *          <ConceptNode name="bark_144" />
+ *          <WordSenseNode name="bark_sense_23" />
+ *       </InheritanceLink>
+ *    </CosenseLink>
+ */
+bool NNAdjust::sense_of_second_inst(Handle second_word_sense_h,
+                                        Handle second_sense_link)
+{
+	// Rule out relations that aren't actual word-senses.
+	Node *sense = dynamic_cast<Node *>(TLB::getAtom(second_word_sense_h));
+	if (!sense || sense->getType() != WORD_SENSE_NODE) return false;
+	// printf("second sense %s!\n", sense->getName().c_str());
+
+	// Create a link connecting the first pair to the second pair.
+	std::vector<Handle> out;
+	out.push_back(first_sense_link);
+	out.push_back(second_sense_link);
+
+	// Use a word-sense similarity/relationship measure to assign an 
+	// initial truth value to the edge.
+	SenseSimilarity *ss = new SenseSimilarity();
+	SimpleTruthValue stv = ss->lch_similarity(first_word_sense, second_word_sense_h);
+
+	atom_space->addLink(COSENSE_LINK, out, stv);
+
+	return false;
+}
---
+++ src/nlp/wsd/NNAdjust.h
@@ -0,0 +1,44 @@
+/*
+ * NNAdjust.h
+ *
+ * Bumps up word-sense dependencies between nouns in a 
+ * noun-modifier relationship with each other.
+ *
+ * Copyright (c) 2008 Linas Vepstas <linas@linas.org>
+ */
+
+#ifndef OPENCOG_NN_ADJUST_H
+#define OPENCOG_NN_ADJUST_H
+
+#include <string>
+
+#include "Atom.h"
+#include "AtomSpace.h"
+
+namespace opencog {
+
+class NNAdjust
+{
+	private:
+		bool adjust_parse(Handle);
+		bool adjust_word(Handle);
+		bool adjust_relation(const std::string &, Handle, Handle);
+
+/*
+		bool annotate_word_pair(Handle, Handle);
+
+		Handle second_word_inst;
+		Handle first_word_sense;
+		Handle first_sense_link;
+		bool sense_of_first_inst(Handle, Handle);
+		bool sense_of_second_inst(Handle, Handle);
+*/
+
+	public:
+		NNAdjust(void);
+		~NNAdjust();
+		void adjust_sentence(Handle);
+};
+}
+
+#endif /* OPENCOG_NN_ADJUST_H */
