60324b0eca450ece93a4b9db49e9fe48b75626b3
Linas Vepstas <linasvepstas@gmail.com>
2008-05-21 18:11:12 -0500

    more layout fixes

--- src/nlp/semcor/strip-text.pl
+++ src/nlp/semcor/strip-text.pl
@@ -1,28 +1,35 @@
 #! /usr/bin/env perl
 #
-# This file just stripes out the sentences from the semcor files.
+# This utility just strips out the sentences from the semcor files.
+#
 
 # Loop -- read from stdin, and put quotes around the attributes.
 $leading_quote = 0;
 while(<>)
 {
 	chop;
-	s/<[\/\:\$\'\w\s=-]+>//g;  # strip out all xml markup
+	s/<[\/\:\$\'\(\)\w\s=-]+>//g;  # strip out all xml markup
 
+	s/^ //;
 	s/_/ /g;  # replace underscores.
 
 	s/\'\'/\"/g;
 	s/\`\`/\"/g;
 
-	if ((!(/^[\'\"\,\.]/)) && (leading_quote == 0))
+	if (!($_)) { next; }
+
+	if ((!(/^[\'\"\,\.]/)) && ($leading_quote == 0))
 	{ 
 		print " ";
 	}
-	print $_;
-	if (/\./) { print "\n"; }
 
 	$leading_quote = 0;
 	if (/^\"/) { $leading_quote = 1; }
+
+	print $_;
+	if (/\?/) {print " "; }
+	if (/\./) { print "\n"; $leading_quote = 1; }
+
 
 }
 
