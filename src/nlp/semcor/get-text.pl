e8190f16be7596c99e0425ef25fdafbbda3111fa
Linas Vepstas <linasvepstas@gmail.com>
2008-05-21 16:09:57 -0500

    add quotes to semcor xml files

--- src/nlp/semcor/get-text.pl
+++ src/nlp/semcor/get-text.pl
@@ -1,6 +1,8 @@
 #! /usr/bin/env perl
 
 # XML::Simple complains about the xml markup used in the semcore files :-(
+# Grrr so does XML::SimpleObject
+#
 # use XML::Simple;
 # my $xs1 = XML::Simple->new();
 # my $doc = $xs1->XMLin("-");
@@ -9,12 +11,31 @@ use XML::SimpleObject;
 
 $xml = "";
 
+# Loop -- read from stdin, and put quotes around the attributes.
 while(<>)
 {
-	$xml = $xml . $_;
+	s/=/=\"/g;
+	split;
+	foreach(@_)
+	{
+		if (/=/) { 
+			if (/>/) {
+				s/>/\">/;
+				$xml .= $_;
+			} else {
+				$xml .= $_;
+				$xml .= "\"";
+			}
+		} else {
+			$xml .= $_;
+		}
+		$xml .= " ";
+	}
+	$xml .= "\n";
 }
+print $xml;
 
-my $xmlobj = new XML::SimpleObject(XML => $xml);
+# my $xmlobj = new XML::SimpleObject(XML => $xml);
 
 
 
