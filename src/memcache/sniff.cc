049b50da60cd0d29c3d9c3fdb80c60d3c1825fd3
Linas Vepstas <linasvepstas@gmail.com>
2008-06-12 15:51:54 -0500

    working version.

--- src/memcache/sniff.cc
+++ src/memcache/sniff.cc
@@ -5,7 +5,9 @@
  * Linas Vepstas June 2008
  */
 
+#include <assert.h>
 #include <memcached.h>
+#include <stdio.h>
 
 int main ()
 {
@@ -14,15 +16,27 @@ int main ()
 	mc = memcached_create(NULL);
 
 	memcached_server_st *servers;
-	char servername[] = "localhost";
-	servers = memcached_server_list_append(NULL, servername, 400, &rc);
+	// char servername[] = "localhost";
+	char servername[] = "127.0.0.1";
+	servers = memcached_server_list_append(NULL, servername, 21201, &rc);
 
 	rc = memcached_server_push(mc, servers);
+	assert(rc == MEMCACHED_SUCCESS);
 	memcached_server_list_free(servers);
 
+	
+	rc = memcached_set (mc, "asdf", 4, "pqrs", 4, 0, 0);
+	if (rc != MEMCACHED_SUCCESS)
+	{
+		printf("oops its %s\n", memcached_strerror(mc, rc));
+	}
 
-
-	memcached_set (mc, "asdf", 4, "pqrs", 4, 0, 0);
+	size_t vlen;
+	uint32_t flags;
+	char *val = memcached_get(mc, "asdf", 4, &vlen, &flags, &rc);
+	assert(rc == MEMCACHED_SUCCESS);
+	printf ("duude val=%s len=%d\n", val, vlen);
+	
 
 	memcached_free(mc);
 }
