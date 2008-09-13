76ff49e8274522d323cfc323d63d26a73fc0d06d
Joel Pitt <joel@fruition-ubuntu>
2008-09-13 15:37:41 -0400

    add strtok

--- src/atomspace/CompositeTruthValue.cc
+++ src/atomspace/CompositeTruthValue.cc
@@ -31,6 +31,27 @@
 
 using namespace opencog;
 
+#ifndef HAVE_STRTOK_R
+#define HAVE_STRTOK_R 1
+char * __strtok_r(char *s1, const char *s2, char **lasts)
+{
+    char *ret;
+    if (s1 == NULL)
+        s1 = *lasts;
+    while (*s1 && strchr(s2, *s1))
+        ++s1;
+    if (*s1 == '\0')
+        return NULL;
+    ret = s1;
+    while (*s1 && !strchr(s2, *s1))
+        ++s1;
+    if (*s1)
+        *s1++ = '\0';
+    *lasts = s1;
+    return ret;
+}
+#endif /* HAVE_STRTOK_R */ 
+
 void CompositeTruthValue::init(const TruthValue& tv, VersionHandle vh)
 {
     primaryTV = NULL;
