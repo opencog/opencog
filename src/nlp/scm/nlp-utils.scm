f7840576469c8729c2171adc8748111660518019
Linas Vepstas <linasvepstas@gmail.com>
2008-09-14 19:11:52 -0500

    move generic utilities to its own file

---
+++ src/nlp/scm/nlp-utils.scm
@@ -0,0 +1,62 @@
+;
+; nlp-utils.scm
+;
+; Assorted NLP utilities.
+; 
+; Copyright (c) 2008 Linas Vepstas <linasvepstas@gmail.com>
+;
+; =============================================================
+
+; Right now, every parse of a sentence is anchored to a ConceptNode
+(define ParseAnchor 'ConceptNode)
+
+; Every word of a parse is also a concept node.
+(define WordAnchor 'ConceptNode)
+
+; =============================================================
+; Assorted sentence, parse, word, word-sense wrangling utilities below.
+;
+; map-parses proc sent
+; Call proceedure 'proc' on every parse of the sentence 'sent' 
+; 
+; Expected input is a SentenceNode, which serves as an anchor to all
+; of the parses of a sentence. It is connected via ParseLink's to 
+; each of the individual parses of the sentence. So, look for 
+; ParseAnchor's, which anchor the parses.
+;
+(define (map-parses proc sent) 
+	(cog-map-chase-link 'ParseLink ParseAnchor
+		" ========= end of sentence ============ \n" ""
+		proc sent
+	)
+)
+
+; map-word-instances proc parse
+; Call proceedure 'proc' on each word-instance of 'parse'
+;
+; Expected input is a ParseAnchor, which serves as an anchor
+; to all of the word instances in a parse. The ParseAnchor is
+; connnected via a ParseInstanceLink to the individual words.
+; 
+(define (map-word-instances proc parse) 
+	(cog-map-chase-link 'ParseInstanceLink WordAnchor
+		" --------- end of parse ------------ \n" ""
+		proc parse
+	)
+)
+
+; map-word-node proc word-inst
+; Call proceedure 'proc' on the word-node associated to 'word-inst'
+;
+; Expected input is a WordAnchor, which serves as an anchor
+; to a word instance. The WordAnchor is connnected via a ReferenceLink
+; to the actual word node.
+;
+(define (map-word-node proc word-inst) 
+	(cog-map-chase-link 'ReferenceLink 'WordNode 
+		"" ""  ; " --------- word-found ------------ \n"
+		proc word-inst
+	)
+)
+
+; =============================================================
--- src/nlp/scm/parse-rank.scm
+++ src/nlp/scm/parse-rank.scm
@@ -15,60 +15,8 @@
 ; (use-modules (dbi dbi))
 ; (define db-connection (dbi-open "postgresql" db-login))
 
-; Right now, every parse of a sentence is anchored to a ConceptNode
-(define ParseAnchor 'ConceptNode)
-
-; Every word of a parse is also a concept node.
-(define WordAnchor 'ConceptNode)
-
 (define (prt-stuff h) (display h) #t)
 (define (prt-inc h) (display (cog-incoming-set h)) #t)
-
-; =============================================================
-; Assorted sentence, parse, word, word-sense wrangling utilities below.
-;
-; map-parses proc sent
-; Call proceedure 'proc' on every parse of the sentence 'sent' 
-; 
-; Expected input is a SentenceNode, which serves as an anchor to all
-; of the parses of a sentence. It is connected via ParseLink's to 
-; each of the individual parses of the sentence. So, look for 
-; ParseAnchor's, which anchor the parses.
-;
-(define (map-parses proc sent) 
-	(cog-map-chase-link 'ParseLink ParseAnchor
-		" ========= end of sentence ============ \n" ""
-		proc sent
-	)
-)
-
-; map-word-instances proc parse
-; Call proceedure 'proc' on each word-instance of 'parse'
-;
-; Expected input is a ParseAnchor, which serves as an anchor
-; to all of the word instances in a parse. The ParseAnchor is
-; connnected via a ParseInstanceLink to the individual words.
-; 
-(define (map-word-instances proc parse) 
-	(cog-map-chase-link 'ParseInstanceLink WordAnchor
-		" --------- end of parse ------------ \n" ""
-		proc parse
-	)
-)
-
-; map-word-node proc word-inst
-; Call proceedure 'proc' on the word-node associated to 'word-inst'
-;
-; Expected input is a WordAnchor, which serves as an anchor
-; to a word instance. The WordAnchor is connnected via a ReferenceLink
-; to the actual word node.
-;
-(define (map-word-node proc word-inst) 
-	(cog-map-chase-link 'ReferenceLink 'WordNode 
-		"" ""  ; " --------- word-found ------------ \n"
-		proc word-inst
-	)
-)
 
 ; =============================================================
 
