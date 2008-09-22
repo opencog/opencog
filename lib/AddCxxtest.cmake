786a1a8567f2cd235b520458ec89d7bf7ffa6600
Gustavo Machado Campagnani Gama <gama@balrog>
2008-09-22 15:56:09 -0300

    Minor build system fixes:   * Check guile's version if cmake >= 2.6   * Make cxxtest an optional dependency.

--- lib/AddCxxtest.cmake
+++ lib/AddCxxtest.cmake
@@ -1,67 +1,66 @@
 # ADD_CXXTEST macro from http://www.cmake.org/Wiki/CMakeMacroAddCxxTest
+FIND_PACKAGE(Cxxtest)
 
-FIND_PACKAGE(Cxxtest REQUIRED)
+IF (CXXTEST_FOUND)
 
-MACRO(ADD_CXXTEST NAME)
-    IF (CXXTEST_FOUND)
-        ADD_CUSTOM_COMMAND(
-            OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.cpp
-            COMMAND
-                "${PYTHON_EXECUTABLE}" ${CXXTEST_GEN}
-                --runner=ErrorPrinter
-                -o ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.cpp ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}.cxxtest
-            DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}.cxxtest ${ARGN}
-            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        )
-    ENDIF (CXXTEST_FOUND)
+	MACRO(ADD_CXXTEST NAME)
+		ADD_CUSTOM_COMMAND(
+			OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.cpp
+			COMMAND
+				"${PYTHON_EXECUTABLE}" ${CXXTEST_GEN}
+				--runner=ErrorPrinter
+				-o ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.cpp ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}.cxxtest
+			DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}.cxxtest ${ARGN}
+			WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+		)
 
-    ADD_EXECUTABLE(${NAME} ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.cpp ${ARGN})
-    INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR} ${CXXTEST_INCLUDE_DIRS})
+		ADD_EXECUTABLE(${NAME} ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.cpp ${ARGN})
+		INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR} ${CXXTEST_INCLUDE_DIRS})
 
-    ADD_TEST(${NAME} ${NAME})
-	ADD_DEPENDENCIES(tests ${NAME})
-ENDMACRO(ADD_CXXTEST)
+		ADD_TEST(${NAME} ${NAME})
+		ADD_DEPENDENCIES(tests ${NAME})
+	ENDMACRO(ADD_CXXTEST)
 
-#The above macro generates a single source file for all input test headers. 
-#If by some reason you prefer separate compilation of each part, you may use the variation:
+	#The above macro generates a single source file for all input test headers. 
+	#If by some reason you prefer separate compilation of each part, you may use the variation:
 
-MACRO(ADD_CXXTEST_SEP NAME)
-    IF(CXXTEST_FOUND)
-        # generate the parts
-        FOREACH(_PART ${ARGN})
-            GET_FILENAME_COMPONENT(_NAME ${_PART} NAME)
-            GET_FILENAME_COMPONENT(_NAME_WE ${_PART} NAME_WE)
-            ADD_CUSTOM_COMMAND(
-                OUTPUT ${_NAME_WE}.cpp
-                COMMAND
-                    ${PYTHON_EXECUTABLE} ${CXXTEST_GEN}
-                    --part
-                    -o ${_NAME_WE}.cpp
-                    ${_NAME}
-                DEPENDS ${_PART}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-            )
-        ENDFOREACH(_PART)
+	MACRO(ADD_CXXTEST_SEP NAME)
+		# generate the parts
+		FOREACH(_PART ${ARGN})
+			GET_FILENAME_COMPONENT(_NAME ${_PART} NAME)
+			GET_FILENAME_COMPONENT(_NAME_WE ${_PART} NAME_WE)
+			ADD_CUSTOM_COMMAND(
+				OUTPUT ${_NAME_WE}.cpp
+				COMMAND
+					${PYTHON_EXECUTABLE} ${CXXTEST_GEN}
+					--part
+					-o ${_NAME_WE}.cpp
+					${_NAME}
+				DEPENDS ${_PART}
+				WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+			)
+		ENDFOREACH(_PART)
 
-        # generate the runner
-        ADD_CUSTOM_COMMAND(
-            OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}_runner.cpp
-            COMMAND
-                ${PYTHON_EXECUTABLE} ${CXXTEST_GEN}
-                --runner=ErrorPrinter --root
-                -o ${NAME}_runner.cpp
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        )
-    ENDIF(CXXTEST_FOUND)
+		# generate the runner
+		ADD_CUSTOM_COMMAND(
+			OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}_runner.cpp
+			COMMAND
+				${PYTHON_EXECUTABLE} ${CXXTEST_GEN}
+				--runner=ErrorPrinter --root
+				-o ${NAME}_runner.cpp
+			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+		)
 
-    # enumerate all generated files
-    SET(PARTS ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}_runner.cpp)
-    FOREACH(_PART ${ARGN})
-        GET_FILENAME_COMPONENT(_PART_WE ${_PART} NAME_WE)
-        SET(PARTS ${PARTS} ${_PART_WE}.cpp)
-    ENDFOREACH(_PART)
+		# enumerate all generated files
+		SET(PARTS ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}_runner.cpp)
+		FOREACH(_PART ${ARGN})
+			GET_FILENAME_COMPONENT(_PART_WE ${_PART} NAME_WE)
+			SET(PARTS ${PARTS} ${_PART_WE}.cpp)
+		ENDFOREACH(_PART)
 
-    ADD_EXECUTABLE(${NAME} ${PARTS})
+		ADD_EXECUTABLE(${NAME} ${PARTS})
 
-    ADD_TEST(${NAME} ${NAME})
-ENDMACRO(ADD_CXXTEST_SEP)
+		ADD_TEST(${NAME} ${NAME})
+	ENDMACRO(ADD_CXXTEST_SEP)
+
+ENDIF(CXXTEST_FOUND)
