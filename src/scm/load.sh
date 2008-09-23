9e6914f39a527eb1b121c2bae58e58e999a080b4
Linas Vepstas <linasvepstas@gmail.com>
2008-09-23 16:20:22 -0500

    split out fanouts into thier own file.

--- src/scm/load.sh
+++ src/scm/load.sh
@@ -12,5 +12,6 @@ cat type_constructors.scm |netcat $HOST $PORT
 cat utilities.scm |netcat $HOST $PORT
 cat cgw-wire.scm |netcat $HOST $PORT
 cat cgw-simple.scm |netcat $HOST $PORT
+cat cgw-fan.scm |netcat $HOST $PORT
 cat cgw-filter.scm |netcat $HOST $PORT
 cat cgw-predicate.scm |netcat $HOST $PORT
