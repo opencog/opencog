04cfaee8e5f61d452d96c46686955f8d14b7dc7b
Linas Vepstas <linasvepstas@gmail.com>
2008-09-24 20:17:18 -0500

    resume programming work on disjuncts

--- src/nlp/scm/disjunct.scm
+++ src/nlp/scm/disjunct.scm
@@ -8,9 +8,9 @@ scm
 ; Copyright (c) 2008 Linas Vepstas <linasvepstas@gmail.com>
 ;
 ; =============================================================
+; Callback-style disjuncts
 ;
 (define (prt-stuff h) (display h) #f)
-
 
 ; callback-style disjunct stuff.
 (define (cb-get-sentence-disjuncts sent-node)
@@ -31,31 +31,40 @@ scm
 ; Do it callback-style
 ; (cog-map-type cb-get-sentence-disjuncts 'SentenceNode)
 
-;===========================
+; ===========================
+; List-style disjuncts
+;
 ; Return a list of all of the link-grammar links the word particpates in
 (define (get-lgl word)
-	
+	(display "duuude: ")
+	;(cog-
+	(display word)
+	(newline)
 	'()
 )
 
+; Given a single sentence, process the disjuncts for that sentence
 (define (list-get-sentence-disjuncts sent-node)
+
 	; Get list of all words in the sentence
 	(define (get-word-list sent)
 		(cog-chase-link 'SentenceLink 'ConceptNode sent)
 	)
 
-	(display (get-word-list sent-node))
+	(for-each get-lgl (get-word-list sent-node))
 )
 
-; List-style disjuncts
+; Given a list of sentences, process each sentence to extract disjuncts
 (define (list-get-sentence-list-disjuncts sent-list)
+
+	; Loop over a list of sentences, getting disjuncts for each sentence.
 	(for-each list-get-sentence-disjuncts sent-list)
 )
 
 ; Do it list-style
 (list-get-sentence-list-disjuncts (cog-get-atoms 'SentenceNode))
 
-;===========================
+; ===========================
 ; wire-style
 
 (define (wire-it)
@@ -63,15 +72,11 @@ scm
 	; Create a wire to transport a stream of sentences
 	(define sentences (make-wire))
 
-	; More wires to transport various bits and peices.
+	; More wires to transport various bits and pieces.
 	(define word-instances (make-wire))
 	(define word-nodes (make-wire))
-	(define wa (make-wire))
-	(define wb (make-wire))
-	(define misc-a (make-wire))
-	(define misc-b (make-wire))
-	(define misc-c (make-wire))
-	(define misc-d (make-wire))
+	(define word-instance-pairs (make-wire))
+	(define lg-connectors (make-wire))
 
 	; Put the sentences on the wire
 	(cgw-source-atoms sentences 'SentenceNode)
@@ -82,23 +87,13 @@ scm
 	; Get the word-nodes associated with the word-instances.
 	; (cgw-follow-link word-instances word-nodes 'ReferenceLink 'WordNode)
 	
-	; (cgw-filter-incoming word-instances misc-a 'ListLink)
-	; (cgw-follow-link-pos word-instances misc-b 'ListLink 0 1)
-	; (wire-fan-out misc-a misc-b misc-c)
-	; (wire-fan-in misc-a misc-d misc-c)
-	; (cgw-splitter misc-a misc-b word-instances 'ListLink 0 0)
-	; (cgw-splitter misc-a word-instances misc-b 'ListLink 0 1)
-	; (cgw-splitter misc-a misc-b misc-c 'ListLink 1 0)
-	;
-	(wire-fan-out word-instances wa wb)
-	(cgw-splitter misc-a wa misc-b 'ListLink 0 1)
-	(cgw-splitter misc-c wb misc-b 'ListLink 0 1)
+	(cgw-filter-incoming word-instances word-instance-pairs 'ListLink)
+
+	(cgw-follow-link word-instance-pairs lg-connectors
+		'EvaluationLink 'LinkGrammarRelationshipNode)
 
 	; print things out
-	; (wire-probe "links" misc-a)
-	(wire-probe "back" misc-c)
-	; (display (wire-drain-count misc-b))
-	; (wire-drain misc-d)
+	(wire-probe "conns" lg-connectors)
 )
 
 ;===========================
