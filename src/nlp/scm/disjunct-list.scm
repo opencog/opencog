1a0e43b7e83bbb2e666b13ae919a67911d5e9ba9
Linas Vepstas <linasvepstas@gmail.com>
2008-09-25 18:17:48 -0500

    split the list processing bit into its own file.

--- src/nlp/scm/disjunct-list.scm
+++ src/nlp/scm/disjunct-list.scm
@@ -1,37 +1,17 @@
 scm
 ; 
-; disjunct.scm
+; disjunct-list.scm
 ;
 ; Build lists of link-grammar disjuncts; update the SQL
 ; database counts with the results.
 ;
+; This is also a part of a test exploring different coding styles.
+; Similar fuunctionality is (paritally) implemented in disjunct.scm,
+; but using a different approach.
+;
 ; Copyright (c) 2008 Linas Vepstas <linasvepstas@gmail.com>
 ;
 ; =============================================================
-; Callback-style disjuncts
-;
-(define (prt-stuff h) (display h) #f)
-
-; callback-style disjunct stuff.
-(define (cb-get-sentence-disjuncts sent-node)
-
-	(define (dj-per-word word)
-		(display word)
-		; (display (cog-incoming-set word))
-	
-		#f
-	)
-	
-	; loop over all the words in the sentence
-	(cog-map-chase-link 'SentenceLink 'ConceptNode "" "" dj-per-word sent-node)
-	
-	#f
-)
-
-; Do it callback-style
-; (cog-map-type cb-get-sentence-disjuncts 'SentenceNode)
-
-; ===========================
 ; List-style disjuncts
 ;
 ; Return a list of all of the link-grammar links the word particpates in
@@ -46,11 +26,13 @@ scm
 	(map get-rel (cog-filter-incoming 'ListLink word))
 )
 
+; ---------------------------------------------------------------------
 ; Get list of all words in the sentence
 (define (get-word-list sent)
 	(reverse! (cog-chase-link 'SentenceLink 'ConceptNode sent))
 )
 
+; ---------------------------------------------------------------------
 ; Given a word, and the sentence in which the word appears, return
 ; a list of the ling-grammar relations in which the word appears. 
 ; The relations are sorted in sentence word-order.
@@ -84,6 +66,7 @@ scm
 	)
 )
 
+; ---------------------------------------------------------------------
 ; Return a string listing all of the link-grammar relations for the
 ; word in the sentence. This string will be in proper sorted order,
 ; according to the appearence of the words in the sentence word-order.
@@ -107,6 +90,7 @@ scm
 	(mk-dj-string (map get-name sorted-rels) "")
 )
 
+; ---------------------------------------------------------------------
 ;
 ; Process the disjuncts for a single word in a sentence
 ;
@@ -123,6 +107,7 @@ scm
 (display "\n")
 )
 
+; ---------------------------------------------------------------------
 ; Given a single sentence, process the disjuncts for that sentence
 (define (list-get-sentence-disjuncts sent-node)
 
@@ -134,6 +119,7 @@ scm
 	(for-each mk-disjunct (get-word-list sent-node))
 )
 
+; ---------------------------------------------------------------------
 ; Given a list of sentences, process each sentence to extract disjuncts
 (define (list-get-sentence-list-disjuncts sent-list)
 
@@ -141,43 +127,12 @@ scm
 	(for-each list-get-sentence-disjuncts sent-list)
 )
 
+
+; ---------------------------------------------------------------------
 ; Do it list-style
 (define (list-it) 
 	(list-get-sentence-list-disjuncts (cog-get-atoms 'SentenceNode))
 )
 
-
-; ===========================
-; wire-style
-
-(define (wire-it)
-
-	; Create a wire to transport a stream of sentences
-	(define sentences (make-wire))
-
-	; More wires to transport various bits and pieces.
-	(define word-instances (make-wire))
-	(define word-nodes (make-wire))
-	(define word-instance-pairs (make-wire))
-	(define lg-connectors (make-wire))
-
-	; Put the sentences on the wire
-	(cgw-source-atoms sentences 'SentenceNode)
-
-	; Get the incoming links.
-	(cgw-follow-link sentences word-instances 'SentenceLink 'ConceptNode)
-	
-	; Get the word-nodes associated with the word-instances.
-	; (cgw-follow-link word-instances word-nodes 'ReferenceLink 'WordNode)
-	
-	(cgw-filter-incoming word-instances word-instance-pairs 'ListLink)
-
-	(cgw-follow-link word-instance-pairs lg-connectors
-		'EvaluationLink 'LinkGrammarRelationshipNode)
-
-	; print things out
-	(wire-probe "conns" lg-connectors)
-)
-
-;===========================
+; =====================================================================
 
