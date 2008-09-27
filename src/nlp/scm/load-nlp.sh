460aaa59daf8861c8736a66c013b403c7033c783
Linas Vepstas <linasvepstas@gmail.com>
2008-09-27 13:41:27 -0500

    add utility to load the NLP scripts, too

---
+++ src/nlp/scm/load-nlp.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+#
+# load-nlp.sh
+#
+# Initialize the scheme system with assorted nlp utilities
+#
+
+HOST=localhost
+PORT=17001
+
+cat nlp-utils.scm |netcat $HOST $PORT
+cat disjunct-list.scm |netcat $HOST $PORT
+
