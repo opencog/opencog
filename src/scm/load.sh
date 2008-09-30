d48e393a8a1beadfd4b6862e6abb967472aab06d
Linas Vepstas <linasvepstas@gmail.com>
2008-09-29 19:55:59 -0500

    misc hacks.

--- src/scm/load.sh
+++ src/scm/load.sh
@@ -8,7 +8,6 @@
 HOST=localhost
 PORT=17001
 
-# echo "(use-modules (ice-9 session))"  |netcat $HOST $PORT
 cat type_constructors.scm |netcat $HOST $PORT
 cat utilities.scm |netcat $HOST $PORT
 cat cgw-wire.scm |netcat $HOST $PORT
@@ -17,3 +16,7 @@ cat cgw-fan.scm |netcat $HOST $PORT
 cat cgw-filter.scm |netcat $HOST $PORT
 cat cgw-bidi.scm |netcat $HOST $PORT
 cat cgw-predicate.scm |netcat $HOST $PORT
+
+# Session doesn't work over telnet.
+# echo -e "scm\n(use-modules (ice-9 session))\n.\nexit\n"  |netcat $HOST $PORT
+
