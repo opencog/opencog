3337d30f4af2251175c38d28f86ab276d116dd70
Linas Vepstas <linasvepstas@gmail.com>
2008-12-08 19:17:08 -0600

    initial rough-in for a generic index class, plus an implementation.

---
+++ opencog/atomspace/StringIndex.h
@@ -0,0 +1,53 @@
+/*
+ * opencog/atomspace/StringIndex.h
+ *
+ * Copyright (C) 2008 Linas Vepstas <linasvepstas@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_STRINGINDEX_H
+#define _OPENCOG_STRINGINDEX_H
+
+#include <opencog/atomspace/AtomIndex.h>
+
+namespace opencog
+{
+
+/**
+ * This class provides a simple, generic interface for maintaining 
+ * arbitrary indexes of Atoms. Indexes are used whenever a system needs
+ * rapid lookup of atoms having some particular type or property.
+ *
+ * A secondary goal of this interface class is to allow different
+ * implementations to use the same interface: implementations as
+ * linked-lists, rb-trees, hash tables, etc.
+ *
+ * Typically, Value will be Handle, possibly PredicateEvaluator*.
+ * The Key will typically be an int, string, etc.
+ */
+class StringIndex:
+	public AtomIndex<const char *,Handle>
+{
+public:
+		virtual void insert(const char *, Handle);
+		virtual Handle get(const char *);
+		virtual void remove(const char *);
+};
+
+} //namespace opencog
+
+#endif // _OPENCOG_STRINGINDEX_H
