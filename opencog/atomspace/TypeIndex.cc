95ae1d7e15d4b386a1a46c1186dd916da726f0fe
Linas Vepstas <linasvepstas@gmail.com>
2008-12-09 16:49:02 -0600

    initial checkin of a type index.

---
+++ opencog/atomspace/TypeIndex.cc
@@ -0,0 +1,194 @@
+/*
+ * opencog/atomspace/TypeIndex.cc
+ *
+ * Copyright (C) 2008 Linas Vepstas <linasvepstas@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <opencog/atomspace/TypeIndex.h>
+#include <opencog/atomspace/ClassServer.h>
+#include <opencog/atomspace/type_codes.h>
+#include <opencog/atomspace/HandleEntry.h>
+
+using namespace opencog;
+
+void TypeIndex::insert(int i, Handle h)
+{
+	std::set<Handle> &s = idx.at(i);
+	s.insert(h);
+}
+
+Handle TypeIndex::get(int i) const
+{
+	return Handle::UNDEFINED;
+}
+
+void TypeIndex::remove(int i, Handle h)
+{
+	std::set<Handle> &s = idx.at(i);
+	s.erase(h);
+}
+
+size_t TypeIndex::size(void) const
+{
+	size_t cnt = 0;
+	std::vector<std::set<Handle> >::const_iterator s;
+	for (s = idx.begin(); s != idx.end(); s++)
+	{
+		cnt += s->size();
+	}
+	return cnt;
+}
+
+void TypeIndex::remove(bool (*filter)(Handle))
+{
+	std::vector<std::set<Handle> >::iterator s;
+	for (s = idx.begin(); s != idx.end(); s++)
+	{
+		std::set<Handle>::iterator i, j;
+	
+		i = s->begin();
+		while (i != s->end())
+		{
+			j = i;
+			i++;
+			if (filter(*j))
+				s->erase(*j);
+		}
+	}
+}
+
+void TypeIndex::resize(size_t sz)
+{
+	idx.resize(sz);
+}
+
+// ================================================================
+
+HandleEntry * TypeIndex::getHandleSet(Type type, bool subclass) const
+{
+	iterator it = begin(type, subclass);
+	iterator itend = end();
+
+	HandleEntry *he = NULL;
+	while (it != itend)
+	{
+		HandleEntry *nhe = new HandleEntry(*it);
+		nhe->next = he;
+		he = nhe;
+		it++;
+	}
+	return he;
+}
+
+// ================================================================
+
+TypeIndex::iterator TypeIndex::begin(Type t, bool sub) const
+{
+	iterator it(t, sub);
+	it.send = idx.end();
+
+	it.s = idx.begin();
+	it.currtype = 0;
+	while (it.s != it.send)
+	{
+		// Find the first type which is a subtype, and start iteration there.
+		if ((it.type == it.currtype) || 
+		    (sub && (ClassServer::isAssignableFrom(it.type, it.currtype))))
+		{
+			it.se = it.s->begin();
+			if (it.se != it.s->end()) return it;
+		}
+		it.currtype++;
+		it.s++;
+	}
+
+	return it;
+}
+
+TypeIndex::iterator TypeIndex::end(void) const
+{
+	iterator it(NOTYPE, false);
+	it.se = idx.at(NOTYPE).end();
+	it.s = idx.end();
+	it.send = idx.end();
+	it.currtype = NOTYPE;
+	return it;
+}
+
+TypeIndex::iterator::iterator(Type t, bool sub)
+{
+	type = t;
+	subclass = sub;
+}
+
+TypeIndex::iterator& TypeIndex::iterator::operator=(iterator v)
+{
+	s = v.s;
+	send = v.send;
+	se = v.se;
+	currtype = v.currtype;
+	type = v.type;
+	subclass = v.subclass;
+	return *this;
+}
+
+Handle TypeIndex::iterator::operator*(void)
+{
+	if (s == send) return Handle::UNDEFINED;
+	return *se;
+}
+
+bool TypeIndex::iterator::operator==(iterator v)
+{
+	if ((v.s == v.send) && (s == send)) return true;
+	return v.se == se;
+}
+
+bool TypeIndex::iterator::operator!=(iterator v)
+{
+	if ((v.s == v.send) && (s != send)) return v.se != se;
+	if ((v.s != v.send) && (s == send)) return v.se != se;
+	return false;
+}
+
+TypeIndex::iterator& TypeIndex::iterator::operator++(int i)
+{
+	if (s == send) return *this;
+
+	se++;
+	if (se == s->end())
+	{
+		do
+		{
+			s++;
+			currtype++;
+
+			// Find the first type which is a subtype, and start iteration there.
+			if ((type == currtype) || 
+			    (subclass && (ClassServer::isAssignableFrom(type, currtype))))
+			{
+				se = s->begin();
+				if (se != s->end()) return *this;
+			}
+		} while (s != send);
+	}
+
+	return *this;
+}
+
+// ================================================================
---
+++ opencog/atomspace/TypeIndex.h
@@ -0,0 +1,70 @@
+/*
+ * opencog/atomspace/TypeIndex.h
+ *
+ * Copyright (C) 2008 Linas Vepstas <linasvepstas@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_TYPEINDEX_H
+#define _OPENCOG_TYPEINDEX_H
+
+#include <set>
+#include <vector>
+
+#include <opencog/atomspace/FixedIntegerIndex.h>
+#include <opencog/atomspace/Handle.h>
+#include <opencog/atomspace/types.h>
+
+namespace opencog
+{
+class HandleEntry;
+
+/**
+ * Implements an integer index as an RB-tree (C++ map)
+ */
+class TypeIndex:
+	public FixedIntegerIndex
+{
+	public:
+		HandleEntry* getHandleSet(Type type, bool subclass) const;
+
+		class iterator
+		{
+			friend class TypeIndex;
+			public:
+				iterator(Type, bool);
+				iterator& operator++(int);
+				iterator& operator=(iterator);
+				bool operator==(iterator);
+				bool operator!=(iterator);
+				Handle operator*(void);
+			private:
+				Type type;
+				bool subclass;
+				std::vector<std::set<Handle> >::const_iterator s;
+				std::vector<std::set<Handle> >::const_iterator send;
+				Type currtype;
+				std::set<Handle>::const_iterator se;
+		};
+
+		iterator begin(Type, bool) const;
+		iterator end(void) const;
+};
+
+} //namespace opencog
+
+#endif // _OPENCOG_TYPEINDEX_H
