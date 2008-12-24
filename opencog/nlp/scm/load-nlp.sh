4d8835a25e5607dc03318579a891932ec2ebc186
Linas Vepstas <linasvepstas@gmail.com>
2008-12-24 15:51:19 -0600

    Since the scheme interpreter is now single-threaded, the sleep work-arounds are no longer needed.

--- opencog/nlp/scm/load-nlp.sh
+++ opencog/nlp/scm/load-nlp.sh
@@ -9,12 +9,8 @@ HOST=localhost
 PORT=17001
 
 cat type-definitions.scm |netcat -q0 $HOST $PORT
-sleep 2;
 cat nlp-utils.scm        |netcat -q0 $HOST $PORT
-sleep 2;
 cat disjunct-list.scm    |netcat -q0 $HOST $PORT
-sleep 3;
 cat stats-collection.scm |netcat -q0 $HOST $PORT
-sleep 3;
 cat file-process.scm     |netcat -q0 $HOST $PORT
 
--- opencog/scm/load.sh
+++ opencog/scm/load.sh
@@ -10,29 +10,16 @@ PORT=17001
 
 echo -e "scm\n(turn-on-debugging)\n.\nexit\n"  |netcat -q0 $HOST $PORT
 
-# the sleeps are to work-around a guile bug in 1.8.5 involving
-# multi-threaded defines. The current opencog design is highly
-# multi-threaded just right now. :-/
-sleep 1
-
 cat type_constructors.scm |netcat -q0 $HOST $PORT
-sleep 4;
 cat utilities.scm         |netcat -q0 $HOST $PORT
-sleep 3;
 cat file-utils.scm         |netcat -q0 $HOST $PORT
 
 # The cgw code is not being used at this time. 
-# sleep 2;
 # cat cgw-wire.scm          |netcat -q0 $HOST $PORT
-# sleep 2;
 # cat cgw-simple.scm        |netcat -q0 $HOST $PORT
-# sleep 2;
 # cat cgw-fan.scm           |netcat -q0 $HOST $PORT
-# sleep 2;
 # cat cgw-filter.scm        |netcat -q0 $HOST $PORT
-# sleep 2;
 # cat cgw-bidi.scm          |netcat -q0 $HOST $PORT
-# sleep 2;
 # cat cgw-predicate.scm     |netcat -q0 $HOST $PORT
 
 # Session doesn't work over telnet.
