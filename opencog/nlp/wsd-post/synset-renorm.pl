7940adf799a84100769e12b4a71b3f703b8cfd76
Linas Vepstas <linasvepstas@gmail.com>
2008-12-29 13:25:39 -0600

    move the handy-dandy utility funcs over to the generic wordnet package

--- opencog/nlp/wsd-post/synset-renorm.pl
+++ opencog/nlp/wsd-post/synset-renorm.pl
@@ -25,68 +25,6 @@ my $wn_dictionary_location = "/usr/share/wordnet";
 my $wn = WordNet::QueryData->new($wn_dictionary_location);
 my $sk = WordNet::SenseKey->new($wn_dictionary_location);
 
-# get_synset -- return a wordnet synset.
-# Given a sense key as input, this will
-# return a list of sense keys in the synset.
-sub get_synset
-{
-	my ($sense_key) = @_;
-	my $sense_str = $sk->get_sense($sense_key);
-
-	my @synset = $wn->querySense($sense_str, "syns");
-	my @keyset = ();
-	foreach (@synset)
-	{
-		my $lempos = $_;
-		my $off = $wn->offset($lempos);
-		my $skey = $sk->get_sense_key($off, $lempos);
-		push @keyset, $skey;
-	}
-
-	return @keyset;
-}
-
-# ----------------------------------------------------------
-
-# get_lemma_sense -- get matching lemma from a synset.
-# Return an alternate sense key that belongs to the same
-# synset ass the input sense key, but has the the lemmatized
-# form $lemma at its root.
-#
-# Thus, for example:
-#
-#      get_lemma_sense("join#v", "connect%2:42:02::");
-#
-# will return "join%2:42:01", because "join%2:42:01" is in the same
-# synset as "connect%2:42:02::", but has "join" as its root. 
-#
-sub get_lemma_sense
-{
-	my ($lemma, $sense) = @_;
-
-	# strip off the part-of-speech marker from the lemma.
-	$lemma =~ m/([\w\.]+)#/;
-	if (defined($1))
-	{
-		$lemma = $1;
-	}
-
-	# Loop over the synset, looking for a matching form.
-	my @synset = get_synset($sense);
-	foreach (@synset)
-	{
-		my $altsense = $_;
-		$altsense =~ m/([\w\.]+)%/;
-		if ($1 eq $lemma)
-		{
-			return $altsense;
-		}
-	}
-
-	my $notfound; # this is undefined!
-	return $notfound;
-}
-
 # ----------------------------------------------------------
 
 sub update_record
@@ -158,7 +96,7 @@ for (my $i=0; $i<$select->rows; $i++)
 	}
 
 	# Find the canonical sense for this word.
-	my $canon_sense = get_lemma_sense($lemma, $sense);
+	my $canon_sense = $sk->get_canonical_sense($wn, $lemma, $sense);
 	if (!defined($canon_sense))
 	{
 		next;
