84f1748d7ed5600417b521b22b0c96f5a8e78490
Linas Vepstas <linasvepstas@gmail.com>
2008-12-28 22:43:15 -0600

    start working out the synset for a given sense.

--- opencog/nlp/wsd-post/synset-renorm.pl
+++ opencog/nlp/wsd-post/synset-renorm.pl
@@ -1,21 +1,27 @@
-
+#! /usr/bin/env perl
 #
-# sysnset-renorm.pl
+# synset-renorm.pl
 #
 
 use WordNet::QueryData;
 use WordNet::SenseKey;
 use strict;
 
-my $lempos = "run#v#2";
-
 my $wn_dictionary_location = "/usr/share/wordnet";
 my $wn = WordNet::QueryData->new($wn_dictionary_location);
-
-my $off = $wn->offset($lempos);
-
 my $sk = WordNet::SenseKey->new($wn_dictionary_location);
-my $skey = $sk->get_sense_key($off, $lempos);
 
-print "found the sense $skey for $lempos\n";
+my $sense_key = "run_away%2:38:00::";
+my $sense_str = $sk->get_sense($sense_key);
+
+my @synset = $wn->querySense($sense_str, "syns");
+foreach (@synset)
+{
+	my $lempos = $_;
+	my $off = $wn->offset($lempos);
+	my $skey = $sk->get_sense_key($off, $lempos);
+	print "found the sense $skey for $lempos\n";
+}
+
+
 
