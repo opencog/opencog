582c8aa1978296c46a80f8a6e1a7a566fda35b01
Linas Vepstas <linasvepstas@gmail.com>
2008-12-29 10:13:10 -0600

    create a synset subrutine

--- opencog/nlp/wsd-post/synset-renorm.pl
+++ opencog/nlp/wsd-post/synset-renorm.pl
@@ -11,17 +11,31 @@ my $wn_dictionary_location = "/usr/share/wordnet";
 my $wn = WordNet::QueryData->new($wn_dictionary_location);
 my $sk = WordNet::SenseKey->new($wn_dictionary_location);
 
-my $sense_key = "run_away%2:38:00::";
-my $sense_str = $sk->get_sense($sense_key);
-
-my @synset = $wn->querySense($sense_str, "syns");
-foreach (@synset)
+# Given a since sense key as input, this will
+# return a list of sense keys in the synset.
+sub get_synset
 {
-	my $lempos = $_;
-	my $off = $wn->offset($lempos);
-	my $skey = $sk->get_sense_key($off, $lempos);
-	print "found the sense $skey for $lempos\n";
+	my ($sense_key) = @_;
+	my $sense_str = $sk->get_sense($sense_key);
+
+	my @synset = $wn->querySense($sense_str, "syns");
+	my @keyset = ();
+	foreach (@synset)
+	{
+		my $lempos = $_;
+		my $off = $wn->offset($lempos);
+		my $skey = $sk->get_sense_key($off, $lempos);
+		push @keyset, $skey;
+	}
+
+	return @keyset;
 }
 
 
+my $sense_key = "run_away%2:38:00::";
+my @synset = get_synset($sense_key);
 
+foreach (@synset)
+{
+	print "its $_\n";
+}
