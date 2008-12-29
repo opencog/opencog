c63cca1d049ebb72bae131506ce2bff487a417b7
Linas Vepstas <linasvepstas@gmail.com>
2008-12-29 10:58:17 -0600

    add code to start looping over the database

--- opencog/nlp/wsd-post/synset-renorm.pl
+++ opencog/nlp/wsd-post/synset-renorm.pl
@@ -2,16 +2,28 @@
 #
 # synset-renorm.pl
 #
+# Rework the database contents so as to combine multiple synset entries
+# into one. This step is needed in part because the original WSD code
+# handled synsets in a buggy way ... which still needs to be fixed.
+
+use utf8;
+binmode STDIN, ':encoding(UTF-8)';
+binmode STDOUT, ':encoding(UTF-8)';
+
+use strict;
+use warnings;
 
 use WordNet::QueryData;
 use WordNet::SenseKey;
-use strict;
+use DBI;
 
+# ----------------------------------------------------------
 my $wn_dictionary_location = "/usr/share/wordnet";
 my $wn = WordNet::QueryData->new($wn_dictionary_location);
 my $sk = WordNet::SenseKey->new($wn_dictionary_location);
 
-# Given a since sense key as input, this will
+# get_synset -- return a wordnet synset.
+# Given a sense key as input, this will
 # return a list of sense keys in the synset.
 sub get_synset
 {
@@ -29,6 +41,36 @@ sub get_synset
 	}
 
 	return @keyset;
+}
+
+# ----------------------------------------------------------
+
+my $dbh = DBI->connect('DBI:Pg:dbname=lexat', 'linas', 'asdf')
+   or die "Couldn't connect to database: " . DBI->errstr;
+
+my $select = $dbh->prepare('SELECT * FROM DisjunctSenses WHERE count > 0.0;')
+	or die "Couldn't prepare statement: " . $dbh->errstr;
+
+$select->execute()
+	or die "Couldn't execute statement: " . $select->errstr;
+
+for (my $i=0; $i<$select->rows; $i++)
+{
+	my ($sense, $infword, $disjunct, $count, $lp) = $select->fetchrow_array();
+
+	# Extract the lemma form from the sense key, and from the word.
+	$sense =~ m/(\w+)%(\d+)/;
+	my $sense_lemma = $1;
+	my $pos = $2;
+	$infword =~ m/(\w+)\.(\w+)/;
+	my $word = $1;
+	my $flect = $2;
+
+	my @forms = $wn->validForms($word);
+	my $lemma = $forms[0];
+
+print "duude $sense, $infword  $sense_lemma $word $lemma\n";
+
 }
 
 
