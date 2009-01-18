ca529b80acbae62d92a36ad63e49363a373a1310
Linas Vepstas <linasvepstas@gmail.com>
2009-01-18 15:00:09 -0600

    Cleanup the WSD processing scripts.

--- opencog/nlp/scm/load-nlp.sh
+++ opencog/nlp/scm/load-nlp.sh
@@ -13,4 +13,5 @@ cat nlp-utils.scm        |netcat -q0 $HOST $PORT
 cat disjunct-list.scm    |netcat -q0 $HOST $PORT
 cat stats-collection.scm |netcat -q0 $HOST $PORT
 cat file-process.scm     |netcat -q0 $HOST $PORT
+cat A.scm                |netcat -q0 $HOST $PORT
 
