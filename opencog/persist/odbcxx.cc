ca90bf2cd1ae62555d4c9112b1bc844c16277db7
Linas Vepstas <linasvepstas@gmail.com>
2009-02-12 10:06:00 -0600

    Add license boilerplate.

--- opencog/persist/odbcxx.cc
+++ opencog/persist/odbcxx.cc
@@ -12,6 +12,22 @@
  * Copyright (c) 2002,2008 Linas Vepstas <linas@linas.org>
  * created by Linas Vepstas  March 2002
  * ported to C++ March 2008
+ *
+ * LICENSE:
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifdef HAVE_SQL_STORAGE
