09ad80403c0b28ba51aae4dc5f657b7791fb56ce
Linas Vepstas <linasvepstas@gmail.com>
2009-02-14 22:07:16 -0600

    tweak to use defined interfaces

--- opencog/guile/SchemeAdHoc.cc
+++ opencog/guile/SchemeAdHoc.cc
@@ -32,7 +32,7 @@ SCM SchemeSmob::ss_ad_hoc(SCM command, SCM optargs)
 	{
 		WordSenseProcessor wsp;
 		wsp.use_threads(false);
-		wsp.run_no_delay(static_cast<CogServer *>(&server())); // XXX What an ugly interface. Alas.
+		wsp.run_no_delay(&cogserver()); // XXX What an ugly interface. Alas.
 		return SCM_BOOL_T;
 	}
 
@@ -43,8 +43,7 @@ SCM SchemeSmob::ss_ad_hoc(SCM command, SCM optargs)
 		// XXX we should also allow opt-args to be a list of handles
 		Handle h = verify_handle(optargs, "cog-ad-hoc do-implication");
 
-		CogServer *s = static_cast<CogServer *>(&server());
-		AtomSpace *as = s->getAtomSpace();
+		AtomSpace *as = &atomspace();
 
 		PatternMatch pm;
 		pm.set_atomspace(as);
