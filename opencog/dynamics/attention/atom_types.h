8d2890c27f9515125daa7ca3d7996cc2d9e00654
Welter Luigi <welterls@gmail.com>
2009-03-12 18:17:45 -0300

    reverted changes to atom types definitions and keep them all in atomspace script just as they were before merging with classserver branch

--- opencog/dynamics/attention/atom_types.h
+++ opencog/dynamics/attention/atom_types.h
@@ -2,9 +2,4 @@
 #include <opencog/atomspace/types.h>
 namespace opencog
 {
-extern opencog::Type HEBBIAN_LINK;
-extern opencog::Type ASYMMETRIC_HEBBIAN_LINK;
-extern opencog::Type SYMMETRIC_HEBBIAN_LINK;
-extern opencog::Type INVERSE_HEBBIAN_LINK;
-extern opencog::Type SYMMETRIC_INVERSE_HEBBIAN_LINK;
 } // namespace opencog
