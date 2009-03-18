ddcfeb2c31f9e5ccd77baa330afae52fc9e8a6b2
Linas Vepstas <linasvepstas@gmail.com>
2009-03-17 20:54:35 -0500

    Update README

---
+++ opencog/nlp/triples/load.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+#
+# Load all required scm bits and prieces in order to extract triples
+#
+# Load the generic scm bits
+pushd ../../scm
+./load3.sh
+popd
+
+# Load the NLP scm bits
+pushd ../scm
+./load-nlp-3.sh
+popd
+
+# Load the preposition dictionary
+cat prep-maps.scm | netcat localhost 17003
+
+# Load add rules
+cat rules.txt | ./rules-to-implications.pl | netcat localhost 17003
+
+# Load some previously parsed sentences.
+cat o | netcat localhost 17003
+
