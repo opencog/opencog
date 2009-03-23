a2c6b10954619022ca0362c13eaa5b6f621ff5ff
Nil Geisweiller <ngeiswei@gmail.com>
2009-03-23 15:31:37 +0200

    added AutomatedSystemTest

---
+++ opencog/embodiment/AutomatedSystemTest/TestParameters.h
@@ -0,0 +1,24 @@
+/**
+ * TestParameters.h
+ *
+ * Author: Welter Luigi
+ */
+
+#ifndef _TEST_PARAMETERS_H_
+#define _TEST_PARAMETERS_H_
+
+#include "SystemParameters.h"
+
+namespace AutomatedSystemTest {
+
+class TestParameters : public Control::SystemParameters {
+
+    public:
+
+        TestParameters();
+        ~TestParameters();
+
+}; // class
+}  // namespace
+
+#endif
