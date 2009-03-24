b168fd90385215f36d7e15c431227a4edfb4c105
Nil Geisweiller <ngeiswei@gmail.com>
2009-03-24 07:20:54 +0200

    added LS_CogServer that is identical to LS but derive CogServer

---
+++ opencog/embodiment/Learning/LearningServer/LS_CogServerExecutable.cc
@@ -0,0 +1,56 @@
+#include <SystemParameters.h>
+#include <exception>
+#include <cstdlib>
+#include "util/files.h"
+#include "util/Logger.h"
+#include "LS_CogServer.h"
+
+using namespace LearningServer;
+using namespace opencog;
+
+void ls_unexpected_handler(){
+    throw;
+}
+
+int main(int argc, char *argv[]) {
+  
+    Control::SystemParameters parameters;
+
+    // if exists load file with configuration parameters 
+    // IMPORTANT: this file should be the same for all executables that create
+    // a systemParameter object. 
+    if(fileExists(parameters.get("CONFIG_FILE").c_str())){
+    	parameters.loadFromFile(parameters.get("CONFIG_FILE"));
+    }
+
+    // setting unexpected handler in case a different exception from the
+    // especified ones is throw in the code
+    std::set_unexpected(ls_unexpected_handler);
+
+    server(LS::derivedCreateInstance);
+
+    //LS* ls = new LS(parameters.get("LS_ID"),
+    //              parameters.get("LS_IP"),
+    //              std::atoi(parameters.get("LS_PORT").c_str()),
+    //              parameters);
+
+    static_cast<LS&>(server()).init(parameters.get("LS_ID"),
+                                    parameters.get("LS_IP"),
+                                    std::atoi(parameters.get("LS_PORT").c_str()),
+                                    parameters);
+
+    try  {
+        static_cast<LS&>(server()).NetworkElement::serverLoop();
+
+    } catch(std::bad_alloc){
+        logger().log(opencog::Logger::ERROR, "LSExec - LS raised a bad_alloc exception.");
+        
+    } catch(...) {
+        logger().log(opencog::Logger::ERROR, 
+                "LSExec - An exceptional situation occured. Check log for more information.");
+    }
+
+    //delete ls;
+
+    return (0);
+}
