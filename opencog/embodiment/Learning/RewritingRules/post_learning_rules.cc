3cacdeb0037382b66c95efe75f7481cf5106f681
Nil Geisweiller <ngeiswei@gmail.com>
2009-04-01 02:02:17 +0300

    fixed bug related to trick learned badly typed because containing several empty and_seq

--- opencog/embodiment/Learning/RewritingRules/post_learning_rules.cc
+++ opencog/embodiment/Learning/RewritingRules/post_learning_rules.cc
@@ -22,29 +22,39 @@
 #include "post_learning_rules.h"
 #include "PetComboVocabulary.h"
 
-namespace reduct {
+namespace reduct
+{
 
-  using namespace PetCombo;
+using namespace PetCombo;
 
-  typedef combo_tree::sibling_iterator sib_it;
-  typedef combo_tree::iterator pre_it;
+typedef combo_tree::sibling_iterator sib_it;
+typedef combo_tree::iterator pre_it;
 
-  //add a drop action in front of a grab action
-  void post_learning_drop_before_grab::operator()(combo_tree& tr,
-						  combo_tree::iterator it) const {
-    if(*it==id::sequential_exec || *it==id::sequential_or
-       || *it==id::sequential_and) {
-      for(sib_it sib = it.begin(); sib != it.end(); ++sib) {
-	if(*sib==instance(id::grab)) {
-	  sib_it pre_sib = tr.previous_sibling(sib);
-	  if(tr.is_valid(pre_sib)) {
-	    if(*pre_sib!=instance(id::drop))
-	      sib = tr.insert(sib, instance(id::drop));
-	  }
-	  else sib = tr.insert(sib, instance(id::drop));
-	}
-      }
+//add a drop action in front of a grab action
+void post_learning_drop_before_grab::operator()(combo_tree& tr,
+                                                combo_tree::iterator it) const
+{
+    if (*it == id::sequential_exec || *it == id::sequential_or
+            || *it == id::sequential_and) {
+        for (sib_it sib = it.begin(); sib != it.end(); ++sib) {
+            if (*sib == instance(id::grab)) {
+                sib_it pre_sib = tr.previous_sibling(sib);
+                if (tr.is_valid(pre_sib)) {
+                    if (*pre_sib != instance(id::drop))
+                        sib = tr.insert(sib, instance(id::drop));
+                } else sib = tr.insert(sib, instance(id::drop));
+            }
+        }
     }
-  }
+}
+
+//add action_success at child of any empty and_seq
+void post_learning_empty_and_seq::operator()(combo_tree& tr,
+                                             combo_tree::iterator it) const
+{
+    if (*it == id::sequential_and && it.is_childless()) {
+        tr.append_child(it, id::action_success);
+    }
+}
 
 }//~namespace reduct
