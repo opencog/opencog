10b17f2c8cc06ab93a76b0f45985172ae6fea0c6
Linas Vepstas <linasvepstas@gmail.com>
2009-04-17 21:11:00 -0500

    take an initial stab at creating preposition maps automatically.

--- opencog/nlp/triples/prep-maps.scm
+++ opencog/nlp/triples/prep-maps.scm
@@ -2,8 +2,36 @@ scm
 ;
 ; prep-maps.scm
 ;
-; (static) Map of verb-preposition pairs to a single term
+; Create maps linking verb-preposition pairs to a single term
+; These are needed to output verb-prep-style triples.
 ;
+; For example, given the input 
+;    (make-verb-prep (WordNode "capital")
+;       (DefinedLinguisticRelationshipNode "of"))
+;
+; this will generate the following structure:
+;
+;    (EvaluationLink (stv 1.0 1.0)
+;       (DefinedLinguisticRelationshipNode "capital_of")
+;       (ListLink
+;          (WordNode "capital")
+;          (DefinedLinguisticRelationshipNode "of")
+;       )
+;    )
+
+
+(define (make-verb-prep verb prep)
+	(EvaluationLink
+		(DefinedLinguisticRelationshipNode
+			(string-append (cog-name verb) "_" (cog-name prep))
+		)
+		(ListLink
+			(WordNode (cog-name verb))
+			(DefinedLinguisticRelationshipNode (cog-name prep))
+		)
+	)
+)
+
 (EvaluationLink (stv 1.0 1.0)
 	(DefinedLinguisticRelationshipNode "capital_of")
 	(ListLink
