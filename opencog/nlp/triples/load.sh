5f168bc8a1b553856181aa9581a7b7f4b6cd1b08
Linas Vepstas <linasvepstas@gmail.com>
2009-04-18 12:40:51 -0500

    teaks to loading of examples.

--- opencog/nlp/triples/load.sh
+++ opencog/nlp/triples/load.sh
@@ -8,8 +8,8 @@ cat prep-maps.scm | netcat -q1 localhost 17003 > /dev/null
 cat preps.txt | ./preplist-to-atoms.pl | netcat -q1 localhost 17003 > /dev/null
 
 # Load add rules
-cat rules.txt | ./rules-to-implications.pl | netcat -q1 localhost 17003 > /dev/null
+cat rules.txt | ./rules-to-implications.pl | netcat -q2 localhost 17003 > /dev/null
 
 # Load some previously parsed sentences.
-cat example-sentences.scm | netcat -q2 localhost 17003 > /dev/null
+cat example-sentences.scm | netcat -q3 localhost 17003 > /dev/null
 
