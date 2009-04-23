080addb8fd03e911dbc4b69f8820379bab4af428
Linas Vepstas <linasvepstas@gmail.com>
2009-04-23 09:52:27 -0500

    Alter the relex interface to return a list of handles to the parsed sentences.

--- opencog/nlp/chatbot/chat-interface.scm
+++ opencog/nlp/chatbot/chat-interface.scm
@@ -14,21 +14,42 @@
 ; This routine takes plain-text input (in english), and sends it off 
 ; to a running instance of the RelEx parser, which should be listening 
 ; on port 4444. The parser will return a set of atoms, and these are
-; then loaded into this opencog instance.
+; then loaded into this opencog instance. This routine returns a list
+; of atoms to a list of SentenceNode's that were generated as a result
+; of the parse.
 ;
-(define (do-parse txt)
-	(let ((s (socket PF_INET SOCK_STREAM 0)))
-		(connect s AF_INET (inet-aton "127.0.0.1") 4444)
+(define (do-parse plain-txt)
 
-		(display txt s)
-		(display "\n" s) ; must send newline to flush socket
-		(system (string-join (list "echo Info: send to parser: " txt)))
-		(exec-scm-from-port s)
-		(shutdown s 2)
-		(system (string-join (list "echo Info: close socket to parser" )))
-		""
+	; A little short routine that sends the plain-text to the
+	; RelEx parser, and then loads the resulting parse into the
+	; atomspace (using exec-scm-from-port to do the load)
+	(define (do-sock-io sent-txt)
+		(let ((s (socket PF_INET SOCK_STREAM 0)))
+			(connect s AF_INET (inet-aton "127.0.0.1") 4444)
+	
+			(display sent-txt s)
+			(display "\n" s) ; must send newline to flush socket
+			(system (string-join (list "echo Info: send to parser: " sent-txt)))
+			(exec-scm-from-port s)
+			(shutdown s 2)
+			(system (string-join (list "echo Info: close socket to parser" )))
+		)
+	)
+
+	; Perform the actual processing
+	(do-sock-io plain-txt)
+	(let ((new-sent-anchor (ConceptNode "# New Parsed Sentence"))
+			(new-sent (cog-chase-link 'ListLink 'SentenceNode new-sent-anchor))
+		)
+
+		; Delete the ListLink's that connect the anchor to the new sentences
+		(for-each (lambda (x) (cog-delete x)) (cog-incoming-set new-sent-anchor))
+
+		; return the list of newly parsed sentences
+		new-sent
 	)
 )
+
 ; -----------------------------------------------------------------------
 ; say-id-english -- process user input from chatbot.
 ; args are: user nick from the IRC channel, and the text that the user entered.
@@ -43,6 +64,5 @@
 	(newline)
 	(display "My answer is:\n")
 	(do-parse txt)
-	(cog-map-type (lambda (h) (display h) #f) 'SentenceNode)
 )
 
