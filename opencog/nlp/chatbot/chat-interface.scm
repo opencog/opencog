bf17ef0c7214c93338abfbabfe506c9439f4d24a
Linas Vepstas <linasvepstas@gmail.com>
2009-04-22 23:07:05 -0500

    Add code to connect to the relex parser

--- opencog/nlp/chatbot/chat-interface.scm
+++ opencog/nlp/chatbot/chat-interface.scm
@@ -5,6 +5,30 @@
 ; 
 ; Linas Vepstas April 2009
 ;
+
+(use-modules (ice-9 rdelim))
+
+; -----------------------------------------------------------------------
+; do-parse -- send text to parser, load the resulting opencog atoms
+;
+; This routine takes plain-text input (in english), and sends it off 
+; to a running instance of the RelEx parser, which should be listening 
+; on port 4444. The parser will return a set of atoms, and these are
+; then loaded into this opencog instance.
+;
+(define (do-parse txt)
+	(let ((s (socket PF_INET SOCK_STREAM 0)))
+		(connect s AF_INET (inet-aton "127.0.0.1") 4444)
+
+		(display txt s)
+		(display "\n" s) ; must send newline to flush socket
+		(system (string-join (list "echo Info: send to parser: " txt)))
+		(exec-scm-from-port s)
+		(shutdown s 2)
+		(system (string-join (list "echo Info: close socket to parser" )))
+		""
+	)
+)
 ; -----------------------------------------------------------------------
 ; say-id-english -- process user input from chatbot.
 ; args are: user nick from the IRC channel, and the text that the user entered.
@@ -16,4 +40,6 @@
 	(display nick)
 	(display ", you said: ")
 	(display txt)
+	(do-parse txt)
 )
+
