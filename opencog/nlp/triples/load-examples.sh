4addc7826611fbeae55b0f28c280594415aaca60
Linas Vepstas <linasvepstas@gmail.com>
2009-04-24 13:25:54 -0500

    Fix example sentences so that they work again.

--- opencog/nlp/triples/load-examples.sh
+++ opencog/nlp/triples/load-examples.sh
@@ -3,5 +3,5 @@
 # Load example sentences for the example demo.
 #
 # Load some previously parsed sentences.
-cat example-sentences.scm | netcat -q3 localhost 17003 > /dev/null
+cat example-sentences.scm | netcat -q60 localhost 17003 > /dev/null
 
