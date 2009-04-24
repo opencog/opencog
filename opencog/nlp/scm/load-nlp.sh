bdefb6f12bd45d75d6107e8565206d81acb77e02
Linas Vepstas <linasvepstas@gmail.com>
2009-04-24 13:27:10 -0500

    increase timeouts to a safe value.

--- opencog/nlp/scm/load-nlp.sh
+++ opencog/nlp/scm/load-nlp.sh
@@ -8,10 +8,10 @@
 HOST=localhost
 PORT=17001
 
-# echo "scm" | cat - type-definitions.scm |netcat -q2 $HOST $PORT
-# echo "scm" | cat - nlp-utils.scm        |netcat -q2 $HOST $PORT
-cat disjunct-list.scm    |netcat -q2 $HOST $PORT
-cat stats-collection.scm |netcat -q2 $HOST $PORT
-cat wsd-process.scm      |netcat -q2 $HOST $PORT
-cat A.scm                |netcat -q2 $HOST $PORT
+# echo "scm" | cat - type-definitions.scm |netcat -q60 $HOST $PORT
+# echo "scm" | cat - nlp-utils.scm        |netcat -q60 $HOST $PORT
+cat disjunct-list.scm    |netcat -q60 $HOST $PORT
+cat stats-collection.scm |netcat -q60 $HOST $PORT
+cat wsd-process.scm      |netcat -q60 $HOST $PORT
+cat A.scm                |netcat -q60 $HOST $PORT
 
