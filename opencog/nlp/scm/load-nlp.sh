f3f27496ec0916dd7a7e0fa2fd09adfcde803f62
Linas Vepstas <linasvepstas@gmail.com>
2009-05-03 19:26:03 -0500

    Autoload a file.

--- opencog/nlp/scm/load-nlp.sh
+++ opencog/nlp/scm/load-nlp.sh
@@ -10,7 +10,7 @@ PORT=17001
 
 # echo "scm" | cat - type-definitions.scm |netcat -q60 $HOST $PORT
 # echo "scm" | cat - nlp-utils.scm        |netcat -q60 $HOST $PORT
-cat disjunct-list.scm    |netcat -q60 $HOST $PORT > /dev/null
+# cat disjunct-list.scm    |netcat -q60 $HOST $PORT > /dev/null
 cat stats-collection.scm |netcat -q60 $HOST $PORT > /dev/null
 cat wsd-process.scm      |netcat -q60 $HOST $PORT > /dev/null
 cat A.scm                |netcat -q60 $HOST $PORT > /dev/null
