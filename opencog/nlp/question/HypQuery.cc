6e25091b5cd1d3f4d4c19bf999633f4bfc646aa0
Linas Vepstas <linasvepstas@gmail.com>
2009-05-05 15:01:08 -0500

    start implementing hypothetical queries.

--- opencog/nlp/question/HypQuery.cc
+++ opencog/nlp/question/HypQuery.cc
@@ -1,5 +1,5 @@
 /**
- * HypQuery.h
+ * HypQuery.cc
  *
  * Impelement hypothetical query processing (yes/no questions)
  *
@@ -21,34 +21,44 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#ifndef _OPENCOG_HYP_QUERY_H
-#define _OPENCOG_HYP_QUERY_H
+#include "HypQuery.h"
 
-#include <map>
+#include <stdio.h>
 
-#include <opencog/atomspace/AtomSpace.h>
+#include <opencog/atomspace/ForeachChaseLink.h>
+#include <opencog/atomspace/Node.h>
 
-namespace opencog {
-
-class HypQuery 
-{
-	private:
-
-	protected:
-		AtomSpace *atom_space;
-	public:
-		HypQuery(void);
-		virtual ~HypQuery();
-};
-
-} // namespace opencog
-
-#endif // _OPENCOG_HYP_QUERY_H
+using namespace opencog;
 
 HypQuery::HypQuery(void)
 {
+	atom_space = NULL;
 }
 
 HypQuery::~HypQuery()
 {
 }
+
+bool HypQuery::is_truth_query(Handle word_prop)
+{
+	Atom *atom = TLB::getAtom(word_prop);
+	if (DEFINED_LINGUISTIC_CONCEPT_NODE != atom->getType()) return false;
+
+	Node *n = static_cast<Node *>(atom);
+	const std::string& name = n->getName();
+	const char * str = name.c_str();
+	if (0 == strcmp(str, "truth-query"))
+		return true;
+	return false;
+}
+
+/**
+ * Return true if the indicated word-instance has the "truth-query" 
+ * flag set.
+ */
+bool HypQuery::is_word_a_query(Handle word_inst)
+{
+	return foreach_binary_link(word_inst, INHERITANCE_LINK, &HypQuery::is_truth_query, this);
+}
+
+/* ============================= END OF FILE ======================== */
