0b9060213c96f0416356447e2c6243063fab6b87
Linas Vepstas <linasvepstas@gmail.com>
2009-05-05 14:48:59 -0500

    initial framework for hypothetical query.

---
+++ opencog/nlp/question/HypQuery.cc
@@ -0,0 +1,54 @@
+/**
+ * HypQuery.h
+ *
+ * Impelement hypothetical query processing (yes/no questions)
+ *
+ * Copyright (c) 2008,2009 Linas Vepstas <linas@linas.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_HYP_QUERY_H
+#define _OPENCOG_HYP_QUERY_H
+
+#include <map>
+
+#include <opencog/atomspace/AtomSpace.h>
+
+namespace opencog {
+
+class HypQuery 
+{
+	private:
+
+	protected:
+		AtomSpace *atom_space;
+	public:
+		HypQuery(void);
+		virtual ~HypQuery();
+};
+
+} // namespace opencog
+
+#endif // _OPENCOG_HYP_QUERY_H
+
+HypQuery::HypQuery(void)
+{
+}
+
+HypQuery::~HypQuery()
+{
+}
