39a0ba0c374b165a2e3b2d1c04a0996143087abb
Nil Geisweiller <ngeiswei@gmail.com>
2009-05-07 11:48:33 +0300

    reformatted local_structure.h/cc according to OpenCog standard

--- opencog/learning/moses/eda/local_structure.cc
+++ opencog/learning/moses/eda/local_structure.cc
@@ -28,125 +28,132 @@
 #include <opencog/util/selection.h>
 #include <opencog/util/exceptions.h>
 
-namespace eda {
-  //true if the range is uniform on the variable at index idx
-  bool local_structure_model::is_uniform_on(iptr_iter l,iptr_iter u,int idx) {
-    return (adjacent_find(make_indirect_iterator(l),make_indirect_iterator(u),
-			  bind(std::not_equal_to<disc_t>(),
-			       bind(&field_set::get_raw,&_fields,_1,idx),
-			       bind(&field_set::get_raw,&_fields,_2,idx)))==
-	    make_indirect_iterator(u));
-  }
+namespace eda
+{
+//true if the range is uniform on the variable at index idx
+bool local_structure_model::is_uniform_on(iptr_iter l, iptr_iter u, int idx)
+{
+    return (adjacent_find(make_indirect_iterator(l), make_indirect_iterator(u),
+                          bind(std::not_equal_to<disc_t>(),
+                               bind(&field_set::get_raw, &_fields, _1, idx),
+                               bind(&field_set::get_raw, &_fields, _2, idx))) ==
+            make_indirect_iterator(u));
+}
 
-  void local_structure_model::rec_split_onto(iptr_iter l,iptr_iter u,
-					     int src_idx,int idx,
-					     dtree::iterator node,
-					     onto_tree::iterator osrc) 
-  {
-    int raw_arity=osrc.number_of_children()+1;
-    
-    if (is_uniform_on(l,u,idx) || raw_arity==1) {
-      //make us a leaf
-      *node=dtree_node(raw_arity+1,0);
-      return;
+void local_structure_model::rec_split_onto(iptr_iter l, iptr_iter u,
+        int src_idx, int idx,
+        dtree::iterator node,
+        onto_tree::iterator osrc)
+{
+    int raw_arity = osrc.number_of_children() + 1;
+
+    if (is_uniform_on(l, u, idx) || raw_arity == 1) {
+        //make us a leaf
+        *node = dtree_node(raw_arity + 1, 0);
+        return;
     }
 
-    opencog::cassert(TRACE_INFO, node->size()==1,
-            "dtree node size should be equals to 1.");
-    (begin()+idx)->append_children(node,*node,raw_arity);
-    node->front()=src_idx;
-    (*node.begin())=dtree_node(2,0);
+    opencog::cassert(TRACE_INFO, node->size() == 1,
+                     "dtree node size should be equals to 1.");
+    (begin() + idx)->append_children(node, *node, raw_arity);
+    node->front() = src_idx;
+    (*node.begin()) = dtree_node(2, 0);
 
-    if (src_idx<idx-1) { //important - needs to match onto_spec::Left/Right
-      vector<iptr_iter> pivots(raw_arity);
-      pivots.back()=u;
-      opencog::n_way_partition
-	(l,u,bind(&field_set::get_raw,&_fields,
-		  bind(opencog::valueof<const instance>,_1),src_idx),
-	 raw_arity,pivots.begin());
+    if (src_idx < idx - 1) { //important - needs to match onto_spec::Left/Right
+        vector<iptr_iter> pivots(raw_arity);
+        pivots.back() = u;
+        opencog::n_way_partition
+        (l, u, bind(&field_set::get_raw, &_fields,
+                    bind(opencog::valueof<const instance>, _1), src_idx),
+         raw_arity, pivots.begin());
 
-      //we don't recurse on the leftmost partition - it's onto_spec::Stop
-      //instead just make it a (null) leaf 
-      for_each(pivots.begin(),--pivots.end(),++pivots.begin(),
-	       make_counting_iterator(++node.begin()),	       
-	       make_counting_iterator(osrc.begin()),
-	       bind(&local_structure_model::rec_split_onto,this,_1,_2,
-		    src_idx+1,idx,_3,_4));
+        //we don't recurse on the leftmost partition - it's onto_spec::Stop
+        //instead just make it a (null) leaf
+        for_each(pivots.begin(), --pivots.end(), ++pivots.begin(),
+                 make_counting_iterator(++node.begin()),
+                 make_counting_iterator(osrc.begin()),
+                 bind(&local_structure_model::rec_split_onto, this, _1, _2,
+                      src_idx + 1, idx, _3, _4));
     } else {
-      //we're the parent of leaves
-      dtree::sibling_iterator dsib=++node.begin();
-      for (onto_tree::sibling_iterator osib=osrc.begin();osib!=osrc.end();
-	   ++osib,++dsib) {
-	*dsib=dtree_node(raw_arity+1,0);
-      }
+        //we're the parent of leaves
+        dtree::sibling_iterator dsib = ++node.begin();
+        for (onto_tree::sibling_iterator osib = osrc.begin();osib != osrc.end();
+                ++osib, ++dsib) {
+            *dsib = dtree_node(raw_arity + 1, 0);
+        }
     }
-  }
+}
 
-  void local_structure_model::rec_split_contin
-  (iptr_iter l,iptr_iter u,int src_idx,int idx,dtree::iterator node) {
+void local_structure_model::rec_split_contin
+(iptr_iter l, iptr_iter u, int src_idx, int idx, dtree::iterator node)
+{
 
-    if (adjacent_find(make_indirect_iterator(l),make_indirect_iterator(u),
-		      bind(std::not_equal_to<disc_t>(),
-			   bind(&field_set::get_raw,&_fields,_1,idx),
-			   bind(&field_set::get_raw,&_fields,_2,idx)))==
-	make_indirect_iterator(u))
-      return;
+    if (adjacent_find(make_indirect_iterator(l), make_indirect_iterator(u),
+                      bind(std::not_equal_to<disc_t>(),
+                           bind(&field_set::get_raw, &_fields, _1, idx),
+                           bind(&field_set::get_raw, &_fields, _2, idx))) ==
+            make_indirect_iterator(u))
+        return;
 
-    split(src_idx,idx,node);
+    split(src_idx, idx, node);
 
-    if (src_idx<idx-1) { //important - needs to match contin_spec::Left/Right
-      vector<iptr_iter> pivots(2);
-      opencog::n_way_partition
-	(l,u,bind(&field_set::get_raw,&_fields,
-		  bind(opencog::valueof<const instance>,_1),src_idx),
-	 3,pivots.begin());
-      rec_split_contin(pivots[0],pivots[1],src_idx+1,idx,++node.begin());
-      rec_split_contin(pivots[1],u,src_idx+1,idx,node.last_child());
+    if (src_idx < idx - 1) { //important - needs to match contin_spec::Left/Right
+        vector<iptr_iter> pivots(2);
+        opencog::n_way_partition
+        (l, u, bind(&field_set::get_raw, &_fields,
+                    bind(opencog::valueof<const instance>, _1), src_idx),
+         3, pivots.begin());
+        rec_split_contin(pivots[0], pivots[1], src_idx + 1, idx, ++node.begin());
+        rec_split_contin(pivots[1], u, src_idx + 1, idx, node.last_child());
     }
-  }
+}
 
-  void local_structure_model::make_dtree(super::iterator dtr,int arity) {
-    dtr->set_head(dtree_node(arity+1,0));
-  }	
-  
-  void local_structure_model::split(int src_idx,int tgt_idx,
-				    dtree::iterator tgt) {
-    opencog::cassert(TRACE_INFO, tgt.number_of_children()==0, 
-            "dtree node should have exactly zero child (split)");
-    (begin()+tgt_idx)->append_children(tgt,*tgt,tgt->size()-1);
-    *tgt=dtree_node(1,src_idx);
-  }
-  
-  //sample from a model
-  instance local_structure_model::operator()() const {
+void local_structure_model::make_dtree(super::iterator dtr, int arity)
+{
+    dtr->set_head(dtree_node(arity + 1, 0));
+}
+
+void local_structure_model::split(int src_idx, int tgt_idx,
+                                  dtree::iterator tgt)
+{
+    opencog::cassert(TRACE_INFO, tgt.number_of_children() == 0,
+                     "dtree node should have exactly zero child (split)");
+    (begin() + tgt_idx)->append_children(tgt, *tgt, tgt->size() - 1);
+    *tgt = dtree_node(1, src_idx);
+}
+
+//sample from a model
+instance local_structure_model::operator()() const
+{
     vector<disc_t> tmp(size());
-    foreach(unsigned int idx,_ordering) {
-      sample((begin()+idx)->begin(),tmp[idx],tmp);
+    foreach(unsigned int idx, _ordering) {
+        sample((begin() + idx)->begin(), tmp[idx], tmp);
     }
     instance res(_instance_length);
-    _fields.pack(tmp.begin(),res.begin());
+    _fields.pack(tmp.begin(), res.begin());
 
     return res;
-  }
+}
 
-  void local_structure_model::sample(dtree::iterator dtr,disc_t& dst,
-				     const vector<disc_t>& margs) const {
+void local_structure_model::sample(dtree::iterator dtr, disc_t& dst,
+                                   const vector<disc_t>& margs) const
+{
     if (dtr.is_childless()) { //a leaf
-      if (dtr->back()>0)
-	dst=distance(dtr->begin(),
-		     opencog::roulette_select(dtr->begin(),--(dtr->end()),
-					   dtr->back(), rng));
-      else
-	dst=0;//rng.randint(dtr->size()-1); //if no data, do uniform selection
+        if (dtr->back() > 0)
+            dst = distance(dtr->begin(),
+                           opencog::roulette_select(dtr->begin(), --(dtr->end()),
+                                                    dtr->back(), rng));
+        else
+            dst = 0;//rng.randint(dtr->size()-1); //if no data, do uniform selection
     } else {
-      opencog::cassert(TRACE_INFO, dtr->size()==1, 
-             "dtree node size should be equals to 1.");
-      opencog::cassert(TRACE_INFO, (int)dtr.number_of_children()>margs[dtr->front()],
-             "dtree node children number grearter than margs.");
-      dtree::sibling_iterator child=dtr.begin();
-      child+=margs[dtr->front()];
-      sample(child,dst,margs);
+        opencog::cassert(TRACE_INFO, dtr->size() == 1,
+                         "dtree node size should be equals to 1.");
+        opencog::cassert(TRACE_INFO, (int)dtr.number_of_children() > margs[dtr->front()],
+                         "dtree node children number grearter than margs.");
+        dtree::sibling_iterator child = dtr.begin();
+        child += margs[dtr->front()];
+        sample(child, dst, margs);
     }
-  }
+}
 
 } //~namespace eda
--- opencog/learning/moses/eda/local_structure.h
+++ opencog/learning/moses/eda/local_structure.h
@@ -27,24 +27,26 @@
 #include "field_set.h"
 #include <opencog/util/digraph.h>
 
-namespace eda {
+namespace eda
+{
 
-  typedef vector<int> dtree_node;
-  typedef opencog::tree<dtree_node> dtree;
+typedef vector<int> dtree_node;
+typedef opencog::tree<dtree_node> dtree;
 
-  struct local_structure_model : public nullary_function<instance>,
-				 public vector<dtree> { 
+struct local_structure_model : public nullary_function<instance>,
+                               public vector<dtree> {
     typedef vector<dtree> super;
-    
+
     //creates a model based on a set of fields and a range of instances
     template<typename It>
-    local_structure_model(const field_set& fields,It from,It to, opencog::RandGen& _rng);
+    local_structure_model(const field_set& fields,
+                          It from, It to, opencog::RandGen& _rng);
 
-    instance operator()() const; //sample from the model 
+    instance operator()() const; //sample from the model
 
-    void split(int,int,dtree::iterator);
+    void split(int,int, dtree::iterator);
 
-  protected:
+protected:
     typedef vector<const instance*> iptr_seq;
     typedef iptr_seq::iterator iptr_iter;
 
@@ -55,157 +57,161 @@ namespace eda {
     opencog::RandGen& rng;
 
     //true if the range is uniform on the variable at index idx
-    bool is_uniform_on(iptr_iter l,iptr_iter u,int idx);
+    bool is_uniform_on(iptr_iter l, iptr_iter u, int idx);
 
-    void rec_split_onto(iptr_iter l,iptr_iter u,int src_idx,int idx,
-			dtree::iterator node,onto_tree::iterator osrc);
-    void rec_split_contin(iptr_iter l,iptr_iter u,
-			  int src_idx,int idx,dtree::iterator node);
-    
-    void make_dtree(super::iterator,int);
+    void rec_split_onto(iptr_iter l, iptr_iter u, int src_idx, int idx,
+                        dtree::iterator node, onto_tree::iterator osrc);
+    void rec_split_contin(iptr_iter l, iptr_iter u,
+                          int src_idx, int idx, dtree::iterator node);
 
-    void sample(dtree::iterator,disc_t&,const vector<disc_t>&) const;
-  };
+    void make_dtree(super::iterator, int);
 
-  struct univariate { //i.e., no structure learning
+    void sample(dtree::iterator, disc_t&, const vector<disc_t>&) const;
+};
+
+struct univariate { //i.e., no structure learning
     typedef local_structure_model model_type;
 
     template<typename It>
-    void operator()(const field_set&,It,It,const local_structure_model&) const { }
-  };
+    void operator()(const field_set&, It, It,
+                    const local_structure_model&) const { }
+};
 
-  /*struct bde_local_structure_learning {
-    typedef local_structure_model model_type;
+/*struct bde_local_structure_learning {
+  typedef local_structure_model model_type;
 
-    //scratch_model should go here
-    template<typename It>
-    void operator()(const field_set&,It,It,local_structure_model&) const { }
-    };*/
-
-  struct local_structure_probs_learning {
-    typedef local_structure_model model_type;
-
-    template<typename It>
-    void operator()(const field_set&,It,It,local_structure_model&) const;
-  protected:
-    template<typename It>
-    void rec_learn(const field_set&,It,It,int,dtree::iterator) const;
-  };
-
-  /////////////TEMPLATIZED FUNCTION IMPLEMENTATION///////////////
-
-  //creates a model based on a set of fields and a range of instances
+  //scratch_model should go here
   template<typename It>
-  local_structure_model::local_structure_model(const field_set& fs,It from,It to, opencog::RandGen& _rng) :
-    vector<dtree>(fs.raw_size()),_instance_length(fs.packed_width()),
-    _ordering(make_counting_iterator(0),make_counting_iterator(int(size()))),
-    _initial_deps(size()),_fields(fs), rng(_rng)
-  {
-    super::iterator dtr=begin();
+  void operator()(const field_set&,It,It,local_structure_model&) const { }
+  };*/
+
+struct local_structure_probs_learning {
+    typedef local_structure_model model_type;
+
+    template<typename It>
+    void operator()(const field_set&, It, It, local_structure_model&) const;
+protected:
+    template<typename It>
+    void rec_learn(const field_set&, It, It, int, dtree::iterator) const;
+};
+
+/////////////TEMPLATIZED FUNCTION IMPLEMENTATION///////////////
+
+//creates a model based on a set of fields and a range of instances
+template<typename It>
+local_structure_model::local_structure_model(const field_set& fs,
+                                             It from, It to,
+                                             opencog::RandGen& _rng) :
+    vector<dtree>(fs.raw_size()), _instance_length(fs.packed_width()),
+    _ordering(make_counting_iterator(0), make_counting_iterator(int(size()))),
+    _initial_deps(size()), _fields(fs), rng(_rng)
+{
+    super::iterator dtr = begin();
 
     if (!_fields.contin().empty() || !_fields.onto().empty()) {
-      iptr_seq iptrs(make_transform_iterator(from,opencog::addressof<const instance>),
-		     make_transform_iterator(to,opencog::addressof<const instance>));
+        iptr_seq iptrs(make_transform_iterator(from, opencog::addressof<const instance>),
+                       make_transform_iterator(to, opencog::addressof<const instance>));
+        
+        foreach(const field_set::onto_spec& o, _fields.onto()) { //onto vars
+            int idx_base = distance(begin(), dtr);
+            make_dtree(dtr++, o.tr->begin().number_of_children() + 1);
 
-      foreach(const field_set::onto_spec& o,_fields.onto()) { //onto vars
-	int idx_base=distance(begin(),dtr);
-	make_dtree(dtr++,o.tr->begin().number_of_children()+1);
+            for (field_set::arity_t i = 1;i < o.depth;++i, ++dtr) {
+                make_dtree(dtr, 0);
+                _initial_deps.insert(idx_base + i - 1, idx_base + i);
+                //need to recursively split on gggparent, ... , gparent, parent
+                rec_split_onto(iptrs.begin(), iptrs.end(),
+                               idx_base, idx_base + i,
+                               dtr->begin(), o.tr->begin());
 
-	for (field_set::arity_t i=1;i<o.depth;++i,++dtr) {
-	  make_dtree(dtr,0);
-	  _initial_deps.insert(idx_base+i-1,idx_base+i);
-	  //need to recursively split on gggparent, ... , gparent, parent
-	  rec_split_onto(iptrs.begin(),iptrs.end(),
-			 idx_base,idx_base+i,dtr->begin(),o.tr->begin());
+                //make_dtree(dtr,o.branching);
+            }
+        }
 
-	  //make_dtree(dtr,o.branching);
-	}
-      }
+        foreach(const field_set::contin_spec& c, _fields.contin()) { //contin vars
+            int idx_base = distance(begin(), dtr);
+            make_dtree(dtr++, 3); //contin arity is 3
+            for (field_set::arity_t i = 1;i < c.depth;++i, ++dtr) {
+                make_dtree(dtr, 3);
+                _initial_deps.insert(idx_base + i - 1, idx_base + i); //add dep to parent
 
-      foreach(const field_set::contin_spec& c,_fields.contin()) { //contin vars
-	int idx_base=distance(begin(),dtr);
-	make_dtree(dtr++,3); //contin arity is 3
-	for (field_set::arity_t i=1;i<c.depth;++i,++dtr) {
-	  make_dtree(dtr,3);
-	  _initial_deps.insert(idx_base+i-1,idx_base+i); //add dep to parent
+                //recursively split on gggparent, ... , gparent, parent
+                /*rec_split_contin(iptrs.begin(),iptrs.end(),
+                  idx_base,idx_base+i,dtr->begin());*/
 
-	  //recursively split on gggparent, ... , gparent, parent
-	  /*rec_split_contin(iptrs.begin(),iptrs.end(),
-	    idx_base,idx_base+i,dtr->begin());*/
-
-	  //alternatively, only split on parent
-	  rec_split_contin(iptrs.begin(),iptrs.end(),
-			   idx_base+i-1,idx_base+i,dtr->begin());
-	}
-      }
+                //alternatively, only split on parent
+                rec_split_contin(iptrs.begin(), iptrs.end(),
+                                 idx_base + i - 1, idx_base + i, dtr->begin());
+            }
+        }
     }
 
     //model disc & bool vars
-    foreach(const field_set::disc_spec& d,_fields.disc_and_bits())
-      make_dtree(dtr++,d.arity);
+    foreach(const field_set::disc_spec& d, _fields.disc_and_bits())
+    make_dtree(dtr++, d.arity);
 
     //now that we have created all of the dtrees, construct a feasible order
     //that respects the intitial dependencies
-    opencog::randomized_topological_sort(_initial_deps,_ordering.begin());
-  }
+    opencog::randomized_topological_sort(_initial_deps, _ordering.begin());
+}
 
-  //instance_set is not const so that we can reorder it - the instances
-  //themselves shouldn't change
-  template<typename It>
-  void local_structure_probs_learning::operator()(const field_set& fs,It from,It to,
-						  local_structure_model& dst) const 
-  {
-    for_each(dst.begin(),dst.end(),make_counting_iterator(0),
-	     bind(&local_structure_probs_learning::rec_learn<It>,this,ref(fs),
-		  from,to,_2,bind(&dtree::begin,_1)));
-  }
+//instance_set is not const so that we can reorder it - the instances
+//themselves shouldn't change
+template<typename It>
+void local_structure_probs_learning::operator()(const field_set& fs,
+                                                It from, It to,
+                                                local_structure_model& dst) const
+{
+    for_each(dst.begin(), dst.end(), make_counting_iterator(0),
+             bind(&local_structure_probs_learning::rec_learn<It>, this, ref(fs),
+                  from, to, _2, bind(&dtree::begin, _1)));
+}
 
-  template<typename It>
-  void local_structure_probs_learning::rec_learn(const field_set& fs,It from,It to,
-						 int idx,dtree::iterator dtr) const
-  {
+template<typename It>
+void local_structure_probs_learning::rec_learn(const field_set& fs,
+                                               It from, It to,
+                                               int idx, dtree::iterator dtr) const
+{
     if (dtr.is_childless()) { //a leaf
-      while (from!=to) {
-	assert(fs.get_raw(*from,idx)<int(dtr->size())-1);
-	++(*dtr)[fs.get_raw(*from++,idx)];
-      }
+        while (from != to) {
+            assert(fs.get_raw(*from, idx) < int(dtr->size()) - 1);
+            ++(*dtr)[fs.get_raw(*from++,idx)];
+        }
 
-      //add +1 noisy
-      /*if (dtr->size()>2) {
-	for (dtree_node::iterator it=dtr->begin();it!=dtr->end();++it)
-	  ++(*it);
-	  }*/
-      //std::cout << "XX " << (*dtr)[0] << " " << (*dtr)[1] << std::endl;
-      //(*dtr)[0]=(*dtr)[0]/(*dtr)[0]+(*dtr)[1]
+        //add +1 noisy
+        /*if (dtr->size()>2) {
+        for (dtree_node::iterator it=dtr->begin();it!=dtr->end();++it)
+        ++(*it);
+        }*/
+        //std::cout << "XX " << (*dtr)[0] << " " << (*dtr)[1] << std::endl;
+        //(*dtr)[0]=(*dtr)[0]/(*dtr)[0]+(*dtr)[1]
 
-      dtr->back()=accumulate(dtr->begin(),--(dtr->end()),0);
+        dtr->back() = accumulate(dtr->begin(), --(dtr->end()), 0);
     } else { //an internal node (split) - sort [from,to) on the src idx
-      int raw_arity=dtr.number_of_children();
-      vector<It> pivots(raw_arity+1);
-      pivots.front()=from;
-      pivots.back()=to;
-      opencog::n_way_partition(from,to,bind(&field_set::get_raw,&fs,_1,dtr->front()),
-			    raw_arity,++pivots.begin());
-      for_each(pivots.begin(),--pivots.end(),++pivots.begin(),
-	       make_counting_iterator(dtr.begin()),
-	       bind(&local_structure_probs_learning::rec_learn<It>,this,
-		    ref(fs),_1,_2,idx,_3));
+        int raw_arity = dtr.number_of_children();
+        vector<It> pivots(raw_arity + 1);
+        pivots.front() = from;
+        pivots.back() = to;
+        opencog::n_way_partition(from, to,
+                                 bind(&field_set::get_raw, &fs,
+                                      _1, dtr->front()),
+                                 raw_arity, ++pivots.begin());
+        for_each(pivots.begin(), --pivots.end(), ++pivots.begin(),
+                 make_counting_iterator(dtr.begin()),
+                 bind(&local_structure_probs_learning::rec_learn<It>, this,
+                      ref(fs), _1, _2, idx, _3));
     }
-  }
+}
 
 } //~namespace eda
 
-inline std::ostream& operator<<(std::ostream& o,const std::vector<int>& v) {
-  o << "< ";
-  for (std::vector<int>::const_iterator it=v.begin();it!=v.end();++it)
-    o << (*it) << " ";
-  o << ">";
-  return o;
+inline std::ostream& operator<<(std::ostream& o, const std::vector<int>& v)
+{
+    o << "< ";
+    for (std::vector<int>::const_iterator it = v.begin();it != v.end();++it)
+        o << (*it) << " ";
+    o << ">";
+    return o;
 }
-    
-
-
-
-
 
 #endif
