28fa07c3ea5fb9f80986f7804f50fcca989ef9d3
Joel Pitt <joel@joelpitt.com>
2009-05-17 12:01:41 +1200

    Fix path used for page names

--- doc/README_to_doxygen.sh
+++ doc/README_to_doxygen.sh
@@ -20,10 +20,11 @@ SOURCE=$1
 SOURCE_PATH=`dirname $1`
 SOURCE_NAME=`basename $1`
 DEPTH=`echo $SOURCE_PATH | awk -F "/" '{ print NF }'`
-PATH_UNDERSCORE=`echo $SOURCE_PATH | sed -e 's/\//_/g'`
+# e.g. atomspace
 LAST_DIR=`echo "$SOURCE_PATH" | awk -F "/" '{ print $NF }'`
 
-# find root dir
+# find root dir (from relation to script dir)
+# e.g. /home/user/src/opencog
 ROOT_SRC_DIR=`dirname $0 | awk -F "/" '{ for (i=2; i<NF; i++) printf "/"$i; \
 print "" }'`
 
@@ -38,7 +39,10 @@ if [ -n "${IS_MAINPAGE}" ]; then
     echo "//---- 
 /*! @mainpage
 "
-    cat $SOURCE
+    
+    tail -n+4 $SOURCE 
+# need to figure out way of adding reference labels to section command
+    # | sed -n '1h;1!H;${;g;s/\([^\n]\+\)\n-\+/@section \1/g;p;}'
     
     cat $SOURCE_PATH/AUTHORS
 
@@ -49,9 +53,15 @@ http://opencog.org/wiki/BuildingOpenCog
 "
 
 else
+# e.g. opencog/atomspace
+    IN_CODE_DIR="${SOURCE_PATH#$ROOT_SRC_DIR/}"
+#    echo $IN_CODE_DIR
+# e.g. opencog_atomspace
+    PATH_UNDERSCORE=`echo $IN_CODE_DIR | sed -e 's/\//_/g'`
+#    echo $PATH_UNDERSCORE
     echo "//---- 
 /*! @page ${PATH_UNDERSCORE} ${LAST_DIR}
-From directory \\ref ./${SOURCE_PATH#$ROOT_SRC_DIR/}/
+From directory \\ref ${SOURCE_PATH#$ROOT_SRC_DIR/}/
     "
 fi
 echo "@section submodules Components:"
@@ -59,17 +69,18 @@ echo "@section submodules Components:"
 # Now we have to find all sub dirs that contain README
 for i in `find ${SOURCE_PATH}/ -name README | sort`
 do
+    SUBDIR_PATH=`dirname $i`
     # crappy way to check exclusions 
     # don't include the current README
     #if [ `dirname $i` != "./README"  ]; then
     # don't include the bzr dir (it has a README) 
-    if [ `dirname $i | awk -F "/" '{ print $NF }'` != ".bzr"  ]; then
+    if [ `echo ${SUBDIR_PATH} | awk -F "/" '{ print $NF }'` != ".bzr"  ]; then
     # don't include doc dir (or maybe we should)
     #if [ `dirname $i | awk -F "/" '{print $2}'` != "doc" ]; then
         SUB_DEPTH=$(( ${DEPTH} + 1 ))
-        if [ `dirname $i | awk -F "/" '{ print NF }'` -eq ${SUB_DEPTH} ]
+        if [ `echo ${SUBDIR_PATH} | awk -F "/" '{ print NF }'` -eq ${SUB_DEPTH} ]
         then
-            SUBPATH_UNDERSCORE=`dirname $i | sed -e 's/\//_/g'`
+            SUBPATH_UNDERSCORE=`echo ${SUBDIR_PATH#$ROOT_SRC_DIR/} | sed -e 's/\//_/g'`
             # Remember to remove ._
             echo " - @subpage ${SUBPATH_UNDERSCORE#._}"
         fi
