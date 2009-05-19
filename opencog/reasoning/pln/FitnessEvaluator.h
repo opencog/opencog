7dd8d8067b66d141bd99392e0393f9656f69ee89
Joel Pitt <joel@joelpitt.com>
2009-05-20 09:25:06 +1200

    Forgot file, silly me

---
+++ opencog/reasoning/pln/FitnessEvaluator.h
@@ -0,0 +1,9 @@
+#ifndef __PLN_FITNESS_EVALUATOR
+#define __PLN_FITNESS_EVALUATOR
+
+#define PLN_FITNESS_BEST "best"
+#define PLN_FITNESS_RANDOM "random"
+#define PLN_FITNESS_SOFTMAX "softmax"
+enum FitnessEvaluatorT { BEST=0, RANDOM=1, SOFTMAX=2 };
+
+#endif
