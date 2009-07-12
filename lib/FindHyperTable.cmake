723153bf24270d6eee69f36ad4238e0a897e9844
Linas Vepstas <linasvepstas@gmail.com>
2009-07-11 20:09:17 -0500

    try to get find-hypertable to work correctly

--- lib/FindHyperTable.cmake
+++ lib/FindHyperTable.cmake
@@ -13,20 +13,23 @@
 
 # Look for the header file
 FIND_PATH(HYPERTABLE_INCLUDE_DIR Hypertable/Lib/Client.h 
-/usr/include
-/usr/local/include 
-/usr/hypertable/0.9.2.4/include
-/usr/local/hypertable/0.9.2.4/include
-/opt/hypertable/0.9.2.4/include
+	/usr/include
+	/usr/local/include 
+	/usr/hypertable/0.9.2.4/include
+	/usr/local/hypertable/0.9.2.4/include
+	/opt/hypertable/0.9.2.4/include
 )
 
-# Look for the library
-FIND_LIBRARY(HYPERTABLE_LIBRARY NAMES HyperComm PATH 
-/usr/lib 
-/usr/local/lib
-/usr/hypertable/0.9.2.4/lib
-/usr/local/hypertable/0.9.2.4/lib
-/opt/hypertable/0.9.2.4/lib
+# Look for the libraries
+FIND_LIBRARY(HYPERTABLE_LIBRARY 
+	NAMES
+		HyperCommon 
+	PATH 
+		/usr/lib 
+		/usr/local/lib
+		/usr/hypertable/0.9.2.4/lib
+		/usr/local/hypertable/0.9.2.4/lib
+		/opt/hypertable/0.9.2.4/lib
 )
 
 # Copy the results to the output variables.
@@ -53,6 +56,6 @@ IF(NOT HYPERTABLE_FOUND)
 ENDIF(NOT HYPERTABLE_FOUND)
 
 MARK_AS_ADVANCED(
-	HYPERTABLE_INCLUDE_DIR
-	HYPERTABLE_LIBRARY
+	HYPERTABLE_INCLUDE_DIRS
+	HYPERTABLE_LIBRARIES
 )
