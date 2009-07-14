a85607c97905d4e82d5556da96398b48f256e712
kizzobot <davidpkilgore@gmail.com>
2009-07-14 09:35:15 -0700

    Exposed removeAtom method of AtomSpace class.

--- opencog/python/atomspace/AtomSpace_wrap.cc
+++ opencog/python/atomspace/AtomSpace_wrap.cc
@@ -11,6 +11,7 @@ using namespace boost::python;
 
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(addNode_overloads, addNode, 1, 3)
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(addRealAtom_overloads, addRealAtom, 1, 2)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(removeAtom_overloads, removeAtom, 1, 2)
 //BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(addLink_overloads, addLink, 1, 11)
 
 void init_AtomSpace_py()
@@ -20,6 +21,8 @@ void init_AtomSpace_py()
         .def("addNode", &AtomSpace::addNode, addNode_overloads())
         .def("addRealAtom", &AtomSpace::addRealAtom,
             addRealAtom_overloads())
+        .def("removeAtom", &AtomSpace::removeAtom,
+            removeAtom_overloads())
         //.def("addLink", &AtomSpace::addLink, addLink_overloads())
         .def("getSpaceServer", &AtomSpace::getSpaceServer,
             return_value_policy<reference_existing_object>())
