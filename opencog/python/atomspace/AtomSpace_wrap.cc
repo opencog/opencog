fac70a50557c64b51e892a7429eb457467d071bf
kizzobot <davidpkilgore@gmail.com>
2009-07-14 09:27:16 -0700

    Exposed addRealAtom method of AtomSpace class.

--- opencog/python/atomspace/AtomSpace_wrap.cc
+++ opencog/python/atomspace/AtomSpace_wrap.cc
@@ -9,14 +9,17 @@
 using namespace opencog;
 using namespace boost::python;
 
-//BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(addNode_overloads, addNode, 1, 3)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(addNode_overloads, addNode, 1, 3)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(addRealAtom_overloads, addRealAtom, 1, 2)
 //BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(addLink_overloads, addLink, 1, 11)
 
 void init_AtomSpace_py()
 {
     class_<AtomSpace, bases<SpaceServerContainer> >("AtomSpace")
         .def("storeAtom", &AtomSpace::storeAtom)
-        //.def("addNode", &AtomSpace::addNode, addNode_overloads())
+        .def("addNode", &AtomSpace::addNode, addNode_overloads())
+        .def("addRealAtom", &AtomSpace::addRealAtom,
+            addRealAtom_overloads())
         //.def("addLink", &AtomSpace::addLink, addLink_overloads())
         .def("getSpaceServer", &AtomSpace::getSpaceServer,
             return_value_policy<reference_existing_object>())
