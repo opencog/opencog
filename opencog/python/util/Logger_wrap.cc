1072cc851409d9a34463ae35b3ef61154e1bb904
kizzobot <davidpkilgore@gmail.com>
2009-07-16 12:51:59 -0700

    Exposed constructors of Logger class.

--- opencog/python/util/Logger_wrap.cc
+++ opencog/python/util/Logger_wrap.cc
@@ -9,6 +9,8 @@ using namespace opencog;
 
 void init_Logger_py()
 {
-    class_<Logger>("Logger")
+    class_<Logger>("Logger", no_init)
+        .def(init< optional<const std::string, Logger::Level, bool> >())
+        .def(init<const Logger&>())
     ;
 }
