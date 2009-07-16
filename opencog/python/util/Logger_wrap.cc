911a4b136c1793f8ab42c21d20b9b4bcdb4d7bb4
kizzobot <davidpkilgore@gmail.com>
2009-07-16 13:10:18 -0700

    Exposed Level enum under Logger class.

--- opencog/python/util/Logger_wrap.cc
+++ opencog/python/util/Logger_wrap.cc
@@ -1,16 +1,32 @@
 #include <boost/python/class.hpp>
+#include <boost/python/scope.hpp>
+#include <boost/python/enum.hpp>
 
 #include <opencog/util/Logger.h>
 
 #include "Logger_wrap.h"
 
-using namespace boost::python;
 using namespace opencog;
+using namespace boost::python;
 
 void init_Logger_py()
 {
-    class_<Logger>("Logger", no_init)
-        .def(init< optional<const std::string, Logger::Level, bool> >())
-        .def(init<const Logger&>())
+    // Expose Logger class.
+    class_<Logger> logger("Logger", no_init);
+    logger.def(init< optional<const std::string, Logger::Level, bool> >());
+    logger.def(init<const Logger&>());
+    logger.def("setLevel", &Logger::setLevel);
+
+    // Change scope to be within the just-exposed Logger class.
+    scope within(logger);
+
+    // Now expose Level enum (under new scope).
+    enum_<Logger::Level>("Level")
+        .value("NONE", Logger::NONE)
+        .value("ERROR", Logger::ERROR)
+        .value("WARN", Logger::WARN)
+        .value("INFO", Logger::INFO)
+        .value("DEBUG", Logger::DEBUG)
+        .value("FINE", Logger::FINE)
     ;
 }
