ed848954d1402012e0dbe62760ecc488994ed50f
Linas Vepstas <linasvepstas@gmail.com>
2009-07-24 16:27:10 -0500

    Allow the scheme "apply" function to take more arguments.

--- opencog/guile/SchemeExec.cc
+++ opencog/guile/SchemeExec.cc
@@ -19,30 +19,34 @@ using namespace opencog;
 /**
  * do_apply -- apply named function func to arguments in ListLink
  * It is assumed that varargs is a ListLink, containing a list of
- * atom handles. This list is unpacked, and then the fuction func 
- * is applied to them. If the function returns an atom handle, then 
+ * atom handles. This list is unpacked, and then the fuction func
+ * is applied to them. If the function returns an atom handle, then
  * this is returned, in turn.
  */
 Handle SchemeEval::do_apply(const std::string &func, Handle varargs)
 {
 	per_thread_init();
 	SCM sfunc = scm_from_locale_symbol(func.c_str());
+	SCM expr = sfunc;
 
+	// If there were args, pass the args to the function.
 	Link *largs = dynamic_cast<Link *>(TLB::getAtom(varargs));
-	if (NULL == largs) return Handle::UNDEFINED;
-
-	const std::vector<Handle> &oset = largs->getOutgoingSet();
-
-	SCM expr = SCM_EOL;
-	size_t sz = oset.size();
-	for (int i=sz-1; i>=0; i--)
+	if (largs)
 	{
-		Handle h = oset[i];
-		SCM sh = SchemeSmob::handle_to_scm(h);
-		expr = scm_cons(sh, expr);
+		const std::vector<Handle> &oset = largs->getOutgoingSet();
+
+		expr = SCM_EOL;
+		size_t sz = oset.size();
+		for (int i=sz-1; i>=0; i--)
+		{
+			Handle h = oset[i];
+			SCM sh = SchemeSmob::handle_to_scm(h);
+			expr = scm_cons(sh, expr);
+		}
+		expr = scm_cons(sfunc, expr);
 	}
-	expr = scm_cons(sfunc, expr);
-	
+
+	// Apply the function to the args
    SCM sresult = do_scm_eval(expr);
 
 	// If the result is a handle, return the handle.
