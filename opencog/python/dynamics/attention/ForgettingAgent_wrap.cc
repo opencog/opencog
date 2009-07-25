d57ae07a06edf2fc7df17c7f5e2a2c1053a82087
kizzobot <davidpkilgore@gmail.com>
2009-07-25 10:43:02 -0700

    Fixed subclassing bug.

--- opencog/python/dynamics/attention/ForgettingAgent_wrap.cc
+++ opencog/python/dynamics/attention/ForgettingAgent_wrap.cc
@@ -1,6 +1,6 @@
 #include "ForgettingAgent_wrap.h"
-//#include <opencog/dynamics/attention/ForgettingAgent.h>
-#include "ForgettingAgent.h"
+#include <opencog/dynamics/attention/ForgettingAgent.h>
+#include <opencog/server/Agent.h>
 #include <boost/python/class.hpp>
 
 using namespace opencog;
@@ -8,6 +8,6 @@ using namespace boost::python;
 
 void init_ForgettingAgent_py()
 {
-    class_<ForgettingAgent>("ForgettingAgent")
+    class_<ForgettingAgent, bases<Agent> >("ForgettingAgent")
     ;
 }
