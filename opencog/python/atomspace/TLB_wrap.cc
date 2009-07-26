cc1fc65dacd261cba4526136365f2d219a4000a1
kizzobot <davidpkilgore@gmail.com>
2009-07-26 11:07:39 -0700

    Began exposing getAtom() method of TLB class but noticed a bug and stopped.

--- opencog/python/atomspace/TLB_wrap.cc
+++ opencog/python/atomspace/TLB_wrap.cc
@@ -1,6 +1,8 @@
 #include "TLB_wrap.h"
 #include "TLB.h"
 #include <boost/python/class.hpp>
+#include <boost/python/return_value_policy.hpp>
+#include <boost/python/manage_new_object.hpp>
 
 using namespace opencog;
 using namespace boost::python;
@@ -8,5 +10,8 @@ using namespace boost::python;
 void init_TLB_py()
 {
     class_<TLB>("TLB", no_init)
+        /*.def("getAtom", &TLB::getAtom,
+            return_value_policy<manage_new_object>())
+        .staticmethod("getAtom")*/
     ;
 }
