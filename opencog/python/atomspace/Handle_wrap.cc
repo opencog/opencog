4f0e2fa8170aba5f711ad14d1e29d423bc370315
kizzobot <davidpkilgore@gmail.com>
2009-07-26 10:41:38 -0700

    Exposed to-string methods of Handle class.

--- opencog/python/atomspace/Handle_wrap.cc
+++ opencog/python/atomspace/Handle_wrap.cc
@@ -1,5 +1,7 @@
 #include "Handle_wrap.h"
+#include "Handle.h"
 #include <boost/python/class.hpp>
+#include <boost/lexical_cast.hpp>
 
 using namespace opencog;
 using namespace boost::python;
@@ -9,5 +11,7 @@ void init_Handle_py()
     class_<Handle>("Handle", no_init)
         .def(init<const Handle&>())
         .def(init<>())
+        .def("str", &Handle::str)
+        .def("__str__", boost::lexical_cast<std::string, Handle const&>)
     ;
 }
--- opencog/python/atomspace/Handle_wrap.h
+++ opencog/python/atomspace/Handle_wrap.h
@@ -1,9 +1,6 @@
 #ifndef _OPENCOG_HANDLE_WRAP_H
 #define _OPENCOG_HANDLE_WRAP_H
 
-#include "Handle.h"
-using namespace opencog;
-
 void init_Handle_py();
 
 #endif // _OPENCOG_HANDLE_WRAP_H
