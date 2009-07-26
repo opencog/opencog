a9bb9d0819ace6de478e1491475ceeab1564945e
Linas Vepstas <linasvepstas@gmail.com>
2009-07-26 16:24:50 -0500

    Add license notices.

--- opencog/query/CrispLogicPMCB.h
+++ opencog/query/CrispLogicPMCB.h
@@ -1,7 +1,25 @@
 /*
  * CrispLogicPMCB.h
  *
- * Linas Vepstas January, March 2009
+ * Copyright (C) 2009 Linas Vepstas <linasvepstas@gmail.com>
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Created by Linas Vepstas January, March 2009
  */
 
 #ifndef _OPENCOG_CRISP_LOGIC_PATTERN_MATCH_H
--- opencog/query/OutgoingTree.h
+++ opencog/query/OutgoingTree.h
@@ -3,7 +3,25 @@
  *
  * Utilities for navigating a tree of outgoing edges.
  *
- * Linas Vepstas February 2008
+ * Copyright (C) 2009 Linas Vepstas <linasvepstas@gmail.com>
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Created by Linas Vepstas February 2008
  */
 
 #ifndef _OPENCOG_OUTGOING_TREE_H
