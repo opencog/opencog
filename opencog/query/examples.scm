ff56060b737c66706f363d06f90b2c01f041c208
Linas Vepstas <linasvepstas@gmail.com>
2009-07-26 21:38:26 -0500

    tweak the examples a bit.

--- opencog/query/examples.scm
+++ opencog/query/examples.scm
@@ -12,6 +12,11 @@
     (PredicateNode "grab@a6460c2d-b5f8-4287-8882-028d12de42d2_Manipulation")
     (DefinedFrameNode "#Manipulation")
 )
+(FrameElementLink (stv 1.0 1.0) (cog-new-av 1 0 #f)
+   (DefinedFrameNode "#Manipulation")
+   (DefinedFrameElementNode "#Manipulation:Agent")
+)
+
 
 ;; Example of an implication with just one predicate
 (define v
@@ -78,48 +83,27 @@
 )
 
 
-(define z
-  (ImplicationLink
-     (AndLink 
-       (InheritanceLink  (stv 1 1)
-    (PredicateNode "grab@a6460c2d-b5f8-4287-8882-028d12de42d2_Manipulation")
-         (DefinedFrameNode "#Manipulation")
-       )
-       (InheritanceLink (stv 1 1)
-         (VariableNode "$predicateNode")
-         (DefinedFrameNode "#Manipulation")
-       )
-     )
-     (VariableNode "$predicateNode")
-  )
-)
-
-
-(FrameElementLink (stv 1.0 1.0) (cog-new-av 1 0 #f)
-    (DefinedFrameNode "#Manipulation")
-    (DefinedFrameElementNode "#Manipulation:Agent")
- )
-
 (define w
- (ImplicationLink
-                 (AndLink
-                  (InheritanceLink
-                   (VariableNode "$framePredicateNode")
-                   (DefinedFrameNode "#Manipulation")
-                   )
-                  (InheritanceLink
-                   (VariableNode "$frameAgentPredicateNode")
-                   (DefinedFrameElementNode "#Manipulation:Agent")
-                   )
-                  
-                  (FrameElementLink
-                   (VariableNode "$framePredicateNode")
-                   (VariableNode "$frameAgentPredicateNode")
-                   )
-                  (FrameElementLink
-                   (PredicateNode "grab@a6460c2d-b5f8-4287-8882-028d12de42d2_Manipulation")
-                   (VariableNode "$frameAgentPredicateNode")
-                   )
-                  )
-     (VariableNode "$frameAgentPredicateNode")
-))
+   (ImplicationLink
+      (AndLink
+         (InheritanceLink
+            (VariableNode "$framePredicateNode")
+            (DefinedFrameNode "#Manipulation")
+         )
+         (InheritanceLink
+            (VariableNode "$frameAgentPredicateNode")
+            (DefinedFrameElementNode "#Manipulation:Agent")
+         )
+        
+         (FrameElementLink
+            (VariableNode "$framePredicateNode")
+            (VariableNode "$frameAgentPredicateNode")
+         )
+         (FrameElementLink
+            (PredicateNode "grab@a6460c2d-b5f8-4287-8882-028d12de42d2_Manipulation")
+            (VariableNode "$frameAgentPredicateNode")
+         )
+      )
+      (VariableNode "$frameAgentPredicateNode")
+   )
+)
