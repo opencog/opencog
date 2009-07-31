e6fca02a9d6f3d530f88a063de00783923067481
Linas Vepstas <linasvepstas@gmail.com>
2009-07-31 11:42:01 -0500

    some minor code formating, and a diatriabe about the future of this code.

--- opencog/atomspace/CoreUtils.cc
+++ opencog/atomspace/CoreUtils.cc
@@ -31,17 +31,30 @@
 using namespace opencog;
 
 /* Module for including any core-specific common utilities */
+// XXX this should be deprecated/made obsolete ... Why?
+// Because its a bizarre hack having to do with how the
+// load-from-file code works. The load-from-file stuff
+// should go into its own directory. Why?  Because it is doing 
+// soemthing fairly "illegal" -- monkeying with Handle assignments
+// in really dangerous ways. -- and worse, in dangerous, 
+// hard-to-understand ways.  This means that whenever something
+// about TLB or Handles changes, this code is in the path of 
+// breaken stuff, and thus has to be repeatedly debugged and fixed. 
+// This sucks. Its a bad design.  The save-to-file, load-from-file
+// code needs to be redesigned, and this re-jiggering should go. 
+// At least, go away from here. XXXX
 void CoreUtils::updateHandle(Handle *handle, HandleMap<Atom *> *handles) throw (RuntimeException)
 {
     //printf("CoreUtils::updateHandle(%p)\n", *handle);
     //if (TLB::isInvalidHandle(*handle)) return;
 
+    Atom *a  = handles->get(*handle);
     // Assume that the HandleMap stores <Handle, Atom *> pairs ....
-    Handle newH = TLB::getHandle(handles->get(*handle));
+    Handle newH = TLB::getHandle(a);
     if (TLB::isValidHandle(newH)) {
         *handle = newH;
     } else {
-        newH = TLB::addAtom(handles->get(*handle));
+        newH = TLB::addAtom(a);
         if (TLB::isValidHandle(newH)) {
             *handle = newH;
         } else {
