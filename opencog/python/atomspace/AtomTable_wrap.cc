2a0dcb80d5e2a6415def587fdf412816a526b713
kizzobot <davidpkilgore@gmail.com>
2009-08-02 20:30:08 -0700

    Exposed getHandle() methods of AtomTable class.

--- opencog/python/atomspace/AtomTable_wrap.cc
+++ opencog/python/atomspace/AtomTable_wrap.cc
@@ -1,6 +1,8 @@
 #include "AtomTable_wrap.h"
 #include <opencog/atomspace/AtomTable.h>
 #include <boost/python/class.hpp>
+#include <boost/python/return_value_policy.hpp>
+#include <boost/python/manage_new_object.hpp>
 
 using namespace opencog;
 using namespace boost::python;
@@ -10,5 +12,21 @@ void init_AtomTable_py()
     class_<AtomTable, boost::noncopyable>("AtomTable", no_init)
         .def(init<optional<bool> >())
         .def("getSize", &AtomTable::getSize)
+        .def("getHandle",
+            (Handle (AtomTable::*)(const char*, Type) const)
+            &AtomTable::getHandle)
+        .def("getHandle",
+            (Handle (AtomTable::*)(const Node*) const)
+            &AtomTable::getHandle)
+        .def("getHandle",
+            (Handle (AtomTable::*)(Type, const HandleSeq &seq) const)
+            &AtomTable::getHandle)
+        .def("getHandle",
+            (Handle (AtomTable::*)(const Link*) const)
+            &AtomTable::getHandle)
+        .def("getHandleSet",
+            (HandleEntry* (AtomTable::*)(Type, bool) const)
+            &AtomTable::getHandleSet,
+            return_value_policy<manage_new_object>())
     ;
 }
