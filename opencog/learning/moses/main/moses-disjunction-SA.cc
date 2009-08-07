b70ce4b97e59e2395d70bbb95a7783c0abca7836
xiaohui-zju <xiaohui.zju@gmail.com>
2009-08-07 15:38:02 +0800

    add moses-disjunction-SA for test the SA algorithm

--- opencog/learning/moses/main/moses-disjunction-SA.cc
+++ opencog/learning/moses/main/moses-disjunction-SA.cc
@@ -37,14 +37,26 @@ using namespace std;
 
 int main(int argc,char** argv) { 
   int arity,max_evals,rand_seed;
+  double min_temp, init_temp, temp_step_size;
+  double accept_prob_temp_intensity, dist_temp_intensity, fraction_of_remaining;
+      
   try {
-    if (argc!=4)
+    if (argc!= 10)
       throw "foo";
-    rand_seed=lexical_cast<int>(argv[1]);
-    arity=lexical_cast<int>(argv[2]);
-    max_evals=lexical_cast<int>(argv[3]);
+    rand_seed = lexical_cast<int>(argv[1]);
+    arity = lexical_cast<int>(argv[2]);
+    max_evals = lexical_cast<int>(argv[3]);
+    init_temp = lexical_cast<double>(argv[4]);
+    min_temp = lexical_cast<double>(argv[5]);
+    temp_step_size = lexical_cast<double>(argv[6]);
+    accept_prob_temp_intensity = lexical_cast<double>(argv[7]);
+    dist_temp_intensity = lexical_cast<double>(argv[8]);
+    fraction_of_remaining = lexical_cast<double>(argv[9]);
+
   } catch (...) {
-    cerr << "usage: " << argv[0] << " seed arity maxevals" << endl;
+    cerr << "usage: " << argv[0] 
+         << " seed  arity  maxevals  init_temp  min_temp  temp_step_size" 
+         << " accept_prob_temp_intensity(0~1) dist_temp_intensity(0~1) fraction_of_remaing" << endl;
     exit(1);
   }
 
@@ -55,18 +67,15 @@ int main(int argc,char** argv) {
   type_tree tt(id::lambda_type);
   tt.append_children(tt.begin(),id::boolean_type,arity+1);
 
-  metapopulation<logical_score,logical_bscore,iterative_hillclimbing> 
-      metapop(rng,combo_tree(id::logical_and),tt,logical_reduction(),
-              logical_score(scorer,arity,rng),
-              logical_bscore(scorer,arity,rng),
-              iterative_hillclimbing(rng));
-
-  /* metapopulation<logical_score, logical_bscore, simulated_annealing>
+  metapopulation<logical_score, logical_bscore, simulated_annealing>
       metapop(rng, combo_tree(id::logical_and), tt, logical_reduction(),
               logical_score(scorer, arity, rng),
               logical_bscore(scorer, arity, rng),
-              simulated_annealing(rng));
-  */
+              simulated_annealing(rng, init_temp, min_temp, temp_step_size,
+                                  accept_prob_temp_intensity,
+                                  dist_temp_intensity,
+                                  fraction_of_remaining)
+                                  );
   
   cout << "build metapop" << endl;
 
