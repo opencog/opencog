43097f4187115e607063ea9d119fc11ed0d15c58
Linas Vepstas <linasvepstas@gmail.com>
2009-09-02 17:02:16 -0500

    provide a prep-rule template

--- opencog/nlp/triples/prep-rules.scm
+++ opencog/nlp/triples/prep-rules.scm
@@ -11,8 +11,21 @@
 ; The following rules create word-preposition linkages; these must
 ; be created in advance, before the later rules run. These do not
 ; need to be precise (its OK to generate some garbage, I think(??)
-; but its important that all prep phrases be considered
+; but its important that all prep phrases be considered.
 ;
+(define (prep-rule-template rel a b)
+	(r-varscope
+		(r-and
+			(r-anchor-trips "$sent")
+			(r-decl-word-inst "$var1" "$sent")
+			(r-rlx rel a b)
+			(r-rlx "$prep" "$var1" "$var2")
+			(r-decl-lemma "$var1" "$word1")
+		)
+		(r-schema "scm:make-prep-phrase" "$word1" "$prep")
+	)
+)
+
 ; -----------------------------------------------------------
 ; # IF %ListLink("# APPLY TRIPLE RULES", $sent)
 ;       ^ %WordInstanceLink($var1,$sent)  ; limit scope of rule
@@ -22,16 +35,7 @@
 ;       THEN ^3_&scm:make-prep-phrase($word1,$prep)
 
 (define prep-rule-0
-	(r-varscope
-		(r-and
-			(r-anchor-trips "$sent")
-			(r-decl-word-inst "$var1" "$sent")
-			(r-rlx "_obj" "be" "$var1")
-			(r-rlx "$prep" "$var1" "$var2")
-			(r-decl-lemma "$var1" "$word1")
-		)
-		(r-schema "scm:make-prep-phrase" "$word1" "$prep")
-	)
+	(prep-rule-template "_obj" "be" "$var1")
 )
 
 ; -----------------------------------------------------------
@@ -43,16 +47,7 @@
 ;       THEN ^3_&scm:make-prep-phrase($word1,$prep)
 
 (define prep-rule-1
-	(r-varscope
-		(r-and
-			(r-anchor-trips "$sent")
-			(r-decl-word-inst "$var1" "$sent")
-			(r-rlx "_subj" "be" "$var1")
-			(r-rlx "$prep" "$var1" "$var2")
-			(r-decl-lemma "$var1" "$word1")
-		)
-		(r-schema "scm:make-prep-phrase" "$word1" "$prep")
-	)
+	(prep-rule-template "_subj" "be" "$var1")
 )
 
 ; -----------------------------------------------------------
@@ -63,12 +58,22 @@
 ;       ^ $prep($var1,$var2)              ; preposition 
 ;       ^ %LemmaLink($var1,$word1)        ; word of word instance
 ;       THEN ^3_&scm:make-prep-phrase($word1,$prep)
-; 
+ 
+(define prep-rule-2
+	(prep-rule-template "_predadj" "$var1" "$var0")
+)
+
+; -----------------------------------------------------------
 ; ; Sentence: "Pottery is made from clay."
 ; # IF %ListLink("# APPLY TRIPLE RULES", $sent)
 ;       ^ %WordInstanceLink($var1,$sent)  ; limit scope of rule
-;       ^ _obj($var0,$var1)
-;       ^ $prep($var0,$var2)              ; preposition 
-;       ^ %LemmaLink($var0,$word0)        ; word of word instance
-;       THEN ^3_&scm:make-prep-phrase($word0,$prep)
-; 
+;       ^ _obj($var1,$var0)
+;       ^ $prep($var1,$var2)              ; preposition 
+;       ^ %LemmaLink($var1,$word1)        ; word of word instance
+;       THEN ^3_&scm:make-prep-phrase($word1,$prep)
+ 
+(define prep-rule-3
+	(prep-rule-template "_obj" "$var1" "$var0")
+)
+
+; ----------------------- END OF FILE -----------------------
