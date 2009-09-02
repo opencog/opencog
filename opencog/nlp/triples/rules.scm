9e4f53bd80f3fb359c40de7449451628a2f49c5a
Linas Vepstas <linasvepstas@gmail.com>
2009-09-02 10:27:53 -0500

    bug fix and more work

--- opencog/nlp/triples/rules.scm
+++ opencog/nlp/triples/rules.scm
@@ -117,7 +117,7 @@
 		; The clauses needed to work with this relex expression
 		(define clauses
 			(cond
-				((and av bv) pred)
+				((and av bv) (list pred))
 				((and (not av) bv)
 					(list pred (lem avn a))
 				)
@@ -268,28 +268,49 @@
 		(r-decl-var "WordInstanceNode" word-inst)
 	)
 )
+
+; -----------------------------------------------------------------
+; r-decl-lemma -- declare a lemma of a word-instance
+;
+(define (r-decl-lemma word-inst lemma)
+	(r-and
+		(r-link LemmaLink word-inst lemma)
+		(r-decl-var "WordNode" lemma)
+	)
+)
 ; -----------------------------------------------------------------
 (define (x)
 	; Sentence: "Lisbon is the capital of Portugaul"
+	; The RelEx parse is:
 	;   _subj(be, Lisbon)
 	;   _obj(be, capital)
 	;   of(capital, Portugaul)
+	;
+	; We expect the following variable grounding:
 	; var0=Lisbon, var1=capital var2=Portugaul
+	;
 	(r-and
 		; We are looking for sentences anchored to the 
-		; triples-processing ; node
+		; triples-processing node.
 		(r-anchor-trips "$sent")
 	
-		; $var0 and $var1 must belong to the same sentence
+		; $var0 and $var1 are word-instances that must 
+		; belong to the same sentence.
 		(r-decl-word-inst "$var0" "$sent") 
 		(r-decl-word-inst "$var1" "$sent")
 	
-		; Match subject and object as indicated above
+		; Match subject and object as indicated above.
 		(r-rlx "_subj" "be" "$var0")
 		(r-rlx "_obj" "be" "$var1")
 
 		; Match the proposition
 		(r-rlx "$prep" "$var1" "$var2")
+
+		; Get the lemma form of the word instance
+		(r-decl-lemma "$var1" "$word1")
+
+		; Convert to a phrase
+		(r-rlx "$phrase" "$word1" "$prep")
 	)
 )
 
