b938e5e22314000fb1353afcd43f25ffb1098b2f
Samir Araujo <samir.araujo@gmail.com>
2009-09-24 20:27:59 -0300

    - fixed a bug in the NLGenClient constructor

--- opencog/embodiment/Control/OperationalPetController/NLGenClient.cc
+++ opencog/embodiment/Control/OperationalPetController/NLGenClient.cc
@@ -35,6 +35,8 @@ NLGenClient::NLGenClient(const std::string& host, unsigned int port)
     
     this->port = port;
     this->host = host;
+    this->connected = false;
+    this->socket = NULL;
     connect();
 #endif
 }
@@ -43,7 +45,9 @@ NLGenClient::~NLGenClient()
 {
 #if BOOST_MINOR_VERSION >= BOOST_ACCEPTED_VERSION
     logger().debug("[NLGenClient.%s] - closing socket connection",__FUNCTION__);
-    socket->close();
+    if ( this->connected ) {
+        socket->close();
+    } // if
     delete socket;
     logger().debug("[NLGenClient.%s] - socket connection closed with success",__FUNCTION__);
 #endif
