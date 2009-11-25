471b0d9b16e3180205e6e0ee631b8e2b78714a43
Linas Vepstas <linasvepstas@gmail.com>
2009-11-25 15:12:42 -0600

    revert previous whitespace changes to the way that I like it. Arghhhh.

--- opencog/guile/SchemeSmob.h
+++ opencog/guile/SchemeSmob.h
@@ -30,129 +30,129 @@ class Atom;
 
 class SchemeSmob
 {
-    friend class SchemeEval;
-
-private:
-
-    enum {
-        COG_HANDLE = 1,
-        COG_TV,  // truth values
-        COG_VH,  // version handles
-        COG_AV,  // attention values
-    };
-
-    static bool is_inited;
-    static void register_procs(void);
-
-    // The handle tag is for opencog handles, only.
-    static scm_t_bits cog_handle_tag;
-
-    // The cog_misc_tag are for all other opencog types, such
-    // as truth values, which are ephemeral (garbage-collected)
-    static scm_t_bits cog_misc_tag;
-
-    // Initialization functions
-    static void init_smob_type(void);
-
-    static int print_atom(SCM, SCM, scm_print_state *);
-    static SCM equalp_atom(SCM, SCM);
-    static size_t free_atom(SCM);
-    static int print_misc(SCM, SCM, scm_print_state *);
-    static SCM mark_misc(SCM);
-    static size_t free_misc(SCM);
-
-    // Atom creation and deletion functions
-    static SCM ss_new_node(SCM, SCM, SCM);
-    static SCM ss_new_link(SCM, SCM);
-    static SCM ss_node(SCM, SCM, SCM);
-    static SCM ss_link(SCM, SCM);
-    static SCM ss_delete(SCM);
-    static SCM ss_delete_recursive(SCM);
-    static SCM ss_atom_p(SCM);
-    static SCM ss_node_p(SCM);
-    static SCM ss_link_p(SCM);
-    
-    // Atoms to ints, and back.
-    static SCM ss_atom(SCM);
-    static SCM ss_handle(SCM);
-
-    // Set properties of atoms
-    static SCM ss_set_av(SCM, SCM);
-    static SCM ss_set_tv(SCM, SCM);
-
-    // Atom properties
-    static SCM ss_name(SCM);
-    static SCM ss_type(SCM);
-    static SCM ss_arity(SCM);
-    static SCM ss_av(SCM);
-    static SCM ss_tv(SCM);
-    static SCM ss_incoming_set(SCM);
-    static SCM ss_outgoing_set(SCM);
-
-    // AtomTable query functions
-    static SCM ss_map_type(SCM, SCM);
-    static SCM ss_get_types(void);
-    static SCM ss_type_p(SCM);
-    static SCM ss_get_subtypes(SCM);
-    static SCM ss_subtype_p(SCM, SCM);
-
-    // Truth values
-    static SCM ss_new_stv(SCM, SCM);
-    static SCM ss_new_ctv(SCM, SCM, SCM);
-    static SCM ss_new_itv(SCM, SCM, SCM);
-    static SCM ss_tv_p(SCM);
-    static SCM tv_p(SCM, TruthValueType);
-    static SCM ss_stv_p(SCM);
-    static SCM ss_ctv_p(SCM);
-    static SCM ss_itv_p(SCM);
-    static SCM take_tv(TruthValue *);
-    static SCM ss_tv_get_value(SCM);
-
-    // Version handles
-    static SCM ss_new_vh(SCM, SCM);
-    static SCM ss_vh_p(SCM);
-    static SCM take_vh(VersionHandle *);
-    static SCM ss_vh_get_value(SCM);
-
-    // Attention values
-    static SCM ss_new_av(SCM, SCM, SCM);
-    static SCM ss_av_p(SCM);
-    static SCM take_av(AttentionValue *);
-    static SCM ss_av_get_value(SCM);
-
-    // Callback into misc C++ code.
-    static SCM ss_ad_hoc(SCM, SCM);
-    static SCM pln_bc(SCM, SCM);
-
-    // Misc utilities
-    static std::string decode_string (SCM, const char *, const char *);
-    static std::string to_string(SCM);
-    static std::string handle_to_string(SCM);
-    static std::string handle_to_string(Handle, int);
-    static std::string misc_to_string(SCM);
-    static TruthValue *get_tv_from_list(SCM);
-    static AttentionValue *get_av_from_list(SCM);
-    
-    static Type validate_atom(SCM, const char *);
-    static Type validate_node(SCM, const char *);
-    static Atom * verify_atom(SCM, const char *);
-    static Handle verify_handle(SCM, const char *);
-    static TruthValue * verify_tv(SCM, const char *);
-    static AttentionValue * verify_av(SCM, const char *);
-    static std::vector<Handle> decode_handle_list (SCM, const char *);
-
-public:
-    // Helper functions
-    static SCM handle_to_scm(Handle);
-    static Handle scm_to_handle(SCM);
-
-    static void init(void);
-    SchemeSmob(void);
-
-    static std::string to_string(Handle);
-    static std::string av_to_string(const AttentionValue *);
-    static std::string tv_to_string(const TruthValue *);
-    static std::string vh_to_string(const VersionHandle *);
+		friend class SchemeEval;
+	
+	private:
+	
+		enum {
+			COG_HANDLE = 1,
+			COG_TV,  // truth values
+			COG_VH,  // version handles
+			COG_AV,  // attention values
+		};
+	
+		static bool is_inited;
+		static void register_procs(void);
+	
+		// The handle tag is for opencog handles, only.
+		static scm_t_bits cog_handle_tag;
+	
+		// The cog_misc_tag are for all other opencog types, such
+		// as truth values, which are ephemeral (garbage-collected)
+		static scm_t_bits cog_misc_tag;
+	
+		// Initialization functions
+		static void init_smob_type(void);
+	
+		static int print_atom(SCM, SCM, scm_print_state *);
+		static SCM equalp_atom(SCM, SCM);
+		static size_t free_atom(SCM);
+		static int print_misc(SCM, SCM, scm_print_state *);
+		static SCM mark_misc(SCM);
+		static size_t free_misc(SCM);
+	
+		// Atom creation and deletion functions
+		static SCM ss_new_node(SCM, SCM, SCM);
+		static SCM ss_new_link(SCM, SCM);
+		static SCM ss_node(SCM, SCM, SCM);
+		static SCM ss_link(SCM, SCM);
+		static SCM ss_delete(SCM);
+		static SCM ss_delete_recursive(SCM);
+		static SCM ss_atom_p(SCM);
+		static SCM ss_node_p(SCM);
+		static SCM ss_link_p(SCM);
+		
+		// Atoms to ints, and back.
+		static SCM ss_atom(SCM);
+		static SCM ss_handle(SCM);
+	
+		// Set properties of atoms
+		static SCM ss_set_av(SCM, SCM);
+		static SCM ss_set_tv(SCM, SCM);
+	
+		// Atom properties
+		static SCM ss_name(SCM);
+		static SCM ss_type(SCM);
+		static SCM ss_arity(SCM);
+		static SCM ss_av(SCM);
+		static SCM ss_tv(SCM);
+		static SCM ss_incoming_set(SCM);
+		static SCM ss_outgoing_set(SCM);
+	
+		// AtomTable query functions
+		static SCM ss_map_type(SCM, SCM);
+		static SCM ss_get_types(void);
+		static SCM ss_type_p(SCM);
+		static SCM ss_get_subtypes(SCM);
+		static SCM ss_subtype_p(SCM, SCM);
+	
+		// Truth values
+		static SCM ss_new_stv(SCM, SCM);
+		static SCM ss_new_ctv(SCM, SCM, SCM);
+		static SCM ss_new_itv(SCM, SCM, SCM);
+		static SCM ss_tv_p(SCM);
+		static SCM tv_p(SCM, TruthValueType);
+		static SCM ss_stv_p(SCM);
+		static SCM ss_ctv_p(SCM);
+		static SCM ss_itv_p(SCM);
+		static SCM take_tv(TruthValue *);
+		static SCM ss_tv_get_value(SCM);
+	
+		// Version handles
+		static SCM ss_new_vh(SCM, SCM);
+		static SCM ss_vh_p(SCM);
+		static SCM take_vh(VersionHandle *);
+		static SCM ss_vh_get_value(SCM);
+	
+		// Attention values
+		static SCM ss_new_av(SCM, SCM, SCM);
+		static SCM ss_av_p(SCM);
+		static SCM take_av(AttentionValue *);
+		static SCM ss_av_get_value(SCM);
+	
+		// Callback into misc C++ code.
+		static SCM ss_ad_hoc(SCM, SCM);
+		static SCM pln_bc(SCM, SCM);
+	
+		// Misc utilities
+		static std::string decode_string (SCM, const char *, const char *);
+		static std::string to_string(SCM);
+		static std::string handle_to_string(SCM);
+		static std::string handle_to_string(Handle, int);
+		static std::string misc_to_string(SCM);
+		static TruthValue *get_tv_from_list(SCM);
+		static AttentionValue *get_av_from_list(SCM);
+		
+		static Type validate_atom(SCM, const char *);
+		static Type validate_node(SCM, const char *);
+		static Atom * verify_atom(SCM, const char *);
+		static Handle verify_handle(SCM, const char *);
+		static TruthValue * verify_tv(SCM, const char *);
+		static AttentionValue * verify_av(SCM, const char *);
+		static std::vector<Handle> decode_handle_list (SCM, const char *);
+	
+	public:
+		// Helper functions
+		static SCM handle_to_scm(Handle);
+		static Handle scm_to_handle(SCM);
+	
+		static void init(void);
+		SchemeSmob(void);
+	
+		static std::string to_string(Handle);
+		static std::string av_to_string(const AttentionValue *);
+		static std::string tv_to_string(const TruthValue *);
+		static std::string vh_to_string(const VersionHandle *);
 };
 
 } // namespace opencog
