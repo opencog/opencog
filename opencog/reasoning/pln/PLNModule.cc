4e5a14ce45affdadd2f110111e5d744b25e2efd8
Jared Wigmore <jared.wigmore@gmail.com>
2010-03-04 11:49:04 +1300

    Hack in support for loading .conf tests when using cogserver

--- opencog/reasoning/pln/PLNModule.cc
+++ opencog/reasoning/pln/PLNModule.cc
@@ -46,6 +46,10 @@
 
 #include <opencog/adaptors/tulip/TulipWriter.h>
 
+//! @todo For PLN testing. Maybe move it to another file used by this module and PLNUTest
+#include <opencog/guile/SchemeEval.h>
+#include <opencog/server/load-file.h>
+
 using namespace opencog;
 using namespace opencog::pln;
 
@@ -288,6 +292,130 @@ Handle opencog::pln::applyRule(const string& ruleName,
         return vhp.first;
     }
     else return Handle::UNDEFINED;
+}
+
+//! @todo tacky hack implementation (refactor if it works)
+
+// Now uses SCM. Won't search any alternative locations for the file.
+void initAxiomSet(string premiseFile)
+{
+    AtomSpaceWrapper *atw = GET_ASW;
+    atw->reset();
+    atw->allowFWVarsInAtomSpace = true;
+
+    //cprintf(-2,"loading...\n");
+    std::cout << "loading " << premiseFile << std::endl;
+
+#if 0
+    atw->archiveTheorems = true;
+    bool axioms_ok = atw->loadAxioms(premiseFile);
+    if (!axioms_ok) throw std::string("failed to load file");
+    atw->archiveTheorems = false;
+
+#elif HAVE_GUILE
+    int rc = load_scm_file(premiseFile.c_str());
+
+    atw->archiveTheorems = true;
+    atw->makeCrispTheorems();
+    atw->archiveTheorems = false;
+
+    // The error-checking is after the call to makeCrispTheorems, because
+    // if there was an error, then the AS is now empty and makeCrispTheorems
+    // will (correctly) record that there are none.
+    if (rc) throw std::string("failed to load file");
+#else
+    throw std::string("Not allowed to use XML or SCM!");
+#endif
+
+    cprintf(-2,"%s loaded. Next test: ", premiseFile.c_str());
+}
+
+float getCount(float c)
+{ return SimpleTruthValue::confidenceToCount(c); }
+
+
+//! @todo Replace with an existing method, after tweaking various things to
+//! make it suitable.
+static SimpleTruthValue* parseSTV(const char* tvStr) {
+    float mean, conf;
+    sscanf(tvStr, "[%f,%f]", &mean, &conf);
+    return new SimpleTruthValue(mean, getCount(conf));
+}
+
+
+
+//! @todo tacky hack implementation (refactor if it works)
+void runSCMTarget(const char* conf_file)
+{
+#ifdef HAVE_GUILE
+	//!@ todo haxx
+	FitnessEvaluatorT testFitnessEvaluator = BEST;
+
+    Config test;
+    std::cout << conf_file << std::endl;
+    test.load(conf_file);
+    //std::cout << test.to_string() << std::endl;
+
+    std::cout << test["COMMENT"] << std::endl;
+
+    //std::string axiomsFile = std::string(PLN_TEST_DIR)+test["LOAD"]+".xml";
+    //! @todo define this somewhere else.
+    std::string PLN_TEST_DIR = "../tests/reasoning/pln/";
+    std::string axiomsFile = PLN_TEST_DIR+"scm/"+test["LOAD"]+".scm";
+
+    initAxiomSet(axiomsFile);
+
+    std::string targetScheme = test["TARGET"];
+	std::cout << "Target: " << targetScheme << std::endl;
+
+    // get target atom
+    SchemeEval& eval = SchemeEval::instance();
+    Handle h = eval.eval_h(targetScheme);
+
+    pHandleSeq fakeHandles =
+        ((AtomSpaceWrapper*)ASW())->realToFakeHandle(h);
+    pHandle fakeHandle = fakeHandles[0];
+    //meta target(new vtree(fakeHandle));
+
+    meta target_(new vtree(fakeHandle));
+
+    meta target = ForceAllLinksVirtual(target_);
+
+    // Set parameters
+    uint maxSteps = test.get_int("MAX_STEPS");
+/*
+    // Make sure FC doesn't take too long / only test it on simpler tests.
+    // It takes disproportionately long once the AtomSpace gets large, due
+    // to the indexes probably.
+    if (!TESTING_BACKWARD_CHAINING && maxSteps > 201) {
+        std::cout << "Skipping test due to time constraints" << std::endl;
+        return; //minEvalsOfFittestBIT = 500;
+    }
+*/
+    uint minEvalsOfFittestBIT = maxSteps/100;
+
+    // Changed because we don't want to have to specify the count
+    // as well, in the test's .conf file
+    //TruthValue* minTV = SimpleTruthValue::fromString(test["MIN_TV"].c_str());
+    //TruthValue* maxTV = SimpleTruthValue::fromString(test["MAX_TV"].c_str());
+    TruthValue* minTV = parseSTV(test["MIN_TV"].c_str());
+    TruthValue* maxTV = parseSTV(test["MAX_TV"].c_str());
+
+    std::cout << "Loaded parameters of test OK" << std::endl;
+
+
+
+    state = new BITNodeRoot(target, new DefaultVariableRuleProvider(), config().get_bool("PLN_RECORD_TRAILS"), testFitnessEvaluator);
+    Bstate.reset(state);
+
+
+/*    // Run the test using runPLNTest (via the maketest macro)
+    maketest(target,
+             minTV,
+             maxTV,
+             minEvalsOfFittestBIT,
+             0); // Alternative (not used) method for limiting test time*/
+#endif // HAVE_GUILE
 }
 
 //! Used by forward chainer 
@@ -601,6 +729,10 @@ std::string PLNModule::runCommand(std::list<std::string> args)
             state = Bstate.get();
             ss << "Test target set:" << endl;
             ss << rawPrint(*tests[test_i],tests[test_i]->begin(),0);
+        }
+        else if (c == "load-test") {
+        	input(temps, args);
+        	runSCMTarget(temps.c_str());
         }
         else if (c == "=") { input(h, args); input(h2, args); 
 /*          cprintf(0, ((BITNodeRoot*)h)->eq((BITNodeRoot*)h2) ? "EQ\n" : "IN-EQ\n"); */
