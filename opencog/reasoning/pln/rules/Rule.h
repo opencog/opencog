3b73accc867ee8995976d34e54418f8b6814d8b6
Jared Wigmore <jared.wigmore@gmail.com>
2010-04-23 17:24:21 +1200

    Meta-woops. Make fullInputFilter virtual

--- opencog/reasoning/pln/rules/Rule.h
+++ opencog/reasoning/pln/rules/Rule.h
@@ -222,7 +222,7 @@ public:
     //! Get the inputFilter for this Rule.
     MPsIn& getInputFilter() { return inputFilter; }
 
-    setOfMPs fullInputFilter() const;
+    virtual setOfMPs fullInputFilter() const;
 
     //! Does the Rule have unfixed input arity?
     bool hasFreeInputArity() const { return freeInputArity; }
