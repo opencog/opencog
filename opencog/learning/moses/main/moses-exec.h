64d2727801ca7335634127f618d638de2503fc35
Nil Geisweiller <nilg@nilg-laptop>
2010-08-26 09:42:28 +0300

    MOSES: removed cache failure log if using distributed moses

--- opencog/learning/moses/main/moses-exec.h
+++ opencog/learning/moses/main/moses-exec.h
@@ -193,9 +193,12 @@ void metapop_moses_results(opencog::RandGen& rng,
                               score_cache, bscore_cache, opt_algo,
                               eda_param, meta_param, moses_param, vm, pa);
         // log the number of cache failures
-        logger().info("Number of cache failures for score = %u and bscore = %u",
-                      score_cache.get_cache_failures(),
-                      bscore_cache.get_cache_failures());
+        if(pa.jobs.empty()) { // do not print the cache if using distributed moses
+            logger().info("Number of cache failures for score = %u"
+                          " and bscore = %u",
+                          score_cache.get_cache_failures(),
+                          bscore_cache.get_cache_failures());
+        }
     } else {
         bscore_based_score<BScore> score(bsc);
         metapop_moses_results(rng, bases, tt, si_ca, si_kb, score, bsc,
