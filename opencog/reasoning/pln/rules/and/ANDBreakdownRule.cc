512a2c2cc894be83cbd928d8c166e7618604a741
Jared Wigmore <jared.wigmore@gmail.com>
2010-09-14 10:33:04 +1200

    using template<int N> is tentatively evil (defeats the debugger, and means you can't create them with a for loop)

--- opencog/reasoning/pln/rules/and/ANDBreakdownRule.cc
+++ opencog/reasoning/pln/rules/and/ANDBreakdownRule.cc
@@ -37,4 +37,99 @@ namespace opencog { namespace pln {
     MPs->push_back(
 }*/
 
+Rule::setOfMPs ANDBreakdownRule::o2iMetaExtra(meta outh, bool& overrideInputFilter) const
+{
+    /// And parameters aren't ordered. Therefore, the order in which we feed them
+    /// here is irrelavent. But we need a hypothetical parameter that will later remind
+    /// which kind of atom we need to produce.
+
+    ///haxx:: (would also ignore nested ANDLinks)
+    // Mainly to decrease the combinatorial explosion by not producing infinite
+    // series of ANDBDRules within the BIT.
+    if (asw->isSubType(_v2h(*outh->begin()), AND_LINK))
+        return Rule::setOfMPs();
+
+    MPs ret;
+    BBvtree andlink(new BoundVTree);
+
+    andlink->set_head(Vertex((pHandle)AND_LINK));
+
+    andlink->append_child(andlink->begin(), outh->begin());
+
+    for (uint i = 1; i < N; i++)
+        andlink->append_child(andlink->begin(),
+                              BoundVTree(CreateVar(asw)).begin());
+
+    ret.push_back(andlink);
+    ret.push_back(BBvtree(new BoundVTree(mva((pHandle)HYPOTHETICAL_LINK,*outh))));
+
+    overrideInputFilter = true;
+
+    return makeSingletonSet(ret);
+}
+
+BoundVertex ANDBreakdownRule::compute(const std::vector<Vertex>& premiseArray,
+                    pHandle CX, bool fresh) const
+{
+    std::vector<pHandle> hs = asw->getOutgoing(boost::get<pHandle>(premiseArray[0]));
+
+    assert(premiseArray.size() == 2);
+    assert(hs.size() == N);
+    assert(asw->getArity(boost::get<pHandle>(premiseArray[1])) == 1);
+
+    atom topological_model(asw->getOutgoing(boost::get<pHandle>(premiseArray[1]))[0]);
+
+    for (uint i = 0; i < hs.size(); i++)
+        if (atom(hs[i]) == topological_model) {
+            pHandle a = hs[i];
+            TruthValue* resultTV;
+            TruthValue* tvs[1];
+
+            tvs[0] = asw->getTV(_v2h(premiseArray[0])).clone();
+            resultTV = formula.compute(tvs, 1);
+
+            //! @todo More pHandles messiness.
+            Handle realHandle = asw->fakeToRealHandle(a).first;
+            atomspace().setTV(realHandle, *resultTV);
+            //asw->addAtom(mva(hs[i]), *tv, false);
+
+            delete[] &tvs;
+            delete resultTV;
+
+            return BoundVertex(a);
+        }
+
+/*LOG(0,"Topo model was:");
+      printAtomTree(topological_model,0,0);
+      LOG(0,"hs:");
+      for (uint i = 0; i < hs.size(); i++)
+      printTree(hs[i],0,0);*/
+
+//LOG(0, "ANDBREAKDOWN: NO TOPOLOGICAL MODEL FOUND!");
+    assert(0);
+
+    return Vertex((pHandle)PHANDLE_UNDEFINED);
+
+    /*  std::vector<Handle> hs = asw->getOutgoing(premiseArray[0]);
+
+assert(premiseArray.size() == 2);
+assert(hs.size() == N);
+assert(asw->getArity(premiseArray[1]) == 1);
+
+tree<Vertex>::iterator top0 = premiseArray[0].begin();
+
+tree<Vertex> topological_model(premiseArray[1])[0]
+tree<Vertex>::iterator topological_model = (premiseArray[1])[0];
+
+for (int i = 0; i < hs.size(); i++)
+for (tree<Vertex>::sibling_iterator i = premiseArray[0].begin();
+        i != premiseArray[0].end(); i++)
+    if (*i == topological_model)
+        return *i;
+
+//LOG(0, "ANDBREAKDOWN: NO TOPOLOGICAL MODEL FOUND!");
+assert(0);*/
+}
+
+
 }} // namespace opencog { namespace pln {
--- opencog/reasoning/pln/rules/and/ANDBreakdownRule.h
+++ opencog/reasoning/pln/rules/and/ANDBreakdownRule.h
@@ -31,15 +31,15 @@ Rule::setOfMPs makeSingletonSet(Rule::MPs& mp);
 
 /// Produces A given an ANDLink containing A (and N other Atoms)
 /// You want multiple copies of this Rule, for different N.
-template<int N>
 class ANDBreakdownRule : public Rule
 {
 protected:
     ANDBreakdownFormula formula;
+    const uint N;
     
 public:
-    ANDBreakdownRule(AtomSpaceWrapper *_asw)
-	: Rule(_asw,true,true,"")
+    ANDBreakdownRule(AtomSpaceWrapper *_asw, int _N)
+	: Rule(_asw,true,true,""), N(_N)
     {
         name = "ANDBreakdownRule/" + i2str(N);
 	
@@ -55,102 +55,13 @@ public:
     
     bool validate2(MPs& args) const { return true; }
     
-    Rule::setOfMPs o2iMetaExtra(meta outh, bool& overrideInputFilter) const
-    {
-        /// And parameters aren't ordered. Therefore, the order in which we feed them
-        /// here is irrelavent. But we need a hypothetical parameter that will later remind
-        /// which kind of atom we need to produce.
-        
-        ///haxx:: (would also ignore nested ANDLinks)
-        // Mainly to decrease the combinatorial explosion by not producing infinite
-        // series of ANDBDRules within the BIT.
-        if (asw->isSubType(_v2h(*outh->begin()), AND_LINK))
-            return Rule::setOfMPs();
-
-        MPs ret;
-        BBvtree andlink(new BoundVTree);
-        
-        andlink->set_head(Vertex((pHandle)AND_LINK));
-	
-        andlink->append_child(andlink->begin(), outh->begin());
-	
-        for (int i = 1; i < N; i++)
-            andlink->append_child(andlink->begin(),
-                                  BoundVTree(CreateVar(asw)).begin());
-        
-        ret.push_back(andlink);
-        ret.push_back(BBvtree(new BoundVTree(mva((pHandle)HYPOTHETICAL_LINK,*outh))));
-	
-        overrideInputFilter = true;	
-	
-        return makeSingletonSet(ret);
-    }
+    Rule::setOfMPs o2iMetaExtra(meta outh, bool& overrideInputFilter) const;
     
     NO_DIRECT_PRODUCTION;
     
     BoundVertex compute(const std::vector<Vertex>& premiseArray,
                         pHandle CX = PHANDLE_UNDEFINED,
-                        bool fresh = true) const
-    {
-        std::vector<pHandle> hs = asw->getOutgoing(boost::get<pHandle>(premiseArray[0]));
-        
-        assert(premiseArray.size() == 2);
-        assert(hs.size() == N);
-        assert(asw->getArity(boost::get<pHandle>(premiseArray[1])) == 1);	  	  
-        
-        atom topological_model(asw->getOutgoing(boost::get<pHandle>(premiseArray[1]))[0]);
-        
-        for (uint i = 0; i < hs.size(); i++)
-            if (atom(hs[i]) == topological_model) {
-                pHandle a = hs[i];
-                TruthValue* resultTV;
-                TruthValue* tvs[1];
-
-                tvs[0] = asw->getTV(_v2h(premiseArray[0])).clone();
-                resultTV = formula.compute(tvs, 1);
-
-                //! @todo More pHandles messiness.
-                Handle realHandle = asw->fakeToRealHandle(a).first;
-                atomspace().setTV(realHandle, *resultTV);
-                //asw->addAtom(mva(hs[i]), *tv, false);
-
-                delete[] &tvs;
-                delete resultTV;
-
-                return BoundVertex(a);
-            }
-        
-	/*LOG(0,"Topo model was:");
-          printAtomTree(topological_model,0,0);
-          LOG(0,"hs:");
-          for (uint i = 0; i < hs.size(); i++)
-          printTree(hs[i],0,0);*/
-	
-	//LOG(0, "ANDBREAKDOWN: NO TOPOLOGICAL MODEL FOUND!");
-        assert(0);
-        
-        return Vertex((pHandle)PHANDLE_UNDEFINED);
-	
-        /*	std::vector<Handle> hs = asw->getOutgoing(premiseArray[0]);
-
-	assert(premiseArray.size() == 2);
-	assert(hs.size() == N);
-	assert(asw->getArity(premiseArray[1]) == 1);
-	  
-	tree<Vertex>::iterator top0 = premiseArray[0].begin();
-
-	tree<Vertex> topological_model(premiseArray[1])[0]
-	tree<Vertex>::iterator topological_model = (premiseArray[1])[0];
-
-	for (int i = 0; i < hs.size(); i++)
-	for (tree<Vertex>::sibling_iterator i = premiseArray[0].begin();
-			i != premiseArray[0].end(); i++)		
-		if (*i == topological_model)
-			return *i;
-
-	//LOG(0, "ANDBREAKDOWN: NO TOPOLOGICAL MODEL FOUND!");
-	assert(0);*/
-  }
+                        bool fresh = true) const;
 };
 
 }} // namespace opencog { namespace pln {
