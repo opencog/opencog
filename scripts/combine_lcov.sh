3f5f88035626fa87ec3090dc9eb0f8f95270b1c2
Joel Pitt <joel@joelpitt.com>
2010-10-01 21:07:57 +1300

    combine script has var to indicate base dir for coverage output

--- scripts/combine_lcov.sh
+++ scripts/combine_lcov.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 # This script is used to merge separate lcov output from each test.
 # Once they are in a single file, we create the html output with genhtml
-rm -r ../lcov
+BASEDIR=.
+rm -r ${BASEDIR}/lcov
 
 ifiles=""
-for f in ../coverage/*.info; do
+for f in ${BASEDIR}/coverage/*.info; do
     ifiles="$ifiles -a $f"
 done
-lcov --directory . $ifiles --output-file ../coverage/alltemp.info
-lcov --directory . --remove ../coverage/alltemp.info /usr/include/\* --output-file ../coverage/all.info
-rm ../coverage/alltemp.info
+lcov --directory ${BASEDIR} $ifiles --output-file ${BASEDIR}/coverage/alltemp.info
+lcov --directory ${BASEDIR} --remove ${BASEDIR}/coverage/alltemp.info /usr/include/\* --output-file ${BASEDIR}/coverage/all.info
+rm ${BASEDIR}/coverage/alltemp.info
 
-genhtml -s -o ../lcov --demangle-cpp --num-spaces 4 --title "OpenCog Coverage Analysis" ../coverage/all.info
-mkdir -p ../coverage/old
-for f in ../coverage/*.info; do
-    mv $f ../coverage/old
+genhtml -s -o ${BASEDIR}/lcov --demangle-cpp --num-spaces 4 --title "OpenCog Coverage Analysis" ${BASEDIR}/coverage/all.info
+mkdir -p ${BASEDIR}/coverage/old
+for f in ${BASEDIR}/coverage/*.info; do
+    mv $f ${BASEDIR}/coverage/old
 done
 
