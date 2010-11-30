faa7899242279c633d06eb7a672da595fd5f8ff7
Nil Geisweiller <ngeiswei@gmail.com>
2010-11-30 15:04:27 +0200

    added TVs in the ridingBicycle example

--- tests/reasoning/pln/scm/new/ridingBicycle.scm
+++ tests/reasoning/pln/scm/new/ridingBicycle.scm
@@ -2,8 +2,9 @@
 ; example to proof "MoveForward" given
 ; 1) in the context "RidingBicycle", "Pedaling" implies "MoveForward"
 ; 2) "RidingBicycle"
-(define RB (ConceptNode "RindingBicycle"))
-(define Ped (ConceptNode "Pedaling"))
+(define RB (ConceptNode "RindingBicycle" (stv 1.0 1.0)))
+(define Ped (ConceptNode "Pedaling" (stv 7.0 5.0)))
 (define MF (ConceptNode "MovingForward"))
 (define PedImpliesMF (ImplicationLink (stv 0.2 0.7) Ped MF))
 (define CXPedImpliesMF (ContextLink (stv 0.8 0.9) RB PedImpliesMF))
+
