4eef9c1b2cba9136b4be3f2fd9d95fbdf9e1b5d1
Nil Geisweiller <ngeiswei@gmail.com>
2010-12-08 18:00:05 +0200

    added ContextualizerRule header

---
+++ opencog/reasoning/pln/rules/context/ContextualizerRule.h
@@ -0,0 +1,74 @@
+/** ContextualizerRule.h --- 
+ *
+ * Copyright (C) 2010 OpenCog Foundation
+ *
+ * Author: Nil Geisweiller <nilg@laptop>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+#ifndef _OPENCOG_CONTEXTUALIZERRULE_H
+#define _OPENCOG_CONTEXTUALIZERRULE_H
+
+namespace opencog { namespace pln {
+
+/**
+ * Rule to turn non contextual knowledge into Contextual knowledge
+ *
+ * More specifically apply the following inference
+ * a)
+ *
+ * R <TV>
+ *     C ANDExt A
+ *     C ANDExt B
+ * |-
+ * ContextLink <TV>
+ *     C
+ *     R A B
+ *
+ * b)
+ *
+ * ExtensionalInheritanceLink <TV>
+ *     C
+ *     A
+ * |-
+ * ContextLink <TV>
+ *     C
+ *     A
+ *
+ * where A is a Node. that is because
+ *
+ * A <TV> is equivalent to
+ *
+ * ExtensionalInheritanceLink <TV> Universe A
+ *
+ * the rest follows from a).
+ */
+class ContextualizerRule : public GenericRule<TautologyFormula>
+{
+    meta i2oType(const std::vector<Vertex>& h) const;
+    TruthValue** formatTVarray(const std::vector<Vertex>& premiseArray,
+                               int* newN) const;
+public:
+    ContextualizerRule(AtomSpaceWrapper* _asw);
+    Rule::setOfMPs o2iMetaExtra(meta outh, bool& overrideInputFilter) const;
+    meta targetTemplate() const;
+};
+
+}} // namespace opencog { namespace pln {
+
+#endif // _OPENCOG_CONTEXTUALIZERRULE_H
