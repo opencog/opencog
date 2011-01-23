cb535aacb40049753e9f0b48e037d2dfbc0a525b
Linas Vepstas <linasvepstas@gmail.com>
2011-01-22 18:45:18 -0600

    rename file, and convert it to use cog-bind.

--- opencog/query/cog-bind-example.scm
+++ opencog/query/cog-bind-example.scm
@@ -5,17 +5,17 @@ scm
 ; Linas Vepstas January 2009
 ;
 ; This file contains a simple, visual-inspection demo example for the 
-; use of the query engine in the handling of ImplicationLinks. The below 
-; defines a single implication link and two chunks of data, and then calls
-; the implication engine. 
+; use of the query engine to find pattern matches using the scheme
+; interfaces.  The below defines a single BindLink and two chunks of
+; data, and then calls the pattern matcher.
 ;
 ; This example can be loaded by cut-n-paste, or by issuing:
-;    cat implication-example.scm | telnet localhost 17001
+;    cat cog-bind-example.scm | telnet localhost 17001
 ;
 ; The expected result, after running the below, is that the following
 ; should be printed:
 ;
-; guile> (do-implication x)
+; guile> (cog-bind x)
 ; (ListLink (EvaluationLink (PredicateNode "make_from")
 ;     (ListLink (ConceptNode "pottery")
 ;            (ConceptNode "clay"))))
@@ -25,28 +25,34 @@ scm
 ; possible result.
 ;
 (define x
-	(ImplicationLink 
-		(AndLink
-			(EvaluationLink
-				(PredicateNode "_obj")
-				(ListLink
-					(ConceptNode "make")
-					(VariableNode "$var0")
+	(BindLink
+		(ListLink
+			(VariableNode "$var0")
+			(VariableNode "$var1")
+		)
+		(ImplicationLink 
+			(AndLink
+				(EvaluationLink
+					(PredicateNode "_obj")
+					(ListLink
+						(ConceptNode "make")
+						(VariableNode "$var0")
+					)
+				)
+				(EvaluationLink
+					(PredicateNode "from")
+					(ListLink
+						(ConceptNode "make")
+						(VariableNode "$var1")
+					)
 				)
 			)
 			(EvaluationLink
-				(PredicateNode "from")
+				(PredicateNode "make_from")
 				(ListLink
-					(ConceptNode "make")
+					(VariableNode "$var0")
 					(VariableNode "$var1")
 				)
-			)
-		)
-		(EvaluationLink
-			(PredicateNode "make_from")
-			(ListLink
-				(VariableNode "$var0")
-				(VariableNode "$var1")
 			)
 		)
 	)
@@ -71,4 +77,4 @@ scm
 	)
 )
 
-(do-implication x)
+(cog-bind x)
