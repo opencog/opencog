4ef4dd4b96f1483e42f20fb1f4510204f0d91ce8
Nil Geisweiller <ngeiswei@gmail.com>
2011-01-25 20:47:14 +0200

    updated Clark Scheme inference example

--- tests/reasoning/pln/scm/new/ClarkLogTrafficking.scm
+++ tests/reasoning/pln/scm/new/ClarkLogTrafficking.scm
@@ -41,8 +41,18 @@
 (define X (VariableNode "X"))
 (define Y (VariableNode "Y"))
 
-; axioms
+;;;;;;;;;;;;
+;; axioms ;;
+;;;;;;;;;;;;
 ; 1) In the context of music Alice frequently mentions Canadian place names
+;
+; ContextLink <.5, .9>
+;     Music
+;     EvaluationLink
+;         Mention
+;         ListLink
+;             Alice
+;             CanadianPlaceNames
 
 (define axiom1 (ContextLink (stv .5 .9)
                             Music
@@ -51,6 +61,14 @@
                                                       CanadianPlaceNames))))
 
 ; 2) In the context of music Bob frequently mentions Canadian place names
+;
+; ContextLink <.5, .9>
+;     Music
+;     EvaluationLink
+;         Mention
+;         ListLink
+;             Bob
+;             CanadianPlaceNames
 
 (define axiom2 (ContextLink (stv .5 .9)
                             Music
@@ -60,6 +78,14 @@
 
 ; 3) In the context of music Clark does not frequently mention
 ; Canadian place names
+;
+; ContextLink <.01, .9>
+;     Music
+;     EvaluationLink
+;         Mention
+;         ListLink
+;             Clark
+;             CanadianPlaceNames
 
 (define axiom3 (ContextLink (stv .01 .9)
                             Music
@@ -69,6 +95,14 @@
 
 ; 4) In the context of accounting Alice does not frequently mention
 ; Canadian place names
+;
+; ContextLink <.01, .9>
+;     Accounting
+;     EvaluationLink
+;         Mention
+;         ListLink
+;             Alice
+;             CanadianPlaceNames
 
 (define axiom4 (ContextLink (stv .01 .9)
                             Accounting
@@ -77,6 +111,14 @@
                                                       CanadianPlaceNames))))
 
 ; 5) In the context of accounting Bob frequently mentions Canadian place names
+;
+; ContextLink <.5, .9>
+;     Accounting
+;     EvaluationLink
+;         Mention
+;         ListLink
+;             Bob
+;             CanadianPlaceNames
 
 (define axiom5 (ContextLink (stv .5 .9)
                             Accounting
@@ -86,6 +128,14 @@
 
 ; 6) In the context of accounting Clark frequently mentions Canadian
 ; place names
+;
+; ContextLink <.5, .9>
+;     Accounting
+;     EvaluationLink
+;         Mention
+;         ListLink
+;             Clark
+;             CanadianPlaceNames
 
 (define axiom6 (ContextLink (stv .5 .9)
                             Accounting
@@ -94,18 +144,42 @@
                                                       CanadianPlaceNames))))
 
 ; 7) Accounting is associated with Money
+;
+; ExtensionalInheritance <.7, .9>
+;     Money
+;     Accounting
 
 (define axiom7 (SubsetLink (stv .7 .9)
                            Money
                            Accounting))
 
-; 8) CanadianPlaces is associated with Canada
+; 8) CanadianPlaceNames is associated with Canada
+;
+; Inheritance <.8, .9>
+;     CanadianPlaceNames
+;     Canada
 
 (define axiom8 (InheritanceLink (stv .8 .9)
                                 CanadianPlaceNames
                                 Canada))
 
-; 10) If someone X frequently mentions Y then he/she is highly involved with Y
+; 9) If someone X frequently mentions Y then he/she is highly involved with Y
+;
+; AverageLink <.9, .8>
+;     ListLink
+;     X
+;     Y
+;     ImplicationLink
+;         EvaluationLink
+;             Mention
+;             ListLink
+;                 X
+;                 Y
+;         EvaluationLink
+;             Involved
+;             ListLink
+;                 X
+;                 Y
 
 (define axiom9 (AverageLink (stv .9 .8)
                             (ListLink X Y)
@@ -114,11 +188,39 @@
                                              (EvaluationLink Involved
                                                              (ListLink X Y)))))
 
-; 11) Canada is associated with Canadian people
+; 10) Canada is associated with Canadian people
+;
+; InheritanceLink <.7, .7>
+;     CanadianPeople
+;     Canada
 
 (define axiom10 (InheritanceLink (stv .7 .7)
                                  CanadianPeople
                                  Canada))
+
+; 11) If X is involved with Y and Z is similar to Y then X is involved
+; with Z.
+;
+; AverageLink <0.95, .9>
+;     ListLink
+;         X
+;         Y
+;         Z
+;     Implication
+;         AndLink
+;             EvaluationLink
+;                 Involved
+;                 ListLink
+;                     X
+;                     Y
+;             SimilarityLink
+;                 X
+;                 Z
+;         EvaluationLink
+;             Involved
+;             ListLink
+;                 X
+;                 Z
 
 ; 12) Non Canadian People involved with Canadian people in the context
 ; of Money (only) have a chance of being associated with log
@@ -227,33 +329,31 @@
 ;
 ; SubsetLink <0.45, 0.48>
 ;     Accounting
-;     EvaluationLink
-;         Involved
-;         ListLink
-;             Clark
-;             CanadianPlaceNames
-;
-; Note that the ExtensionalInheritance of Accounting and
-; EvaluationLink is implicitely defined by considering the
-; satisfyingSet of EvaluationLink.
+;     SatisfyingSetLink
+;         EvaluationLink
+;             Involved
+;             ListLink
+;                 Clark
+;                 CanadianPlaceNames
 
 (define step4 (DecontextualizerRule (ContextLink Accounting step3)))
 
 ; 5) Apply SubsetDeductionRule on axiom7 and the result of step4
 ;
-; SubsetLink <?>
+; SubsetLink <0.17, 0>     /// TV is weird ???
 ;     Money
-;     EvaluationLink
-;         Involved
-;         ListLink
-;             Clark
-;             CanadianPlaceNames
+;     SatisfyingSetLink
+;         EvaluationLink
+;             Involved
+;             ListLink
+;                 Clark
+;                 CanadianPlaceNames
 
 (define step5 (SubsetDeductionRule axiom7 step4))
 
 ; 6) Contextualize step5 using ContextualizerRule
 ;
-; ContextLink <?>
+; ContextLink <0.17, 0> /// @todo update once step5 is fixed
 ;     Money
 ;     EvaluationLink
 ;         Involved
@@ -262,3 +362,4 @@
 ;             CanadianPlaceNames
 
 (define step6 (ContextualizerRule step5))
+
