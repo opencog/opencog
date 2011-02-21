12d99b67a24d615d931d5c867068cb1dab6c6fac
Joel Pitt <joel@joelpitt.com>
2011-02-21 18:09:32 +0800

    rename embodiment brain start and stop scripts and make cleanup do a proper removal instead of recreating the log and data directories

--- scripts/embodiment/cleanup.sh
+++ scripts/embodiment/cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # remove log and pet database directories
 rm -rf /tmp/$USER/Petaverse/
+
 # the -p of these commands prevent them from complaining if the directories
 # already exist
 mkdir -p /tmp/$USER/Petaverse/Logs
--- scripts/embodiment/start_embodiment.sh
+++ scripts/embodiment/start_embodiment.sh
@@ -4,7 +4,7 @@
 # try to kill existing embodiment session
 ./stop_embodiment.sh
 
-if [ "$1" == "noclean" ]
+if [ "$1" != "noclean" ]
 then
     # Remove the log and data dirs from last embodiment session
     ./cleanup.sh
