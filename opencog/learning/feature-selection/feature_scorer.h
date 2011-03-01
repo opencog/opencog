270ba105a0ede85394fdc7a35719be0aee431355
Nil Geisweiller <ngeiswei@gmail.com>
2011-03-01 13:51:32 +0200

    simplified feature selection scorer and added mutual information scrorer

--- opencog/learning/feature-selection/feature_scorer.h
+++ opencog/learning/feature-selection/feature_scorer.h
@@ -55,7 +55,7 @@ namespace opencog {
  *
  * However in the context of measuring the usefulness of X across
  * several problems mutual information migh be better that conditional
- * entropy.
+ * entropy. See MutualInformation below.
  */
 template<typename IT, typename OT, typename FeatureSet>
 struct ConditionalEntropy : public std::unary_function<FeatureSet, double> {
@@ -64,42 +64,46 @@ struct ConditionalEntropy : public std::unary_function<FeatureSet, double> {
         : _it(it), _ot(ot) {}
 
     double operator()(const FeatureSet& features) const {
-        typedef typename IT::value_type::value_type EntryT;
-        // the following mapping is used to keep track of the number
-        // of inputs a given setting. For instance X1=false, X2=true,
-        // X3=true is one possible setting. It is then used to compute
-        // H(Y, X1, ..., Xn) and H(X1, ..., Xn)
-        typedef std::map<std::vector<EntryT>, unsigned int> TupleCount;
-        TupleCount ic, // for H(X1, ..., Xn)
-            ioc; // for H(Y, X1, ..., Xn)
-        double total = _ot.size();
-        typename IT::const_iterator i_it = _it.begin();
-        typename OT::const_iterator o_it = _ot.begin();
-        for(; i_it != _it.end(); i_it++, o_it++) {
-            std::vector<EntryT> ic_vec;
-            foreach(const typename FeatureSet::value_type& idx, features)
-                ic_vec.push_back((*i_it)[idx]);
-            ic[ic_vec]++;
-            std::vector<EntryT> ioc_vec(ic_vec);
-            ioc_vec.push_back(*o_it);
-            ioc[ioc_vec]++;
-        }
-        // Compute conditional entropy
-        std::vector<double> ip, iop;
-        foreach(const typename TupleCount::value_type& vic, ic)
-            ip.push_back(vic.second/total);
-        foreach(const typename TupleCount::value_type& vioc, ioc)
-            iop.push_back(vioc.second/total);
-        // compute H(Y|X1, ..., Xn)
-        double cond_entropy =
-            entropy(iop.begin(), iop.end()) - entropy(ip.begin(), ip.end());
-        return 1 - cond_entropy;
+        // compute H(X1, ..., Xn) an H(X1, ..., Xn, Y) at once
+        std::pair<double, double> jioh = jointIOTEntropies(_it, _ot, features);
+        // return 1 - H(Y|X1, ..., Xn)
+        double ce = jioh.first - jioh.second;
+        return 1 - ce;
     }
 
 protected:
     const IT& _it;
     const OT& _ot;
 };
+
+/**
+ * Compute the MutualInformation
+ *
+ * H(Y;X1, ..., Xn) = H(X1, ..., Xn) + H(Y) - H(X1, ..., Xn, Y)
+ *
+ * Slightly more expensive than Conditional Entropy but more relevant
+ * across problems.
+ */
+template<typename IT, typename OT, typename FeatureSet>
+struct MutualInformation : public std::unary_function<FeatureSet, double> {
+
+    MutualInformation(const IT& it, const OT& ot) 
+        : _it(it), _ot(ot) {}
+
+    double operator()(const FeatureSet& features) const {
+        // compute H(X1, ..., Xn) and H(X1, ..., Xn, Y) at once
+        std::pair<double, double> jioh = jointIOTEntropies(_it, _ot, features);
+        // compute H(Y)
+        double hy = OTEntropy(_ot);
+        // return H(Y;X1, ..., Xn)
+        return jioh.first + hy - jioh.second;
+    }
+
+protected:
+    const IT& _it;
+    const OT& _ot;
+};
+
 
 } // ~namespace opencog
 
