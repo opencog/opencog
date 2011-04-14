67b4010a94f6be5cb89e20d31ace76bffb9baf64
Linas Vepstas <linasvepstas@gmail.com>
2011-04-14 08:07:41 -0500

    expand the new link-test a bit more.

--- tests/query/match-link.scm
+++ tests/query/match-link.scm
@@ -22,6 +22,33 @@
    )
 )
 
+(AssociativeLink (stv 1.0 1.0)
+   (MemberLink
+      (WordInstanceNode "color")
+      (WordInstanceNode "blue")
+   )
+   (ConceptNode "want-this")
+)
+
+(AssociativeLink (stv 1.0 1.0)
+   (ConceptNode "want-this")
+   (InheritanceLink
+      (WordInstanceNode "color")
+      (WordInstanceNode "green")
+   )
+)
+
+(AssociativeLink (stv 1.0 1.0)
+   (ConceptNode "want-this")
+   (MemberLink
+      (WordInstanceNode "color")
+      (WordInstanceNode "blue")
+   )
+   (ConceptNode "mehh2")
+   (ConceptNode "mehh3")
+)
+
+; Match any arity-2 structure of the desired form.
 (define (untyped-link-match)
    (BindLink
       (ListLink 
@@ -39,4 +66,47 @@
    )
 )
 
+; Match arity-2 with the link having a type.
+(define (typed-link-match)
+   (BindLink
+      (ListLink 
+         (TypedVariableLink
+            (VariableNode "$var")
+            (VariableTypeNode "MemberLink")
+         )
+      )
+      (ImplicationLink
+         (AndLink
+            (AssociativeLink (stv 1 0.99999988)
+               (ConceptNode "want-this")
+               (VariableNode "$var")
+            )
+         )
+         (VariableNode "$var")
+      )
+   )
+)
+
+
+; Match any arity two structure
+(define (untyped-any-match)
+   (BindLink
+      (ListLink 
+         (VariableNode "$var-a")
+         (VariableNode "$var-b")
+      )
+      (ImplicationLink
+         (AndLink
+            (AssociativeLink (stv 1 0.99999988)
+               (VariableNode "$var-a")
+               (VariableNode "$var-b")
+            )
+         )
+         (ListLink
+            (VariableNode "$var-a")
+            (VariableNode "$var-b")
+         )
+      )
+   )
+)
 
