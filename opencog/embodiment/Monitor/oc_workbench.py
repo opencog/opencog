81a2d9b96451fe305f5bd0d780ae89fba16dd6ad
Troy Huang <huangdeheng@gmail.com>
2011-04-20 12:04:56 +0800

    Add a new oc workbench prototype.

--- opencog/embodiment/Monitor/oc_workbench.py
+++ opencog/embodiment/Monitor/oc_workbench.py
@@ -1,261 +1,109 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
-# An elegant chrome-like workbench UI for OpenCog.
+# An elegant workbench GUI for OpenCog.
 #
 # @author Troy Huang <huangdeheng@gmail.com>
-# @date 2011-03-30
-# @update 2011-04-19
+# @date 2011-04-18
+#
 
 
 
 import sys
-import zmq
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-from monitor_widget import *
-
-class OCMonitorPanel(QFrame):
-    """
-    OCMonitorPanel is the container of MonitorWidgets.
-    View of MonitorWidgets can be changed through this panel.
-    """
-    def __init__(self, parent=None, columnNum=2):
-        super(OCMonitorPanel, self).__init__(parent)
-
-        self.setFrameStyle(QFrame.Panel | QFrame.Sunken)
-        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding, True)
-        sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
-        self.setSizePolicy(sizePolicy)
-        
-        self.monitorList = []
-        self.columnNum = columnNum
-
-        self.soloist = None
-        self.zmqUrl = None
-
-        gridLayout = QGridLayout(self)
-
-        self.setLayout(gridLayout)
-
-    def addMonitors(self, monitors):
-        oldCount = len(self.monitorList)
-        self.monitorList.extend(monitors)
-        for i, monitor in enumerate(monitors):
-            row = (oldCount + i) / self.columnNum
-            col = (oldCount + i) % self.columnNum
-            self.layout().addWidget(monitor.widget, row, col)
-            monitor.widget.clicked.connect(self.showSoloView)
-
-    def createMonitors(self, zmqUrl=""):
-        if zmqUrl == "" or (not str(zmqUrl).startswith("tcp://") and\
-            not str(zmqUrl).startswith("ipc://")):
-            warning = QMessageBox.warning(self, "Warning",
-                    "Please input a valid zmq url starting with "
-                    "tcp:// or ipc://")
-            return
-
-        if self.soloist != None:
-            self.showGroupView()
-
-        if not self.zmqUrl or self.zmqUrl != zmqUrl:
-            self.zmqUrl = zmqUrl
-            if len(self.monitorList) > 0:
-                for monitor in self.monitorList:
-                    monitor.widget.hide()
-                    #self.layout().removeWidget(monitor.widget)
-                del self.monitorList[:]
-        else:
-            return 
-
-        self.zmq_context = zmq.Context()
-        
-        monitor1 = MonitorThread(self.zmq_context,
-                             str(self.zmqUrl),
-                             "PsiModulatorUpdaterAgent", 
-                             self)
-        monitor2 = MonitorThread(self.zmq_context,
-                             str(self.zmqUrl), 
-                             "PsiFeelingUpdaterAgent", 
-                             self)
-        monitor3 = MonitorThread(self.zmq_context,
-                             str(self.zmqUrl), 
-                             "PsiDemandUpdaterAgent", 
-                             self)
-        self.addMonitors([monitor1, \
-                    monitor2, \
-                    monitor3]) 
-        self.startMonitor()
-
-    def startMonitor(self):
-        for monitor in self.monitorList:
-            monitor.start()
-
-    def showSoloView(self):
-        sender = self.sender()
-
-        if self.soloist != sender:
-            self.soloist = sender
-            self.soloistOldSize = QSize(sender.size())
-        else:
-            return
-
-        for monitor in self.monitorList:
-            if sender != monitor.widget:
-                monitor.widget.hide()
-        sender.update()
-        sender.updateGeometry()
-
-
-    def updateSoloView(self):
-        """
-        Update solo view when there's a resize event
-        """
-        rect = QRect(0, 0, self.geometry().width(), self.geometry().height())
-        self.soloist.setGeometry(rect)
-
-    def showGroupView(self):
-        self.soloist = None
-        for monitor in self.monitorList:
-            monitor.widget.show()
-
-class OCMonitorTabView(QWidget):
-    """
-    The tab view include two components: navigation bar and monitor panel.
-    """
-    def __init__(self, parent=None):
-        super(OCMonitorTabView, self).__init__(parent)
-
-        # Initialize a navigation bar
-        self.navigationBar = QToolBar(self)
-        
-        # Initialize a monitor panel
-        self.monitorPanel = OCMonitorPanel(self)
-        
-        self.createNavigationBar()
-
-        tabLayout = QVBoxLayout(self)
-        tabLayout.addWidget(self.navigationBar, 0, Qt.AlignTop)
-        tabLayout.addWidget(self.monitorPanel, 1)
-        self.setLayout(tabLayout)
-
-
-    def createNavigationBar(self):
-        # Back action: go back to group monitors view.
-        self.actionBack = QAction(self)
-        self.actionBack.setIcon(QIcon("images/backward.png"))
-        self.actionBack.setObjectName("actionBack")
-        self.actionBack.triggered.connect(\
-                self.monitorPanel.showGroupView)
-
-        # Forward action: go forward to previous monitor panel view.
-        self.actionForward = QAction(self)
-        self.actionForward.setIcon(QIcon("images/forward.png"))
-        self.actionForward.setObjectName("actionForward")
-
-        # Start action: start a monitor instance.
-        self.actionStart = QAction(self)
-        self.actionStart.setIcon(QIcon("images/start.png"))
-        self.actionStart.setObjectName("actionStart")
-
-        # Add a navigation bar that contains above actions.
-        self.navigationBar.setOrientation(Qt.Horizontal)
-
-        self.navigationBar.setObjectName("navigationBar")
-        self.navigationBar.setMovable(False)
-        self.navigationBar.setFloatable(False)
-        
-        self.navigationBar.addAction(self.actionBack)
-        self.navigationBar.addAction(self.actionForward)
-        self.navigationBar.addAction(self.actionStart)
-
-        # Add an address bar.
-        self.addressEdit = QLineEdit(self.navigationBar)
-        self.navigationBar.insertWidget(self.actionStart, self.addressEdit)
-        self.addressEdit.setText("tcp://127.0.0.1:18002")
-        self.actionStart.triggered.connect(self.createMonitors)
-
-    def createMonitors(self):
-        self.monitorPanel.createMonitors(self.addressEdit.text())
-
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Return:
-            self.createMonitors()
-    
-
-class OCMonitorTabWidget(QTabWidget):
-    def __init__(self, parent=None):
-        super(OCMonitorTabWidget, self).__init__(parent)
-        
-        # Create an "add tab" button.
-        self.addTabButton = QPushButton()
-        self.addTabButton.setIcon(QIcon("images/add.png"))
-        # Connect clicked signal to add new tab slot.
-        self.addTabButton.clicked.connect(self.addNewTab)
-        
-        self.setCornerWidget(self.addTabButton)
-        
-        self.setTabsClosable(True)
-        # Connect a tab close signal to close tab slot.
-        self.tabCloseRequested[int].connect(self.closeTab)
-        
-        # Create a single tab initially
-        self.addTab(OCMonitorTabView(), "OAC")
-        
-    def addNewTab(self):
-        newTab = OCMonitorTabView()
-        self.addTab(newTab, "OAC")
-        self.setCurrentWidget(newTab)
-    
-    def closeTab(self, index):
-        if self.count() > 1:
-            self.removeTab(index)
-        else:
-            qApp.quit()
-
+from monitor_browser import *
 
 class OCWorkbench(QMainWindow):
-
+    """The main window of OpenCog workbench.
+    """
     def __init__(self):
-        """
-        Constructor
-
-        @param parent parent widget
+        """Constructor
         """
         super(OCWorkbench, self).__init__(None)#, Qt.FramelessWindowHint)
 
-        self.setupWidget()
+        self.setupWorkbench()
         #self.createMenus()
+
+        # Create a central widget to contain module switchs and scenes.
         self.centralWidget = QWidget(self)
         sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.centralWidget.sizePolicy().hasHeightForWidth())
         self.centralWidget.setSizePolicy(sizePolicy)
-        self.mainLayout = QGridLayout(self.centralWidget)
 
-        monitorTabWidget = OCMonitorTabWidget(self.centralWidget)
+        # Layout for central widget.
+        self.centralLayout = QHBoxLayout(self.centralWidget)
 
-        self.mainLayout.addWidget(monitorTabWidget, 0, 0)
-        self.setCentralWidget(self.centralWidget)
+        # A list of module icons to switch scenes
+        self.moduleIconList = QListWidget()
+        self.moduleIconList.setViewMode(QListView.IconMode)
+        self.moduleIconList.setIconSize(QSize(96, 96))
+        self.moduleIconList.setMovement(QListView.Static)
+        self.moduleIconList.setMaximumWidth(128)
+        self.moduleIconList.setMinimumWidth(128)
+        self.moduleIconList.setSpacing(12)
+
+        self.createModuleIconList()
+        # A stack of scenes that can be switched.
+        self.sceneStack = QStackedWidget()
+        self.sceneStack.addWidget(OCMonitorBrowser())
+        self.sceneStack.addWidget(QLabel("Dialog"))
         
-    def setupWidget(self):
-        """Setup the initial properties of this widget"""
+        self.centralLayout.addWidget(self.moduleIconList)
+        self.centralLayout.addWidget(self.sceneStack)
+
+        #self.monitorTabWidget = OCMonitorTabWidget(self.frame)
+        self.setCentralWidget(self.centralWidget)
+
+        # Create the menu
+        self.createMenus()
+        
+    def setupWorkbench(self):
+        """Setup the initial properties of workbench"""
         desktop = QApplication.desktop()
         screenRect = desktop.screenGeometry(desktop.primaryScreen())
-        windowRect = QRect(0, 0, 800, 600)
+        windowRect = QRect(0, 0, 1024, 768)
 
-        if screenRect.width() < 800:
+        if screenRect.width() < 1024:
             windowRect.setWidth(screenRect.width())
 
-        if screenRect.height() < 600:
+        if screenRect.height() < 768:
             windowRect.setHeight(screenRect.height())
 
         windowRect.moveCenter(screenRect.center())
         self.setGeometry(windowRect)
-        self.setMinimumSize(320, 240)
+        self.setMinimumSize(800, 600)
     
-        self.setWindowTitle("OpenCog Monitor")
-        self.setWindowIcon(QIcon("images/opencog-logo.png"))
+        self.setWindowTitle("OpenCog Workbench")
+        self.setWindowIcon(QIcon('images/opencog-logo.png'))
+
+    def changeScene(self, current, previous):
+        """A slot used to change scene when clicking on the module icon."""
+        if not current:
+            current = previous
+
+        self.sceneStack.setCurrentIndex(self.moduleIconList.row(current))
+
+    def createModuleIconList(self):
+        # icon for psi monitor
+        monitorButton = QListWidgetItem(self.moduleIconList)
+        monitorButton.setIcon(QIcon('images/psi-monitor.png'))
+        monitorButton.setText("Psi Monitor")
+        monitorButton.setTextAlignment(Qt.AlignHCenter)
+        monitorButton.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+        # icon for dialog system
+        dialogButton = QListWidgetItem(self.moduleIconList)
+        dialogButton.setIcon(QIcon('images/language1.png'))
+        dialogButton.setText("Dialog System")
+        dialogButton.setTextAlignment(Qt.AlignHCenter)
+        dialogButton.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+        self.moduleIconList.currentItemChanged.connect(self.changeScene)
 
     ########################################
     # Create GUI components
@@ -268,8 +116,7 @@ class OCWorkbench(QMainWindow):
         self.menuBar().addMenu(self.fileMenu)
 
         self.actionExit.triggered.connect(qApp.quit)
-
-
+    
     ##################################################
     # Event handler
     ##################################################
@@ -283,7 +130,31 @@ class OCWorkbench(QMainWindow):
             self.move(event.globalPos() - self.dragPosition)
             event.accept()
 
+    # Obsolete method.
+    def createToolBox(self):
+        self.toolBox = QTabWidget(self.centralWidget)
+        # define size policy of tool box.
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.toolBox.sizePolicy().hasHeightForWidth())
+        self.toolBox.setSizePolicy(sizePolicy)
+        self.toolBox.setMinimumSize(QSize(512, 512))
+        self.toolBox.setTabPosition(QTabWidget.West)
+        self.toolBox.setTabShape(QTabWidget.Rounded)
 
+        self.toolBox.addTab(QLabel("RelEx"), "Language")
+        self.toolBox.addTab(OCMonitorTabWidget(), "OAC")
+
+        self.toolBox.setTabIcon(1, QIcon("images/psi_monitor.png"))
+
+        self.toolBox.setStyleSheet("QTabBar::tab {min-width: 128px; \
+                        text-align: center; \
+                        border: 1px solid #333; \
+                        margin-top: 4px; \
+                        padding:6px;}")
+
+    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     workbench = OCWorkbench()
