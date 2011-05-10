9a3a477943898157a92d7b7e53d4138e681347ca
Linas Vepstas <linasvepstas@gmail.com>
2011-05-09 22:46:24 -0500

    make the test case a little bit harder ... and it crashes ...

--- tests/query/unordered.scm
+++ tests/query/unordered.scm
@@ -1,9 +1,27 @@
 
 (define (stv mean conf) (cog-new-stv mean conf))
 
-(UnorderedLink (stv 1.0 1.0)
-;; (ListLink (stv 1.0 1.0)
+;; should match to this.
+(SimilarityLink (stv 1.0 1.0)
 	(NumberNode "0.24")
+	(ExecutionOutputLink (stv 1.0 1.0)
+		(GroundedSchemaNode "ActivationModulatorUpdater")
+		(ListLink)
+	)
+)
+
+;; this should not match.
+(UnorderedLink (stv 1.0 1.0)
+	(NumberNode "0.24")
+	(ExecutionOutputLink (stv 1.0 1.0)
+		(GroundedSchemaNode "ActivationModulatorUpdater")
+		(ListLink)
+	)
+)
+
+;; this should not match.
+(SimilarityLink (stv 1.0 1.0)
+	(WordNode "0.24")
 	(ExecutionOutputLink (stv 1.0 1.0)
 		(GroundedSchemaNode "ActivationModulatorUpdater")
 		(ListLink)
@@ -22,8 +40,7 @@
 		)
 		(ImplicationLink
 			;; body
-			(UnorderedLink
-			;; (ListLink
+			(SimilarityLink
 				(VariableNode "$var_number_node_type")
 				(ExecutionOutputLink
 					(GroundedSchemaNode "ActivationModulatorUpdater")
