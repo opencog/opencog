49d642f972c6a411275c2069d3c41b959166c522
Joel Pitt <joel@joelpitt.com>
2011-05-16 15:03:59 +0800

    add missing file

---
+++ opencog/cython/opencog/atom.pyx
@@ -0,0 +1,83 @@
+
+# Atom wrapper object, we should really do something similar in the
+# core OpenCog API.
+cdef class Atom(object):
+    cdef Handle handle
+    cdef AtomSpace atomspace
+    cdef object _atom_type
+    cdef object _name
+    cdef object _outgoing
+
+    def __init__(self,Handle h,AtomSpace a):
+        self.handle = h
+        self.atomspace = a
+        # cache the results after first retrieval of
+        # immutable properties
+        self._atom_type = None
+        self._name = None
+        self._outgoing = None
+
+    def __nonzero__(self):
+        """ Allows boolean comparison, return false is handle is
+        UNDEFINED or doesn't exist in AtomSpace """
+        return self.atomspace.is_valid(self.handle)
+    
+    property name:
+        def __get__(self):
+            if self._name is None:
+                self._name = self.atomspace.get_name(self.handle)
+            return self._name
+
+    property tv:
+        def __get__(self):
+            return self.atomspace.get_tv(self.handle)
+        def __set__(self,val):
+            self.atomspace.set_tv(self.handle,val)
+
+    property av:
+        def __get__(self):
+            return self.atomspace.get_av(self.handle)
+        def __set__(self,val):
+            self.atomspace.set_av(self.handle,av_dict=val)
+
+    property out:
+        def __get__(self):
+            if self._outgoing is None:
+                self._outgoing = self.atomspace.get_outgoing(self.handle)
+            return self._outgoing
+
+    property incoming:
+        def __get__(self):
+            return self.atomspace.get_incoming(self.handle)
+
+    property type:
+        def __get__(self):
+            if self._atom_type is None:
+                self._atom_type = self.atomspace.get_type(self.handle)
+            return self._atom_type
+
+    property t:
+        def __get__(self):
+            return self.type
+
+    def is_source(self,h):
+        return self.atomspace.is_source(h,self.handle)
+
+    def is_node(self):
+        is_a(self.t,types.Node)
+
+    def is_link(self):
+        is_a(self.t,types.Link)
+
+    def is_a(self,t):
+        is_a(self.t,t)
+
+    def long_string(self):
+        return self.atomspace.get_atom_string(self.handle,terse=False)
+
+    def __str__(self):
+        return self.atomspace.get_atom_string(self.handle,terse=True)
+
+    def __repr__(self):
+        return "Atom(%s,%s)" % (repr(self.handle),repr(self.atomspace))
+
