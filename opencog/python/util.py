c360149e6cf4ce323852900612417cf12b218b13
Jared Wigmore <jared.wigmore@gmail.com>
2011-06-23 11:57:00 +0800

    Some nice simple utilities in Python.

---
+++ opencog/python/util.py
@@ -0,0 +1,57 @@
+import collections
+
+def if_(cond, t, f):
+    if cond:
+        return t
+    else:
+        return f
+
+def pp(x):
+    """Pretty-print any collection type (or generator). Prints the easy-to-read version of its members."""
+    if isinstance(x, dict):
+        return ppdict(x)
+    elif isinstance(x, set):
+        return ppset(x)
+    elif isinstance(x, (tuple, list) ):
+        return x.__class__ ( [pp(e) for e in x] )
+    elif isinstance(x, collections.Iterable):
+        return x.__class__.__name__ + [pp(e) for e in x]
+    else:
+        return str(x)
+
+# From AIMA logic.py
+def ppdict(d):
+    """Print the dictionary d.
+    
+    Prints a string representation of the dictionary
+    with keys in sorted order according to their string
+    representation: {a: A, d: D, ...}.
+    >>> ppdict({'m': 'M', 'a': 'A', 'r': 'R', 'k': 'K'})
+    {'a': 'A', 'k': 'K', 'm': 'M', 'r': 'R'}
+    >>> ppdict({z: C, y: B, x: A})
+    {x: A, y: B, z: C}
+    """
+
+    def format(k, v):
+        return "%s: %s" % (repr(k), repr(v))
+
+    ditems = d.items()
+    ditems.sort(key=str)
+    k, v = ditems[0]
+    dpairs = format(k, v)
+    for (k, v) in ditems[1:]:
+        dpairs += (', ' + format(k, v))
+    return '{%s}' % dpairs
+
+def ppset(s):
+    """Print the set s.
+
+    >>> ppset(set(['A', 'Q', 'F', 'K', 'Y', 'B']))
+    set(['A', 'B', 'F', 'K', 'Q', 'Y'])
+    >>> ppset(set([z, y, x]))
+    set([x, y, z])
+    """
+
+    slist = list(s)
+    slist.sort(key=str)
+    return 'set(%s)' % slist
