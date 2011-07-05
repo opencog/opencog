101887ca877ec134389c668324b337937d04ca52
Joel Pitt <joel@joelpitt.com>
2011-07-05 13:52:17 +0800

    fix formatting and remove useless document strings

--- opencog/spatial/math/SquareFace.h
+++ opencog/spatial/math/SquareFace.h
@@ -27,74 +27,63 @@
 #include <opencog/spatial/math/Face.h>
 #include <vector>
 
-namespace opencog
+namespace opencog { namespace spatial {
+namespace math {
+
+class SquareFace : public Face
 {
-    namespace spatial
+public:
+
+    Vector3 pointD;
+
+    /**
+     * Build a face using four points
+     *
+     * @param pointA
+     * @param pointB
+     * @param pointC
+     * @param pointD
+     */
+    SquareFace( Vector3 pointA, Vector3 pointB, Vector3 pointC, Vector3 pointD ) : 
+        Face( pointA, pointB, pointC ), pointD( pointD )  
     {
-        namespace math {
 
-            class SquareFace : public Face
-            {
-            public:
+    }
 
-                Vector3 pointD;
+    Face& addSelf( const Vector3& vector ) 
+    {
+        Face::addSelf( vector );
+        this->pointD + vector;
+        return *this;
+    }
 
-                /**
-                 * Build a face using four points
-                 *
-                 * @param pointA
-                 * @param pointB
-                 * @param pointC
-                 * @param pointD
-                 */
-                SquareFace( Vector3 pointA, Vector3 pointB, Vector3 pointC, Vector3 pointD ) : 
-                    Face( pointA, pointB, pointC ), pointD( pointD )  
-                {
+    /**
+     * Get the edges of the face
+     *    A+-----+B
+     *    /     /
+     *   /     /
+     * D+-----+C
+     *
+     * @return
+     */
+    std::vector<LineSegment> getAllEdges( void ) 
+    {
+        std::vector<LineSegment> edges;
+        edges.push_back( LineSegment( pointA, pointB ) );
+        edges.push_back( LineSegment( pointB, pointC ) );
+        edges.push_back( LineSegment( pointC, pointD ) );
+        edges.push_back( LineSegment( pointD, pointA ) );
+        return edges;
+    }
 
-                }
-
-                /* (non-Javadoc)
-                 * @see com.vettalabs.petaverse.math.Face#addSelf(com.vettalabs.petaverse.math.Vector3)
-                 */
-                Face& addSelf( const Vector3& vector ) 
-                {
-                    Face::addSelf( vector );
-                    this->pointD + vector;
-                    return *this;
-                }
-
-                /**
-                 * Get the edges of the face
-                 *    A+-----+B
-                 *    /     /
-                 *   /     /
-                 * D+-----+C
-                 *
-                 * @return
-                 */
-                std::vector<LineSegment> getAllEdges( void ) 
-                {
-                    std::vector<LineSegment> edges;
-                    edges.push_back( LineSegment( pointA, pointB ) );
-                    edges.push_back( LineSegment( pointB, pointC ) );
-                    edges.push_back( LineSegment( pointC, pointD ) );
-                    edges.push_back( LineSegment( pointD, pointA ) );
-                    return edges;
-                }
-
-                /* (non-Javadoc)
-                 * @see com.vettalabs.petaverse.math.Face#toString()
-                 */
-                std::string toString( void ) const 
-                {
-                    return Face::toString( ) + "|" + pointD.toString();
-                }
+    std::string toString( void ) const 
+    {
+        return Face::toString( ) + "|" + pointD.toString();
+    }
 
 
-            }; // SquareFace
+}; // SquareFace
 
-        } // math
-    } // spatial
-} // opencog
+} } } // namespace opencog::spatial::math
 
 #endif // _SPATIAL_MATH_SQUAREFACE_H_
