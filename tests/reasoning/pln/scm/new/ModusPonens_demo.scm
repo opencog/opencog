1819a05de67cee609149a239d5d37d9a0d40523b
Jared Wigmore <jared.wigmore@gmail.com>
2011-10-13 17:39:01 +0800

    Add a 3-step ModusPonens test

--- tests/reasoning/pln/scm/new/ModusPonens_demo.scm
+++ tests/reasoning/pln/scm/new/ModusPonens_demo.scm
@@ -8,3 +8,9 @@
 (define eval_b (EvaluationLink (PredicateNode "B")))                 ; conclusion
 (define imp_eva_evb (ImplicationLink eval_a eval_b (stv 0.85 0.79))) ; premise #1
 
+(define eval_c (EvaluationLink (PredicateNode "C")))
+(define imp_evb_evc (ImplicationLink eval_b eval_c (stv 0.85 0.79)))
+
+(define eval_d (EvaluationLink (PredicateNode "D")))
+(define imp_evc_evd (ImplicationLink eval_c eval_d (stv 0.85 0.79)))
+
