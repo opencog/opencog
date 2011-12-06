e12488f210b5966c0a4ebbb95d06d4efcd893381
Linas Vepstas <linasvepstas@gmail.com>
2011-12-05 21:10:09 -0600

    build pln even if cython not installed.
    Tweak the CMakefiles to allow pln to build without python.

--- opencog/reasoning/pln/Testing.cc
+++ opencog/reasoning/pln/Testing.cc
@@ -11,7 +11,9 @@
 #include <opencog/guile/SchemeEval.h>
 #include <opencog/guile/load-file.h>
 
+#ifdef HAVE_CYTHON
 #include <opencog/cython/logic_wrapper_api.h>
+#endif
 
 // for backward compatibility as from boost 1.46 filesystem 3 is the default
 #define BOOST_FILESYSTEM_VERSION 2
@@ -268,6 +270,7 @@ bool runPLNTest(Btr<PLNTest> t, bool test_bc)
     test::custom_duration = 0.0;
     start = clock();
 
+#ifdef HAVE_CYTHON
     // Code to run the Python backward chainer.
     PyGILState_STATE gstate;
     gstate = PyGILState_Ensure(); 
@@ -278,6 +281,10 @@ bool runPLNTest(Btr<PLNTest> t, bool test_bc)
     PyGILState_Release(gstate); 
 
     TruthValuePtr etv = cogserver().getAtomSpace()->getTV(eh, NULL_VERSION_HANDLE);
+#else
+    TruthValuePtr etv;
+    Handle eh;
+#endif
 
     if (*etv != TruthValue::NULL_TV()) {
         /* Print resulting truth value compared to test requirements */
