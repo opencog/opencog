2166368b11430c58cd0dc727917803a7da06016a
Linas Vepstas <linasvepstas@gmail.com>
2011-12-16 11:16:10 -0600

    Capitalization, punctuation of comments, whitespace.

--- opencog/learning/moses/eda/eda.h
+++ opencog/learning/moses/eda/eda.h
@@ -38,9 +38,9 @@
 
 //#define EDALIB_64
 
-namespace opencog { 
+namespace opencog {
 namespace moses {
-//storage types for packed populations
+// Storage types for packed populations.
 #ifdef EDALIB_64
 #  if LLONG_MAX==9223372036854775807LL
 typedef unsigned long long int packed_t;
@@ -57,7 +57,7 @@ typedef unsigned int packed_t; //if EDALIB_64 is not set, just use ints
 #  error EDALIB_64 is not defined, so ints must be 32 bits (but they arent)
 #endif
 
-//shorthands for the number of bits in packed type
+// Shorthands for the number of bits in packed type.
 #ifdef EDALIB_64
 BOOST_STATIC_ASSERT(std::numeric_limits<packed_t>::digits == 64);
 const unsigned int bits_per_packed_t = 64;
@@ -66,9 +66,9 @@ BOOST_STATIC_ASSERT(std::numeric_limits<packed_t>::digits == 32);
 const unsigned int bits_per_packed_t = 32;
 #endif
 
-//value types accessing unpacked instances
-typedef double       contin_t;
-typedef int          disc_t;
+// Value types accessing unpacked instances.
+typedef double       contin_t;  // continuous
+typedef int          disc_t;    // discrete
 typedef std::string  onto_t;
 typedef tree<onto_t> onto_tree;
 
