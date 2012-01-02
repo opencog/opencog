da7f8209356697c15065727aa186bfe45d0bf371
Nil Geisweiller <ngeiswei@gmail.com>
2012-01-02 16:55:34 +0200

    fix bug in KLD when the distributions would contain duplicates

--- opencog/util/KLD.h
+++ opencog/util/KLD.h
@@ -26,6 +26,7 @@
 
 #include "dorepeat.h"
 #include <opencog/util/Logger.h>
+#include <opencog/util/Counter.h>
 
 /**
  * Functions to compute the Kullback-Leibler Divergence of discrete
@@ -51,37 +52,39 @@ struct KLDS {
     // @param q sorted sequence of values representing the distribution of Q
     KLDS(const SortedSeq& p_, const SortedSeq& q_) :
         p(p_), q(q_), p_s(p.size()), q_s(q.size()),
-        margin(1.0), epsilon(0.00000000000000000000000000000000000000000000000001),
-        it_p(p.cbegin()), it_q(q.cbegin()),
-        x_very_first(*it_p - margin), x_very_last(p.back() + margin),
-        p_x_pre(x_very_first), q_x_pre(p_x_pre) {}
+        margin(1.0), epsilon(1e-16),
+        p_pdf(Counter<result_type, result_type>(p)),
+        q_pdf(Counter<result_type, result_type>(q)),
+        it_p(p_pdf.cbegin()), it_q(q_pdf.cbegin()),
+        x_very_first(p.front() - margin), x_very_last(p.back() + margin),
+        p_x_pre(x_very_first), q_x_pre(p_x_pre) {
+            // pre-compute the pdf of p and q
+            // TODO
+        }
 
     // computes the components log(delta_p / delta_q) once at a
     // time. This is useful when one want to treat each component as a
     // seperate optimization of a multi-optimization problem.
     result_type next() {
         // compute delta P
-        result_type p_x = *it_p,
+        result_type p_x = it_p->first,
             delta_p_x = std::max(epsilon, p_x - p_x_pre),
-            delta_p = q_s / delta_p_x;
-
-        logger().fine("q_s = %u, p_x = %f, p_x_pre = %f, p_x - p_x_pre = %f", q_s, p_x, p_x_pre, p_x - p_x_pre);
+            delta_p = (q_s * it_p->second) / delta_p_x;
 
         // compute delta Q
-        result_type q_x = it_q == q.cend()? x_very_last : *it_q;
+        result_type q_x = it_q == q_pdf.cend()? x_very_last : it_q->first;
         // search the points of q right before and after p_x
         while (q_x < p_x) {
             q_x_pre = q_x;
             ++it_q;
-            q_x = it_q == q.cend()? x_very_last : *it_q;
+            q_x = it_q == q_pdf.cend()? x_very_last : it_q->first;
         }
         result_type delta_q_x = std::max(epsilon, q_x - q_x_pre),
-            delta_q = p_s / delta_q_x;
+            n_duplicates = it_q == q_pdf.cend()? 1.0 : it_q->second,
+            delta_q = (p_s * n_duplicates) / delta_q_x;
         
         p_x_pre = p_x;
         ++it_p;
-
-        logger().fine("delta_p = %f, delat_q = %f, Q = %f", delta_p, delta_q, delta_p / delta_q);
         
         return std::log(delta_p / delta_q);
     }
@@ -89,17 +92,22 @@ struct KLDS {
     // @return estimate of KL(P||Q)
     result_type operator()() {
         result_type res = 0;
-        dorepeat(p_s)
+        dorepeat(p_pdf.size())
             res += next();
-        return res / p_s - 1;
+        return res / p_s - 1; // I don't fully understand the -1
     }
 
+private:
+    // map 1-dimensional inputs to their probability density
+    typedef std::map<result_type, result_type> pdf_t;
     const SortedSeq &p, &q;
-    const size_t p_s, q_s;            // sizes of p and q
-    result_type margin;
-    result_type epsilon;        // minimum distance between 2 values
-                                // (to avoid getting infinite delta)
-    typename SortedSeq::const_iterator it_p, it_q;
+    const result_type p_s, q_s,            // sizes of p and q used in
+                                           // the computation of LKD
+        margin,
+        epsilon;        // minimum distance between 2 values
+                        // (to avoid getting infinite delta)
+    pdf_t p_pdf, q_pdf;
+    typename pdf_t::const_iterator it_p, it_q;
     result_type x_very_first, x_very_last, p_x_pre, q_x_pre;
 };
 
