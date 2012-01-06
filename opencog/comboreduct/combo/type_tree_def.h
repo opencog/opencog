0dd6ce79ad791dfc1b694619dfb9da13ec7f6fb0
Linas Vepstas <linasvepstas@gmail.com>
2012-01-05 19:38:01 -0600

    reveiw & cleanup combo tree types.

--- opencog/comboreduct/combo/type_tree_def.h
+++ opencog/comboreduct/combo/type_tree_def.h
@@ -30,38 +30,41 @@ namespace opencog { namespace combo {
 
 namespace id {
   enum type_node {
-    //type operators
-    lambda_type, //example : lambda_type(T1 T2 T3)
-                 //represents a function that takes
-                 //T1 and T2 argument's type and returns
-                 //an output of type T3
-    application_type, //represent the application of a function with
-                      //its arguments
+    // Type operators.
+    lambda_type,      // Example : lambda_type(T1 T2 T3)
+                      // represents a function that takes arguments of
+                      // type T1 and T2, and returns an output of type T3
+    application_type, // Represents the application of a function with
+                      // its arguments
     union_type,
-    arg_list_type,         //denote a 0 or more argument(s) of a given type
-                           //example : arg_list(T) corresponds to a list of
-                           //0 or more elements of type T
-    //elementary types
+    arg_list_type,    // Denotes a list of zero or more arguments of
+                      // a given type.
+                      // Example : arg_list(T) corresponds to a list of
+                      // zero or more elements of type T
+
+    // Elementary types.
     boolean_type,
     contin_type,
     action_result_type,
     definite_object_type,
-    action_definite_object_type, //like definite_object but contains the suffix _action
+    action_definite_object_type, // Like definite_object, but contains
+                                 // the suffix _action
     indefinite_object_type,
     message_type,
     action_symbol_type,
     wild_card_type,
-    ann_type, //for evolving anns
-    //unknown type
-    unknown_type,  //it is the uber type all types inherite from unkown
-                   //but ill_formed_type
-    //ill formed
-    ill_formed_type, //when the type is just wrong
+    ann_type,                   // For evolving anns
 
-    //argument, below is a small hack to avoid using union or variant
-    //the rest of the integers enumerate variables
-    //i.e. argument_type corresponds to #1, variable_type+1 corresponds to #2
-    //and so on
+    unknown_type,  // The uber type.  All types inherite from unkown
+                   // but ill_formed_type
+
+    ill_formed_type, // When the type is just wrong
+
+    // Argument. This is a small hack to avoid using union or variant
+    // or similar ideas to list argument types.  THIS MUST BE LAST IN
+    // THE ENUM.  The rest of the integers enumerate variables. That
+    // is, argument_type corresponds to #1, argument_type+1 corresponds
+    // to #2, and so on.
     argument_type
   };
 }
