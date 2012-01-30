0069da15500ebde81fd7502c1c8e07c0c5957df4
Nil Geisweiller <ngeiswei@gmail.com>
2012-01-30 16:25:45 +0200

    remove isnormal from scope which is actually not doing what I expected

--- opencog/util/numeric.h
+++ opencog/util/numeric.h
@@ -58,7 +58,6 @@ using std::numeric_limits;
 using boost::math::isnan;
 using boost::math::isfinite;
 using boost::math::isinf;
-using boost::math::isnormal;
 
 #ifndef WIN32
 // This needs to be changed for non-gcc. Note however that so far it
