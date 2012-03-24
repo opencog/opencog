e535dec06457c39a5b1edd3d056117fa3104ae7b
Linas Vepstas <linasvepstas@gmail.com>
2012-03-24 11:51:34 -0500

    util: lack of whitespace seems to confuse gcc-4.7.0 ...

--- opencog/util/exceptions.h
+++ opencog/util/exceptions.h
@@ -33,7 +33,7 @@
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
-#define TRACE_INFO " ("__FILE__":"TOSTRING(__LINE__)")"
+#define TRACE_INFO " (" __FILE__ ":" TOSTRING(__LINE__) ")"
 
 namespace opencog 
 {
