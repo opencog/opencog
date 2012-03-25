d0398aa29417ed23d0c360452cd74b616b25b447
Linas Vepstas <linasvepstas@gmail.com>
2012-03-25 15:19:40 -0500

    combo: add whitespace, improve readability.

--- opencog/comboreduct/reduct/flat_normal_form.cc
+++ opencog/comboreduct/reduct/flat_normal_form.cc
@@ -25,26 +25,33 @@
 
 namespace opencog { namespace reduct {
 
-  //does c contain p and !p?
-  bool tautology(const clause& c) {
-    return (std::adjacent_find(c.begin(),c.end(),
-			       bind(std::equal_to<int>(),_1,
-				    bind(std::negate<int>(),_2)))!=c.end());
+  /// Does c contain p and !p?
+  bool tautology(const clause& c)
+  {
+    return (std::adjacent_find(c.begin(), c.end(),
+			       bind(std::equal_to<int>(), _1,
+				    bind(std::negate<int>(), _2))) != c.end());
   }
-  //is c1 a subset of (or equal to) c2?
-  bool subset_eq(const clause& c1,const clause& c2) {
-    return (c2.size()>=c1.size() && 
-	    std::includes(c2.begin(),c2.end(),
-			  c1.begin(),c1.end(),c2.key_comp()));
+
+  /// Is c1 a subset of (or equal to) c2?
+  bool subset_eq(const clause& c1, const clause& c2)
+  {
+    return (c2.size() >= c1.size() && 
+	    std::includes(c2.begin(), c2.end(),
+			  c1.begin(), c1.end(), c2.key_comp()));
   }
-  bool subset(const clause& c1,const clause& c2) {
-    return (c2.size()>c1.size() && 
-	    std::includes(c2.begin(),c2.end(),
-			  c1.begin(),c1.end(),c2.key_comp()));
+
+  bool subset(const clause& c1, const clause& c2)
+  {
+    return (c2.size() > c1.size() && 
+	    std::includes(c2.begin(), c2.end(),
+			  c1.begin(), c1.end(), c2.key_comp()));
   }
-  int number_of_literals(const nf& f) {
-    return std::accumulate(f.begin(),f.end(),0,
-			   bind(std::plus<int>(),_1,bind(&clause::size,_2)));
+
+  int number_of_literals(const nf& f)
+  {
+    return std::accumulate(f.begin(), f.end(), 0,
+			   bind(std::plus<int>(), _1, bind(&clause::size, _2)));
   }
 
 } // ~namespace reduct
