e8f3b530b0901ee3901041f8e598778b36ca6dcc
Linas Vepstas <linasvepstas@gmail.com>
2012-03-25 21:10:20 -0500

    moses: fix bug I introduced in bzr rev 6695 Unfortunately, this will not compile with gcc-4.7.0 So I'm not sure what to do...

--- opencog/comboreduct/reduct/flat_normal_form.h
+++ opencog/comboreduct/reduct/flat_normal_form.h
@@ -31,13 +31,14 @@
 #include <unordered_map>
 #include <boost/bind.hpp>
 
+#include <opencog/util/Logger.h>
 #include <opencog/util/numeric.h>
 #include <opencog/util/tree.h>
 
 #include <opencog/comboreduct/combo/vertex.h>
 
 namespace opencog { namespace reduct {
-typedef std::set<int,opencog::absolute_value_order<int> > clause;
+typedef std::set<int, opencog::absolute_value_order<int> > clause;
 typedef std::list<clause> nf;
 using namespace opencog::combo;
 
@@ -101,31 +102,31 @@ public:
                      opencog::lexicographic_subtree_order<T> > Item2Int;
     typedef std::unordered_map<int,tree> Int2Item;
 
-    nf add_cnf(sib_it,sib_it);
-    nf add_dnf(sib_it,sib_it);
+    nf add_cnf(sib_it, sib_it);
+    nf add_dnf(sib_it, sib_it);
 
     template<typename It>
-    void extract_cnf(It,It,tree&,sib_it) const;
+    void extract_cnf(It, It, tree&, sib_it) const;
     template<typename It>
-    void extract_dnf(It,It,tree&,sib_it) const;
+    void extract_dnf(It, It, tree&, sib_it) const;
 
-    void extract_conjunction(const clause&,tree&,sib_it) const;
+    void extract_conjunction(const clause&, tree&, sib_it) const;
 protected:
     Item2Int _item2int;
     Int2Item _int2item;
 
     int add_item(sib_it);
 
-    void create(tree&,sib_it,int) const;
+    void create(tree&, sib_it, int) const;
 };
 
 template<typename T>
 nf nf_mapper<T>::add_cnf(sib_it from,sib_it to)
 {
-    nf res(distance(from,to));
-    for (nf::iterator out=res.begin();out!=res.end();++from,++out)
-        if (*from==id::logical_or)
-            for (sib_it item=from.begin();item!=from.end();++item)
+    nf res(distance(from, to));
+    for (nf::iterator out = res.begin(); out != res.end(); ++from, ++out)
+        if (*from == id::logical_or)
+            for (sib_it item = from.begin(); item != from.end(); ++item)
                 out->insert(add_item(item));
         else
             out->insert(add_item(from));
@@ -133,12 +134,12 @@ nf nf_mapper<T>::add_cnf(sib_it from,sib_it to)
 }
 
 template<typename T>
-nf nf_mapper<T>::add_dnf(sib_it from,sib_it to)
+nf nf_mapper<T>::add_dnf(sib_it from, sib_it to)
 {
-    nf res(distance(from,to));
-    for (nf::iterator out=res.begin();out!=res.end();++from,++out)
-        if (*from==id::logical_and)
-            for (sib_it item=from.begin();item!=from.end();++item)
+    nf res(distance(from, to));
+    for (nf::iterator out = res.begin(); out != res.end(); ++from, ++out)
+        if (*from == id::logical_and)
+            for (sib_it item = from.begin(); item != from.end(); ++item)
                 out->insert(add_item(item));
         else 
             out->insert(add_item(from));
@@ -168,30 +169,32 @@ void nf_mapper<T>::extract_cnf(It from,It to,tree& t,sib_it sib) const
     }
 }
 
+
 template<typename T>
 template<typename It>
-void nf_mapper<T>::extract_dnf(It from,It to,tree& t,sib_it sib) const
+void nf_mapper<T>::extract_dnf(It from, It to, tree& t, sib_it sib) const
 {
     t.erase_children(sib);
-    for (nf::const_iterator c=from;c!=to;++c)
+    for (nf::const_iterator c = from; c != to; ++c) {
         if (c->empty()) {
             t.erase_children(sib);
-            *sib=id::logical_true;
+            *sib = id::logical_true;
             return;
-        } else if (c->size()==1) {
-            if (*c->begin()!=0)
-                create(t,sib,*c->begin());
+        } else if (c->size() == 1) {
+            if (*c->begin() != 0)
+                create(t, sib, *c->begin());
         } else {
-            sib_it clause=t.append_child(sib,id::logical_and);
-            for (clause::const_iterator item=c->begin();item!=c->end();++item) {
-                create(t,clause,*item);
+            sib_it clause = t.append_child(sib, id::logical_and);
+            for (clause::const_iterator item = c->begin(); item != c->end(); ++item) {
+                create(t, clause, *item);
             }
         }
+    }
     if (sib.has_one_child()) {
-        *sib=*sib.begin();
+        *sib = *sib.begin();
         t.erase(t.flatten(sib.begin()));
     } else {
-        *sib=sib.is_childless() ? id::logical_false : id::logical_or;
+        *sib = sib.is_childless() ? id::logical_false : id::logical_or;
     }
 }
 
@@ -233,7 +236,7 @@ int nf_mapper<T>::add_item(sib_it item)
 
 
     // Perhaps its under minus-the-index?
-    if (argument* arg=boost::get<argument>(&*item)) {
+    if (argument* arg = boost::get<argument>(&*item)) {
         arg->negate();
         item_loc = _item2int.find(item);
         arg->negate();
@@ -244,19 +247,20 @@ int nf_mapper<T>::add_item(sib_it item)
 
     // Add it directly
     int next_id = _int2item.size() + 1;
-    tree tr = tree(*item);
 
-    std::pair<int, tree> pr = make_pair(next_id, tr);
+    // XXX The next line will not compuile under gcc-4.7.0
+    // The compiler complains it cannot convert sib_it to tree.
+    std::pair<int, tree> pr = make_pair(next_id, item);
 
-    return _item2int.insert(make_pair
-                            (item,
-                             _int2item.insert(pr).first->first)).first->second;
+    int actual_id = _int2item.insert(pr).first->first;
+
+    return _item2int.insert(make_pair(item, actual_id)).first->second;
 }
 
 template<typename T>
-void nf_mapper<T>::create(tree& t,sib_it at,int idx) const
+void nf_mapper<T>::create(tree& t, sib_it at, int idx) const
 {
-    if (idx<0) {
+    if (idx < 0) {
         get_argument(*t.append_child
                      (typename tree::pre_order_iterator(at),
                       _int2item.find(-idx)->second.begin())).negate();
