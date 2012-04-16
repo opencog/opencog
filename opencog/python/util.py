ec38ac2f275afc3fe5729bd1f82d143e507d2aac
Jared Wigmore <jared.wigmore@gmail.com>
2012-04-16 17:53:34 +0800

    utility function (not used now)

--- opencog/python/util.py
+++ opencog/python/util.py
@@ -24,6 +24,19 @@ def product(seq):
     """Product of a sequence."""
     return functools.reduce(operator.mul, seq, 1)
 
+def list_powerset(lst):
+    # the power set of the empty set has one element, the empty set
+    result = [[]]
+    for x in lst:
+        # for every additional element in our set
+        # the power set consists of the subsets that don't
+        # contain this element (just take the previous power set)
+        # plus the subsets that do contain the element (use list
+        # comprehension to add [x] onto everything in the
+        # previous power set)
+        result.extend([subset + [x] for subset in result])
+    return result
+
 # These can't be used in PyPy but otherwise are fine.
 def list_powerset(lst):
     # the power set of the empty set has one element, the empty set
