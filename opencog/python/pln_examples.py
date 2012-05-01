c7db515f8eccfc3a6694cad0be802ca88757a913
Jared Wigmore <jared.wigmore@gmail.com>
2012-05-01 11:47:22 +0800

    Update PLN example files. Allow commenting out tests

--- opencog/python/pln_examples.py
+++ opencog/python/pln_examples.py
@@ -13,24 +13,24 @@ future_fc/InheritanceOsamaAbu_test.conf
 future_fc/6_test.conf
 future_fc/InheritanceMuhammadTerrorist_test.conf
 psi/psi_planning_one_step_test.conf
-psi/psi_planning_two_step_test.conf\n''' +
+psi/psi_planning_two_step_test.conf
+# The obsolete agisim tests are known not to work.
 #agisim_planning/19_test.conf
 #agisim_planning/23_test.conf
 #agisim_planning/FetchDemo5_test.conf
 #agisim_planning/FetchDemo5_subgoal_test.conf
 #agisim_planning/11_test.conf
-'''bc/AnotBdemo_full_test.conf
+bc/AnotBdemo_full_test.conf
 bc/new/NotEvaluatorRule_test.conf
 bc/new/and_test.conf
 bc/new/or_test.conf
-bc/new/scaling_test.conf
 bc/new/LookupRule_test.conf
-bc/plus_test.conf
+#bc/plus_test.conf
 bc/targetslist/4_test.conf
-bc/targetslist/9_test.conf
+#bc/targetslist/9_test.conf
 bc/targetslist/0_test.conf
-bc/targetslist/30_test.conf
-bc/multiple_roots_spawning_test.conf
+#bc/targetslist/30_test.conf
+#bc/multiple_roots_spawning_test.conf
 bc/PsiActionSelection_test.conf
 both/new/BasicForAllDemo_test.conf
 both/new/ModusPonensRule_test.conf
@@ -39,18 +39,20 @@ both/new/ModusPonensRule_2steps_test.conf
 both/new/InversionRule_test.conf
 both/new/BasicForAllDemo2_test.conf
 both/new/DeductionRule_test.conf
-both/inverse_binding_test.conf
+#both/inverse_binding_test.conf
 both/targetslist/21_test.conf
-both/woa_demo_test.conf
-both/AnotBdemo_partial_test.conf
+#both/woa_demo_test.conf
+#both/AnotBdemo_partial_test.conf
 both/28_test.conf
 ''')
+
 
 files = files_list.split('\n')
 
 def test_all(a):
     for f in files:
-        run_pln_example(a, f)
+        if f != '' and not f.startswith('#'):
+            run_pln_example(a, f)
 
     print 'Passed %s out of %s tests' % (len(passed), len(passed+failed))
     if len(failed):
@@ -62,12 +64,9 @@ passed = []
 failed = []
 
 def run_pln_example(a, f):
-    if f == '':
-        return
-    
     a.clear()
     
-    testdir = '../tests/reasoning/pln/targets_modified/'
+    testdir = '../tests/reasoning/pln/targets/'
     fname = testdir+f
     config = ConfigParser.ConfigParser()
     read_files = config.read(fname)
