2169f81e56171a0615de07b25ad378f8ab240bc7
Linas Vepstas <linasvepstas@gmail.com>
2012-05-10 10:41:14 -0500

    combo: introduce a new enum_type class

---
+++ opencog/comboreduct/combo/enum_type.cc
@@ -0,0 +1,33 @@
+/*
+ * opencog/comboreduct/combo/message.cc
+ *
+ * Copyright (C) 2002-2008 Novamente LLC
+ * All Rights Reserved
+ *
+ * Written by Nil Geisweiller
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+#include "message.h"
+
+namespace opencog { namespace combo {
+
+std::ostream& operator<<(std::ostream& out, const combo::message& m) {
+    return out << combo::message::prefix() << '\"' << m.getContent() << '\"';
+}
+
+} // ~namespace combo
+} // ~namespace opencog
