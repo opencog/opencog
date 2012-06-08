b464701ee0a455d46869a6b60806dfd5752b743b
Mandi Wang <mandiwangm@gmail.com>
2012-06-08 17:14:02 -0400

    Added Read-Eval-Print Loop for combo

---
+++ opencog/comboreduct/main/repl.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "../combo/eval.h"
+#include "../ant_combo_vocabulary/ant_combo_vocabulary.h"
+
+using namespace ant_combo;
+using namespace std;
+using namespace opencog;
+
+int main(){
+
+  combo_tree tr;
+  vertex_seq empty;
+
+  while(cin.good()){
+    cout<<"combo_repl> ";
+    cin >> tr;
+    if(!cin.good())
+      break;
+  cout<<eval_throws_binding(empty, tr)<<endl;
+  }
+
+  return 0;
+}
