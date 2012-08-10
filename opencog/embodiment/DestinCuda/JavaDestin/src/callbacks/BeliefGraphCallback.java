0eaf83b8739622c1c08ea14fc56cb05d487a27fc
Ted Sanders <tpsjr7@gmail.com>
2012-08-09 20:20:04 -0500

    tweaked belief display

--- opencog/embodiment/DestinCuda/JavaDestin/src/callbacks/BeliefGraphCallback.java
+++ opencog/embodiment/DestinCuda/JavaDestin/src/callbacks/BeliefGraphCallback.java
@@ -1,18 +1,21 @@
 package callbacks;
 
+import java.text.DecimalFormat;
 import javadestin.*;
 
 
 public class BeliefGraphCallback extends DestinIterationFinishedCallback {
 	private FPSCallback fps = new FPSCallback(0);
+        private DecimalFormat df = new DecimalFormat("0.0000");
 	
 @Override
 	public void callback(INetwork network) {
-                
-		int states = network.getBeliefsPerNode(7);
-		float [] beliefs = Util.toJavaArray(network.getNodeBeliefs(7, 0, 0), states);
+        int layer = 7;
+		int states = network.getBeliefsPerNode(layer);
+		float [] beliefs = Util.toJavaArray(network.getNodeBeliefs(layer, 0, 0), states);
 		
-		int beliefWidth = 20;
+		int beliefWidth = 80; // how many character columns in chart line, doesn't
+                                      // have to match the size of the belief vector
 		System.out.print("\033[2J");//clearscreen
 		System.out.flush();
 		
@@ -21,9 +24,9 @@ public class BeliefGraphCallback extends DestinIterationFinishedCallback {
 		
 		for(int s = 0 ; s < states; s++){
 			int b = (int)(beliefs[s] * beliefWidth);
-                        System.out.print(s+":"+beliefs[s]+":");
+                        System.out.print(s+":"+df.format(beliefs[s])+":");
 			for(int i = 0 ; i < b ; i++){
-				System.out.print(s);
+				System.out.print("X");
 			}
 			System.out.println();
 		}
