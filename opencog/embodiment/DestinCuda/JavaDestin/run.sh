7026c233c5f12792e1017f8a9da01d1d81ac5f6c
Ted Sanders <tpsjr7@gmail.com>
2012-08-09 19:36:40 -0500

    run.sh removed an echo

--- opencog/embodiment/DestinCuda/JavaDestin/run.sh
+++ opencog/embodiment/DestinCuda/JavaDestin/run.sh
@@ -4,5 +4,5 @@ destin_alt=`cd ../../DestinCudaAlt ; pwd`
 #java -cp build/classes/ -Djava.library.path="$parentdir" javadestin.VideoExperiment
 echo $destin_alt
 the_path="${parentdir}:${destin_alt}"
-echo "The path: $the_path"
+
 java -cp build/classes/ -Djava.library.path="$the_path" javadestin.Dashboard
