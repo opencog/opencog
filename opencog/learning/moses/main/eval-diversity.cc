6a6637463118ca488b8116cdd2b100cd0246e1d2
Nil Geisweiller <ngeiswei@gmail.com>
2012-09-19 14:55:10 +0200

    add options in eval-diversity (output the distances instead of statistics + load combo results from table)

--- opencog/learning/moses/main/eval-diversity.cc
+++ opencog/learning/moses/main/eval-diversity.cc
@@ -26,28 +26,30 @@
  */
 
 #include <boost/program_options.hpp>
-#include <boost/accumulators/accumulators.hpp>
-#include <boost/accumulators/statistics/stats.hpp>
-#include <boost/accumulators/statistics/mean.hpp>
-#include <boost/accumulators/statistics/variance.hpp>
-#include <boost/accumulators/statistics/min.hpp>
-#include <boost/accumulators/statistics/max.hpp>
 
 #include <opencog/util/oc_assert.h>
+#include <opencog/util/iostreamContainer.h>
+#include <opencog/comboreduct/combo/table_io.h>
 #include <opencog/learning/moses/moses/types.h>
+
+#include "eval-diversity.h"
 
 using namespace std;
 using namespace boost::program_options;
-using namespace boost::accumulators;
 using namespace opencog;
 using namespace moses;
 
-struct eval_diversity_params {
-    vector<string> moses_files;
-    float diversity_p_norm;
-};
+// given the sequence of distances write the results
+void write_results(const eval_diversity_params& edp, const vector<float>& dsts) {
+    if (edp.output_file.empty())
+        ostream_results(cout, edp, dsts);
+    else {
+        ofstream out(edp.output_file.c_str());
+        ostream_results(out, edp, dsts);
+    }
+}
 
-int main(int argc, char** argv) {    
+int main(int argc, char** argv) {
     eval_diversity_params edp;
     
     // Declare the supported options.
@@ -55,10 +57,28 @@ int main(int argc, char** argv) {
     desc.add_options()
         ("help,h", "Produce help message.\n")
 
+        ("input-file,i", value<vector<string>>(&edp.input_files),
+         "DSV file containing a target feature (indicated by option -f)"
+         "used to compute the distance between the other files. Can be used"
+         "several times to enter several input files. "
+         "The distance will be computed between each target across all files.\n")
+
+        ("target-feature,u", value<string>(&edp.target_feature),
+         "Name of the target feature to load.\n")
+
         ("moses-file,m", value<vector<string>>(&edp.moses_files),
          "File containing the candidates as output by moses. The output "
          "must contain the bscores (see option -t or moses). "
-         "Can be used several times for several files")
+         "Can be used several times for several files.\n")
+        
+        ("output-file,o", value<string>(&edp.output_file),
+         "File to write the results. If none is given it write on the stdout.\n")
+
+        ("display-stats", value<bool>(&edp.display_stats)->default_value(true),
+         "Display statistics.\n")
+
+        ("display-values", value<bool>(&edp.display_values)->default_value(false),
+         "Display the actually values (distances).\n")
 
         ("diversity-p-norm",
          value<float>(&edp.diversity_p_norm)->default_value(2.0),
@@ -67,9 +87,6 @@ int main(int argc, char** argv) {
          "correspond to the Manhatan distance. A value of 2.0 corresponds to "
          "the Euclidean distance. A value of 0.0 or less correspond to the "
          "max component-wise. Any other value corresponds to the general case.\n")
-
-        // ("output_file,o",
-        //  value<string>)
 
         ;
 
@@ -88,30 +105,50 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    // load the bscores
-    vector<behavioral_score> bscores;
-    vector<bscored_combo_tree> bcts;
-    foreach(string file, edp.moses_files) {
-        ifstream in(file);
-        while (in.good()) {
+    if (!edp.moses_files.empty()) {
+        // load the bscores
+        vector<behavioral_score> bscores;
+        vector<bscored_combo_tree> bcts;
+        foreach(string file, edp.moses_files) {
+            ifstream in(file);
             in.exceptions(ifstream::failbit | ifstream::badbit | ifstream::eofbit);
-            try {
-                bcts.push_back(istream_bscored_combo_tree(in));
-            } catch(...) {}
+            while (in.good()) {
+                try {
+                    bcts.push_back(istream_bscored_combo_tree(in));
+                } catch(...) {}
+            }
         }
+
+        // compute the distances between the bscores
+        vector<float> dsts;
+        for (unsigned i = 0; i < bcts.size(); ++i)
+            for (unsigned j = 0; j < i; ++j)
+                dsts.push_back(lp_distance(get_bscore(bcts[i]), get_bscore(bcts[j]),
+                                           edp.diversity_p_norm));
+
+        // write the results
+        write_results(edp, dsts);
     }
 
-    
-    // // compute the distances
-    accumulator_set<float, stats<tag::mean, tag::variance, tag::min, tag::max>> acc;
-    for (unsigned i = 0; i < bcts.size(); ++i)
-        for (unsigned j = 0; j < i; ++j)
-            acc(lp_distance(get_bscore(bcts[i]), get_bscore(bcts[j]),
-                            edp.diversity_p_norm));
-    
-    // write the results
-    std::cout << "mean: " << mean(acc) << std::endl;
-    std::cout << "std dev: " << sqrt(variance(acc)) << std::endl;
-    std::cout << "min: " << boost::accumulators::min(acc) << std::endl;
-    std::cout << "max: " << boost::accumulators::max(acc) << std::endl;
+    if (!edp.input_files.empty()) {        
+        // load the target features (assumed to be of float type) and
+        // dump them in vectors of floats
+        vector<vector<float>> targets;
+        foreach(string file, edp.input_files) {
+            vector<float> target;
+            boost::transform(combo::loadOTable(file, edp.target_feature),
+                             back_inserter(target), combo::cast_contin);
+            targets.push_back(target);
+        }
+
+        // compute the distances between the targets
+        vector<float> dsts;
+        for (unsigned i = 0; i < targets.size(); ++i)
+            for (unsigned j = 0; j < i; ++j)
+                dsts.push_back(p_norm(targets[i], targets[j],
+                                      edp.diversity_p_norm));
+
+        // write the results
+        write_results(edp, dsts);        
+    }
 }
