41095bddde63fe8126993c50b8c1ccdb31ce7c79
Linas Vepstas <linasvepstas@gmail.com>
2012-09-24 09:11:55 -0500

    moses: split out univariate to its own .cc file.

--- opencog/learning/moses/optimization/optimization.cc
+++ opencog/learning/moses/optimization/optimization.cc
@@ -31,11 +31,6 @@
 
 #include <opencog/util/oc_omp.h>
 
-#include "../eda/termination.h"
-#include "../eda/replacement.h"
-#include "../eda/logging.h"
-#include "../eda/local_structure.h"
-#include "../eda/optimize.h"
 #include "../moses/neighborhood_sampling.h"
 
 #include "optimization.h"
@@ -142,65 +137,6 @@ bool optim_parameters::score_improved(score_t best_score, score_t prev_hi)
     }
 
     return big_step;
-}
-
-/////////////////////////////
-// Univariate Optimization //
-/////////////////////////////
-
-//return # of evaluations actually performed
-unsigned univariate_optimization::operator()(instance_set<composite_score>& deme,
-                    const iscorer_base& iscorer, unsigned max_evals)
-{
-    unsigned pop_size = opt_params.pop_size(deme.fields());
-    unsigned max_gens_total = information_theoretic_bits(deme.fields());
-    unsigned max_gens_improv = opt_params.max_gens_improv(deme.fields());
-    unsigned n_select = double(pop_size) * eda_params.selection_ratio;
-    unsigned n_generate = double(pop_size) * eda_params.replacement_ratio;
-
-    // Adjust parameters based on the maximal # of evaluations allowed
-    if (max_evals < pop_size) {
-        pop_size = max_evals;
-        max_gens_total = 0;
-    } else {
-        max_gens_total = min(max_gens_total,
-                             (max_evals - pop_size) / n_generate);
-    }
-
-    // Create the initial sample
-    // Generate the initial sample to populate the deme
-    deme.resize(pop_size);
-    generate_initial_sample(deme.fields(), pop_size, deme.begin(), deme.end());
-
-    if (eda_params.is_tournament_selection()) {
-        cout_log_best_and_gen logger;
-        return optimize
-               (deme, n_select, n_generate, max_gens_total, iscorer,
-                terminate_if_gte_or_no_improv<composite_score>
-                (composite_score(opt_params.terminate_if_gte,
-                                  get_complexity(worst_composite_score),
-                                  0),
-                 max_gens_improv),
-                tournament_selection((unsigned)eda_params.selection),
-                univariate(), local_structure_probs_learning(),
-                // rtr_replacement(deme.fields(),
-                //                      opt_params.rtr_window_size(deme.fields())),
-                replace_the_worst(),
-                logger);
-    } else { //truncation selection
-        OC_ASSERT(false,
-                  "Trunction selection not implemented."
-                  " Tournament should be used instead.");
-        return 42;
-        /*
-        return optimize(deme,n_select,n_generate,args.max_gens,score,
-          terminate_if_gte_or_no_improv(opt_params.terminate_if_gte,
-                   max_gens_improv),
-          //truncation selection goes here
-          univariate(),local_structure_probs_learning(),
-          replace_the_worst(),cout_log_best_and_gen());
-        */
-    }
 }
 
 ///////////////////
