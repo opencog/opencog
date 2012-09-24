aa6d3bd8874d0a4d4bea816694edd1007153ddb2
Linas Vepstas <linasvepstas@gmail.com>
2012-09-24 10:06:24 -0500

    moses: add a simulated-annealing .cc file.

--- opencog/learning/moses/optimization/optimization.cc
+++ opencog/learning/moses/optimization/optimization.cc
@@ -27,11 +27,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include <math.h>   // for sqrtf, cbrtf
-
-#include <opencog/util/oc_omp.h>
-
-#include "../moses/neighborhood_sampling.h"
+#include <math.h>   // for sqrt
 
 #include "optimization.h"
 
@@ -137,114 +133,6 @@ bool optim_parameters::score_improved(score_t best_score, score_t prev_hi)
     }
 
     return big_step;
-}
-
-/////////////////////////
-// Star-shaped search  //
-/////////////////////////
-
-unsigned simulated_annealing::operator()(instance_set<composite_score>& deme,
-                    const instance& init_inst,
-                    const iscorer_base& iscorer, unsigned max_evals)
-{
-    const field_set& fields = deme.fields();
-    max_distance = opt_params.max_distance(fields);
-
-    // @todo this should be adapted for SA
-    unsigned pop_size = opt_params.pop_size(fields);
-    // unsigned max_gens_total = information_theoretic_bits(deme.fields());
-    deme_size_t max_number_of_instances =
-        /*(deme_size_t)max_gens_total * */
-        (deme_size_t)pop_size;
-    if (max_number_of_instances > max_evals)
-        max_number_of_instances = max_evals;
-
-    deme_size_t current_number_of_instances = 0;
-
-    unsigned step = 1;
-
-    // Score the initial instance
-    instance center_instance(init_inst);
-    scored_instance<composite_score> scored_center_inst =
-        score_instance(center_instance, iscorer);
-    energy_t center_instance_energy = energy(scored_center_inst);
-    double current_temp = sa_params.init_temp;
-
-    // Logger
-    {
-        std::stringstream ss;
-        ss << "Star search initial instance: " << fields.stream(center_instance);
-        logger().debug(ss.str());
-        logger().debug("Energy = %f", center_instance_energy);
-    }
-    // ~Logger
-
-    do {
-        unsigned current_distance = dist_temp(current_temp);
-
-        logger().debug("Step: %u  Distance = %u, Temperature = %f",
-                       step, current_distance, current_temp);
-
-        deme_size_t number_of_new_instances =
-            sample_new_instances(sa_params.max_new_instances,
-                                 current_number_of_instances,
-                                 center_instance, deme,
-                                 current_distance);
-
-        // If the temperature is too high, then the distance will
-        // be too large, and no instances will be found at that
-        // distance.
-        if (0 == number_of_new_instances) {
-            current_temp = cooling_schedule((double)step
-                                            * sa_params.temp_step_size);
-            step++;
-            continue;
-         }
-
-        // score all new instances in the deme
-        OMP_ALGO::transform(next(deme.begin(), current_number_of_instances),
-                            deme.end(),
-                            next(deme.begin_scores(), current_number_of_instances),
-                            boost::bind(boost::cref(iscorer), _1));
-
-        // get the best instance
-        scored_instance<composite_score>& best_scored_instance =
-            *OMP_ALGO::max_element(next(deme.begin(), current_number_of_instances),
-                                   deme.end());
-
-        instance& best_instance = best_scored_instance.first;
-        energy_t best_instance_energy = energy(best_scored_instance);
-
-        // check if the current instance in the deme is better than
-        // the center_instance
-        double actual_accept_prob = sa_params.accept_prob_temp_intensity *
-            accept_probability(best_instance_energy,
-                               best_instance_energy, current_temp);
-
-        if (actual_accept_prob >= randGen().randdouble()) {
-            center_instance_energy = best_instance_energy;
-            center_instance = best_instance;
-            // Logger
-            {
-                std::stringstream ss;
-                ss << "Center instance: "
-                   << fields.stream(center_instance);
-                logger().debug(ss.str());
-                logger().debug("Energy = %f", center_instance_energy);
-            }
-            // ~Logger
-        }
-
-        current_number_of_instances += number_of_new_instances;
-
-        current_temp = cooling_schedule((double)step
-                                        * sa_params.temp_step_size);
-        step++;
-    } while(current_number_of_instances < max_number_of_instances &&
-            current_temp >= sa_params.min_temp &&
-            center_instance_energy > energy(opt_params.terminate_if_gte));
-
-    return current_number_of_instances;
 }
 
 } // ~namespace moses
