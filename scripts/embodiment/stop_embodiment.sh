33418a5e1c87b0adbdc2ccb9587adb1deb2f45f4
Linas Vepstas <linasvepstas@gmail.com>
2012-10-19 13:35:12 -0500

    fix the stupid cshell script, again.

--- scripts/embodiment/stop_embodiment.sh
+++ scripts/embodiment/stop_embodiment.sh
@@ -1,12 +1,12 @@
-#!/bin/tcsh
+#!/bin/sh
 
-killall -9 oac >& /dev/null
-killall -9 spawner >& /dev/null
-killall -9 router >& /dev/null
-killall -9 learningServer >& /dev/null
-killall -9 learningServer_CogServer >& /dev/null
-killall -9 LSMocky >& /dev/null
-killall -9 pvpSimulator >& /dev/null
-killall -9 pbTester >& /dev/null
+killall -9 oac &> /dev/null
+killall -9 spawner &> /dev/null
+killall -9 router &> /dev/null
+killall -9 learningServer &> /dev/null
+killall -9 learningServer_CogServer &> /dev/null
+killall -9 LSMocky &> /dev/null
+killall -9 pvpSimulator &> /dev/null
+killall -9 pbTester &> /dev/null
 
-rm -rf *.ipc >& /dev/null
+rm -rf *.ipc &> /dev/null
