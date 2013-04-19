c2189a4807443532db35d827f034cebdd001188d
Nil Geiswiller <ngeiswei@gmail.com>
2013-04-19 11:51:42 +0200

    Add moses program options to control feature selection diversity

--- opencog/learning/moses/main/moses_exec.cc
+++ opencog/learning/moses/main/moses_exec.cc
@@ -1218,6 +1218,26 @@ int moses_exec(int argc, char** argv)
          "will be selected. \n"
          "For the -ainc algo only, the -C flag over-rides this setting.\n")
 
+        // ======= Feature-selection diveristy pressure =======
+        ("fs-diversity-pressure",
+         value<float>(&festor_params.diversity_pressure)->default_value(0),
+         "Multiplicative coefficient of the diversity penalty "
+         "(itself being in [0,1]).\n")
+
+        ("fs-diversity-cap",
+         value<int>(&festor_params.diversity_cap)->default_value(100),
+         "Place a cap on the maximum number of feature set to consider. "
+         "If negative, no cap is used (warning could be very slow)"
+         "Use this to speed up diversity computation on feature sets.\n")
+
+        ("fs-diversity-interaction",
+         value<int>(&festor_params.diversity_interaction)->default_value(-1),
+         "Maximum number of interactions to be considered when computing "
+         "the mutual information between feature sets. "
+         "This is used in case the number of selected features tends to "
+         "be high compared to the number of datapoints to decrease inacuracy "
+         "of the mutual information.\n")
+
         // ======= Feature-selection incremental algo params =======
         ("fs-inc-redundant-intensity",
          value<double>(&fs_params.inc_red_intensity)->default_value(-1.0),
