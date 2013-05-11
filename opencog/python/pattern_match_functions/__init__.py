bfaeb42e751adfce2d7c4bd3378c36348bad6346
Ramin Barati <rekino@gmail.com>
2013-05-11 07:17:10 -0700

    Resolving #172

--- opencog/python/pattern_match_functions/__init__.py
+++ opencog/python/pattern_match_functions/__init__.py
@@ -1,2 +1,10 @@
+from opencog.atomspace import Handle
+
 __author__ = 'keyvan'
 import rules
+
+
+def execute_user_defined_function(func, handle_uuid):
+    handle = Handle(handle_uuid)
+    args_list_link = ATOMSPACE[handle]
+    
