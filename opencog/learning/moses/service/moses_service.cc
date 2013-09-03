dd6ef06c236a0992317e339a61716590c89b64d3
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2013-09-03 14:17:28 +0800

    Just invoke moses with no additional arguments for now

--- opencog/learning/moses/service/moses_service.cc
+++ opencog/learning/moses/service/moses_service.cc
@@ -19,7 +19,11 @@ moses_service::~moses_service()
 void moses_service::run()
 {
     const std::vector<std::string>& args = {
-        "moses",
+        "moses"
+
+        // For now, just demonstrate that we can invoke moses."
+
+        /*,
         "-i",
         //"/home/cosmo/opencog/src/qtbin/opencog/learning/moses/main/test/xor.txt",
         "./xor.txt",
@@ -27,7 +31,8 @@ void moses_service::run()
         "1",
         "-o",
         //"/home/cosmo/opencog/src/qtbin/opencog/learning/moses/main/test/out.txt"
-        "./out.txt"
+        "./out.txt"*/
+
     };
 
     moses_exec(args);
