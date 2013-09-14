722b42f5d86640fcba08e472f4a7afccf3f589e7
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2013-09-15 07:10:10 +0800

    Add score to python MosesCandidate objects, add unit tests & documentation.

--- opencog/cython/opencog/pymoses.pyx
+++ opencog/cython/opencog/pymoses.pyx
@@ -1,21 +1,29 @@
-__author__ = 'cosmo'
-
-'''
+"""
 Wrapper for MOSES. Uses the C++ moses_exec function to access MOSES functionality.
 
-run:
-The run method returns a collection of candidates as MosesCandidate objects.
-Each candidate containing a score, program, and program_type.
-Enumeration of program_type: python, combo.
-For python, there is an eval method that will evaluate the model on new data.
+*** run:
+Parameters:
+    input (list of lists) - training data for regression [optional]
+        Example: input=[[0, 0, 0], [1, 1, 0], [1, 0, 1], [0, 1, 1]]
+    args (string) - arguments for MOSES (see MOSES documentation) [optional]
+    python (bool) - if True, return Python instead of Combo [optional]
+Either input or args must be provided, otherwise, MOSES would have no input
 
-run_manually:
+Output:
+Returns a collection of candidates as MosesCandidate objects.
+Each MosesCandidate contains:
+    score (int)
+    program (string)
+    program_type (string - Enumeration: python, combo)
+    eval (Runnable method - Only valid for program_type python) Run this method to evaluate the model on new data.
+
+*** run_manually:
 The run_manually method invokes MOSES without any extra integration.
-Useful for using it non-programatically via stdout.
+Useful for interacting with MOSES non-programatically via stdout.
 
-Options for use:
+Options for using the pymoses wrapper:
 1. Within the CogServer, from an embedded MindAgent
-2. Within the CogServer, from the interactive python shell
+2. Within the CogServer, from the interactive Python shell
 3. In your Python IDE or interpreter. You need to ensure that
    your path includes '{PROJECT_BINARY_DIR}/opencog/cython'
 
@@ -30,11 +38,13 @@ Example #1: XOR example with Python output and Python input
 
 input_data = [[0, 0, 0], [1, 1, 0], [1, 0, 1], [0, 1, 1]]
 output = moses.run(input=input_data, python=True)
+print output[0].score # Prints: 0
 model = output[0].eval
 model([0, 1])  # Returns: True
 model([1, 1])  # Returns: False
 
 Example #2: Run the majority demo problem, return only one candidate, and use Python output
+
 output = moses.run(args="-H maj -c 1", python=True)
 model = output[0].eval
 model([0, 1, 0, 1, 0])  # Returns: False
@@ -52,7 +62,9 @@ moses.run_manually("--version")
 moses.run_manually("-i input.txt -o output.txt")
 
 @todo Implement an option to use a Python function as the MOSES scoring function
-'''
+"""
+
+__author__ = 'Cosmo Harrigan'
 
 from libc.stdlib cimport malloc, free
 import shlex
@@ -80,6 +92,10 @@ class MosesCandidate(object):
 
 cdef class moses:
     def run(self, input = None, args = "", python = False):
+        if (input is None or input == "") and args == "":
+            raise MosesException('Error: input and args cannot both be empty. You should pass your input using the ' +
+                                 'input parameter, or refer to it in the args parameter.')
+
         # Create temporary files for sending input/output to the moses_exec function
         if input is not None:
             input_file = tempfile.NamedTemporaryFile()
@@ -101,28 +117,30 @@ cdef class moses:
         _args_list.extend(shlex.split(args))
 
         self._run_args_list(_args_list)
-
         # Process the output file
         output = output_file.file.read()
 
         candidates = []
+        # Python header declared in opencog/learning/moses/moses/types.h (ostream_combo_tree_composite_pbscore_python)
+        python_header = "#!/usr/bin/python"
 
         if len(output) == 0:
-            raise MosesException('Error: No output file was obtained from MOSES. Check to make sure the input file and arguments provided were valid.')
+            raise MosesException('Error: No output file was obtained from MOSES. ' +
+                                 'Check to make sure the input file and arguments provided were valid.')
 
         # Python output
-        elif output.splitlines()[0].startswith('#!/usr/bin/python'):
-            # The Python header is declared in opencog/learning/moses/moses/types.h (ostream_combo_tree_composite_pbscore_python)
-            python_header = "#!/usr/bin/python"
-
+        elif output.splitlines()[0].startswith(python_header):
             output_list = [python_header + "\n" + element for element in output.split(python_header)[1:]]
 
-            for candidate in output_list: #output.split("#!/usr/bin/python")[1:]:
-                program = candidate #.splitlines()
+            for candidate in output_list:
+                program = candidate
                 # @todo Fix opencog/comboreduct/combo/iostream_combo.h (ostream_combo_it) to remove the unneeded trailing comma that is inserted by the Python formatter
                 program = program.rpartition(',')[0]
-                # @todo Add score for python programs
-                candidates.append(MosesCandidate(score = None, program = program, program_type = "python"))
+                if "#score: " in program:
+                    score = int(program.split("#score: ")[1].splitlines()[0])
+                else:
+                    raise MosesException('Error: A score value was expected but not found in the Python program.')
+                candidates.append(MosesCandidate(score = score, program = program, program_type = "python"))
 
         # Combo output
         else:
@@ -136,6 +154,8 @@ cdef class moses:
         return candidates
 
     def run_manually(self, args=""):
+        if args == "":
+            raise MosesException('Error: No arguments provided')
         self._run_args_list(shlex.split(args))
 
     def _run_args_list(self, args_list):
@@ -148,6 +168,6 @@ cdef class moses:
         try:
             moses_exec(len(args_list), c_argv)
         except RuntimeError, ex:
-            raise MosesException('Error: exception occured when calling C++ MOSES. Exception message:\n' + ex.message)
+            raise MosesException('Error: exception occurred when calling C++ MOSES. Exception message:\n' + ex.message)
         finally:
             free(c_argv)
