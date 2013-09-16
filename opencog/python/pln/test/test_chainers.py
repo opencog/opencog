1c3df112a2321a798d4c0af03d8cb58b0b95376a
Jade O'Neill <jade@opencog.org>
2013-09-16 17:01:08 +0800

    Remove use of tree class from chainer test

--- opencog/python/pln/test/test_chainers.py
+++ opencog/python/pln/test/test_chainers.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 
 from opencog.atomspace import AtomSpace, types, Atom
 from pln.chainers import Chainer, get_attentional_focus
-import utility.tree as tree
 
 class BackwardChainerTest(TestCase):
 
@@ -26,9 +25,9 @@ class BackwardChainerTest(TestCase):
         atoms = self._simple_atoms_1()
         atoms[0].av = {'sti': 1}
 
-        result_tree = self.chainer._selectOne(atoms, self.atomspace)
-        self.assertNotEquals(result_tree, None)
-        self.assertEqual(result_tree, tree.T(atoms[0]))
+        atom = self.chainer._selectOne(atoms, self.atomspace)
+        self.assertNotEquals(atom, None)
+        self.assertEqual(atom, atoms[0])
 
         atoms[1].av = {'sti': 1}
         atoms[2].av = {'sti': 1}
@@ -50,14 +49,9 @@ class BackwardChainerTest(TestCase):
         atoms = self._simple_atoms_1()
         atoms[2].av = {'sti': 1}
 
-        template = tree.tree_from_atom(atoms[2])
-        assert type(template) == tree.Tree
+        template = atoms[2]
 
         result = self.chainer._select_one_matching(template)
-        self.assertNotEqual( result, None )
-        print result
-        print type(result)
-        assert type(result) == tree.Tree
         self.assertEqual( result, atoms[2] )
 
         
@@ -66,9 +60,8 @@ class BackwardChainerTest(TestCase):
 
         print get_attentional_focus(self.atomspace)
 
-        template = tree.Tree(1)
+        template = self.atomspace.add_node(types.VariableNode, "$v1")
         result = self.chainer._select_one_matching(template)
-        print result
         self.assertNotEqual( result, None )
 
     def test__find_inputs_recursive(self):
@@ -92,23 +85,24 @@ class BackwardChainerTest(TestCase):
 
         atoms[0].av = {'sti': 1}
         #from nose.tools import set_trace; set_trace()
-        generic_inputs = [tree.T(atoms[0])]
+        generic_inputs = [atoms[0]]
         generic_outputs = []
         (inputs, outputs) = apply(generic_inputs, generic_outputs)
         print inputs, outputs
-        self.assertEquals( inputs, [tree.T(atoms[0])] )
+        self.assertEquals( inputs, [atoms[0]] )
         self.assertEquals( outputs, [] )
 
-        generic_inputs = [tree.new_var()]
+        v1 = self.atomspace.add_node(types.VariableNode, "$v1")
+
+        generic_inputs = [v1]
         generic_outputs = []
         (inputs, outputs) = apply(generic_inputs, generic_outputs)
         print inputs, outputs
         self.assertEquals( len(inputs), 1 )
         self.assertEquals( outputs, [] )
 
-        A = tree.new_var()
-        generic_inputs = [A]
-        generic_outputs = [A]
+        generic_inputs = [v1]
+        generic_outputs = [v1]
         (inputs, outputs) = apply(generic_inputs, generic_outputs)
         print inputs, outputs
         self.assertEquals( len(inputs), 1 )
