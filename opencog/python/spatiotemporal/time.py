b339e1f873618ee069730a65d0d2e49f2a5ab9a1
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2013-09-30 15:38:32 +0330

    got matplotlib to work

--- opencog/python/spatiotemporal/time.py
+++ opencog/python/spatiotemporal/time.py
@@ -1,1 +1,11 @@
 __author__ = 'keyvan'
+
+from scipy.stats import norm, t
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = t.pdf(x, 1, 3)
+
+plt.plot(x, y)
+plt.xlabel('T')
+plt.ylabel('P')
+plt.show()
