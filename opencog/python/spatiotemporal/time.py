13eaeae9ac5e74168e7462fca534afdb4b4efe46
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2013-10-01 17:33:44 +0330

    Moved Dario's code to spatiotemporal package and applied PEP8 to it to make it more readable

--- opencog/python/spatiotemporal/time.py
+++ opencog/python/spatiotemporal/time.py
@@ -1,11 +1,14 @@
 __author__ = 'keyvan'
 
-from scipy.stats import norm, t
+from scipy.stats import norm, t, uniform
 import matplotlib.pyplot as plt
-x = [1, 2, 3, 4, 5]
-y = t.pdf(x, 1, 3)
+x = 'abcde'
+y1 = uniform.pdf(x)
+#y1 = t.pdf(x, 1, '3')
+#y2 = norm.pdf(x, '3')
 
-plt.plot(x, y)
+plt.plot(x, y1)
+#plt.plot(x, y2)
 plt.xlabel('T')
 plt.ylabel('P')
 plt.show()
