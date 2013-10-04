1e39bbd6ad0789dda0d5ba2aedc96b7b2e9b68e5
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2013-10-04 13:19:46 -0700

    Continue work on Python REST API. Implement atom update POST operation. Move atomspace out of global variable space.

--- opencog/python/web/api.py
+++ opencog/python/web/api.py
@@ -6,6 +6,7 @@ Implemented using the Flask micro-framework and flask-restful extension
 
 __author__ = 'Cosmo Harrigan'
 
+from flask import * # @todo remove
 from flask import Flask, abort
 from flask import request, make_response, url_for
 from flask.views import MethodView
@@ -24,8 +25,18 @@ class FormatHandleList(fields.Raw):
             handles.append(elem.h.value())
         return handles
 
+class FormatAtomList(fields.Raw):
+    def format(self, value):
+
+        {
+            'complete': True,
+            'skipped':
+"skipped":0,
+"total":10,
+        }
+        'complete', 'skipped', 'total', 'result'
+
 tv_fields = {
-    # @todo: verify the mappings of the following terminology
     'strength': fields.Float(attribute='mean'),
     'confidence': fields.Float(attribute='confidence'),
     'count': fields.Float(attribute='count')
@@ -47,9 +58,20 @@ atom_fields = {
     'truthvalue': fields.Nested(tv_fields, attribute='tv'),
     'attentionvalue': fields.Nested(av_fields, attribute='av')
 }
-
+'''
+>>> resource_fields = {'name': fields.String}
+>>> resource_fields['address'] = {}
+>>> resource_fields['address']['line 1'] = fields.String(attribute='addr1')
+atom_list_fields = {
+    'complete': fields.String
+}
+'''
 
 class AtomListAPI(Resource):
+    @classmethod
+    def set_atomspace(cls, atomspace):
+        cls.atomspace = atomspace
+
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('type', type=str, location='args', choices=types.__dict__.keys())
@@ -59,46 +81,77 @@ class AtomListAPI(Resource):
         args = self.reqparse.parse_args()
         type_lookup = args.get('type')
         if type_lookup is None:
-            atoms = atomspace.get_atoms_by_type(types.Atom)
+            atoms = self.atomspace.get_atoms_by_type(types.Atom)
         else:
-            atoms = atomspace.get_atoms_by_type(types.__dict__.get(type_lookup))
+            atoms = self.atomspace.get_atoms_by_type(types.__dict__.get(type_lookup))
 
-        # @todo: Implement pagination with 'complete', 'skipped', 'total', 'result' attributes
+        # @todo: Implement pagination
+        # 'complete', 'skipped', 'total', 'result'
         return {'atoms': map(lambda t: marshal(t, atom_fields), atoms)}
 
 
 class AtomAPI(Resource):
+    @classmethod
+    def set_atomspace(cls, atomspace):
+        cls.atomspace = atomspace
+
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        # @todo: do we need to specify location='json'?
         self.reqparse.add_argument('type', type=str, choices=types.__dict__.keys())
         self.reqparse.add_argument('name', type=str)
         super(AtomAPI, self).__init__()
 
     def get(self, id):
         try:
-            atom = atomspace[Handle(id)]
+            atom = self.atomspace[Handle(id)]
         except IndexError:
             abort(404)
 
         return {'atom': marshal(atom, atom_fields)}
 
+    '''
+    @todo PUT
+    "update the STI, LTI, or TV of an atom with JSON message",
+    "Usage: json-update-atom handle {JSON}\n\n"
+    "   Update an atom based on a JSON message with format:\n"
+    "   { \"sti\": STI } \n"
+    "   { \"lti\": LTI } \n"
+    "   { \"tv\": {tv details} } \n",
+    '''
+
     # @todo: support PATCH for partial update
     #def put(self, id):
+
+    '''
+    @todo POST
+    "Usage: json-create-atom<CRLF>JSON<CRLF><Ctrl-D><CRLF>\n\n"
+            "   Create an atom based on the JSON format:\n"
+            "   { \"type\": TYPENAME, \"name\": NAME, \n"
+            "     \"outgoing\": [ UUID1, UUID2 ... ], \n"
+            "     \"truthvalue\": {\"simple|composite|count|indefinite\":\n"
+            "          [truthvalue details] } \n",
+            true, false
+
+            TruthValue: See opencog/web/JsonUtil.cc JSONToTV method
+    '''
+
+    # @todo what happens if i post to an existing atom
+
     def post(self):
-        args = self.reqparse.parse_args()
-        print 'Type:'
-        print args.get('type')
-        print 'Name:'
-        print args.get('name')
+        data = reqparse.request.get_json()
+        type_data = types.__dict__.get(data['type'])
+        truthvalue_data = TruthValue(data['truthvalue']['strength'], data['truthvalue']['count'])
 
-        # @todo: Convert 'truthvalue' arg to TruthValue
-        # @todo: During testing, if you add a node twice, using the same name, both nodes receive the same handle. That might be a bug in the API.
-        # @todo: Document how to test the API using curl and Python 'request'
+        if type_data is None or truthvalue_data is None or data['name'] is None:
+            # @todo: check what is the proper error type
+            abort(500)
 
-        type_lookup = types.__dict__.get(args.get('type'))
-
-        atom = atomspace.add(type_lookup, args.get('name'), TruthValue(.5, .6))
+        try:
+            atom = self.atomspace.add(t=type_data, name=data['name'], tv=truthvalue_data)
+        except TypeError:
+            # @todo: check what is the proper error type
+            print "Debug: TypeError - check your parameters"
+            abort(500)
 
         print 'Atom created:'
         print atom
@@ -106,11 +159,11 @@ class AtomAPI(Resource):
 
     def delete(self, id):
         try:
-            atom = atomspace[Handle(id)]
+            atom = self.atomspace[Handle(id)]
         except IndexError:
             abort(404)
 
-        success = atomspace.remove(atom)
+        success = self.atomspace.remove(atom)
         return {'result': success}
 
 
@@ -118,26 +171,80 @@ class RESTApi(object):
     def __init__(self, atomspace):
         ######### For testing purposes, populate an AtomSpace with nodes & links:
         self.atomspace = atomspace
-        animal = self.atomspace.add_node(types.ConceptNode, 'animal', TruthValue(.1, .9))
-        bird = self.atomspace.add_node(types.ConceptNode, 'bird', TruthValue(.01, .9))
-        swan = self.atomspace.add_node(types.ConceptNode, 'swan', TruthValue(.001, .9))
-        swan_bird = self.atomspace.add_link(types.InheritanceLink, [swan, bird], TruthValue(1, 1))
-        bird_animal = self.atomspace.add_link(types.InheritanceLink, [bird, animal], TruthValue(1, 1))
-        bird.av = {'sti': 9}
 
         # Initialize the web server and set the routing
         self.app = Flask(__name__, static_url_path="")
         self.api = Api(self.app)
-        self.api.add_resource(AtomListAPI, '/api/v1.0/atoms', '/api/v1.0/atoms/', endpoint='atoms')
-        self.api.add_resource(AtomAPI, '/api/v1.0/atoms/<int:id>', '/api/v1.0/atoms/', endpoint='atom')
+        AtomListAPI.set_atomspace(self.atomspace)
+        AtomAPI.set_atomspace(self.atomspace)
+        self.api.add_resource(AtomListAPI, '/api/v1.0/atoms', endpoint='atoms')
+        self.api.add_resource(AtomAPI, '/api/v1.0/atoms/<int:id>', '/api/v1.0/atoms', endpoint='atom')
 
     def run(self):
         self.app.run(debug=True)
 
 if __name__ == '__main__':
-    atomspace = AtomSpace()
-    api = RESTApi(atomspace)
+    test_atomspace = AtomSpace()
+
+    animal = test_atomspace.add_node(types.ConceptNode, 'animal', TruthValue(.1, .9))
+    bird = test_atomspace.add_node(types.ConceptNode, 'bird', TruthValue(.01, .9))
+    swan = test_atomspace.add_node(types.ConceptNode, 'swan', TruthValue(.001, .9))
+    swan_bird = test_atomspace.add_link(types.InheritanceLink, [swan, bird], TruthValue(1, 1))
+    bird_animal = test_atomspace.add_link(types.InheritanceLink, [bird, animal], TruthValue(1, 1))
+    bird.av = {'sti': 9}
+
+    api = RESTApi(test_atomspace)
     api.run()
 
-# @todo: Return JSON errors
+'''
+now
+# @todo: Return JSON errors\
 
+Specify the type of the TruthValue returned in the JSON
+Specify the type of the TruthValue passed in the JSON
+        # @todo: ? Linas says handle isn't supposed to be the only id - how about a get method using name/type pair?
+        # @todo: support setting outgoing links (and incoming?)
+        # @todo: Verify that the 'strength' and 'count' terms are proper for TruthValue
+        # @todo: Document how to test the API using curl and Python 'request'
+
+Wrap the results as if there were pagination, even though there isn't with 'complete', 'skipped', 'total', 'result' attributes
+'''
+
+# Later:
+# @todo: support multiple TruthValue types in the get and post and update
+# @todo: create an enumeration of valid TruthValue types
+# @todo: get methods for TruthValue: see opencog\web\GetAtomRequest.cc tvToJSON method
+
+'''
+Types:
+SIMPLE_TRUTH_VALUE
+COUNT_TRUTH_VALUE
+INDEFINITE_TRUTH_VALUE
+COMPOSITE_TRUTH_VALUE
+
+As noted in the cdef for TruthValue in opencog\cython\opencog\atomspace_details.pyx, only SimpleTruthValue has been implemented so far in the Python bindings
+'''
+
+"""
+Testing:
+
+import json
+from opencog.atomspace import *
+from requests import *
+
+# GET
+#  LIST
+g = get('http://localhost:5000/api/v1.0/atoms')
+print json.dumps(g.json(), indent=2)
+
+#  ONE ATOM
+g = get('http://localhost:5000/api/v1.0/atoms/1')
+print json.dumps(g.json(), indent=2)
+
+# POST
+payload = {'type': 'ConceptNode', 'name': 'ugly_frog'}
+headers = {'content-type': 'application/json'}
+r = post('http://localhost:5000/api/v1.0/atoms/', data=json.dumps(payload), headers=headers)
+print json.dumps(r.json(), indent=2)
+
+"""
