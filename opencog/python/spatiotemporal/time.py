c5dae1c378994cfd9362df8f5f732e683cc72ab5
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2013-10-06 18:41:16 +0330

    TemporalRelation almost complete (dgree function remains to be implemented). The first draft of create_event_relation_hashtable is now in place...

--- opencog/python/spatiotemporal/time.py
+++ opencog/python/spatiotemporal/time.py
@@ -5,6 +5,22 @@ from random import randrange
 from scipy.stats import t
 from scipy.stats.distributions import rv_frozen
 
+TEMPORAL_RELATIONS = {
+    'p': 'precedes',
+    'm': 'meets',
+    'o': 'overlaps',
+    'F': 'finished by',
+    'D': 'contains',
+    's': 'starts',
+    'e': 'equals',
+    'S': 'started by',
+    'd': 'during',
+    'f': 'finishes',
+    'O': 'overlapped by',
+    'M': 'met by',
+    'P': 'preceded by'
+}
+
 
 def is_unix_time(time):
     return (isinstance(time, int) or isinstance(time, float)) and time > 0
@@ -30,7 +46,7 @@ def random_time(start, end=None,
     end = convert_to_unix_time(end)
     if probability_distribution is None:
         return randrange(start, end)
-        assert isinstance(probability_distribution, rv_frozen)
+    assert isinstance(probability_distribution, rv_frozen)
     assert end is not None
     random_value = probability_distribution.rvs()
     if random_value < start:
@@ -69,56 +85,6 @@ class Period(object):
     def __len__(self):
         return float(self.end - self.start)
 
-TEMPORAL_RELATION_NAMES = {
-    'p': 'precedes',
-    'm': 'meets',
-    'o': 'overlaps',
-    'F': 'finished by',
-    'D': 'contains',
-    's': 'starts',
-    'e': 'equals',
-    'S': 'started by',
-    'd': 'during',
-    'f': 'finishes',
-    'O': 'overlapped by',
-    'M': 'met by',
-    'P': 'preceded by'
-}
-
-
-class TemporalRelation(list):
-    def __init__(self, constituent_string):
-        self.append(constituent_string)
-
-    def append(self, string):
-        for char in string:
-            assert isinstance(char, str) and len(char) is 1 and char in TEMPORAL_RELATION_NAMES
-            list.append(self, char)
-
-    def __eq__(self, other):
-        assert isinstance(other, TemporalRelation) or isinstance(other, str)
-        for char in other:
-            if char not in self:
-                return False
-        return True
-
-    def __str__(self):
-        if len(self) is 13:
-            return 'temporal_relation_full'
-        result = 'temporal_relation_'
-        result += ''.join(self)
-        result += '(' + ', '.join([TEMPORAL_RELATION_NAMES[char] for char in self]) + ')'
-        return result
-
-    def __repr__(self):
-        return str(self)
-
-TEMPORAL_RELATION_FULL = TemporalRelation('pmoFDseSdfOMP')
-
-
-def probability_of_relation(temporal_event_1, temporal_event_2, temporal_relation):
-    pass
-
 
 class TemporalEvent(object):
     beginning_factor = 5
@@ -142,17 +108,19 @@ class TemporalEvent(object):
         while not self.period.start < self.beginning < self.ending < self.period.end:
             self.beginning = self.period.random(
                 probability_distribution=t(
-                    4,                                                      # df
-                    self.period.start + len(self) / self.beginning_factor,  # mean
-                    len(self) / self.beginning_factor                       # variance
+                    # df, mean, variance
+                    4,
+                    self.period.start + len(self) / self.beginning_factor,
+                    len(self) / self.beginning_factor
                 )
             )
 
             self.ending = self.period.random(
                 probability_distribution=t(
-                    4,                                                  # df
-                    self.period.end - len(self) / self.ending_factor,   # mean
-                    len(self) / self.ending_factor                      # variance
+                    # df, mean, variance
+                    4,
+                    self.period.end - len(self) / self.ending_factor,
+                    len(self) / self.ending_factor
                 )
             )
 
@@ -182,8 +150,55 @@ class TemporalEvent(object):
             return self._fuzzy_membership_single_point(time)
         return result
 
+    def __str__(self):
+        return 'TemporalEvent(start:{0} , beginning:{1}, ending:{2}, end:{3})'.format(self.period.start,
+                                                                                      self.beginning,
+                                                                                      self.ending,
+                                                                                      self.period.end)
+
+    def __repr__(self):
+        return str(self)
+
     def __len__(self):
         return len(self.period)
+
+
+class TemporalRelation(list):
+    def __init__(self, constituent_string, temporal_event_1, temporal_event_2):
+        self.append(constituent_string)
+        assert isinstance(temporal_event_1, TemporalEvent) and isinstance(temporal_event_2, TemporalEvent)
+        self.temporal_event_1 = temporal_event_1
+        self.temporal_event_2 = temporal_event_2
+
+    def append(self, other):
+        for char in other:
+            assert isinstance(char, str) and len(char) is 1 and char in TEMPORAL_RELATIONS
+            list.append(self, char)
+
+    def degree(self):
+        return 0.5
+
+    def __eq__(self, other):
+        assert isinstance(other, TemporalRelation) or isinstance(other, str)
+        if isinstance(other, TemporalRelation):
+            if (other.temporal_event_1, other.temporal_event_2) != (self.temporal_event_1, self.temporal_event_2):
+                return False
+        for char in other:
+            if char not in self:
+                return False
+        return True
+
+    def __str__(self):
+        if len(self) == len(TEMPORAL_RELATIONS):
+            return 'TemporalRelation(pmoFDseSdfOMP(full))'
+        if self == 'oFDseSdfO':
+            return 'TemporalRelation(oFDseSdfO(concur))'
+        return 'TemporalRelation({0}({1}))'.format(
+            ''.join(self),
+            ', '.join([TEMPORAL_RELATIONS[char] for char in self]))
+
+    def __repr__(self):
+        return str(self)
 
 
 def generate_random_events(size=100):
@@ -199,12 +214,40 @@ def generate_random_events(size=100):
     return events
 
 
-if __name__ == '__main__':
-    events = generate_random_events()
-    import matplotlib.pyplot as plt
-    for event in events:
-        x = range(event.period.start, event.period.end, 100)
-        y = event.fuzzy_membership(x)
-        plt.plot(x, y)
+def create_event_relation_hashtable(temporal_events):
+    table = {}
 
-    plt.show()
+    for A in temporal_events:
+        for B in temporal_events:
+            if B == A:
+                continue
+            for C in temporal_events:
+                if C in (A, B):
+                    continue
+                for r1 in TEMPORAL_RELATIONS:
+                    for r2 in TEMPORAL_RELATIONS:
+                        for r3 in TEMPORAL_RELATIONS:
+                            relation1 = TemporalRelation(r1, A, B)
+                            relation2 = TemporalRelation(r2, B, C)
+                            relation3 = TemporalRelation(r3, A, C)
+                            degrees = (relation1.degree(), relation2.degree(), relation3.degree())
+                            if degrees != (0, 0, 0):
+                                table[(r1, r2, r3)] = degrees
+
+    return table
+
+
+if __name__ == '__main__':
+    #events = generate_random_events()
+    #import matplotlib.pyplot as plt
+    #
+    #for event in events:
+    #    x = range(event.period.start, event.period.end, 100)
+    #    y = event.fuzzy_membership(x)
+    #    plt.plot(x, y)
+    #
+    #plt.show()
+
+    events = generate_random_events(10)
+    table = create_event_relation_hashtable(events)
+    print table
