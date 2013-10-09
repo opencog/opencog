3a42976ef2a5f4ab01e04c45e7f9f0016696a8e4
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2013-10-08 20:09:13 -0700

    Fix Python MOSES score parsing bug and upgrade unit tests

--- tests/cython/test_moses.py
+++ tests/cython/test_moses.py
@@ -1,10 +1,10 @@
 __author__ = 'Cosmo Harrigan'
 
-from unittest import TestCase
+from nose.tools import *
 from opencog.pymoses import moses, MosesException, MosesCandidate
 
 
-class MosesTest(TestCase):
+class TestMOSES:
     def setUp(self):
         self.moses = moses()
 
@@ -14,38 +14,37 @@ class MosesTest(TestCase):
     def test_run_xor_python(self):
         input_data = [[0, 0, 0], [1, 1, 0], [1, 0, 1], [0, 1, 1]]
         output = self.moses.run(input=input_data, python=True)
-        self.assertIsInstance(output[0], MosesCandidate)
-        self.assertEquals(output[0].score, 0)
+        assert isinstance(output[0], MosesCandidate)
+        assert output[0].score == 0
         model = output[0].eval
-        self.assertEquals(model([0, 0]), False)
-        self.assertEquals(model([1, 1]), False)
-        self.assertEquals(model([0, 1]), True)
-        self.assertEquals(model([1, 0]), True)
+        assert not model([0, 0])
+        assert not model([1, 1])
+        assert model([0, 1])
+        assert model([1, 0])
 
     def test_run_xor_combo(self):
         input_data = [[0, 0, 0], [1, 1, 0], [1, 0, 1], [0, 1, 1]]
         output = self.moses.run(input=input_data, args="-c 1")
-        self.assertEquals(len(output), 1)
-        self.assertIsInstance(output[0], MosesCandidate)
-        self.assertEquals(output[0].score, 0)
-        self.assertEquals(output[0].program, "and(or(!$1 !$2) or($1 $2))")
+        assert len(output) == 1
+        assert isinstance(output[0], MosesCandidate)
+        assert output[0].score == 0
+        assert output[0].program == "and(or(!$1 !$2) or($1 $2))"
 
     def test_run_majority_python(self):
         output = self.moses.run(args="-H maj -c 2", python=True)
-        self.assertIsInstance(output[0], MosesCandidate)
-        self.assertEquals(output[0].score, 0)
+        assert isinstance(output[0], MosesCandidate)
+        assert output[0].score == 0
         model = output[0].eval
-        self.assertEquals(model([0, 1, 0, 1, 0]), False)  # Returns: False
-        self.assertEquals(model([1, 1, 0, 1, 0]), True)  # Returns: True
-        self.assertIsInstance(output[1], MosesCandidate)
-        self.assertEquals(output[1].score, -1)
+        assert not model([0, 1, 0, 1, 0])
+        assert model([1, 1, 0, 1, 0])
+        assert isinstance(output[1], MosesCandidate)
+        assert output[1].score == -1
 
+    @raises(MosesException)
     def test_run_raise(self):
-        self.assertRaises(MosesException, self.moses.run, args="-c")
+        assert raises(MosesException, self.moses.run(args="-c"))
 
-    def test_run_manually(self):
-        self.moses.run_manually("--version")
-
+    @raises(MosesException)
     def test_run_manually_raise(self):
-        self.assertRaises(MosesException, self.moses.run_manually, args="-c")
+        assert raises(MosesException, self.moses.run_manually(args="-c"))
 
