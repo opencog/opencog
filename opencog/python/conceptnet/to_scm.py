ca011f0dc6d30eb320884cd525739657c8ee78e5
Amen Belayneh <amenbelayneh@gmail.com>
2013-11-25 10:38:23 +0300

    some more code cleanups

--- opencog/python/conceptnet/to_scm.py
+++ opencog/python/conceptnet/to_scm.py
@@ -1,9 +1,9 @@
 __author__ = 'Amen Belayneh'
 
-# This code creates a scheme file after inputing the address to a conceptnet csv file dump
-# The conceptnet csv file should be in the same folder as this script.
-# Make sure you add .scm when inputting the name for the scheme output file
-# The output file will be in the same folder as the script
+# This code creates a scheme file, after inputing the address to a conceptnet
+#  csv file dump. The conceptnet csv file should be in the same folder as this
+# script. Make sure you add '.scm' when inputting the name for the scheme
+# output file. The output file will be in the same folder as the script
 
 
 from opencog.atomspace import TruthValue
@@ -14,6 +14,8 @@ corpus_path = ""
 corpus_dict = {}
 conceptnet_dict = {}
 twf = 0
+# ConceptNet relation to Opencog Link mappings
+map_dict = {"/r/IsA": 'InheritanceLink'}
 
 
 def set_TV(word):
@@ -40,22 +42,23 @@ def set_TV(word):
             return stv
 
 
-def write_file(cn_assertion):  # assertion is a list
-    # 0.5 confidence is used(for links) because that is the weight given to
-    # most of the assertions on ConceptNet
-    DEFAULT_TV = TruthValue(1, 0.5)
-
-    if cn_assertion[0] == "/r/IsA":
-        return ('(InheritanceLink ' + '(stv ' + str(DEFAULT_TV.mean) + ' ' + str(DEFAULT_TV.count) + ')' + '\n\t' +
-                '(ConceptNode  ' + '"' + cn_assertion[1][6:] + '"' + ' (stv {cn_assertion1.mean} {cn_assertion1.count})' + ')\n\t' +
-                '(ConceptNode  ' + '"' + cn_assertion[2][6:] + '"' + ' (stv {cn_assertion2.mean} {cn_assertion2.count})' + ')\n)').format(cn_assertion1=set_TV(cn_assertion[1][6:]), cn_assertion2=set_TV(cn_assertion[2][6:]))
-
-    else:
-        return ('(EvaluationLink  ' + '(stv ' + str(DEFAULT_TV.mean) + ' ' + str(DEFAULT_TV.count) + ')' + '\n\t' +
-                '(PredicateNode  ' + '"' + cn_assertion[0][3:] + '"' + ' ' + ' (stv ' + str(set_TV(cn_assertion[0][3:]).mean) + ' ' + str(set_TV(cn_assertion[0][3:]).count) + ')' + ')\n\t\t' +
-                '(ListLink  ' + '\n\t\t\t' +
-                '(ConceptNode  ' + '"' + cn_assertion[1][6:] + '"' + ' (stv {cn_assertion1.mean} {cn_assertion1.count})' + ')\n\t\t\t' +
-                '(ConceptNode  ' + '"' + cn_assertion[2][6:] + '"' + ' (stv {cn_assertion2.mean} {cn_assertion2.count})' + ')\n\t\t)\n\t)\n)').format(cn_assertion1=set_TV(cn_assertion[1][6:]), cn_assertion2=set_TV(cn_assertion[2][6:]))
+def write_file(cn_assertion):
+    # Assertion is a list. 0.5 confidence is used(for links)because that is
+    # the weight given to most of the assertions on ConceptNet
+    TV = TruthValue(1, 0.5)
+    try:
+        link_type = map_dict[cn_assertion[0]]
+        return ('(' + str(link_type) + ' (stv ' + str(TV.mean) + ' ' + str(TV.count) + ')\n\t' +
+                '(ConceptNode "' + cn_assertion[1][6:] + '" (stv {cn_argument1.mean} {cn_argument1.count}))\n\t' +
+                '(ConceptNode "' + cn_assertion[2][6:] + '" (stv {cn_assertion2.mean} {cn_assertion2.count}))\n)'
+                ).format(cn_argument1=set_TV(cn_assertion[1][6:]), cn_assertion2=set_TV(cn_assertion[2][6:]))
+    except KeyError:
+        return ('(EvaluationLink (stv ' + str(TV.mean) + ' ' + str(TV.count) + ')\n\t' +
+                '(PredicateNode "' + cn_assertion[0][3:] + '" (stv ' + str(set_TV(cn_assertion[0][3:]).mean) + ' ' + str(set_TV(cn_assertion[0][3:]).count) + '))\n\t' +
+                '(ListLink \n\t\t' +
+                '(ConceptNode "' + cn_assertion[1][6:] + '" (stv {cn_argument1.mean} {cn_argument1.count}))\n\t\t' +
+                '(ConceptNode "' + cn_assertion[2][6:] + '" (stv {cn_assertion2.mean} {cn_assertion2.count}))\n\t)\n)'
+                ).format(cn_argument1=set_TV(cn_assertion[1][6:]), cn_assertion2=set_TV(cn_assertion[2][6:]))
 
 
 def from_file(cn_path, scm_name):
