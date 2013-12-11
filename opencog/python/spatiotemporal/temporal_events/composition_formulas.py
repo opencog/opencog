94b615cb40a5a3c874a05a4eddfffb92ac2548bc
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2013-12-11 21:48:29 +0330

    passing the same relation instead of all 26 ones to see if it fixes learning (it didn't)

--- opencog/python/spatiotemporal/temporal_events/composition_formulas.py
+++ opencog/python/spatiotemporal/temporal_events/composition_formulas.py
@@ -21,24 +21,13 @@ def create_sample_file(number_of_events=100):
 
 
 def create_learning_arguments(predicate='p', size=None):
-    train_x = []
-    for i in xrange(26):
-        train_x.append([])
+    train_x = [[], []]
     train_y = []
-    test_x = []
-    for i in xrange(26):
-        test_x.append([])
+    test_x = [[], []]
     test_y = []
     csv_reader = csv.reader(open(os.path.dirname(os.path.abspath(__file__)) + '/data.csv~'))
 
     predicate_index = TemporalRelation.all_relations.index(predicate)
-    no_of_ones = 0
-    no_of_zeros = 0
-    no_of_fracs = 0
-    training_size = 0
-
-    if size is None:
-        size = -1
 
     row_number = -1
     for row in csv_reader:
@@ -55,35 +44,18 @@ def create_learning_arguments(predicate='p', size=None):
         else:
             x, y = test_x, test_y
 
-        predicate_value = row[26 + predicate_index]
+        a_b_value = row[predicate_index]
+        b_c_value = row[13 + predicate_index]
+        a_c_value = row[26 + predicate_index]
 
-        if 0 < predicate_value < 1:
-            if no_of_fracs > no_of_ones or no_of_fracs > no_of_zeros:
-                continue
-            no_of_fracs += 1
-        elif predicate_value == 0:
-            if no_of_zeros > no_of_ones or no_of_zeros > no_of_fracs:
-                continue
-            no_of_zeros += 1
-        elif predicate_value == 1:
-            if no_of_ones > no_of_zeros or no_of_ones > no_of_fracs:
-                continue
-            no_of_ones += 1
-
-        training_size += 1
-
-        for i in xrange(26):
-            x[i].append(row[i])
-
-        y.append(predicate_value)
+        x[0].append(a_b_value)
+        x[1].append(b_c_value)
+        y.append(a_c_value)
 
     train_x = np.array(train_x).T
     test_x = np.array(test_x).T
     train_y = np.array(train_y)
     test_y = np.array(test_y)
-
-    print no_of_fracs, no_of_ones, no_of_zeros
-    print 'training size:', training_size
 
     return train_x, train_y, test_x, test_y
 
@@ -96,14 +68,14 @@ def learn_ffx():
 
 
 def learn_lasso():
-    size = 1000000
+    size = 1000
     train_x, train_y, test_x, test_y = create_learning_arguments(size=size)
     from sklearn.linear_model import Lasso, Ridge, ElasticNet, LinearRegression, LassoLars, BayesianRidge, ElasticNetCV, SGDRegressor
     from sklearn.svm import SVR
     from sklearn.neighbors import KNeighborsRegressor
     from random import randrange
 
-    clf = KNeighborsRegressor(n_neighbors=20)
+    clf = KNeighborsRegressor()
     clf.fit(train_x, train_y)
 
     for i in [randrange(0, len(train_y) / 2) for x in xrange(20)]:
