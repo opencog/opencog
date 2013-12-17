de59130812229911f105afa2f78839bdd7efc3aa
Linas Vepstas <linasvepstas@gmail.com>
2013-12-16 21:43:41 -0600

    Add rules about neighbors

--- tests/query/deduct-einstein.scm
+++ tests/query/deduct-einstein.scm
@@ -6,7 +6,22 @@
 
 (define (stv mean conf) (cog-new-stv mean conf))
 
+;; A little handly-dandy utility to avoid over-reporting of "obvious"
+;; results. We declare that person1 is the same asm person1, etc.
+;; A kind-of pauli-exclusion-principle at work.
+(define (same person)
+	(EvaluationLink (stv 1 1)
+		(PredicateNode "IsSamePerson")
+		(ListLink
+			(AvatarNode person)
+			(AvatarNode person)
+		)
+	)
+)
+
+;; A declaration of fact: it is true that pred has value for person.
 (define (fact person pred value)
+	(same person)
 	(EvaluationLink (stv 1 1)
 		(PredicateNode pred)
 		(ListLink
@@ -15,6 +30,20 @@
 		)
 	)
 )
+
+;; A neighbor-predicate
+(define (neighbor person1 person2)
+	(same person1)
+	(same person2)
+	(EvaluationLink (stv 1 1)
+		(PredicateNode "Neighbor")
+		(ListLink
+			(AvatarNode person1)
+			(AvatarNode person2)
+		)
+	)
+)
+
 
 ;; 1. The Englishman lives in the red house.
 (fact "person1" "Nationality" "British")
@@ -45,15 +74,16 @@
 
 ;; 8. The man in the center house drinks milk.
 (fact "person8" "Drinks" "milk")
-;; ()
+(fact "person8" "Address" "103 Main Street")
 
 ;; 9. The Norwegian lives in the first house.
 (fact "person9" "Nationality" "Norwegian")
-;; ()
+(fact "person9" "Address" "101 Main Street")
 
 ;; 10. The Blend smoker has a neighbor who keeps cats.
 (fact "person10" "Smokes" "Blend")
-;; ()
+(neighbor "person10" "catperson")
+(fact "catperson" "Keeps" "cats")
 
 ;; 11. The man who smokes Blue Masters drinks bier.
 (fact "person11" "Smokes" "Blue Master")
@@ -61,7 +91,8 @@
 
 ;; 12. The man who keeps horses lives next to the Dunhill smoker.
 (fact "person12" "Keeps" "horses")
-;; ()
+(neighbor "person12" "dun_smoke_person")
+(fact "dun_smoke_person" "Smokes" "Dunhill")
 
 ;; 13. The German smokes Prince.
 (fact "person13" "Nationality" "German")
@@ -73,34 +104,22 @@
 
 ;; 15. The Blend smoker has a neighbor who drinks water.
 (fact "person15" "Smokes" "Blend")
-;; ()
+(neighbor "person15" "water_person")
+(fact "water_person" "Drinks" "water")
 
-
-;; A little handly-dandy utility to avoid over-reporting of "obvious"
-;; results. We declare that person1 is the same asm person1, etc.
-;; A kind-of pauli-exclusion-principle at work.
-(define (same person)
+;; State some facts about neighboring houses
+(define (left-of house1 house2)
 	(EvaluationLink (stv 1 1)
-		(PredicateNode "IsSamePerson")
+		(PredicateNode "LeftOf")
 		(ListLink
-			(AvatarNode person)
-			(AvatarNode person)
+			(ConceptNode house1)
+			(ConceptNode house2)
 		)
 	)
 )
 
-(same "person1")
-(same "person2")
-(same "person3")
-(same "person4")
-(same "person5")
-(same "person6")
-(same "person7")
-(same "person8")
-(same "person9")
-(same "person10")
-(same "person11")
-(same "person12")
-(same "person13")
-(same "person14")
-(same "person15")
+(left-of "101 Main Street" "102 Main Street")
+(left-of "102 Main Street" "103 Main Street")
+(left-of "103 Main Street" "104 Main Street")
+(left-of "104 Main Street" "105 Main Street")
+
