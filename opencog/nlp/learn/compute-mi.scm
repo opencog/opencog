271cc3556caea0c6f051b94d5e5d97bfda55832a
Linas Vepstas <linasvepstas@gmail.com>
2013-12-27 15:56:23 -0600

    ongoing MI calculations

--- opencog/nlp/learn/compute-mi.scm
+++ opencog/nlp/learn/compute-mi.scm
@@ -10,6 +10,8 @@
 ; been observed.  The observation-count for a single atom is stored in
 ; the 'count' value of its CountTruthValue. This routine just fetches
 ; those, and addes them up.
+;
+; The returned value is the total count.
 
 (define (get-total-atom-count atom-list)
 	(let ((cnt 0))
@@ -25,6 +27,8 @@
 ; The liklihood will be stored in the atom's TV 'confidence' location.
 ; The log liklihood is -log_2(frequency), with the frequency computed
 ; by simply taking the atom's count value, and dividing by the total.
+;
+; This returns the atom that was provided, but now with the logli set.
 
 (define (compute-atom-logli atom total)
 	(let* (
@@ -40,22 +44,41 @@
 )
 
 ; ----------------------------------------------------
+; Compute the occurance logliklihoods for a list of atoms.
+;
+; This sums up the occurance-count over the entire list of atoms,
+; and uses that as the normalization for the probability frequency
+; for the individual atoms in the list. It then computes the log_2
+; likelihood for each atom in the list, based on the total.
+;
+; As usual, the raw counts are obtained from the 'count' slot on a
+; CountTruthValue, and the logli is stored in the 'confidence' slot.
+;
+; This returns the atom-list, but now with the logli's set.
+
+(define (compute-all-logli atom-list)
+	(let ((total (get-total-atom-count atom-list)))
+		(map
+			(lambda (atom) (compute-atom-logli atom total))
+			atom-list
+		)
+	)
+)
+
+; ----------------------------------------------------
 ; Compute the occurance logliklihoods for all words.
 ;
 ; Load all word-nodes into the atomspace, first, so that an accurate
 ; count of word-occurances can be obtained.  The loglikli for a given
 ; word node is stored in the 'confidence' slot of the CountTruthValue.
+;
+; This returns the list of all word-nodes, with the logli's set.
 
 (define (compute-all-word-freqs)
 	(begin
 		; Make sure that all word-nodes are in the atom table.
 		(load-atoms-of-type 'WordNode)
-		(let ((total (get-total-atom-count (cog-get-atoms 'WordNode))))
-			(cog-map-type
-				(lambda (atom) (compute-atom-logli atom total) #f)
-				'WordNode
-			)
-		)
+		(compute-all-logli (cog-get-atoms 'WordNode))
 	)
 )
 
@@ -102,7 +125,7 @@
 ; are too numerous to keep around.
 
 (define (compute-left-pair-logli word lg_rel)
-	(define bind-link 
+	(define left-bind-link 
 		(BindLink
 			(TypedVariableLink
 				(VariableNode "$left-word")
@@ -126,6 +149,30 @@
 			)
 		)
 	)
+	(define right-bind-link 
+		(BindLink
+			(TypedVariableLink
+				(VariableNode "$right-word")
+				(VariableTypeNode "WordNode")
+			)
+			(ImplicationLink
+				(EvaluationLink
+					lg_rel
+					(ListLink
+						word
+						(VariableNode "$right-word")
+					)
+				)
+				(EvaluationLink
+					lg_rel
+					(ListLink
+						word
+						(VariableNode "$right-word")
+					)
+				)
+			)
+		)
+	)
 	(let* (
 			; inset is a list of ListLink's of word-pairs
 			(inset (cog-incoming-set (fetch-incoming-set word)))
@@ -134,9 +181,15 @@
 					(lambda (ll) (cog-incoming-set (fetch-incoming-set ll)))
 					inset)
 			)
-			; lefties are those with the varyig left side only.
-			(lefties (cog-bind bind-link))
+			; lefties are those with the varying left side only.
+			(lefties (cog-outgoing-set (cog-bind left-bind-link)))
+			(righties (cog-outgoing-set (cog-bind right-bind-link)))
+
+			; the total occurance counts
+			(left-total (get-total-atom-count lefties))
+			(right-total (get-total-atom-count righties))
 		)
+		;(compute-all-logli (cog-get-atoms 'WordNode))
 	)
 )
 
