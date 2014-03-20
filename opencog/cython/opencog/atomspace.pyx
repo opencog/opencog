219d1afdc14df89c2517505cad8148264613431e
Linas Vepstas <linasvepstas@gmail.com>
2014-03-20 10:29:25 -0500

    misc defered cython maintenance

--- opencog/cython/opencog/atomspace.pyx
+++ opencog/cython/opencog/atomspace.pyx
@@ -1,8 +1,9 @@
-# cython/distutils can only handle a single file as the source for a python module
-# since it is helpful to be able to split the binding code into separate files,
-# we just include them here
-
-# note that the ordering of include statements may influence whether things work or not
+# Cython/distutils can only handle a single file as the source for
+# a python module.  Since it is helpful to be able to split the binding
+# code into separate files, we just include them here.
+#
+# Note that the ordering of include statements may influence whether
+# things work or not
 
 include "classserver.pyx"
 include "atomspace_details.pyx"
