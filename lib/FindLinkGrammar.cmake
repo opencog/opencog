5bac6985511e6c807cadb3f962d2243862a61b2d
Linas Vepstas <linasvepstas@gmail.com>
2014-03-20 12:22:21 -0500

    synchronize with master sources in link-grammar

--- lib/FindLinkGrammar.cmake
+++ lib/FindLinkGrammar.cmake
@@ -5,17 +5,39 @@
 #  LINK_GRAMMAR_LIBRARIES - The libraries needed to use link-grammar
 #  LINK_GRAMMAR_DATA_DIR - the dir where you will find the dictionaries
 
-# Copyright (c) 2008, 2014 OpenCog.org (http://opencog.org)
+# Copyright (c) 2008 OpenCog.org (http://opencog.org)
+# Copyright (c) 2014 Linas Vepstas
 #
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # Look for the header file
 FIND_PATH(LINK_GRAMMAR_INCLUDE_DIR link-grammar/link-includes.h)
 FIND_PATH(LINK_GRAMMAR_DATA_DIR 4.0.dict
 	PATHS 
 		/usr/share/link-grammar/en/
-		/usr/local/share/link-grammar/en/)
+		/usr/local/share/link-grammar/en/
+		/opt/local/share/link-grammar/en/)
 
 # Look for the library
 FIND_LIBRARY(LINK_GRAMMAR_LIBRARY
