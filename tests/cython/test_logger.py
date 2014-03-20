a93775e1c7f226c25923e5fc116d2eaf928f701c
Linas Vepstas <linasvepstas@gmail.com>
2014-03-20 13:03:56 -0500

    Arghhh. One more spot

--- tests/cython/test_logger.py
+++ tests/cython/test_logger.py
@@ -12,7 +12,7 @@ class LoggerTest(TestCase):
         # close the temp file as Logger will want to manually
         # open it
         os.close(tempfd) 
-        self.log = opencog.util.create_logger(self.tempfn)
+        self.log = opencog.logger.create_logger(self.tempfn)
 
     def tearDown(self):
         del self.log
