f1faadfe067846546987563c2492523585f86927
Linas Vepstas <linasvepstas@gmail.com>
2014-03-29 15:40:26 -0500

    Attempt to fix initialization issues

--- opencog/cython/opencog/scheme_wrapper.pyx
+++ opencog/cython/opencog/scheme_wrapper.pyx
@@ -14,17 +14,16 @@ cdef extern from "opencog/query/PatternSCM.h" namespace "opencog":
     cdef cppclass PatternSCM:
         PatternSCM()
 
+cdef PatternSCM pattern_object
+def __init__():
+    global pattern_object
+    pattern_object = PatternSCM()
+
+# Now actually run it ... 
+__init__()
+
 cdef extern from "opencog/cython/opencog/PyScheme.h" namespace "opencog":
     string eval_scheme(cAtomSpace& as, const string& s)
-
-cdef extern from "opencog/cython/opencog/PyScheme.h" namespace "opencog":
-    cHandle eval_scheme_h(cAtomSpace& as, const string& s)
-
-cdef extern from "opencog/guile/load-file.h" namespace "opencog":
-    int load_scm_file (cAtomSpace& as, char* filename)
-
-def __init__():
-    PatternSCM()
 
 # Returns a string value
 def scheme_eval(AtomSpace a, char* s):
@@ -34,6 +33,9 @@ def scheme_eval(AtomSpace a, char* s):
     ret = eval_scheme(deref(a.atomspace), expr)
     return ret.c_str()
 
+cdef extern from "opencog/cython/opencog/PyScheme.h" namespace "opencog":
+    cHandle eval_scheme_h(cAtomSpace& as, const string& s)
+
 # Returns a Handle
 def scheme_eval_h(AtomSpace a, char* s):
     cdef cHandle ret
@@ -41,6 +43,9 @@ def scheme_eval_h(AtomSpace a, char* s):
     expr = string(s)
     ret = eval_scheme_h(deref(a.atomspace), expr)
     return Handle(ret.value())
+
+cdef extern from "opencog/guile/load-file.h" namespace "opencog":
+    int load_scm_file (cAtomSpace& as, char* filename)
 
 def load_scm(AtomSpace a, char* fname):
     status = load_scm_file(deref(a.atomspace), fname)
