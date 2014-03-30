94dcd52322841d572b142dc5d42b0c01f5787cab
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2014-03-29 23:56:46 -0700

    cog-af will return list of atoms in AttentionalFocus in Scheme

--- opencog/guile/SchemeSmob.cc
+++ opencog/guile/SchemeSmob.cc
@@ -216,9 +216,10 @@ void SchemeSmob::register_procs(void)
 	scm_c_define_gsubr("cog-av?",               1, 0, 0, C(ss_av_p));
 	scm_c_define_gsubr("cog-av->alist",         1, 0, 0, C(ss_av_get_value));
 
-	// AttentionalFocus Boundary
+	// AttentionalFocus
 	scm_c_define_gsubr("cog-af-boundary",       0, 0, 0, C(ss_af_boundary));
 	scm_c_define_gsubr("cog-set-af-boundary!",  1, 0, 0, C(ss_set_af_boundary));
+	scm_c_define_gsubr("cog-af",                0, 0, 0, C(ss_af));
 
 	// Atom types
 	scm_c_define_gsubr("cog-get-types",         0, 0, 0, C(ss_get_types));
