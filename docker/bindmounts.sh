b646ef865fc715e604082bb5323a762916c0fafc
David Hart <dhart@opencog.org>
2014-05-14 08:28:28 +0800

    added start of embodiment dockerfile, fixed password exposure in buildslave dockerfile

--- docker/bindmounts.sh
+++ docker/bindmounts.sh
@@ -1,10 +1,15 @@
-#mkdir -v ~/opencog/docker/opencog #in git
-#mkdir -v ~/opencog/docker/distcc/opencog #in git
-#mkdir -v ~/opencog/docker/buildslave/opencog #in git
+mkdir -pv ~/opencog/docker/opencog
+mkdir -pv ~/opencog/docker/distcc/opencog
+mkdir -pv ~/opencog/docker/buildslave/opencog
+mkdir -pv ~/opencog/docker/manualbuild/opencog
+mkdir -pv ~/opencog/docker/embodiment/opencog
 #echo opencog > ~/opencog/docker/.gitignore #in git
 #echo opencog > ~/opencog/docker/distcc/.gitignore #in git
 #echo opencog > ~/opencog/docker/buildslave/.gitignore #in git
-sudo mount -v --bind ~/opencog ~/opencog/docker/opencog 
+#echo opencog > ~/opencog/docker/manualbuild/.gitignore #in git
+#echo opencog > ~/opencog/docker/embodiment/.gitignore #in git
+sudo mount -v --bind ~/opencog ~/opencog/docker/opencog
 #sudo mount -v --bind ~/opencog ~/opencog/docker/distcc/opencog #not needed
-#sudo mount -v --bind ~/opencog ~/opencog/docker/buildslave/opencog #not needed
-sudo mount -v --bind ~/opencog ~/opencog/docker/opencog-build/opencog
+sudo mount -v --bind ~/opencog ~/opencog/docker/buildslave/opencog
+sudo mount -v --bind ~/opencog ~/opencog/docker/manualbuild/opencog
+sudo mount -v --bind ~/opencog ~/opencog/docker/embodiment/opencog
