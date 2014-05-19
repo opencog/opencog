b1b0b0a1057db97f8b8e457ac11ed82307388bf1
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-05-19 22:48:59 +0200

    Added example sentences to be-inheritance & be-inheritance-det-rule

--- opencog/nlp/scm/relex-to-logic.scm
+++ opencog/nlp/scm/relex-to-logic.scm
@@ -305,12 +305,14 @@
     	)
 )
 
+; Examples: "Cats are animals", "Socrates is a man", "Trees are plants"
 (define (be-inheritance-rule subj_concept subj_instance obj_concept obj_instance)
     (InheritanceLink (ConceptNode subj_instance) (ConceptNode subj_concept))
 	(InheritanceLink (ConceptNode obj_instance) (ConceptNode obj_concept))
     (InheritanceLink (ConceptNode subj_instance) (ConceptNode obj_instance))
 )
 
+; Examples: "This man is a plumber", "The girls are vegetarians", "That scientist is a genius"
 (define (be-inheritance-det-rule subj_concept subj_instance obj_concept obj_instance determiner)
     (InheritanceLink (ConceptNode subj_instance) (ConceptNode subj_concept))
     (InheritanceLink (ConceptNode obj_instance) (ConceptNode obj_concept))
