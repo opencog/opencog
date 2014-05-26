63f36b08100e7511e42999d7956abd6e7c1346af
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-05-26 16:28:10 +0200

    Added stvs to ConceptNodes for testing r2l output

--- opencog/python/pln/examples/relex2logic/r2l-output-test.scm
+++ opencog/python/pln/examples/relex2logic/r2l-output-test.scm
@@ -1,3 +1,8 @@
+(ConceptNode "man@b4016560-6832-4ae1-a140-d469730ef140" (stv 1.0 1.0))
+(ConceptNode "man" (stv 1.0 1.0))
+(ConceptNode "Socrates@19c31060-a1e3-4cc3-ac89-cdea1b0cbe29" (stv 1.0 1.0))
+(ConceptNode "men@ef0fa2d2-334d-4895-bcf5-0597f5133290" (stv 1.0 1.0)) 
+
 (InheritanceLink (stv 1.000000 0.990000)
   (ConceptNode "man@b4016560-6832-4ae1-a140-d469730ef140") ; [129]
   (ConceptNode "man") ; [130]
