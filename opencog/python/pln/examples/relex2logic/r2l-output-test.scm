4887cb5ab2ec3ceaaa21c870b45e154497fc3de9
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-05-26 10:13:01 +0200

    Created file for debugging r2l output

---
+++ opencog/python/pln/examples/relex2logic/r2l-output-test.scm
@@ -0,0 +1,14 @@
+(InheritanceLink (stv 1.000000 0.990000)
+  (ConceptNode "man@b4016560-6832-4ae1-a140-d469730ef140") ; [129]
+  (ConceptNode "man") ; [130]
+) ; [131]
+
+(InheritanceLink (stv 1.000000 1.000000)
+  (ConceptNode "Socrates@19c31060-a1e3-4cc3-ac89-cdea1b0cbe29") ; [126]
+  (ConceptNode "man@b4016560-6832-4ae1-a140-d469730ef140") ; [129]
+) ; [132]
+
+(InheritanceLink (stv 1.000000 0.990000)
+  (ConceptNode "men@ef0fa2d2-334d-4895-bcf5-0597f5133290") ; [211]
+  (ConceptNode "man") ; [212]
+) ; [213]
