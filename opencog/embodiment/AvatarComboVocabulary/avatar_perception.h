e5b56e538ad2a499787fb9f566a20af7a408f6a0
Nil Geisweiller <ngeiswei@gmail.com>
2014-05-27 21:18:48 +0300

    Add is_battery_ahead combo perception vocabulary

--- opencog/embodiment/AvatarComboVocabulary/avatar_perception.h
+++ opencog/embodiment/AvatarComboVocabulary/avatar_perception.h
@@ -141,6 +141,9 @@ enum avatar_perception_enum {
     is_last_avatar_schema,
     is_last_group_command,
 
+    // For Santa Fe trailing problem
+    is_battery_ahead,
+
     avatar_perception_count
 };
 }
@@ -267,6 +270,8 @@ static const perception_basic_description pbd[] = {
     { id::is_last_agent_action, "is_last_agent_action", "->(union(definite_object indefinite_object wild_card) action_definite_object arg_list(union(definite_object indefinite_object)) boolean)" },
     { id::is_last_avatar_schema,   "is_last_avatar_schema",   "->(action_definite_object action_result arg_list(union(definite_object indefinite_object wild_card)) boolean)" },
     { id::is_last_group_command, "is_last_group_command", "->(definite_object definite_object arg_list(union(definite_object contin)) boolean)" }
+
+    { id::is_battery_ahead, "is_battery_ahead", "->(boolean)" }
 };
 
 static const perception_property_description ppd[] = {
@@ -367,6 +372,8 @@ static const perception_property_description ppd[] = {
     { id::is_last_agent_action, false,   false,     false,      false,    false,    false },
     { id::is_last_avatar_schema,   false,   false,     false,      false,    false,    false },
     { id::is_last_group_command,   false,   false,     false,      false,    false,    false }
+
+    { id::is_battery_ahead,        false,   false,     false,      false,    false,    false }
 };
 
 
