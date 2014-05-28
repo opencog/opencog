5c4e7f415eff772cff9b453eea90fe4d1e1f55b1
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-05-28 17:17:25 +0200

    Added exact PLN input as produced by R2L

--- opencog/python/pln/examples/relex2logic/r2l-output-test.scm
+++ opencog/python/pln/examples/relex2logic/r2l-output-test.scm
@@ -1,19 +1,82 @@
-(ConceptNode "man@b4016560-6832-4ae1-a140-d469730ef140" (stv .1 1.0))
-(ConceptNode "man" (stv .1 1.0))
-(ConceptNode "Socrates@19c31060-a1e3-4cc3-ac89-cdea1b0cbe29" (stv .1 1.0))
-(ConceptNode "men@ef0fa2d2-334d-4895-bcf5-0597f5133290" (stv .1 1.0)) 
+(ConceptNode "Socrates@f250f429-5078-4453-8662-c63cc8f58a22") ; [217]
+
+(ConceptNode "Socrates" (stv .1 1.0)) ; [218]
+
+(ConceptNode "man@80d4e852-1b93-4c49-84e1-5a7352b0dcb1" (stv .1 1.0)) ; [220]
+
+(ConceptNode "men@83d83a2e-940c-46aa-91f1-a7d2c106ef13" (stv .1 1.0)) ; [290]
+
+(ConceptNode "man" (stv .1 1.0)) ; [221]
+
+(ConceptNode "air@e3f175ea-c3d1-45cf-883d-a6d2f6a879ac") ; [292]
+
+(ConceptNode "air") ; [293]
+
+(ConceptNode "present") ; [227]
+
+(ListLink (stv 1.000000 0.000000)
+  (ConceptNode "men@83d83a2e-940c-46aa-91f1-a7d2c106ef13") ; [290]
+  (ConceptNode "air@e3f175ea-c3d1-45cf-883d-a6d2f6a879ac") ; [292]
+) ; [295]
+
+(EvaluationLink (stv 1.000000 1.000000)
+  (PredicateNode "breathe@218b15b2-52a8-430c-93f6-b4bba225418c") ; [287]
+  (ListLink (stv 1.000000 0.000000)
+    (ConceptNode "men@83d83a2e-940c-46aa-91f1-a7d2c106ef13") ; [290]
+    (ConceptNode "air@e3f175ea-c3d1-45cf-883d-a6d2f6a879ac") ; [292]
+  ) ; [295]
+) ; [296]
+
+(InheritanceLink (stv 1.000000 0.000000)
+  (PredicateNode "breathe@218b15b2-52a8-430c-93f6-b4bba225418c") ; [287]
+  (PredicateNode "breathe") ; [288]
+) ; [289]
 
 (InheritanceLink (stv 1.000000 0.990000)
-  (ConceptNode "man@b4016560-6832-4ae1-a140-d469730ef140") ; [129]
-  (ConceptNode "man") ; [130]
-) ; [131]
+  (ConceptNode "men@83d83a2e-940c-46aa-91f1-a7d2c106ef13") ; [290]
+  (ConceptNode "man") ; [221]
+) ; [291]
+
+(InheritanceLink (stv 1.000000 0.000000)
+  (ConceptNode "air@e3f175ea-c3d1-45cf-883d-a6d2f6a879ac") ; [292]
+  (ConceptNode "air") ; [293]
+) ; [294]
+
+(InheritanceLink (stv 1.000000 0.000000)
+  (PredicateNode "breathe@218b15b2-52a8-430c-93f6-b4bba225418c") ; [287]
+  (ConceptNode "present") ; [227]
+) ; [297]
 
 (InheritanceLink (stv 1.000000 1.000000)
-  (ConceptNode "Socrates@19c31060-a1e3-4cc3-ac89-cdea1b0cbe29") ; [126]
-  (ConceptNode "man@b4016560-6832-4ae1-a140-d469730ef140") ; [129]
-) ; [132]
+  (ConceptNode "Socrates@f250f429-5078-4453-8662-c63cc8f58a22") ; [217]
+  (ConceptNode "Socrates") ; [218]
+) ; [219]
 
 (InheritanceLink (stv 1.000000 0.990000)
-  (ConceptNode "men@ef0fa2d2-334d-4895-bcf5-0597f5133290") ; [211]
-  (ConceptNode "man") ; [212]
-) ; [213]
+  (ConceptNode "man@80d4e852-1b93-4c49-84e1-5a7352b0dcb1") ; [220]
+  (ConceptNode "man") ; [221]
+) ; [222]
+
+(InheritanceLink (stv 1.000000 1.000000)
+  (ConceptNode "Socrates@f250f429-5078-4453-8662-c63cc8f58a22") ; [217]
+  (ConceptNode "man@80d4e852-1b93-4c49-84e1-5a7352b0dcb1") ; [220]
+) ; [223]
+
+(InheritanceLink (stv 1.000000 0.000000)
+  (PredicateNode "is@9fb63f23-52ce-44ab-9bc4-806f0c4926e4") ; [224]
+  (PredicateNode "be") ; [225]
+) ; [226]
+
+(InheritanceLink (stv 1.000000 0.000000)
+  (PredicateNode "is@9fb63f23-52ce-44ab-9bc4-806f0c4926e4") ; [224]
+  (ConceptNode "present") ; [227]
+) ; [228]
+
+(PredicateNode "breathe@218b15b2-52a8-430c-93f6-b4bba225418c") ; [287]
+
+(PredicateNode "breathe") ; [288]
+
+(PredicateNode "is@9fb63f23-52ce-44ab-9bc4-806f0c4926e4") ; [224]
+
+(PredicateNode "be") ; [225]
+
