735ace9fb881360a3fdcc96eb3e6d3fa1dddfde9
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-05-31 11:56:51 +0200

    Put all inputs in ListLink

--- tests/python/test_pln/scm/relex2logic_pln_examples/context-link.scm
+++ tests/python/test_pln/scm/relex2logic_pln_examples/context-link.scm
@@ -1,50 +1,55 @@
-; In the context of music, a bass is an instrument.
-(ContextLink (stv .9 .9)
-    (ConceptNode "music")
-    (InheritanceLink
-        (ConceptNode "bass")
-        (ConceptNode "instrument")))
+; Inference example using ContextLinks
 
-; In the context of food, a bass is a fish.
-(ContextLink (stv .9 .9)
-    (ConceptNode "food")
-    (InheritanceLink
-        (ConceptNode "bass")
-        (ConceptNode "fish")))
-
-(HebbianLink (stv .9 .9)
-    (ConceptNode "eating")
-    (ConceptNode "food"))
-
-; I like eating bass.
-; RelEx at the moment delivers the wrong output for this sentence.
-; Eating is interpreted as an adjective:
-; _obj(like, bass)
-; _subj(like, I)
-; _amod(bass, eating)
-; The output produced by RelEx2Logic should be:
 (EvaluationLink
     (PredicateNote "inputs")
-        (ListLink
-            (EvaluationLink
-                (PredicateNode "like")
-                (ListLink
-                    (ConceptNode "I")
-                    (EvaluationLink
-                        (PredicateNode "eat")
-                        (ListLink
-                            (ConceptNode "I")
-                            (ConceptNode "bass@12345")))))
+    (ListLink
+        ; "I like eating bass."
+        ; RelEx at the moment delivers the wrong output for this sentence.
+        ; Eating is interpreted as an adjective:
+        ; _obj(like, bass)
+        ; _subj(like, I)
+        ; _amod(bass, eating)
+        ; The output produced by RelEx2Logic should be:
+        (EvaluationLink
+            (PredicateNode "like")
+            (ListLink
+                (ConceptNode "I")
+                (EvaluationLink
+                    (PredicateNode "eat")
+                    (ListLink
+                        (ConceptNode "I")
+                        (ConceptNode "bass@12345")))))
+        (InheritanceLink
+            (ConceptNode "bass@12345")
+            (ConceptNode "bass"))
+        ; I think there should be some other meaningful R2L from this
+        ; sentence, but I'm not sure what is necessary.
+
+        ; "In the context of music, a bass is an instrument."
+        (ContextLink (stv .9 .9)
+            (ConceptNode "music")
             (InheritanceLink
-                (ConceptNode "bass@12345")
-                (ConceptNode "bass"))
-        ; there should be some more inputs here, but I'm not sure
-        ; what else meaningful R2L would derive from the sentence.
+                (ConceptNode "bass")
+                (ConceptNode "instrument")))
+        
+        ; "In the context of food, a bass is a fish."
+        (ContextLink (stv .9 .9)
+            (ConceptNode "food")
+            (InheritanceLink
+                (ConceptNode "bass")
+                (ConceptNode "fish")))
+        
+        ; "Eating is associated with food."
+        ; I don't think this should be produced by R2L but
+        ; rather be part of a general knowledge base.
+        (HebbianLink (stv .9 .9)
+            (ConceptNode "eating")
+            (ConceptNode "food"))
     )
 )
 
 ; The final InheritanceLink would be derived with the DeductionRule.
-; I'm not sure how to get their, though. Particularly, I'm uncertain
+; I'm not sure how to get there, though. Particularly, I'm uncertain
 ; how to leverage ContextLink and HebbianLink and how to derive
 ; a relationship between "bass" and "eating" from the example sentence.
 
