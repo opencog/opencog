8aefeabfba0879937978e24bf90e28d4969d04e7
rodsol <rodisolomon@gmail.com>
2014-06-05 16:12:51 +0300

    lower order to-do-rule-5

--- opencog/nlp/scm/relex-to-logic.scm
+++ opencog/nlp/scm/relex-to-logic.scm
@@ -235,16 +235,16 @@
 
 ;Example: "She wants to sing."; verb1 = want, verb2 = sing, subj = she
 (define (to-do-rule-5 verb1 verb1_instance verb2 verb2_instance subj subj_instance)
-	(InheritanceLink (ConceptNode subj_instance) (ConceptNode subj))
-	(InheritanceLink (PredicateNode verb1_instance) (PredicateNode verb1))
-	(InheritanceLink (PredicateNode verb2_instance) (PredicateNode verb2))
-	(EvaluationLink
-		(PredicateNode verb1_instance)
-		(EvaluationLink
-			(PredicateNode verb2_instance)		 
-			(ConceptNode subj_instance)
-		)
-	)
+    (InheritanceLink (ConceptNode subj_instance) (ConceptNode subj))
+    (InheritanceLink (PredicateNode verb1_instance) (PredicateNode verb1))
+    (InheritanceLink (PredicateNode verb2_instance) (PredicateNode verb2))
+    (EvaluationLink
+        (PredicateNode verb1_instance)
+        (ListLink
+            (ConceptNode subj_instance)
+            (PredicateNode verb2_instance)
+        )
+    )
 )
 
 (define (to-be-rule verb verb_ins adj adj_ins subj subj_ins)
