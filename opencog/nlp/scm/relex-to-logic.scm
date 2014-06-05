8deb43e1a8d6f3a8b5205342a101d19faedde32f
rodsol <rodisolomon@gmail.com>
2014-06-05 10:15:50 +0300

    correct indentation

--- opencog/nlp/scm/relex-to-logic.scm
+++ opencog/nlp/scm/relex-to-logic.scm
@@ -222,16 +222,16 @@
 
 ;Example: "She must be able to sing." ; v1_instance = dance , v2_instance = she
 (define (to-do-rule-4 v1 v1_instance v2 v2_instance) 
-        (InheritanceLink (ConceptNode v2_instance) (ConceptNode v2))
-        (InheritanceLink (PredicateNode v1_instance) (PredicateNode v1))
-	    (EvaluationLink
-		    (PredicateNode "able_to")
-		    (ListLink
-                (EvaluationLink
-                    (PredicateNode v1_instance)
-                    (ListLink
-			            (ConceptNode v2_instance)))
-	    )
+    (InheritanceLink (ConceptNode v2_instance) (ConceptNode v2))
+    (InheritanceLink (PredicateNode v1_instance) (PredicateNode v1))
+    (EvaluationLink
+        (PredicateNode "able_to")
+        (ListLink
+            (EvaluationLink
+                (PredicateNode v1_instance)
+                (ListLink
+                    (ConceptNode v2_instance)))
+        )
     )
 )
 
