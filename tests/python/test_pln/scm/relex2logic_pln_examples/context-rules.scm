3c03bdd001b85169abdea4b2adb84caceea4e281
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-06-09 12:40:28 +0200

    Created file with simple test examples using pattern matcher context rules

---
+++ tests/python/test_pln/scm/relex2logic_pln_examples/context-rules.scm
@@ -0,0 +1,64 @@
+; Examples to test the contextualize and decontextualize rules
+; using the pattern matcher
+; The rules can be loaded from opencog/learning/RuleEngine/rules/pln
+; together with this input data.
+
+(EvaluationLink
+    (PredicateNode "inputs")
+    (ListLink
+        ; 1) input for (pln-rule-contextualize-inheritance)
+        ; "Ben is competent in doing mathematics."
+        (Inheritance
+            (AndLink
+                (ConceptNode "Ben")
+                (ConceptNode "doing_mathematics"))
+            (AndLink
+                (ConceptNode "competent")
+                (ConceptNode "doing_mathematics")))
+        ; 2) input for (pln-rule-contextualize-evaluation)
+        ; "The sky is blue in the context of the earth.
+        ; The sky is not blue in the context of the moon."
+        (EvaluationLink (stv .8 .7)
+            (PredicateNode "isBlue")
+            (AndLink
+                (ConceptNode "sky")
+                (ConceptNode "earth")))
+        (EvaluationLink (stv .0 .2)
+            (PredicateNode "isBlue")
+            (AndLink
+                (ConceptNode "sky")
+                (ConceptNode "moon")))
+        ; 3) input for (pln-rule-contextualize-subset)
+        ; "Dogs are animals"
+        (SubsetLink
+            (ConceptNode "dogs")
+            (ConceptNode "animals"))))
+
+(define check-output
+    (BindLink
+        (ImplicationLink
+            (AndLink
+                ; 1) output
+                (ContextLink
+                    (ConceptNode "doing_mathematics")
+                    (InheritanceLink
+                        (ConceptNode "Ben")
+                        (ConceptNode "competent")))
+                ; 2) output 
+                (ContextLink
+                    (ConceptNode "earth")
+                    (EvaluationLink (stv .8 .7)
+                        (PredicateNode "isBlue")
+                        (ListLink
+                            (ConceptNode "sky"))))
+                (ContextLink 
+                    (ConceptNode "moon")
+                    (EvaluationLink (stv .0 .2)
+                        (PredicateNode "isBlue")
+                        (ListLink
+                            (ConceptNode "sky"))))
+                ; 3) output
+                (ContextLink
+                    (ConceptNode "dogs")
+                    (ConceptNode "animals")))
+            (ConceptNode "true"))))
