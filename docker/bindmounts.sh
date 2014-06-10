d716c80526c26b4c631e30271750d27b496c3b2a
David Hart <dhart@opencog.org>
2014-06-10 12:09:46 +0800

    preicse buildslave tweaks

--- docker/bindmounts.sh
+++ docker/bindmounts.sh
@@ -1,6 +1,8 @@
 mkdir -pv ~/opencog/docker/opencog
 mkdir -pv ~/opencog/docker/buildslave/opencog
 mkdir -pv ~/opencog/docker/embodiment/opencog
+mkdir -pv ~/opencog/docker/precise/buildslave/opencog
 sudo mount -v --bind ~/opencog ~/opencog/docker/opencog
 sudo mount -v --bind ~/opencog ~/opencog/docker/buildslave/opencog
 sudo mount -v --bind ~/opencog ~/opencog/docker/embodiment/opencog
+sudo mount -v --bind ~/opencog ~/opencog/docker/precise/buildslave/opencog
