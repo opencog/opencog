2a56e3ae253605e592968f7af07e6b801be6410f
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-06-12 13:05:13 +0200

    Adapted imports/declaration to name-change

--- opencog/python/pln/examples/relex2logic/socrates_example.py
+++ opencog/python/pln/examples/relex2logic/socrates_example.py
@@ -3,7 +3,7 @@ For running evaluation_to_member_agent.py without the cogserver
 """
 
 from __future__ import print_function
-from pln.examples.relex2logic import evaluation_to_member_agent
+from pln.examples.relex2logic import socrates_agent
 from opencog.atomspace import types, AtomSpace, TruthValue
 from opencog.scheme_wrapper import load_scm, scheme_eval, scheme_eval_h, __init__
 from pln.examples.interactive_agent import InteractiveAgent
@@ -15,14 +15,13 @@ __init__(atomspace)
 
 coreTypes = "opencog/atomspace/core_types.scm"
 utilities = "opencog/scm/utilities.scm"
-#data = "opencog/python/pln/examples/relex2logic/evaluation-to-member.scm"
-data = "opencog/python/pln/examples/relex2logic/r2l-output-test.scm"
+data = "opencog/python/pln/examples/relex2logic/socrates-r2l.scm"
 
 for item in [coreTypes, utilities, data]:
     load_scm(atomspace, item)
 
 agent = InteractiveAgent(atomspace=atomspace,
-                         agent=evaluation_to_member_agent.EvaluationToMemberAgent(),
+                         agent=socrates_agent.SocratesAgent(),
                          num_steps=1000,
                          print_starting_contents=True)
 agent.run()
