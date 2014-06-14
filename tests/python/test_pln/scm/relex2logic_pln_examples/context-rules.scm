854b12fbe9d92bbd1e6eb7c883acf7dda5c9a0cf
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-06-14 15:39:49 +0200

    Added truth values to context examples
    So examples are going to be recognized by PLN

--- tests/python/test_pln/scm/relex2logic_pln_examples/context-rules.scm
+++ tests/python/test_pln/scm/relex2logic_pln_examples/context-rules.scm
@@ -8,7 +8,7 @@
     (ListLink
         ; 1) input for (pln-rule-contextualize-inheritance)
         ; "Ben is competent in doing mathematics."
-        (InheritanceLink
+        (InheritanceLink (stv .8 .8)
             (AndLink
                 (ConceptNode "Ben")
                 (ConceptNode "doing_mathematics"))
@@ -32,19 +32,19 @@
                     (ConceptNode "moon"))))
         ; 3) input for (pln-rule-contextualize-subset)
         ; "Dogs are animals."
-        (SubsetLink
+        (SubsetLink (stv .8 .8)
             (ConceptNode "dogs")
             (ConceptNode "animals"))
         ; 4) input for (pln-rule-decontextualize-inheritance)
         ; "Ben is competent in the domain of mathematics.
-        (ContextLink
+        (ContextLink (stv .8 .8)
             (ConceptNode "doing_mathematics")
             (InheritanceLink
                 (ConceptNode "Ben")
                 (ConceptNode "competent")))
         ; 5) input for (pln-rule-decontextualize-evaluation)
         ; "In the context of the earth, the sky is blue."
-        (ContextLink
+        (ContextLink (stv .8 .8)
             (ConceptNode "earth")
             (EvaluationLink
                 (PredicateNode "isBlue")
@@ -52,7 +52,7 @@
                     (ConceptNode "sky"))))
         ; 6) input for (pln-rule-decontextualize-subset)
         ; "Dogs are animals."
-        (ContextLink
+        (ContextLink (stv .8 .8)
             (ConceptNode "dogs")
             (ConceptNode "animals"))))
 
@@ -61,32 +61,32 @@
         (ImplicationLink
             (AndLink
                 ; 1) output
-                (ContextLink
+                (ContextLink (stv .8 .8)
                     (ConceptNode "doing_mathematics")
                     (InheritanceLink
                         (ConceptNode "Ben")
                         (ConceptNode "competent")))
                 ; 2) output 
-                (ContextLink
+                (ContextLink (stv .8 .8)
                     (ConceptNode "earth")
                     (EvaluationLink (stv .8 .7)
                         (PredicateNode "isBlue")
                         (ListLink
                             (ConceptNode "sky"))))
-                (ContextLink 
+                (ContextLink (stv .8 .8)
                     (ConceptNode "moon")
                     (EvaluationLink (stv .0 .2)
                         (PredicateNode "isBlue")
                         (ListLink
                             (ConceptNode "sky"))))
                 ; 3) output
-                (ContextLink
+                (ContextLink (stv .8 .8)
                     (ConceptNode "dogs")
                     (ConceptNode "animals"))
                 ; output of decontextualize-rules is
                 ; input 1-3
                 ; 4) output                
-                (InheritanceLink
+                (InheritanceLink (stv .8 .8)
                     (AndLink
                         (ConceptNode "Ben")
                         (ConceptNode "doing_mathematics"))
@@ -94,14 +94,14 @@
                         (ConceptNode "competent")
                         (ConceptNode "doing_mathematics")))
                 ; 5) output
-                (EvaluationLink
+                (EvaluationLink (stv .8 .8)
                     (PredicateNode "isBlue")
                     (ListLink
                         (AndLink
                             (ConceptNode "sky")
                             (ConceptNode "earth"))))
                 ; 6) output
-                (SubsetLink
+                (SubsetLink (stv .8 .8)
                     (ConceptNode "dogs")
                     (ConceptNode "animals")))
             (ConceptNode "true"))))
