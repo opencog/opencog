8d6300bcede481cfa35ccf99274239fae744d698
rodsol <rodisolomon@gmail.com>
2014-06-17 15:34:43 +0300

    add variable

--- opencog/nlp/scm/relex-to-logic-post-processing.scm
+++ opencog/nlp/scm/relex-to-logic-post-processing.scm
@@ -2,6 +2,7 @@
 ;Variables used for post processing process of Relex2Logic 
 (define anticident-inst (TypedVariableLink (VariableNode "$anticident-inst") (VariableTypeNode "ConceptNode")))
 (define concept-sub-1 (TypedVariableLink (VariableNode "$concept-sub-1") (VariableTypeNode "ConceptNode")))
+(define concept-sub-2 (TypedVariableLink (VariableNode "$concept-sub-2") (VariableTypeNode "ConceptNode")))
 (define concept-main-1 (TypedVariableLink (VariableNode "$concept-main-1") (VariableTypeNode "ConceptNode")))
 (define sub-root-verb (TypedVariableLink (VariableNode "$sub-root-verb") (VariableTypeNode "PredicateNode")))
 (define main-root-verb (TypedVariableLink (VariableNode "$main-root-verb") (VariableTypeNode "PredicateNode")))
