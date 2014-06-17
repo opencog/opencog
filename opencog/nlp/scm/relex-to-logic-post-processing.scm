25bf6a8b3d18ba6a7f979d590a1b429f747e199f
rodsol <rodisolomon@gmail.com>
2014-06-17 14:29:37 +0300

    adjective clause SVO&SVP rule

--- opencog/nlp/scm/relex-to-logic-post-processing.scm
+++ opencog/nlp/scm/relex-to-logic-post-processing.scm
@@ -16,3 +16,33 @@
                 #f))
         lst) temp-node))
 ;-------------------------------------------------------------------------------
+;For sentences with SVO and SVP rules
+;Example: "Restaurants which serve frogs are famous."
+;RelEx2Logic representaion:
+;(InheritanceLink
+;   (SatisfyingSetLink (VariableNode "$X")
+;        (AndLink
+;           (InheritanceLink(VariableNode "$X")(ConceptNode "Restaurant"))
+;           (EvaluationLink
+;               (PredicateNode "serves@12")
+;               (ListLink
+;                   (VariableNode "$X")
+;                   (ConceptNode "food@11")))))
+;   (SatisfyingSetLink (VariableNode "$X")
+;        (InheritanceLink(VariableNode "$X")(ConceptNode "Famous@45"))
+;$anticident-inst=Restaurant,$concept-main-1=famous,$sub-root-verb=serve and $concept-sub-1=frogs
+(define Adjective-clause-SVO&SVP
+    (BindLink
+        (ListLink anticident-inst sub-root-verb  concept-sub-1 concept-main-1)
+        (ImplicationLink
+            (AndLink
+                (InheritanceLink (VariableNode "$anticident-inst")(VariableNode "$concept-main-1"))
+                (EvaluationLink (VariableNode "$sub-root-verb")(ListLink(VariableNode "$anticident-inst")(VariableNode "$concept-sub-1")))
+                (EvaluationLink (PredicateNode "whichmarker") (ListLink (VariableNode "$anticident-inst")(VariableNode "$sub-root-verb"))))
+            (InheritanceLink
+                (SatisfyingSetLink (VariableNode "$X")
+                    (AndLink
+                        (InheritanceLink (VariableNode "$X") (VariableNode "$anticident-inst"))
+                        (EvaluationLink (VariableNode "$sub-root-verb") (ListLink (VariableNode "$X") (VariableNode "$concept-sub-1")))))
+                (SatisfyingSetLink (VariableNode "$X")
+                    (InheritanceLink (VariableNode "$X") (VariableNode "$concept-main-1")))))))
