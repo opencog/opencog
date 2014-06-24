01ade624abe3353d104e3080cc2a739cd9ca76e7
Misgana Bayetta <misgana.bayetta@gmail.com>
2014-06-24 15:33:51 +0800

    added copyright

--- opencog/query/AttentionalFocusCB.cc
+++ opencog/query/AttentionalFocusCB.cc
@@ -1,3 +1,25 @@
+/*
+ * AttentionalFocusCB.cc
+ *
+ * Copyright (C) 2014 Misgana bayetta
+ *
+ * Author: Misgana Bayetta <misgana.bayetta@gmail.com>  July 2014
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 #include "AttentionalFocusCB.h"
 using namespace opencog;
 
@@ -37,7 +59,7 @@ IncomingSet AttentionalFocusCB::get_incoming_set(Handle h) {
 	// if none is in AF
 	if (filtered_set.empty()) {
 		//xxx what shall we do here?, return the default or return empty ?
-		filtered_set=incoming_set;
+		filtered_set = incoming_set;
 	}
 	std::sort(filtered_set.begin(), filtered_set.end(), compare_sti); //sort by STI for better performance
 
