bfd2e293f5af110a629119f7054bbe69e3fb4319
Misgana Bayetta <misgana.bayetta@gmail.com>
2014-06-24 20:12:38 +0800

    fix

--- opencog/query/AttentionalFocusCB.h
+++ opencog/query/AttentionalFocusCB.h
@@ -27,16 +27,21 @@
 #include "DefaultPatternMatchCB.h"
 
 namespace opencog {
-class AttentionalFocusCB: public  virtual PatternMatchCallback {
+
+class AttentionalFocusCB: public virtual DefaultPatternMatchCB {
+
 private:
 	AtomSpace * _atom_space;
-	static bool compare_sti(LinkPtr lptr1,LinkPtr lptr2){
-		return lptr1->getAttentionValue()->getSTI() > lptr2->getAttentionValue()->getSTI();
+	static bool compare_sti(LinkPtr lptr1, LinkPtr lptr2) {
+		return lptr1->getAttentionValue()->getSTI()
+				> lptr2->getAttentionValue()->getSTI();
 	}
 public:
-	AttentionalFocusCB(AtomSpace * as) : _atom_space(as){}
+	AttentionalFocusCB(AtomSpace * as) :
+			DefaultPatternMatchCB(as), _atom_space(as) {
+	}
 	bool node_match(Handle& node1, Handle& node2);
-	bool link_match(LinkPtr& link1, LinkPtr& link2);
+	bool link_match(LinkPtr& lpat, LinkPtr& lsoln);
 	IncomingSet get_incoming_set(Handle h);
 };
 } //namespace opencog
