9122bf2ebac8ce566a4de09cdc997cb527d4dfc3
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-07-16 19:34:52 +0200

    Added premises for syllogism, demo, notes

--- opencog/python/pln/examples/relex2logic/syllogisms/syllogism-canadian.scm
+++ opencog/python/pln/examples/relex2logic/syllogisms/syllogism-canadian.scm
@@ -1,36 +1,90 @@
+; All Canadians are right-handed.
+; All right-handed are opticians.
+; |- Some right-handed are opticians.
 
+(EvaluationLink (PredicateNode "inputs")
+    (ListLink
+        ; All Canadians are right-handed.
+        (ForAllLink (stv 1.000000 0.000000) ; tv needs to be set properly
+            (VariableNode "$X") ; [2040]
+            (ImplicationLink (stv 0.990000 0.990000)
+                (InheritanceLink (stv 0.990000 0.990000)
+                    (VariableNode "$X") ; [2040]
+                    (ConceptNode "Canadians@c5669c61-1069-478c-abb9-aaa69c4f2217") ; [2041]
+                ) ; [2042]
+                (InheritanceLink (stv 0.990000 0.990000)
+                    (VariableNode "$X") ; [2040]
+                    (ConceptNode "right-handed@54ed2540-87ba-4c5f-9cce-02053ea91857") ; [2043]
+                ) ; [2044]
+            ) ; [2045]
+        ) ; [2046]
+        (InheritanceLink (stv 0.990000 0.990000)
+            (ConceptNode "right-handed@54ed2540-87ba-4c5f-9cce-02053ea91857") ; [2043]
+            (ConceptNode "right-handed") ; [2047]
+        ) ; [2048]
+        (InheritanceLink (stv 0.990000 0.990000)
+            (ConceptNode "Canadians@c5669c61-1069-478c-abb9-aaa69c4f2217") ; [2041]
+            (ConceptNode "Canadian") ; [2049]
+        ) ; [2050]
+        ; All right-handed people are opticians.
+        ; (needs another r2l all-rule to generate appropriate representation.)
+        ; (exact representation is still up for discussion)
+        (ForAllLink (stv 1.000000 0.000000) ; tv needs to be set properly
+            (VariableNode "$X") ; [2040]
+            (ImplicationLink (stv 0.990000 0.990000)
+                (InheritanceLink (stv 0.990000 0.990000)
+                    (VariableNode "$X") ; [2040]
+                    (AndLink
+                        (ConceptNode "people@AAA"); [2041]
+                        (ConceptNode "right-handed@AAA")
+                    )
+                ) ; [2042]
+                (InheritanceLink (stv 0.990000 0.990000)
+                    (VariableNode "$X") ; [2040]
+                    (ConceptNode "opticians@AAA") ; [2043]
+                ) ; [2044]
+            ) ; [2045]
+        ) ; [2046]
+        (InheritanceLink (stv 0.990000 0.990000)
+            (ConceptNode "people@AAA")
+            (ConceptNode "people")
+        )
+        (InheritanceLink (stv 0.990000 0.990000)
+            (ConceptNode "right-handed@AAA")
+            (ConceptNode "right-handed")
+        )
+        (InheritanceLink (stv 0.990000 0.990000)
+            (ConceptNode "opticians@AAA")
+            (ConceptNode "optician")
+        )
+        ; Besides that, the system also needs to know that
+        ; Canadians are people.
+        (InheritanceLink (stv 0.990000 0.990000)
+            (ConceptNode "Canadian")
+            (ConceptNode "people")
+        )
 
-All Canadians are right-handed
+    )
+)
 
-(ForAllLink (stv 0.990000 0.990000)
-  (ConceptNode "Canadians@53ae4de2-ff7b-4f46-b47b-9dcc04b1f24c") ; [2270]
-  (InheritanceLink (stv 0.990000 0.990000)
-    (ConceptNode "Canadians@53ae4de2-ff7b-4f46-b47b-9dcc04b1f24c") ; [2270]
-    (ConceptNode "Canadian") ; [2271]
-  ) ; [2272]
-) ; [2274]
+(EvaluationLink (PredicateNode "rules")
+	(ListLink
+	    ; which rules need to be used here? inference path to be specified in
+	    ; README
+		(ConceptNode "")
+	)
+)
 
-(EvaluationLink (stv 0.990000 0.990000)
-  (PredicateNode "definite") ; [2275]
-  (ListLink (stv 0.990000 0.990000)
-    (ConceptNode "Canadians@53ae4de2-ff7b-4f46-b47b-9dcc04b1f24c") ; [2270]
-  ) ; [2276]
-) ; [2277]
+(EvaluationLink (PredicateNode "forwardSteps")
+	(ListLink
+	    ; set this in test_syllogisms.py
+		(NumberNode "")
+	)
+)
 
-(InheritanceLink (stv 0.990000 0.990000)
-  (ConceptNode "right-handed@9c5f2bd1-caea-4fde-b2fc-bc8650239cbd") ; [2267]
-  (ConceptNode "right-handed") ; [2268]
-) ; [2269]
-
-(InheritanceLink (stv 0.990000 0.990000)
-  (ConceptNode "Canadians@53ae4de2-ff7b-4f46-b47b-9dcc04b1f24c") ; [2270]
-  (ConceptNode "Canadian") ; [2271]
-) ; [2272]
-
-(InheritanceLink (stv 0.990000 0.990000)
-  (ConceptNode "Canadians@53ae4de2-ff7b-4f46-b47b-9dcc04b1f24c") ; [2270]
-  (ConceptNode "right-handed@9c5f2bd1-caea-4fde-b2fc-bc8650239cbd") ; [2267]
-) ; [2273]
-
-All right-handed are opticians
-Conclusion: Some opticians are Canadian
+(EvaluationLink (PredicateNode "outputs")
+	(ListLink
+	    ; Conclusion: Some opticians are Canadian
+	    ; needs proper handling of quantities!
+	)
+)
--- opencog/python/pln/examples/relex2logic/syllogisms/test_syllogisms.py
+++ opencog/python/pln/examples/relex2logic/syllogisms/test_syllogisms.py
@@ -2,3 +2,57 @@ __author__ = 'sebastian'
 
 # Socrates example is a categorical syllogism --> move in this directory?
 
+"""
+For testing syllogisms
+"""
+
+from __future__ import print_function
+from opencog.atomspace import types, AtomSpace, TruthValue
+from opencog.scheme_wrapper import load_scm, scheme_eval, scheme_eval_h, __init__
+from pln.examples.interactive_agent import InteractiveAgent
+from opencog.cogserver import MindAgent
+from opencog.atomspace import types
+from pln.chainers import Chainer
+from pln.rules import *
+
+__author__ = 'Sebastian Ruder'
+
+
+class SyllogismAgent(MindAgent):
+    def __init__(self):
+        self.chainer = None
+
+    def create_chainer(self, atomspace):
+        self.chainer = Chainer(atomspace,
+                               stimulateAtoms=False,
+                               allow_output_with_variables=True,
+                               delete_temporary_variables=True)
+
+    def run(self, atomspace):
+        if self.chainer is None:
+            self.create_chainer(atomspace)
+            return
+
+        result = self.chainer.forward_step()
+        return result
+
+
+atomspace = AtomSpace()
+__init__(atomspace)
+
+coreTypes = "opencog/atomspace/core_types.scm"
+utilities = "opencog/scm/utilities.scm"
+data = "opencog/python/pln/examples/relex2logic/socrates-r2l.scm"
+
+for item in [coreTypes, utilities, data]:
+    load_scm(atomspace, item)
+
+agent = InteractiveAgent(atomspace=atomspace,
+                         agent=SyllogismAgent(),
+                         num_steps=100,
+                         print_starting_contents=True)
+agent.run()
+
+# iterate over input file, only load inputs
+# load outputs in separate atomspace and compare them against each other
+# only load rules that are specified
