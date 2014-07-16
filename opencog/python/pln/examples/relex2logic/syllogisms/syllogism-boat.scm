db933c1c6de5d8f12a31d7b5d355dbd3f8fed200
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-07-16 19:52:35 +0200

    Added comments to boat negative syllogism

--- opencog/python/pln/examples/relex2logic/syllogisms/syllogism-boat.scm
+++ opencog/python/pln/examples/relex2logic/syllogisms/syllogism-boat.scm
@@ -1,13 +1,17 @@
+; A car is not a boat.
+; Some boats are water bicycles
+; |- ?
+; A. No boat is a water bicycle
+; B. Some water bicycles are no cars
+; C. No boat is a car
+; D. Some cars ware no water bicycles
 
+; Note: Original input sentence was "A car is no boat."
+; Evokes relation _quantity(boat, no).
+; This prompts the question: Should this be handled with a NotLink
+; or use a ForAllLink which itself contains a NotLink?
 
-
-1. A car is no boat.
-
-Negation is not properly propagated yet. NotLink only wraps the PredicateNode
-and is not present in other links.
-
-2. Some boats are water bicycles
-A. No boat is a water bicycle
-B. Some water bicycles are no cars
-C. No boat is a car
-D. Some cars ware no water bicycles
+(EvaluationLink (PredicateNode "inputs")
+    (ListLink
+    )
+)
