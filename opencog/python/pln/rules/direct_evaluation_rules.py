16bbd550be5930b3361aecb07ba2905386ae717f
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-07-24 17:30:15 +0200

    Deleted alternatives as suggested by @AmeBel

--- opencog/python/pln/rules/direct_evaluation_rules.py
+++ opencog/python/pln/rules/direct_evaluation_rules.py
@@ -344,11 +344,8 @@ class GeneralEvaluationToMemberRule(Rule):
         # ListLink
         # Key = the atom under consideration
         # Values = the index of the atom in the ListLink
-
-        # Are these the same?
-        # arg_indexs = dict(((j, [p for p, q in enumerate(arg.out) if q == j]) for i, j in enumerate(arg.out)))
-        # arg_indexs = dict(((j, [p]) for p, j in enumerate(arg.out)))
-        arg_indexs = {j: [p] for p, j in enumerate(arg.out)}
+        arg_indexs = dict(((j, [p for p, q in enumerate(arg.out) if q == j])
+                           for i, j in enumerate(arg.out)))
 
         if arg.type == types.ListLink:
             for i in arg.out:
