6f0b218e89e8d0ffdd13d02aabc9a4b987c180fa
Linas Vepstas <linasvepstas@gmail.com>
2014-07-28 16:45:29 -0500

    more entropy cases

--- opencog/nlp/learn/learn-lang-diary/link-type/gifty.scm
+++ opencog/nlp/learn/learn-lang-diary/link-type/gifty.scm
@@ -17,7 +17,7 @@
 (define gifty-rank 5)
 (define gift-rank 6)
 
-; given a rank, return a probability. This is hacky
+; Given a rank, return a probability. This is hacky
 (define (zipf rank)
 	(define (powie x)  (expt x -1.0))
 	(define norm (reduce + 0 (map powie '(1 2 3 4 5 6))))
@@ -31,6 +31,17 @@
 (define gifty-prob (zipf 5))
 (define gift-prob (zipf 6))
 
+; diary distribution
+(set! tester-prob (/ 2 7))
+(set! testy-prob (/ 1 7))
+(set! test-prob (/ 1 7))
+(set! gifter-prob (/ 1 7))
+(set! gifty-prob (/ 1 7))
+(set! gift-prob (/ 1 7))
+
+
+
+; debug print
 (display "Probability tester-prob ") (display tester-prob) (newline)
 (display "Probability testy-prob ") (display testy-prob) (newline)
 (display "Probability test-prob ") (display test-prob) (newline)
@@ -38,5 +49,31 @@
 (display "Probability gifter-prob ") (display gifter-prob) (newline)
 (display "Probability gifty-prob ") (display gifty-prob) (newline)
 (display "Probability gift-prob ") (display gift-prob) (newline)
+(newline)
 
 
+; convert to notation used in diary
+(define ta-prob test-prob)
+(define tb-prob testy-prob)
+(define tc-prob tester-prob)
+
+(define ga-prob gift-prob)
+(define gb-prob gifty-prob)
+(define gc-prob gifter-prob)
+
+; Compute entropy of a list of probability values
+(define (h lst) 
+   (define (lg x) (/ (log x) (log 2.0)))
+	(define (plogp x)  (* x (lg x)))
+	(- 0 (reduce + 0.0 (map plogp lst)))
+)
+
+; Initial case.
+(define h-init-obs
+	(h (list ta-prob tb-prob tc-prob ga-prob gb-prob gc-prob)))
+
+(display "h initial observed: ") (display h-init-obs) (newline)
+
+; Case 1.
+(define gg-prob (+ gb-prob gc-prob))
+(define tt-prob (+ tb-prob tc-prob))
