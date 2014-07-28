187ff46b99369397738a80054d5cd95c8df1961a
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-07-28 13:16:05 +0200

    Reverted some changes that appeared in merge conflicts
    Removed check from agent as it is already performed in demo;
    check is still available in attention_allocation_agent

--- opencog/python/pln/examples/socrates_demo/socrates_agent.py
+++ opencog/python/pln/examples/socrates_demo/socrates_agent.py
@@ -12,8 +12,6 @@ __author__ = 'Cosmo Harrigan'
 
 class SocratesAgent(MindAgent):
 
-    steps = 0
-
     def __init__(self):
         self.chainer = None
 
@@ -38,46 +36,7 @@ class SocratesAgent(MindAgent):
     def run(self, atomspace):
         if self.chainer is None:
             self.create_chainer(atomspace)
-            print "PLN Chainer created."
             return
 
-        #print "PLN continuing."
-        print("Step #{0}".format(self.steps))
-
-        # there is no query for this example
-
-        if not check_result(atomspace):
-            result = self.chainer.forward_step()
-            self.steps += 1
-            # no query to stimulate
-
-            return result
-
-        # agent needs to be stopped when result is reached
-
-
-def check_result(atomspace):
-    """
-    Searches for an instance of
-    EvaluationLink
-        PredicateNode "breathe"
-        ListLink
-            ConceptNode "Socrates"
-            ConceptNode "air"
-    """
-    result_found = False
-    eval_links = atomspace.get_atoms_by_type(types.EvaluationLink)
-
-    for eval_link in eval_links:
-        out = atomspace.get_outgoing(eval_link.h)
-        if out[0].is_a(types.PredicateNode) and "breathe" in out[0].name\
-            and out[1].is_a(types.ListLink)\
-            and "Socrates" in out[1].out[0].name\
-                and "air" in out[1].out[1].name:
-            result_found = True
-            break
-
-    if result_found:
-        print("Result found? {0}.".format(result_found))
-
-    return result_found
+        result = self.chainer.forward_step()
+        return result
--- opencog/python/pln/examples/socrates_demo/socrates_example.py
+++ opencog/python/pln/examples/socrates_demo/socrates_example.py
@@ -3,7 +3,7 @@ For running socrates_agent.py without the cogserver
 """
 
 from __future__ import print_function
-from pln.examples.relex2logic import socrates_agent
+from pln.examples.socrates_demo import socrates_agent
 from opencog.atomspace import types, AtomSpace, TruthValue
 from opencog.scheme_wrapper import load_scm, scheme_eval, scheme_eval_h, __init__
 from pln.examples.interactive_agent import InteractiveAgent
