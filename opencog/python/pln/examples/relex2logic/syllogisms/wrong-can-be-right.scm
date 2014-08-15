1808d80eab16b8511ce1628c115dc962d66d3d8b
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-08-15 20:01:38 +0200

    Cleaned up formatting, put txt syllogisms in one file

--- opencog/python/pln/examples/relex2logic/syllogisms/wrong-can-be-right.scm
+++
@@ -1,8 +0,0 @@
-Within logical reasoning it can sometimes happen that the premises and conclusion seem obviously wrong, but are logically speaking correct when applying one of the logical reasoning types mentioned above. Be aware that conclusions are drawn based on logical reasoning and not on the validity of the context of certain premises or conclusions. Example:
-
-Major premise:  Eating a lot makes you lose weight
-Minor premise:  Craig is obese
-Question:          What can we do to make Craig lose weight?
-Conclusion:       Make Craig eat a lot
-
-By just observing the context of the words you would think that this conclusion is incorrect, since you know form everyday life that eating a lot does not make you lose weight at all. On the contrary it makes you gain weight. However based on logical reasoning this conclusion is most certainly correct, since both premises are valid, which automatically makes the conclusion a valid conclusion. What you need to understand is that the correct answer to any given logical reasoning argument requires the proper identification of relationships between assertions (typically facts and opinions), not the accuracy of those assertions.
