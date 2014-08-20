8680535ec6fe29efb84950716b82c2f2144c0bd2
Nil Geisweiller <ngeiswei@gmail.com>
2014-08-20 14:01:50 +0300

    Minor whitespace fixes

--- opencog/embodiment/Control/OperationalAvatarController/OACExecutable.cc
+++ opencog/embodiment/Control/OperationalAvatarController/OACExecutable.cc
@@ -120,15 +120,13 @@ int main(int argc, char *argv[])
         oac.serverLoop();
 
     } catch (std::bad_alloc) {
-        logger().error(
-                     "OACExecutable - OAC raised a bad_alloc exception.");
+        logger().error("OACExecutable - OAC raised a bad_alloc exception.");
         static_cast<OAC&>(server()).saveState();
     } catch (StandardException se) {
-        logger().error(
-                     "OACExecutable - An exceptional situation occured"
-                     " with the following message '%s'"
-                     ". Check log for more information.",
-                     se.getMessage());
+        logger().error("OACExecutable - An exceptional situation occured"
+                       " with the following message '%s'"
+                       ". Check log for more information.",
+                       se.getMessage());
         static_cast<OAC&>(server()).saveState();
     } /*catch (...) {
         logger().error(
