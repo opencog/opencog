6afce87ecb992ea8b27a03315241d774f55649fe
Linas Vepstas <linasvepstas@gmail.com>
2014-08-25 18:13:11 -0500

    pattern match: more code movement.

--- opencog/query/Implicator.cc
+++ opencog/query/Implicator.cc
@@ -172,4 +172,63 @@ Handle PatternMatch::pln_bindlink(Handle himplication)
 	return gl;
 }
 
+/* ================================================================= */
+/**
+ * DEPRECATED: USE BIND_LINK INSTEAD!
+ * Right now, this method is used only in the unit test cases;
+ * and it should stay that way.
+ *
+ * Default evaluator of implication statements.  Does not consider
+ * the truth value of any of the matched clauses; instead, looks
+ * purely for a structural match.
+ *
+ * See the do_imply function for details.
+ */
+Handle PatternMatch::imply (Handle himplication)
+{
+	// Now perform the search.
+	DefaultImplicator impl(_atom_space);
+	std::set<Handle> varset;
+
+	do_imply(himplication, impl, varset);
+
+	// The result_list contains a list of the grounded expressions.
+	// Turn it into a true list, and return it.
+	Handle gl = _atom_space->addLink(LIST_LINK, impl.result_list);
+	return gl;
+}
+
+/**
+ * DEPRECATED: USE CRISP_LOGIC_BINDLINK INSTEAD!
+ * At this time, this method is used only by the unit test cases.
+ * It should stay that way, too; no one else should use this.
+ *
+ * Use the crisp-logic callback to evaluate boolean implication
+ * statements; i.e. statements that have truth values assigned
+ * their clauses, and statements that start with NotLink's.
+ * These are evaluated using "crisp" logic: if a matched clause
+ * is true, its accepted, if its false, its rejected. If the
+ * clause begins with a NotLink, true and false are reversed.
+ *
+ * The NotLink is also interpreted as an "absence of a clause";
+ * if the atomspace does NOT contain a NotLink clause, then the
+ * match is considered postive, and the clause is accepted (and
+ * it has a null or "invalid" grounding).
+ *
+ * See the do_imply function for details.
+ */
+Handle PatternMatch::crisp_logic_imply (Handle himplication)
+{
+	// Now perform the search.
+	CrispImplicator impl(_atom_space);
+	std::set<Handle> varset;
+
+	do_imply(himplication, impl, varset);
+
+	// The result_list contains a list of the grounded expressions.
+	// Turn it into a true list, and return it.
+	Handle gl = _atom_space->addLink(LIST_LINK, impl.result_list);
+	return gl;
+}
+
 /* ===================== END OF FILE ===================== */
