b676461ae48e0857e848c978d628645a115cc0e5
Linas Vepstas <linasvepstas@gmail.com>
2014-09-07 13:56:15 -0500

    Remove a dead file

--- tests/scm/multispace.scm
+++
@@ -1,8 +0,0 @@
-
-; Define an alternate atomspace
-; (define alt-as (cog-new-atomspace))
-
-; execute the thunk in the alternate atomspace.
-;(define (alt-env func)
-;	(begin (cog-set-atomspace! alt-as) (f))
-;)
