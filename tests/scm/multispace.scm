77dbdff4a40a3dca786aa1be7df7d9a81abd1393
Linas Vepstas <linasvepstas@gmail.com>
2014-09-07 13:06:48 -0500

    multiple atomspace unit test: test new API

--- tests/scm/multispace.scm
+++ tests/scm/multispace.scm
@@ -1,25 +1,8 @@
 
-(define (dbg)
+; Define an alternate atomspace
+; (define alt-as (cog-new-atomspace))
 
-	(define olde *-atomspace-*)
-	(let ((*-atomspace-* (cog-new-atomspace)))
-		(list olde *-atomspace-*)
-	)
-)
-
-(define (func-env)
-
-	; This is sort-of cheesy, but it does work ... 
-	; Create a new atomspace, set the current environment atomspace
-	; pointer to it, create an atom, then set it back to the old
-	; value.  
-	(let* ((olde *-atomspace-*)
-			(newe (cog-new-atomspace))
-			(h2 #f)
-			)
-		(set! *-atomspace-* newe)
-		(set! h2 (ConceptNode "stuff"))
-		(set! *-atomspace-* olde)
-		h2
-	)
-)
+; execute the thunk in the alternate atomspace.
+;(define (alt-env func)
+;	(begin (cog-set-atomspace! alt-as) (f))
+;)
