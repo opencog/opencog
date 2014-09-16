3e6c1088d8ead667e5ae59964947b4df1c333a68
Amen Belayneh <amenbelayneh@gmail.com>
2014-09-16 15:51:50 +0800

    SuReal which produces possible outputs

--- opencog/nlp/scm/surface-realization.scm
+++ opencog/nlp/scm/surface-realization.scm
@@ -149,18 +149,6 @@
 )
 
 ; ---------------------------------------------------------------------
-; Create BindLink for query when a certain atom is given.
-(define (bind-link an-atom)
-    (if (cog-link? an-atom)
-        (display "This is a link")
-        (if (equal? 'VariableNode (cog-type an-atom))
-            (display "BindLink is not created for VariableNode.")
-            (display "This is a node.")
-        )
-    )
-)
-
-; ---------------------------------------------------------------------
 ; A SetLink is the input because it is assumed that the output of the micro-planner
 ; is unordered.
 (define (sureal a-set-link)
@@ -175,14 +163,38 @@
     )
 )
 
+; Returns a possible set of SuReals from an output SetLink
+; * 'a-set-link' : A SetLink which is to be SuRealed
 (define (create-sentence a-set-link)
-    (display "creating")
+    (define clean-set (SetLink (remove is-abstraction? (cog-outgoing-set a-set-link))))
+    (define (replace-wins-with-str a-list)
+        (map (lambda (x)
+                (if (and (cog-atom? x) (equal? 'WordInstanceNode (cog-type x)))
+                    (word-inst-get-word-str x)
+                    x
+                ))
+            a-list
+        )
+    )
+
+    (let ((output-lst (par-map replace-wins-with-str (get-chunks clean-set))))
+        (map delete (make-list (length output-lst) "###LEFT-WALL###") output-lst)
+    )
 )
 
+; Returns a number that could be used to compare atoms of the same type. For eg.
+; if the nodes are instaces of the same concept such as (ConceptNode "fly@123")
+; (ConceptNode "fly@345") . Then using this funciton one can rank the atoms in terms
+; of 'truthfulness'.
+; * 'constant' : A parameter used to adjust the calcualtion of the heuristic value.
+;                The choice of value (where x = constant) will go like,
+;                       0<x<1 if higher confidence is preferred
+;                       x > 1 if higher strength is preferred
 (define (cog-stv-heuristic-value constant atom)
     (* (expt (cog-stv-strength atom) constant) (expt (cog-stv-confidence atom) (- 2 constant)))
 )
 
+; This just takes the one of the many InterpretationNode.
 (define (get-sentence interpret-node-lst)
     (define parse (list-ref (cog-chase-link 'InterpretationLink 'ParseNode (list-ref interpret-node-lst 0)) 0))
     (string-join (cdr (map word-inst-get-word-str (parse-get-words-in-order parse))))
@@ -190,7 +202,9 @@
 
 
 ; ---------------------------------------------------------------------
-; Some Utilities
+; Some Utilities : These are likely to be moved to a separate file.
+
+
 ; Returns a list of the r2l logic outputs associated with the InterpretationNode.
 (define (interp-get-logic-outputs interp-node)
     (cog-outgoing-set (list-ref (cog-chase-link 'ReferenceLink 'SetLink interp-node) 0))
@@ -203,5 +217,256 @@
     )
 )
 
+; Returns the InterpretationNode associated with the SetLink.
+(define (logic-get-interp a-set-link)
+    (car (cog-chase-link 'ReferenceLink 'InterpretationNode a-set-link))
+)
 
+; Returns the ParseNode associated with an InterpretationNode
+; * 'interp' : An InterpretationNode
+(define (interp-get-parse interp)
+    (car (cog-chase-link 'InterpretationLink 'ParseNode interp))
+)
+
+; ---------------------------------------------------------------------
+; The functions below are used for the pattern/knowledgebase based approach to
+; SuReal.
+
+; Return a match structure describing the results of the match on the node's name,
+; for the given pattern or #f if no match could be found.
+; * 'pattern' : It is the compiled regular expression of the pattern being matched.
+; * 'node' : The node with name  being matched.
+(define (cog-name-match pattern node)
+    (if (cog-node? node)
+        (regexp-exec pattern (cog-name node))
+        #f
+    )
+)
+
+; Return the name of a node without any UUID or indexes. It is assumed that '@'
+; is used as a delimiter.
+(define (cog-name-clean node)
+    (define pattern (make-regexp "@"))
+    (define has-match (regexp-exec pattern (cog-name node)))
+    (if has-match (match:prefix has-match) (cog-name node))
+)
+
+; It will return a list  of atoms that are similar. Similar links are atoms that are of the
+; same type , have the same subgraph and have part of the node name match for
+; at least one atom. This only work first-order links. Similar nodes are atoms
+; that have similar type and naming. A similar name is a name that matches before
+; UUID or other indexes or index markers. For e.g.,
+;   (WordInstanceNode "sentence@1ba8b794-93ba-47d9-93f4-a0db91dfc9d4")
+;   (WordInstanceNode "sentence")
+; * 'atom' : is the atom for which similar atoms are being searched for.
+; * 'lst' : is the list of atoms being checked for similarity.
+(define (similar-atoms atom lst)
+    (define (similar-node? node)
+        (and (equal? (cog-type node) (cog-type atom))
+            (cog-name-match (make-regexp (cog-name-clean atom) regexp/icase) node)
+        )
+    )
+    (define (similar-link? link)
+        (let ((link-out-set (cog-outgoing-set link))
+                (atom-out-set (cog-outgoing-set atom)))
+            (and (equal? (cog-type link) (cog-type atom))
+                (not (null?
+                    (append-map similar-atoms atom-out-set (make-list (length atom-out-set) link-out-set))))
+            )
+        )
+    )
+    (cond
+        ((cog-node? atom) (filter-hypergraph similar-node? lst))
+        ((cog-link? atom) (remove (negate similar-link?) lst))
+    )
+)
+
+
+; Returns #t if node1 and node2 are equivalent. The Nodes are equivalent if
+; they are of the same type and have similar names. A similar name is a name
+; that matches before UUID or other indexes or index markers.
+; * 'node1' : node with a name being used for matching.
+; * 'node2' ; node which is being checked if it has a name which matchs node1's name.
+(define (eqv-node? node1 node2)
+    (if
+        (and (equal? (cog-type node1) (cog-type node2))
+            (cog-name-match (make-regexp (cog-name-clean node1) regexp/icase) node2)
+        )
+        #t
+        #f
+    )
+)
+
+; Returns SetLinks associated with the dialogue system
+(define (get-dialogue-sets)
+    (delete '()
+        (par-map (lambda (x)
+                    (if (or (not (null? (cog-chase-link 'ReferenceLink 'InterpretationNode x)))
+                            (not (null? (cog-chase-link 'ReferenceLink 'SentenceNode x)))
+                        )
+                        x
+                        '()
+                    )
+                 ) (cog-get-atoms 'SetLink)
+         )
+     )
+)
+
+; Check if any of the atoms in the outgoing set of SetLinks, relevant for dialogue,
+; are similar to 'an-atom', and return a list containing those which do.
+; Should one want to define a different criteria for changes can be made here.
+(define (filter-dialogue-sets an-atom)
+    (delete '()
+        (par-map (lambda (x) (if (null? (similar-atoms an-atom (cog-outgoing-set x))) '() x)
+                 ) (get-dialogue-sets)
+        )
+    )
+)
+
+; Returns a alist in which the keys are the handles of the outgoing-set of
+; the SuRealizable SetLink and the values are a list of handles of the SetLinks
+; which have atoms similar to the keys.
+; * 'output-set' is the SetLink to be surealed.
+(define (index output-set)
+    (define out-set (remove is-abstraction? (cog-outgoing-set output-set)))
+    (define dict '())
+        (append-map
+            (lambda (x) (acons (cog-handle x) (par-map cog-handle (filter-dialogue-sets x)) dict))
+            out-set
+        )
+)
+
+; creates value to key alist of key-value pair, where the cdr of a-pair is a list.
+; It has a weired side-effect in that it both modifies 'a-dict' as well as
+; returning a new alist which is equal (as in #t for equal? and #f for eq?)
+(define (alist-inverse a-pair a-dict)
+    (delete-duplicates (append-map (lambda (x)
+            (if (null? x)
+                (if (assoc-ref a-dict "NO_PATTERN")
+                    (acons "NO_PATTERN" (delete-duplicates! (append (assoc-ref a-dict "NO_PATTERN") (list (car a-pair)))) a-dict)
+                    (acons "NO_PATTERN" (list (car a-pair)) a-dict)
+                )
+                (if (assoc-ref a-dict x)
+                    (acons x (delete-duplicates! (append! (assoc-ref a-dict x) (list (car a-pair)))) a-dict)
+                    (acons x (list (car a-pair)) a-dict)
+                )
+            )
+        )
+        (delete-duplicates (cdr a-pair))))
+)
+
+; Returns an alist of key-value pairs structured as handles of
+; (r2l-SetLink . (output-link1 output-link2 ...))
+; * 'old-dict' : It is an alist of key-value pairs structured as
+;                (output-link1 . (r2l-SetLink1 .... r2l-SetLinkn))
+; * 'new-dict' : It could be an empty list or an existing index to be updated.
+(define (reorder old-dict new-dict)
+    (if (null? (cdr old-dict))
+        new-dict
+        (reorder (cdr old-dict) (alist-inverse (car old-dict) new-dict))
+    )
+)
+; Checks if the outgoing set of an InheritanceLink or an ImplicationLink are similar
+; and returns #t if they are or #f if they are not. An abstraction is the linking of an
+; instance verison of an atom with it full abstracted version.
+(define (is-abstraction? link)
+    (let ((out-set (cog-outgoing-set link)))
+        (and
+            (null? (filter-map cog-link? out-set))
+            (equal? (cog-name-clean (car out-set)) (cog-name-clean (cadr out-set)))
+        )
+    )
+)
+
+; Returns a pair of atom handles structured as (r2l-node . output-node) provided
+; they are equivalent as in (eqv-node? arg1 arg2).
+; or an empty list other wise.
+; * 'pattern' : It is a link we want to surealize
+; * 'link' ; It is a link we are using for extracting relations between words.
+(define (get-mapping output-atom r2l-atom)
+    (if (equal? (cog-type output-atom) (cog-type r2l-atom))
+        (cond
+            ((cog-node? output-atom)
+                (if (eqv-node? output-atom r2l-atom)
+                    (cons (cog-handle r2l-atom) (cog-handle output-atom))
+                    'not-eqv-node
+                )
+            )
+            ((cog-link? output-atom)
+                (map
+                    (lambda (x y)
+                        (let ((mapping (get-mapping x y)))
+                            (if (equal? mapping 'not-eqv-node)
+                                (cons (cog-handle y) (cog-handle x))
+                                mapping
+                            )
+                        )
+                    )
+                (cog-outgoing-set output-atom) (cog-outgoing-set r2l-atom))
+            )
+        )
+        '()
+    )
+)
+
+; Return chunks of a sentence. It ranks sentences by swaping equivalent structures
+; depending on the name of the constituent Node names.
+; TODO : Figure out an appropirate ranking formula. The ranking should
+; be across all the outgoing-set of the output SetLink (I think???)
+(define (get-chunks output-set)
+    (define dict (reorder (index output-set) '()))
+    (define (get-sntc r2l-set)
+        (parse-get-words-in-order (interp-get-parse (logic-get-interp r2l-set)))
+    )
+
+    ; Takes a pair input structured as the handles of (r2l-SetLink . (output-link1 output-link2))
+    ; and returns a list of lists, containing pairs of (r2l-node . output-node)
+    ; handles. This is the function that should be edited to gain multiple possible
+    ; output per r2l-SetLink.
+    ; TODO: 1. have to remove tag atoms while mapping.
+    ;       2. When there is muliple mapping for a r2l-node merge them to a list so as multiple alternative
+    ;        sentences could be generated . OR , make a choice of the most likely pair.
+    (define (get-mapping-pair a-pair)
+
+        (let
+            ((r2l-set (remove is-abstraction? (cog-outgoing-set (cog-atom (car a-pair)))))
+            )
+            (apply append (par-map (lambda (x)
+                        (remove null?
+                        (append-map get-mapping (make-list (length r2l-set) (cog-atom x)) r2l-set))
+                    )
+                    (cdr a-pair))
+            )
+        )
+    )
+
+    ; Replaces the WordInstanceNodes with names matching the name of the car atom
+    ; with the clean name of the cdr of the pair
+    (define (replace-word r2l-sntc a-pair)
+        (let ((an-index (list-index (lambda (x)
+                    (if (cog-atom? x)
+                        (equal? (cog-name (cog-atom (car a-pair))) (cog-name x)) #f)) r2l-sntc))
+            )
+            (if (number? an-index)
+                (begin
+                    (list-set! r2l-sntc an-index (cog-name-clean (cog-atom (cdr a-pair))))
+                    #t
+                )
+                #f
+            )
+        )
+    )
+
+    ; Returns a random merge of possible sentences that can be formed from one r2l SetLink
+    ; TODO : Ranking of all possible alternative.
+    ; * 'a-pair' : the pair passed as input has to be a mapping sturctured as handles of
+    ;               (r2l-SetLink . (output-link1 output-link2))
+    (define (chunk a-pair)
+        (let ((mapping (get-mapping-pair a-pair)) (sntc (get-sntc (cog-atom (car a-pair)))))
+                (map-in-order (lambda (x) (replace-word sntc x)) mapping)
+             sntc
+        )
+    )
+    (map chunk dict)
+)
 
