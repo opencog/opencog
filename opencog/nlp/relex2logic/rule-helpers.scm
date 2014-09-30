f1dd8bc4ef7bd4176bc24c852730df8d49c5925f
Amen Belayneh <amenbelayneh@gmail.com>
2014-09-30 11:28:26 +0800

    change InheritanceLink to ImplicationLink for predicates

--- opencog/nlp/relex2logic/rule-helpers.scm
+++ opencog/nlp/relex2logic/rule-helpers.scm
@@ -417,7 +417,7 @@
 ;
 (define (predadj-ynQ-rule subj_concept subj_instance predicative_concept predicative_instance)
 	(list (InheritanceLink (ConceptNode subj_instance df-node-stv) (ConceptNode subj_concept df-node-stv) df-link-stv)
-	(InheritanceLink (PredicateNode predicative_instance df-node-stv) (ConceptNode predicative_concept df-node-stv) df-link-stv)
+	(ImplicationLink (PredicateNode predicative_instance df-node-stv) (PredicateNode predicative_concept df-node-stv) df-link-stv)
 	(EvaluationLink (PredicateNode "Truth Value")
 		(EvaluationLink df-link-stv (PredicateNode predicative_instance df-node-stv) 
 			(ListLink df-link-stv (ConceptNode subj_instance df-node-stv)))
@@ -433,7 +433,7 @@
 (define (prepobj-ynQ-rule subj_concept subj_instance predprep_concept predprep_instance obj_concept obj_instance)
 	(list (InheritanceLink (ConceptNode subj_instance df-node-stv) (ConceptNode subj_concept df-node-stv) df-link-stv)
 	(InheritanceLink (ConceptNode obj_instance df-node-stv) (ConceptNode obj_concept df-node-stv) df-link-stv)
-	(InheritanceLink (PredicateNode predprep_instance df-node-stv) (ConceptNode predprep_concept df-node-stv) df-link-stv)
+	(ImplicationLink (PredicateNode predprep_instance df-node-stv) (PredicateNode predprep_concept df-node-stv) df-link-stv)
 	(EvaluationLink (PredicateNode "Truth Value")
 		(EvaluationLink df-link-stv (PredicateNode predprep_instance df-node-stv)
 			(ListLink df-link-stv (ConceptNode subj_instance df-node-stv)(ConceptNode obj_instance df-node-stv)))
