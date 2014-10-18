659ae6910567acc071834092531621f9704ddd78
Linas Vepstas <linasvepstas@gmail.com>
2014-10-18 17:04:08 -0500

    Expand the uniordered test some more

--- tests/query/unordered-exhaust.scm
+++ tests/query/unordered-exhaust.scm
@@ -11,6 +11,19 @@
 	(ConceptNode "w")
 )
 
+(SetLink
+	(ConceptNode "p")
+	(ConceptNode "q")
+	(ConceptNode "r")
+	(SetLink
+		(ConceptNode "s")
+		(ConceptNode "t")
+		(ConceptNode "u")
+	)
+)
+
+;; This should match in 4! + 4! = 48 different ways, with 4! for
+;; the xyzw permuations, and another 4! for the pqr-set permuations.
 (define (exhaust)
    (BindLink
       ;; variable decls
@@ -39,5 +52,43 @@
 		)
 	)
 )
-				
+
+;; This should match in 3! * 3! = 36 different ways, viz a combinatorial
+;; explosion.  The embedded SetLink match is unique
+(define (exhaust-2)
+   (BindLink
+      ;; variable decls
+      (ListLink
+         (VariableNode "$a")
+         (VariableNode "$b")
+         (VariableNode "$c")
+         (VariableNode "$d")
+         (VariableNode "$e")
+         (VariableNode "$f")
+      )
+		(ImplicationLink
+			(AndLink
+				(SetLink ; sets are inherenetly unordered
+         		(VariableNode "$a")
+         		(VariableNode "$b")
+         		(VariableNode "$c")
+					(SetLink
+         			(VariableNode "$d")
+         			(VariableNode "$e")
+         			(VariableNode "$f")
+					)
+				)
+			)
+			; The result to report
+			(ListLink
+        		(VariableNode "$a")
+        		(VariableNode "$b")
+        		(VariableNode "$c")
+        		(VariableNode "$d")
+        		(VariableNode "$e")
+        		(VariableNode "$f")
+			)
+		)
+	)
+)
 
