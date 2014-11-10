a068488d879af05dd30c833ad16cbe44f642ebc0
Amen Belayneh <amenbelayneh@gmail.com>
2014-11-10 14:34:33 +0800

    specify the rules to be used by the agent

--- opencog/python/pln/examples/deception_demo/deception_agent.py
+++ opencog/python/pln/examples/deception_demo/deception_agent.py
@@ -8,32 +8,51 @@ from opencog.atomspace import types
 from pln.chainers import Chainer
 from pln.rules import *
 
-TARGET_STIMULUS = 20
-
 class DeceptionAgent(MindAgent):
     def __init__(self):
         self.chainer = None
+        self.query = None
         print "Initialized DeceptionAgent."
 
     def create_chainer(self, atomspace, stimulate_atoms=False):
         self.chainer = Chainer(atomspace,
                                agent=self,
-                               stimulateAtoms=stimulate_atoms,
-                               allow_output_with_variables=False,
+                               stimulateAtoms=False,
+                               preferAttentionalFocus=False,
+                               allow_output_with_variables=True,
                                delete_temporary_variables=True)
 
-        self.chainer.add_rule(
-            ModusPonensRule(self.chainer, types.ImplicationLink))
+        # Transformation rules
+        # This rules are invoked randomly and are run in the background because,
+        # they imply the creation of AndLinks between different sets of Links so
+        # as to represent the existance of a state in the world or of state of knowledge
+        # being simulated. In the ure this wouldn't be required as AndLink is used to denote
+        # the existance of the required set of Links without the need of creating
+        # the AndLink. If one wants to match AndLinked set of Links then the QuoteLink
+        # can be used.(This is based on the assumption that ure doesn't make a fundamental
+        # change on how the pattern matcher works presently)
+        self.chainer.add_rule(AndCreationRule(self.chainer, 2, types.EvaluationLink))
+        self.chainer.add_rule(AndCreationRule(self.chainer, 3, types.EvaluationLink))
+        self.chainer.add_rule(AndCreationRule(self.chainer, 2))
 
     def run(self, atomspace):
         if self.chainer is None:
             self.create_chainer(atomspace)
             return
 
-        result = self.chainer.forward_step()
+        # This insures that one of the added rules are invoked in the background
+        # on each step.
+        self.chainer.forward_step()
 
-        if __VERBOSE__:
-            print result
+        # Any rule that is used below is assured to be executed on each step
+        self.chainer.forward_step(AndCreationRule(self.chainer, 2)) #
+
+        # Depending on choice of demo, either the result on deception reasoning
+        # or the behavior that result because of the reasoning can be enabled
+        # * Enable output of reasoning on rules
+        result = self.chainer.forward_step(ImplicationAndRule(self.chainer))
+        # * Enable ouput of behavior
+#        result = self.chainer.forward_step(ModusPonensRule(self.chainer, types.ImplicationLink))
 
         return result
 
@@ -45,6 +64,7 @@ class DeceptionAgent(MindAgent):
 
 
 def check_result(atomspace):
+    # TODO : check the atomspace for the expected output
     pass
 
 
