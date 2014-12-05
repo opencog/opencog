9d18b34a52c9918e2b106778ad6529edabddbcf1
Nil Geisweiller <ngeiswei@gmail.com>
2014-12-05 15:55:00 +0100

    Fix path so that run_cogserver.sh can be called anywhere

--- scripts/run_cogserver.sh
+++ scripts/run_cogserver.sh
@@ -5,13 +5,16 @@
 # It assumes by default that the building directory name is 'build`
 # otherwise the first command argument can be given to overwrite it
 
-branch_dir=../
+prg_path="$(readlink -f "$0")"
+prg_dir="$(dirname "$prg_path")"
+branch_dir="$prg_dir/.."
 build_dir_name=build
 if [ $# -ge 1 ]    # one or more command line arguments
 then
     # only the first argument is considered the others are ignored
-    build_dir_name=$1
+    build_dir_name="$1"
 fi
-build_dir=${branch_dir}/${build_dir_name}
+build_dir="${branch_dir}/${build_dir_name}"
 
-${build_dir}/opencog/server/cogserver -c ../lib/opencog.conf
+"${build_dir}/opencog/server/cogserver" \
+    -c "${branch_dir}/lib/opencog.conf"
