b99852d1c13546c95ef823d5e058944b92c21511
Nil Geisweiller <ngeiswei@gmail.com>
2014-12-08 15:11:20 +0100

    Improve command argument control

--- scripts/run_cogserver.sh
+++ scripts/run_cogserver.sh
@@ -1,19 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Simple script to run the cogserver with the default configuration
 #
 # It assumes by default that the building directory name is 'build`
 # otherwise the first command argument can be given to overwrite it
 
+set -u
+# set -x
+
 prg_path="$(readlink -f "$0")"
 prg_dir="$(dirname "$prg_path")"
 branch_dir="$prg_dir/.."
-build_dir_name=build
-if [ $# -ge 1 ]    # one or more command line arguments
-then
-    # only the first argument is considered the others are ignored
+if [[ $# == 0 ]]; then # No argument
+    build_dir_name=build
+elif [[ $# == 1 ]]; then # One argument
     build_dir_name="$1"
+else # More than one command line arguments
+    echo "Usage: $0 [BUILD_DIR_NAME=build]"
+    exit 1
 fi
+
 build_dir="${branch_dir}/${build_dir_name}"
 
 "${build_dir}/opencog/server/cogserver" \
