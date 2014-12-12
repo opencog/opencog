9b79e264528166ee42ac1e4bfb4f678785e7219f
William Ma <william.p.ma+github@gmail.com>
2014-12-12 17:13:21 +0800

    documentation changes

--- opencog/nlp/microplanning/atomW.scm
+++ opencog/nlp/microplanning/atomW.scm
@@ -5,7 +5,7 @@
 ; <atomW> -- Custom wrapper for an atom to differentiate same atoms
 ;
 (define-class <atomW> ()
-	(at #:init-keyword #:atom #:getter get-atom)
-	(tw #:init-keyword #:time-weight #:getter get-time-weight)
+	(at #:init-keyword #:atom #:getter get-atom)			; the original atom
+	(tw #:init-keyword #:time-weight #:getter get-time-weight)	; the time weight
 )
 
