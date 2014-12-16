82b92f5ad744b811188839f65f96b18ce6ff11d1
Nil Geisweiller <ngeiswei@gmail.com>
2014-12-16 16:53:37 +0100

    Remove MOSES4OpenCog prototype, instead moved the code under its own project
    I've moved that code under
    
    https://github.com/ngeiswei/proto-AGI-Bio
    
    It's too much of a prototype and is not yet useful to OpenCog.

--- scripts/learning/moses/MOSES4OpenCog/background_knowledge.scm
+++
@@ -1,8 +0,0 @@
-; File containing background knowledge about features and models
-
-; Predicate Top is the predicate which is always true
-(PredicateNode "Top" (stv 1 1))
-
-; Predicate Bottom is the predicate which is always false
-(PredicateNode "Bottom" (stv 0 1))
-
