1990939cfee3fd90a06591fbebd8ebbc65fad946
Nil Geisweiller <ngeiswei@gmail.com>
2015-01-12 11:59:46 +0100

    Simplify foreach_outgoing_handle + add comments

--- opencog/atomspace/Foreach.h
+++ opencog/atomspace/Foreach.h
@@ -18,16 +18,16 @@ namespace opencog
  */
 
 
-//! Invoke the callback on each atom in the outgoing set of handle h.
+//! Invoke the callback on each atom in the outgoing set of handle h
+//! till one of them return true, and returns true as a
+//! result. Otherwise the callback is called on all outgoings and
+//! false is returned.
 /// XXX TODO this should be moved to class Link
 template<class T>
 inline bool foreach_outgoing_handle(LinkPtr ll, bool (T::*cb)(Handle), T *data)
 {
-    const std::vector<Handle> &vh = ll->getOutgoingSet();
-    size_t sz = vh.size();
-
-    for (size_t i = 0; i < sz; i++) {
-        bool rc = (data->*cb)(vh[i]);
+    for (Handle out_h : ll->getOutgoingSet()) {
+        bool rc = (data->*cb)(out_h);
         if (rc) return rc;
     }
     return false;
@@ -35,7 +35,10 @@ inline bool foreach_outgoing_handle(LinkPtr ll, bool (T::*cb)(Handle), T *data)
 
 /* ----------------------------------------------------------- */
 
-//! Invoke the callback on each atom in the incoming set of handle h.
+//! Invoke the callback on each atom in the incoming set of handle h
+//! till one of them returns true, and returns true as a
+//! result. Otherwise the callback is called on all incomings and
+//! false is returned.
 /// XXX TODO this routine should be moved to class Atom
 template<class T>
 inline bool foreach_incoming_handle(Handle h, bool (T::*cb)(Handle), T *data)
