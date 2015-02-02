b6ca73a1a6f010494bcdaaebe3e7ca72596bbe73
Nil Geisweiller <ngeiswei@gmail.com>
2015-02-02 11:00:03 +0100

    Add options to IO combo programs from arguments and files

--- opencog/comboreduct/main/combo-fmt-converter.cc
+++ opencog/comboreduct/main/combo-fmt-converter.cc
@@ -22,6 +22,8 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include <fstream>
+
 #include <boost/program_options.hpp>
 
 #include "../combo/iostream_combo.h"
@@ -56,26 +58,29 @@ pgrParameters parse_program_args(int argc, char** argv)
 	desc.add_options()
 		("help,h", "Produce help message.\n")
 
-		// (opt_desc_str(combo_str_opt).c_str(),
-		//  value<vector<string>>(&pa.combo_programs),
-		//  "Alternative way to entering the programs to convert (as opposed to "
-		//  "via stdin). "
-		//  "Remember to escape the $ signs or to wrap them in single "
-		//  "quotes as to not let the shell perform variable expansions "
-		//  "on the combo program variables."
-		//  "This option may be "
-		//  "used several times to convert several programs.\n")
+		("combo-program,c",
+		 value<vector<string>>(&pa.combo_programs),
+		 "Alternative way to entering the programs to convert "
+		 "(as opposed to via stdin, not that using this option will "
+		 "disable reading on the stdin). "
+		 "Remember to escape the $ signs or to wrap them in single "
+		 "quotes as to not let the shell perform variable expansions "
+		 "on the combo program variables."
+		 "This option may be "
+		 "used several times to convert several programs.\n")
 
-		// (opt_desc_str(combo_prog_file_opt).c_str(),
-		//  value<vector<string>>(&pa.combo_programs_files),
-		//  "Other alternative way. Indicate the path of a file containing combo "
-		//  "programs (seperated by newlines) to convert. "
-		//  "This option may be used several times to progress several files.\n")
+		("combo-programs-file,C",
+		 value<vector<string>>(&pa.combo_programs_files),
+		 "Other alternative way (disables stdin). "
+		 "Indicate the path of a file containing combo "
+		 "programs (seperated by newlines) to convert. "
+		 "This option may be used several times to progress several "
+		 "files.\n")
 
-		// (opt_desc_str(output_file_opt).c_str(),
-		//  value<string>(&pa.output_file),
-		//  "File where to save the results. If empty then outputs on "
-		//  "stdout.\n")
+		("output-file,o",
+		 value<string>(&pa.output_file),
+		 "File where to save the results. If empty then outputs on "
+		 "stdout.\n")
 
 		("output-format,f",
 		 value<string>(&pa.output_format_str)->default_value("combo"),
@@ -95,6 +100,63 @@ pgrParameters parse_program_args(int argc, char** argv)
 	return pa;
 }
 
+vector<string> get_all_combo_tree_str(const pgrParameters& pa)
+{
+    vector<string> res(pa.combo_programs);     // from command line
+
+    // from files
+    for (const string& combo_programs_file : pa.combo_programs_files) {
+        ifstream in(combo_programs_file.c_str());
+        if (in) {
+            while (in.good()) {
+                string line;
+                getline(in, line);
+                if (line.empty())
+                    continue;
+                res.push_back(line);
+            }
+        } else {
+            logger().error("Error: file %s can not be found.",
+                           combo_programs_file.c_str());
+            exit(1);
+        }
+    }
+
+    return res;
+}
+
+void output_tree(const pgrParameters& pa,
+                 const combo_tree& tr,
+                 const vector<string>& vars,
+                 combo::output_format fmt)
+{
+    if(pa.output_file.empty())
+	    ostream_combo_tree(cout, tr, vars, fmt) << std::endl;
+    else {
+        ofstream of(pa.output_file.c_str());
+	    ostream_combo_tree(of, tr, vars, fmt) << std::endl;
+    }
+}
+
+// Convert a single combo program gotten from istream
+istream& convert(istream& in, const pgrParameters& pa,
+                 combo::output_format fmt) {
+	// Parse combo tree
+	string line;
+	getline(in, line);
+	if (line.empty())
+		return in;
+
+	vector<string> variables = parse_combo_variables(line);
+	combo_tree tr = str2combo_tree(line, variables);
+	type_tree tt = infer_type_tree(tr);
+
+	// Write to the right format
+	output_tree(pa, tr, variables, fmt);
+
+	return in;
+}
+
 int main(int argc, char** argv)
 {
 	pgrParameters pa = parse_program_args(argc, argv);
@@ -102,20 +164,20 @@ int main(int argc, char** argv)
 	// Parse output format
 	combo::output_format fmt = parse_output_format(pa.output_format_str);
 
-	// IO loop
-	while (cin.good()) {
-		// Parse combo tree
-		string line;
-		getline(cin, line);
-		if (line.empty())
-			continue;
+	// read combo program strings given by an alternative way than
+	// stdin
+	vector<string> combo_trees_str = get_all_combo_tree_str(pa);
 
-		vector<string> variables = parse_combo_variables(line);
-		combo_tree tr = str2combo_tree(line, variables);
-		type_tree tt = infer_type_tree(tr);
-
-		// Write to the right format
-		ostream_combo_tree(cout, tr, variables, fmt) << std::endl;
+	if (combo_trees_str.empty()) {
+		// If empty then use stdin
+		while (cin.good())
+			convert(cin, pa, fmt);
+	} else {
+		// Don't use stdin
+		for (const string& str : combo_trees_str) {
+			stringstream ss(str);
+			convert(ss, pa, fmt);
+		}
 	}
 	
 	return 0;	
