2e4a48a43060f13be8071626ee4783a3aed98eec
Nil Geisweiller <ngeiswei@gmail.com>
2015-02-02 11:58:53 +0100

    Reformat indentation of DefaultPatternMatchCB.h as it was
    After look at dozens of include files on linux system I found a few
    with that indentation format, so it's not that nonstandard.

--- opencog/query/DefaultPatternMatchCB.h
+++ opencog/query/DefaultPatternMatchCB.h
@@ -42,132 +42,132 @@ namespace opencog {
  */
 class DefaultPatternMatchCB : public virtual PatternMatchCallback
 {
-public:
-	DefaultPatternMatchCB(AtomSpace* as) :
-		_type_restrictions(NULL),
-		_as(as)
-	{}
+	public:
+		DefaultPatternMatchCB(AtomSpace* as) :
+			_type_restrictions(NULL),
+			_as(as)
+		{}
 
-	/**
-	 * Called when a node in the template pattern needs to
-	 * be compared to a possibly matching node in the atomspace.
-	 * The first argument is a node from the pattern, and the
-	 * second is a possible solution (grounding) node from the
-	 * atomspace.
-	 *
-	 * Return true if the nodes match, else return
-	 * false. (i.e. return false if mis-match).
-	 *
-	 * By default, the nodes must be identical.
-	 */
-	virtual bool node_match(Handle& npat_h, Handle& nsoln_h)
-	{
-		// If equality, then a match.
-		return npat_h == nsoln_h;
-	}
+		/**
+		 * Called when a node in the template pattern needs to
+		 * be compared to a possibly matching node in the atomspace.
+		 * The first argument is a node from the pattern, and the
+		 * second is a possible solution (grounding) node from the
+		 * atomspace.
+		 *
+		 * Return true if the nodes match, else return
+		 * false. (i.e. return false if mis-match).
+		 *
+		 * By default, the nodes must be identical.
+		 */
+		virtual bool node_match(Handle& npat_h, Handle& nsoln_h)
+		{
+			// If equality, then a match.
+			return npat_h == nsoln_h;
+		}
 
-	/**
-	 * Called when a variable in the template pattern
-	 * needs to be compared to a possible grounding
-	 * node in the atomspace. The first argument
-	 * is a variable from the pattern, and the second
-	 * is a possible grounding node from the atomspace.
-	 * Return true if the nodes match, else return
-	 * false. (i.e. return false if mis-match).
-	 */
-	virtual bool variable_match(Handle& npat_h, Handle& nsoln_h)
-	{
-		Type pattype = npat_h->getType();
+		/**
+		 * Called when a variable in the template pattern
+		 * needs to be compared to a possible grounding
+		 * node in the atomspace. The first argument
+		 * is a variable from the pattern, and the second
+		 * is a possible grounding node from the atomspace.
+		 * Return true if the nodes match, else return
+		 * false. (i.e. return false if mis-match).
+		 */
+		virtual bool variable_match(Handle& npat_h, Handle& nsoln_h)
+		{
+			Type pattype = npat_h->getType();
 
-		// If the ungrounded term is not of type VariableNode, then just
-		// accept the match. This allows any kind of node types to be
-		// explicitly bound as variables.  However, the type VariableNode
-		// gets special handling, below.
-		if (pattype != VARIABLE_NODE) return true;
+			// If the ungrounded term is not of type VariableNode, then just
+			// accept the match. This allows any kind of node types to be
+			// explicitly bound as variables.  However, the type VariableNode
+			// gets special handling, below.
+			if (pattype != VARIABLE_NODE) return true;
 
-		// If the ungrounded term is a variable, then see if there
-		// are any restrictions on the variable type.
-		// If no restrictions, we are good to go.
-		if (NULL == _type_restrictions) return true;
+			// If the ungrounded term is a variable, then see if there
+			// are any restrictions on the variable type.
+			// If no restrictions, we are good to go.
+			if (NULL == _type_restrictions) return true;
 
-		// If we are here, there's a restriction on the grounding type.
-		// Validate the node type, if needed.
-		VariableTypeMap::const_iterator it = _type_restrictions->find(npat_h);
-		if (it == _type_restrictions->end()) return true;
+			// If we are here, there's a restriction on the grounding type.
+			// Validate the node type, if needed.
+			VariableTypeMap::const_iterator it = _type_restrictions->find(npat_h);
+			if (it == _type_restrictions->end()) return true;
 
-		// Is the ground-atom type in our list of allowed types?
-		Type soltype = nsoln_h->getType();
-		const std::set<Type> &tset = it->second;
-		std::set<Type>::const_iterator allow = tset.find(soltype);
-		return allow != tset.end();
-	}
+			// Is the ground-atom type in our list of allowed types?
+			Type soltype = nsoln_h->getType();
+			const std::set<Type> &tset = it->second;
+			std::set<Type>::const_iterator allow = tset.find(soltype);
+			return allow != tset.end();
+		}
 
-	/**
-	 * Called when a link in the template pattern
-	 * needs to be compared to a possibly matching
-	 * link in the atomspace. The first argument
-	 * is a link from the pattern, and the second
-	 * is a possible solution link from the atomspace.
-	 * Return true if the links match, else return
-	 * false. (i.e. return false if mis-match).
-	 *
-	 * By default, the link arity and the
-	 * link types must match.
-	 */
-	virtual bool link_match(LinkPtr& lpat, LinkPtr& lsoln)
-	{
-		// If the pattern is exactly the same link as the proposed
-		// grounding, then its a perfect match. 
-		if (lpat == lsoln) return true;
+		/**
+		 * Called when a link in the template pattern
+		 * needs to be compared to a possibly matching
+		 * link in the atomspace. The first argument
+		 * is a link from the pattern, and the second
+		 * is a possible solution link from the atomspace.
+		 * Return true if the links match, else return
+		 * false. (i.e. return false if mis-match).
+		 *
+		 * By default, the link arity and the
+		 * link types must match.
+		 */
+		virtual bool link_match(LinkPtr& lpat, LinkPtr& lsoln)
+		{
+			// If the pattern is exactly the same link as the proposed
+			// grounding, then its a perfect match. 
+			if (lpat == lsoln) return true;
 
-		if (lpat->getArity() != lsoln->getArity()) return false;
-		Type pattype = lpat->getType();
-		Type soltype = lsoln->getType();
+			if (lpat->getArity() != lsoln->getArity()) return false;
+			Type pattype = lpat->getType();
+			Type soltype = lsoln->getType();
 
-		// If types differ, no match
-		return pattype == soltype;
-	}
+			// If types differ, no match
+			return pattype == soltype;
+		}
 
-	/**
-	 * Called when a virtual link is encountered. Returns false
-	 * to reject the match.
-	 */
-	virtual bool virtual_link_match(LinkPtr& lpat, Handle& args);
+		/**
+		 * Called when a virtual link is encountered. Returns false
+		 * to reject the match.
+		 */
+		virtual bool virtual_link_match(LinkPtr& lpat, Handle& args);
 
-	/**
-	 * Check that all clauses are connected
-	 */
-	virtual void validate_clauses(std::set<Handle>& vars,
-	                              std::vector<Handle>& clauses,
-	                              std::vector<Handle>& negations);
+		/**
+		 * Check that all clauses are connected
+		 */
+		virtual void validate_clauses(std::set<Handle>& vars,
+		                              std::vector<Handle>& clauses,
+		                              std::vector<Handle>& negations);
 
-	/**
-	 * Called to perform the actual search. This makes some default
-	 * assumptions about the kind of things that might be matched,
-	 * in order to drive a reasonably-fast search.
-	 */
-	virtual void perform_search(PatternMatchEngine *,
-	                            std::set<Handle> &vars,
-	                            std::vector<Handle> &clauses,
-	                            std::vector<Handle> &negations);
+		/**
+		 * Called to perform the actual search. This makes some default
+		 * assumptions about the kind of things that might be matched,
+		 * in order to drive a reasonably-fast search.
+		 */
+		virtual void perform_search(PatternMatchEngine *,
+		                            std::set<Handle> &vars,
+		                            std::vector<Handle> &clauses,
+		                            std::vector<Handle> &negations);
 
-	/**
-	 * Indicate a set of restrictions on the types of the ground atoms.
-	 * The typemap contains a map from variables to a set of types
-	 * that the groundings for the variable are allowed to have.
-	 */
-	virtual void set_type_restrictions(VariableTypeMap &tm)
-	{
-		_type_restrictions = &tm;
-	}
-private:
-	Handle _root;
-	Handle _starter_pred;
-	VariableTypeMap *_type_restrictions;
-protected:
-	virtual Handle find_starter(Handle, size_t&, Handle&, size_t&);
-	virtual Handle find_thinnest(std::vector<Handle>&, Handle&, size_t&);
-	AtomSpace *_as;
+		/**
+		 * Indicate a set of restrictions on the types of the ground atoms.
+		 * The typemap contains a map from variables to a set of types
+		 * that the groundings for the variable are allowed to have.
+		 */
+		virtual void set_type_restrictions(VariableTypeMap &tm)
+		{
+			_type_restrictions = &tm;
+		}
+	private:
+		Handle _root;
+		Handle _starter_pred;
+		VariableTypeMap *_type_restrictions;
+	protected:
+		virtual Handle find_starter(Handle, size_t&, Handle&, size_t&);
+		virtual Handle find_thinnest(std::vector<Handle>&, Handle&, size_t&);
+		AtomSpace *_as;
 };
 
 } // namespace opencog
