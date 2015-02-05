1aeb3e756d8d70fc02ad6e7d2a1bc4350b136c15
Misgana Bayetta <misgana.bayetta@gmail.com>
2015-02-05 16:28:17 +0800

    Class to Load the Json rule engine conf file

---
+++ opencog/reasoning/RuleEngine/rule-engine-src/JsonicControlPolicyParamLoader.h
@@ -0,0 +1,39 @@
+/*
+ * JsonicControlPolicyLoader.h
+ *
+ *  Created on: 29 Dec, 2014
+ *      Author: misgana
+ */
+
+#ifndef JSONICCONTROLPOLICYLOADER_H_
+#define JSONICCONTROLPOLICYLOADER_H_
+
+#include "ControlPolicyParamLoader.h"
+#include "Rule.h"
+
+#include <lib/json_spirit/json_spirit.h>
+
+using namespace opencog;
+using namespace json_spirit;
+
+class JsonicControlPolicyLoader:public virtual ControlPolicyParamLoader {
+private:
+	Rule* cur_read_rule_ = NULL;
+	map<Rule*,vector<string>> rule_mutex_map_;
+	void read_json(const Value &v, int level = -1);
+	void read_array(const Value &v, int level);
+	void read_obj(const Value &v, int level);
+	void read_null(const Value &v, int level);
+	template <typename T> void read_primitive(const Value &v,int level);
+	/**
+	 * sets the mutually exclusive rules
+	 */
+	void set_mutex_rules(void);
+	Rule* get_rule(string& name);
+public:
+	JsonicControlPolicyLoader(AtomSpace* as, string conf_path);
+	virtual ~JsonicControlPolicyLoader();
+	virtual void load_config();
+};
+
+#endif /* JSONICCONTROLPOLICYLOADER_H_ */
