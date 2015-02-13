db5b867af1c119f9a75afdef76fd9c755a23d1e4
Linas Vepstas <linasvepstas@gmail.com>
2015-02-13 12:32:24 -0600

    Expanded demo

--- examples/guile/gperf.scm
+++ examples/guile/gperf.scm
@@ -1,21 +1,26 @@
+#!/usr/bin/env guile
+!#
+;
+; The below hangs; it should not. See
+; https://bugs.launchpad.net/ubuntu/+source/coreutils/+bug/1421760
+; #!/usr/bin/env LTDL_LIBRARY_PATH=../../build/opencog/guile guile
 ;
 ; Basic guile atom creation benchmark.
 ;
 ; See opencog/guile/README or http://wiki.opencog.org/w/Scheme
 ; for additional documentation.
 ;
-; If you have installed opencog, then start guile simply by saying
-; "guile" at the bash prompt $.  Otherwise, you will need to do this:
+; To run this, say:
 ;
 ; $ export LTDL_LIBRARY_PATH=build/opencog/guile
-; $ guile -L opencog/scm -L build
+; $ ./gperf.scm
 ;
 ; where "build" is where-ever you built opencog.
-;
-; Another possibility: add stuff to your ~/.guile file, for example:
-; (add-to-load-path "/home/yourname/opencog/build")
-; (add-to-load-path "/home/yourname/opencog/opencog/scm")
-;
+
+; Add search paths for the opencog module.
+(add-to-load-path "../../bin")
+(add-to-load-path "../../build")
+(add-to-load-path "../../opencog/scm")
 
 (use-modules (opencog))
 (use-modules (ice-9 format))
@@ -28,41 +33,73 @@
 ; 3) creation of the ConceptNode in the AtomSpace
 ; 4) creation of the ListLink in the AtomSpace.
 ; 5) tail recursion, to loop and repeat the process again.
-(define c1 (ConceptNode "first one ever"))
-(define (make-atoms prefix n)
-	(define c2 c1)
-	(define c1 (ConceptNode (string-append/shared prefix (number->string n))))
-	; Hmmm.. formatted printing is kind-of slow.
-	; (ConceptNode (format #f "Object_~d" n))
-	(ListLink c1 c2)
-	(if (< 0 n)
-		(make-atoms prefix (- n 1))
-		'()
+(define (make-node-and-link prefix n)
+	(define (make-atoms prefix node n)
+		(define cpt (ConceptNode (string-append/shared prefix (number->string n))))
+		; Hmmm.. formatted printing is kind-of slow.
+		; (ConceptNode (format #f "Object_~d" n))
+		(ListLink node cpt)
+		(if (< 0 n)
+			(make-atoms prefix cpt (- n 1))
+			'()
+		)
 	)
+	(make-atoms prefix (ConceptNode prefix) n)
+)
+
+; Define a function that creates large tree of links.
+; We expect this to run much faster than the above, because it does a
+; lot less.  Things that waste CPU time here are:
+; 1) creation of the ListLink in the AtomSpace.
+; 2) tail recursion, to loop and repeat the process again.
+(define (make-link-tree n)
+	(define (make-atoms atom n)
+		(define linky (ListLink atom atom))
+		(if (< 0 n)
+			(make-atoms linky (- n 1))
+			'()
+		)
+	)
+	(make-atoms (ConceptNode "nil") n)
 )
 
 ; A handy utility to report the elapsed time and the rate.
-(define (report-perf start stop niter)
+(define (report-perf id start stop niter)
 	(define elapsed (time-difference stop start))
 	(define delta 
 		(+ (time-second elapsed) 
 			(/ (time-nanosecond elapsed) 1000000000.0)))
 	(define rate (round (/ niter delta)))
+	(display id) (newline)
 	(display "Elapsed time (secs): ") (display delta) (newline)
 	(display "Loops per second: ") (display rate) (newline)
+	(newline)
 )
 
+
 ; Measure how long it takes to create a bunch of links.
-(define niter 150000)
+(define niter 250000)
 (define start (current-time))
-(make-atoms "hello world" niter)
+(make-node-and-link "hello world" niter)
 (define stop (current-time))
-(report-perf start stop niter)
+(report-perf "node and link, first time:" start stop niter)
 
 ; Do it again. This time, its twice as fast, because the atoms already
 ; exist in the atomspace, and are not being made for the first time.
 (define start (current-time))
-(make-atoms "hello world" niter)
+(make-node-and-link "hello world" niter)
 (define stop (current-time))
-(report-perf start stop niter)
+(report-perf "node and link, second time:" start stop niter)
+
+; Make a large binary tree.
+(define start (current-time))
+(make-link-tree niter)
+(define stop (current-time))
+(report-perf "binary tree, first time:" start stop niter)
+
+; Make a large binary tree, again.
+(define start (current-time))
+(make-link-tree niter)
+(define stop (current-time))
+(report-perf "binary tree, second time:" start stop niter)
 
