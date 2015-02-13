f8bfc4687b23021aa505e9c3ff2f79f93a5a0fcb
Misgana Bayetta <misgana.bayetta@gmail.com>
2015-02-13 19:11:09 +0800

    Added several utility functions

--- opencog/reasoning/RuleEngine/rule-engine-src/pln/PLNCommons.h
+++ opencog/reasoning/RuleEngine/rule-engine-src/pln/PLNCommons.h
@@ -1,10 +1,25 @@
 /*
- * PLNCommons.h
+ * PLNCommons.cc
  *
- *  Created on: 9 Oct, 2014
- *      Author: misgana
+ * Copyright (C) 2014 Misgana Bayetta
+ *
+ * Author: Misgana Bayetta <misgana.bayetta@gmail.com>  Oct 2014
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 #ifndef PLNCOMMONS_H_
 #define PLNCOMMONS_H_
 
@@ -20,6 +35,7 @@ using namespace opencog;
 class PLNCommons {
 public:
 	AtomSpace * as_;
+	const float FITNESS_PARAM = 0.9;
 	PLNCommons(AtomSpace *as);
 	virtual ~PLNCommons();
 	/**
@@ -27,7 +43,8 @@ public:
 	 * @param himplicant - an implicant part of the BindLink must have a variable node.
 	 * @return - a Handle to the BindLink instance created
 	 */
-	Handle create_bindLink(Handle himplicant,bool is_quoted = false) throw (opencog::InvalidParamException);
+	Handle create_bindLink(Handle himplicant, bool is_quoted = false)
+			throw (opencog::InvalidParamException);
 	/**
 	 * Given an atom (a link or node), Find and return all the nodes associated
 	 * @param hinput - an atoms to be looked
@@ -38,7 +55,7 @@ public:
 	/**
 	 * checks if a handle already exists in a HandleSeq
 	 */
-	bool exists(HandleSeq hseq, Handle h);
+	bool exists_in(Handle& hlink, Handle& h);
 	/**
 	 *wraps all variables of the input handle in a QuoteLink. TODO adding flexibility for wrapping particular variables only
 	 *@param himplicant a Handle containing variables
@@ -49,11 +66,13 @@ public:
 	 * Remove created bind link and quote links inf exist
 	 * @param hbindLink a reference to the bindLink to be removed
 	 */
-	void clean_up_bind_link(Handle& hbindLink) throw (opencog::InvalidParamException);
+	void clean_up_bind_link(Handle& hbindLink)
+			throw (opencog::InvalidParamException);
 	/**
 	 * Remove implication link created with unique vars
 	 */
-	void clean_up_implication_link(Handle& himplication_link) throw (opencog::InvalidParamException);
+	void clean_up_implication_link(Handle& himplication_link)
+			throw (opencog::InvalidParamException);
 	/**
 	 * Remove a variable node. All the incoming sets are also removed in a recursive manner.
 	 * @param h a var node to be deleted
@@ -62,7 +81,8 @@ public:
 	/**
 	 * create a copy of the given implicatoin link with unique variables
 	 */
-	Handle replace_nodes_with_varnode(Handle& himplication_link,Type t=VARIABLE_NODE);
+	Handle replace_nodes_with_varnode(Handle& himplication_link, Type t =
+			VARIABLE_NODE);
 
 	/*
 	 * Generate UUID
@@ -71,37 +91,43 @@ public:
 	/**
 	 * create a copy of the handle with a different variable name passed in @param var_uniq_var map input
 	 */
-	Handle change_node_types(Handle& h, map<Handle, Handle>& replacement_map,
-			Type t) {
-		Handle hcpy;
-		if (LinkCast(h)) {
-			HandleSeq hs = as_->getOutgoing(h);
-			HandleSeq hs_cpy;
-			for (Handle hi : hs) {
-				if (NodeCast(hi)) {
-					if (t == NODE || as_->getType(hi) == t)
-						hs_cpy.push_back(replacement_map[hi]);
-					else
-						hs_cpy.push_back(hi);
-				} else if (LinkCast(hi)) {
-					hs_cpy.push_back(change_node_types(hi, replacement_map, t));
-				}
-			}
-			hcpy = as_->addLink(as_->getType(h), hs_cpy, as_->getTV(h));
-		} else if (NodeCast(h)) {
-			if (t == NODE || as_->getType(h) == t)
-				hcpy = replacement_map[h];
-			else
-				hcpy = h;
-		}
-
-		return hcpy;
-	}
+	Handle change_node_types(Handle& h, map<Handle, Handle>& replacement_map);
 	/**
 	 * Get top level parent of the handle
 	 * @return a Link or Handle::UNDEFINED if there is no
 	 */
-	void get_top_level_parent(Handle h,HandleSeq parents);
+	void get_top_level_parent(Handle h, HandleSeq& parents);
+	template<class Type> Type tournament_select(map<Type, float> tfitnes_map) {
+		if (tfitnes_map.size() == 1) {
+			return tfitnes_map.begin()->first;
+		}
+
+		map<Type, float> winners;
+		int size = tfitnes_map.size() / 2; //TODO change the way tournament size is calculated
+		for (auto i = 0; i < size; i++) {
+			int index = (random() % tfitnes_map.size());
+			auto it = tfitnes_map.begin();
+			advance(it, index);
+			winners[it->first] = it->second;
+		}
+		auto it = winners.begin();
+		Type hbest = it->first;
+		float max = it->second;
+		for (; it != winners.end(); ++it) {
+			if (it->second > max) {
+				hbest = it->first;
+				max = it->second;
+			}
+		}
+		return hbest;
+	}
+	/**
+	 * calculates fitness values in target_list_atom_space using the formula F = s^x * c^(2-x)
+	 * where s is strength,c is confidence and x is some fixed value
+	 * @param h - a handle
+	 * @return a fitness value
+	 */
+	float target_tv_fitness(Handle h);
 };
 
 #endif /* PLNCOMMONS_H_ */
