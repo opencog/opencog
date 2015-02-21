134d649c7f0e20d93b6653f4d3fa166f98fa2faa
Linas Vepstas <linasvepstas@gmail.com>
2015-02-21 14:27:06 -0600

    Add an exception-handling demo

---
+++ examples/guile/except.scm
@@ -0,0 +1,42 @@
+;
+; Guile excpetion handling example.
+; The cog-execute! function is demonstrated.
+;
+
+(use-modules (opencog))
+
+; First, just give it some broken junk.  See what happens.
+(cog-execute!
+   (ExecutionOutputLink
+      (GroundedSchemaNode "py:b0rk3n_junk")
+      (ListLink
+         (ConceptNode "1")
+         (ConceptNode "2"))))
+
+; C++ exceptions are converted into scheme exceptions, and can be
+; caught, as usual.
+(catch
+   #t
+   (lambda ()
+      (cog-execute!
+         (ExecutionOutputLink
+            (GroundedSchemaNode "py:b0rk3n_junk")
+            (ListLink
+               (ConceptNode "1")
+               (ConceptNode "2")))))
+   (lambda (key . args)
+      (display "Ohhh noooo Mr. Bill!!! ") (display key)
+		(newline)
+		(display "Sluggo says to ... ") (display args)
+		(newline) (newline)
+   ))
+
+
+; Exception-producing code, but for mal-formed scheme.
+;
+(cog-execute!
+   (ExecutionOutputLink
+      (GroundedSchemaNode "scm:(((((uber-badf")
+      (ListLink
+         (ConceptNode "1")
+         (ConceptNode "2"))))
