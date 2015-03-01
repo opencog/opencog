d7612421ed04bf20e3314752f4f9d6a4cb262e25
rohit12 <rohit.shinde12194@gmail.com>
2015-03-01 09:41:11 +0530

    Miscellanous scripts fixed including atom_types.script

--- opencog/nlp/learn/misc-scripts/config-fr.scm
+++ opencog/nlp/learn/misc-scripts/config-fr.scm
@@ -6,10 +6,10 @@
 ;
 ;--------------------------------------------------------------
 ; The scheme shell listen port.
-(define shell-port 18002)
+(define shell-port 17001)
 (define shell-prompt "[0;34mscheme-fr[1;34m> [0m")
 
 ;--------------------------------------------------------------
 ; The relex server host and port
-(define relex-server-host "10.1.1.4")
+(define relex-server-host "127.0.0.1")
 (define relex-server-port 4444)
