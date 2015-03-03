a15846ef0128964b71e4c9213175de4becfb94f9
Curtis Faith <curtis.m.faith@gmail.com>
2015-03-02 22:33:28 -0500

    Fixed crash and problems with Python error handling

--- opencog/python/preload_functions/test_functions.py
+++ opencog/python/preload_functions/test_functions.py
@@ -1,7 +1,11 @@
 # 
 # Test Functions preloaded into the CogServer
 
+from opencog.atomspace import types
+
 def print_arguments(argOne, argTwo):
     print "argOne = ", argOne
     print "argTwo = ", argTwo
-
+    atom = ATOMSPACE.add_link(types.ListLink, [argOne, argTwo])
+    print "atom = ", atom
+    return atom
