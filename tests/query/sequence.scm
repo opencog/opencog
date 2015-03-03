7b6c3930810d00aa11e1c1867f6a605751781a9e
Linas Vepstas <linasvepstas@gmail.com>
2015-03-02 22:21:19 -0600

    Expand the unit test.

--- tests/query/sequence.scm
+++ tests/query/sequence.scm
@@ -9,15 +9,39 @@
 (define green-light  (ConceptNode "green light"))
 (define red-light  (ConceptNode "red light"))
 
+; Return SimpleTV of true if green light, false if red light, and
+; throw an exception if neither.
 (define (stop-go atom)
 	(cond
-		((eq? atom green-light) (stv 1 1))
-		((eq? atom red-light) (stv 0 1))
-		(else (throw 'not-a-stoplight))
+		((eq? atom green-light) (begin (display "go\n") (stv 1 1)))
+		((eq? atom red-light) (begin (display "stop\n") (stv 0 1)))
+		(else (begin (display "busted\n") (throw 'not-a-stoplight)))
 	)
 )
 
-(define (traffic)
+; Should throw an exception in all cases. Shouldn't do donuts on
+; corn fields.
+(define (off-road)
+	(BindLink
+		(ListLink)
+		(ImplicationLink
+			(AndLink
+				(SequentialAndLink
+					(EvaluationLink
+						(GroundedPredicateNode "scm: stop-go")
+						(ListLink
+							(ConceptNode "corn field")
+						)
+					)
+				)
+			)
+
+			(ListLink)
+		)
+	)
+)
+
+(define (traffic-lights)
 	(BindLink
 		(ListLink)
 		(ImplicationLink
