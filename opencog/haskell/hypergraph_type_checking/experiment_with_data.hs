a639e9e2791dd31850da45919dc2fb729a98ee6b
Nil Geisweiller <ngeiswei@gmail.com>
2015-03-05 16:03:26 +0100

    Add GetTVLink in Haskell type checking experiment (cause we need one)

--- opencog/haskell/hypergraph_type_checking/experiment_with_data.hs
+++ opencog/haskell/hypergraph_type_checking/experiment_with_data.hs
@@ -10,6 +10,7 @@ data TV = NullTV
         | TVAndLink TV TV
         | TVOrLink TV TV
         | TVEvaluationLink EvaluationLink
+        | TVGetTVLink Atom
 
 data Atom = ANode Node | ALink Link
 
@@ -42,6 +43,8 @@ data EvaluationLink = EvaluationLink Predicate [Atom]
 
 data SchemaLink = SchemaLink ([Atom] -> Atom)
 
+data GetTVLink = GetTVLink Atom
+
 -------------
 -- Example --
 -------------
@@ -63,10 +66,17 @@ h1 = COrLink (CAndLink (Concept "A") (Concept "B")) (Concept "C")
 h2 = POrLink (PAndLink (Predicate is_top) (Predicate is_car)) (Predicate is_bottom)
 
 -- Apply EvaluationLink to predicate h2
-h3 = TVEvaluationLink (EvaluationLink h2 [ANode (NConcept (Concept "BMW"))])
+tv3 = TVEvaluationLink (EvaluationLink h2 [ANode (NConcept (Concept "BMW"))])
 
 -- Build a SchemaLink
 add :: [Atom] -> Atom
 add [ANode (NNumber x), ANode (NNumber y)] = ANode (NNumber (x + y))
 add _ = ANode (NConcept (Concept "undefined"))
 h4 = SchemaLink add
+
+-- Test GetTVLink
+tv1 = TVGetTVLink (ANode (NConcept h1))
+tv2 = TVGetTVLink (ANode (NPredicate h2))
+
+-- Test AndLink with TV
+tv4 = TVAndLink tv1 tv3
