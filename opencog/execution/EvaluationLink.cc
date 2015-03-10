9ac9e7f4a21a70e96a1a4c001de79c9a0e859aed
Linas Vepstas <linasvepstas@gmail.com>
2015-03-10 13:12:52 -0500

    Nothing but a white-space change

--- opencog/execution/EvaluationLink.cc
+++ opencog/execution/EvaluationLink.cc
@@ -29,27 +29,27 @@
 using namespace opencog;
 
 EvaluationLink::EvaluationLink(const HandleSeq& oset,
-                             TruthValuePtr tv,
-                             AttentionValuePtr av)
+                               TruthValuePtr tv,
+                               AttentionValuePtr av)
     : Link(EVALUATION_LINK, oset, tv, av)
 {
-    if ((2 != oset.size()) or
-       (LIST_LINK != oset[1]->getType()))
-    {
-        throw RuntimeException(TRACE_INFO,
-            "EvaluationLink must have predicate and args!");
-    }
+	if ((2 != oset.size()) or
+	   (LIST_LINK != oset[1]->getType()))
+	{
+		throw RuntimeException(TRACE_INFO,
+		    "EvaluationLink must have predicate and args!");
+	}
 }
 
 EvaluationLink::EvaluationLink(Handle schema, Handle args,
-                             TruthValuePtr tv,
-                             AttentionValuePtr av)
+                               TruthValuePtr tv,
+                               AttentionValuePtr av)
     : Link(EVALUATION_LINK, schema, args, tv, av)
 {
-    if (LIST_LINK != args->getType()) {
-        throw RuntimeException(TRACE_INFO,
-            "EvaluationLink must have args in a ListLink!");
-    }
+	if (LIST_LINK != args->getType()) {
+		throw RuntimeException(TRACE_INFO,
+		    "EvaluationLink must have args in a ListLink!");
+	}
 }
 
 /// do_evaluate -- evaluate the GroundedPredicateNode of the EvaluationLink
@@ -69,11 +69,11 @@ EvaluationLink::EvaluationLink(Handle schema, Handle args,
 ///
 TruthValuePtr EvaluationLink::do_evaluate(AtomSpace* as, Handle execlnk)
 {
-    if (EVALUATION_LINK != execlnk->getType()) {
-        throw RuntimeException(TRACE_INFO, "Expecting to get an EvaluationLink!");
-    }
-    LinkPtr l(LinkCast(execlnk));
-    return do_evaluate(as, l->getOutgoingSet());
+	if (EVALUATION_LINK != execlnk->getType()) {
+		throw RuntimeException(TRACE_INFO, "Expecting to get an EvaluationLink!");
+	}
+	LinkPtr l(LinkCast(execlnk));
+	return do_evaluate(as, l->getOutgoingSet());
 }
 
 /// do_evaluate -- evaluate the GroundedPredicateNode of the EvaluationLink
@@ -85,11 +85,11 @@ TruthValuePtr EvaluationLink::do_evaluate(AtomSpace* as, Handle execlnk)
 ///
 TruthValuePtr EvaluationLink::do_evaluate(AtomSpace* as, const HandleSeq& sna)
 {
-    if (2 != sna.size())
-    {
-        throw RuntimeException(TRACE_INFO, "Incorrect arity for an EvaluationLink!");
-    }
-    return do_evaluate(as, sna[0], sna[1]);
+	if (2 != sna.size())
+	{
+		throw RuntimeException(TRACE_INFO, "Incorrect arity for an EvaluationLink!");
+	}
+	return do_evaluate(as, sna[0], sna[1]);
 }
 
 /// do_evaluate -- evaluate the GroundedPredicateNode of the EvaluationLink
@@ -100,91 +100,91 @@ TruthValuePtr EvaluationLink::do_evaluate(AtomSpace* as, const HandleSeq& sna)
 ///
 TruthValuePtr EvaluationLink::do_evaluate(AtomSpace* as, Handle gsn, Handle args)
 {
-    if (GROUNDED_PREDICATE_NODE != gsn->getType())
-    {
-        throw RuntimeException(TRACE_INFO, "Expecting GroundedPredicateNode!");
-    }
-    if (LIST_LINK != args->getType())
-    {
-        throw RuntimeException(TRACE_INFO, "Expecting arguments to EvaluationLink!");
-    }
+	if (GROUNDED_PREDICATE_NODE != gsn->getType())
+	{
+		throw RuntimeException(TRACE_INFO, "Expecting GroundedPredicateNode!");
+	}
+	if (LIST_LINK != args->getType())
+	{
+		throw RuntimeException(TRACE_INFO, "Expecting arguments to EvaluationLink!");
+	}
 
-    // Get the schema name.
-    const std::string& schema = NodeCast(gsn)->getName();
-    // printf ("Grounded schema name: %s\n", schema.c_str());
+	// Get the schema name.
+	const std::string& schema = NodeCast(gsn)->getName();
+	// printf ("Grounded schema name: %s\n", schema.c_str());
 
-    // A very special-case C++ comparison.
-    // This compares two NumberNodes, by their numeric value.
-    // Hard-coded in C++ for speed. (well, and for convenience ...)
-    if (0 == schema.compare("c++:greater"))
-    {
-        LinkPtr ll(LinkCast(args));
-        Handle h1(ll->getOutgoingAtom(0));
-        Handle h2(ll->getOutgoingAtom(1));
-        if (NUMBER_NODE != h1->getType() or NUMBER_NODE != h2->getType())
-            throw RuntimeException(TRACE_INFO,
-                "Expecting c++:greater arguments to be NumberNode's!");
-        const std::string& s1 = NodeCast(h1)->getName();
-        const std::string& s2 = NodeCast(h2)->getName();
-        double v1 = atof(s1.c_str());
-        double v2 = atof(s2.c_str());
-        if (v1 > v2)
-            return TruthValue::TRUE_TV();
-        else
-            return TruthValue::FALSE_TV();
-    }
+	// A very special-case C++ comparison.
+	// This compares two NumberNodes, by their numeric value.
+	// Hard-coded in C++ for speed. (well, and for convenience ...)
+	if (0 == schema.compare("c++:greater"))
+	{
+		LinkPtr ll(LinkCast(args));
+		Handle h1(ll->getOutgoingAtom(0));
+		Handle h2(ll->getOutgoingAtom(1));
+		if (NUMBER_NODE != h1->getType() or NUMBER_NODE != h2->getType())
+			throw RuntimeException(TRACE_INFO,
+			     "Expecting c++:greater arguments to be NumberNode's!");
+		const std::string& s1 = NodeCast(h1)->getName();
+		const std::string& s2 = NodeCast(h2)->getName();
+		double v1 = atof(s1.c_str());
+		double v2 = atof(s2.c_str());
+		if (v1 > v2)
+			return TruthValue::TRUE_TV();
+		else
+			return TruthValue::FALSE_TV();
+	}
 
-    // A very special-case C++ comparison.
-    // This compares a set of atoms, verifying that they are all different.
-    // Hard-coded in C++ for speed. (well, and for convenience ...)
-    if (0 == schema.compare("c++:exclusive"))
-    {
-        LinkPtr ll(LinkCast(args));
-        Arity sz = ll->getArity();
-        for (Arity i=0; i<sz-1; i++) {
-            Handle h1(ll->getOutgoingAtom(i));
-            for (Arity j=i+1; j<sz; j++) {
-                Handle h2(ll->getOutgoingAtom(j));
-                if (h1 == h2) return TruthValue::FALSE_TV();
-            }
-        }
-        return TruthValue::TRUE_TV();
-    }
+	// A very special-case C++ comparison.
+	// This compares a set of atoms, verifying that they are all different.
+	// Hard-coded in C++ for speed. (well, and for convenience ...)
+	if (0 == schema.compare("c++:exclusive"))
+	{
+		LinkPtr ll(LinkCast(args));
+		Arity sz = ll->getArity();
+		for (Arity i=0; i<sz-1; i++) {
+			Handle h1(ll->getOutgoingAtom(i));
+			for (Arity j=i+1; j<sz; j++) {
+				Handle h2(ll->getOutgoingAtom(j));
+				if (h1 == h2) return TruthValue::FALSE_TV();
+			}
+		}
+		return TruthValue::TRUE_TV();
+	}
 
-    // At this point, we only run scheme and python schemas.
-    if (0 == schema.compare(0,4,"scm:", 4))
-    {
+	// At this point, we only run scheme and python schemas.
+	if (0 == schema.compare(0,4,"scm:", 4))
+	{
 #ifdef HAVE_GUILE
-        // Be friendly, and strip leading white-space, if any.
-        size_t pos = 4;
-        while (' ' == schema[pos]) pos++;
+		// Be friendly, and strip leading white-space, if any.
+		size_t pos = 4;
+		while (' ' == schema[pos]) pos++;
 
-        SchemeEval* applier = get_evaluator(as);
-        return applier->apply_tv(schema.substr(pos), args);
+		SchemeEval* applier = get_evaluator(as);
+		return applier->apply_tv(schema.substr(pos), args);
 #else
-        throw RuntimeException(TRACE_INFO,
-             "Cannot evaluate scheme GroundedPredicateNode!");
+		throw RuntimeException(TRACE_INFO,
+			 "Cannot evaluate scheme GroundedPredicateNode!");
 #endif /* HAVE_GUILE */
-    }
+	}
 
-    if (0 == schema.compare(0, 3,"py:", 3))
-    {
+	if (0 == schema.compare(0, 3,"py:", 3))
+	{
 #ifdef HAVE_CYTHON
-        // Be friendly, and strip leading white-space, if any.
-        size_t pos = 3;
-        while (' ' == schema[pos]) pos++;
+		// Be friendly, and strip leading white-space, if any.
+		size_t pos = 3;
+		while (' ' == schema[pos]) pos++;
 
-        PythonEval &applier = PythonEval::instance();
-        // std::string rc = applier.apply(schema.substr(pos), args);
-        // if (rc.compare("None") or rc.compare("False")) return false;
-        return applier.apply_tv(schema.substr(pos), args);
+		PythonEval &applier = PythonEval::instance();
+		// std::string rc = applier.apply(schema.substr(pos), args);
+		// if (rc.compare("None") or rc.compare("False")) return false;
+		return applier.apply_tv(schema.substr(pos), args);
 #else
-        throw RuntimeException(TRACE_INFO,
-             "Cannot evaluate python GroundedPredicateNode!");
+		throw RuntimeException(TRACE_INFO,
+			 "Cannot evaluate python GroundedPredicateNode!");
 #endif /* HAVE_CYTHON */
-    }
+	}
 
-    // Unkown proceedure type.
-    throw RuntimeException(TRACE_INFO,
-        "Cannot evaluate unknown GroundedPredicateNode!");
+	// Unkown proceedure type.
+	throw RuntimeException(TRACE_INFO,
+	     "Cannot evaluate unknown GroundedPredicateNode!");
 }
