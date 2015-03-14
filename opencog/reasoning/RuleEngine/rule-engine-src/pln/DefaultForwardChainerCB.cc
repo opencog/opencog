2accb9e9a0112f8a2fe56255e91f4329bed27332
Linas Vepstas <linasvepstas@gmail.com>
2015-03-13 22:27:11 -0500

    Remove pointless use of scheme

--- opencog/reasoning/RuleEngine/rule-engine-src/pln/DefaultForwardChainerCB.cc
+++ opencog/reasoning/RuleEngine/rule-engine-src/pln/DefaultForwardChainerCB.cc
@@ -25,17 +25,17 @@
 #include "PLNCommons.h"
 
 #include <opencog/query/PatternMatch.h>
-#include <opencog/guile/SchemeEval.h>
-#include <opencog/guile/SchemeSmob.h>
 
 DefaultForwardChainerCB::DefaultForwardChainerCB(AtomSpace* as) :
-		ForwardChainerCallBack(as) {
+		ForwardChainerCallBack(as)
+{
 	as_ = as;
 	fcim_ = new ForwardChainInputMatchCB(as);
 	fcpm_ = new ForwardChainPatternMatchCB(as);
 }
 
-DefaultForwardChainerCB::~DefaultForwardChainerCB() {
+DefaultForwardChainerCB::~DefaultForwardChainerCB()
+{
 	delete fcim_;
 	delete fcpm_;
 }
@@ -46,12 +46,11 @@ vector<Rule*> DefaultForwardChainerCB::choose_rule(FCMemory& fcmem)
 {
 	//create temporary atomspace and copy target
 	AtomSpace rule_atomspace;
-	SchemeEval *sc = get_evaluator(&rule_atomspace);
 	Handle target = fcmem.get_cur_target();
 	if (target == Handle::UNDEFINED or NodeCast(target))
 		throw InvalidParamException(TRACE_INFO,
 				"Needs a target atom of type LINK");
-	sc->eval(SchemeSmob::to_string(target));
+	as_->addAtom(target);
 	//create bindlink with target as an implicant
 	PLNCommons pc(&rule_atomspace);
 	Handle target_cpy = pc.replace_nodes_with_varnode(target, NODE);
@@ -59,7 +58,7 @@ vector<Rule*> DefaultForwardChainerCB::choose_rule(FCMemory& fcmem)
 	//copy rules to the temporary atomspace
 	vector<Rule*> rules = fcmem.get_rules();
 	for (Rule* r : rules)
-		sc->eval(SchemeSmob::to_string(r->get_handle()));
+		as_->addAtom(r->get_handle());
 	//pattern match
 	DefaultImplicator imp(&rule_atomspace);
 	PatternMatch pm;
@@ -92,10 +91,9 @@ vector<Rule*> DefaultForwardChainerCB::choose_rule(FCMemory& fcmem)
 		}
 	}
 	//transfer back bindlinks to main atomspace and  get their handle
-	SchemeEval *scm = get_evaluator(as_);
 	HandleSeq copied_back;
 	for (Handle h : bindlinks)
-		copied_back.push_back(scm->eval_h(SchemeSmob::to_string(h)));
+		copied_back.push_back(as_->addAtom(h));
 	//find the rules containing the bindLink in copied_back
 	vector<Rule*> matched_rules;
 	for (Rule* r : rules)
