3c957d34323e1ea2e968b25618fea5098072baba
Misgana Bayetta <misgana.bayetta@gmail.com>
2015-03-16 15:12:28 +0800

    Indentation and copyright

--- opencog/reasoning/RuleEngine/rule-engine-src/JsonicControlPolicyParamLoader.h
+++ opencog/reasoning/RuleEngine/rule-engine-src/JsonicControlPolicyParamLoader.h
@@ -1,10 +1,25 @@
 /*
- * JsonicControlPolicyLoader.h
+ * JsonicControlPolicyParamLoader.h
  *
- *  Created on: 29 Dec, 2014
- *      Author: misgana
+ * Copyright (C) 2015 Misgana Bayetta
+ *
+ * Author: Misgana Bayetta <misgana.bayetta@gmail.com>   2015
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 #ifndef JSONICCONTROLPOLICYLOADER_H_
 #define JSONICCONTROLPOLICYLOADER_H_
 
@@ -16,26 +31,26 @@
 using namespace opencog;
 using namespace json_spirit;
 
-class JsonicControlPolicyParamLoader:public virtual ControlPolicyParamLoader {
+class JsonicControlPolicyParamLoader: public virtual ControlPolicyParamLoader {
 private:
-	Rule* cur_read_rule_ = NULL;
-	map<Rule*,vector<string>> rule_mutex_map_;
-	void read_json(const Value &v, int level = -1);
-	void read_array(const Value &v, int level);
-	void read_obj(const Value &v, int level);
-	void read_null(const Value &v, int level);
-	template <typename T> void read_primitive(const Value &v,int level);
-	/**
-	 * sets the disjunct rules
-	 */
-	void set_disjunct_rules(void);
-	Rule* get_rule(string& name);
-	const string load_json_file_relative(const string& filename,
-	            vector<string> search_paths = { });
+    Rule* cur_read_rule_ = NULL;
+    map<Rule*, vector<string>> rule_mutex_map_;
+    void read_json(const Value &v, int level = -1);
+    void read_array(const Value &v, int level);
+    void read_obj(const Value &v, int level);
+    void read_null(const Value &v, int level);
+    template<typename T> void read_primitive(const Value &v, int level);
+    /**
+     * sets the disjunct rules
+     */
+    void set_disjunct_rules(void);
+    Rule* get_rule(string& name);
+    const string load_json_file_relative(const string& filename,
+            vector<string> search_paths = { });
 public:
-	JsonicControlPolicyParamLoader(AtomSpace* as, string conf_path);
-	virtual ~JsonicControlPolicyParamLoader();
-	virtual void load_config();
+    JsonicControlPolicyParamLoader(AtomSpace* as, string conf_path);
+    virtual ~JsonicControlPolicyParamLoader();
+    virtual void load_config();
 };
 
 #endif /* JSONICCONTROLPOLICYLOADER_H_ */
