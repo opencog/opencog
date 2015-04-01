4658f978101610f8216a7c2ffe74238d95e4d3b7
Linas Vepstas <linasvepstas@gmail.com>
2015-04-01 11:22:58 -0500

    Add a new variable declaration class

---
+++ opencog/atoms/bind/VariableList.cc
@@ -0,0 +1,225 @@
+/*
+ * VariableList.cc
+ *
+ * Copyright (C) 2009, 2014, 2015 Linas Vepstas
+ *
+ * Author: Linas Vepstas <linasvepstas@gmail.com>  January 2009
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the
+ * exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <opencog/atomspace/ClassServer.h>
+#include <opencog/atoms/TypeNode.h>
+
+#include "VariableList.h"
+
+using namespace opencog;
+
+void VariableList::validate_vardecl(const HandleSeq& oset)
+{
+	for (Handle h: oset)
+	{
+		Type t = h->getType();
+		if (VARIABLE_NODE == t)
+		{
+			_varset.insert(h);
+		}
+		else if (TYPED_VARIABLE_LINK == t)
+		{
+			if (get_vartype(h, _varset, _typemap))
+				throw InvalidParamException(TRACE_INFO,
+					"Don't understand the TypedVariableLink");
+		}
+		else
+			throw InvalidParamException(TRACE_INFO,
+				"Expected a VariableNode or a TypedVariableLink");
+	}
+}
+
+VariableList::VariableList(const HandleSeq& oset,
+                       TruthValuePtr tv, AttentionValuePtr av)
+	: Link(VARIABLE_LIST, oset, tv, av)
+{
+	validate_vardecl(oset);
+}
+
+VariableList::VariableList(Type t, const HandleSeq& oset,
+                       TruthValuePtr tv, AttentionValuePtr av)
+	: Link(t, oset, tv, av)
+{
+	// derived classes have a different initialization order
+	if (VARIABLE_LIST != t) return;
+	validate_vardecl(oset);
+}
+
+VariableList::VariableList(Link &l)
+	: Link(l)
+{
+	// Type must be as expected
+	Type tscope = l.getType();
+	if (not classserver().isA(tscope, VARIABLE_LIST))
+	{
+		const std::string& tname = classserver().getTypeName(tscope);
+		throw InvalidParamException(TRACE_INFO,
+			"Expecting a VariableList, got %s", tname.c_str());
+	}
+
+	validate_vardecl(l.getOutgoingSet());
+}
+
+/* ================================================================= */
+typedef std::pair<Handle, const std::set<Type> > ATPair;
+
+/**
+ * Extract the variable type(s) from a TypedVariableLink
+ *
+ * The call is expecting htypelink to point to one of the two
+ * following structures:
+ *
+ *    TypedVariableLink
+ *       VariableNode "$some_var_name"
+ *       TypeNode  "ConceptNode"
+ *
+ * or
+ *
+ *    TypedVariableLink
+ *       VariableNode "$some_var_name"
+ *       TypeChoice
+ *          TypeNode  "ConceptNode"
+ *          TypeNode  "NumberNode"
+ *          TypeNode  "WordNode"
+ *
+ * In either case, the variable itself is appended to "vset",
+ * and the list of allowed types are associated with the variable
+ * via the map "typemap".
+ */
+int VariableList::get_vartype(const Handle& htypelink,
+                            std::set<Handle> &vset,
+                            VariableTypeMap &typemap)
+{
+	const std::vector<Handle>& oset = LinkCast(htypelink)->getOutgoingSet();
+	if (2 != oset.size())
+	{
+		logger().warn("%s: TypedVariableLink has wrong size",
+		              __FUNCTION__);
+		return 1;
+	}
+
+	Handle varname = oset[0];
+	Handle vartype = oset[1];
+
+	// The vartype is either a single type name, or a list of typenames.
+	Type t = vartype->getType();
+	if (TYPE_NODE == t)
+	{
+		Type vt = TypeNodeCast(vartype)->getValue();
+		std::set<Type> ts = {vt};
+		typemap.insert(ATPair(varname, ts));
+		vset.insert(varname);
+	}
+	else if (TYPE_CHOICE == t)
+	{
+		std::set<Type> ts;
+
+		const std::vector<Handle>& tset = LinkCast(vartype)->getOutgoingSet();
+		size_t tss = tset.size();
+		for (size_t i=0; i<tss; i++)
+		{
+			Handle h(tset[i]);
+			Type var_type = h->getType();
+			if (TYPE_NODE != var_type)
+			{
+				logger().warn("%s: VariableChoiceLink has unexpected content:\n"
+				              "Expected TypeNode, got %s",
+				              __FUNCTION__,
+				              classserver().getTypeName(h->getType()).c_str());
+				return 3;
+			}
+			Type vt = TypeNodeCast(h)->getValue();
+			ts.insert(vt);
+		}
+
+		typemap.insert(ATPair(varname,ts));
+		vset.insert(varname);
+	}
+	else
+	{
+		logger().warn("%s: Unexpected contents in TypedVariableLink\n"
+				        "Expected TypeNode or TypeChoice, got %s",
+		              __FUNCTION__,
+		              classserver().getTypeName(t).c_str());
+		return 2;
+	}
+
+	return 0;
+}
+
+/* ================================================================= */
+/**
+ * Validate variable declarations for syntax correctness.
+ *
+ * This will check to make sure that a set of variable declarations are
+ * of a reasonable form. Thus, for example, a structure similar to the
+ * below is expected.
+ *
+ *       VariableList
+ *          VariableNode "$var0"
+ *          VariableNode "$var1"
+ *          TypedVariableLink
+ *             VariableNode "$var2"
+ *             TypeNode  "ConceptNode"
+ *          TypedVariableLink
+ *             VariableNode "$var3"
+ *             TypeChoice
+ *                 TypeNode  "PredicateNode"
+ *                 TypeNode  "GroundedPredicateNode"
+ *
+ * As a side-effect, the variables and type restrictions are unpacked.
+ */
+
+void VariableList::validate_vardecl(const Handle& hdecls)
+{
+	// Expecting the declaration list to be either a single
+	// variable, or a list of variable declarations
+	Type tdecls = hdecls->getType();
+	if ((VARIABLE_NODE == tdecls) or
+	    NodeCast(hdecls)) // allow *any* node as a variable
+	{
+		_varset.insert(hdecls);
+	}
+	else if (TYPED_VARIABLE_LINK == tdecls)
+	{
+		if (get_vartype(hdecls, _varset, _typemap))
+			throw InvalidParamException(TRACE_INFO,
+				"Cannot understand the typed variable definition");
+	}
+	else if (VARIABLE_LIST == tdecls or LIST_LINK == tdecls)
+	{
+		// The list of variable declarations should be .. a list of
+		// variables! Make sure its as expected.
+		const std::vector<Handle>& dset = LinkCast(hdecls)->getOutgoingSet();
+		validate_vardecl(dset);
+	}
+	else
+	{
+		throw InvalidParamException(TRACE_INFO,
+			"Expected a VariableList holding variable declarations");
+	}
+}
+
+/* ===================== END OF FILE ===================== */
---
+++ opencog/atoms/bind/VariableList.h
@@ -0,0 +1,94 @@
+/*
+ * opencog/atoms/VariableList.h
+ *
+ * Copyright (C) 2015 Linas Vepstas
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_VARIABLE_LIST_H
+#define _OPENCOG_VARIABLE_LIST_H
+
+#include <map>
+
+#include <opencog/atomspace/AtomSpace.h>
+#include <opencog/atomspace/Link.h>
+
+namespace opencog
+{
+/** \addtogroup grp_atomspace
+ *  @{
+ *
+ * Experimental VariableList class. This is a rough sketch for how things
+ * like this might be done. It is not necessarily a good idea, and might
+ * be replaced by something completely different, someday ...
+ */
+
+typedef std::map<Handle, const std::set<Type> > VariableTypeMap;
+
+class PatternMatch;
+
+class VariableList : public Link
+{
+   friend class PatternMatch;
+protected:
+	/// Unbundled variables and types for them.
+	/// _typemap is the (possibly empty) list of restrictions on
+	/// the variable types. Set by validate_vars()
+	std::set<Handle> _varset;
+	VariableTypeMap _typemap;
+
+	// See VariableList.cc for comments
+	static int get_vartype(const Handle&,
+	                       std::set<Handle>&,
+	                       VariableTypeMap&);
+
+	// Validate the variable decls
+	void validate_vardecl(const Handle&);
+	void validate_vardecl(const HandleSeq&);
+
+	VariableList(Type, const HandleSeq&,
+	           TruthValuePtr tv = TruthValue::DEFAULT_TV(),
+	           AttentionValuePtr av = AttentionValue::DEFAULT_AV());
+
+public:
+	VariableList(const HandleSeq& vardecls,
+	           TruthValuePtr tv = TruthValue::DEFAULT_TV(),
+	           AttentionValuePtr av = AttentionValue::DEFAULT_AV());
+
+	VariableList(const Handle& varcdecls,
+	           TruthValuePtr tv = TruthValue::DEFAULT_TV(),
+	           AttentionValuePtr av = AttentionValue::DEFAULT_AV());
+
+	VariableList(Link &l);
+
+	const VariableTypeMap& get_typemap(void) { return _typemap; }
+};
+
+typedef std::shared_ptr<VariableList> VariableListPtr;
+static inline VariableListPtr VariableListCast(const Handle& h)
+	{ AtomPtr a(h); return std::dynamic_pointer_cast<VariableList>(a); }
+static inline VariableListPtr VariableListCast(AtomPtr a)
+	{ return std::dynamic_pointer_cast<VariableList>(a); }
+
+// XXX temporary hack ...
+#define createVariableList std::make_shared<VariableList>
+
+/** @}*/
+}
+
+#endif // _OPENCOG_VARIABLE_LIST_H
