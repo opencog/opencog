fb1485ab79b40a2e5496eb0edd1084a1edaa232b
Curtis Faith <curtis.m.faith@gmail.com>
2015-04-09 12:43:43 -0400

    Added execute_atom and evaluate_atom

--- opencog/cython/opencog/bindlink.pyx
+++ opencog/cython/opencog/bindlink.pyx
@@ -1,7 +1,6 @@
-# Seems that TruthValue is not defined in an pxd file. Why not ???
-# from opencog.atomspace cimport AtomSpace, Handle, TruthValue
-from opencog.atomspace cimport AtomSpace, Handle
+from opencog.atomspace cimport Atom, AtomSpace, Handle, TruthValue
 from opencog.atomspace cimport cHandle, cAtomSpace, cTruthValue
+from opencog.atomspace cimport tv_ptr, strength_t, count_t
 from cython.operator cimport dereference as deref
 
 
@@ -34,10 +33,26 @@ def pln_bindlink(AtomSpace atomspace, Handle handle):
     cdef Handle result = Handle(c_result.value())
     return result
 
-# The below will not compile for some reason, I can't figure it out.
-#
-#def satisfaction_link(AtomSpace atomspace, Handle handle):
-#    cdef cTruthValue c_result = c_satisfaction_link(atomspace.atomspace,
-#                                                deref(handle.h))
-#    cdef TruthValue result = TruthValue(c_result.value())
-#    return result
+def satisfaction_link(AtomSpace atomspace, Handle handle):
+    cdef tv_ptr result_tv_ptr = c_satisfaction_link(atomspace.atomspace,
+                                                 deref(handle.h))
+    cdef cTruthValue* result_tv = result_tv_ptr.get()
+    cdef strength_t strength = deref(result_tv).getMean()
+    cdef count_t count = deref(result_tv).getCount()
+    return TruthValue(strength, count)
+
+def execute_atom(AtomSpace atomspace, Atom atom):
+    cdef Handle atom_h = atom.h
+    cdef cHandle result_c_handle = c_execute_atom(atomspace.atomspace,
+                                                  deref(atom_h.h))
+    cdef result_handle = Handle(result_c_handle.value())
+    return Atom(result_handle, atomspace)
+
+def evaluate_atom(AtomSpace atomspace, Atom atom):
+    cdef Handle atom_h = atom.h
+    cdef tv_ptr result_tv_ptr = c_evaluate_atom(atomspace.atomspace,
+                                                deref(atom_h.h))
+    cdef cTruthValue* result_tv = result_tv_ptr.get()
+    cdef strength_t strength = deref(result_tv).getMean()
+    cdef count_t count = deref(result_tv).getCount()
+    return TruthValue(strength, count)
