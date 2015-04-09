7c04775b1513c6a0cb7dbff70c60857b412733ea
Linas Vepstas <linasvepstas@gmail.com>
2015-04-09 09:33:52 -0500

    Extend the arcana unit test

--- tests/query/arcana-repeat.scm
+++ tests/query/arcana-repeat.scm
@@ -2,6 +2,89 @@
 ; Unit testing for a strange repeated-clause
 ;
 ;;; Populate the atomspace with two things
+
+; Two parts, both identical
+(ImplicationLink
+	(ListLink
+		(EvaluationLink
+			(PredicateNode "this way")
+			(ListLink
+				(ConceptNode "this one")
+				(ConceptNode "thing two")
+			)
+		)
+	)
+	(ListLink
+		(EvaluationLink
+			(PredicateNode "this way")
+			(ListLink
+				(ConceptNode "this one")
+				(ConceptNode "thing two")
+			)
+		)
+	)
+)
+
+; Three parts, all three identical
+(ImplicationLink
+	(ListLink
+		(EvaluationLink
+			(PredicateNode "this way")
+			(ListLink
+				(ConceptNode "this one")
+				(ConceptNode "thing two")
+			)
+		)
+	)
+	(ListLink
+		(EvaluationLink
+			(PredicateNode "this way")
+			(ListLink
+				(ConceptNode "this one")
+				(ConceptNode "thing two")
+			)
+		)
+	)
+	(ListLink
+		(EvaluationLink
+			(PredicateNode "this way")
+			(ListLink
+				(ConceptNode "this one")
+				(ConceptNode "thing two")
+			)
+		)
+	)
+)
+
+; One part, three identical sub-parts
+(ImplicationLink
+	(ListLink
+		(EvaluationLink
+			(PredicateNode "this way")
+			(ListLink
+				(ConceptNode "this one")
+				(ConceptNode "thing two")
+			)
+		)
+		(EvaluationLink
+			(PredicateNode "this way")
+			(ListLink
+				(ConceptNode "this one")
+				(ConceptNode "thing two")
+			)
+		)
+		(EvaluationLink
+			(PredicateNode "this way")
+			(ListLink
+				(ConceptNode "this one")
+				(ConceptNode "thing two")
+			)
+		)
+	)
+)
+
+;; Two parts, one different than the other, but having a common
+;; subexpression.
 (ImplicationLink
 	(ListLink
 		(EvaluationLink
@@ -23,6 +106,8 @@
 	)
 )
 
+;; Three parts, all three different, but each having a common
+;; subexpression.
 (ImplicationLink
 	(ListLink
 		(EvaluationLink
@@ -33,7 +118,16 @@
 			)
 		)
 	)
-	(ListLink
+	(MemberLink
+		(EvaluationLink
+			(PredicateNode "this way")
+			(ListLink
+				(ConceptNode "this one")
+				(ConceptNode "thing two")
+			)
+		)
+	)
+	(SubsetLink
 		(EvaluationLink
 			(PredicateNode "this way")
 			(ListLink
@@ -74,6 +168,7 @@
 		)
 	)
 )
+
 ;;; Note that the evaluationLink is repeated twice, inside of
 ;;; two different links
 (define (repeat-different)
@@ -91,6 +186,121 @@
 					)
 				)
 				(MemberLink
+					(EvaluationLink
+						(PredicateNode "this way")
+						(ListLink
+							(VariableNode "$x")
+							(ConceptNode "thing two")
+						)
+					)
+				)
+			)
+			(VariableNode "$x")
+		)
+	)
+)
+
+;;; Note that the evaluationLink is repeated three times, inside of
+;;; three different links
+(define (repeat-diff-thrice)
+	(BindLink
+		(VariableNode "$x")
+		(ImplicationLink
+			(ImplicationLink
+				(ListLink
+					(EvaluationLink
+						(PredicateNode "this way")
+						(ListLink
+							(VariableNode "$x")
+							(ConceptNode "thing two")
+						)
+					)
+				)
+				(MemberLink
+					(EvaluationLink
+						(PredicateNode "this way")
+						(ListLink
+							(VariableNode "$x")
+							(ConceptNode "thing two")
+						)
+					)
+				)
+				(SubsetLink
+					(EvaluationLink
+						(PredicateNode "this way")
+						(ListLink
+							(VariableNode "$x")
+							(ConceptNode "thing two")
+						)
+					)
+				)
+			)
+			(VariableNode "$x")
+		)
+	)
+)
+
+;;; Note that the evaluationLink is repeated three times, inside of
+;;; a ListLink, each time.
+(define (repeat-thrice)
+	(BindLink
+		(VariableNode "$x")
+		(ImplicationLink
+			(ImplicationLink
+				(ListLink
+					(EvaluationLink
+						(PredicateNode "this way")
+						(ListLink
+							(VariableNode "$x")
+							(ConceptNode "thing two")
+						)
+					)
+				)
+				(ListLink
+					(EvaluationLink
+						(PredicateNode "this way")
+						(ListLink
+							(VariableNode "$x")
+							(ConceptNode "thing two")
+						)
+					)
+				)
+				(ListLink
+					(EvaluationLink
+						(PredicateNode "this way")
+						(ListLink
+							(VariableNode "$x")
+							(ConceptNode "thing two")
+						)
+					)
+				)
+			)
+			(VariableNode "$x")
+		)
+	)
+)
+;;; Note that the evaluationLink is repeated three times, inside of
+;;; just one ListLink, total.
+(define (repeat-once)
+	(BindLink
+		(VariableNode "$x")
+		(ImplicationLink
+			(ImplicationLink
+				(ListLink
+					(EvaluationLink
+						(PredicateNode "this way")
+						(ListLink
+							(VariableNode "$x")
+							(ConceptNode "thing two")
+						)
+					)
+					(EvaluationLink
+						(PredicateNode "this way")
+						(ListLink
+							(VariableNode "$x")
+							(ConceptNode "thing two")
+						)
+					)
 					(EvaluationLink
 						(PredicateNode "this way")
 						(ListLink
