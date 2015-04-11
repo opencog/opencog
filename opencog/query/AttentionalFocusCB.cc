d688e4a320860344bcd162921fa1416febe0b450
Linas Vepstas <linasvepstas@gmail.com>
2015-04-11 02:50:17 -0500

    It is now safe to remove bad AF code
    Needed a bug fix to get it all working right

--- opencog/query/AttentionalFocusCB.cc
+++ opencog/query/AttentionalFocusCB.cc
@@ -36,7 +36,7 @@ AttentionalFocusCB::AttentionalFocusCB(AtomSpace* as) :
 	DefaultPatternMatchCB(as)
 {
 	// Temporarily disable the AF mechanism during PLN development
-	_as->setAttentionalFocusBoundary(AttentionValue::MINSTI);
+	// _as->setAttentionalFocusBoundary(AttentionValue::MINSTI);
 }
 
 bool AttentionalFocusCB::node_match(const Handle& node1, const Handle& node2)
@@ -89,80 +89,4 @@ IncomingSet AttentionalFocusCB::get_incoming_set(const Handle& h)
 	std::sort(filtered_set.begin(), filtered_set.end(), compare_sti);
 
 	return filtered_set;
-}
-
-// XXX FIXME, this is a bad-cut-n-paste job of the DefaultCB,
-// with some unclear modifications..
-bool AttentionalFocusCB::initiate_search(PatternMatchEngine *pme,
-                                         const std::set<Handle> &vars,
-                                         const std::vector<Handle> &clauses)
-{
-	_search_fail = false;
-
-	bool found = neighbor_search(pme, vars, clauses);
-	if (found) return true;
-	if (not _search_fail) return false;
-
-	// If we are here, then we could not find a clause at which to start,
-	// as, apparently, the clauses consist entirely of variables!! So,
-	// basically, we must search the entire!! atomspace, in this case.
-	// Yes, this hurts.
-	_root = clauses[0];
-	_starter_term = _root;
-
-	dbgprt("Start term is: %s\n", _starter_term->toShortString().c_str());
-
-	// XXX TODO -- as a performance optimization, we should try all
-	// the different clauses, and find the one with the smallest number
-	// of atoms of that type, or otherwise try to find a small ("thin")
-	// incoming set to search over.
-
-	// Plunge into the deep end - start looking at all viable
-	// candidates in the AtomSpace.
-	HandleSeq handle_set;
-	_as->getHandleSetInAttentionalFocus(back_inserter(handle_set));
-// xxxxxxxxxxxxxx here xxxxxxxxxxx
-
-	// WARNING: if there's nothing in the attentional focus then get
-	// the whole atomspace  ... XXX This cannot possibly be right!!
-	// If nothig is in attentional focus, then certainly, the whole
-	// atomspace should NOT be searched!! XXX So this is utterly wrong!
-	// However, both RuleUTest and ForwardChainerUTest fail unless
-	// we search the whole atomspace.  So that means that these two
-	// unit tests ae failing to correctly set up the attentional focus
-	// boundary!  Boooo! the tests are broken, they need to be fixed...
-	if (handle_set.empty())
-		_as->getHandlesByType(back_inserter(handle_set), ATOM, true);
-
-#if WHAT_THE_HUHH_THIS_CANT_BE_RIGHT
-	// Get the set of types of the potential candidates
-	// XXX FIXME ... this cannot be right; when-ever would the
-	// first clause just be a variable, all by itself??
-	std::set<Type> ptypes;
-	if (_root->getType() == VARIABLE_NODE)
-		ptypes = _type_restrictions->at(_root);
-
-
-	// Filter by variable types
-	if (not ptypes.empty()) {
-		auto it = remove_if(handle_set.begin(), handle_set.end(),
-		                    [&ptypes](const Handle& h) {
-			                    return ptypes.find(h->getType()) == ptypes.end();
-		                    });
-		handle_set.erase(it, handle_set.end());
-	}
-#endif
-
-#ifdef DEBUG
-	size_t i = 0;
-#endif
-	for (const Handle& h : handle_set)
-	{
-		dbgprt("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
-		dbgprt("Loop candidate (%lu/%lu): %s\n", ++i, handle_set.size(),
-		       h->toShortString().c_str());
-		bool rc = pme->explore_neighborhood(_root, _starter_term, h);
-		if (rc) return true;
-	}
-	return false;
 }
