fb7b16aa10d02f02c63177af3683ce06a1d86f43
Amen Belayneh <amenbelayneh@gmail.com>
2015-04-14 18:20:19 +0800

    a tested dependency installation script for fedora
    a single dependency not found, that could be the reason
    for the failure of tests.

--- scripts/install-fedora-dependencies.sh
+++ scripts/install-fedora-dependencies.sh
@@ -1,177 +1,85 @@
-# trap errors 
+#!/bin/bash
+#
+# This script is used for installing the dependencies required for
+# building opencog on fedora.The script has been tested using docker
+# image fedora:21.
+
+# trap errors
 set -e
 
-PATH_PREFIX=/usr/local
-CURRENT_DIR=$(pwd)
-
-HOST_SOURCE_BRANCH=$CURRENT_DIR/opencog/src
-HOST_BUILD_DIR=$CURRENT_DIR/opencog/build
-
+# Environment Variables
 SELF_NAME=$(basename $0)
-TOOL_NAME=octool
 
-# get processor count
-PROCESSORS=$(grep "^processor" /proc/cpuinfo | wc -l)
-
-# make job count equal to processor count
-MAKE_JOBS=$(($PROCESSORS+0))
-
-VERBOSE="-v"				#for mount, umount, rm, etc.
-QUIET="" 				#for apt-get
-
-
-PACKAGES_TOOLS="	
-		squashfs-tools \
-		genisoimage \
-		"
-
-PACKAGES_FETCH="	
+# Some tools
+PACKAGES_TOOLS="
 		git \
+		python-pip \
 			"
 
-PACKAGE_GROUPS_BUILD="
-			
-			"
-
-PACKAGES_BUILD="	
+# Packages for building opencog
+# FIXME fedora version of libjson-spirit-dev on ubuntu not found yet.
+PACKAGES_BUILD="
 		make \
-		automake \
 		gcc \
 		gcc-c++ \
 		cmake \
 		cxxtest \
 		rlwrap \
 		guile-devel \
-		binutils-devel \
 		libicu-devel \
 		bzip2-devel \
 		Cython \
 		python-devel \
 		python-zmq \
-		python-matplotlib \
-		python-nose \
-		numpy \
-		scipy \
-		python-mock \
-		python-simplejson \
-		pyparsing \
-		python-dateutil \
-		python-six \
-		python-tornado \
-		python-wsgiref \
 		boost-devel \
-                zeromq-devel \
-                tbb-devel \
-                gsl-devel \
-                unixODBC-devel \
-                xerces-c-devel \
+		zeromq-devel \
+		tbb-devel \
+		binutils-devel \
+		gsl-devel \
+		unixODBC-devel \
+		xerces-c-devel \
 		uuid-devel \
 		protobuf-c \
-                protobuf-devel \
 		protobuf-compiler
-                gnutls-devel \
-                curl \
-                expat-devel \
-
+		gnutls-devel \
+		expat-devel \
 		SDL_gfx-devel \
 		openssl-devel \
 		tcl-devel \
 		tcsh \
 		freetype-devel \
 		atlas-devel \
-		libgfortran gcc-gfortran \
+		gcc-gfortran \
 		"
-		#liblua5.1-0-dev \
-		#libxmlrpc-c3-dev \
-		#python-flask \
-		#python-flask-restful \
 
-PACKAGES_RUNTIME="	
+# Packages required for integrating opencog with other services
+PACKAGES_RUNTIME="
 		unixODBC \
 		postgresql-odbc \
 		postgresql-devel \
 		"
 
-# SECTION
+# Path to requirements file
+PACKAGES_PYTHON="../opencog/python/requirements.txt"
+
+# Template for messages printed.
 message() {
 echo -e "\e[1;34m[$SELF_NAME] $MESSAGE\e[0m"
 }
 
-PACKAGE_TYPE=$DEFAULT_PACKAGE_TYPE
-
-exit_trap() {
-if [ "$SELF_NAME" != "$TOOL_NAME" ] ; then
-  MESSAGE="Exiting $SELF_NAME normally..." ; message
-fi
-}
-
-quit_trap() {
-if [ "$SELF_NAME" != "$TOOL_NAME" ] ; then
-  MESSAGE="Exiting $SELF_NAME by request..." ; message
-fi
-}
-
-error_trap() {
-if [ "$SELF_NAME" != "$TOOL_NAME" ] ; then
-  MESSAGE="Error trapped while running $SELF_NAME." ; message
-  MESSAGE="Cleanup will run after debug." ; message
-  debug
-fi
-}
-
-# execute error_trap on signal ERR
-trap error_trap ERR 
-# execute exit_trap on signal EXIT
-trap exit_trap  EXIT
-# execute quit_trap on signal INT HUP QUIT TERM
-trap quit_trap  INT HUP QUIT TERM
-
-# SECTION 4: Function Definitions
-add_repositories() {
-MESSAGE="Adding/Updating software repositories..." ; message
-# Update package database and upgrade
-  yum distro-sync
-}
-
-
+# Function for installing all required dependenceis for building OpenCog,
+# as well as dependencies required for running opencog with other services.
 install_dependencies() {
+MESSAGE="Updating Package db...." ; message
+yum updateinfo
+
 MESSAGE="Installing OpenCog build dependencies...." ; message
-if !  yum install $QUIET $PACKAGES_BUILD $PACKAGES_RUNTIME $PACKAGES_FETCH; then
+if !  (yum -y install $PACKAGES_BUILD $PACKAGES_RUNTIME $PACKAGES_TOOLS && \
+		pip install -U -r $PACKAGES_PYTHON); then
   MESSAGE="Error installing some of dependencies... :( :("  ; message
   exit 1
 fi
 }
 
-update_opencog_source() { 
-yum install $QUIET $PACKAGES_FETCH ; 
-  echo -n
-OPENCOG_SOURCE_DIR=$LIVE_SOURCE_BRANCH
-mkdir -p $OPENCOG_SOURCE_DIR || true
-if [ ! "$(ls -A $OPENCOG_SOURCE_DIR/.git)" ]; then
-  MESSAGE="Fetching OpenCog source at $OPENCOG_SOURCE_DIR..." ; message
-  git clone https://github.com/opencog/opencog $OPENCOG_SOURCE_DIR
-else
-  if [ $UPDATE_OPENCOG ] ; then
-    MESSAGE="Updating OpenCog source at $OPENCOG_SOURCE_DIR..." ; message
-    cd $OPENCOG_SOURCE_DIR
-    git pull 
-    cd -
-  fi
-fi
-}
-
-build_opencog() {
-mkdir -p -v $HOST_BUILD_DIR || true
-cd $HOST_BUILD_DIR
-MESSAGE="cmake $HOST_SOURCE_BRANCH" ; message
-cmake $LIVE_SOURCE_BRANCH
-MESSAGE="make -j$MAKE_JOBS" ; message
-make -j$MAKE_JOBS
-make test
-}
-
-# SECTION 5: Main Program (MAIN main)
+# Main Program
 install_dependencies
-update_opencog_source
-build_opencog
-
