f157f96670f66527a08bd992d5140d76ea5b86f2
Curtis Faith <curtis.m.faith@gmail.com>
2015-04-15 13:12:30 -0400

    Only import the agent finder one time in PyRequest

--- opencog/cython/PyRequest.cc
+++ opencog/cython/PyRequest.cc
@@ -7,6 +7,8 @@
 
 using namespace opencog;
 
+static bool module_initialized = false;
+
 PyRequest::PyRequest(CogServer& cs,
                      const std::string& moduleName,
                      const std::string& className,
@@ -21,8 +23,12 @@ PyRequest::PyRequest(CogServer& cs,
     // NOTE: You need to call the import functions in each separate
     // shared library that accesses Cython defined api. If you don't
     // then you get a crash when you call an api function.
-    import_opencog__agent_finder();
-
+    if (!module_initialized)
+    {
+        import_opencog__agent_finder();
+        module_initialized = true;
+    }
+    
     // Call out to our helper module written in cython  NOTE: Cython api calls
     // defined "with gil" can be called without grabbing the GIL manually.
     _pyrequest = instantiate_request(moduleName, className, this);
