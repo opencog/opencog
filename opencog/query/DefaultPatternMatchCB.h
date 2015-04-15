a0dd50c45380c0fc923b9999321f38920c33df26
Linas Vepstas <linasvepstas@gmail.com>
2015-04-15 03:18:03 -0500

    Move large block of code out of the header file

--- opencog/query/DefaultPatternMatchCB.h
+++ opencog/query/DefaultPatternMatchCB.h
@@ -46,86 +46,9 @@ class DefaultPatternMatchCB : public virtual PatternMatchCallback
 	public:
 		DefaultPatternMatchCB(AtomSpace*);
 
-		/**
-		 * Called when a node in the template pattern needs to
-		 * be compared to a possibly matching node in the atomspace.
-		 * The first argument is a node from the pattern, and the
-		 * second is a possible solution (grounding) node from the
-		 * atomspace.
-		 *
-		 * Return true if the nodes match, else return false.
-		 * By default, the nodes must be identical.
-		 */
-		virtual bool node_match(const Handle& npat_h, const Handle& nsoln_h)
-		{
-			// If equality, then a match.
-			return npat_h == nsoln_h;
-		}
-
-		/**
-		 * Called when a variable in the template pattern
-		 * needs to be compared to a possible grounding
-		 * node in the atomspace. The first argument
-		 * is a variable from the pattern, and the second
-		 * is a possible grounding node from the atomspace.
-		 * Return true if the nodes match, else return false.
-		 */
-		virtual bool variable_match(const Handle& npat_h, const Handle& nsoln_h)
-		{
-			Type pattype = npat_h->getType();
-
-			// If the ungrounded term is not of type VariableNode, then just
-			// accept the match. This allows any kind of node types to be
-			// explicitly bound as variables.  However, the type VariableNode
-			// gets special handling, below.
-			if (pattype != VARIABLE_NODE) return true;
-
-			// If the ungrounded term is a variable, then see if there
-			// are any restrictions on the variable type.
-			// If no restrictions, we are good to go.
-			if (NULL == _type_restrictions) return true;
-
-			// If we are here, there's a restriction on the grounding type.
-			// Validate the node type, if needed.
-			VariableTypeMap::const_iterator it = _type_restrictions->find(npat_h);
-			if (it == _type_restrictions->end()) return true;
-
-			// Is the ground-atom type in our list of allowed types?
-			Type soltype = nsoln_h->getType();
-			const std::set<Type> &tset = it->second;
-			std::set<Type>::const_iterator allow = tset.find(soltype);
-			return allow != tset.end();
-		}
-
-		/**
-		 * Called when a link in the template pattern
-		 * needs to be compared to a possibly matching
-		 * link in the atomspace. The first argument
-		 * is a link from the pattern, and the second
-		 * is a possible solution link from the atomspace.
-		 * Return true if the links should be compared,
-		 * else return false.
-		 *
-		 * By default, the search continues if the link
-		 * arity and the link types match.
-		 */
-		virtual bool link_match(const LinkPtr& lpat, const LinkPtr& lsoln)
-		{
-			// If the pattern is exactly the same link as the proposed
-			// grounding, then its a perfect match. 
-			if (lpat == lsoln) return true;
-
-			// Accept all ChoiceLink's by default! We will get another shot
-			// at it when the contents of the ChoiceLink are examined.
-			Type pattype = lpat->getType();
-			if (CHOICE_LINK == pattype) return true;
-
-			if (lpat->getArity() != lsoln->getArity()) return false;
-			Type soltype = lsoln->getType();
-
-			// If types differ, no match
-			return pattype == soltype;
-		}
+		virtual bool node_match(const Handle&, const Handle&);
+		virtual bool variable_match(const Handle&, const Handle&);
+		virtual bool link_match(const LinkPtr&, const LinkPtr&);
 
 		/**
 		 * Typically called for AbsentLink
