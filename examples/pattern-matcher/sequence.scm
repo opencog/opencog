c4867ce80a8bb9cb88ceb1ced7d150e8e201763b
Linas Vepstas <linasvepstas@gmail.com>
2015-04-17 23:45:41 -0500

    Polish up the sequence demo a little bit

--- examples/pattern-matcher/sequence.scm
+++ examples/pattern-matcher/sequence.scm
@@ -1,16 +1,45 @@
 ;
 ; sequence.scm
 ;
-; Test data for the GroundedPredicateNode, which is a virtual node.
-; A sequence of these is evaluated, verifying that sequential evaluation
-; works.
+; Demonstrate using the GroundedPredicateNode to provide a simple
+; behavior sequence: i.e. a set of steps that are conditionally played
+; out, depending on whether the predicate node returns true of false.
+; There are no variables in this demo; thus, the sequence is not
+; a graph search, but is rather a straight-forward if-else sequence
+; evaluation.
+;
+; This demo also demonstrates the use of a SatisfactionLink: there is no
+; output, and no graph-rewriting occurs, thus a BindLink is not needed,
+; and the simpler SatisfactionLink is enough.
+;
+; The run this, you probably need to do this:
+;
+; OCDIR=home/home/yourname/opencog
+; export LTDL_LIBRARY_PATH=$OCDIR/build/opencog/guile:$OCDIR/build/opencog/query
+;
+; Add the following to your ~/.guile file:
+; (add-to-load-path "/home/yourname/opencog/build")
+; (add-to-load-path "/home/yourname/opencog/opencog/scm")
+; (add-to-load-path ".")
+;
+; Start guile:
+; guile
+;
+; and then load this file:
+; (load-from-path "sequence.scm")
+;
+; Then, scroll to the bottom, and some of the commented-out
+; examples.
 ;
 (use-modules (opencog))
 (use-modules (opencog query))
 
+(load-from-path "utilities.scm")
+
 (define green-light  (ConceptNode "green light"))
 (define red-light  (ConceptNode "red light"))
 
+; Counts of how many times the green and red lights were seen.
 (define num-green 0)
 (define num-red 0)
 
@@ -27,7 +56,7 @@
 
 ; Should throw an exception in all cases. Shouldn't do donuts on
 ; corn fields.
-(define (off-road)
+(define off-road
 	(SatisfactionLink
 		(VariableList)  ; no variables
 		(SequentialAndLink
@@ -44,7 +73,7 @@
 ;; Should see two green lights, and one red light, after which
 ;; the matching should stop.  There should be no exceptions or
 ;; errors when evaluating this.
-(define (traffic-lights)
+(define traffic-lights
 	(SatisfactionLink
 		(VariableList)  ; no variables
 		(SequentialAndLink
@@ -72,3 +101,20 @@
 		)
 	)
 )
+
+(define (start-again)
+	(cog-satisfy traffic-lights)
+	(simple-format #t "Went through ~A green lights and ~A  red lights\n"
+		num-green num-red)
+)
+
+;;; Try the below.  This should result in an exception being thrown.
+;;;
+; (cog-satisfy off-road)
+
+;;; The below should result in the green light being seen twice, and
+;;; the red light once, and no exceptions or errors.
+;;;
+; (start-again)
+; (start-again)
+; (start-again)
