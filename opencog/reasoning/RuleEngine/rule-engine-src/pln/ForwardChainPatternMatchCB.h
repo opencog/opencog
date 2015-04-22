a67a5169a64fb721bcf2caf199c10752182fdff3
Misgana Bayetta <misgana.bayetta@gmail.com>
2015-04-22 23:57:20 +0800

    Few modifications on ForwarchainingCallBack

--- opencog/reasoning/RuleEngine/rule-engine-src/pln/ForwardChainPatternMatchCB.h
+++ opencog/reasoning/RuleEngine/rule-engine-src/pln/ForwardChainPatternMatchCB.h
@@ -25,29 +25,33 @@
 #define FORWARDCHAINPATTERNMATCHCB_H_
 
 #include "FCMemory.h"
+#include <opencog/query/Implicator.h>
+#include <opencog/query/DefaultPatternMatchCB.h>
+#include <opencog/query/AttentionalFocusCB.h>
 
-#include <opencog/query/DefaultImplicator.h>
+namespace opencog
+{
 
-namespace opencog {
-
-class ForwardChainPatternMatchCB: public virtual PLNImplicator {
+class ForwardChainPatternMatchCB: public virtual Implicator,
+        public virtual DefaultPatternMatchCB,
+        public virtual AttentionalFocusCB {
 private:
-	AtomSpace* _as;
-	FCMemory * _fcmem;
+    AtomSpace* _as;
+    FCMemory * _fcmem;
 public:
-	ForwardChainPatternMatchCB(AtomSpace * as);
-	virtual ~ForwardChainPatternMatchCB();
-	HandleSeq get_products(void);
-	void set_fcmem(FCMemory *fcmem);
-	//the follwing callbacks are used for guiding the PM to look only the source list
-	bool node_match(const Handle& node1, const Handle& node2);
-	bool link_match(const LinkPtr& lpat, const LinkPtr& lsoln);
+    ForwardChainPatternMatchCB(AtomSpace * as);
+    virtual ~ForwardChainPatternMatchCB();
+    HandleSeq get_products(void);
+    void set_fcmem(FCMemory *fcmem);
+    //the follwing callbacks are used for guiding the PM to look only the source list
+    virtual bool node_match(const Handle& node1, const Handle& node2);
+    virtual bool link_match(const LinkPtr& lpat, const LinkPtr& lsoln);
 
-	/**
-	 * A callback handler of the Pattern matcher used to store references to new conclusion the source list
-	 */
-	bool grounding(const std::map<Handle, Handle> &var_soln,
-	               const std::map<Handle, Handle> &pred_soln);
+    /**
+     * A callback handler of the Pattern matcher used to store references to new conclusion the source list
+     */
+    virtual bool grounding(const std::map<Handle, Handle> &var_soln,
+                           const std::map<Handle, Handle> &pred_soln);
 
 };
 
