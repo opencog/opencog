dd5f652f41244b07e513241e0ae971e81a238485
Amen Belayneh <amenbelayneh@gmail.com>
2015-05-06 17:56:45 +0800

    updated archlinux installation script

--- scripts/install-archlinux-dependencies.sh
+++ scripts/install-archlinux-dependencies.sh
@@ -1,31 +1,27 @@
-# SECTION 1 Declare vars and collect environment info.
-# trap errors 
+#!/bin/bash
+#
+# This script is used for installing the dependencies required for
+# building opencog on archlinux.The script has been tested using docker
+# image base/archlinux:latest & l3iggs/archlinux
+
+# trap errors
 set -e
 
-PATH_PREFIX=/usr/local
-CURRENT_DIR=$(pwd)
-
-HOST_SOURCE_BRANCH=$CURRENT_DIR/opencog/src  
-HOST_BUILD_DIR=$CURRENT_DIR/opencog/build
-
-# SECTION 2 Assemble install info.
+# Environment Variables
 SELF_NAME=$(basename $0)
 
-# get processor count
-PROCESSORS=$(grep "^processor" /proc/cpuinfo | wc -l)
+PACKAGES_TOOLS="
+ 		git \
+		python-pip \
+		wget \
+		sudo \
+		pkg-config \
+		"
 
-# make job count equal to processor count
-MAKE_JOBS=$(($PROCESSORS+0))
-
-VERBOSE="-v"				#for mount, umount, rm, etc.
-QUIET="-q" 				
-
-PACKAGES_FETCH=" git "
-
-PACKAGES_ADMIN=" epiphany-browser "
-
+# Packages for building opencog
 PACKAGES_BUILD="
-		base-devel	
+		gcc \
+		make \
 		cmake \
 		cxxtest \
 		rlwrap \
@@ -33,134 +29,113 @@ PACKAGES_BUILD="
 		icu
 		bzip2 \
 		cython \
-		python \
-		python-pyzmq \
-		python-matplotlib \
-		python-nose \
-		python-numpy \
-		python-scipy \
-		python-mock \
-		python-simplejson \
-		python-pyparsing \
-		python-dateutil \
-		python-six \
-		python-tornado \
-		python2-pydot \
-		boost-libs \
-                zeromq \
-                intel-tbb \
-                binutils \
-                gsl \
-                unixodbc \
-                xerces-c \
-                libutil-linux \
-                protobuf \
-                protobuf-c \
-                gnurl \
-                curl \
-                expat \
-
+		python2 \
+		python2-pyzmq \
+		python2-simplejson \
+		boost \
+        zeromq \
+        intel-tbb \
+        binutils \
+        gsl \
+        unixodbc \
+        xerces-c \
+        protobuf \
+        protobuf-c \
+        gnurl \
+        curl \
+        expat \
 		sdl_gfx \
 		openssl \
 		tcl \
 		tcsh \
 		freetype2 \
 		blas \
+		lapack \
 		gcc-fortran \
-		python-flask \
 		"
-		#liblua5.1-0-dev \
-		#libxmlrpc-c3-dev \
-		#python-flask-restful \
-
-PACKAGES_RUNTIME="	
+#libutil-linux \
+PACKAGES_RUNTIME="
 		unixodbc \
 		psqlodbc \
 		libpqxx \
 		"
-# SECTION 3 Debug info routines.
+
+# Template for messages printed.
 message() {
 echo -e "\e[1;34m[$SELF_NAME] $MESSAGE\e[0m"
 }
 
-PACKAGE_TYPE=$DEFAULT_PACKAGE_TYPE
-
-exit_trap() {
-if [ "$SELF_NAME" != "$TOOL_NAME" ] ; then
-  MESSAGE="Exiting $SELF_NAME normally..." ; message
-fi
+# Install  json-spirit (4.05)
+install_json_spirit(){
+MESSAGE="Installing json-spirit library...." ; message
+cd /tmp
+export BOOST_ROOT=/usr/include/boost/
+wget http://http.debian.net/debian/pool/main/j/json-spirit/json-spirit_4.05.orig.tar.gz
+tar -xvf json-spirit_4.05.orig.tar.gz
+cd json_spirit_v4_05
+mkdir build
+cd build/
+cmake ..
+make -j$(nproc)
+sudo make install
+cd ../..
+rm -rf json-spirit_4.05.orig.tar.gz json_spirit_v4_05
 }
 
-quit_trap() {
-if [ "$SELF_NAME" != "$TOOL_NAME" ] ; then
-  MESSAGE="Exiting $SELF_NAME by request..." ; message
-fi
+# Install cogutils
+install_cogutil(){
+MESSAGE="Installing cogutils...." ; message
+cd /tmp/
+wget https://github.com/opencog/cogutils/archive/master.tar.gz
+tar -xvf master.tar.gz
+cd cogutils-master/
+mkdir build
+cd build/
+cmake ..
+make -j$(nproc)
+sudo make install
+cd ../..
+rm -rf master.tar.gz cogutils-master/
 }
 
-error_trap() {
-if [ "$SELF_NAME" != "$TOOL_NAME" ] ; then
-  MESSAGE="Error trapped while running $SELF_NAME." ; message
-  debug
-fi
+# Install Python Packages
+install_python_packages(){
+MESSAGE="Installing python packages...." ; message
+cd /tmp
+wget https://raw.githubusercontent.com/opencog/opencog/master/opencog/python/requirements.txt
+sudo pip install -U -r /tmp/requirements.txt
+rm requirements.txt
 }
 
-# execute error_trap on signal ERR
-trap error_trap ERR 
-# execute exit_trap on signal EXIT
-trap exit_trap  EXIT
-# execute quit_trap on signal INT HUP QUIT TERM
-trap quit_trap  INT HUP QUIT TERM
-
-# SECTION 4: Function Definitions
-add_repositories() {
-MESSAGE="Adding software repositories..." ; message
-for REPO in $REPOSITORIES ; do 
-#Install a 'remote' package (not from a repository stated in pacman's configuration files)
-  pacman -U $REPO
-done
-# Update package database 
-  pacman -Sy
+# Install AtomSpace
+install_atomspace(){
+MESSAGE="Installing atomspace...." ; message
+cd /tmp/
+wget https://github.com/opencog/atomspace/archive/master.tar.gz
+tar -xvf master.tar.gz
+cd atomspace-master/
+mkdir build
+cd build/
+cmake ..
+make -j$(nproc)
+sudo make install
+cd ../..
+rm -rf master.tar.gz atomspace-master/
 }
 
-
+# Function for installing all required dependenceis for building OpenCog,
+# as well as dependencies required for running opencog with other services.
 install_dependencies() {
 MESSAGE="Installing OpenCog build dependencies...." ; message
-if !  pacman -S $QUIET $PACKAGES_BUILD $PACKAGES_RUNTIME $PACKAGES_FETCH; then
+if !  pacman -S --noconfirm $PACKAGES_BUILD $PACKAGES_RUNTIME $PACKAGES_TOOLS; then
   MESSAGE="Error installing some of dependencies... :( :("  ; message
   exit 1
 fi
+install_python_packages
+install_json_spirit
+install_cogutil
+install_atomspace
 }
 
-update_opencog_source() { 
-OPENCOG_SOURCE_DIR=$LIVE_SOURCE_BRANCH
-mkdir -p $OPENCOG_SOURCE_DIR || true
-if [ ! "$(ls -A $OPENCOG_SOURCE_DIR/.git)" ]; then
-  MESSAGE="Fetching OpenCog source at $OPENCOG_SOURCE_DIR..." ; message
-  git clone https://github.com/opencog/opencog $OPENCOG_SOURCE_DIR
-else
-  if [ $UPDATE_OPENCOG ] ; then
-    MESSAGE="Updating OpenCog source at $OPENCOG_SOURCE_DIR..." ; message
-    cd $OPENCOG_SOURCE_DIR
-    git pull 
-    cd -
-  fi
-fi
-}
-
-build_opencog() {
-mkdir -p -v $LIVE_BUILD_DIR || true
-cd $HOST_BUILD_DIR
-MESSAGE="cmake $HOST_SOURCE_BRANCH" ; message
-cmake $HOST_SOURCE_BRANCH
-MESSAGE="make -j$MAKE_JOBS" ; message
-make -j$MAKE_JOBS
-
-if [ $TEST_OPENCOG ] ; then 
-  make test
-fi
-
-}
-# SECTION 5: Main Program (MAIN main)
+# Main Program
 install_dependencies
-update_opencog_source
-build_opencog
