9f207defcae253a5641928bceb488841fe8f4fe3
Amen Belayneh <amenbelayneh@gmail.com>
2015-05-06 18:29:04 +0800

    updated fedora installation script

--- scripts/install-fedora-dependencies.sh
+++ scripts/install-fedora-dependencies.sh
@@ -14,10 +14,12 @@ SELF_NAME=$(basename $0)
 PACKAGES_TOOLS="
 		git \
 		python-pip \
+		wget \
+		tar \
+		sudo \
 			"
 
 # Packages for building opencog
-# FIXME fedora version of libjson-spirit-dev on ubuntu not found yet.
 PACKAGES_BUILD="
 		make \
 		gcc \
@@ -59,12 +61,67 @@ PACKAGES_RUNTIME="
 		postgresql-devel \
 		"
 
-# Path to requirements file
-PACKAGES_PYTHON="../opencog/python/requirements.txt"
-
 # Template for messages printed.
 message() {
 echo -e "\e[1;34m[$SELF_NAME] $MESSAGE\e[0m"
+}
+
+# Install  json-spirit (4.05)
+install_json_spirit(){
+MESSAGE="Installing json-spirit library...." ; message
+cd /tmp
+export BOOST_ROOT=/usr/include/boost/
+wget http://http.debian.net/debian/pool/main/j/json-spirit/json-spirit_4.05.orig.tar.gz
+tar -xvf json-spirit_4.05.orig.tar.gz
+cd json_spirit_v4_05
+mkdir build
+cd build/
+cmake ..
+make -j$(nproc)
+sudo make install
+cd ../..
+rm -rf json-spirit_4.05.orig.tar.gz json_spirit_v4_05
+}
+
+# Install Python Packages
+install_python_packages(){
+MESSAGE="Installing python packages...." ; message
+cd /tmp
+wget https://raw.githubusercontent.com/opencog/opencog/master/opencog/python/requirements.txt
+sudo pip install -U -r /tmp/requirements.txt
+rm requirements.txt
+}
+
+# Install cogutils
+install_cogutil(){
+MESSAGE="Installing cogutils...." ; message
+cd /tmp/
+wget https://github.com/opencog/cogutils/archive/master.tar.gz
+tar -xvf master.tar.gz
+cd cogutils-master/
+mkdir build
+cd build/
+cmake ..
+make -j$(nproc)
+sudo make install
+cd ../..
+rm -rf master.tar.gz cogutils-master/
+}
+
+# Install AtomSpace
+install_atomspace(){
+MESSAGE="Installing atomspace...." ; message
+cd /tmp/
+wget https://github.com/opencog/atomspace/archive/master.tar.gz
+tar -xvf master.tar.gz
+cd atomspace-master/
+mkdir build
+cd build/
+cmake ..
+make -j$(nproc)
+sudo make install
+cd ../..
+rm -rf master.tar.gz atomspace-master/
 }
 
 # Function for installing all required dependenceis for building OpenCog,
@@ -74,11 +131,14 @@ MESSAGE="Updating Package db...." ; message
 yum updateinfo
 
 MESSAGE="Installing OpenCog build dependencies...." ; message
-if !  (yum -y install $PACKAGES_BUILD $PACKAGES_RUNTIME $PACKAGES_TOOLS && \
-		pip install -U -r $PACKAGES_PYTHON); then
+if !  (yum -y install $PACKAGES_BUILD $PACKAGES_RUNTIME $PACKAGES_TOOLS); then
   MESSAGE="Error installing some of dependencies... :( :("  ; message
   exit 1
 fi
+install_json_spirit
+install_python_packages
+install_cogutil
+install_atomspace
 }
 
 # Main Program
