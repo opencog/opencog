d512d954a7a7f65e1cac73eae0e249e1b8e05ea8
rodsol <rodisolomon@gmail.com>
2015-05-28 15:08:25 +0300

    call fuzzy matcher

--- opencog/nlp/chatbot/nlp-chat-interface.scm
+++ opencog/nlp/chatbot/nlp-chat-interface.scm
@@ -15,11 +15,12 @@
 ;if query is yes-or-no call 'truth_query_process' else call 'wh_query_process'
 ;----------------------------------------------------------------------------------------------
 (define (process_query user query)
+        (define querySentence)
+        (set! querySentence (car (nlp-parse query)))
         (display "Hello ")
         (display user)
         (display ", you said: \"")
         (display query)
-        (define querySentence (car (nlp-parse query)))
         (cond
                 ((equal? (check_query_type querySentence) "TruthQuerySpeechAct")(truth_query_process querySentence)
                 (display "You ask a TruthQuerySpeechAct"))
@@ -32,20 +33,19 @@
 ;depending on the backward chaning generate the answer (using SuRel)
 ;----------------------------------------------------------------------------------------------
 (define (truth_query_process query)
-	(define tmp (fAtom query))
-	(define bc (cog-bc (cog-new-link 'InheritanceLink 	
-				(VariableNode "$x")
-				(gdr tmp))))
+        (define tmp)
+        (define bc)
+        (set! tmp (fAtom query))
+        (set! bc (cog-bc (cog-new-link 'InheritanceLink (VariableNode "$x")(gdr tmp))))
 )
 ;----------------------------------------------------------------------------------------------
 ; process wh-question using the fuzzy hyper graph Matcher
 ;----------------------------------------------------------------------------------------------
 (define (wh_query_process query)
 ;;call the graph matcher 
+ (get-similar-sentences query)
+
 )
-
-
-
 
 ;---------------------------------------------------------------------------------------------
 ; Used by 'truth_query_process' to find the input for the backward chaining
