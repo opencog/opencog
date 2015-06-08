2a07de8bd5dcae2b0bdd1c880c1d702893262b9b
Linas Vepstas <linasvepstas@gmail.com>
2015-06-08 18:44:47 -0500

    Fix up the shell scripts

--- opencog/nlp/learn/misc-scripts/ss-one.sh
+++ opencog/nlp/learn/misc-scripts/ss-one.sh
@@ -1,24 +1,34 @@
 #!/bin/sh
 #
-# Support script for batch parsing of wikipedia data
-# Sentence-split one file, submit it, via perl script,
-# to the parser. When done, move the file over to a 'finished'
-# directory.
+# ss-one.sh <lang> <filename> <cogserver-host> <cogserver-port>
 #
-# Set up assorted constants needed to run.
+# Support script for batch parsing of wikipedia articles.
+# Sentence-split one file, submit it, via perl script, to the parser.
+# When done, move the file over to a 'finished' directory.
+#
+#
 
+# Set up assorted constants needed to run.
 lang=$1
 filename="$2"
+# coghost="localhost"
+# cogport=17002
+coghost="$3"
+cogport=$4
+
 
 splitter=/home/linas/src/relex/src/split-sentences/split-sentences.pl
+splitter=/usr/local/bin/split-sentences.pl
 splitdir=split-articles
 subdir=submitted-articles
 
-# Punt if the cogserver has crashed
+# Punt if the cogserver has crashed.
 haveserver=`ps aux |grep cogserver |grep opencog-$lang`
 if [[ -z "$haveserver" ]] ; then
 	exit 1
 fi
+
+# Punt if relex or link-grammar have crashed.
 haveserver=`ps aux |grep relex |grep linkgram`
 if [[ -z "$haveserver" ]] ; then
 	exit 1
@@ -35,7 +45,7 @@ echo "Processing file $rest"
 cat "$filename" | $splitter -l $lang >  "$splitdir/$rest" 
 
 # Submit the split article
-cat "$splitdir/$rest" | ./submit-one.pl 
+cat "$splitdir/$rest" | ./submit-one.pl $coghost $cogport
 
 # Punt if the cogserver has crashed (second test,
 # before doing the mv and rm below)
