666cc2ab305c11ff70c9aa22b100903367d35dd8
Prateek Saxena <prateeksaxena2809@gmail.com>
2015-06-23 12:52:34 +0530

    Added implication and rule

---
+++ opencog/reasoning/pln/rules/implication-and-rule.scm
@@ -0,0 +1,62 @@
+; =============================================================================
+; ImplicationAndRule
+;
+; AndLink
+;   ImplicationLink
+;       A
+;       B
+;   ImplicationLink
+;       AndLink
+;           B
+;           C
+;       D
+; |-
+; ImplicationLink
+;   AndLink
+;       A
+;       C
+;   D
+;
+; -----------------------------------------------------------------------------
+
+(define pln-rule-implication-and
+    (BindLink
+        (VariableList
+            (VariableNode "$A")
+            (VariableNode "$B")
+            (VariableNode "$C")
+            (VariableNode "$D"))
+        (AndLink
+            (ImplicationLink
+                (VariableNode "$A")
+                (VariableNode "$B"))
+            (ImplicationLink
+                (AndLink
+                    (VariableNode "$B")
+                    (VariableNode "$C"))
+                (VariableNode "$D")))
+        (ExecutionOutputLink
+            (GroundedSchemaNode "scm: pln-formula-implication-and")
+            (ListLink
+                (ImplicationLink
+                    (AndLink
+                        (VariableNode "$A")
+                        (VariableNode "$C"))
+                    (VariableNode "$D"))
+                (ImplicationLink
+                    (VariableNode "$A")
+                    (VariableNode "$B"))
+                (ImplicationLink
+                    (AndLink
+                        (VariableNode "$B")
+                        (VariableNode "$C"))
+                    (VariableNode "$D"))))))
+
+(define (pln-formula-implication-and ACD AB BCD)
+    (cog-set-tv!
+        (pln-formula-implication-and-side-effect-free ACD AB BCD)
+    )
+)
+
+(define (pln-formula-implication-and-side-effect-free ACD AB BCD)
+    (stv 1 1))
