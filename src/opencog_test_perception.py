d77d8d726ee40c47ebee97981e6f963ff6ba9e80
chenesan <pipio1994@gmail.com>
2015-06-26 14:53:14 +0000

    add timeinfo on eval/location links

--- src/opencog_test_perception.py
+++ src/opencog_test_perception.py
@@ -16,13 +16,13 @@ class PerceptionManager:
     def __init__(self, atomspace, spaceserver,timeserver):
         self._atomspace = atomspace
         self._spaceserver = spaceserver
-        self._timeserver = timerserver
+        self._timeserver = timeserver
 
         #TODO:For now the border of map is defined by Brad's flat world size,from(x,z)=(-100,-100) to (0,0)
-            #But actually it should receive these borders argument from ROS to initialize the SpaceMap
-        self._spaceserver.addMap(data.timestamp,"MCWorld",
-                                 (-100,-100,-100),
-                                 (100,100,100),-100)
+        #But actually it should receive these borders argument from ROS to initialize the SpaceMap
+        self._spaceserver.addMap(123456,"MCWorld",
+                                 -100,-100,-100,
+                                 100,100,100,-100)
 
     def buildBlockNodes(self,block,maphandle):
 
@@ -32,75 +32,121 @@ class PerceptionManager:
         #Note: in 3DSpaceMap using structure node to represent block.
         objnode=self._atomspace.add_node(types.StructureNode,"obj%s".format(self.buildBlockNodes.__func__.objNo))
         self.buildBlockNodes.__func__.objNo+=1
+        updatedevallinks=[]
 
-        self._atomspace.add_link(types.AtLocationLink,[
+        locationlink=self._atomspace.add_link(types.AtLocationLink,[
             objnode,
             maphandle,
             self._atomspace.add_link(types.ListLink,[
                 self._atomspace.add_node(types.NumberNode,str(block.x)),
                 self._atomspace.add_node(types.NumberNode,str(block.y)),
                 self._atomspace.add_node(types.NumberNode,str(block.z))])])
-        
-        self._atomspace.add_link(types.EvaluationLink,[
+        updatedevallinks.append(locationlink)
+
+        materiallink=self._atomspace.add_link(types.EvaluationLink,[
             self._atomspace.add_node(types.PredicateNode,"material"),
             self._atomspace.add_link(types.ListLink,[
                 objnode,
                 self._atomspace.add_node(types.ConceptNode,str(block.btype))])])
-        return objnode
+        updatedevallinks.append(materiallink)
+        return objnode,updatedevallinks
 
     def buildEntityNode(self,entity,maphandle):
         entitynode=self._atomspace.add_node(types.ObjectNode,str(entity.eid))
-        self._atomspace.add_link(types.AtLocationLink,[
+        updatedevallinks=[]
+
+        locationlink=self._atomspace.add_link(types.AtLocationLink,[
             entitynode,
             maphandle,
             self._atomspace.add_link(types.ListLink,[
                 self._atomspace.add_node(types.NumberNode,str(entity.x)),
                 self._atomspace.add_node(types.NumberNode,str(entity.y)),
                 self._atomspace.add_node(types.NumberNode,str(entity.z))])])
-        self._atomspace.add_link(types.EvaluationLink,[
-            self._atomspace.add_node(types.PredicateNode,"entitytype"),
+        updatedevallinks.append(locationlink)
+
+        typelink=self._atomspace.add_link(types.EvaluationLink,[
+            self._atomspace.add_node(types.PredicateNode,"type"),
             self._atomspace.add_link(types.ListLink,[
                 entitynode,
-                self._atomspace.add_node(types.ConceptNode,str(entity.entity_type))])])
-        self._atomspace.add_link(types.EvaluationLink,[
+                self._atomspace.add_node(types.ConceptNode,str(entity.mob_type))])])
+        updatedevallinks.append(typelink)
+
+        looklink=self._atomspace.add_link(types.EvaluationLink,[
             self._atomspace.add_node(types.PredicateNode,"look"),
             self._atomspace.add_link(types.ListLink,[
                 entitynode,
-                self._atomspace.add_node(types.NumberNode,str(entity.yaw)),
-                self._atomspace.add_node(types.NumberNode,str(entity.pitch))])])
+                self._atomspace.add_node(types.NumberNode,str(entity.head_yaw)),
+                self._atomspace.add_node(types.NumberNode,str(entity.head_pitch))])])
+        updatedevallinks.append(looklink)
 
-        self._atomspace.add_link(types.EvaluationLink,[
+        sizelink=self._atomspace.add_link(types.EvaluationLink,[
             self._atomspace.add_node(types.PredicateNode,"size"),
             self._atomspace.add_link(types.ListLink,[
                 entitynode,
                 self._atomspace.add_node(types.NumberNode,str(entity.length)),
                 self._atomspace.add_node(types.NumberNode,str(entity.width)),
                 self._atomspace.add_node(types.NumberNode,str(entity.height))])])
-        self._atomspace.add_link(types.EvaluationLink,[
+        updatedevallinks.append(sizelink)
+
+        looklink=self._atomspace.add_link(types.EvaluationLink,[
+            self._atomspace.add_node(types.PredicateNode,"look"),
+            self._atomspace.add_link(types.ListLink,[
+                entitynode,
+                self._atomspace.add_node(types.NumberNode,str(entity.head_yaw)),
+                self._atomspace.add_node(types.NumberNode,str(entity.head_pitch))])])
+        updatedevallinks.append(looklink)
+        
+
+        velocitylink=self._atomspace.add_link(types.EvaluationLink,[
             self._atomspace.add_node(types.PredicateNode,"velocity"),
             self._atomspace.add_link(types.ListLink,[
                 entitynode,
                 self._atomspace.add_node(types.NumberNode,str(entity.velocity_x)),
                 self._atomspace.add_node(types.NumberNode,str(entity.velocity_y)),
                 self._atomspace.add_node(types.NumberNode,str(entity.velocity_z))])])
+        updatedevallinks.append(velocitylink)
 
-        return entitynode
+        return entitynode,updatedevallinks
 
     def updateEntityNode(self,entitynode,entity,maphandle):
         
-        atlocationlink=self._atomspace.add_link(types.AtLocationLink,[
+        updatedevallinks=[]
+        
+        locationlink=self._atomspace.add_link(types.AtLocationLink,[
             entitynode,
             maphandle,
             self._atomspace.add_link(types.ListLink,[
                 self._atomspace.add_node(types.NumberNode,str(entity.x)),
                 self._atomspace.add_node(types.NumberNode,str(entity.y)),
                 self._atomspace.add_node(types.NumberNode,str(entity.z))])])
-        #TODO: update velocitypredicate and lookprediacte
-        return atlocationlink
+        updatedevallinks.append(locationlink)
+
+        looklink=self._atomspace.add_link(types.EvaluationLink,[
+            self._atomspace.add_node(types.PredicateNode,"look"),
+            self._atomspace.add_link(types.ListLink,[
+                entitynode,
+                self._atomspace.add_node(types.NumberNode,str(entity.head_yaw)),
+                self._atomspace.add_node(types.NumberNode,str(entity.head_pitch))])])
+        updatedevallinks.append(looklink)
+
+        velocitylink=self._atomspace.add_link(types.EvaluationLink,[
+            self._atomspace.add_node(types.PredicateNode,"velocity"),
+            self._atomspace.add_link(types.ListLink,[
+                entitynode,
+                self._atomspace.add_node(types.NumberNode,str(entity.velocity_x)),
+                self._atomspace.add_node(types.NumberNode,str(entity.velocity_y)),
+                self._atomspace.add_node(types.NumberNode,str(entity.velocity_z))])])
+        updatedevallinks.append(velocitylink)
+        
+        return updatedevallinks
         
 
     def ProcessANewWorld(self,data):
-        pass
+        #TODO:For now the border of map is defined by Brad's flat world size,from(x,z)=(-100,-100) to (0,0)
+        #But actually it should receive these borders argument from ROS to initialize the SpaceMap
+        self._spaceserver.addMap(data.timestamp,"MCWorld",
+                                 -100,-100,-100,
+                                 100,100,100,-100)
 
     def processMapMessage(self,data):
 
@@ -110,81 +156,81 @@ class PerceptionManager:
         print 'callback, timestamp %s'%(data.timestamp)
 
         try:
-            maphandle=(a.get_atoms_by_name(types.SpaceMapNode,data.scene)[0]).h
+            maphandle=(self._atomspace.get_atoms_by_name(types.SpaceMapNode,data.scene)[0]).h
         except IndexError:
             return 
 
             curscenemap=self._spaceserver.getMap(maphandle)
 
-            for block in data.blocks:
-                blockhandle=curscenemap.getUnitBlockHandleFromLocation((block.x,block.y,block.z))
-                #Check if it's undefined, note that we don't use the is_undefined(),
-                #see the bug info in spacetime.pyx
-                if blockhandle.value() != -1:
-                    blockinfo=curscenemap.getBlockAtLocation((block.x,block.y,block.z))
-                    if blockinfo.getMaterial()==str(block.btype):
-                        self._timeserver.addTimeInfo(blockhandle,data.timestamp)
-                        continue
-                    else:
-                        self._spaceserver.removeMapInfo(blockhandle,maphandle,data.timestamp)
-                blocknode=self.buildBlockNodes(block,maphandle)
-                self._spaceserver.addMapInfo(blocknode.h,maphandle,False,data.timestamp,
-                              block.x,block.y,block.z,
-                              1,1,1,0,
-                              'block',blocknode.name,str(block.btype))
-                self._timeserver.addTimeInfo(blocknode.h,data.timestamp)
+        for block in data.blocks:
+            blockhandle=curscenemap.getUnitBlockHandleFromLocation((block.x,block.y,block.z))
+            #Check if it's undefined, note that we don't use the is_undefined(),
+            #see the bug info in spacetime.pyx
+            if blockhandle.value() != -1:
+                blockinfo=curscenemap.getBlockAtLocation((block.x,block.y,block.z))
+                if blockinfo.getMaterial()==str(block.btype):
+                    continue
+                else:
+                    self._spaceserver.removeMapInfo(blockhandle,maphandle,data.timestamp)
+                    #TODO: add a predicate to mark block is disappeared->This predicate should be added later in PredicateUpdater
+            blocknode,updatedatoms=self.buildBlockNodes(block,maphandle)
+            self._spaceserver.addMapInfo(blocknode.h,maphandle,False,data.timestamp,
+                                         block.x,block.y,block.z,
+                                         1,1,1,0,
+                                         'block',blocknode.name,str(block.btype))
+            self._timeserver.addTimeInfo(blocknode.h,data.timestamp)
+            for link in updatedatoms:
+                self._timeserver.addTimeInfo(link.h,data.timestamp)
 
-    print self._atomspace.size()
-
-    #TODO
     def processMobMessage(self,data):
         
         try:
-            maphandle=(a.get_atoms_by_name(types.SpaceMapNode,data.scene)[0]).h
+            maphandle=(self._atomspace.get_atoms_by_name(types.SpaceMapNode,data.scene)[0]).h
         except IndexError:
             return
 
         curscenemap=self._spaceserver.getMap(maphandle)
 
-        for mobdata in data.mobs:
+        for mob in data.mobs:
         
             try:
-                mobnode=(a.get_atoms_by_name(types.ObjectNode,str(mobdata.eid))[0])
-                atlocationlink=self.updateEntityNode(mobnode,mobdata,maphandle)
-                self._timeserver.addTimeInfo(atlocationlink.h,data.timestamp)
+                mobnode=(self._atomspace.get_atoms_by_name(types.ObjectNode,str(mob.eid))[0])
+                allupdatedatoms=self.updateEntityNode(mobnode,mob,maphandle)
             except IndexError:
-                mobnode=self.buildEntityNode(mobdata,maphandle)
+                mobnode,allupdatedatoms=self.buildEntityNode(mob,maphandle)
                 self._timeserver.addTimeInfo(mobnode.h,data.timestamp)            
             self._spaceserver.addMapInfo(mobnode.h,maphandle,False,data.timestamp,
-                                       mobdata.x,mobdata.y,mobdata.z,
-                                       mobdata.length,mobdata.width,mobdata.height,mobdata.yaw,
-                                       'mob',mobnode.name,mobdata.mob_type)
+                                       mob.x,mob.y,mob.z,
+                                       mob.length,mob.width,mob.height,mob.head_yaw,
+                                       'mob',mobnode.name,str(mob.mob_type))
+            for atom in allupdatedatoms:
+                self._timeserver.addTimeInfo(atom.h,data.timestamp)
 
 
 
     def processPlayerPosMessage(self,data):
     
         try:
-            maphandle=(a.get_atoms_by_name(types.SpaceMapNode,data.scene)[0]).h
+            maphandle=(self._atomspace.get_atoms_by_name(types.SpaceMapNode,data.scene)[0]).h
         except IndexError:
             return
             
         curscenemap=self._spaceserver.getMap(maphandle)
         
         try:
-            playernode=a.get_atoms_by_name(types.ObjectNode,"myself")[0]
-            atlocationlink=self.updateEntityLocationNode(mobnode,mobdata,maphandle)
+            playernode=self._atomspace.get_atoms_by_name(types.ObjectNode,"myself")[0]
+            allupdatedatoms=self.updatePlayerNode(playernode,data,maphandle)
             self._timeserver.addTimeInfo(atlocationlink.h,data.timestamp)
 
         except IndexError:
-            playernode=self.buildEntityNode(data,maphandle)
+            playernode,allupdatedatoms=self.buildPlayerNode(data,maphandle)
             self._timeserver.addTimeInfo(playernode.h,data.timestamp)            
         self._spaceserver.addMapInfo(playernode.h,maphandle,True,data.timestamp,
                           data.x,data.y,data.z,
                           data.length,data.width,data.height,data.yaw,
                           'player',"myself","player")
-
-
+        for atom in allupdatedatoms:
+            self._timeserver.addTimeInfo(atom.h,data.timestamp)
 
 
     def remove(self):
@@ -197,9 +243,9 @@ class PerceptionManager:
 def main():
 
     a = AtomSpace()
-    pm = PerceptionManager(a)
     ss = SpaceServer(a)
     ts = TimeServer(a,ss)
+    pm = PerceptionManager(a,ss,ts)
 
     ss.setTimeServer(ts)
     initialize_opencog(a)
@@ -208,7 +254,6 @@ def main():
     rospy.Subscriber('visible_blocks_data',visible_blocks_msg,pm.processMapMessage)    
     rospy.Subscriber('mobs_data',mobs_msg,pm.processMobMessage)
     rospy.Subscriber('playerpos_data',playerpos_msg,pm.processPlayerPosMessage)
-    rospy.Subscriber('entity_data',entity_msg,pm.processEntityMessage)
 
     while not rospy.is_shutdown():
         rospy.sleep(1.)
