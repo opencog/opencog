d869c3675811da08a6b207c13e87c1fb81cffe9e
chenesan <pipio1994@gmail.com>
2015-06-30 02:48:02 +0000

    forget to add the testbot3.py for testing perception......

---
+++ minecraft_bot/src/testbot3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# to test map data and timer plugins
+# import roslib; roslib.load_manifest('minecraft_bot')
+# import rospy
+# from minecraft_bot.msg import movement_msg
+
+from spock import Client, PluginLoader
+from spock.plugins import DefaultPlugins
+#from spock.plugins.helpers.entities import EntityPlugin
+
+# load custom plugins. I use capitalized names to indicate non-standard plugins
+#from spockextras.plugins.MineAndPlace import MineAndPlacePlugin
+#from spockextras.plugins.NewMovement import NewMovementPlugin
+#from spockextras.plugins.NewPhysics import NewPhysicsPlugin
+from spockextras.plugins.SpockControl import SpockControlPlugin
+from spockextras.plugins.SendMapData import SendMapDataPlugin
+
+# connect to localhost server
+settings = {'start': {'username': 'Bot',},'auth': {'authenticated': False,},}
+plugins = DefaultPlugins
+
+#plugins.append(('MineAndPlace', MineAndPlacePlugin))
+#plugins.append(('NewMovement', NewMovementPlugin))
+#plugins.append(('NewPhysics', NewPhysicsPlugin))
+plugins.append(('SpockControl', SpockControlPlugin))
+plugins.append(('SendMapData', SendMapDataPlugin))
+
+client = Client(plugins = plugins, settings = settings)
+
+print("connecting to localhost on port 25565")
+
+#client.start() with no arguments will automatically connect to localhost
+client.start('localhost', 25565)
+
