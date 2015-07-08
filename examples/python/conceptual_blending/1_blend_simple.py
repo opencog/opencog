d3c01e2d73a347d99c8446a9dc9bea177ab56346
DongMin Kim <kim135797531@naver.com>
2015-07-08 16:07:26 +0900

    Implement a correcting the link's truthvalue
    * Corrects if there exists conflict links.

--- examples/python/conceptual_blending/1_blend_simple.py
+++ examples/python/conceptual_blending/1_blend_simple.py
@@ -1,4 +1,4 @@
-from opencog.atomspace import AtomSpace
+from opencog.atomspace import AtomSpace, TruthValue
 from opencog.type_constructors import types
 from opencog.utilities import initialize_opencog
 from opencog.logger import log
@@ -13,8 +13,23 @@ log.set_level("WARN")
 a = AtomSpace()
 initialize_opencog(a)
 
-a.add_node(types.ConceptNode, "car")
-a.add_node(types.ConceptNode, "man")
+# Make custom concept network.
+"""
+Make test nodes.
+"""
+# Nodes will be blended:
+car = a.add_node(types.ConceptNode, "car")
+man = a.add_node(types.ConceptNode, "man")
+vehicle = a.add_node(types.ConceptNode, "vehicle")
+
+"""
+Make test links.
+"""
+l4 = a.add_link(types.SimilarityLink, [car, vehicle])
+l5 = a.add_link(types.SimilarityLink, [man, vehicle])
+a.set_tv(l4.h, TruthValue(0.9, 0.8))
+a.set_tv(l5.h, TruthValue(0.1, 0.9))
 
 inst = ConceptualBlending(a)
 inst.run()
+
--- opencog/python/blending/src/random_blender.py
+++ opencog/python/blending/src/random_blender.py
@@ -1,7 +1,9 @@
 import random
+from opencog.atomspace import Handle, TruthValue
 from opencog.type_constructors import types, ConceptNode
 from opencog.logger import *
 from blending.src.base_blender import Blender
+
 
 __author__ = 'DongMin Kim'
 
@@ -30,15 +32,78 @@ class RandomBlender(Blender):
 
         return ret
 
-    def _copy_link_from_exist_node(self, src_node_list, dst_node):
-        for node in src_node_list:
-            link_list = self.a.get_atoms_by_target_atom(types.Link, node)
-            for link in link_list:
-                out_list = self.a.get_outgoing(link.h)
-                for out in out_list:
-                    if (out.name != node.name) and \
-                            (out.name != dst_node.name):
-                        self.a.add_link(link.t, [out, dst_node], link.tv)
+    def _correct_strength_of_links(
+            self, src_link_dict, dst_link_dict, non_exclusive_link_set
+    ):
+        for link in non_exclusive_link_set:
+            src_link = self.a[Handle(src_link_dict[link])]
+            dst_link = self.a[Handle(dst_link_dict[link])]
+
+            found_s = src_link.tv.mean
+            found_c = src_link.tv.confidence
+            exist_s = dst_link.tv.mean
+            exist_c = dst_link.tv.confidence
+
+            # sC = (cA sA + cB sB) / (cA + cB)
+            # https://groups.google.com/forum/#!topic/opencog/fa5c4yE8YdU
+            new_strength = \
+                ((found_c * found_s) + (exist_c * exist_s)) \
+                / (found_c + exist_c)
+
+            # TODO: Currently, conflicting confidence value for new blended node
+            # is just average of old value.
+            new_confidence = (found_c + exist_c) / 2
+
+            dst_link.tv = TruthValue(new_strength, new_confidence)
+
+    def _get_linktype_linktarget_pair_dict(self, link_list, src_node):
+        ret_dict = dict()
+        for link in link_list:
+            xget_link_node = self.a.xget_outgoing(link.h)
+            for link_node in xget_link_node:
+                if link_node.h != src_node.h:
+                    type_target_pair = (link.t, link_node.h.value())
+                    ret_dict[type_target_pair] = link.h.value()
+
+        return ret_dict
+
+    def _copy_all_link_to_new_node(self, src_node_list, dst_node):
+        dst_link_list = self.a.get_atoms_by_target_atom(types.Link, dst_node)
+
+        dst_link_dict = self._get_linktype_linktarget_pair_dict(
+            dst_link_list, dst_node
+        )
+        dst_link_set = set(dst_link_dict.keys())
+
+        for src_node in src_node_list:
+            src_link_list = self.a.get_atoms_by_target_atom(
+                types.Link, src_node
+            )
+            src_link_dict = self._get_linktype_linktarget_pair_dict(
+                src_link_list, src_node
+            )
+            src_link_set = set(src_link_dict.keys())
+
+            exclusive_link_set = src_link_set - dst_link_set
+            non_exclusive_link_set = src_link_set & dst_link_set
+
+            # Add new link to target node.
+            for link_pair in exclusive_link_set:
+                src_link = self.a[Handle(src_link_dict[link_pair])]
+                src_link_type = link_pair[0]
+                src_link_node = self.a[Handle(link_pair[1])]
+                new_link = self.a.add_link(
+                    src_link_type,
+                    [dst_node, src_link_node],
+                    src_link.tv
+                )
+                dst_link_dict[link_pair] = new_link.h.value()
+                dst_link_set.add(link_pair)
+
+            # Correct conflict link value in target node.
+            self._correct_strength_of_links(
+                src_link_dict, dst_link_dict, non_exclusive_link_set
+            )
 
     def blend(self):
         log.warn("Start RandomBlending")
@@ -57,17 +122,29 @@ class RandomBlender(Blender):
         a_node_1 = a_nodes[1]
         a_blended_node = ConceptNode(
             str(a_node_0.name) +
+            '-' +
             str(a_node_1.name)
         )
 
-        # Make the links between exist nodes and newly blended node.
-        self._copy_link_from_exist_node([a_node_0, a_node_1], a_blended_node)
+        # Make the links between source nodes and newly blended node.
+        # TODO: Change to make with proper reason, not make in every blending.
+        self.a.add_link(
+            types.AssociativeLink,
+            [a_node_0, a_blended_node],
+            TruthValue(0.7, 0.6)
+        )
+        self.a.add_link(
+            types.AssociativeLink,
+            [a_node_1, a_blended_node],
+            TruthValue(0.7, 0.6)
+        )
 
+        # Make the links between exist nodes and newly blended node.
         # Correct some attribute value of new links.
-        # - Do nothing.
+        self._copy_all_link_to_new_node([a_node_0, a_node_1], a_blended_node)
 
         # Detect and improve conflict links in newly blended node.
         # - Do nothing.
 
-        log.warn(str(a_blended_node.h)+str(a_blended_node.name))
+        log.warn(str(a_blended_node.h) + " " + str(a_blended_node.name))
         log.warn("Finish RandomBlending")
