991e3f75068e0a36f9cee0bbe5d795b97c915af7
Linas Vepstas <linasvepstas@gmail.com>
2015-07-16 14:14:14 -0500

    undo previous commit

--- opencog/nlp/learn/misc-scripts/wiki-ss-lt.sh
+++ opencog/nlp/learn/misc-scripts/wiki-ss-lt.sh
@@ -4,4 +4,4 @@
 # Loop over all the files in 'beta-pages', sentence-split them
 # and submit them for parsing.
 #
-time find beta-pages -type f -exec ./ss-one.sh lt \"{}\" localhost 17002 \;
+time find beta-pages -type f -exec ./ss-one.sh lt {} localhost 17002 \;
--- opencog/nlp/learn/misc-scripts/wiki-ss-pl.sh
+++ opencog/nlp/learn/misc-scripts/wiki-ss-pl.sh
@@ -4,4 +4,4 @@
 # Loop over all the files in 'beta-pages', sentence-split them
 # and submit them for parsing.
 #
-time find beta-pages -type f -exec ./ss-one.sh pl \"{}\" locahost 17004 \;
+time find beta-pages -type f -exec ./ss-one.sh pl {} locahost 17004 \;
