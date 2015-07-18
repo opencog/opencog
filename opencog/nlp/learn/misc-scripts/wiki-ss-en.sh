7bc5dab804fcc84a053fd29e1b30a4272ba5f977
Linas Vepstas <linasvepstas@gmail.com>
2015-07-18 17:39:21 -0500

    Add missing space

--- opencog/nlp/learn/misc-scripts/wiki-ss-en.sh
+++ opencog/nlp/learn/misc-scripts/wiki-ss-en.sh
@@ -4,4 +4,4 @@
 # Loop over all the files in 'beta-pages', sentence-split them
 # and submit them for parsing.
 #
-time find beta-pages -type f -exec ./ss-one.sh en {} localhost 17005\;
+time find beta-pages -type f -exec ./ss-one.sh en {} localhost 17005 \;
