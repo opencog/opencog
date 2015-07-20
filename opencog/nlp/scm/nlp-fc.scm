eaad310ae5d2dd93cfdaffb7b981c9a4b9a6be89
Mandeep <mandeep.singh.bhatia@gmail.com>
2015-07-20 15:52:18 +0800

    incomplete work on nlp-forward chaining

---
+++ opencog/nlp/scm/nlp-fc.scm
@@ -0,0 +1,63 @@
+(define (nlp-files-fc sentence-file rules-file)
+  (cog-fc
+  (load-sentences sentence-file)
+  (load-rules-from-file rules-file)
+  )
+) 
+
+(define (load-rules-from-file fl)
+)
+
+(define (load-sentences sfile)
+  (map-to-relex-parse-atoms (map-sent-nodes-to-parse (nlp-parse-from-file-mod sfile)))
+)
+;(parse-get-relations (car(sentence-get-parses (car(nlp-parse "how are you doing")))))
+;(for-each (lambda (x) (display x)) (list "hello" "there"))
+(define (nlp-parse-from-file-mod filepath)
+  (define sent-node-list '())
+    (let*
+        ((cmd-string (string-join (list "cat " filepath) ""))
+        (port (open-input-pipe cmd-string))
+        (line (get-line port))
+        )
+        (while (not (eof-object? line))
+            (if (or (= (string-length line) 0)
+                    (char=? #\; (string-ref (string-trim line) 0))
+                )
+                (set! line (get-line port))
+                (if (string=? "END." line)  ; continuing the tradition of RelEx
+                    (break)
+                    (begin
+                        (catch #t
+                            (lambda ()
+                                (set! sent-node-list (append sent-node-list (nlp-parse line)))
+                            )
+                            (lambda (key . parameters)
+                                (begin
+                                    (display "*** Unable to parse: \"")
+                                    (display line)
+                                    (display "\"\n")
+                                )
+                            )
+                        )
+                        (set! line (get-line port))
+                    )
+                )
+            )
+        )
+        sent-node-list
+        ;(close-pipe port)
+    )
+)
+
+(define (map-sent-nodes-to-parse sent-node-list)
+  (define mylist '())
+  (for-each (lambda (x)(set! mylist (append mylist (sentence-get-parses x))))sent-node-list)
+  mylist
+)
+
+(define (map-to-relex-parse-atoms parse-node-list)
+  (define mylist '())
+  (for-each (lambda (x)(set! mylist (append mylist (parse-get-relations x))))parse-node-list)
+  mylist
+)  
