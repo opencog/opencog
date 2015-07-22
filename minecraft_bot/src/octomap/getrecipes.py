8ba1344fe5ece36db90f3c5fb78e7134a82671d0
Brad Sheneman <bradsheneman@gmail.com>
2015-07-22 15:21:16 -0400

    completed parser for recipe list

--- minecraft_bot/src/octomap/getrecipes.py
+++ minecraft_bot/src/octomap/getrecipes.py
@@ -19,44 +19,98 @@ def getShapedRecipe(line):
     namelist = line[ni1:ni2].split(',')
     #print namelist
 
-    ns = namelist[0].find('.') + 1
-    recipe['name'] = namelist[0][ns:]
+    ns = namelist[0].find('Blocks.')
+    if ns < 0:
+        ns = namelist[0].find('Items.')
+
+    if ns >= 0:
+        recipe['name'] = namelist[0][ns:]
+    
     if len(namelist) > 1:
         recipe['num'] = int(namelist[1])
     else:
         recipe['num'] = 1
     
-    if namelist[0].find('Items'):
-        ntype = 'item'
-    elif namelist[0].find('Blocks'):
-        ntype = 'block'
-    else:
-        ntype = 'none'
+    #if namelist[0].find('Items'):
+    #    ntype = 'item'
+    #elif namelist[0].find('Blocks'):
+    #    ntype = 'block'
+    #else:
+    #    ntype = 'none'
     
-    print recipe['name']
+    #print recipe['name']
     
-    recipe['type'] = ntype
+    #recipe['type'] = ntype
 
     ri1 = line.find('{') + 1
     ri2 = line.find('}')
 
-    shapelist = [item.strip(" \'\"") for item in line[ri1:ri2].split(',')]
-    
+    shapelist = [item.strip() for item in line[ri1:ri2].split(",")]
+    #print shapelist
+
     layers = []
     for item in shapelist:
         found = item.find('Character.valueOf')
         if found < 0:
-            layers.append(item.strip(" \'\""))
+            layers.append(item.strip('\"'))
         else:
             break
-    
-    remaining = shapelist[len(layers):]
-    
-    characters = [item[item.find('\'')+1] for item in remaining[0::2]]
-    objects = [item for item in remaining[1::2]]
 
-    print characters
-    print objects
+    recipe['shape'] = layers
+    #print layers
+
+    remaining = " ".join(shapelist[len(layers):])
+    #print remaining
+    
+    #print len(remaining)
+    print remaining
+
+   
+    characters = [remaining[i+1]
+            for i in range(len(remaining))
+            if remaining[i] == "\'" and remaining[i+2] == "\'"]
+    #print characters
+
+    objects = []
+    substring = remaining
+    while len(substring) > 0:
+        founditem = substring.find("Items.")
+        foundblock = substring.find("Blocks.")
+        
+        # if nothing found, break
+        if founditem < 0 and foundblock < 0:
+            break
+
+        #if only one exists, pick it
+        if founditem < 0:
+            foundobject = foundblock
+        elif foundblock < 0:
+            foundobject = founditem
+
+        # otherwise both exist, so pick the smaller
+        else:
+            foundobject = min(foundblock, founditem)
+        
+        substring = substring[foundobject:]
+        foundspace = substring.find(" ")
+        
+        if foundspace >= 0:
+            item = substring[0:foundspace+1]
+            substring = substring[foundspace+1:]
+            objects.append(item.strip())
+        else:
+            item = substring[0:]
+            objects.append(item.strip())
+            break
+    
+    #print objects
+    recipe['mats'] = {}
+    for char,obj in zip(characters, objects):
+        recipe['mats'][char] = obj
+
+    print recipe
+    return recipe
+
 
 def getShapelessRecipe(line):
     pass
