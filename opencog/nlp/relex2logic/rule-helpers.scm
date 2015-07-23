dba9463884fbbddaa52b509bb805bdf3c0dc0227
rodsol <rodisolomon@gmail.com>
2015-07-23 21:10:09 +0300

    define adverbialpp

--- opencog/nlp/relex2logic/rule-helpers.scm
+++ opencog/nlp/relex2logic/rule-helpers.scm
@@ -419,6 +419,19 @@
 	(InheritanceLink  (SatisfyingSetLink (PredicateNode instance)) (ConceptNode adv_instance))
 	)
 )
+
+(define (adverbialpp-rule verb instance prep  prep_instance noun noun_instance)
+    (list (InheritanceLink  (ConceptNode noun_instance) (ConceptNode noun))
+    (ImplicationLink  (PredicateNode instance) (PredicateNode verb))
+    (ImplicationLink  (PredicateNode prep_instance) (PredicateNode prep))
+    (InheritanceLink
+        (SatisfyingSetLink (PredicateNode instance))
+        (SatisfyingSetLink
+            (EvaluationLink(PredicateNode prep_instance)
+                (ListLink (ConceptNode noun_instance)))
+        )
+	))
+)
 ;-----------------------------------------------------------------------
 ; prepositional phrase rule
 ;-----------------------------------------------------------------------
