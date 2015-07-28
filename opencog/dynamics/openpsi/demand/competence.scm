4ba160672cea96fc5a303d68753bd9255f67ae93
Amen Belayneh <amenbelayneh@gmail.com>
2015-07-28 07:56:47 +0000

    add psi demand = Competence

---
+++ opencog/dynamics/openpsi/demand/competence.scm
@@ -0,0 +1,26 @@
+; Energy psi-demand definition
+(InheritanceLink
+    ; The strength of the stv is the demand-value, & confidence is always 1.
+    (ConceptNode "OpenPsi: Competence" (stv .6 1))
+    (ConceptNode "OpenPsi: Demand")
+)
+
+; The
+(EvaluationLink
+    (PredicateNode "must_have_value_within")
+    (ListLink
+        (ConceptNode "OpenPsi: Competence")
+        (NumberNode .8)
+        (NumberNode 1.)
+    )
+)
+
+; actions that can act on the psi-demand Competence.
+(EvaluationLink
+    (PredicateNode "Psi: acts-on")
+    (ListLink
+        ; This is the default function that each psi-demand must have.
+        (GroundedSchemaNode "scm: psi-demand-updater")
+        (VariableNode "Demand")
+    )
+)
