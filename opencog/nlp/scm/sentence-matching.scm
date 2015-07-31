d7f71e318023b10e7a8a67497383f019f1e82326
leungmanhin <manhinleung@gmail.com>
2015-07-31 18:14:57 +0800

    Support return type

--- opencog/nlp/scm/sentence-matching.scm
+++ opencog/nlp/scm/sentence-matching.scm
@@ -17,12 +17,9 @@
 ; Possible result: 
 ;    (Pete ate apples .)
 ;
-(define (main-search input-sentence reject-list)
-    ; Make sure it is at least a SetLink before sending it to sureal for sentence generation
-    (define (to-sureal a-link) (if (equal? 'SetLink (cog-type a-link)) (sureal a-link) #f))
-
+(define (main-search input-sentence rej-list)
     ; Generate sentences from each of the R2L-SetLinks
-    (define (generate-sentences r2l-setlinks) (if (> (length r2l-setlinks) 0) (map to-sureal r2l-setlinks) '()))
+    (define (generate-sentences r2l-setlinks) (if (> (length r2l-setlinks) 0) (map sureal r2l-setlinks) '()))
 
     ; Check if the input-sentence is an actual sentence or a SentenceNode,
     ; parse and return the corresponding SentenceNode if it is a sentence
@@ -51,7 +48,8 @@
                             ))
                         ))
                     ))
-                    reject-list
+                    'SetLink
+                    rej-list
                 ))
             )
         )
