655773c99abd33e56f21b79f8e142df755efa42d
chenesan <pipio1994@gmail.com>
2015-08-06 11:41:25 +0000

    update the code which hasn't been merged in opencog and needed in project

--- opencog/opencog/cython/opencog/spatial.pyx
+++ opencog/opencog/cython/opencog/spatial.pyx
@@ -1,86 +1,135 @@
 from cython.operator cimport dereference as deref, preincrement as inc
-from opencog.atomspace cimport Handle
-
-
-
-cdef class Block3D:
-    cdef cBlock3D* cblock3d    
-
-    def __init__(self,long addr):
-        self.cblock3d = <cBlock3D*>PyLong_AsVoidPtr(addr)
-
-    @classmethod
-    def initNewBlock(self,int composedlevel,pos,material="",color="",candestroy=True):
-        assert len(pos)==3
-        cdef cBlockVector cpos = cBlockVector(pos[0],pos[1],pos[2])
-        cdef string cmaterial = material.encode('UTF-8')
-        cdef string ccolor = color.encode('UTF-8')
-        cdef cBlock3D* cblock = new cBlock3D(composedlevel,cpos,
-                                       cmaterial,ccolor,candestroy)
-        newblock=self(PyLong_FromVoidPtr(cblock))
-        return newblock
-        
-
-    def getLocation(self):
-        cdef cBlockVector cpos=self.cblock3d.getPosition()
-        return (cpos.x,cpos.y,cpos.z)
-    
-    def getMaterial(self):
-        cdef cBlockMaterial cmat=self.cblock3d.getBlockMaterial()
-        return cmat.materialType.decode('UTF-8')
-
-    def getMaterial(self):
-        cdef cBlockMaterial cmat=self.cblock3d.getBlockMaterial()
-        return cmat.color.decode('UTF-8')
+from opencog.atomspace cimport Handle, AtomSpace
 
 cdef class Octree3DMapManager:
-    
-    cdef cOctree3DMapManager* coctree3dmap 
+    cdef cOctree3DMapManager* c_octree_map
 
     def __cinit__(self):
         pass
 
     def __init__(self, long addr):
-        self.coctree3dmap = <cOctree3DMapManager*> PyLong_AsVoidPtr(addr)
+        self.c_octree_map = <cOctree3DMapManager*> PyLong_AsVoidPtr(addr)
 
     def __dealloc__(self):
         #spaceserver will handle this
         pass
 
     @classmethod
-    def initNewMap(self,_mapName, _xMin, _yMin, _zMin,  _xDim, _yDim, _zDim, _floorHeight):
-        cdef cOctree3DMapManager* cspmap = new cOctree3DMapManager(_mapName, _xMin, _yMin, _zMin,  _xDim, _yDim, _zDim, _floorHeight)
-        newmap=self(PyLong_FromVoidPtr(cspmap))
+    def init_new_map(cls, AtomSpace atomspace, map_name,
+                     resolution, floor_height, agent_height):
+        cdef cOctree3DMapManager* cspmap = new cOctree3DMapManager(atomspace.atomspace,
+                                                                   map_name, resolution,
+                                                                   floor_height,
+                                                                   agent_height)
+
+        newmap = cls(PyLong_FromVoidPtr(cspmap))
         return newmap
-    
-    def getMapName(self):
-        cdef string cname=self.coctree3dmap.getMapName()
-        return cname.decode('UTF-8')
 
-    def getObjectLocation(self,Handle objhandle):
-        if self.coctree3dmap.containsObject(deref((<Handle>objhandle).h))==False:
+    def get_floor_height(self):
+        return self.c_octree_map.getFloorHeight()
+
+    def get_map_name(self):
+        cdef string c_map_name = self.c_octree_map.getMapName()
+        return c_map_name.decode('UTF-8')
+
+    def get_agent_height(self):
+        return self.c_octree_map.getAgentHeight()
+
+    def set_agent_height(self,height):
+        self.c_octree_map.setAgentHeight(height)
+
+    def get_total_depth_of_octree(self):
+        return self.c_octree_map.getTotalDepthOfOctree()
+
+    def get_total_unit_block_num(self):
+        return self.c_octree_map.getTotalUnitBlockNum()
+
+    def get_log_odds_occupied_threshold(self):
+        return self.c_octree_map.getLogOddsOccupiedThreshold()
+
+    def set_log_odds_occupied_threshold(self,log_odds_occupancy):
+        self.c_octree_map.setLogOddsOccupiedThreshold(log_odds_occupancy)
+
+    def get_known_space_min_coord(self):
+        cdef cBlockVector c_pos = self.c_octree_map.getKnownSpaceMinCoord()
+        return (c_pos.x, c_pos.y, c_pos.z)
+
+    def get_known_space_max_coord(self):
+        cdef cBlockVector c_pos = self.c_octree_map.getKnownSpaceMaxCoord()
+        return (c_pos.x, c_pos.y, c_pos.z)
+
+    def get_known_space_dim(self):
+        cdef cBlockVector c_dim = self.c_octree_map.getKnownSpaceDim()
+        return (c_dim.x, c_dim.y, c_dim.z)
+
+    def add_solid_unit_block(self, Handle handle, pos):
+        assert len(pos) == 3
+        cdef cBlockVector c_pos = cBlockVector(pos[0], pos[1], pos[2])
+        self.c_octree_map.addSolidUnitBlock(deref((<Handle>handle).h), c_pos)
+
+    def remove_solid_unit_block(self, Handle handle):
+        self.c_octree_map.removeSolidUnitBlock(deref((<Handle>handle).h))
+
+    def set_unit_block(self,Handle handle, pos, update_log_odds_occupancy):
+        assert len(pos) == 3
+        cdef cBlockVector c_pos = cBlockVector(pos[0], pos[1], pos[2])
+        self.c_octree_map.setUnitBlock(deref((<Handle>handle).h), c_pos,
+                                       update_log_odds_occupancy)
+
+    def check_standable(self, pos, log_odds_occupancy = None):
+        assert len(pos) == 3
+        cdef cBlockVector c_pos = cBlockVector(pos[0], pos[1], pos[2])
+        if log_odds_occupancy is None:
+            log_odds_occupancy = self.c_octree_map.getLogOddsOccupiedThreshold()
+        return self.c_octree_map.checkStandable(c_pos, log_odds_occupancy)
+
+    def get_block(self, pos, log_odds_occupancy = None):
+        assert len(pos) == 3
+        cdef cBlockVector c_pos = cBlockVector(pos[0], pos[1], pos[2])
+        if log_odds_occupancy is None:
+            log_odds_occupancy = self.c_octree_map.getLogOddsOccupiedThreshold()
+        cdef cHandle cblock = self.c_octree_map.getBlock(c_pos, log_odds_occupancy)
+        return Handle(cblock.value())
+
+    def get_block_location(self, Handle handle, log_odds_occupancy = None):
+        if log_odds_occupancy is None:
+            log_odds_occupancy = self.c_octree_map.getLogOddsOccupiedThreshold()
+        cdef cHandle c_handle = deref((<Handle>handle).h)
+        cdef cBlockVector c_pos = self.c_octree_map.getBlockLocation(c_handle,
+                                                                     log_odds_occupancy)
+        #TODO: in BlockVector it returns BlockVector::ZERO, which is (0,0,0)
+        #if there's really one block in the zero point this will fail
+        if (c_pos.x, c_pos.y, c_pos.z) == (0,0,0):
             return None
-        cdef cBlockVector cvec=self.coctree3dmap.getObjectLocation(deref((<Handle>objhandle).h))
-        ret=(cvec.x,cvec.y,cvec.z)
-        return ret
-    
-    def addSolidUnitBlock(self,pos,Handle handle,material,color):
-        assert len(pos)==3
-        cdef cBlockVector cpos=cBlockVector(pos[0],pos[1],pos[2])
-        self.coctree3dmap.addSolidUnitBlock(cpos,deref((<Handle>handle).h),material,color)
+        return (c_pos.x, c_pos.y, c_pos.z)
 
-    def getBlockAtLocation(self,pos):
-        assert len(pos)==3
-        cdef cBlock3D* cblock= self.coctree3dmap.getBlockAtLocation(pos[0],pos[1],pos[2])
-        if <long>cblock == 0:
-            return None
-        ret=Block3D(<long>cblock)
-        return ret
-        
-    def getUnitBlockHandleFromLocation(self,pos):
-        assert len(pos)==3
-        cdef cBlockVector cvec=cBlockVector(pos[0],pos[1],pos[2])
-        cdef cHandle chandle=self.coctree3dmap.getUnitBlockHandleFromPosition(cvec)
+    def get_block_log_odds_occupancy(self, pos):
+        assert len(pos) == 3
+        cdef cBlockVector c_pos = cBlockVector(pos[0], pos[1], pos[2])
+        return self.c_octree_map.getBlockLogOddsOccupancy(c_pos)
 
-        return Handle(chandle.value())
-    
+    def add_none_block_entity(self, Handle handle, pos, isSelfObject, isAvatarEntity, timestamp):
+        assert len(pos) == 3
+        cdef cBlockVector c_pos = cBlockVector(pos[0], pos[1], pos[2])
+        self.c_octree_map.addNoneBlockEntity(deref((<Handle>handle).h), c_pos,
+                                             isSelfObject, isAvatarEntity, timestamp)
+
+    def remove_none_block_entity(self, Handle handle):
+        self.c_octree_map.removeNoneBlockEntity(deref((<Handle>handle).h))
+
+    def update_none_block_entity_location(self, Handle handle, pos, timestamp):
+        assert len(pos) == 3
+        cdef cBlockVector c_pos = cBlockVector(pos[0], pos[1], pos[2])
+        self.c_octree_map.updateNoneBlockEntityLocation(deref((<Handle>handle).h), c_pos,
+                                                        timestamp)
+
+    def get_last_appeared_location(self, Handle handle):
+        cdef cHandle c_handle = deref((<Handle>handle).h)
+        cdef cBlockVector c_pos = self.c_octree_map.getLastAppearedLocation(c_handle)
+        return (c_pos.x, c_pos.y, c_pos.z)
+
+    def get_entity(self, pos):
+        assert len(pos) == 3
+        cdef cBlockVector c_pos = cBlockVector(pos[0], pos[1], pos[2])
+        cdef cHandle c_handle = self.c_octree_map.getEntity(c_pos)
+        return Handle(c_handle.value())
--- opencog/opencog/spacetime/SpaceServer.h
+++ opencog/opencog/spacetime/SpaceServer.h
@@ -63,7 +63,8 @@ namespace opencog
 class AtomSpace;
 class TimeServer;
 class SpaceServerSavable;
-
+typedef std::string TimeDomain;
+extern TimeDomain DEFAULT_TIMEDOMAIN;
 /**
  * New SpaceServer for 3D map, using Octree3DMapManager, not using LocalSpaceMap2D anymore.
  * The biggest change in orgnization is every Map handle match one map for a scene (such as a room)
@@ -84,7 +85,6 @@ public:
     typedef spatial::Octree3DMapManager SpaceMap;
     typedef std::map<Handle, SpaceMap*> HandleToSpaceMap;
 
-
     explicit SpaceServer(AtomSpace&);
     virtual ~SpaceServer();
 
@@ -100,7 +100,7 @@ public:
      * Checks if this SpaceServer contains a map with the given handle
      **/
     const bool containsMap(Handle spaceMapHandle) const;
-	//	const bool containsMap(string spaceMapName) const;
+
     /**
      * Gets a const reference to the latest (more recent) map in this
      * SpaceServer
@@ -127,7 +127,8 @@ public:
      * just get it and set it to be the current map, 
      * not to create a new spaceMap
      */
-    Handle addOrGetSpaceMap(octime_t timestamp, std::string _mapName, unsigned resolution, int _floorHeight);
+
+    Handle addOrGetSpaceMap(octime_t timestamp, std::string _mapName,double _resolution, int _floorHeight, float _agentHeight, const TimeDomain& timeDomain = DEFAULT_TIMEDOMAIN);
 
     /**
      * comment@20150520 by YiShan
@@ -138,14 +139,13 @@ public:
      * Some member functions having the same problem also have the argument.
      */
     bool addSpaceInfo(Handle objectNode, Handle spaceMapHandle, 
-		      bool isSelfObject, octime_t timestamp,
-                      int objX, int objY, int objZ,
-                      int objLength, int objWidth, int objHeight,
-                      double objYaw,std::string entityClass, 
-					  std::string objectName, std::string material = "");
+                      bool isSelfObject, bool isAvatarEntity, 
+                      octime_t timestamp,
+                      double objX, double objY, double objZ,
+                      const TimeDomain& timeDomain = DEFAULT_TIMEDOMAIN);
 
 
-    void removeSpaceInfo(Handle objectNode, Handle spaceMapHandle, octime_t timestamp = 0);
+    void removeSpaceInfo(Handle objectNode, Handle spaceMapHandle, octime_t timestamp = 0, const TimeDomain& timeDomain = DEFAULT_TIMEDOMAIN);
 
     /**
      * SpaceServerContainer virtual methods:
@@ -205,28 +205,39 @@ public:
 
     void markCurMapPerceptedForFirstTime();
 
+	//
+	// Comment on 20150716 by Yi-Shan,
+	// The following is old functions about BlockEntity add/remove/query
+	// Because the BlockEntity feature has not been designed well, 
+	// so we comment out all the code related to BlockEntity
+	// Once we need to use it/decide to do it, maybe we'll need the legacy code.
+	
+
     /**
      * after the first time percept a map, 
      * we should find all the blockEntities on this map
      */
-    void findAllBlockEntitiesOnTheMap(Handle spaceMapHandle);
+    //void findAllBlockEntitiesOnTheMap(Handle spaceMapHandle);
 
     /**
      *  add all the newly constructed BlockEntity nodes to the atomspace
      */
-    void addBlockEntityNodes(HandleSeq &toUpdateHandles, Handle spaceMapHandle);
+    //void addBlockEntityNodes(HandleSeq &toUpdateHandles, Handle spaceMapHandle);
+
     /**
      *  add blocklist to an entity
      */
-    void addBlocksListPredicateToEntity(opencog::spatial::BlockEntity* _entity, const octime_t timeStamp, Handle spaceMapHandle);
+    //void addBlocksListPredicateToEntity(opencog::spatial::BlockEntity* _entity, const octime_t timeStamp, Handle spaceMapHandle, const TimeDomain& timeDomain = DEFAULT_TIMEDOMAIN);
+
 
     /**
      * add properties predicate link to an entity node when there is a change
      * this including addBlocksListPredicateToEntity
      */
-    void updateBlockEntityProperties(opencog::spatial::BlockEntity* entity, octime_t timestamp,Handle spaceMapHandle);
+    //void updateBlockEntityProperties(opencog::spatial::BlockEntity* entity, octime_t timestamp,Handle spaceMapHandle, const TimeDomain& timeDomain = DEFAULT_TIMEDOMAIN);
 
-    void updateBlockEntitiesProperties(octime_t timestamp, HandleSeq &toUpdateHandles, Handle spaceMapHandle);
+    //void updateBlockEntitiesProperties(octime_t timestamp, HandleSeq &toUpdateHandles, Handle spaceMapHandle, const TimeDomain& timeDomain = DEFAULT_TIMEDOMAIN);
+
 
 
 
