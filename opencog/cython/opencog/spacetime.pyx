2fb6a15ad6820d273f665bad1066ca076605d2de
chenesan <pipio1994@gmail.com>
2015-08-07 06:48:15 +0000

    fix format and update time domain param

--- opencog/cython/opencog/spacetime.pyx
+++ opencog/cython/opencog/spacetime.pyx
@@ -1,68 +1,77 @@
 from cython.operator cimport dereference as deref
 
-from opencog.atomspace cimport AtomSpace,Handle,TruthValue
+from opencog.atomspace cimport AtomSpace, Handle, TruthValue
 from opencog.spatial import Octree3DMapManager as SpaceMap
 
-#TO DEBUG:if we import opencog.spacetime before opencog.atomspace,
-#once we get an undefined handle h1 in python, which UUID is -1
-#then call h.is_undefined() method, it will return false!!
-#After check the uuid of h1 and h1.h.UNDEFINED.value(),the latter becomes 0
-#inside the is_undefined function!!
-#It's strange, but I don't know how to debug this,
-#to prevent this, you can choose import spacetime after the opencog.atomspace
-#(but it will cause spacetime atom types not included in atomspace.types)
-#or you can choose to check if it's undefined by h.value()==-1
-
-
 cdef class TimeServer:
-    cdef cTimeServer* ctimeserver
+    cdef cTimeServer* c_time_server
 
     def __cinit__(self,AtomSpace _a, SpaceServer _ss):
-        self.ctimeserver= new cTimeServer(deref((<AtomSpace>_a).atomspace),_ss.cspaceserver)
+        self.c_time_server = new cTimeServer(deref((<AtomSpace>_a).atomspace), _ss.c_space_server)
 
     def __dealloc__(self):
-        if self.ctimeserver:
-            del self.ctimeserver
+        if self.c_time_server:
+            del self.c_time_server
 
-    def addTimeInfo(self,Handle h,timestamp, TruthValue tv=TruthValue(1.0,1.0e35)):
-        cdef cHandle cattimelinkhandle = self.ctimeserver.addTimeInfo(deref((<Handle>h).h),timestamp, deref(tv._tvptr()))
-        return Handle(cattimelinkhandle.value())
+    def add_time_info(self,Handle h, timestamp,
+                      time_domain = None, TruthValue tv = TruthValue(1.0,1.0e35)):
+        cdef string c_time_domain = DEFAULT_TIMEDOMAIN if time_domain is None else time_domain.encode('UTF-8')
+
+        cdef cHandle c_at_time_link = self.c_time_server.addTimeInfo(deref((<Handle>h).h),
+                                                                     timestamp,
+                                                                     c_time_domain,
+                                                                     deref(tv._tvptr()))
+        return Handle(c_at_time_link.value())
 
 
 cdef class SpaceServer:
+    cdef cSpaceServer *c_space_server
 
-    cdef cSpaceServer *cspaceserver
-    def __cinit__(self,AtomSpace _a):
-        self.cspaceserver= new cSpaceServer(deref((<AtomSpace>_a).atomspace))
+    def __cinit__(self, AtomSpace _a):
+        self.c_space_server = new cSpaceServer(deref((<AtomSpace>_a).atomspace))
+
     def __dealloc__(self):
-        if self.cspaceserver:
-            self.cspaceserver.clear()
-            del self.cspaceserver
+        if self.c_space_server:
+            self.c_space_server.clear()
+            del self.c_space_server
 
-    def getMap(self,Handle handle):
-        mapinstance=SpaceMap(<long>(&(self.cspaceserver.getMap(deref((<Handle>handle).h)))))
-        return mapinstance
-    
-    def addMap(self,timestamp, mapName, resolution, infloorHeight, agentHeight):
-        cdef string cmapName=mapName.encode('UTF-8')
-        cdef cHandle ch=self.cspaceserver.addOrGetSpaceMap(timestamp, cmapName, resolution, infloorHeight, agentHeight)
-        pyhandle=Handle(ch.value())
-        return pyhandle
-    
-    def addMapInfo(self,Handle objectNode,Handle spacemapNode,
-                   isSelfObject,isAvatarEntity, timestamp,
-                      objX, objY, objZ):
-        cdef bool b=isSelfObject
-        cdef bool ret=self.cspaceserver.addSpaceInfo(deref((<Handle>objectNode).h),deref((<Handle>spacemapNode).h),isSelfObject,isAvatarEntity,timestamp,
-                      objX, objY, objZ)
+    def get_map(self,Handle handle):
+        cdef cHandle c_handle = deref((<Handle>handle).h)
+        map_instance = SpaceMap(<long>(&(self.c_space_server.getMap(c_handle))))
+        return map_instance
+
+    def add_map(self,timestamp, map_name, resolution, floor_height, agent_height, time_domain = None):
+        cdef string c_map_name = map_name.encode('UTF-8')
+        cdef string c_time_domain = DEFAULT_TIMEDOMAIN if time_domain is None else time_domain.encode('UTF-8')
+
+        cdef cHandle ch = self.c_space_server.addOrGetSpaceMap(timestamp, c_map_name,
+                                                               resolution, floor_height,
+                                                               agent_height, c_time_domain)
+        handle = Handle(ch.value())
+        return handle
+
+    def add_map_info(self,Handle object_node, Handle space_map_node,
+                     is_self_object, is_avatar_entity, timestamp,
+                     obj_x, obj_y, obj_z, time_domain = None):
+        cdef string c_time_domain = DEFAULT_TIMEDOMAIN if time_domain is None else time_domain.encode('UTF-8')
+
+        cdef bool ret = self.c_space_server.addSpaceInfo(deref((<Handle>object_node).h),
+                                                         deref((<Handle>space_map_node).h),
+                                                         is_self_object, is_avatar_entity,
+                                                         timestamp, obj_x, obj_y, obj_z,
+                                                         c_time_domain)
         return ret
 
-    
-    def removeMapInfo(self,Handle objectNode,Handle spacemapNode,timestamp):
-        self.cspaceserver.removeSpaceInfo(deref((<Handle>objectNode).h),deref((<Handle>spacemapNode).h),timestamp)
+    def remove_map_info(self,Handle object_node,Handle space_map_node,
+                        timestamp, time_domain = None):
+        cdef string c_time_domain = DEFAULT_TIMEDOMAIN if time_domain is None else time_domain.encode('UTF-8')
+
+        self.c_space_server.removeSpaceInfo(deref((<Handle>object_node).h),
+                                            deref((<Handle>space_map_node).h),
+                                            timestamp, c_time_domain)
 
     def clear(self):
-        self.cspaceserver.clear()
-    
-    def setTimeServer(self,TimeServer timeserver):
-        self.cspaceserver.setTimeServer(timeserver.ctimeserver)
+        self.c_space_server.clear()
+
+    def set_time_server(self,TimeServer time_server):
+        self.c_space_server.setTimeServer(time_server.c_time_server)
--- tests/cython/spatial/test_Octree3DMap.py
+++ tests/cython/spatial/test_Octree3DMap.py
@@ -1,231 +1,217 @@
 import unittest
 import opencog.spacetime #for import spacetime types
-from opencog.spatial import Octree3DMapManager    
+from opencog.spatial import Octree3DMapManager
 from opencog.atomspace import AtomSpace,Handle,types
 
 class TestMap(unittest.TestCase):
 
     def setUp(self):
-        self._atomspace=AtomSpace()
-        resolution=1
-        floorHeight=-255
-        agentHeight=1.6
-        self._testmap=Octree3DMapManager.initNewMap(self._atomspace,"testmap",resolution,floorHeight,agentHeight)
+        self.atomspace = AtomSpace()
+        resolution = 1
+        floor_height = -255
+        agent_height = 1.6
+        self.testmap = Octree3DMapManager.init_new_map(self.atomspace, "testmap", resolution,
+                                                       floor_height, agent_height)
 
     def tearDown(self):
-        del self._testmap
-        del self._atomspace
+        del self.testmap
+        del self.atomspace
 
-    def test_getMapName(self):
-        self.assertEqual("testmap",self._testmap.getMapName())
+    def test_GetMapName(self):
+        self.assertEqual("testmap", self.testmap.get_map_name())
 
     def test_GetBlock_NoBlockAdded_ReturnUndefinedHandle(self):
-        testpos=(7,8,9)
+        test_pos = (7, 8, 9)
 
-        testHandle=self._testmap.getBlock(testpos)
+        test_handle = self.testmap.get_block(test_pos)
 
-        self.assertTrue(testHandle.is_undefined())
+        self.assertTrue(test_handle.is_undefined())
 
     def testBinaryAddandRemove_NormalUnitBlock_AllGetFunctionWork(self):
-        testpos1=(7,8,9)
-        testHandle1=Handle(100)
+        test_pos1 = (7, 8, 9)
+        test_handle1 = Handle(100)
 
-        self._testmap.addSolidUnitBlock(testHandle1,testpos1)
-        testHandle2=self._testmap.getBlock(testpos1)
-        self.assertEqual(testHandle1,testHandle2)
-        self.assertTrue(self._testmap.checkIsSolid(testpos1))
-        self.assertEqual(testpos1,self._testmap.getBlockLocation(testHandle1))
+        self.testmap.add_solid_unit_block(test_handle1, test_pos1)
+        test_handle2 = self.testmap.get_block(test_pos1)
+        self.assertEqual(test_handle1, test_handle2)
+        self.assertTrue(self.testmap.check_is_solid(test_pos1))
+        self.assertEqual(test_pos1, self.testmap.get_block_location(test_handle1))
 
-        self._testmap.removeSolidUnitBlock(testHandle1)
-        testHandle3=self._testmap.getBlock(testpos1)
-        self.assertTrue(testHandle3.is_undefined())
-        self.assertFalse(self._testmap.checkIsSolid(testpos1))
-        self.assertIsNone(self._testmap.getBlockLocation(testHandle1))
+        self.testmap.remove_solid_unit_block(test_handle1)
+        test_handle3 = self.testmap.get_block(test_pos1)
+        self.assertTrue(test_handle3.is_undefined())
+        self.assertFalse(self.testmap.check_is_solid(test_pos1))
+        self.assertIsNone(self.testmap.get_block_location(test_handle1))
 
     def testAddSolidUnitBlock__PositionOverBorder__GetBlockFailed(self):
-        border=32768
-        testpos1=(border,8,9)
-        testHandle1=Handle(100)
+        border = 32768
+        test_pos1 = (border, 8, 9)
+        test_handle1 = Handle(100)
 
-        self._testmap.addSolidUnitBlock(testHandle1,testpos1)
-        testHandle2=self._testmap.getBlock(testpos1)
+        self.testmap.add_solid_unit_block(test_handle1, test_pos1)
+        test_handle2 = self.testmap.get_block(test_pos1)
 
-        self.assertTrue(testHandle2.is_undefined())
+        self.assertTrue(test_handle2.is_undefined())
 
     def testSetBlock_AddBlockWithProbabilityControl_GetFunctionsWorkWithProb(self):
-	
-        testpos1=(7,8,9)
-        testHandle1=Handle(100)
-        logOddThreshold=self._testmap.getLogOddsOccupiedThreshold()
+        test_pos1 = (7, 8, 9)
+        test_handle1 = Handle(100)
+        log_odds_threshold = self.testmap.get_log_odds_occupied_threshold()
 
-        self._testmap.setUnitBlock(testHandle1,testpos1,logOddThreshold)
-    
-        #test binary getBlock
-        self.assertEqual(testHandle1,self._testmap.getBlock(testpos1))
-        self.assertEqual(testHandle1,self._testmap.getBlock(testpos1,logOddThreshold))
-        self.assertTrue(self._testmap.checkIsSolid(testpos1))
-        # test prob checkIsSolid
-        self.assertTrue(self._testmap.checkIsSolid(testpos1,logOddThreshold))
-        self.assertEqual(testpos1,self._testmap.getBlockLocation(testHandle1))
-        #test getBlockLocation
-        self.assertEqual(testpos1,self._testmap.getBlockLocation(testHandle1,logOddThreshold))
+        self.testmap.set_unit_block(test_handle1, test_pos1, log_odds_threshold)
+
+        #test binary get_block
+        self.assertEqual(test_handle1, self.testmap.get_block(test_pos1))
+        self.assertEqual(test_handle1, self.testmap.get_block(test_pos1, log_odds_threshold))
+        self.assertTrue(self.testmap.check_is_solid(test_pos1))
+        # test prob check_is_solid
+        self.assertTrue(self.testmap.check_is_solid(test_pos1, log_odds_threshold))
+        self.assertEqual(test_pos1, self.testmap.get_block_location(test_handle1))
+        #test get_block_location
+        self.assertEqual(test_pos1, self.testmap.get_block_location(test_handle1,
+                                                                    log_odds_threshold))
 
         #change the occupancy so it's small enough to make getter find nothing
-        self._testmap.setUnitBlock(testHandle1,testpos1,-0.1)
+        self.testmap.set_unit_block(test_handle1, test_pos1, -0.1)
 
-        self.assertTrue(self._testmap.getBlock(testpos1).is_undefined())
-        self.assertTrue(self._testmap.getBlock(testpos1,logOddThreshold).is_undefined())
-        self.assertFalse(self._testmap.checkIsSolid(testpos1))
-        self.assertFalse(self._testmap.checkIsSolid(testpos1,logOddThreshold))
-        self.assertIsNone(self._testmap.getBlockLocation(testHandle1))
-        self.assertIsNone(self._testmap.getBlockLocation(testHandle1,logOddThreshold))
-		
+        self.assertTrue(self.testmap.get_block(test_pos1).is_undefined())
+        self.assertTrue(self.testmap.get_block(test_pos1, log_odds_threshold).is_undefined())
+        self.assertFalse(self.testmap.check_is_solid(test_pos1))
+        self.assertFalse(self.testmap.check_is_solid(test_pos1, log_odds_threshold))
+        self.assertIsNone(self.testmap.get_block_location(test_handle1))
+        self.assertIsNone(self.testmap.get_block_location(test_handle1,log_odds_threshold))
+
         #change the threshold, so the occupancy is large enough to find it
-        self._testmap.setLogOddsOccupiedThreshold(-0.2)
-        logOddThreshold=self._testmap.getLogOddsOccupiedThreshold()
-        self.assertEqual(testHandle1,self._testmap.getBlock(testpos1))
-        self.assertEqual(testHandle1,self._testmap.getBlock(testpos1,logOddThreshold))
+        self.testmap.set_log_odds_occupied_threshold(-0.2)
+        log_odds_threshold = self.testmap.get_log_odds_occupied_threshold()
+        self.assertEqual(test_handle1, self.testmap.get_block(test_pos1))
+        self.assertEqual(test_handle1, self.testmap.get_block(test_pos1, log_odds_threshold))
 
-        self.assertTrue(self._testmap.checkIsSolid(testpos1))
-        self.assertTrue(self._testmap.checkIsSolid(testpos1,logOddThreshold))
-        self.assertEqual(testpos1,self._testmap.getBlockLocation(testHandle1))
-        self.assertEqual(testpos1,self._testmap.getBlockLocation(testHandle1,logOddThreshold))
-	
+        self.assertTrue(self.testmap.check_is_solid(test_pos1))
+        self.assertTrue(self.testmap.check_is_solid(test_pos1, log_odds_threshold))
+        self.assertEqual(test_pos1, self.testmap.get_block_location(test_handle1))
+        self.assertEqual(test_pos1, self.testmap.get_block_location(test_handle1,
+                                                                    log_odds_threshold))
+
     def testStandable_NormalBlock_Standable(self):
-	
         #case1: single block
-        testpos=(1,2,4)
-        blockpos1=(1,2,3)
-        self.assertFalse(self._testmap.checkStandable(testpos))
-        testBlock1=self._atomspace.add_node(types.StructureNode,"block1").h
-        materialNode1=self._atomspace.add_node(types.ConceptNode,"dirt").h
-	materialPredNode=self._atomspace.add_node(types.PredicateNode,"material").h
-        listLink=self._atomspace.add_link(types.ListLink,
-                                                  [testBlock1,materialNode1]).h
-        evalLink=self._atomspace.add_link(types.EvaluationLink,
-                                                  [materialPredNode,listLink]).h
-        self._testmap.addSolidUnitBlock(testBlock1,blockpos1)
+        test_pos = (1, 2, 4)
+        block_pos = (1, 2, 3)
+        self.assertFalse(self.testmap.check_standable(test_pos))
+        test_block = self.atomspace.add_node(types.StructureNode, "block1").h
+        material_node = self.atomspace.add_node(types.ConceptNode, "dirt").h
+        material_pred_node = self.atomspace.add_node(types.PredicateNode, "material").h
+        list_link = self.atomspace.add_link(types.ListLink, [test_block,material_node]).h
+        eval_link = self.atomspace.add_link(types.EvaluationLink,
+                                            [material_pred_node,list_link]).h
+        self.testmap.add_solid_unit_block(test_block, block_pos)
 
-        standable=self._testmap.checkStandable(testpos)
+        standable = self.testmap.check_standable(test_pos)
 
         self.assertTrue(standable)
-	
 
-	
     def testStandable_WaterBlock_Unstandable(self):
-	
         #case2: single block which is water, cannot stand on water
-        testpos=(1,2,4)
-        blockpos1=(1,2,3)
-        self.assertFalse(self._testmap.checkStandable(testpos))
-        testBlock1=self._atomspace.add_node(types.StructureNode,"block1").h
-        materialNode1=self._atomspace.add_node(types.ConceptNode,"water").h
-	materialPredNode=self._atomspace.add_node(types.PredicateNode,"material").h
-        listLink=self._atomspace.add_link(types.ListLink,
-                                                  [testBlock1,materialNode1]).h
-        evalLink=self._atomspace.add_link(types.EvaluationLink,
-                                                  [materialPredNode,listLink]).h
-        self._testmap.addSolidUnitBlock(testBlock1,blockpos1)
+        test_pos = (1, 2, 4)
+        block_pos = (1, 2, 3)
+        self.assertFalse(self.testmap.check_standable(test_pos))
+        test_block = self.atomspace.add_node(types.StructureNode, "block1").h
+        material_node = self.atomspace.add_node(types.ConceptNode, "water").h
+        material_pred_node = self.atomspace.add_node(types.PredicateNode, "material").h
+        list_link = self.atomspace.add_link(types.ListLink, [test_block,material_node]).h
+        eval_link = self.atomspace.add_link(types.EvaluationLink,
+                                          [material_pred_node,list_link]).h
+        self.testmap.add_solid_unit_block(test_block, block_pos)
 
-        standable=self._testmap.checkStandable(testpos)
+        standable = self.testmap.check_standable(test_pos)
 
         self.assertFalse(standable)
-	
-	
+
+
     def testStandable_TwoNearBlock_Unstandable(self):
-	
         # case3: two block which z coord is close ( ditance < agentHeight)
         # so it's not standable
-        testpos=(1,2,4)
-        blockpos1=(1,2,3)
-        self.assertFalse(self._testmap.checkStandable(testpos))
-        testBlock1=self._atomspace.add_node(types.StructureNode,"block1").h
-        materialNode1=self._atomspace.add_node(types.ConceptNode,"dirt").h
-	materialPredNode=self._atomspace.add_node(types.PredicateNode,"material").h
-        listLink1=self._atomspace.add_link(types.ListLink,
-                                           [testBlock1,materialNode1]).h
-        evalLink1=self._atomspace.add_link(types.EvaluationLink,
-                                           [materialPredNode,listLink1]).h
-        self._testmap.addSolidUnitBlock(testBlock1,blockpos1)
-		
-        blockpos2=(1,2,5)
-        testBlock2=self._atomspace.add_node(types.StructureNode,"block2").h
-        materialNode2=self._atomspace.add_node(types.ConceptNode,"stone").h
-	materialPredNode=self._atomspace.add_node(types.PredicateNode,"material").h
-        listLink2=self._atomspace.add_link(types.ListLink,
-                                           [testBlock2,materialNode2]).h
-        evalLink2=self._atomspace.add_link(types.EvaluationLink,
-                                           [materialPredNode,listLink2]).h
-        self._testmap.addSolidUnitBlock(testBlock2,blockpos2)
-        self.assertFalse(self._testmap.getBlock(blockpos2).is_undefined())
-        self.assertFalse(self._testmap.getBlock(blockpos1).is_undefined())
-        standable=self._testmap.checkStandable(testpos)
-    
-        self.assertFalse(standable)
-	
+        test_pos = (1, 2, 4)
+        block_pos1 = (1, 2, 3)
+        self.assertFalse(self.testmap.check_standable(test_pos))
+        test_block1 = self.atomspace.add_node(types.StructureNode, "block1").h
+        material_node1 = self.atomspace.add_node(types.ConceptNode, "dirt").h
+        material_pred_node = self.atomspace.add_node(types.PredicateNode,"material").h
+        list_link1 = self.atomspace.add_link(types.ListLink, [test_block1,material_node1]).h
+        eval_link1 = self.atomspace.add_link(types.EvaluationLink,
+                                             [material_pred_node,list_link1]).h
+        self.testmap.add_solid_unit_block(test_block1, block_pos1)
 
+        blockpos2 = (1, 2, 5)
+        testBlock2 = self.atomspace.add_node(types.StructureNode, "block2").h
+        material_node2 = self.atomspace.add_node(types.ConceptNode, "stone").h
+        material_pred_node = self.atomspace.add_node(types.PredicateNode, "material").h
+        list_link2 = self.atomspace.add_link(types.ListLink, [testBlock2, material_node2]).h
+        eval_link2 = self.atomspace.add_link(types.EvaluationLink,
+                                             [material_pred_node,list_link2]).h
+        self.testmap.add_solid_unit_block(testBlock2, blockpos2)
+        self.assertFalse(self.testmap.get_block(blockpos2).is_undefined())
+        self.assertFalse(self.testmap.get_block(block_pos1).is_undefined())
+        standable = self.testmap.check_standable(test_pos)
+
+        self.assertFalse(standable)
 
     def testAddandRemoveNoneBlockEntity_NormalEntity_AllGetFunctionsWork(self):
-	
-        testpos1=(17,28,39)
-	timestamp1=12345
-        testHandle1=self._atomspace.add_node(types.EntityNode,"entity").h
-        entityIsSelf=False
-        entityIsAvatar=False
+        test_pos = (17, 28, 39)
+        timestamp = 12345
+        test_handle1 = self.atomspace.add_node(types.EntityNode, "entity").h
+        entity_is_self = False
+        entity_is_avatar = False
 
-        self._testmap.addNoneBlockEntity(testHandle1,testpos1,
-					entityIsSelf, entityIsAvatar, 
-					timestamp1)
+        self.testmap.add_none_block_entity(test_handle1,test_pos,
+                                           entity_is_self, entity_is_avatar,
+                                           timestamp)
 
-        testHandle2=self._testmap.getEntity(testpos1)
-        self.assertEqual(testHandle1,testHandle2)
-        self.assertEqual(testpos1,self._testmap.getLastAppearedLocation(testHandle1))
-        self._testmap.removeNoneBlockEntity(testHandle1)
-		
-        self.assertTrue(self._testmap.getEntity(testpos1).is_undefined())
+        test_handle2 = self.testmap.get_entity(test_pos)
+        self.assertEqual(test_handle1, test_handle2)
+        self.assertEqual(test_pos, self.testmap.get_last_appeared_location(test_handle1))
+        self.testmap.remove_none_block_entity(test_handle1)
+
+        self.assertTrue(self.testmap.get_entity(test_pos).is_undefined())
         #preserve record
-        self.assertEqual(testpos1,self._testmap.getLastAppearedLocation(testHandle1))
+        self.assertEqual(test_pos, self.testmap.get_last_appeared_location(test_handle1))
 
     def testUpdateEntityLocation_MultipleLocation_LastLocationIsPos2(self):
+        test_pos1 = (17, 28, 39)
+        timestamp1 = 12345
+        test_handle1 = self.atomspace.add_node(types.EntityNode, "entity").h
+        entity_is_self = False
+        entity_is_avatar = False
 
-        testpos1=(17,28,39)
-	timestamp1=12345
-        testHandle1=self._atomspace.add_node(types.EntityNode,"entity").h
-        entityIsSelf=False
-        entityIsAvatar=False
+        self.testmap.add_none_block_entity(test_handle1, test_pos1,
+                                           entity_is_self, entity_is_avatar,
+                                           timestamp1)
 
-        self._testmap.addNoneBlockEntity(testHandle1,testpos1,
-					entityIsSelf, entityIsAvatar, 
-					timestamp1)
+        test_pos2 = (17, 28, 40)
+        timestamp2 = 12346
+        self.testmap.update_none_block_entity_location(test_handle1, test_pos2, timestamp2)
+        last_location = self.testmap.get_last_appeared_location(test_handle1)
 
-        testpos2=(17,28,40)
-        timestamp2=12346
-        self._testmap.updateNoneBlockEntityLocation(testHandle1,testpos2,timestamp2)
-        lastLocation=self._testmap.getLastAppearedLocation(testHandle1)
-
-        self.assertEqual(testpos2,lastLocation)
+        self.assertEqual(test_pos2, last_location)
 
     def testAddEntity_PressureTest(self):
-	
-        count=10000
-        timestamp=12345
-        while count!=0:
+        count = 10000
+        timestamp = 12345
+        while count != 0:
             print count
-            count-=1
-            testpos1=(count,count,count)
-            testHandle1=self._atomspace.add_node(types.EntityNode,"entity"+str(count)).h
-            entityIsSelf=False
-            entityIsAvatar=False
+            count -= 1
+            test_pos = (count, count, count)
+            test_handle1 = self.atomspace.add_node(types.EntityNode, "entity"+str(count)).h
+            entity_is_self = False
+            entity_is_avatar = False
 
-            self._testmap.addNoneBlockEntity(testHandle1,testpos1,
-                                             entityIsSelf, entityIsAvatar, timestamp)
-            testHandle2=self._testmap.getEntity(testpos1)
-            self.assertEqual(testHandle1,testHandle2)
-            self.assertEqual(testpos1,self._testmap.getLastAppearedLocation(testHandle1))
-            self._testmap.removeNoneBlockEntity(testHandle1)
-			
-            self.assertTrue(self._testmap.getEntity(testpos1).is_undefined())
+            self.testmap.add_none_block_entity(test_handle1, test_pos,
+                                               entity_is_self, entity_is_avatar, timestamp)
+            test_handle2 = self.testmap.get_entity(test_pos)
+            self.assertEqual(test_handle1, test_handle2)
+            self.assertEqual(test_pos,self.testmap.get_last_appeared_location(test_handle1))
+            self.testmap.remove_none_block_entity(test_handle1)
+
+            self.assertTrue(self.testmap.get_entity(test_pos).is_undefined())
             #preserve record
-            self.assertEqual(testpos1,self._testmap.getLastAppearedLocation(testHandle1))	
-		
-	
+            self.assertEqual(test_pos,self.testmap.get_last_appeared_location(test_handle1))
