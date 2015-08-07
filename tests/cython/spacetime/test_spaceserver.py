70da20690d72df564994cf624e6457c6097ec6ce
chenesan <pipio1994@gmail.com>
2015-08-07 06:56:42 +0000

    fix format

--- tests/cython/spacetime/test_spaceserver.py
+++ tests/cython/spacetime/test_spaceserver.py
@@ -1,33 +1,34 @@
-from opencog.spacetime import SpaceServer,TimeServer
-from opencog.atomspace import AtomSpace,Handle,types
+from opencog.spacetime import SpaceServer, TimeServer
+from opencog.atomspace import AtomSpace, Handle, types
 
 class TestSpaceServer:
 
     def setUp(self):
-        self._atomspace=AtomSpace()
-        self._testserver=SpaceServer(self._atomspace)
-        self._timeserver=TimeServer(self._atomspace,self._testserver)
-        self._testserver.setTimeServer(self._timeserver)
+        self._atomspace = AtomSpace()
+        self.space_server = SpaceServer(self._atomspace)
+        self.time_server = TimeServer(self._atomspace, self.space_server)
+        self.space_server.set_time_server(self.time_server)
         
     def tearDown(self):
-        del self._testserver
+        del self.space_server
         del self._atomspace
 
     def test_addMap(self):
-        handle=self._testserver.addMap(123456,"testmap",1,0,1)
-        assert self._atomspace.get_name(handle)=="testmap"
+        handle = self.space_server.add_map(123456, "testmap", 1, 0, 1)
+        assert self._atomspace.get_name(handle) == "testmap"
     
     def test_getMap(self):
-        handle=self._testserver.addMap(123456,"testmap",1,0,1)
-        mapinstance=self._testserver.getMap(handle)
-        assert mapinstance.getMapName()=="testmap"
+        handle = self.space_server.add_map(123456, "testmap", 1, 0, 1)
+        mapinstance = self.space_server.get_map(handle)
+        assert mapinstance.get_map_name() == "testmap"
 
     
     def test_addAndRemoveMapInfo(self):
-        maphandle=self._testserver.addMap(123456,"testmap",1,0,1)
-        objnode=self._atomspace.add_node(types.StructureNode,"object111")
-        assert self._testserver.addMapInfo(objnode.h,maphandle,False,False,123456,4,5,6)==True
-        mapinstance=self._testserver.getMap(maphandle)        
-        assert mapinstance.getBlockLocation(objnode.h)==(4,5,6)
-        self._testserver.removeMapInfo(objnode.h,maphandle,234567)
-        assert mapinstance.getBlockLocation(objnode.h)==None    
+        map_handle = self.space_server.add_map(123456,"testmap",1,0,1)
+        obj_node = self._atomspace.add_node(types.StructureNode,"object111")
+        assert self.space_server.add_map_info(obj_node.h, map_handle,
+                                             False, False, 123456, 4, 5, 6) == True
+        map_instance = self.space_server.get_map(map_handle) 
+        assert map_instance.get_block_location(obj_node.h) == (4, 5, 6)
+        self.space_server.remove_map_info(obj_node.h, map_handle, 234567)
+        assert map_instance.get_block_location(obj_node.h) == None    
--- tests/cython/spacetime/test_timeserver.py
+++ tests/cython/spacetime/test_timeserver.py
@@ -1,19 +1,19 @@
-from opencog.spacetime import SpaceServer,TimeServer
-from opencog.atomspace import AtomSpace,Handle,types
+from opencog.spacetime import SpaceServer, TimeServer
+from opencog.atomspace import AtomSpace, Handle, types
 
 class TestTimeServer:
 
     def setUp(self):
-        self._atomspace=AtomSpace()
-        self._spaceserver=SpaceServer(self._atomspace)
-        self._timeserver=TimeServer(self._atomspace,self._spaceserver)
-        self._spaceserver.setTimeServer(self._timeserver)
+        self._atomspace = AtomSpace()
+        self._spaceserver = SpaceServer(self._atomspace)
+        self._timeserver = TimeServer(self._atomspace, self._spaceserver)
+        self._spaceserver.set_time_server(self._timeserver)
         
     def tearDown(self):
         del self._spaceserver
         del self._atomspace
 
     def test_addTimeInfo(self):
-        objnode=self._atomspace.add_node(types.StructureNode,"object111")
-        attimelink=self._timeserver.addTimeInfo(objnode.h,123456)
-        assert objnode in self._atomspace.get_outgoing(attimelink)
+        objnode = self._atomspace.add_node(types.StructureNode, "object111")
+        at_time_link = self._timeserver.add_time_info(objnode.h, 123456)
+        assert objnode in self._atomspace.get_outgoing(at_time_link)
