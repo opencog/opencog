0ff4cf03a97cfbabf3a39bc32a6eae5d3c49f52c
Linas Vepstas <linasvepstas@gmail.com>
2015-08-13 22:36:34 -0500

    Fix module loading order.
    Turns out, if you load the py-shell module too early, the system
    crashes, in a most mysterious way.  I was unable to debug it.
    Loading it later seems to cure the problem.

--- opencog/scm/opencog/cogserver.scm
+++ opencog/scm/opencog/cogserver.scm
@@ -3,6 +3,10 @@
 ;
 (define-module (opencog cogserver))
 
+
+; Hack LD path, but argh, this doesn't work!
+; (setenv "LD_LIBRARY_PATH" "/usr/local/lib/opencog/modules")
+
 (load-extension "libguile-cogserver" "opencog_cogserver_init")
 
 ; config path name is optional.
