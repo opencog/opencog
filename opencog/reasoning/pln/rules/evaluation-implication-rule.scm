18815c83b6d7f2bbf8ab486f030f3f766b907953
Prateek Saxena <prateeksaxena2809@gmail.com>
2015-08-17 12:26:48 +0530

    Added comments and discount factor

--- opencog/reasoning/pln/rules/evaluation-implication-rule.scm
+++ opencog/reasoning/pln/rules/evaluation-implication-rule.scm
@@ -13,6 +13,15 @@
 ;   C
 ;   B
 ; -----------------------------------------------------------------------------
+; Based on Chapter 10: Higher Order Extensional Inference
+; This rule handles some cases of PLN higher order logic that don't
+; require quantifiers
+
+; ImplicationLink PredicateNode:A PredicateNode:A
+; can be handled trivially using the existing first-order PLN rules.
+; This is where we handle a special case.
+
+
 (load "formulas.scm")
 
 (define pln-rule-evaluation-implication
@@ -57,9 +66,45 @@
                 (cog-stv-strength C)
                 (cog-stv-strength AB)
                 (cog-stv-strength AC))
-            (min
-                (cog-stv-confidence B)
-                (cog-stv-confidence A)
-                (cog-stv-confidence C)
-                (cog-stv-confidence AC)
-                (* 0.9 (cog-stv-confidence AB))))))
+            (*
+                0.9
+                (min
+                    (cog-stv-confidence B)
+                    (cog-stv-confidence A)
+                    (cog-stv-confidence C)
+                    (cog-stv-confidence AC)
+                    (* 0.9 (cog-stv-confidence AB)))))))
+
+; Similarity substitution Rule is equivalent to combining that rule with
+; Abduction
+; 
+; Deduction:
+;   A does B
+;   B causes C
+;   |-
+;   A causes C
+;
+; Abduction:
+;   A does C
+;   A isa B
+;   |-
+;   B's do C
+;
+; Implication A B
+;   Implication A C (from Eval C A)
+;   |- 
+;   Abduction Rule
+;       Implication B C
+;   
+; Induction:
+;   B causes A
+;   B causes C
+;   |-
+;   A is C
+;
+; Maybe you should just use Mem2Inh and Inh2Mem instead (or even convert
+; everything into inhlinks to see what would happen!)
+; Eval2Implication should be a special case but would only work for
+; one-argument versions of the predicates...
+; Eval2Member + Member2Inheritance + Rules + Inheritance2Member
+
