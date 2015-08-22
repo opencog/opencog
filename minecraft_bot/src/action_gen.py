4ada25dbc7cac3b3597e33ef47303d5438cf6201
chenesan <pipio1994@gmail.com>
2015-08-22 11:26:19 +0000

    Update docstring in action and attention modules

--- minecraft_bot/src/action_gen.py
+++ minecraft_bot/src/action_gen.py
@@ -1,22 +1,78 @@
+# action_gen.py
+#! /usr/bin/env python2.7 python2 python
+""" module to generate action in Opencog
+
+This is the part to decide and execute action in Minecraft embodiment.
+For now all the work is handled by ActionGenerator class.
+It will determine the behavior by a behavior tree and
+execute this behavior by calling GPN/GSN.
+
+TODO:
+In the future we should seperate the decision making
+and action execution to make the ActionGenerator class cleaner.
+Also we should have a more systematic way to handle decision,
+not just determining behavior by if-else statement.
+
+available class:
+    ActionGenerator: used in the main loop of Opencog bot to decide action
+"""
 from opencog.atomspace import types, TruthValue
 from opencog.type_constructors import *
 from opencog.bindlink import bindlink, evaluate_atom
 from opencog.atomspace import Atom
 class ActionGenerator:
+    """ determining and executing action of Opencog bot in each loop
 
-    """
-    for testing, we set a target block(gold, which block id is 14)
-    when generating action it will find the target block at first,
-    and check if its sti is large enough. If so, move toward it(assume it's superflat world),
-    If no result, change look or random move.
+    The main loop will call the generate_action() to do all the work.
+
+    Args:
+        atomspace(opencog.atomspace.AtomSpace): the AtomSpace in main loop
+        space_server(opencog.spacetime.SpaceServer): the SpaceServer in main loop
+        time_server(opencog.spacetime.TimeServer): the TimeServer in main loop
+    Method:
+        generate_action(): generate and execute the action by behavior tree
     """
     def __init__(self, atomspace, space_server, time_server):
-        self.atomspace = atomspace
-        self.space_server = space_server
-        self.time_server = time_server
+        self._atomspace = atomspace
+        self._space_server = space_server
+        self._time_server = time_server
 
     def generate_action(self):
-        result = bindlink(self.atomspace,
+        """ generate and execute the action by behavior tree
+
+        Now (20150822) we generate action by such a behavior tree:
+
+        If target block found in atomspace:
+            If target block is attractive(sti is large enough):
+                move to the near place of the target block
+        else:
+            moving 2 units toward yaw = 90 degree
+
+        For testing, we set a target block(gold, which block id is 14)
+        And we assume the world is superflat world so we only need to move on
+        x-y direction.
+        Note that because the behavior of GSN/GPN,
+        we have to import all schemas(action_schemas.py)
+        in the main script(opencog_initializer.py).
+        Or it will fail to find the function name.
+
+        TODO:
+            Build random action function: It's for the 'else' part
+                in above behavior tree. This will make the bot behaves more
+                naturally.
+            Add pathfinding technique: so the bot can calculate the procedure to
+                get to the destination.
+                it will be able to move to any place he wants.
+                (ex. place block to jump to a higher place)
+                In Opencog there has been an OCPlanner implementation.
+                It should be possible to migrate the old code.
+            More higher level ways to making decision: For now it's just testing
+                so we only use a simple behavior tree to demo. But in general,
+                we should regard these behavior tree as lower level schemas.
+                And we should use a higher level cognition ways(e.g. OpenPsi)
+                to decide what behavior tree we want to execute.
+        """
+        result = bindlink(self._atomspace,
                           BindLink(
                               VariableNode("$block"),
                               AndLink(
@@ -44,14 +100,12 @@ class ActionGenerator:
                               VariableNode("$block")
                           ).h
                       )
-        print "action_gen: result", Atom(result, self.atomspace)
+        print "action_gen: result", Atom(result, self._atomspace)
         
-        if self.atomspace.get_outgoing(result) == []:
-            #TODO: fix the multiple atomspace bug 
-            #between execute_atom and bindlink
+        if self._atomspace.get_outgoing(result) == []:
             print "action_gen: no result, random walk."
-            
-            evaluate_atom(self.atomspace,
+
+            evaluate_atom(self._atomspace,
                              EvaluationLink(
                                  GroundedPredicateNode("py: action_schemas.set_relative_move"),
                                  ListLink(
@@ -61,7 +115,4 @@ class ActionGenerator:
                                  )
                              )
                          )
-            print "action_gen: random walk end"
-        
-                    
-                
+
--- minecraft_bot/src/action_schemas.py
+++ minecraft_bot/src/action_schemas.py
@@ -1,5 +1,48 @@
+# action_schemas.py
+#! usr/bin/env python2.7 python2 python
 """
-action schemas called by Grounded Predicate/Schema Node
+action schemas called by GroundedPredicate/SchemaNode in ActionGenerator
+
+When importing this it will ask for the ROS service to ROS.
+So to make it work we have to start the embodiment-testing/actionsnode.py
+Note:
+1. All function arguments must be atom. Or the GPN/GSN will fail.
+   If the return value of function is not atom, the GSN will fail.
+   If the return value of function is not TruthValue, the GPN will fail.
+2. Because the GSN/GPN will find functions in outermost shell scope,
+   we have to import schemas in starting script(e.g. opencog_initializer.py),
+   not the action generator module.
+   Or, the python evaluator in Opencog will fail to find the function.
+3. We assume the atomspace and Space/Time server in main loop
+   are the ones in the opencog.spacetime.SpaceTimeAndAtomSpace instance.
+   If not, all the schemas will fail because they can't find the argument atoms.
+4. For now(20150822) they are imported through the config file passed to
+   opencog.utilities.initialize_opencog() in starting script. If not do so,
+   the Python evaluator in Opencog cannot find function name through module,
+   ex. It can find "is_attractive"
+       but unable to find "action_schemas.is_attractive".
+   This behavior is strange; for now to make code easier to read we want to
+   put the module name(action_schema) in the GPN/GSN. So we have to import it
+   in such a weird way.
+
+Method:
+is_attractive(atom): To judge an atom is attractive enough(sti is large enough).
+move_toward_block(block_atom): move to a place near the input block.
+set_look(pitch_atom, yaw_atom): look toward the given direction.
+set_relative_look(pitch_atom, yaw_atom): look toward current + given direction.
+set_relative_move(yaw_atom, dist_atom, jump_atom): move toward the given yaw.
+
+Exception:
+    rospy.ServiceException: raised when errored in asking for ROS services.
+
+TODO:
+Seperate the schemas to different modules: some schemas are for decision making
+    (ex. is_attractive), and others are for executing action.
+    We should put them in different modules or it will be hard to manage them.
+    For now it's OK because there's only a few functions.
+    And a problem is that, the initialize_opencog() can't read the config file
+    correctly when we set multiple path in PYTHON_PRELOAD_FUNCTIONS. It must be
+    fixed if we want to seperate schemas.
 """
 
 import math
@@ -28,6 +71,11 @@ except rospy.ServiceException, e:
     print "service call failed: %s"% e
 
 def is_attractive(atom):
+    """judge if atom is attractive enough
+    Return: TruthValue(1,1) if it's attractive else TruthValue(0,1)
+    TODO: 
+    Set the sti standard value as argument: For now we set "sti > 1" as condition.
+    """
     print 'is_attractive'
     sti = atom.av['sti']
     if sti > 1:
@@ -38,6 +86,18 @@ def is_attractive(atom):
         return TruthValue(0,1)
 
 def move_toward_block(block_atom):
+    """Make bot move near the block
+    If there's no enough surrounding block info,
+    the bot may find no place to stand on and stop moving.
+    Also if there's no empty place near ( distance <= 2) the block,
+    the bot will not move.
+    Arg:
+        block_atom(opencog.atomspace.Atom): The atom representing block
+    Return: TruthValue(1,1) if move success else TruthValue(0,1)
+    TODO:
+        Make it faster: the calculation of near place is slow.
+        Add distance argument: make caller control the judgement of "near"
+    """
     print 'move toward atom', block_atom
     jump = False
     map_handle = (atomspace.get_atoms_by_name(
@@ -68,9 +128,16 @@ def move_toward_block(block_atom):
             print 'move fail'
             return TruthValue(0,1)
 
-def set_look(yaw_atom, pitch_atom):
-    yaw = float(yaw_atom.name)
+def set_look(pitch_atom, yaw_atom):
+    """set look toward the given direction.
+    The look direction will be changed to the diretion we pass.
+    Args:
+        pitch_atom(opencog.atomspace.Atom): A NumberNode representing pitch in degree
+        yaw_atom(opencog.atomspace.Atom): A NumberNode representing yaw in degree
+    Return:  TruthValue(1,1) if set look success else TruthValue(0,1)
+    """
     pitch = float(pitch_atom.name)
+    yaw = float(yaw_atom.name)
     response = _ros_set_look(yaw, pitch)
     if response == True:
         return TruthValue(1,1)
@@ -78,6 +145,16 @@ def set_look(yaw_atom, pitch_atom):
         return TruthValue(0,1)
 
 def set_relative_look(pitch_atom, yaw_atom):
+    """set relative look toward the given direction.
+    The look will be changed by adding pitch and yaw on current direction.
+    Args:
+        pitch_atom(opencog.atomspace.Atom):
+            A NumberNode representing relative pitch in degree
+        yaw_atom(opencog.atomspace.Atom):
+            A NumberNode representing relative yaw in degree
+    Return:  TruthValue(1,1) if set look success else TruthValue(0,1)
+    """
+
     pitch = float(pitch_atom.name)
     yaw = float(yaw_atom.name)
     response = _ros_set_relative_look(pitch, yaw)
@@ -87,6 +164,23 @@ def set_relative_look(pitch_atom, yaw_atom):
         return TruthValue(0,1)
             
 def set_relative_move(yaw_atom, dist_atom, jump_atom):
+    """set relative move toward the given direction.
+    The bot will move toward the yaw direction in input distance.
+    For now the jump_atom is not used. We assume bot will not jump
+    during the move.
+    Args:
+        yaw_atom(opencog.atomspace.Atom):
+            A NumberNode representing yaw in degree
+        dist_atom(opencog.atomspace.Atom):
+            A NumberNode representing distance in Minecraft unit
+        jump_atom(opencog.atomspace.Atom):
+            not used now, should be used for determining jump or not.
+    Return: TruthValue(1,1) if move success else TruthValue(0,1)
+    TODO:
+        use jump_atom to determine jump or not: We can determine jump
+        or not by checking the TV of jump_atom.
+    """
+
     print 'set_rel_move'
     yaw = float(yaw_atom.name)
     dist = float(dist_atom.name)
