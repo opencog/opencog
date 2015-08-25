79ea94fbcfabea2a9df8080a0d6b8f040c29dcb5
Nil Geisweiller <ngeiswei@gmail.com>
2015-08-25 11:55:07 +0300

    Support load pln-config.scm from any location

--- opencog/reasoning/pln/pln-config.scm
+++ opencog/reasoning/pln/pln-config.scm
@@ -42,9 +42,11 @@
 ;;;;;;;;;;;;;;;;
 
 ; Load the rules (use load for relative path w.r.t. to that file)
+(define config-dir (dirname (current-filename)))
+(define (prepend-config-dir fp) (string-append config-dir "/" fp))
 (define rule-files (list "rules/deduction.scm"
                          "rules/modus-ponens.scm"))
-(for-each load rule-files)
+(for-each (lambda (fp) (load (prepend-config-dir fp))) rule-files)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Associate rules to PLN ;;
