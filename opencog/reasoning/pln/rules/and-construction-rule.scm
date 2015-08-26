f34450c619f55daa6e04680d2a2f9480505c73ce
Nil Geisweiller <ngeiswei@gmail.com>
2015-08-26 16:19:00 +0300

    Add comment to and-construction rule about limits and assumptions

--- opencog/reasoning/pln/rules/and-construction-rule.scm
+++ opencog/reasoning/pln/rules/and-construction-rule.scm
@@ -2,6 +2,12 @@
 ; AndRule
 ; (http://wiki.opencog.org/w/AndRule TODO)
 ;
+; For now A and B can be predicates or concepts. Note that the rule
+; will not try to prevent mixing predicates and concepts (we need a
+; better type system for that). Also it assumes that A and B are
+; independant. The rule should account for relationships between A and
+; B (like inheritance, etc) to correct that assumption.
+;
 ; A<TV1>
 ; B<TV2>
 ; |-
