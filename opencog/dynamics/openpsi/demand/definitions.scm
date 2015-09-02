978bf862cef4ad8f94f5408bd79e6fc11f06f505
Amen Belayneh <amenbelayneh@gmail.com>
2015-09-02 20:26:46 +0800

    openpsi: use functions to define the demands

---
+++ opencog/dynamics/openpsi/demand/definitions.scm
@@ -0,0 +1,14 @@
+; Define the OpenPsi-Demand Affiliation
+(define-psi-demand "Affiliation" .6 .8 1)
+
+; Define the OpenPsi-Demand Certainty
+(define-psi-demand "Certainty" .6 .8 1)
+
+; Define the OpenPsi-Demand Competence
+(define-psi-demand "Competence" .6 .8 1)
+
+; Define the OpenPsi-Demand Energy
+(define-psi-demand "Energy" .71 .5 .7)
+
+; Define the OpenPsi-Demand Integrity
+(define-psi-demand "Integrity" .6 .8 1)
