4e19f22eaab5a4b2d283eacbd70dc664c0a9826c
leungmanhin <manhinleung@gmail.com>
2015-09-02 12:58:28 +0800

    Update nlp-fc

--- opencog/nlp/scm/nlp-fc.scm
+++ opencog/nlp/scm/nlp-fc.scm
@@ -7,71 +7,104 @@
 )
 
 (define (nlp-fc sent)
-  (define mylist '())
-; run forward chaining on sentence
-  (let* ((temp (run-fc sent)); temp contains list of cog-fc results and parse node
-        (result1 (car temp));result1 contains Listlink nested 3 times
-        (parse-node (cadr temp))
-        (result2 (cog-outgoing-set result1));result2 contains ListLink nested 2 times
-        (parse-name (cog-name parse-node))
+    ; run forward chaining on sentence
+    (let* ((parse-node (car (sentence-get-parses (nlp-parse sent))))
+           (interp-node (gen-interp-node (cog-name parse-node)))
+           (interp-link (gen-interp-link interp-node parse-node))
+           (results (cog-outgoing-set (car (run-fc parse-node interp-link))))
+           (speech-acts '())
+           (result-contents '()))
+
+        (for-each
+            (lambda (ri)
+                (let ((s1 (cog-outgoing-set ri)))
+                    (for-each
+                        (lambda (x)
+                            (let ((s2 (cog-outgoing-set x)))
+                                (for-each
+                                    ;(lambda(y) (set! result-contents (append result-contents (list y))))
+                                    (lambda(y)
+                                        (if (equal? (cog-type y) 'InheritanceLink)
+                                            ; Store the InheritanceLink separately if it is related to speech acts
+                                            ; Assumming it is in the form of:
+                                            ;    (InheritanceLink (InterpretationNode "sentence@123") (ConceptNode "SomeSpeechAct"))
+                                            (if (and (equal? (car (cog-outgoing-set y)) interp-node)
+                                                     (equal? (string-contains "SpeechAct" (cog-name (cadr (cog-outgoing-set y))))))
+                                                (set! speech-acts (append speech-acts (list y)))
+                                            )
+
+                                            ; Store everything else in the result-contents
+                                            (set! result-contents (append result-contents (list y)))
+                                        )
+                                    )
+                                    s2
+                                )
+                            )
+                        )
+                        s1
+                    )
+                )
+            )
+            results
         )
 
-     (for-each (lambda (x)
-                 (let* ((t1 (cog-outgoing-set x));t1 contains ListLink List of results
-                       )
-                       (for-each (lambda (b)
-                                   (let* ((t2 (cog-outgoing-set b)));t2 contains atoms contained in ListLink
-                                    (for-each (lambda(c)
-                                             ;  (display c)
-                                                (set! mylist (append mylist (list c)))
-                                              )
-                                     t2)
-                                   )
-                                 )
-                       t1
-                       )
-                 )
-               )
-      result2)
+        ; Sort the speech acts according to their orders
+        (sort speech-acts (lambda (x y) (< (cdr (assoc (get-speech-act-name x) speech-act-orders)) (cdr (assoc (get-speech-act-name y) speech-act-orders)))))
 
-  (ReferenceLink
-    (InterpretationNode (string-append parse-name "_interpretation_$X"))
-    ; The function in the SetLink returns a list of outputs that
-    ; are the results of the evaluation of the relex-to-logic functions,
-    ; on the relex-opencog-outputs.
-  (SetLink (delete-duplicates mylist))
-  )
+        ; Construct a ReferenceLink as the output
+        (ReferenceLink
+            interp-node
 
-  (InterpretationLink
-    (InterpretationNode (string-append parse-name "_interpretation_$X"))
-    parse-node
-  )
+            ; The function in the SetLink returns a list of outputs that
+            ; are the results of the evaluation of the relex-to-logic functions,
+            ; on the relex-opencog-outputs.
+            (SetLink
+                (delete-duplicates result-contents)
+                (car speech-acts)
+            )
+        )
 
-  (AtTimeLink
-      ; FIXME: maybe opencog's internal time octime should be used. Will do for
-      ; now assuming a single instance deals with a single conversation.
-      (TimeNode (number->string (current-time)))
-      (InterpretationNode (string-append parse-name "_interpretation_$X"))
-      (TimeDomainNode "Dialogue-System")
-  )
- )
-#t
+        (AtTimeLink
+            ; FIXME: maybe opencog's internal time octime should be used. Will do for
+            ; now assuming a single instance deals with a single conversation.
+            (TimeNode (number->string (current-time)))
+            interp-node
+            (TimeDomainNode "Dialogue-System")
+        )
+
+        ; Delete any extra speech acts that might have been generated
+        (map cog-delete-recursive (cdr speech-acts))
+    )
+    #t
 )
 
-(define (run-fc sent)
-  (define parse-node (car (sentence-get-parses (nlp-parse sent))))
-  (list (cog-fc
-    (SetLink (parse-get-relations parse-node))
-    r2l-rules
-   )
-  parse-node
-  )
+(define (gen-interp-node parse-name)
+    (InterpretationNode (string-append parse-name "_interpretation_$X"))
 )
-(define (run-fc-dbg sent)
-  (define parse-node (car (sentence-get-parses (nlp-parse sent))))
-  (display (cog-fc
-    (SetLink (parse-get-relations parse-node))
-    r2l-rules
-   )
-  )
+
+(define (gen-interp-link interp-node parse-node)
+    (InterpretationLink
+       interp-node
+       parse-node
+    )
+)
+
+(define (run-fc parse-node interp-link)
+    (list (cog-fc
+        (SetLink (parse-get-relex-outputs parse-node) interp-link)
+        r2l-rules
+    ))
+)
+
+(define speech-act-orders
+    '(
+        ("InterrogativeSpeechAct" . 1)
+        ("TruthQuerySpeechAct" . 2)
+        ("ImperativeSpeechAct" . 2)
+        ("DeclarativeSpeechAct" . 3)
+    )
+)
+
+(define (get-speech-act-name inheritance-link)
+    (cog-name (cadr (cog-outgoing-set inheritance-link)))
 )
