4ea78c6057f6fbc697afa35b27b5caa629152efe
Nil Geisweiller <ngeiswei@gmail.com>
2015-09-10 12:32:42 +0300

    Remove embodiment scripts
    All those scripts are specific to the old embodiment, now removed.

--- scripts/embodiment/grep_log/tg_action_plan_sent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -z "$1" ]; then 
-    echo "This script prints in real-time the action plans attempted to be sent to PAI"
-    echo "Requires DEBUG log level"
-    echo "usage: $0 OAC_LOG_FILE"
-    exit
-fi
-
-tail -f "${1}" | grep -f tg_action_plan_sent.strings
