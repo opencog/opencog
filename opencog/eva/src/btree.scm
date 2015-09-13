64e81ba696a37f49d501ecc7e90fa4f9509389fb
Linas Vepstas <linasvepstas@gmail.com>
2015-09-13 15:20:10 -0500

    Continue hacking behavior tree basics

--- opencog/eva/src/btree.scm
+++ opencog/eva/src/btree.scm
@@ -20,27 +20,24 @@
 (define room-empty (ConceptNode "room empty"))
 (define room-nonempty (ConceptNode "room nonempty"))
 
+(define soma-state (AnchorNode "Soma State"))
+(define soma-sleeping (ConceptNode "Sleeping"))
+
 ;; Assume room empty at first
 (ListLink room-state room-empty)
+(ListLink soma-state soma-sleeping)
 
 
-(define (print-msg) (display "triggered\n") (stv 1 1))
+(define (print-msg) (display "Triggered\n") (stv 1 1))
+(define (print-atom atom) (format #t "Triggered: ~a \n" atom) (stv 1 1))
 ; ------------------------------------------------------
 ;;
-;; Below are several different ways of determining if the
+;; determining if the
 ;; atomspace contains the link
 ;; (ListLink (AnchorNode "Room State") (ConceptNode "room empty"))
 ;; If the atomspace does contain the above, then the print-msg
 ;; function is run.
 ; ------------------------------------------------------
-
-;; This variant uses PresentLink to place the room-state into
-;; a variable, and then uses EqualLink to check the value of
-;; that variable.  This demo will not work right now, because
-;; PresentLink is not implemented (github bug #218)
-;;
-;; This variant haas an advantage over the next one, as it requires
-;; only one invocation of the pattern matcher, not two.
 #|
 (define empty-seq
 	(SatisfactionLink
@@ -51,28 +48,43 @@
 				(GroundedPredicateNode "scm: print-msg")
 				(ListLink))
 		)))
-
-(cog-satisfy empty-seq)
 |#
 
-; ------------------------------------------------------
-;; This variant uses a GetLink to fetch the room-state from the
-;; AtomSpace, and then uses EqualLink to see if it is in the desired
-;; state. Note that this results in *two* invocations of the pattern
-;; matcher; the GetLink being the inner one.  Note also that the
-;; GetLink returns it's results in a SetLink, so comparison must
-;; use a SetLink as well.
+
+(DefineLink
+	(DefinedPredicateNode "is-empty")
+	(EqualLink
+		(SetLink room-empty)
+		(GetLink (ListLink room-state (VariableNode "$x")))
+	))
 
 (define empty-seq
 	(SatisfactionLink
 		(SequentialAndLink
-			(EqualLink
-				(SetLink room-empty)
-				(GetLink (ListLink room-state (VariableNode "$x"))))
+			(DefinedPredicateNode "is-empty")
 			(EvaluationLink
 				(GroundedPredicateNode "scm: print-msg")
 				(ListLink))
 		)))
 
+#|
+
+(define empty-seq
+	(SatisfactionLink
+		(SequentialAndLink
+			(PresentLink (ListLink room-state (VariableNode "$x")))
+			(EqualLink (VariableNode "$x") room-empty)
+			(EvaluationLink
+				(GroundedPredicateNode "scm: print-msg")
+				(ListLink (VariableNode "$x")))
+
+			(PresentLink (ListLink soma-state (VariableNode "$y")))
+			(EqualLink (VariableNode "$y") soma-sleeping)
+			(EvaluationLink
+				(GroundedPredicateNode "scm: print-msg")
+				(ListLink (VariableNode "$y")))
+		)))
+
+|#
 (cog-satisfy empty-seq)
-; ------------------------------------------------------
+
