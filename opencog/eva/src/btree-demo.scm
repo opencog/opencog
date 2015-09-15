023bb751631494ecfb2ded5f9fd01658accf2df3
Linas Vepstas <linasvepstas@gmail.com>
2015-09-15 12:44:45 -0500

    more btree foundations

--- opencog/eva/src/btree-demo.scm
+++ opencog/eva/src/btree-demo.scm
@@ -27,11 +27,43 @@
 (ListLink room-state room-empty)
 (ListLink soma-state soma-sleeping)
 
-; --------------------------------------------------------
-; temp scaffolding and junk.
 
 (define (print-msg) (display "Triggered\n") (stv 1 1))
 (define (print-atom atom) (format #t "Triggered: ~a \n" atom) (stv 1 1))
+; ------------------------------------------------------
+;;
+;; determining if the
+;; atomspace contains the link
+;; (ListLink (AnchorNode "Room State") (ConceptNode "room empty"))
+;; If the atomspace does contain the above, then the print-msg
+;; function is run.
+; ------------------------------------------------------
+#|
+(define empty-seq
+	(SatisfactionLink
+		(SequentialAndLink
+			(PresentLink (ListLink room-state (VariableNode "$x")))
+			(EqualLink (VariableNode "$x") room-empty)
+			(EvaluationLink
+				(GroundedPredicateNode "scm: print-msg")
+				(ListLink))
+		)))
+|#
+
+
+(DefineLink
+	(DefinedPredicateNode "is-empty")
+	(EqualLink
+		(SetLink room-empty)
+		(GetLink (ListLink room-state (VariableNode "$x")))
+	))
+
+(DefineLink
+	(DefinedPredicateNode "is-sleeping")
+	(EqualLink
+		(SetLink soma-sleeping)
+		(GetLink (ListLink soma-state (VariableNode "$y")))
+	))
 
 (DefineLink
 	(DefinedPredicateNode "Print Msg")
@@ -40,35 +72,12 @@
 		(ListLink))
 	)
 
-; ------------------------------------------------------
-;;
-;; Does the atomspace contains the link
-;; (ListLink (AnchorNode "Room State") (ConceptNode "room empty"))
-;; line 665, were_no_people_in_the_scene
-(DefineLink
-	(DefinedPredicateNode "is-empty")
-	(EqualLink
-		(SetLink room-empty)
-		(GetLink (ListLink room-state (VariableNode "$x")))
-	))
-
-;; line 742, assign_face_target
-(DefineLink
-	(DefinedPredicateNode "look-at-visible-face")
-	(PutLink
-		(EvaluationLink (PredicateNode "lookat-face")
-			(ListLink (VariableNode "$face")))
-		(GetLink
-			(EvaluationLink (PredicateNode "visible face")
-				(ListLink (VariableNode "$face-id"))))
-	))
-
 (define empty-seq
 	(SatisfactionLink
 		(SequentialAndLink
-			;; line 392
 			(DefinedPredicateNode "is-empty")
-			(DefinedPredicateNode "look-at-visible-face")
+			(DefinedPredicateNode "Print Msg")
+			(DefinedPredicateNode "is-sleeping")
 			(DefinedPredicateNode "Print Msg")
 		)))
 
