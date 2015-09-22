ee4d7e14c88c33e269117c2618b8c1e7bba220ec
Linas Vepstas <linasvepstas@gmail.com>
2015-09-21 22:36:58 -0500

    Fixes to get the atom types actually loading

--- opencog/scm/opencog/atom-types.scm
+++ opencog/scm/opencog/atom-types.scm
@@ -8,6 +8,9 @@
 ; (define-module (opencog atomtypes spacetime-types))
 ; and so on, but I don't see the point of that, at the moment...
 
+; (setenv "LTDL_LIBRARY_PATH" "/usr/local/lib/opencog:/usr/local/lib/opencog/modules")
+(load-extension "libnlp-types" "nlp_types_init")
+
 (load-from-path "nlp/types/nlp_types.scm")
 (load-from-path "spacetime/spacetime_types.scm")
 (load-from-path "dynamics/attention/attention_types.scm")
