c2fb4fac846fee4df4942df625b4402813f35325
rodsol <rodisolomon@gmail.com>
2015-10-04 12:05:02 +0300

    det rule bind link

---
+++ opencog/nlp/relex2logic/det.scm
@@ -0,0 +1,59 @@
+
+(define det
+    (BindLink
+        (VariableList
+            (TypedVariableLink
+                (VariableNode "$a-parse")
+                (TypeNode "ParseNode")
+            )
+            (TypedVariableLink
+                (VariableNode "$noun")
+                (TypeNode "WordInstanceNode")
+            )
+            (TypedVariableLink
+                (VariableNode "$det")
+                (TypeNode "WordInstanceNode")
+            )
+        )
+        (AndLink
+            (WordInstanceLink
+                (VariableNode "$noun")
+                (VariableNode "$a-parse")
+            )
+            (WordInstanceLink
+                (VariableNode "$det")
+                (VariableNode "$a-parse")
+            )
+            (EvaluationLink
+                (DefinedLinguisticRelationshipNode "_det")
+                (ListLink
+                    (VariableNode "$noun")
+                    (VariableNode "$det")
+                )
+            )
+            (InheritanceLink 
+                (VariableNode "$noun")
+                (DefinedLinguisticConceptNode "definite")
+            )
+		 
+        )
+      (ListLink
+        (ExecutionOutputLink
+       	   (GroundedSchemaNode "scm: pre-det-rule")
+       	      (ListLink
+       	         (VariableNode "$noun")
+       	         (VariableNode "$det")
+            )
+        )
+      )
+    )
+)
+
+
+(define (pre-det-rule noun det)
+  (ListLink
+    (det-rule  (cog-name (word-inst-get-lemma  noun)) (cog-name noun) 
+              (choose-var-name) (cog-name (word-inst-get-lemma det)) 
+    )
+  )
+)
