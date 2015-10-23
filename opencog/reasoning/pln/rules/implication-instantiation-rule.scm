c00c51cd67a9c01fe4c495aceef2ebea496dba6a
Nil Geisweiller <ngeiswei@gmail.com>
2015-10-23 14:34:37 +0300

    Properly implement implication partial instantiation

--- opencog/reasoning/pln/rules/implication-instantiation-rule.scm
+++ opencog/reasoning/pln/rules/implication-instantiation-rule.scm
@@ -90,74 +90,86 @@
 (DefineLink implication-full-instantiation-rule-name
   implication-full-instantiation-rule)
 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;; Implication partial instantiation rule ;;
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Implication partial instantiation rule ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; (define implication-partial-instantiation-variables
-;;   (VariableList
-;;      (TypedVariableLink
-;;         (VariableNode "$TyVs")
-;;         (TypeNode "VariableList"))
-;;      (VariableNode "$P")
-;;      (VariableNode "$Q")))
+(define implication-partial-instantiation-variables
+  (VariableList
+     (TypedVariableLink
+        (VariableNode "$TyVs")
+        (TypeNode "VariableList"))
+     (VariableNode "$P")
+     (VariableNode "$Q")))
 
-;; (define implication-partial-instantiation-rewrite
-;;   (ExecutionOutputLink
-;;      (GroundedSchemaNode "scm: implication-partial-instantiation-formula")
-;;      (ListLink
-;;         (VariableNode "$TyVs")
-;;         (VariableNode "$P")
-;;         (VariableNode "$Q"))))
+(define implication-partial-instantiation-rewrite
+  (ExecutionOutputLink
+     (GroundedSchemaNode "scm: implication-partial-instantiation-formula")
+     (ListLink
+        (VariableNode "$TyVs")
+        (VariableNode "$P")
+        (VariableNode "$Q"))))
 
-;; ;; Like implication-full-instantiation-rule but only instantiate one
-;; ;; variable amonst a variable list (if there is just one variable in
-;; ;; the implication scope, then this rule will not be invoked).
-;; (define implication-partial-instantiation-rule
-;;   (BindLink
-;;      implication-partial-instantiation-variables
-;;      implication-instantiation-body
-;;      implication-partial-instantiation-rewrite))
+;; Like implication-full-instantiation-rule but only instantiate one
+;; variable amonst a variable list (if there is just one variable in
+;; the implication scope, then this rule will not be invoked).
+(define implication-partial-instantiation-rule
+  (BindLink
+     implication-partial-instantiation-variables
+     implication-instantiation-body
+     implication-partial-instantiation-rewrite))
 
-;; ;; This function
-;; ;;
-;; ;; 1. randomly selects a substitution term that meets the
-;; ;;    implication's condition (the implicant),
-;; ;;
-;; ;; 2. performs the substitution,
-;; ;;
-;; ;; 3. calculates its TV (TODO: just <1 1> for now).
-;; (define (implication-partial-instantiation-formula TyVs P Q)
-;;   (cog-set-tv!
-;;    (let* (
-;;           (TyV-and-remain (select-rm-rnd-outgoing TyVs))
-;;           (TyV (car TyV-and-remain))
-;;           (TyVs-remain (cadr TyV-and-remain))
-;;           (TyVs-remain-len (length (cog-outgoing-set TyVs-remain)))
-;;                                         ; Select the term to substitute
-;;           (term (select-substitution-term TyV TODO: use P))
-;;                                         ; Substitute the variable by
-;;                                         ; the term in the body
-;;           (Q-inst (cog-execute! (PutLink (LambdaLink TyV Q) term)))
-;;                                         ; If there is only one
-;;                                         ; variable left, discard the
-;;                                         ; TypedVariableLink
-;;           (TyVs-remain (if (= TyVs-remain-len 1)
-;;                            (gar TyVs-remain)
-;;                            TyVs-remain)))
-;;      (if (> TyVs-remain-len 0)
-;;                                         ; If there are some variables
-;;                                         ; left, add the ImplicationLink
-;;                                         ; back
-;;          (ImplicationLink TyVs-remain Q-inst)
-;;                                         ; Otherwise just return the
-;;                                         ; Q instance
-;;          Q-inst))
-;;    ;; TODO: implement a probabilistic formula rather than <1 1>
-;;    (stv 1 1)))
+;; This function
+;;
+;; 1. randomly selects a substitution term that meets the
+;;    implication's condition (the implicant),
+;;
+;; 2. performs the substitution,
+;;
+;; 3. calculates its TV (TODO: just <1 1> for now).
+(define (implication-partial-instantiation-formula TyVs P Q)
+  (cog-set-tv!
+   (let* (
+          (TyVs-outgoings (cog-outgoing-set TyVs))
+          (TyVs-outgoings-len (length TyVs-outgoings))
+                                        ; Select all potential
+                                        ; substitution terms
+          (terms (select-conditioned-substitution-terms TyVs P))
+                                        ; Choose one to as substitution term
+          (rnd-index (random TyVs-outgoings-len))
+          (term (list-ref (cog-outgoing-set terms) rnd-index))
+                                        ; Take it's corresponding
+                                        ; variable
+          (TyV (list-ref TyVs-outgoings rnd-index))
+                                        ; Build a VariableList of the
+                                        ; remaining variables
+          (TyVs-remain-list (rm-list-ref TyVs-outgoings rnd-index))
+          (TyVs-remain-len (length TyVs-remain-list))
+          (TyVs-remain (apply cog-new-link 'VariableList TyVs-remain-list))
+                                        ; Substitute the variable by
+                                        ; the term in the P and Q bodies
+          (P-inst (cog-execute! (PutLink (LambdaLink TyV P) term)))
+          (Q-inst (cog-execute! (PutLink (LambdaLink TyV Q) term)))
+                                        ; If there is only one
+                                        ; variable left, discard the
+                                        ; VariableLink
+          (TyVs-remain (if (= TyVs-remain-len 1)
+                           (gar TyVs-remain)
+                           TyVs-remain)))
+     (if (> TyVs-remain-len 0)
+                                        ; If there are some variables
+                                        ; left, rebuild the
+                                        ; ImplicationLink with the
+                                        ; remaining variables
+         (ImplicationLink TyVs-remain P-inst Q-inst)
+                                        ; Otherwise just return the
+                                        ; Q instance
+         Q-inst))
+   ;; TODO: implement a probabilistic formula rather than <1 1>
+   (stv 1 1)))
 
-;; ;; Name the rule
-;; (define implication-partial-instantiation-rule-name
-;;   (Node "implication-partial-instantiation-rule"))
-;; (DefineLink implication-partial-instantiation-rule-name
-;;   implication-partial-instantiation-rule)
+;; Name the rule
+(define implication-partial-instantiation-rule-name
+  (Node "implication-partial-instantiation-rule"))
+(DefineLink implication-partial-instantiation-rule-name
+  implication-partial-instantiation-rule)
