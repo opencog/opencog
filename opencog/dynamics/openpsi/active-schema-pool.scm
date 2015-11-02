d1e12e8c8e727b22debcade80eff2941afca7eaf
Amen Belayneh <amenbelayneh@gmail.com>
2015-11-02 14:07:14 +0800

    cleanup

--- opencog/dynamics/openpsi/active-schema-pool.scm
+++ opencog/dynamics/openpsi/active-schema-pool.scm
@@ -1,15 +1,16 @@
-(define openpsi-active-schema-pool (ConceptNode "OpenPsi: active-schema-pool"))
+;(load "utilities.scm")
+
+(define psi-active-schema-pool
+    (ConceptNode (string-append (psi-prefix-str) "active-schema-pool")))
 
 (InheritanceLink  ; Defining a rule base
-    (ConceptNode "OpenPsi: active-schema-pool")
-	(ConceptNode "URE")
-)
+    psi-active-schema-pool
+    (ConceptNode "URE"))
 
 (ExecutionLink
     (SchemaNode "URE:maximum-iterations")
-    (ConceptNode "OpenPsi: active-schema-pool")
-    (NumberNode "10")
-)
+    psi-active-schema-pool
+    (NumberNode "10"))
 
 ; Reminder : 1. When ure has the capability to forward chain over only a subset
 ; of atoms (either defined by ecan or just passed to it) then if a rule doesn't
@@ -17,21 +18,17 @@
 ;            2. openpsi can be thought of one of the levels descibed in
 ; https://en.wikipedia.org/wiki/Subsumption_architecture. The dialogue system
 ; with it's own pool another. Then there will be a need for a central action
-; selection that ensures that the invidual action-selectors associated with each
+; selection that ensures that the individual action-selectors associated with
+; each
 ; pool don't choose an action that is in conflict with that found in another.
 ; The relations between actions could be hard coded or learnt.
 ;            3. A rule could be defined to affect a single demand/modulator
 ; value. For e.g. a rule that tries
 
-; The action-selection-agent/planner will choose the rules that are member of
-; the active schema pool for execution with the exception of the
-; psi-demand-updater-rule & psi-modulator-updater-rule
 (MemberLink (stv 1 1)
     (Node "psi-demand-updater-rule")
-    (ConceptNode "OpenPsi: active-schema-pool")
-)
+    psi-active-schema-pool)
 
 (MemberLink (stv 1 1)
     (Node "psi-modulator-updater-rule")
-    (ConceptNode "OpenPsi: active-schema-pool")
-)
+    psi-active-schema-pool)
