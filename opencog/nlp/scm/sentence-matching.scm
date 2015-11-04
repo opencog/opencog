51cad0b06a788cf9c7fc31d8c66d840554339cec
leungmanhin <manhinleung@gmail.com>
2015-11-04 17:55:54 +0800

    Refactor get-answers

--- opencog/nlp/scm/sentence-matching.scm
+++ opencog/nlp/scm/sentence-matching.scm
@@ -1,35 +1,3 @@
-; Check if the input is an actual sentence or a SentenceNode,
-; parse and return the corresponding SentenceNode if it is a sentence
-;
-(define (sentence-node input)
-    (if (cog-atom? input)
-        (if (equal? 'SentenceNode (cog-type input))
-            input
-            (display "Please input a SentenceNode only")
-        )
-        (car (nlp-parse input))
-    )
-)
-
-; Calls the fuzzy pattern matcher to find similar sentences from the Atomspace.
-;
-; Accepts either an actual sentence or a SentenceNode as the input.
-; Returns one or more similar sentences.
-;
-; For example:
-;    (get-similar-sentences "What did Pete eat?")
-; OR:
-;    (get-similar-sentences (SentenceNode "sentence@123"))
-;
-; Possible result: 
-;    (Pete ate apples .)
-;
-(define (get-similar-sentences input-sentence)
-    (main-ss (sentence-node input-sentence) '())
-)
-
-; Similar to get-similar-sentence but it's mainly for question answering purpose.
-;
 ; Find answers (i.e., similar sentences that share some keyword) from the
 ; Atomspace by using the fuzzy pattern matcher. By default, it excludes
 ; sentences with TruthQuerySpeechAct and InterrogativeSpeechAct.
@@ -41,11 +9,34 @@
 ;
 ; Returns one or more sentences -- the answers.
 ;
-(define get-answers
-    (case-lambda
-        ((question) (main-ss (sentence-node question) (list (DefinedLinguisticConceptNode "TruthQuerySpeechAct")
-                                                            (DefinedLinguisticConceptNode "InterrogativeSpeechAct"))))
-        ((question exclude-list) (main-ss (sentence-node question) exclude-list))
+; For example:
+;    (get-answers "What did Pete eat?")
+; OR:
+;    (get-answers (SentenceNode "sentence@123"))
+;
+; Possible result:
+;    (Pete ate apples .)
+;
+(define (get-answers question)
+    ; Check if the input is an actual sentence or a SentenceNode,
+    ; parse and return the corresponding SentenceNode if it is a sentence
+    (define (process-q input)
+        (if (cog-atom? input)
+            (if (equal? 'SentenceNode (cog-type input))
+                input
+                #f
+            )
+            (car (nlp-parse input))
+        )
+    )
+
+    (let ((sent-node (process-q question))
+          (ex-list (list (DefinedLinguisticConceptNode "TruthQuerySpeechAct")
+                         (DefinedLinguisticConceptNode "InterrogativeSpeechAct"))))
+        (if sent-node
+            (sent-matching sent-node ex-list)
+            (display "Please input a SentenceNode only")
+        )
     )
 )
 
@@ -53,7 +44,7 @@
 ; Returns one or more sentences that are similar to the input one but
 ; contains no atoms that are listed in the exclude-list
 ;
-(define (main-ss sent-node exclude-list)
+(define (sent-matching sent-node exclude-list)
     ; Generate sentences from each of the R2L-SetLinks
     ; (define (generate-sentences r2l-setlinks) (if (> (length r2l-setlinks) 0) (map sureal r2l-setlinks) '()))
 
