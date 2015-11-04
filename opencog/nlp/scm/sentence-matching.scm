82f5f51ad5e94a1484dc78c97b21857b56020dc8
leungmanhin <manhinleung@gmail.com>
2015-11-04 16:40:08 +0800

    To Microplanner then SuReal

--- opencog/nlp/scm/sentence-matching.scm
+++ opencog/nlp/scm/sentence-matching.scm
@@ -53,21 +53,48 @@
 ; Returns one or more sentences that are similar to the input one but
 ; contains no atoms that are listed in the exclude-list
 ;
-(define (main-ss sentence-node exclude-list)
+(define (main-ss sent-node exclude-list)
     ; Generate sentences from each of the R2L-SetLinks
-    (define (generate-sentences r2l-setlinks) (if (> (length r2l-setlinks) 0) (map sureal r2l-setlinks) '()))
+    ; (define (generate-sentences r2l-setlinks) (if (> (length r2l-setlinks) 0) (map sureal r2l-setlinks) '()))
+
+    ; Generate sentences for each of the SetLinks found by the fuzzy matcher
+    ; TODO: May need to filter out some of the contents of the SetLinks before sending each of them to Microplanner
+    (define (generate-sentences setlinks)
+        ; Find the speech act from the SetLink and use it for Microplanning
+        (define (get-speech-act setlink)
+            (let* ((speech-act-node-name
+                        (filter (lambda (name)
+                            (if (string-suffix? "SpeechAct" name) #t #f))
+                                (map cog-name (cog-filter 'DefinedLinguisticConceptNode (cog-get-all-nodes setlink))))))
+
+                ; If no speech act was found, return "declarative" as default
+                (if (> (length speech-act-node-name) 0)
+                    (string-downcase (substring (car speech-act-node-name) 0 (string-contains (car speech-act-node-name) "SpeechAct")))
+                    "declarative"
+                )
+            )
+        )
+
+        (append-map (lambda (result)
+            ; Send each of the SetLinks returned by Microplanning to SuReal for sentence generation
+            (append-map (lambda (setlink) (sureal setlink))
+                ; Send each of the SetLinks found by the fuzzy matcher to Microplanner to see if they are good
+                (car (microplanning (SequentialAndLink (cog-outgoing-set result)) (get-speech-act result) *default_chunks_option* #f))))
+            setlinks
+        )
+    )
 
     (begin
         ; Delete identical sentences from the return set
         (delete-duplicates
-            ; Use SuReal to generate sentences from their corresponding SetLinks
+            ; Use Mircoplanner and SuReal to generate sentences from the SetLinks found
             (generate-sentences
                 ; Search for any similar SetLinks in the atomspace
                 (cog-outgoing-set (cog-fuzzy-match
                     ; Get the R2L SetLink of the input sentence
                     (car (cog-chase-link 'ReferenceLink 'SetLink
                         (car (cog-chase-link 'InterpretationLink 'InterpretationNode
-                            (car (cog-chase-link 'ParseLink 'ParseNode sentence-node))
+                            (car (cog-chase-link 'ParseLink 'ParseNode sent-node))
                         ))
                     ))
                     'SetLink
