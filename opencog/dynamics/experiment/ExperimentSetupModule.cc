55f066ec64a2dab086d115223649976506005146
Misgana Bayetta <misgana.bayetta@gmail.com>
2015-11-18 18:41:54 +0800

    Rename class and library

--- opencog/dynamics/experiment/ExperimentSetupModule.cc
+++ opencog/dynamics/experiment/ExperimentSetupModule.cc
@@ -16,8 +16,8 @@
 #include <opencog/dynamics/attention/HebbianUpdatingAgent.h>
 #include <opencog/dynamics/attention/SimpleImportanceDiffusionAgent.h>
 #include <opencog/dynamics/experiment/ArtificialStimulatorAgent.h>
-#include <opencog/dynamics/experiment/PLNDynamicsExpSetupModule.h>
-#include <opencog/dynamics/experiment/SurprisingnessEvaluatorAgent.h>
+#include <opencog/dynamics/experiment/ExperimentSetupModule.h>
+//#include <opencog/dynamics/experiment/SurprisingnessEvaluatorAgent.h>
 
 #include <opencog/server/CogServer.h>
 #include <opencog/server/Factory.h>
@@ -29,9 +29,9 @@
 using namespace opencog;
 using namespace opencog::ECANExperiment;
 
-DECLARE_MODULE(PLNDynamicsExpSetupModule);
+DECLARE_MODULE(ExperimentSetupModule);
 
-PLNDynamicsExpSetupModule::PLNDynamicsExpSetupModule(CogServer& cs) :
+ExperimentSetupModule::ExperimentSetupModule(CogServer& cs) :
         Module(cs), _cs(cs)
 {
     _log = new Logger(); //new Logger("ecanexpe.log");
@@ -39,49 +39,53 @@ PLNDynamicsExpSetupModule::PLNDynamicsExpSetupModule(CogServer& cs) :
     _as = &_cs.getAtomSpace();
 
     _AVChangedSignalConnection = _as->AVChangedSignal(
-            boost::bind(&PLNDynamicsExpSetupModule::AVChangedCBListener, this,
-                        _1, _2, _3));
+            boost::bind(&ExperimentSetupModule::AVChangedCBListener, this, _1,
+                        _2, _3));
 }
 
-PLNDynamicsExpSetupModule::~PLNDynamicsExpSetupModule()
+ExperimentSetupModule::~ExperimentSetupModule()
 {
     unregisterAgentRequests();
 }
 
-void PLNDynamicsExpSetupModule::AVChangedCBListener(
-        const Handle& h, const AttentionValuePtr& av_old,
-        const AttentionValuePtr& av_new)
+void ExperimentSetupModule::AVChangedCBListener(const Handle& h,
+                                                const AttentionValuePtr& av_old,
+                                                const AttentionValuePtr& av_new)
 {
     ECANValue ecanval(av_new->getSTI(), av_new->getLTI(), _cs.getCycleCount());
     _data[h] = ecanval;
 }
 
-void PLNDynamicsExpSetupModule::registerAgentRequests()
+void ExperimentSetupModule::registerAgentRequests()
 {
     do_ecan_load_register();
     do_ecan_start_register();
     do_ecan_pause_register();
     do_stimulate_register();
+    do_load_word_dict_register();
+    do_dump_data_register();
 }
 
-void PLNDynamicsExpSetupModule::unregisterAgentRequests()
+void ExperimentSetupModule::unregisterAgentRequests()
 {
     do_ecan_load_unregister();
     do_ecan_start_unregister();
     do_ecan_pause_unregister();
     do_stimulate_unregister();
+    do_load_word_dict_unregister();
+    do_dump_data_unregister();
 }
-void PLNDynamicsExpSetupModule::init(void)
+void ExperimentSetupModule::init(void)
 {
     //Load params
-    _as->set_attentional_focus_boundary(
-            (stim_t) config().get_int("AF_BOUNDARY"));
+    //_as->set_attentional_focus_boundary(
+    //        (stim_t) config().get_int("AF_BOUNDARY"));
 
     registerAgentRequests();
 }
 
-std::string PLNDynamicsExpSetupModule::do_ecan_load(Request *req,
-                                                    std::list<std::string> args)
+std::string ExperimentSetupModule::do_ecan_load(Request *req,
+                                                std::list<std::string> args)
 {
     //These mind agents have already been made registered by the attention module.So no need to register them.
     _forgetting_agentptr = _cs.createAgent(ForgettingAgent::info().id, false);
@@ -94,17 +98,17 @@ std::string PLNDynamicsExpSetupModule::do_ecan_load(Request *req,
 
     //Register experiment specific agents. Add more if you have here.
 
-    Factory<SurprisingnessEvaluatorAgent, Agent> surprisingnessAgentFactory;
-    bool status = _cs.registerAgent(SurprisingnessEvaluatorAgent::info().id,
-                                    &surprisingnessAgentFactory);
-    if (status) {
-        _surprisingness_agentptr = _cs.createAgent(
-                SurprisingnessEvaluatorAgent::info().id, false);
-    }
+    /*Factory<SurprisingnessEvaluatorAgent, Agent> surprisingnessAgentFactory;
+     bool status = _cs.registerAgent(SurprisingnessEvaluatorAgent::info().id,
+     &surprisingnessAgentFactory);
+     if (status) {
+     _surprisingness_agentptr = _cs.createAgent(
+     SurprisingnessEvaluatorAgent::info().id, false);
+     }*/
 
     Factory<ArtificialStimulatorAgent, Agent> artificialStimulatorAgentFactory;
-    status = _cs.registerAgent(ArtificialStimulatorAgent::info().id,
-                               &artificialStimulatorAgentFactory);
+    bool status = _cs.registerAgent(ArtificialStimulatorAgent::info().id,
+                                    &artificialStimulatorAgentFactory);
     if (status) {
         _artificialstimulatoragentptr = _cs.createAgent(
                 ArtificialStimulatorAgent::info().id, false);
@@ -113,8 +117,8 @@ std::string PLNDynamicsExpSetupModule::do_ecan_load(Request *req,
     return "Loaded the following agents:\n" + ECAN_EXP_AGENTS;
 }
 
-std::string PLNDynamicsExpSetupModule::do_ecan_start(
-        Request *req, std::list<std::string> args)
+std::string ExperimentSetupModule::do_ecan_start(Request *req,
+                                                 std::list<std::string> args)
 {
     _cs.startAgent(_forgetting_agentptr);
     _cs.startAgent(_hebbianupdating_agentptr);
@@ -126,8 +130,8 @@ std::string PLNDynamicsExpSetupModule::do_ecan_start(
     return "The following agents were started:\n" + ECAN_EXP_AGENTS;
 }
 
-std::string PLNDynamicsExpSetupModule::do_ecan_pause(
-        Request *req, std::list<std::string> args)
+std::string ExperimentSetupModule::do_ecan_pause(Request *req,
+                                                 std::list<std::string> args)
 {
     _cs.stopAgent(_forgetting_agentptr);
     _cs.stopAgent(_hebbianupdating_agentptr);
@@ -139,8 +143,8 @@ std::string PLNDynamicsExpSetupModule::do_ecan_pause(
     return "The following agents were stopped:\n" + ECAN_EXP_AGENTS;
 }
 
-std::string PLNDynamicsExpSetupModule::do_stimulate(Request *req,
-                                                    std::list<std::string> args)
+std::string ExperimentSetupModule::do_stimulate(Request *req,
+                                                std::list<std::string> args)
 {
     for (std::string& pairs : args) {
         auto pos = pairs.find_first_of(",");
@@ -157,8 +161,8 @@ std::string PLNDynamicsExpSetupModule::do_stimulate(Request *req,
     return "stimulus provided.\n";
 }
 
-std::string PLNDynamicsExpSetupModule::do_dump_data(Request *req,
-                                                    std::list<std::string> args)
+std::string ExperimentSetupModule::do_dump_data(Request *req,
+                                                std::list<std::string> args)
 {
     std::string file_name = args.back();
     // cycle,uuid,sti,lti
@@ -176,20 +180,20 @@ std::string PLNDynamicsExpSetupModule::do_dump_data(Request *req,
     outf.flush();
     outf.close();
 
-    return "Time series data dumped in to " + file_name;
+    return "Time series data dumped in to " + file_name + ".\n";
 
 }
 
-std::string PLNDynamicsExpSetupModule::load_word_dict(
+std::string ExperimentSetupModule::do_load_word_dict(
         Request *req, std::list<std::string> args)
 {
-    auto get_wordvec = [](const std::string& str) {
+    auto tokenize = [](const std::string& str,const char& delim) {
         std::vector<std::string> words;
         auto init = str.begin();
         for (auto i = str.begin(); i != str.end(); ++i) {
-            if (*i == ',') {
+            if (*i == delim) {
                 std::string word = str.substr(
-                        init - str.begin(), i - 1 - str.begin());
+                        init - str.begin(), std::distance(init,i));
                 words.push_back(word);
                 init = i + 1;
             }
@@ -202,18 +206,20 @@ std::string PLNDynamicsExpSetupModule::load_word_dict(
     config().load(file_name.c_str());
     std::string special_wstr = config().get("SPECIAL_WORDS");
     std::string nspecial_wstr = config().get("NON_SPECIAL_WORDS");
-
-    std::vector<std::string> special_words = get_wordvec(special_wstr);
-    std::vector<std::string> nspecial_words = get_wordvec(nspecial_wstr);
-
+    std::vector<std::string> special_words = tokenize(special_wstr, ',');
+    std::vector<std::string> nspecial_words = tokenize(nspecial_wstr, ',');
     std::vector<std::string> sentences = generate_sentence(
             nspecial_words, special_words, config().get_int("SENTENCE_SIZE"));
 
+    //std::cout << "GENERATED SENTENCES" << std::endl;
+    //for(const auto& sentence: sentences) std:: cout << sentence << std::endl;
+
     generated_sentences = sentences;
 
+    return "Loading successful.\n";
 }
 
-std::vector<std::string> PLNDynamicsExpSetupModule::generate_sentence(
+std::vector<std::string> ExperimentSetupModule::generate_sentence(
         const std::vector<std::string>& non_special_words,
         const std::vector<std::string>& special_words, int sent_size)
 {
@@ -233,10 +239,11 @@ std::vector<std::string> PLNDynamicsExpSetupModule::generate_sentence(
         int rw4 = rand() % (nsw_end / 4) + nsw_end * 3 / 4;
 
         //Don't care about order
-        std::string sentence = special_words[sw1] + non_special_words[rw1]
-                               + non_special_words[rw2] + special_words[sw2]
-                               + non_special_words[rw3]
-                               + non_special_words[rw4];
+        std::string sentence = special_words[sw1] + " " + non_special_words[rw1]
+                               + " " + non_special_words[rw2] + " "
+                               + special_words[sw2] + " "
+                               + non_special_words[rw3] + " "
+                               + non_special_words[rw4] + ".";
 
         sentences.push_back(sentence);
     }
