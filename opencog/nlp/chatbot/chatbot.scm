ea04f609f720bc445169485e61b813e8e622eb7c
Linas Vepstas <linasvepstas@gmail.com>
2015-11-28 15:08:11 -0600

    iRe-arrange the config params too

--- opencog/nlp/chatbot/chatbot.scm
+++ opencog/nlp/chatbot/chatbot.scm
@@ -3,9 +3,5 @@
 ;
 (define-module (opencog nlp chatbot))
 
-; User-modifiable config paramters.
-(define relex-server-host "127.0.0.1")
-(define relex-server-port 4444)
-
 (load "chatbot/chat-utils.scm")
 (load "chatbot/process-query.scm")
--- opencog/nlp/scm/nlp.scm
+++ opencog/nlp/scm/nlp.scm
@@ -3,6 +3,11 @@
 
 (use-modules (opencog) (opencog atom-types))
 
+; User-modifiable config paramters.
+(define relex-server-host "127.0.0.1")
+(define relex-server-port 4444)
+
 ; Load various parts....
 (load "nlp/nlp-utils.scm")
 (load "nlp/disjunct-list.scm")
+(load "nlp/processing-utils.scm")
