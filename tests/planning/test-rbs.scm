8b2b8221a2ecf1fefcbea463aa5a54c00d15a6ad
Amen Belayneh <amenbelayneh@gmail.com>
2015-12-08 15:12:10 +0800

    fix the scm file loading issue with the ActionUTest

--- tests/planning/test-rbs.scm
+++ tests/planning/test-rbs.scm
@@ -1,5 +1,5 @@
-(ConceptNode "abc")
-#!
+(use-modules (opencog) (opencog rule-engine))
+
 ; Create a rulebase
 (define initial-rbs (ConceptNode "initial-rbs"))
 (ure-define-rbs initial-rbs 1)
@@ -22,7 +22,7 @@
                 (VariableNode "x")
                 (VariableNode "y")
                 (VariableNode "z"))
-            ; virtual-link
+            ; Function-link
             (EqualLink
                 (VariableNode "x")
                 (VariableNode "y")))
@@ -37,68 +37,3 @@
 )
 
 (ure-add-rule initial-rbs "initial-rule" initial-rule 1)
-
-(define rule2
-    (BindLink
-        (VariableList
-            (TypedVariableLink
-                (VariableNode "x")
-                (TypeNode "NumberNode"))
-            (TypedVariableLink
-                (VariableNode "y")
-                (TypeNode "NumberNode")))
-
-        (AndLink
-            ; virtual-link
-            (ListLink
-                (VariableNode "x")
-                (VariableNode "y"))
-            ; Evaluatable
-            (ListLink
-                (VariableNode "z")
-                (VariableNode "what")))
-        (ListLink
-             (SetLink
-                 (VariableNode "x")
-                 (VariableNode "y"))))
- )
-
-(define rule3
-    (BindLink
-        (VariableList
-            (TypedVariableLink
-                (VariableNode "x")
-                (TypeNode "NumberNode"))
-            (TypedVariableLink
-                (VariableNode "y")
-                (TypeNode "NumberNode")))
-
-        (AndLink
-            ; virtual-link
-
-            ; Evaluatable
-            (ListLink
-                (VariableNode "z")
-                (VariableNode "what")))
-        (ListLink
-             (SetLink
-                 (VariableNode "z")
-                 (VariableNode "what"))))
-)
-
-(NumberNode "1")
-(NumberNode "2")
-
-; Matching atom
-(ListLink
-    (ConceptNode "x")
-    (ConceptNode "x")
-    (PredicateNode "y"))
-
-; Random atom
-(ListLink
-    (ConceptNode "x")
-    (ConceptNode "y")
-    (ConceptNode "x"))
-
-!#
