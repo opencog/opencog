43d1e8e78ed2cfc2c3deedbaf54188664b77a46e
Amen Belayneh <amenbelayneh@gmail.com>
2015-12-13 19:19:00 +0800

    a valid ActionUTest

--- tests/planning/test-rbs.scm
+++ tests/planning/test-rbs.scm
@@ -5,15 +5,18 @@
 (ure-define-rbs initial-rbs 1)
 
 ; Add a rule to initial-rbs
+; NOTE: The rule should be updated as the primitive atom-type(or term) behaviors
+; (virtual-link, evaluatable, function-link, ...) are updated/extended or,
+; new behaviors added.
 (define initial-rule
     (BindLink
         (VariableList
             (TypedVariableLink
                 (VariableNode "x")
-                (TypeNode "ConceptNode"))
+                (TypeNode "NumberNode"))
             (TypedVariableLink
                 (VariableNode "y")
-                (TypeNode "ConceptNode"))
+                (TypeNode "NumberNode"))
             (TypedVariableLink
                 (VariableNode "z")
                 (TypeNode "PredicateNode")))
@@ -22,18 +25,27 @@
                 (VariableNode "x")
                 (VariableNode "y")
                 (VariableNode "z"))
-            ; Function-link
+            (InheritanceLink
+                (VariableNode "x")
+                (VariableNode "z"))
+            ; Virtual-link
             (EqualLink
                 (VariableNode "x")
+                (VariableNode "y"))
+            ; Evaluatable term
+            (EvaluationLink
+                (GroundedPredicateNode "scm: cog-tv")
+                (ListLink
+                    (VariableNode "z")))
+            ; Function-link
+            (TimesLink
+                (VariableNode "x")
                 (VariableNode "y")))
-            ; Evaluatable
-            #!(EvaluationLink)
-             (InheritanceLink
-                 )!#
         (ListLink
              (SetLink
                  (VariableNode "x")
-                 (VariableNode "y"))))
+                 (VariableNode "y")
+                 (VariableNode "z"))))
 )
 
 (ure-add-rule initial-rbs "initial-rule" initial-rule 1)
