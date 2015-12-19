ba7b304f0498e129adc31cee355cf140580d25db
Linas Vepstas <linasvepstas@gmail.com>
2015-12-18 19:42:50 -0600

    Sigh. Rule craziness

--- opencog/nlp/chatbot/eva.scm
+++ opencog/nlp/chatbot/eva.scm
@@ -5,6 +5,18 @@
 ; Probably does not belong in this directory.
 
 ;--------------------------------------------------------------------
+(define (var-decl var type)
+	(TypedVariable (VariableNode var) (TypeNode type)))
+
+;(define look-rule
+;	(BindLink
+;		(VariableList
+;			(var-decl "$a-parse" "ParseNode")
+;		)
+;	)
+;)
+
+;--------------------------------------------------------------------
 (define (imperative_process imp)
 "
   Process imperative IMP, which should be a SentenceNode.
---
+++ opencog/nlp/relex2logic/rule-utils.scm
@@ -0,0 +1,10 @@
+;
+; rule-tuils.scm
+;
+; Some generic rule utilities, not limited to r2l.
+;
+;--------------------------------------------------------------------
+;
+; Short-hand for declaring a variable.
+(define (var-decl var type)
+   (TypedVariableLink (VariableNode var) (TypeNode type)))
