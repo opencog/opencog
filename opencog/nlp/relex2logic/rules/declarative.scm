5007ea9b446d2f41c5a735d594d12a35c6d68dcb
Linas Vepstas <linasvepstas@gmail.com>
2015-12-18 21:10:14 -0600

    Restore two corrupted files

--- opencog/nlp/relex2logic/rules/declarative.scm
+++ opencog/nlp/relex2logic/rules/declarative.scm
@@ -2,33 +2,58 @@
 ; Determine declaratives, based on LG link types.
 ;
 (define declarative
-    (BindLink
-        (VariableList
+	(BindLink
+		(VariableList
 			(var-decl "$interp" "InterpretationNode")
 			(var-decl "$parse" "ParseNode")
 			(var-decl "$wall-inst" "WordInstanceNode")
 			(var-decl "$word-inst" "WordInstanceNode")
-        )
-        (AndLink
-            (InterpretationLink
-                (VariableNode "$interp")
-                (VariableNode "$parse")
-            )
+		)
+		(AndLink
+			(InterpretationLink
+				(VariableNode "$interp")
+				(VariableNode "$parse")
+			)
 			(word-in-parse "$wall-inst" "$parse")
 
-            (ReferenceLink
-                (VariableNode "$wall-inst")
-                (WordNode "###LEFT-WALL###")
-            )
+			(ReferenceLink
+				(VariableNode "$wall-inst")
+				(WordNode "###LEFT-WALL###")
+			)
 
-            ;; If left wall is linked with any of Wd, Wp, Wr, Wt or Wa,
-            ;; then its declarative.
-            (ChoiceLink
-			(dependency  "$word-inst")) "$wall-inst")
+			;; If left wall is linked with any of Wd, Wp, Wr, Wt or Wa,
+			;; then its declarative.
+			(ChoiceLink
+				(EvaluationLink (LinkGrammarRelationshipNode "Wd")
+					(ListLink
+						(VariableNode "$wall-inst")
+						(VariableNode "$word-inst")))
 
-        ; Mark this as declarative.
-        (InheritanceLink
-            (VariableNode "$interp")
-            (DefinedLinguisticConceptNode "DeclarativeSpeechAct"))
-    )
+				(EvaluationLink (LinkGrammarRelationshipNode "Wp")
+					(ListLink
+						(VariableNode "$wall-inst")
+						(VariableNode "$word-inst")))
+
+				(EvaluationLink (LinkGrammarRelationshipNode "Wr")
+					(ListLink
+						(VariableNode "$wall-inst")
+						(VariableNode "$word-inst")))
+
+				(EvaluationLink (LinkGrammarRelationshipNode "Wt")
+					(ListLink
+						(VariableNode "$wall-inst")
+						(VariableNode "$word-inst")))
+
+				(EvaluationLink (LinkGrammarRelationshipNode "Wa")
+					(ListLink
+						(VariableNode "$wall-inst")
+						(VariableNode "$word-inst")))
+			)
+		)
+
+		; Mark this as declarative.
+		(InheritanceLink
+			(VariableNode "$interp")
+			(DefinedLinguisticConceptNode "DeclarativeSpeechAct"))
+	)
 )
--- opencog/nlp/relex2logic/rules/interrogative.scm
+++ opencog/nlp/relex2logic/rules/interrogative.scm
@@ -2,32 +2,62 @@
 ; A more accurate interrogative detector, based on LG link types.
 ;
 (define interrogative
-    (BindLink
-        (VariableList
+	(BindLink
+		(VariableList
 			(var-decl "$interp" "InterpretationNode")
 			(var-decl "$parse" "ParseNode")
 			(var-decl "$wall-inst" "WordInstanceNode")
 			(var-decl "$wh-word-inst" "WordInstanceNode")
-        )
-        (AndLink
-            (InterpretationLink
-                (VariableNode "$interp")
-                (VariableNode "$parse")
-            )
+		)
+		(AndLink
+			(InterpretationLink
+				(VariableNode "$interp")
+				(VariableNode "$parse")
+			)
 			(word-in-parse "$wall-inst" "$parse")
 
-            (ReferenceLink
-                (VariableNode "$wall-inst")
-                (WordNode "###LEFT-WALL###")
-            )
+			(ReferenceLink
+				(VariableNode "$wall-inst")
+				(WordNode "###LEFT-WALL###")
+			)
 
-            ;; If left wall is linked with any of Wq, Ws, Wj or Ww
-            ;; or Qe, Qw then its interrogative.
-            (ChoiceLink
-			(dependency  "$wh-word-inst")) "$wall-inst")
-        ; Mark this as a question.
-        (InheritanceLink
-            (VariableNode "$interp")
-            (DefinedLinguisticConceptNode "InterrogativeSpeechAct"))
-    )
+			;; If left wall is linked with any of Wq, Ws, Wj or Ww
+			;; or Qe, Qw then its interrogative.
+			(ChoiceLink
+				(EvaluationLink (LinkGrammarRelationshipNode "Wq")
+					(ListLink
+						(VariableNode "$wall-inst")
+						(VariableNode "$wh-word-inst")))
+
+				(EvaluationLink (LinkGrammarRelationshipNode "Ws")
+					(ListLink
+						(VariableNode "$wall-inst")
+						(VariableNode "$wh-word-inst")))
+
+				(EvaluationLink (LinkGrammarRelationshipNode "Wj")
+					(ListLink
+						(VariableNode "$wall-inst")
+						(VariableNode "$wh-word-inst")))
+
+				(EvaluationLink (LinkGrammarRelationshipNode "Ww")
+					(ListLink
+						(VariableNode "$wall-inst")
+						(VariableNode "$wh-word-inst")))
+
+				(EvaluationLink (LinkGrammarRelationshipNode "Qe")
+					(ListLink
+						(VariableNode "$wall-inst")
+						(VariableNode "$wh-word-inst")))
+
+				(EvaluationLink (LinkGrammarRelationshipNode "Qw")
+					(ListLink
+						(VariableNode "$wall-inst")
+						(VariableNode "$wh-word-inst")))
+			)
+		)
+		; Mark this as a question.
+		(InheritanceLink
+			(VariableNode "$interp")
+			(DefinedLinguisticConceptNode "InterrogativeSpeechAct"))
+	)
 )
