ddc440c7ca6840750d7855c9fa8c5107513109e2
Linas Vepstas <linasvepstas@gmail.com>
2015-12-21 00:24:05 -0600

    Continued hacking

--- opencog/nlp/chatbot-eva/eva.scm
+++ opencog/nlp/chatbot-eva/eva.scm
@@ -10,7 +10,12 @@
 (define (print-msg node) (display (cog-name node)) (newline) (stv 1 1))
 (define (show-arg node) (display node) node)
 
-; Handle _advmod(look, $direction)
+; Handle short commands, such as "look up", "look left".
+; Relex behaves very inconsistently, soemtimes returning
+; _advmod(look,left) and sometimes _to-be(look, right)
+; when in both cases the correct result would be _to-do(look,left)
+; So instead of trusting relex, we are just going to drop back
+; to link-grammar, and look for the Pa link instead.
 (define look-rule-1
 	(BindLink
 		(VariableList
@@ -27,7 +32,8 @@
 			(word-in-parse   "$verb-inst" "$parse")
 			(LemmaLink (VariableNode "$verb-inst") (WordNode "look"))
 			(word-pos "$verb-inst" "verb")
-			(dependency "_advmod" "$verb-inst" "$direct-inst")
+			; (dependency "_advmod" "$verb-inst" "$direct-inst")
+			(lg-link "Pa" "$verb-inst" "$direct-inst")
 			(word-lemma "$direct-inst" "$direction")
 		)
 		(ExecutionOutput
@@ -38,32 +44,7 @@
 	)
 )
 
-(define look-rule-2
-	(BindLink
-		(VariableList
-			(var-decl "$sent" "SentenceNode")
-			(var-decl "$parse" "ParseNode")
-			(var-decl "$interp" "InterpretationNode")
-			(var-decl "$verb-inst" "WordInstanceNode")
-			(var-decl "$direct-inst" "WordInstanceNode")
-			(var-decl "$direction" "WordNode")
-		)
-		(AndLink
-			(parse-of-sent   "$parse" "$sent")
-			(interp-of-parse "$interp" "$parse")
-			(word-in-parse   "$verb-inst" "$parse")
-			(LemmaLink (VariableNode "$verb-inst") (WordNode "look"))
-			(word-pos "$verb-inst" "verb")
-			(dependency "_advmod" "$verb-inst" "$direct-inst")
-			(word-lemma "$direct-inst" "$direction")
-		)
-		(ExecutionOutput
-			; (GroundedSchema "py: hola")
-			(GroundedSchema "scm: show-arg")
-			(ListLink (Variable "$direction"))
-		)
-	)
-)
+(export look-rule-1)
 
 ;--------------------------------------------------------------------
 (define (imperative_process imp)
