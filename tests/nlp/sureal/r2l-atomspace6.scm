17ce7da2d4bbf3edc5e950bc1fabd2bce9079ad0
manhin <manhinleung@gmail.com>
2016-01-27 14:55:04 +0800

    Remove whitespaces

--- tests/nlp/sureal/r2l-atomspace6.scm
+++ tests/nlp/sureal/r2l-atomspace6.scm
@@ -10,56 +10,56 @@
    (ParseNode "sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0" (stv 1 0.98799998))
    (SentenceNode "sentence@42268c2b-9a82-4247-9c61-2b565818c9ac")
 )
- (SentenceSequenceLink (stv 1 0.99999982)
+(SentenceSequenceLink (stv 1 0.99999982)
    (SentenceNode "sentence@42268c2b-9a82-4247-9c61-2b565818c9ac")
    (NumberNode "1551407.000000")
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (DefinedLinguisticRelationshipNode "_advmod")
    (ListLink
       (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
       (WordInstanceNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
    )
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (DefinedLinguisticRelationshipNode "_subj")
    (ListLink
       (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
       (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    )
 )
- (ReferenceLink
+(ReferenceLink
    (PredicateNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
 )
- (ListLink
+(ListLink
    (WordNode "eat")
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    (DefinedLinguisticConceptNode "present")
 )
- (ListLink
+(ListLink
    (WordNode "she")
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (WordNode "eat")
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
 )
- (ListLink
+(ListLink
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    (WordInstanceNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
 )
- (ListLink
+(ListLink
    (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
 )
- (PartOfSpeechLink (stv 1 0.99999982)
+(PartOfSpeechLink (stv 1 0.99999982)
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    (DefinedLinguisticConceptNode "verb")
 )
- (ListLink
+(ListLink
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
 )
- (LgWordCset
+(LgWordCset
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    (LgAnd
       (LgConnector
@@ -77,47 +77,47 @@
       )
    )
 )
- (TenseLink (stv 1 0.99999982)
+(TenseLink (stv 1 0.99999982)
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    (DefinedLinguisticConceptNode "present")
 )
- (WordInstanceLink (stv 1 0.99999982)
+(WordInstanceLink (stv 1 0.99999982)
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    (ParseNode "sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0" (stv 1 0.98799998))
 )
- (ListLink
+(ListLink
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
 )
- (WordSequenceLink (stv 1 0.99999982)
+(WordSequenceLink (stv 1 0.99999982)
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    (NumberNode "1551404.000000")
 )
- (LemmaLink (stv 1 0.99999982)
+(LemmaLink (stv 1 0.99999982)
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    (WordNode "eat")
 )
- (ReferenceLink (stv 1 0.99999982)
+(ReferenceLink (stv 1 0.99999982)
    (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    (WordNode "eats")
 )
- (ReferenceLink
+(ReferenceLink
    (ConceptNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
    (WordInstanceNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
 )
- (ReferenceLink (stv 1 0.99999982)
+(ReferenceLink (stv 1 0.99999982)
    (WordInstanceNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
    (WordNode "quickly")
 )
- (LemmaLink (stv 1 0.99999982)
+(LemmaLink (stv 1 0.99999982)
    (WordInstanceNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
    (WordNode "quickly")
 )
- (WordInstanceLink (stv 1 0.99999982)
+(WordInstanceLink (stv 1 0.99999982)
    (WordInstanceNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
    (ParseNode "sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0" (stv 1 0.98799998))
 )
- (LgWordCset
+(LgWordCset
    (WordInstanceNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
    (LgAnd
       (LgConnector
@@ -126,19 +126,19 @@
       )
    )
 )
- (PartOfSpeechLink (stv 1 0.99999982)
+(PartOfSpeechLink (stv 1 0.99999982)
    (WordInstanceNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
    (DefinedLinguisticConceptNode "adv")
 )
- (WordSequenceLink (stv 1 0.99999982)
+(WordSequenceLink (stv 1 0.99999982)
    (WordInstanceNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
    (NumberNode "1551405.000000")
 )
- (WordInstanceLink (stv 1 0.99999982)
+(WordInstanceLink (stv 1 0.99999982)
    (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
    (ParseNode "sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0" (stv 1 0.98799998))
 )
- (LgWordCset
+(LgWordCset
    (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
    (LgAnd
       (LgConnector
@@ -155,39 +155,39 @@
       )
    )
 )
- (ListLink
+(ListLink
    (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
    (WordInstanceNode ".@af81ed10-7bd4-4e7c-b4f9-255ac44ff7f5")
 )
- (ListLink
+(ListLink
    (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
 )
- (WordSequenceLink (stv 1 0.99999982)
+(WordSequenceLink (stv 1 0.99999982)
    (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
    (NumberNode "1551402.000000")
 )
- (ReferenceLink (stv 1 0.99999982)
+(ReferenceLink (stv 1 0.99999982)
    (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
    (WordNode "###LEFT-WALL###")
 )
- (WordInstanceLink (stv 1 0.99999982)
+(WordInstanceLink (stv 1 0.99999982)
    (WordInstanceNode ".@af81ed10-7bd4-4e7c-b4f9-255ac44ff7f5")
    (ParseNode "sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0" (stv 1 0.98799998))
 )
- (PartOfSpeechLink (stv 1 0.99999982)
+(PartOfSpeechLink (stv 1 0.99999982)
    (WordInstanceNode ".@af81ed10-7bd4-4e7c-b4f9-255ac44ff7f5")
    (DefinedLinguisticConceptNode "punctuation")
 )
- (LemmaLink (stv 1 0.99999982)
+(LemmaLink (stv 1 0.99999982)
    (WordInstanceNode ".@af81ed10-7bd4-4e7c-b4f9-255ac44ff7f5")
    (WordNode ".")
 )
- (ReferenceLink (stv 1 0.99999982)
+(ReferenceLink (stv 1 0.99999982)
    (WordInstanceNode ".@af81ed10-7bd4-4e7c-b4f9-255ac44ff7f5")
    (WordNode ".")
 )
- (LgWordCset
+(LgWordCset
    (WordInstanceNode ".@af81ed10-7bd4-4e7c-b4f9-255ac44ff7f5")
    (LgAnd
       (LgConnector
@@ -200,35 +200,35 @@
       )
    )
 )
- (WordSequenceLink (stv 1 0.99999982)
+(WordSequenceLink (stv 1 0.99999982)
    (WordInstanceNode ".@af81ed10-7bd4-4e7c-b4f9-255ac44ff7f5")
    (NumberNode "1551406.000000")
 )
- (ReferenceLink
+(ReferenceLink
    (ConceptNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
 )
- (ListLink
+(ListLink
    (WordNode "she")
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
 )
- (WordInstanceLink (stv 1 0.99999982)
+(WordInstanceLink (stv 1 0.99999982)
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (ParseNode "sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0" (stv 1 0.98799998))
 )
- (InheritanceLink (stv 1 0.99999982)
+(InheritanceLink (stv 1 0.99999982)
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (DefinedLinguisticConceptNode "definite")
 )
- (InheritanceLink (stv 1 0.99999982)
+(InheritanceLink (stv 1 0.99999982)
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (DefinedLinguisticConceptNode "feminine")
 )
- (InheritanceLink (stv 1 0.99999982)
+(InheritanceLink (stv 1 0.99999982)
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (DefinedLinguisticConceptNode "pronoun")
 )
- (LgWordCset
+(LgWordCset
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (LgAnd
       (LgConnector
@@ -241,31 +241,31 @@
       )
    )
 )
- (WordSequenceLink (stv 1 0.99999982)
+(WordSequenceLink (stv 1 0.99999982)
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (NumberNode "1551403.000000")
 )
- (LemmaLink (stv 1 0.99999982)
+(LemmaLink (stv 1 0.99999982)
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (WordNode "she")
 )
- (InheritanceLink (stv 1 0.99999982)
+(InheritanceLink (stv 1 0.99999982)
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (DefinedLinguisticConceptNode "singular")
 )
- (PartOfSpeechLink (stv 1 0.99999982)
+(PartOfSpeechLink (stv 1 0.99999982)
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (DefinedLinguisticConceptNode "noun")
 )
- (ReferenceLink (stv 1 0.99999982)
+(ReferenceLink (stv 1 0.99999982)
    (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    (WordNode "she")
 )
- (ReferenceLink
+(ReferenceLink
    (LgLinkInstanceNode "MVa@54917077-b881-4840-b88c-d1c0c9ee06f6")
    (LinkGrammarRelationshipNode "MVa")
 )
- (LgLinkInstanceLink
+(LgLinkInstanceLink
    (LgLinkInstanceNode "MVa@54917077-b881-4840-b88c-d1c0c9ee06f6")
    (LgConnector
       (LgConnectorNode "MV")
@@ -276,25 +276,25 @@
       (LgConnDirNode "-")
    )
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (LgLinkInstanceNode "MVa@54917077-b881-4840-b88c-d1c0c9ee06f6")
    (ListLink
       (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
       (WordInstanceNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
    )
 )
- (ReferenceLink
+(ReferenceLink
    (LgLinkInstanceNode "WV@5bc2e712-4688-4c94-b4b5-fdab55535193")
    (LinkGrammarRelationshipNode "WV")
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (LgLinkInstanceNode "WV@5bc2e712-4688-4c94-b4b5-fdab55535193")
    (ListLink
       (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
       (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    )
 )
- (LgLinkInstanceLink
+(LgLinkInstanceLink
    (LgLinkInstanceNode "WV@5bc2e712-4688-4c94-b4b5-fdab55535193")
    (LgConnector
       (LgConnectorNode "hWV")
@@ -305,18 +305,18 @@
       (LgConnDirNode "-")
    )
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (LgLinkInstanceNode "Ss@d5fed3b0-9ed5-4ec7-8a93-025f76481318")
    (ListLink
       (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
       (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    )
 )
- (ReferenceLink
+(ReferenceLink
    (LgLinkInstanceNode "Ss@d5fed3b0-9ed5-4ec7-8a93-025f76481318")
    (LinkGrammarRelationshipNode "Ss")
 )
- (LgLinkInstanceLink
+(LgLinkInstanceLink
    (LgLinkInstanceNode "Ss@d5fed3b0-9ed5-4ec7-8a93-025f76481318")
    (LgConnector
       (LgConnectorNode "Ss")
@@ -327,18 +327,18 @@
       (LgConnDirNode "-")
    )
 )
- (ReferenceLink
+(ReferenceLink
    (LgLinkInstanceNode "Xp@01d297ec-49e6-4ff5-a8a9-ca153f2ad37b")
    (LinkGrammarRelationshipNode "Xp")
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (LgLinkInstanceNode "Xp@01d297ec-49e6-4ff5-a8a9-ca153f2ad37b")
    (ListLink
       (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
       (WordInstanceNode ".@af81ed10-7bd4-4e7c-b4f9-255ac44ff7f5")
    )
 )
- (LgLinkInstanceLink
+(LgLinkInstanceLink
    (LgLinkInstanceNode "Xp@01d297ec-49e6-4ff5-a8a9-ca153f2ad37b")
    (LgConnector
       (LgConnectorNode "Xp")
@@ -349,18 +349,18 @@
       (LgConnDirNode "-")
    )
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (LgLinkInstanceNode "Wd@dec4f2f9-fef5-4732-bf21-f37a2a923d96")
    (ListLink
       (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
       (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
    )
 )
- (ReferenceLink
+(ReferenceLink
    (LgLinkInstanceNode "Wd@dec4f2f9-fef5-4732-bf21-f37a2a923d96")
    (LinkGrammarRelationshipNode "Wd")
 )
- (LgLinkInstanceLink
+(LgLinkInstanceLink
    (LgLinkInstanceNode "Wd@dec4f2f9-fef5-4732-bf21-f37a2a923d96")
    (LgConnector
       (LgConnectorNode "Wd")
@@ -371,35 +371,35 @@
       (LgConnDirNode "-")
    )
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (LinkGrammarRelationshipNode "MVa")
    (ListLink
       (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
       (WordInstanceNode "quickly@6d5b0484-eb56-4a4e-887a-108fb300e391")
    )
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (LinkGrammarRelationshipNode "WV")
    (ListLink
       (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
       (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    )
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (LinkGrammarRelationshipNode "Ss")
    (ListLink
       (WordInstanceNode "she@cc9806ed-8bbc-4ec2-9c4d-a97ba41f700f")
       (WordInstanceNode "eats@b2862737-d65b-4c81-a1db-7ef65f5bf0c8")
    )
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (LinkGrammarRelationshipNode "Xp")
    (ListLink
       (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
       (WordInstanceNode ".@af81ed10-7bd4-4e7c-b4f9-255ac44ff7f5")
    )
 )
- (EvaluationLink (stv 1 0.99999982)
+(EvaluationLink (stv 1 0.99999982)
    (LinkGrammarRelationshipNode "Wd")
    (ListLink
       (WordInstanceNode "LEFT-WALL@sentence@42268c2b-9a82-4247-9c61-2b565818c9ac_parse_0")
