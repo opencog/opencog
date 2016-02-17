641748207a30b08d16cf29a65a2c5a298c4b40d1
Linas Vepstas <linasvepstas@gmail.com>
2016-02-16 20:44:13 -0600

    Split file into two.

--- opencog/nlp/chatbot-eva/chatbot-eva.scm
+++ opencog/nlp/chatbot-eva/chatbot-eva.scm
@@ -6,5 +6,6 @@
 (load "chatbot/chat-utils.scm")
 (load "chatbot-eva/self-model.scm")
 (load "chatbot-eva/imperative.scm")
+(load "chatbot-eva/model-query.scm")
 (load "chatbot-eva/eva.scm")
 (load "chatbot-eva/process-query.scm")
