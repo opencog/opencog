98c8a9bc377ebfcfd025220cbd6c6bcd7ffcb581
Linas Vepstas <linasvepstas@gmail.com>
2016-03-01 20:56:50 -0600

    Bugfix the SuRealUTest failure

--- opencog/nlp/sureal/surface-realization.scm
+++ opencog/nlp/sureal/surface-realization.scm
@@ -10,12 +10,11 @@
 
 (use-modules (srfi srfi-1)   ; needed for delete-duplicates
              (ice-9 threads) ; needed for par-map
-             (ice-9 rdelim) (ice-9 regex) (ice-9 receive)
-             (opencog)
-             (opencog nlp)
+             (ice-9 rdelim) (ice-9 regex) (ice-9 receive))
+(use-modules (opencog))
+(use-modules (opencog nlp)
              (opencog nlp lg-dict)
-             (opencog nlp relex2logic)
-)
+             (opencog nlp relex2logic))
 
 ; ---------------------------------------------------------------------
 ; Creates a single list  made of the elements of lists within it with the exception
