02f3f5386d5f51cd256a14dd1fcaa010e00122b8
Linas Vepstas <linasvepstas@gmail.com>
2016-03-04 11:08:10 -0600

    Another rule conversion

--- opencog/nlp/relex2logic/rules/whichsubjSVQ.scm
+++ opencog/nlp/relex2logic/rules/whichsubjSVQ.scm
@@ -10,33 +10,31 @@
 			(var-decl "$a-parse" "ParseNode")
 			(var-decl "$subj" "WordInstanceNode")
 			(var-decl "$verb" "WordInstanceNode")
+			(var-decl "$subj-lemma" "WordNode")
+			(var-decl "$verb-lemma" "WordNode")
 			(var-decl "$obj" "WordInstanceNode")
 			(var-decl "$qVar" "WordInstanceNode")
 		)
 		(AndLink
 			(word-in-parse "$subj" "$a-parse")
 			(word-in-parse "$verb" "$a-parse")
+			(word-lemma "$subj" "$subj-lemma")
+			(word-lemma "$verb" "$verb-lemma")
 			(dependency "_subj" "$verb" "$subj")
 			(AbsentLink (dependency "_obj" "$verb" "$obj"))
 			(dependency "_det" "$subj" "$qVar")
 			(word-feat "$qVar" "which")
 		)
 		(ExecutionOutputLink
-			(GroundedSchemaNode "scm: pre-whichsubjSVQ-rule")
+; XXX this is not implemented anywhere!
+			(GroundedSchemaNode "scm: whichsubjSVQ-rule")
 			(ListLink
+				(VariableNode "$subj-lemma")
 				(VariableNode "$subj")
+				(VariableNode "$verb-lemma")
 				(VariableNode "$verb")
 
 			)
 		)
-	)
-)
-
-; XXX FIXME: define the whichsubjSVQ-rule
-; This is function is not needed. It is added so as not to break the existing
-; r2l pipeline.
-(define (pre-whichsubjSVQ-rule subj verb)
-	(whichsubjSVQ-rule (cog-name (word-inst-get-lemma  subj)) (cog-name subj)
-			  (cog-name (word-inst-get-lemma  verb)) (cog-name verb)
 	)
 )
