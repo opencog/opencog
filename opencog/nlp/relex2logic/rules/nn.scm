98d42e3e8184c7fd42aa69e3319c8ba4f1b8d822
Linas Vepstas <linasvepstas@gmail.com>
2016-03-16 16:20:50 -0500

    Fix whitespace

--- opencog/nlp/relex2logic/rules/nn.scm
+++ opencog/nlp/relex2logic/rules/nn.scm
@@ -1,29 +1,30 @@
-;"He stood at the goal line."
+;
+; "He stood at the goal line."
 (define nn
-    (BindLink
-        (VariableList
+	(BindLink
+		(VariableList
 			(var-decl "$a-parse" "ParseNode")
 			(var-decl "$N1" "WordInstanceNode")
 			(var-decl "$N2" "WordInstanceNode")
-        )
-        (AndLink
+		)
+		(AndLink
 			(word-in-parse "$N1" "$a-parse")
 			(word-in-parse "$N2" "$a-parse")
 			(dependency "_nn" "$N1" "$N2")
-        )
-        (ExecutionOutputLink
-       	   (GroundedSchemaNode "scm: pre-nn-rule")
-       	      (ListLink
-       	         (VariableNode "$N1")
-       	         (VariableNode "$N2")
-            )
-        )
-    )
+		)
+		(ExecutionOutputLink
+			(GroundedSchemaNode "scm: pre-nn-rule")
+			(ListLink
+				(VariableNode "$N1")
+				(VariableNode "$N2")
+			)
+		)
+	)
 )
 
 
 (define (pre-nn-rule N1 N2)
-    (nn-rule (cog-name (word-inst-get-lemma  N1)) (cog-name N1)
-             (cog-name (word-inst-get-lemma N2)) (cog-name N2)
-    )
+	(nn-rule (cog-name (word-inst-get-lemma  N1)) (cog-name N1)
+			(cog-name (word-inst-get-lemma N2)) (cog-name N2)
+	)
 )
