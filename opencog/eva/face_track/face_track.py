5c6ac3b721ff5e0fc69d924b35ba29b360c73a28
Vytas <vytas@hansonrobotics.com>
2016-03-25 14:34:14 -0500

    Fix bug that re-enables face tracking

--- opencog/eva/face_track/face_track.py
+++ opencog/eva/face_track/face_track.py
@@ -409,5 +409,5 @@ class FaceTrack:
 			# Need to clear faces:
 			for face in self.visible_faces[:]:
 				self.remove_face(face)
-		else:
+		elif self.control_mode & self.C_FACE_TRACKING == 0:
 			self.atomo.update_ft_state_to_atomspace(True)
