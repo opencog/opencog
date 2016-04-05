b463588fa543bcf26be4d68dec94e4509aa1adf9
Linas Vepstas <linasvepstas@gmail.com>
2016-04-05 16:35:08 -0500

    Get sequence of words

--- opencog/nlp/aiml/running/run.scm
+++ opencog/nlp/aiml/running/run.scm
@@ -2,16 +2,46 @@
 ; Tools for running AIML in the AtomSpace.
 ;
 
-(use-modules (opencog) (opencog nlp) (opencog nlp chatbot-eva))
+(use-modules (srfi srfi-1))
+(use-modules (opencog) (opencog nlp))
 
 ; ==============================================================
-(define (make-token-sequnce SENT)
+
+(define-public (token-seq-of-parse PARSE)
 "
-  make-token-sequence -- Create a list of words from input sentence.
+  token-seq-of-parse PARSE -- Create a list of words from input parse.
+
+  PARSE is assumed to be a ParseNode, pointing to text that has been
+  processed by RelEx.
+
+  Example:
+     (relex-parse \"I love you\")
+     (map token-seq-of-parse
+         (sentence-get-parses (car (get-new-parsed-sentences))))
+"
+
+	(Evaluation
+		(PredicateNode "Token Sequence")
+		PARSE
+		(ListLink
+			(remove nil?
+				(map word-inst-get-lemma (parse-get-words-in-order PARSE)))
+		))
+)
+
+; ==============================================================
+(define-public (token-seq-of-sent SENT)
+"
+  token-seq-of-sent -- Create a list of words from input sentence.
 
   SENT is assumed to be a SentenceNode, pointing to text that has been
-  processed by RelEx and R2L.
+  processed by RelEx.
+
+  Example:
+     (relex-parse \"I love you\")
+     (token-seq-of-sent (car (get-new-parsed-sentences)))
 "
+	(map token-seq-of-parse (sentence-get-parses SENT))
 )
 
 ; ==============================================================
