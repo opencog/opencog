3e1f03bff71432e06e8b85814483747f44f40d59
Amen Belayneh <amenbelayneh@gmail.com>
2016-04-11 19:04:28 +0800

    add missing import

--- opencog/openpsi/goal-selector.scm
+++ opencog/openpsi/goal-selector.scm
@@ -1,6 +1,6 @@
 ; Copyright (C) 2016 OpenCog Foundation
 
-(use-modules (opencog) (opencog exec))
+(use-modules (opencog) (opencog query) (opencog exec))
 
 (load-from-path "openpsi/utilities.scm")
 
