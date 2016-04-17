9a5267b37cdeb173ea78a1c2b7e5cbe6a73feaff
Amen Belayneh <amenbelayneh@gmail.com>
2016-04-17 17:33:29 +0800

    add utility function most-weighted-atom for psi action selection

--- opencog/openpsi/utilities.scm
+++ opencog/openpsi/utilities.scm
@@ -3,6 +3,7 @@
 ; Helper functions for OpenPsi
 
 (use-modules (ice-9 regex)) ; For string-match
+(use-modules (srfi srfi-1)) ; For fold
 
 ; --------------------------------------------------------------
 (define-public (psi-prefix-str)
@@ -28,4 +29,23 @@
                 "\"" (psi-prefix-str) "\". " "Instead got:" a-string) )
         )
     )
+)
+
+; --------------------------------------------------------------
+(define-public (most-weighted-atom atom-list)
+"
+  It returns the atom with the highest weight, weight of an atom is the product
+  of the stength and confidence of the atom.
+
+  atom-list:
+  - A list of atoms to be compared.
+"
+    (define (weight x)
+        (let ((a-stv (cog-tv x)))
+            (* (tv-conf a-stv) (tv-mean a-stv))))
+
+   (fold
+       (lambda (x y) (if (> (weight y) (weight x)) y x))
+       (car atom-list) atom-list
+   )
 )
