f237589f6da2a419c9b1c263f10212a3684dc4b6
Eddie Monroe <eddiemonroe@gmail.com>
2016-05-26 10:45:21 -0400

    minor name changes

--- opencog/openpsi/parameters/updater.scm
+++ opencog/openpsi/parameters/updater.scm
@@ -2,11 +2,11 @@
 ;(use-modules (opencog))
 (use-modules (opencog atom-types))  ;needed for AtTimeLink definition?
 
-;; PARAM DEFINITIONS
-; todo: move to own file
+;;; PARAM DEFINITIONS
+;; todo: move to own file
 
-; todo: should the parmaters be a concept or predicate?
-; todo: and same with the somatic parameters
+;; todo: should the parmaters be a concept or predicate?
+;; todo: and same with the somatic parameters
 
 (define (create-openpsi-param name initial-value)
     (define param
@@ -16,16 +16,16 @@
         (Concept (string-append (psi-prefix-str) "Parameter")))
     param)
 
-; todo: make util function to define
+;; todo: make util function to define
 (define power (create-openpsi-param "Power" .5))
 
 
 
-;; EVENT PREDICATES
+;;; EVENT PREDICATES
 (Predicate "speech-giving starts" (stv 0 1))
 
 
-;; SOMATIC PREDICATES
+;;; SOMATIC PREDICATES
 (define somatic-prefix-str "Somatic: ")
 
 (define (create-somatic-predicate name initial-value)
@@ -35,20 +35,20 @@
 		somatic
 		(Concept "Somatic"))
 	somatic)
-; todo: can a Predicate inherit from a Concept?
-; maybe we instead should do?:
-; (Eval (Predicate "Somatic: voice thickness") (Predicate "Somatic"))
+;; todo: can a Predicate inherit from a Concept?
+;; maybe we instead should do?:
+;; (Eval (Predicate "Somatic: voice width") (Predicate "Somatic"))
 
-(define voice-thickness
-	(create-somatic-predicate "voice thickness" .5))
-
-
+(define voice-width
+	(create-somatic-predicate "voice width" .5))
 
 
-;; PARAM UPDATE RULES
-; todo: move to own file
 
-; When starting a speech -> boost power
+
+;;; PARAM UPDATE RULES
+;; todo: move to own file
+
+;; When starting a speech -> boost power
 ;; todo: how to represent "Starts in Interval"? ask Nil
 
 (Implication
@@ -62,15 +62,15 @@
     (AtTime
         (Variable "$time")
         (ExecutionLink
-            (DefinedSchema "change-openpsi-parameter")
+            (DefinedSchema "adjust-openpsi-parameter")
             (List
-                (Concept (string-append (psi-prefix-str) "Power"))
+                power
                 (NumberNode .7)))))
 
 
 
 ; When x changes --> change y
-; When power changes -> adjust speech-depth
+; When power changes -> adjust voice width
 
 (Implication
 	(TypedVariable
@@ -85,11 +85,14 @@
     (AtTime
         (Variable "$time")
         (ExecutionLink
-            (DefinedSchema "change-openpsi-parameter")
+            (DefinedSchema "adjust-openpsi-parameter")
             (List
-                voice-thickness
-                (NumberNode .3)))))
+                voice-width
+                (NumberNode .3)
+                power))))
 
+;; todo: we are not changing the y var in proportion to the change in the x var
+;; at this point. We may want to add that.
 
 ;(TimeNode (number->string (current-time)))
 
@@ -103,18 +106,21 @@
 
 ;; PARAMETER UPDATING
 
-; alpha is in the range of [-1, 1] and represents the degree of change
-;     0: no change
-;
-(define (change-openpsi-parameter param alpha)
-	(let* ((strength (cog-stv-strength param))
-		   (confidence (cog-stv-confidence param))
+;; alpha is in the range of [-1, 1] and represents the degree of change and
+;;     whether the change is positively or negatively correlated with the
+;;     origin parameter
+;;     0 would lead to no change
+;;
+;; todo: see case-lambda in scheme for function overloading
+(define (adjust-openpsi-parameter target-parm alpha . origin-param)
+	(let* ((strength (cog-stv-strength target-parm))
+		   (confidence (cog-stv-confidence target-parm))
 
 			; todo: replace this with a function
 			(new-strength (min 1 (max 0 (+ strength
 				(string->number (cog-name alpha)))))))
 
-		(cog-set-tv! param (cog-new-stv new-strength confidence))
+		(cog-set-tv! target-parm (cog-new-stv new-strength confidence))
 		(display "new strength: ")(display new-strength)(newline)))
 
 
