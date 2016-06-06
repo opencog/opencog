63c1d77355aed77621b20f21befd621db9bba084
mandeep singh bhatia <mandeep.singh.bhatia@gmail.com>
2016-06-06 20:33:43 +0800

    Bugfix

--- opencog/nlp/chatbot-psi/psi-rules.scm
+++ opencog/nlp/chatbot-psi/psi-rules.scm
@@ -6,12 +6,12 @@
 
 (Member
     (psi-rule
-        (list
+        (list (SequentialAnd
             (Not (DefinedPredicate "fuzzy-qa-search-started?"))
             (DefinedPredicate "is-input-utterance?")
             (DefinedPredicate "no-canned-reply?")
             (DefinedPredicate "is-interrogative?")
-        )
+        ))
         (True (ExecutionOutput (GroundedSchema "scm: do-fuzzy-QA") (List)))
         (True)
         (stv 1 1)
@@ -22,10 +22,10 @@
 
 (Member
     (psi-rule
-        (list
+        (list (SequentialAnd
             (DefinedPredicate "fuzzy-qa-search-started?")
             (DefinedPredicate "is-fuzzy-answer?")
-        )
+        ))
         (True (ExecutionOutput (GroundedSchema "scm: reply") (List fuzzy-answers)))
         (Evaluation (GroundedPredicate "scm: psi-demand-value-maximize") (List sociality (Number "90")))
         (stv .9 1)
@@ -36,10 +36,10 @@
 
 (Member
     (psi-rule
-        (list
+        (list (SequentialAnd
             (Not (DefinedPredicate "aiml-search-started?"))
             (DefinedPredicate "is-input-utterance?")
-        )
+        ))
         (True (ExecutionOutput (GroundedSchema "scm: do-aiml-search") (List)))
         (True)
         (stv 1 1)
@@ -50,10 +50,10 @@
 
 (Member
     (psi-rule
-        (list
+        (list (SequentialAnd
             (DefinedPredicate "aiml-search-started?")
             (DefinedPredicate "is-aiml-reply?")
-        )
+        ))
         (True (ExecutionOutput (GroundedSchema "scm: reply") (List aiml-replies)))
         (Evaluation (GroundedPredicate "scm: psi-demand-value-maximize") (List sociality (Number "90")))
         (stv .9 1)
