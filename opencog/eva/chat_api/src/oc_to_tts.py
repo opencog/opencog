1166ae33573daf0b7c337065de7b34d7b55c26c6
Linas Vepštas <linasvepstas@gmail.com>
2016-06-06 18:19:49 +0800

    Try to cleanup the server

--- opencog/eva/chat_api/src/oc_to_tts.py
+++ opencog/eva/chat_api/src/oc_to_tts.py
@@ -5,15 +5,12 @@ import SocketServer
 import rospy
 from std_msgs.msg import String
 
-global pub 
+global pub
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
+class StringTCPHandler(SocketServer.BaseRequestHandler):
     """
-    The request handler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
+    Receive plain-old strings (from the opencog chatbot)
+    and convert them to ROS messages.
     """
 
     def handle(self):
@@ -30,12 +27,11 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 if __name__ == "__main__":
     global pub
     HOST, PORT = "localhost", 17030
-    rospy.init_node('oc_chat_tts',anonymous=True)
+    rospy.init_node('oc_chat_tts', anonymous=True)
     pub = rospy.Publisher('/tts', String, queue_size=5)
-    # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    # Create the server, binding to localhost on port 17030
+    server = SocketServer.TCPServer((HOST, PORT), StringTCPHandler)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever(poll_interval=0.5)
-
