bc36746c82cdb345b24e7f4879ecd7f716178196
mandeep singh bhatia <mandeep.singh.bhatia@gmail.com>
2016-06-13 14:16:41 +0800

    additions to test

---
+++ opencog/timeoctomap/pointmemory/point-memory.scm
@@ -0,0 +1,13 @@
+(setenv "LTDL_LIBRARY_PATH"
+    (if (getenv "LTDL_LIBRARY_PATH")
+        (string-append (getenv "LTDL_LIBRARY_PATH")
+            ":/usr/local/lib/opencog:/usr/local/lib/opencog/modules")
+        "/usr/local/lib/opencog:/usr/local/lib/opencog/modules"))
+
+(define-module (opencog ato pointmem))
+
+(use-modules (srfi srfi-1) (opencog) (opencog ato))
+
+(load-extension "libpoint_memory" "opencog_ato_pointmem_init")
+
+
