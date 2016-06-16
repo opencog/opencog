601d59ba117d36ffe7fad39c3ce713599d38a185
leungmanhin <manhinleung@gmail.com>
2016-06-16 18:43:00 +0800

    Bugfix

--- opencog/nlp/chatbot-psi/duckduckgo.scm
+++ opencog/nlp/chatbot-psi/duckduckgo.scm
@@ -14,14 +14,17 @@ def set_atomspace(atsp):
     return TruthValue(1, 1)
 
 # TODO: Attribution!
-def call_duckduckgo(query):
+def call_duckduckgo(qq):
     global atomspace
 
     # Anchor for the result
     answer_anchor = atomspace.add_node(types.AnchorNode, 'DuckDuckGoAnswers')
 
+    # Avoid HTTP Error 400: Bad Request
+    query = qq.name.replace(' ', '+')
+
     # Send the query
-    response = urllib2.urlopen('http://api.duckduckgo.com/?q=' + query.name + '&format=json').read()
+    response = urllib2.urlopen('http://api.duckduckgo.com/?q=' + query + '&format=json').read()
     result = json.loads(response)
     abstract_text = result['AbstractText']
 
@@ -29,7 +32,7 @@ def call_duckduckgo(query):
         ans = atomspace.add_node(types.Node, abstract_text)
         atomspace.add_link(types.StateLink, [answer_anchor, ans])
     else:
-        no_result = atomspace.add_node(types.Node, 'NoResult')
+        no_result = atomspace.add_node(types.ConceptNode, 'NoResult')
         atomspace.add_link(types.StateLink, [answer_anchor, no_result])
 
     return TruthValue(1, 1)
