ca25c8091ddeff50696dfced32a4392cdf168e4e
Amen Belayneh <amenbelayneh@gmail.com>
2016-06-21 18:57:21 +0800

    organize psi functions into different files for clarity

--- opencog/openpsi/action-selector.scm
+++ opencog/openpsi/action-selector.scm
@@ -3,6 +3,7 @@
 (use-modules (opencog) (opencog exec))
 
 (load "demand.scm")
+(load "rule.scm")
 (load "utilities.scm")
 
 ; --------------------------------------------------------------
@@ -103,4 +104,120 @@
   DEMAND-NODE.
 "
     (psi-get-functionality demand-node "action-selector")
+)
+
+; --------------------------------------------------------------
+(define-public (psi-default-action-selector a-random-state)
+"
+  Returns a list of one of the most-important-weighted and satisfiable psi-rule
+  or an empty list. A single psi-rule is returned so as help avoid mulitple
+  actions of the same effect or type(aka semantic of the action) from being
+  executed. If a satisfiable rule doesn't exist then the empty list is returned.
+
+  a-random-state:
+  - A random-state object used as a seed for choosing how multiple satisfiable
+  psi-rules with the same weight are to be choosen.
+"
+    (define (choose-rules)
+        ; NOTE: This check is required as ecan isn't being used continuesely.
+        ; Remove `most-weighted-atoms` version once ecan is integrated.
+        (if (or (equal? 0 (cog-af-boundary)) (equal? 1 (cog-af-boundary)))
+            (most-weighted-atoms (psi-get-all-rules))
+            (most-important-weighted-atoms (psi-get-all-satisfiable-rules))
+        )
+    )
+
+    (let ((rules (choose-rules)))
+        (if (null? rules)
+            '()
+            (list (list-ref rules (random (length rules) a-random-state)))
+        )
+    )
+)
+
+; --------------------------------------------------------------
+(define-public (psi-select-rules)
+"
+  Returns a list of psi-rules that are satisfiable by using the action-selector
+  you defined or the default-action-selector predefined if you haven't defined
+  a different action-selector.
+"
+    (let ((dsn (psi-get-action-selector-generic)))
+        (if (null? dsn)
+            (psi-default-action-selector (random-state-from-platform))
+            (let ((result (cog-execute! (car dsn))))
+                (if (equal? (cog-type result) 'SetLink)
+                    (cog-outgoing-set result)
+                    (list result)
+                )
+            )
+        )
+    )
+)
+
+; --------------------------------------------------------------
+(define-public (psi-default-action-selector-per-demand a-random-state demand)
+"
+  Returns a list of one of the most-important-weighted and satisfiable psi-rule
+  or an empty list. A single psi-rule is returned so as help avoid mulitple
+  actions of the same effect or type(aka semantic of the action) from being
+  executed. If a satisfiable rule doesn't exist then the empty list is returned.
+
+  a-random-state:
+  - A random-state object used as a seed for choosing how multiple satisfiable
+  psi-rules with the same weight are to be choosen.
+"
+    (define (choose-rules)
+        ; NOTE: This check is required as ecan isn't being used continuesely.
+        ; Remove `most-weighted-atoms` version once ecan is integrated.
+        (if (or (equal? 0 (cog-af-boundary)) (equal? 1 (cog-af-boundary)))
+            (most-weighted-atoms (psi-get-satisfiable-rules demand))
+            (most-important-weighted-atoms (psi-get-all-satisfiable-rules))
+        )
+    )
+
+    (let ((rules (choose-rules)))
+        (if (null? rules)
+            '()
+            (list (list-ref rules (random (length rules) a-random-state)))
+        )
+    )
+)
+
+; --------------------------------------------------------------
+(define-public (psi-select-rules-per-demand d)
+"
+  Returns a list of psi-rules that are satisfiable by using the action-selector
+  you defined or the default-action-selector predefined if you haven't defined
+  a different action-selector.
+"
+    (if (equal? d (ConceptNode "OpenPsi: AIML chat demand"))
+        (list) ; Skip the aiml chat demand . FIXME: this is a hack
+        (let ((as (psi-get-action-selector d)))
+            (if (null? as)
+                (psi-default-action-selector-per-demand
+                           (random-state-from-platform) d)
+                (let ((result (cog-execute! (car as))))
+                    (if (equal? (cog-type result) 'SetLink)
+                        (cog-outgoing-set result)
+                        (list result)
+                    )
+                )
+            )
+        )
+    )
+
+
+    ;(let ((demands (psi-get-all-demands)))
+    ;    ;NOTE:
+    ;    ; 1. If there is any hierarcy/graph, get the information from the
+    ;    ;    atomspace and do it here.
+    ;    ; 2. Any changes between steps are accounted for, i.e, there is no
+    ;    ;    caching of demands. This has a performance penality.
+    ;    ; FIXME:
+    ;    ; 1. Right now the demands are not separated between those that
+    ;    ;    are used for emotiong modeling vs those that are used for system
+    ;    ;    such as chat, behavior, ...
+    ;    (append-map select-rules demands)
+    ;)
 )
