d4c23bad37b11ca08c38ca9b86605242e868a1ab
Andre Senna <andre.senna@gmail.com>
2016-06-29 17:24:54 -0300

    Utility methods to retrive atoms from SCM files
    - This commit is just the first skeleton of the class. No real functionality implemented.

---
+++ opencog/nlp/scm2pgsql/SCMAtomsParser.cc
@@ -0,0 +1,32 @@
+/*
+ * SCMAtomsParser.cc
+ *
+ * Copyright (C) 2016 OpenCog Foundation
+ *
+ * Author: Andre Senna <https://github.com/andre-senna>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "SCMAtomsParser.h"
+
+SCMAtomsParser::SCMAtomsParser(const char *fileName)
+{
+}
+
+SCMAtomsParser::~SCMAtomsParser()
+{
+}
