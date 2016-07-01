ff4a52f0cd49c5f7b2834a8cc66ba4c398031cda
Andre Senna <andre.senna@gmail.com>
2016-07-01 15:12:04 -0300

    Helper class to load huge SCM files with atoms

---
+++ opencog/nlp/scm2pgsql/SCMLoader.h
@@ -0,0 +1,49 @@
+/*
+ * SCMLoader.h
+ *
+ * Copyright (C) 2016 OpenCog Foundation
+ *
+ * Author: Andre Senna <https://github.com/andre-senna>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License v3 as
+ * published by the Free Software Foundation and including the exceptions
+ * at http://opencog.org/wiki/Licenses
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to:
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _OPENCOG_SCMLOADER_H
+#define _OPENCOG_SCMLOADER_H
+
+#include <fstream>
+
+namespace opencog
+{
+
+/**
+ * 
+ */
+class SCMLoader 
+{
+
+public:
+
+    static bool load(const std:string &fileName, AtomSpace &atomSpace);
+
+private:
+
+    static void parseFile(std:fstream &fin, AtomSpace &atomSpace);
+};
+
+}
+
+#endif // _OPENCOG_SCMLOADER_H
