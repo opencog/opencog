f39e8133d88dc6d92a884b649308e20ee02f0966
Nil Geisweiller <ngeiswei@gmail.com>
2016-07-05 16:27:44 +0300

    Implement implication direct evaluation + add utest

---
+++ tests/pln/rules/implication-direct-evaluation.scm
@@ -0,0 +1,31 @@
+;; For testing implication direct evaluation rule
+
+;; Instances of P
+(Evaluation
+   (Predicate "P")
+   (Concept "A"))
+
+(Evaluation
+   (Predicate "P")
+   (Concept "B"))
+
+(Evaluation
+   (Predicate "P")
+   (Concept "C"))
+
+;; Instances of Q
+(Evaluation
+   (Predicate "Q")
+   (Concept "B"))
+
+(Evaluation
+   (Predicate "Q")
+   (Concept "C"))
+
+(Evaluation
+   (Predicate "Q")
+   (Concept "D"))
+
+(Evaluation
+   (Predicate "Q")
+   (Concept "E"))
