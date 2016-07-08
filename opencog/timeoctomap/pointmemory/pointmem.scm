8a57a4d3196a2fdb07ca6bc7e7f864c10f8c080a
mandeep singh bhatia <mandeep.singh.bhatia@gmail.com>
2016-07-08 17:48:39 +0800

    changes to pointmem.scm, to be tested

--- opencog/timeoctomap/pointmemory/pointmem.scm
+++ opencog/timeoctomap/pointmemory/pointmem.scm
@@ -6,8 +6,20 @@
 
 (define-module (opencog ato pointmem))
 
-(use-modules (srfi srfi-1) (opencog) )
+(use-modules (srfi srfi-1) (opencog) (opencog atom-types) (opencog exec))
 
 (load-extension "libpoint_memory" "opencog_ato_pointmem_init")
 
+;utility functions
 
+;;Convert from at location link to (x y z) list
+(define (space-nodes at-loc-link) (cog-outgoing-set (cadr (cog-outgoing-set (cadr (cog-outgoing-set al-link))))))
+(define (loc-link-x at-loc-link) 
+		(string->number (cog-name (car (space-nodes at-loc-link))))
+)
+(define (loc-link-y at-loc-link)
+                (string->number (cog-name (cadr (space-nodes at-loc-link))))
+)
+(define (loc-link-z at-loc-link)
+                (string->number (cog-name (caddr (space-nodes at-loc-link))))
+)
