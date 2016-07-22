42b1d0bd6ff089ef24036317468f5bed11ea9779
leungmanhin <manhinleung@gmail.com>
2016-07-21 22:47:08 -0400

    Set more psi controlled rules

--- opencog/nlp/chatbot-psi/psi-rules.scm
+++ opencog/nlp/chatbot-psi/psi-rules.scm
@@ -32,39 +32,45 @@
     )
 )
 
-(psi-rule
-    (list (SequentialAnd
-        (DefinedPredicate "fuzzy-finished?")
-        (DefinedPredicate "is-fuzzy-reply?")
-        (SequentialOr
-            (Not (DefinedPredicate "input-is-a-question?"))
-            (DefinedPredicate "fuzzy-reply-is-declarative?"))
-        (SequentialOr
-            (DefinedPredicate "is-fuzzy-reply-good?")
-            (DefinedPredicate "no-other-fast-reply?"))
-        (DefinedPredicate "has-not-replied-anything-yet?")
-    ))
-    (True (ExecutionOutput (GroundedSchema "scm: reply") (List fuzzy-reply)))
-    (True)
-    (stv .9 .9)
-    sociality
+(psi-set-controlled-rule
+    (psi-rule
+        (list (SequentialAnd
+            (DefinedPredicate "fuzzy-finished?")
+            (DefinedPredicate "is-fuzzy-reply?")
+            (SequentialOr
+                (Not (DefinedPredicate "input-is-a-question?"))
+                (DefinedPredicate "fuzzy-reply-is-declarative?"))
+            (SequentialOr
+                (DefinedPredicate "is-fuzzy-reply-good?")
+                (DefinedPredicate "no-other-fast-reply?"))
+            (DefinedPredicate "has-not-replied-anything-yet?")
+        ))
+        (True (ExecutionOutput (GroundedSchema "scm: reply") (List fuzzy-reply)))
+        (True)
+        (stv .9 .9)
+        sociality
+        "fuzzy_matcher"
+    )
 )
 
-(psi-rule
-    (list (SequentialAnd
-        (DefinedPredicate "fuzzy-finished?")
-        (DefinedPredicate "is-fuzzy-reply?")
-        (DefinedPredicate "input-is-a-question?")
-        (Not (DefinedPredicate "fuzzy-reply-is-declarative?"))
-        (SequentialOr
-            (DefinedPredicate "is-fuzzy-reply-good?")
-            (DefinedPredicate "no-good-fast-answer?"))
-        (DefinedPredicate "has-not-replied-anything-yet?")
-    ))
-    (True (ExecutionOutput (GroundedSchema "scm: reply") (List fuzzy-reply)))
-    (True)
-    (stv .7 .7)
-    sociality
+(psi-set-controlled-rule
+    (psi-rule
+        (list (SequentialAnd
+            (DefinedPredicate "fuzzy-finished?")
+            (DefinedPredicate "is-fuzzy-reply?")
+            (DefinedPredicate "input-is-a-question?")
+            (Not (DefinedPredicate "fuzzy-reply-is-declarative?"))
+            (SequentialOr
+                (DefinedPredicate "is-fuzzy-reply-good?")
+                (DefinedPredicate "no-good-fast-answer?"))
+            (DefinedPredicate "has-not-replied-anything-yet?")
+        ))
+        (True (ExecutionOutput (GroundedSchema "scm: reply") (List fuzzy-reply)))
+        (True)
+        (stv .7 .7)
+        sociality
+        "fuzzy_matcher"
+    )
 )
 
 (psi-set-controlled-rule
@@ -84,20 +90,24 @@
     )
 )
 
-(psi-rule
-    (list (SequentialAnd
-        (DefinedPredicate "aiml-finished?")
-        (DefinedPredicate "is-aiml-reply?")
-        (DefinedPredicate "input-is-about-the-robot?")
-        (DefinedPredicate "has-not-replied-anything-yet?")
-    ))
-    (True (ExecutionOutput (GroundedSchema "scm: reply") (List aiml-reply)))
-    (True)
-    (stv .9 .9)
-    sociality
+(psi-set-controlled-rule
+    (psi-rule
+        (list (SequentialAnd
+            (DefinedPredicate "aiml-finished?")
+            (DefinedPredicate "is-aiml-reply?")
+            (DefinedPredicate "input-is-about-the-robot?")
+            (DefinedPredicate "has-not-replied-anything-yet?")
+        ))
+        (True (ExecutionOutput (GroundedSchema "scm: reply") (List aiml-reply)))
+        (True)
+        (stv .9 .9)
+        sociality
+        "aiml"
+    )
 )
 
 (Member
+(psi-set-controlled-rule
     (psi-rule
         (list (SequentialAnd
             (DefinedPredicate "aiml-finished?")
@@ -109,11 +119,14 @@
         (True)
         (stv .9 .9)
         sociality
+        "aiml"
     )
+)
     aiml-reply-rule
 )
 
 (Member
+(psi-set-controlled-rule
     (psi-rule
         (list (SequentialAnd
             (DefinedPredicate "aiml-finished?")
@@ -129,7 +142,9 @@
         (True)
         (stv .9 .9)
         sociality
+        "aiml"
     )
+)
     aiml-reply-rule
 )
 
@@ -148,31 +163,37 @@
     )
 )
 
-(psi-rule
-    (list (SequentialAnd
-        (DefinedPredicate "duckduckgo-finished?")
-        (DefinedPredicate "is-duckduckgo-answer?")
-        (Not (DefinedPredicate "input-is-about-the-robot?"))
-        (DefinedPredicate "has-not-replied-anything-yet?")
-    ))
-    (True (ExecutionOutput (GroundedSchema "scm: reply") (List duckduckgo-answer)))
-    (True)
-    (stv .9 .9)
-    sociality
+(psi-set-controlled-rule
+    (psi-rule
+        (list (SequentialAnd
+            (DefinedPredicate "duckduckgo-finished?")
+            (DefinedPredicate "is-duckduckgo-answer?")
+            (Not (DefinedPredicate "input-is-about-the-robot?"))
+            (DefinedPredicate "has-not-replied-anything-yet?")
+        ))
+        (True (ExecutionOutput (GroundedSchema "scm: reply") (List duckduckgo-answer)))
+        (True)
+        (stv .9 .9)
+        sociality
+        "duckduckgo"
+    )
 )
 
-(psi-rule
-    (list (SequentialAnd
-        (DefinedPredicate "duckduckgo-finished?")
-        (DefinedPredicate "is-duckduckgo-answer?")
-        (DefinedPredicate "input-is-about-the-robot?")
-        (DefinedPredicate "no-other-fast-reply?")
-        (DefinedPredicate "has-not-replied-anything-yet?")
-    ))
-    (True (ExecutionOutput (GroundedSchema "scm: reply") (List duckduckgo-answer)))
-    (True)
-    (stv .9 .9)
-    sociality
+(psi-set-controlled-rule
+    (psi-rule
+        (list (SequentialAnd
+            (DefinedPredicate "duckduckgo-finished?")
+            (DefinedPredicate "is-duckduckgo-answer?")
+            (DefinedPredicate "input-is-about-the-robot?")
+            (DefinedPredicate "no-other-fast-reply?")
+            (DefinedPredicate "has-not-replied-anything-yet?")
+        ))
+        (True (ExecutionOutput (GroundedSchema "scm: reply") (List duckduckgo-answer)))
+        (True)
+        (stv .9 .9)
+        sociality
+        "duckduckgo"
+    )
 )
 
 (psi-set-controlled-rule
@@ -191,31 +212,37 @@
     )
 )
 
-(psi-rule
-    (list (SequentialAnd
-        (DefinedPredicate "wolframalpha-finished?")
-        (DefinedPredicate "is-wolframalpha-answer?")
-        (Not (DefinedPredicate "input-is-about-the-robot?"))
-        (DefinedPredicate "has-not-replied-anything-yet?")
-    ))
-    (True (ExecutionOutput (GroundedSchema "scm: reply") (List wolframalpha-answer)))
-    (True)
-    (stv .9 .9)
-    sociality
+(psi-set-controlled-rule
+    (psi-rule
+        (list (SequentialAnd
+            (DefinedPredicate "wolframalpha-finished?")
+            (DefinedPredicate "is-wolframalpha-answer?")
+            (Not (DefinedPredicate "input-is-about-the-robot?"))
+            (DefinedPredicate "has-not-replied-anything-yet?")
+        ))
+        (True (ExecutionOutput (GroundedSchema "scm: reply") (List wolframalpha-answer)))
+        (True)
+        (stv .9 .9)
+        sociality
+        "wolframalpha"
+    )
 )
 
-(psi-rule
-    (list (SequentialAnd
-        (DefinedPredicate "wolframalpha-finished?")
-        (DefinedPredicate "is-wolframalpha-answer?")
-        (DefinedPredicate "input-is-about-the-robot?")
-        (DefinedPredicate "no-other-fast-reply?")
-        (DefinedPredicate "has-not-replied-anything-yet?")
-    ))
-    (True (ExecutionOutput (GroundedSchema "scm: reply") (List wolframalpha-answer)))
-    (True)
-    (stv .9 .9)
-    sociality
+(psi-set-controlled-rule
+    (psi-rule
+        (list (SequentialAnd
+            (DefinedPredicate "wolframalpha-finished?")
+            (DefinedPredicate "is-wolframalpha-answer?")
+            (DefinedPredicate "input-is-about-the-robot?")
+            (DefinedPredicate "no-other-fast-reply?")
+            (DefinedPredicate "has-not-replied-anything-yet?")
+        ))
+        (True (ExecutionOutput (GroundedSchema "scm: reply") (List wolframalpha-answer)))
+        (True)
+        (stv .9 .9)
+        sociality
+        "wolframalpha"
+    )
 )
 
 (psi-set-controlled-rule
