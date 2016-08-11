c717b8658ec33c0ab76cfc8b15459e0604b2f610
Roman Treutlein <roman@hansonrobotics.com>
2016-08-11 08:56:47 +0200

    Removed Unused Code

--- opencog/attention/scm/attention.scm
+++
@@ -1,9 +0,0 @@
-(setenv "LTDL_LIBRARY_PATH"
-    (if (getenv "LTDL_LIBRARY_PATH")
-        (string-append (getenv "LTDL_LIBRARY_PATH")
-            ":/usr/local/lib/opencog:/usr/local/lib/opencog/modules")
-        "/usr/local/lib/opencog:/usr/local/lib/opencog/modules"))
-
-(define-module (opencog attention))
-
-(load-extension "libguile-attention" "opencog_ecan_init")
