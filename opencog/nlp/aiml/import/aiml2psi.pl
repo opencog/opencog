df10e2355cd56e116e61a534eba7aa1d0773f091
leungmanhin <manhinleung@gmail.com>
2016-09-07 13:12:07 +0800

    Accept plain '0' as a pattern

--- opencog/nlp/aiml/import/aiml2psi.pl
+++ opencog/nlp/aiml/import/aiml2psi.pl
@@ -1288,7 +1288,10 @@ while (my $line = <FIN>)
 	if (length($line) < 1) { next; }
 	my @parms = split(/\,/, $line);
 	my $cmd = $parms[0] || "";
-	my $arg = $parms[1] || "";
+
+	# To accept a pattern like this "<pattern>0</pattern>" as well
+	my $arg = ($parms[1] || $parms[1] eq 0)? $parms[1] : "";
+
 	if (length($cmd) < 1) { next; }
 
 	# Un-do the comma-damage up above.
