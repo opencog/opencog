847d59b7d29c842d92a028707cbf295fa68c8748
yenat <yenatshif@gmail.com>
2016-09-22 15:01:07 +0300

    fixes

--- opencog/eva/src/orchestrate.scm
+++ opencog/eva/src/orchestrate.scm
@@ -234,8 +234,8 @@
 	))
 ;show happy emotion	
 (DefineLink
-    (DefinedPredicate "Quiet")
-    (LambdaLink (Concept "happy")
+    (DefinedPredicate "Quiet:happy")
+ ;   (LambdaLink (Concept "happy")
          (Evaluation
             (GroundedPredicate "py: do_emotion")
            (List (Concept "happy")(NumberNode 3) (NumberNode 0.5)))
