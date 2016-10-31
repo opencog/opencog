1218658897b7de6eefc86965d863075f71125794
Amen Belayneh <amenbelayneh@gmail.com>
2016-10-31 17:09:56 +0800

    Move function around and rename it to (enable-pln-demo)

--- opencog/nlp/chatbot-psi/demo.scm
+++ opencog/nlp/chatbot-psi/demo.scm
@@ -7,26 +7,6 @@
 ; This hasn't been tested.
 
 ; --------------------------------------------------------------
-(define-public (enable-pln-demo-only)
-"
-  Enables the openpsi-pln rules and the openpsi-aiml rules only. The aiml rules
-  are enalbed b/c they are the primary chat interface.
-"
-    ; Stop the loops.
-    (psi-halt)
-    (pln-halt)
-
-    ; Make the weight changes needed for configuration.
-    (disable-all-demos)
-    (psi-rule-enable "select_pln_answer" (psi-get-controlled-rules))
-    (psi-rule-enable "aiml" (psi-get-controlled-rules))
-
-    ; Start the loops.
-    (pln-run)
-    (psi-run)
-)
-
-; --------------------------------------------------------------
 (define-public (enable-all-demos)
 "
   This is the default mode. All the rules are given a weight of 0.9.
@@ -90,3 +70,26 @@
         result
     )
 )
+
+
+; --------------------------------------------------------------
+(define-public (enable-pln-demo)
+"
+  Enables the openpsi-pln rules and the openpsi-aiml rules only. The aiml rules
+  are enalbed b/c they are the primary chat interface.
+"
+    ; Stop the loops.
+    (psi-halt)
+    (pln-halt)
+
+    ; Make the weight changes needed for configuration.
+    (disable-all-demos)
+    (psi-rule-enable "select_pln_answer" (psi-get-controlled-rules))
+    (psi-rule-enable "aiml" (psi-get-controlled-rules))
+
+    ; Start the loops.
+    (pln-run)
+    (psi-run)
+)
+
+; --------------------------------------------------------------
