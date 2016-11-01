f61a5db0b70db6d861df6828e925eceae8d0f840
yenat <yenatshif@gmail.com>
2016-11-01 16:35:49 +0300

    remove atomese script

--- opencog/eva/sound/audioclass.py
+++ opencog/eva/sound/audioclass.py
@@ -43,45 +43,21 @@ class AudioStrength:
     # TODO: Convert each if/elif clause into a psi-rule.
     if value < 35:
         print "very low sound", value
-        x = "(cog-evaluate! (EvaluationLink  (DefinedPredicateNode \"Show expression\")" + \
-       "(ListLink (ConceptNode \"happy\") (NumberNode 3) (NumberNode 0.5))))\n"
-
-
-        netcat(self.hostname, self.port, x + "\n")
-
+        
         return 'Quiet Whisper'
 
     elif value < 65:
         print "Normal conversation:", value
 
-        y = "(cog-evaluate! (EvaluationLink  (DefinedPredicateNode \"Show expression\")" + \
-       "(ListLink (ConceptNode \"amused\") (NumberNode 3) (NumberNode 0.5))))\n"
-
-        netcat(self.hostname, self.port, y + "\n")
-
-        m = "(cog-evaluate! (EvaluationLink  (DefinedPredicateNode \"Show gesture\")" + \
-       "(ListLink (ConceptNode \"nod-1\") (NumberNode 0.2) (NumberNode 2) (NumberNode 0.8))))\n"
-
-        netcat(self.hostname, self.port, m + "\n")
-
         return 'Normal Conversation'
 
     elif value < 90:
         print "high sound:", value
-        z = "(cog-evaluate! (EvaluationLink  (DefinedPredicateNode \"Show expression\")" + \
-       "(ListLink (ConceptNode \"irritated\") (NumberNode 3) (NumberNode 0.5))))\n"
-        netcat(self.hostname, self.port, z + "\n")
-
-        n = "(cog-evaluate! (EvaluationLink  (DefinedPredicateNode \"Show gesture\")" + \
-       "(ListLink (ConceptNode \"think-browsDown.001\") (NumberNode 0.2) (NumberNode 2) (NumberNode 0.8))))\n"
-
-        netcat(self.hostname, self.port, n + "\n")
+       
         return 'Loud: Shouted Conversation'
     else:
         print 'critical:', value
-        t = "(cog-evaluate! (EvaluationLink  (DefinedPredicateNode \"Show expression\")" + \
-       "(ListLink (ConceptNode \"afraid\") (NumberNode 3) (NumberNode 0.5))))\n"
-        netcat(self.hostname, self.port, t + "\n")
+       
         return 'Loud: Critical'
 
   def GetAudioClass(self, data):
@@ -110,10 +86,7 @@ class AudioStrength:
     netcat(self.hostname, self.port, loud + "\n")
     
     if int(change) >= 10:
-        l = "(cog-evaluate! (EvaluationLink (DefinedPredicateNode \"Show expression\")" + \
-           "(ListLink (ConceptNode \"surprised\") (NumberNode 2) (NumberNode 0.5))))\n"
-                  
-        netcat(self.hostname, self.port, l + "\n")
+       print "Sudden change"
     else:
         self.AudioEnergy(self.Decibel)
 
