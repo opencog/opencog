aeb1092a855e4c3f0662ef0cbd38d48d75d685f6
Linas Vepstas <linasvepstas@gmail.com>
2016-11-04 18:17:14 -0500

    Remove some crud

--- opencog/timeoctomap/pointmemory/pointmem.scm
+++ opencog/timeoctomap/pointmemory/pointmem.scm
@@ -1,16 +1,10 @@
-(setenv "LTDL_LIBRARY_PATH"
-    (if (getenv "LTDL_LIBRARY_PATH")
-        (string-append (getenv "LTDL_LIBRARY_PATH")
-            ":/usr/local/lib/opencog:/usr/local/lib/opencog/modules")
-        "/usr/local/lib/opencog:/usr/local/lib/opencog/modules"))
-
 (define-module (opencog ato pointmem))
 
 (use-modules (srfi srfi-1) (opencog) (opencog atom-types) (opencog exec))
 
 (load-extension "libpoint_memory" "opencog_ato_pointmem_init")
 
-;utility functions
+; utility functions
 
 ;;Convert from at location link to (x y z) list
 (define-public (space-nodes at-loc-link) (cog-outgoing-set (cadr (cog-outgoing-set (cadr (cog-outgoing-set at-loc-link))))))
