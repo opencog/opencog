ede3cc7b10a40e5d1063ff29051777b1b09772c2
Amen Belayneh <amenbelayneh@gmail.com>
2016-11-10 15:51:49 +0800

    disable syncing of ros parameters

--- opencog/eva/src/psi-behavior.scm
+++ opencog/eva/src/psi-behavior.scm
@@ -131,7 +131,7 @@
 ;	(DefinedPredicate "Normal:amused")
 ;	speech-demand-satisfied (stv 1 1) speech-demand)
 
-
-(psi-rule (list (DefinedPredicate "ROS is running?"))
-	(DefinedPredicate "update-web-ui")
-		update-demand-satisfied (stv 1 1) update-demand)
+;
+;(psi-rule (list (DefinedPredicate "ROS is running?"))
+;	(DefinedPredicate "update-web-ui")
+;		update-demand-satisfied (stv 1 1) update-demand)
