4ca37a1ed673385509a00951a3a76e08cc71038d
Amen Belayneh <amenbelayneh@gmail.com>
2016-11-10 12:31:43 +0800

    Remove a Skip-Interaction predicate from psi-rules
    B/c of the shift to the wholeshow

--- opencog/eva/src/psi-behavior.scm
+++ opencog/eva/src/psi-behavior.scm
@@ -37,24 +37,20 @@
 			(DefinedPredicate "Someone visible?"))))
 
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "Did Someone New Speak?")))
+(psi-rule (list (DefinedPredicate "Did Someone New Speak?"))
 	(DefinedPredicate "Request interaction with person who spoke")
 	face-demand-satisfied (stv 1 1) face-demand)
 
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "Someone requests interaction?")))
+(psi-rule (list (DefinedPredicate "Someone requests interaction?"))
 	(DefinedPredicate "Interaction requested action")
 	face-demand-satisfied (stv 1 1) face-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "Did someone arrive?")))
+(psi-rule (list (DefinedPredicate "Did someone arrive?"))
 	(DefinedPredicate "New arrival sequence")
 	face-demand-satisfied (stv 1 1) face-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "Did someone leave?")))
+(psi-rule (list (DefinedPredicate "Did someone leave?"))
 	(DefinedPredicate "Someone left action")
 	face-demand-satisfied (stv 1 1) face-demand)
 
@@ -67,12 +63,11 @@
 
 ; TODO: How should rules that could run concurrently be represented, when
 ; we have action compostion(aka Planning/Orchestration)?
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "Someone visible?")))
+(psi-rule (list (DefinedPredicate "Someone visible?"))
 	(DefinedPredicate "Interact with face")
 	track-demand-satisfied (stv .5 .5) track-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
+(psi-rule (list (SequentialAnd
 		; TODO: test the behabior when talking.
 		; (Not (DefinedPredicate "chatbot is talking?"))
 		(DefinedPredicate "Someone visible?")
@@ -80,54 +75,46 @@
 	(DefinedPredicate "Change interaction target by priority")
 	face-demand-satisfied (stv 1 1) face-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "Nothing happening?")))
+(psi-rule (list (DefinedPredicate "Nothing happening?"))
 	(DefinedPredicate "Nothing is happening")
 	face-demand-satisfied (stv 1 1) face-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
+(psi-rule (list (SequentialAnd
 		(Not (DefinedPredicate "chatbot started talking?"))
 		(Not (DefinedPredicate "Is interacting with someone?"))
 		(DefinedPredicateNode "Did someone recognizable arrive?")))
 	(DefinedPredicate "Interacting Sequence for recognized person")
 	face-demand-satisfied (stv 1 1) face-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "chatbot started talking?")))
+(psi-rule (list (DefinedPredicate "chatbot started talking?"))
 	(DefinedPredicate "Speech started")
 	speech-demand-satisfied (stv 1 1) speech-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "chatbot is talking?")))
+(psi-rule (list (DefinedPredicate "chatbot is talking?"))
 	(DefinedPredicate "Speech ongoing")
 	speech-demand-satisfied (stv 1 1) speech-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "chatbot stopped talking?")))
+(psi-rule (list (DefinedPredicate "chatbot stopped talking?"))
 	(DefinedPredicate "Speech ended")
 	speech-demand-satisfied (stv 1 1) speech-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "chatbot started listening?")))
+(psi-rule (list (DefinedPredicate "chatbot started listening?"))
 	(DefinedPredicate "Listening started")
 	speech-demand-satisfied (stv 1 1) speech-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "chatbot is listening?")))
+(psi-rule (list (DefinedPredicate "chatbot is listening?"))
 	(DefinedPredicate "Listening ongoing")
 	speech-demand-satisfied (stv 1 1) speech-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "chatbot stopped listening?")))
+(psi-rule (list (DefinedPredicate "chatbot stopped listening?"))
 	(DefinedPredicate "Listening ended")
 	speech-demand-satisfied (stv 1 1) speech-demand)
 
-(psi-rule (list (DefinedPredicate "Skip Interaction?"))
+(psi-rule (list (DefinedPredicate "ROS is running?"))
 	(DefinedPredicate "Keep alive")
 	speech-demand-satisfied (stv 1 1) speech-demand)
 
-(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-		(DefinedPredicate "Heard Loud Voice?")))
+(psi-rule (list (DefinedPredicate "Heard Loud Voice?"))
 	(DefinedPredicate "Say whoa!")
 	speech-demand-satisfied (stv 1 1) speech-demand)
 
@@ -136,18 +123,15 @@
 ; to run as fast as possible -- something like 30 times a second.
 ; Something is wrong here, see bug
 ; opencog/ros-behavior-scripting/issues/110
-;(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-;		(DefinedPredicate "very low sound?")))
+;(psi-rule (list (DefinedPredicate "very low sound?"))
 ;	(DefinedPredicate "Quiet:happy")
 ;	speech-demand-satisfied (stv 1 1) speech-demand)
 ;
-;(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-;		(DefinedPredicate  "normal conversation?")))
+;(psi-rule (list (DefinedPredicate  "normal conversation?"))
 ;	(DefinedPredicate "Normal:amused")
 ;	speech-demand-satisfied (stv 1 1) speech-demand)
 
 
-;(psi-rule (list (SequentialAnd (NotLink (DefinedPredicate "Skip Interaction?"))
-;	(DefinedPredicate "ROS is running?")))
+;(psi-rule (list (DefinedPredicate "ROS is running?"))
 ;	(DefinedPredicate "update-web-ui")
 ;		update-demand-satisfied (stv 1 1) update-demand)
