66183f0cfeb28d919a703240fd42c77f83d3b577
Amen Belayneh <amenbelayneh@gmail.com>
2016-11-10 19:53:01 +0800

    Document a psi-rule

--- opencog/eva/src/psi-behavior.scm
+++ opencog/eva/src/psi-behavior.scm
@@ -132,6 +132,8 @@
 ;	speech-demand-satisfied (stv 1 1) speech-demand)
 
 
+; Any changes to the weight for controlled-psi-rules are pushed to ros
+; dynamic-parameters. Thus the web-ui mirrors the opencog wholeshow state.
 (psi-rule (list (DefinedPredicate "ROS is running?"))
 	(DefinedPredicate "update-web-ui")
 		update-demand-satisfied (stv 1 1) update-demand)
