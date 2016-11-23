4d9cbb3707f274c503db045222e2b4e272247080
Roman Treutlein <roman.treutlein@gmail.com>
2016-11-23 09:42:51 +0100

    Forgot something in the last Fix

--- opencog/nlp/lojban/CWrapper/build.sh
+++ opencog/nlp/lojban/CWrapper/build.sh
@@ -2,22 +2,17 @@ SOURCE_DIR=$1
 
 sudo ldconfig /usr/local/lib/opencog/
 
-libname=$(stack query | awk 'NR==5' | sed 's/://g'| sed 's/ //g')
-libver=$(stack query | awk 'NR==7' | sed 's/version: //g' | sed "s/'//g" | sed "s/ //g")
+libname=$(stack query --allow-different-user | awk 'NR==5' | sed 's/://g'| sed 's/ //g')
+libver=$(stack query --allow-different-useri | awk 'NR==7' | sed 's/version: //g' | sed "s/'//g" | sed "s/ //g")
 
-if [ "$(id -u)" -ne 0 ]
-then
-    #Cleanup of last build if it exists
-    rm -f "$SOURCE_DIR/lib$libname-$libver.so"
+#Cleanup of last build if it exists
+rm -f "$SOURCE_DIR/lib$libname-$libver.so"
 
-    # Build haskell bindings package.
-    stack build --allow-different-user
+# Build haskell bindings package.
+stack build --allow-different-user
 
-    LIB=$(find . -name "*$libname*.so" | awk 'NR==1')
+LIB=$(find . -name "*$libname*.so" | awk 'NR==1')
 
-    patchelf --set-soname "lib$libname-$libver.so" $LIB
+patchelf --set-soname "lib$libname-$libver.so" $LIB
 
-    cp $LIB "$SOURCE_DIR/lib$libname-$libver.so"
-else
-    echo "Can't run Haskell-Tests as root"
-fi
+cp $LIB "$SOURCE_DIR/lib$libname-$libver.so"
