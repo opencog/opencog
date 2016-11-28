0f681d19462485e6f6a6d30f0928259a53dce0d2
Roman Treutlein <roman.treutlein@gmail.com>
2016-11-28 10:38:44 +0100

    Another try

--- opencog/nlp/lojban/CWrapper/build.sh
+++ opencog/nlp/lojban/CWrapper/build.sh
@@ -8,9 +8,8 @@ libver=$(stack query --allow-different-user | awk 'NR==7' | sed 's/version: //g'
 #Cleanup of last build if it exists
 rm -f "$SOURCE_DIR/lib$libname-$libver.so"
 
-#Force Rebuild 
-LIB=$(find . -name "*$libname*.so" | awk 'NR==1')
-rm -f $LIB
+#Force Rebuild
+rm -fr ".stack-work/"
 
 # Build haskell bindings package.
 stack build --allow-different-user
