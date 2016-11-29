817b8327d895cf79359ef69986d829868bd4664b
Amen Belayneh <amenbelayneh@gmail.com>
2016-11-29 15:20:08 +0800

    Fix ownership change after running make install on haskell code

--- opencog/nlp/lojban/CWrapper/build.sh
+++ opencog/nlp/lojban/CWrapper/build.sh
@@ -5,12 +5,6 @@ export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/lib/opencog"
 libname=$(stack query --allow-different-user | awk 'NR==5' | sed 's/://g'| sed 's/ //g')
 libver=$(stack query --allow-different-user | awk 'NR==7' | sed 's/version: //g' | sed "s/'//g" | sed "s/ //g")
 
-#Cleanup of last build if it exists
-rm -f "$SOURCE_DIR/lib$libname-$libver.so"
-
-#Force Rebuild
-rm -fr ".stack-work/"
-
 # Build haskell bindings package.
 stack build --allow-different-user
 
