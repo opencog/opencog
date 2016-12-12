5b7d141b320cf8efa4cc1140bdb143efca5e388d
Eddie Monroe <eddiemonroe@gmail.com>
2016-12-12 12:32:53 -0700

    use tabs for indents (increase-positive-valence linas)

--- opencog/openpsi/sec.scm
+++ opencog/openpsi/sec.scm
@@ -20,47 +20,47 @@
 
 	name - name of the SEC
 "
-    (define sec
-        (Concept (string-append psi-prefix-str name)))
-    (Inheritance
-        sec
-        psi-sec-node)
-    (psi-set-value! sec initial-value)
+	(define sec
+		(Concept (string-append psi-prefix-str name)))
+	(Inheritance
+		sec
+		psi-sec-node)
+	(psi-set-value! sec initial-value)
 
-    ; For now, assuming the sec alone represents the overall system/agent state,
-    ; for consi1stency with psi var representations. Code below can be used if we
-    ; decide we want this represented explicitly as agent-state.
-    ; Create the agent/system-state variable for this sec
+	; For now, assuming the sec alone represents the overall system/agent state,
+	; for consi1stency with psi var representations. Code below can be used if we
+	; decide we want this represented explicitly as agent-state.
+	; Create the agent/system-state variable for this sec
 	;(psi-create-stimulus-sec agent-state sec agent-state-sec-init-value)
 	; Dynamic variable name creation needs working out.
-    ;(define agent-state-var-name (string-append "agent-state-" name))
-    ;(eval `(define ,(string->symbol agent-state-var-name)
-   	;    ,(psi-create-stimulus-sec agent-state sec agent-state-sec-init-value)))
-   	;(export (string->symbol agent-state-var-name))
+	;(define agent-state-var-name (string-append "agent-state-" name))
+	;(eval `(define ,(string->symbol agent-state-var-name)
+	;    ,(psi-create-stimulus-sec agent-state sec agent-state-sec-init-value)))
+	;(export (string->symbol agent-state-var-name))
 
-    sec)
+	sec)
 
 (define-public (psi-create-stimulus-sec stimulus sec initial-value)
 "
   Create a stimulus-SEC association and assign its initial value.
   A stimulus-SEC takes the form (List stimulus SEC) and its current value is
   stored in a StateLink:
-      (StateLinke (List stimulus SEC) (Number x))
+	  (StateLinke (List stimulus SEC) (Number x))
   The value is assumed to be in [0,1].
 "
 	(define stimulus-sec
 		(List
-	        stimulus
-	        sec))
-    (psi-set-value! stimulus-sec initial-value)
-    stimulus-sec)
+			stimulus
+			sec))
+	(psi-set-value! stimulus-sec initial-value)
+	stimulus-sec)
 
 (define-public (psi-get-secs)
-    (cog-outgoing-set
-        (cog-execute! (Get (Inheritance (Variable "$sec") psi-sec-node)))))
+	(cog-outgoing-set
+		(cog-execute! (Get (Inheritance (Variable "$sec") psi-sec-node)))))
 
 (define-public (psi-is-sec? atom)
-    (member atom (psi-get-secs)))
+	(member atom (psi-get-secs)))
 
 ; Todo: add variable names (?) and add getters for agent-state secs
 
