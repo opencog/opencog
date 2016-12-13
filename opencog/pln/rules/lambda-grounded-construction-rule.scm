d0c3adc7b099eff7c0cc5e464af9031cf3a0bcd3
Nil Geisweiller <ngeiswei@gmail.com>
2016-12-13 15:35:46 +0200

    Fix Lambda Grounded Construction rule

--- opencog/pln/rules/lambda-grounded-construction-rule.scm
+++ opencog/pln/rules/lambda-grounded-construction-rule.scm
@@ -5,16 +5,16 @@
 ;;    V
 ;;    Body
 ;;
-;; Wrap a Lambda around a fully grounded body and assign to the lambda
-;; the TV of the body. So basically it create a constant predicate or
-;; schema.
+;; Wrap a Lambda around a closed atom (that is an atom without free
+;; variables in it) and assign to the lambda the TV of the body. So
+;; basically it create a constant predicate or schema.
 ;;
 ;; Note that in practice we actually match only premises already
 ;; wrapped in a LambdaLink as a workaround the current lack of
 ;; alpha-equivalence support.
 ;; ----------------------------------------------------------------------
 
-(define lambda-grounded-construction-variables
+(define lambda-grounded-construction-vardecl
   (VariableList
      (TypedVariableLink
         (VariableNode "$V")
@@ -24,53 +24,35 @@
            (TypeNode "VariableNode")))
      (VariableNode "$B")))
 
-(define lambda-grounded-construction-body
+(define lambda-grounded-construction-pattern
   (AndLink
-     (LambdaLink
-        (VariableNode "$V")
-        (VariableNode "$B"))
+     (QuoteLink (LambdaLink
+        (UnquoteLink (VariableNode "$V"))
+        (UnquoteLink (VariableNode "$B"))))
      (EvaluationLink
-        (GroundedPredicateNode "scm: fully-grounded")
-        (ListLink
-           (VariableNode "$B")))))
+        (GroundedPredicateNode "scm: closed?")
+        (VariableNode "$B"))))
 
 (define lambda-grounded-construction-rewrite
   (ExecutionOutputLink
      (GroundedSchemaNode "scm: lambda-grounded-construction-formula")
      (ListLink
-        (LambdaLink
-           (VariableNode "$V")
-           (VariableNode "$B")))))
+        (VariableNode "$B")
+        (QuoteLink (LambdaLink
+           (UnquoteLink (VariableNode "$V"))
+           (UnquoteLink (VariableNode "$B")))))))
 
 (define lambda-grounded-construction-rule
   (BindLink
-     lambda-grounded-construction-variables
-     lambda-grounded-construction-body
+     lambda-grounded-construction-vardecl
+     lambda-grounded-construction-pattern
      lambda-grounded-construction-rewrite))
 
-(define (lambda-grounded-construction-formula lamb)
-  (let* ((lamb-outgoings (cog-outgoing-set lamb))
-         (body (cadr lamb-outgoings))
-          (body-tv (cog-tv body)))
-    (cog-set-tv! lamb body-tv)))
+(define (lambda-grounded-construction-formula body lamb)
+  (cog-set-tv! lamb (cog-tv body)))
 
-;; The following are for checking that an atom (i.e. a sub-hypergraph)
-;; doesn't have free variables.
-;;
-;; TODO this should be replaced by a scheme binding from a C++
-;; function, given how frequent its use should be.
-(define (is-variable atom)
-  (equal? (cog-type atom) 'VariableNode))
-
-(define (rec-fully-grounded atom)
-  (if (cog-node? atom)
-      (not (is-variable atom))
-      (every rec-fully-grounded (cog-outgoing-set atom))))
-
-(define (fully-grounded atom)
-  (if (rec-fully-grounded atom)
-      (stv 1 1)
-      (stv 0 1)))
+(define (closed? atom)
+  (bool->tv (cog-closed? atom)))
 
 ;; Name the rule
 (define lambda-grounded-construction-rule-name
