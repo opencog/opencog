26fc091c0a712ecafb51764659b35e395567acd4
mandeep <mandeep.singh.bhatia@gmail.com>
2016-12-15 15:38:16 +0800

    saliency fix

--- opencog/nlp/chatbot-psi/wholeshow.scm
+++ opencog/nlp/chatbot-psi/wholeshow.scm
@@ -39,14 +39,18 @@
 "
   This is the default mode. All the rules are given a weight of 0.9.
 "
+    (define controlled-rules (psi-get-controlled-rules))
     ; Make the weight changes needed for configuration.
+    ; All rules are disabled before enabling them so as to reset all
+    ; weight to 0.9.
     (disable-all-demos)
     (receive (filtered other)
-        (psi-partition-rule-with-alias "" (psi-get-controlled-rules))
+        (psi-partition-rule-with-alias "" controlled-rules)
         (map
             (lambda (psi-rule) (psi-rule-set-atomese-weight psi-rule 0.9))
             other)
     )
+    (psi-rule-disable "saliency-tracking" controlled-rules)
 )
 
 ; --------------------------------------------------------------
@@ -105,14 +109,15 @@
     (psi-rule-enable "random_sentence_pkd" (psi-get-controlled-rules))
     (psi-rule-enable "random_sentence_blogs" (psi-get-controlled-rules))
 )
+
 ; --------------------------------------------------------------
 (define-public (enable-saliency-demo)
 "
-  Enables the random_sentence_pkd and random_sentence_blogs rules.
+  Enables the visual saliency rule only.
 "
     ; Make the weight changes needed for configuration.
     (disable-all-demos)
-    (psi-rule-enable "saliency" (psi-get-controlled-rules))
+    (psi-rule-enable "saliency-tracking" (psi-get-controlled-rules))
 )
 
 ; --------------------------------------------------------------
