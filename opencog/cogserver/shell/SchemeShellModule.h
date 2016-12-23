77b9ad51475aa5e36b1037e80d93bb65d13c6187
Linas Vepstas <linasvepstas@gmail.com>
2016-12-22 22:11:14 -0600

    simplify, optimize the shell handler

--- opencog/cogserver/shell/SchemeShellModule.h
+++ opencog/cogserver/shell/SchemeShellModule.h
@@ -38,19 +38,17 @@ namespace opencog {
 class SchemeShellModule : public Module
 {
 	private:
-		DECLARE_CMD_REQUEST(SchemeShellModule, "scm", shellout,
-			"Enter the scheme shell",
-			"Usage: scm [hush|quiet|sync]\n\n"
-			"Enter the scheme interpreter shell. This shell provides a rich\n"
-			"and easy-to-use environment for creating, deleting and manipulating\n"
-			"OpenCog atoms and truth values. It provides a full R5RS-compliant\n"
-			"interactive scheme shell, based on the GNU Guile extension language.\n\n"
-			"If 'hush' or 'quiet' is specified after the command, then the prompt\n"
-			"will not be returned.  This is nice when catting large scripts using\n"
-			"netcat, as it avoids printing garbage when the scripts work well.\n"
-			"If 'sync' is specified after the command, then the output is sync,\n"
-			"instead of async.\n",
-			true, false)
+		class shelloutRequest : public Request
+		{
+			public:
+				static const RequestClassInfo& info(void);
+				shelloutRequest(CogServer& cs) : Request(cs) {};
+				virtual ~shelloutRequest() {};
+				virtual bool execute(void);
+				virtual bool isShell(void) { return true; }
+		};
+
+		Factory<shelloutRequest, Request> shelloutFactory;
 
 	public:
 		SchemeShellModule(CogServer&);
