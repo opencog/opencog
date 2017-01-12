9e1f8b56a2aff0c22a0a9db652f265190df1bab8
Misgana Bayetta <misgana.bayetta@gmail.com>
2017-01-12 14:13:09 +0800

    Expand/Refactor AttentionParaQuery class

---
+++ opencog/attention/AttentionParamQuery.cc
@@ -0,0 +1,145 @@
+
+#include "AttentionParamQuery.h"
+
+#include <opencog/util/Config.h>
+
+using namespace opencog;
+
+// Attentional Focus Params
+const std::string AttentionParamQuery::af_size = "ECAN_AFB_SIZE"; 
+const std::string AttentionParamQuery::af_decay = "ECAN_AFB_DECAY";
+const std::string AttentionParamQuery::af_bottom = "ECAN_AFB_BOTTOM";
+const std::string AttentionParamQuery::af_min_size = "MIN_AF_SIZE";
+const std::string AttentionParamQuery::af_max_size = "MAX_AF_SIZE";
+const std::string AttentionParamQuery::af_rent_update_freq = "ECAN_AF_RENT_FREQUENCY";
+
+// Forgetting Params
+const std::string AttentionParamQuery::forg_forgetting_threshold = "ECAN_FORGET_THRESHOLD";
+
+// Hebbian Link Params
+const std::string AttentionParamQuery::heb_maxlink = "ECAN_MAXLINKS";
+const std::string AttentionParamQuery::heb_max_alloc_percentage = "HEBBIAN_MAX_ALLOCATION_PERCENTAGE";
+const std::string AttentionParamQuery::heb_local_farlink_ratio = "ECAN_LOCAL_FAR_LINK_RATIO";
+
+// Diffusion/Spreading Params
+const std::string AttentionParamQuery::dif_spread_percentage = "ECAN_MAX_SPREAD_PERCENTAGE";
+const std::string AttentionParamQuery::dif_spread_hebonly = "ECAN_SPREAD_HEBBIAN_ONLY";
+const std::string AttentionParamQuery::dif_tournament_size = "ECAN_DIFFUSION_TOURNAMENT_SIZE";
+
+// Rent Params
+const std::string AttentionParamQuery::rent_starting_sti_rent = "ECAN_STARTING_ATOM_STI_RENT";
+const std::string AttentionParamQuery::rent_starting_lti_rent = "ECAN_STARTING_ATOM_LTI_RENT";
+const std::string AttentionParamQuery::rent_target_sti_funds = "TARGET_STI_FUNDS";
+const std::string AttentionParamQuery::rent_sti_funds_buffer = "STI_FUNDS_BUFFER";
+const std::string AttentionParamQuery::rent_target_lti_funds = "TARGET_LTI_FUNDS";
+const std::string AttentionParamQuery::rent_lti_funds_buffer = "LTI_FUNDS_BUFFER";
+const std::string AttentionParamQuery::rent_tournament_size = "ECAN_RENT_TOURNAMENT_SIZE";
+
+
+/**
+ * The representation of parameters in the atomspace
+ * will be as follows:
+ *
+ * MemberLink
+ *   Concept "$PARAMETER_i"
+ *   Concept "ECAN_PARAMS"
+ *
+ * then each paramter's value will be stored in a
+ * state link.
+ *
+ * StateLink
+ *   Concept "$PARAMETER_i"
+ *   Number   "x"  ; when boolean, x would be 0 or 1
+ */
+AttentionParamQuery::AttentionParamQuery(AtomSpace* as): _as(as)
+{
+    parent_param = _as->add_node(CONCEPT_NODE, "ECAN_PARAMS");
+
+    Handle var = _as->add_node(VARIABLE_NODE, "__ECAN_PARAM__");
+    Handle member = _as->add_link(MEMBER_LINK, 
+            HandleSeq {var, parent_param});
+    hget_params = _as->add_link(BIND_LINK, HandleSeq{member, var});
+} 
+
+std::string AttentionParamQuery::get_param_value(const std::string& param)
+{
+    Handle hparam = _as->add_node(CONCEPT_NODE, param);
+    std::string value = "";
+    HandleSeq hseq;
+    hparam->getIncomingSet(back_inserter(hseq));
+    for(Handle h : hseq){
+        if(h->getType() == STATE_LINK ){
+            Handle hvalue = h->getOutgoingSet()[1];
+            std::string str = hvalue->getName();
+            str.erase (str.find_last_not_of('0') + 1,
+                    std::string::npos);
+
+            if(str.back() == '.') 
+                str.pop_back();
+
+            value = str;
+        }
+    }
+
+    return value;
+}
+
+HandleSeq AttentionParamQuery::get_params(void)
+{
+    Handle rh = satisfying_set(_as, hget_params);
+    if (NULL != rh) rh = _as->add_atom(rh);
+
+    return rh->getOutgoingSet();
+}
+
+void AttentionParamQuery::load_default_values(void)
+{
+    set_param(af_rent_update_freq,
+            config().get_double(af_rent_update_freq, 2)); 
+    set_param(af_size,
+            config().get_double(af_size, 0.2)); 
+    set_param(af_decay,
+            config().get_double(af_decay, 0.05)); 
+    set_param(af_bottom,
+            config().get_int(af_bottom, 50)); 
+    set_param(af_min_size,
+            config().get_int(af_min_size, 100)); 
+    set_param(af_max_size,
+            config().get_int(af_max_size, 500)); 
+
+    set_param(forg_forgetting_threshold,
+            config().get_int(forg_forgetting_threshold,
+                AttentionValue::MAXLTI));
+
+    set_param(heb_maxlink,
+            config().get_double(heb_maxlink,300));
+    set_param(heb_local_farlink_ratio,
+            config().get_double(heb_local_farlink_ratio,10));
+    set_param(heb_max_alloc_percentage,
+            config().get_double(heb_max_alloc_percentage,0.5));
+
+     set_param(dif_spread_hebonly,
+            config().get_bool(dif_spread_hebonly, false));
+    set_param(dif_tournament_size,
+            config().get_int(dif_tournament_size, 5));
+    set_param(dif_spread_percentage,
+            config().get_double(dif_spread_percentage, 0.4));
+
+    set_param(rent_starting_sti_rent,
+            config().get_int(rent_starting_sti_rent, 1));
+    set_param(rent_starting_lti_rent,
+            config().get_int(rent_starting_lti_rent, 1));
+    set_param(rent_target_sti_funds,
+            config().get_int(rent_target_sti_funds, 10000));
+    set_param(rent_sti_funds_buffer,
+            config().get_int(rent_sti_funds_buffer, 10000));
+    set_param(rent_target_lti_funds,
+            config().get_int(rent_target_lti_funds, 10000));
+    set_param(rent_lti_funds_buffer,
+            config().get_int(rent_lti_funds_buffer, 10000));
+    set_param(rent_tournament_size,
+            config().get_int(rent_tournament_size, 5));
+}
+
+
+
