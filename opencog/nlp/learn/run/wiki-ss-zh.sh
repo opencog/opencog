66bc823b1d6e472106b8e390645f36b9d911ec7b
Linas Vepstas <linasvepstas@gmail.com>
2017-02-12 23:39:31 -0600

    Assorted README updates

---
+++ opencog/nlp/learn/run/wiki-ss-zh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Batch parsing script for Cantonese.
+# Loop over all the files in 'beta-pages', sentence-split them
+# and submit them for parsing.
+#
+time find beta-pages -type f -exec ./ss-one.sh zh {} localhost 17007 \;
+# time find beta-pages -type f -exec ./ssx.sh en \"{}\" localhost 17005 \;
+# time find beta-pages -type f -exec echo en \"{}\" localhost 17005 \;
