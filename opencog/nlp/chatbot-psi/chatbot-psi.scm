daf025710bd898d471c45575f4aef216e24d6fc3
leungmanhin <manhinleung@gmail.com>
2017-02-28 11:59:47 +0800

    Remove wholeshow from chatbot-psi

--- opencog/nlp/chatbot-psi/chatbot-psi.scm
+++ opencog/nlp/chatbot-psi/chatbot-psi.scm
@@ -7,7 +7,6 @@
 (load "chatbot-psi/psi-rules.scm")
 (load "chatbot-psi/states.scm")
 (load "chatbot-psi/utils.scm")
-(load "chatbot-psi/wholeshow.scm")
 
 ;-------------------------------------------------------------------------------
 ; Temporary debug support
