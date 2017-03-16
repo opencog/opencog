2391596af4857105678b31ab6f816c1c5202de86
Nil Geisweiller <ngeiswei@gmail.com>
2017-03-16 15:25:40 +0200

    Enable equivalence to implication rule + more iterations

--- examples/pln/moses-pln-synergy/scm/pln-bc-config.scm
+++ examples/pln/moses-pln-synergy/scm/pln-bc-config.scm
@@ -82,7 +82,7 @@
         (list deduction-implication-rule-name 1)
         (list conditional-full-instantiation-meta-rule-name 1)
         ;; (list implication-full-instantiation-rule-name 1)
-        ;; (list equivalence-to-implication-rule-name 1)
+        (list equivalence-to-implication-rule-name 1)
         ;; (list implication-implicant-disjunction-rule-name 1)
         )
   )
@@ -95,7 +95,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Termination criteria parameters
-(ure-set-num-parameter pln-rbs "URE:maximum-iterations" 10000)
+(ure-set-num-parameter pln-rbs "URE:maximum-iterations" 100000)
 
 ;; Attention allocation (0 to disable it, 1 to enable it)
 (ure-set-fuzzy-bool-parameter pln-rbs "URE:attention-allocation" 0)
