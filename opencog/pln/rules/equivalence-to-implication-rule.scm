22e9ad0a9cbe774904cc1340009ba6de93887fa1
Nil Geisweiller <ngeiswei@gmail.com>
2017-03-16 15:24:50 +0200

    Make equivalence to implication BC compatible

--- opencog/pln/rules/equivalence-to-implication-rule.scm
+++ opencog/pln/rules/equivalence-to-implication-rule.scm
@@ -26,53 +26,58 @@
 
 (define equivalence-to-implication-vardecl
   (VariableList
-     (Variable "$A")
-     (Variable "$B")))
+    (Variable "$A")
+    (Variable "$B")))
 
-(define equivalence-to-implication-body
+(define equivalence-to-implication-premise
   (Equivalence
-     (Variable "$A")
-     (Variable "$B")))
+    (Variable "$A")
+    (Variable "$B")))
 
-(define equivalence-to-implication-output
+(define equivalence-to-implication-precondition
+  (EvaluationLink
+    (GroundedPredicate "scm: gt-zero-confidence")
+    equivalence-to-implication-premise))
+
+(define equivalence-to-implication-pattern
+  (And
+    equivalence-to-implication-premise
+    equivalence-to-implication-precondition))
+
+(define equivalence-to-implication-conclusion
   (Implication
-     (Variable "$A")
-     (Variable "$B")))
+    (Variable "$A")
+    (Variable "$B")))
 
 (define equivalence-to-implication-rewrite
   (ExecutionOutput
-     (GroundedSchema "scm: equivalence-to-implication-formula")
-        (List
-           equivalence-to-implication-body
-           equivalence-to-implication-output)))
+    (GroundedSchema "scm: equivalence-to-implication-formula")
+      (List
+        equivalence-to-implication-conclusion
+        equivalence-to-implication-premise)))
 
 (define equivalence-to-implication-rule
   (Bind
-     equivalence-to-implication-vardecl
-     equivalence-to-implication-body
-     equivalence-to-implication-rewrite))
+    equivalence-to-implication-vardecl
+    equivalence-to-implication-pattern
+    equivalence-to-implication-rewrite))
 
-(define (equivalence-to-implication-formula EQ AB)
-  (cog-set-tv!
-     AB
-     (equivalence-to-implication-side-effect-free-formula EQ AB)))
-
-(define (equivalence-to-implication-side-effect-free-formula EQ AB)
-    (let* (
-            (A (gar AB))
-            (B (gdr AB))
-            (sA (cog-stv-strength A))
-            (sB (cog-stv-strength B))
-            (sEQ (cog-stv-strength EQ))
-            (cEQ (cog-stv-confidence EQ))
-            (sAB (if (< 0.99 (* sEQ cEQ)) ; Hack to work around the
-                                          ; lack of distributional
-                                          ; TV. If sEQ is high enough,
-                                          ; we just set sAB as sEQ
-                     sEQ
-                     ;; Formula based on PLN book formula for sim2inh
-                     (/ (* (+ 1 (/ sB sA)) sEQ) (+ 1 sEQ)))))
-            (stv sAB cEQ)))
+(define (equivalence-to-implication-formula AB EQ)
+  (let* ((A (gar AB))
+         (B (gdr AB))
+         (sA (cog-stv-strength A))
+         (sB (cog-stv-strength B))
+         (sEQ (cog-stv-strength EQ))
+         (cEQ (cog-stv-confidence EQ))
+         (sAB (if (< 0.99 (* sEQ cEQ)) ; Hack to work around the
+                                       ; lack of distributional
+                                       ; TV. If sEQ is high enough,
+                                       ; we just set sAB as sEQ
+                  sEQ
+                  ;; Formula based on PLN book formula for sim2inh
+                  (/ (* (+ 1 (/ sB sA)) sEQ) (+ 1 sEQ)))))
+    (if (< 0 cEQ)                       ;
+        (cog-merge-hi-conf-tv! AB (stv sAB cEQ)))))
 
 ;; Name the rule
 (define equivalence-to-implication-rule-name
