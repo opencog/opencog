42cc7ed4fd34eabcf8ad985b5718b6e954198f08
leungmanhin <manhinleung@gmail.com>
2017-03-23 11:27:54 +0800

    Check word sequence draft

--- opencog/nlp/chatlang/translator.scm
+++ opencog/nlp/chatlang/translator.scm
@@ -29,15 +29,40 @@
          (conds (append (cdr atomese-pattern) (cdr atomese-for-term))))
     (cons vars conds)))
 
-; XXX TODO
+; XXX TODO FIXME
+; This is a temporary hack for testing purpose
+; Will think more on this and update it accordingly
 (define (term-sequence-check terms)
   "Checks terms occur in the desired order. To be implemented."
-  '())
+  (define word-list
+    (map (lambda (w)
+      (cond ((equal? 'concept (car w)) (Glob (car (cdr w))))
+            (else (Word (car (cdr w))))))
+      terms))
+  (Evaluation (GroundedPredicate "scm: check-word-sequence")
+              (List (Variable "$S")
+                    (List word-list))))
+
+(define-public (check-word-sequence sent-node word-list)
+  (define result (cog-execute! (Map word-list (Set (get-word-list sent-node)))))
+  (if (not (null? (gar result)))
+    (stv 1 1)
+    (stv 0 1))
+)
 
 (define (say text)
-  (display text)
   (True (Put (DefinedPredicate "Say") (Node text)))
 )
+
+(define (get-word-list sent-node)
+  (List (append-map
+    (lambda (w)
+      ; Ignore LEFT-WALL and punctuations
+      (if (or (string-prefix? "LEFT-WALL" (cog-name w))
+              (word-inst-match-pos? w "punctuation"))
+        '()
+        (cog-chase-link 'ReferenceLink 'WordNode w)))
+    (car (sent-get-words-in-order sent-node)))))
 
 (define yakking (psi-demand "Yakking" 0.9))
 
@@ -52,7 +77,7 @@
 
     (psi-rule
       (list (Satisfaction (VariableList (car proc-terms))
-                          (And (append (cdr proc-terms) seq-check))))
+                          (And (append (cdr proc-terms) (list seq-check)))))
       (primitive-eval action)
       (True)
       (stv .9 .9)
