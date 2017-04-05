8004a39beb8228ed79482bc9f4868f5009aaba9f
leungmanhin <manhinleung@gmail.com>
2017-04-05 18:04:50 +0800

    Cover concept and POS

---
+++ tests/nlp/chatlang/test-concept.scm
@@ -0,0 +1,13 @@
+(use-modules (opencog)
+             (opencog nlp)
+             (opencog nlp chatlang))
+
+(define concept (concept "drink"))
+
+; Just want to see if the ReferenceLink connecting
+; the variable and the concept is there...
+(define test-result
+    (not (null? (filter (lambda (x)
+        (and (eq? (cog-type x) 'ReferenceLink)
+             (equal? (gdr x) (ConceptNode "drink"))))
+        (cdr concept)))))
