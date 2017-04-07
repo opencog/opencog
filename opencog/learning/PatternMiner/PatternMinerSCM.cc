bfb8334b52c94436b0cc781c71f8e7784db2d36f
ShujingKe <rainkekekeke@gmail.com>
2017-04-07 21:58:55 +0200

    Added a bunch of get and set of pattern miner settings in guile module.

--- opencog/learning/PatternMiner/PatternMinerSCM.cc
+++ opencog/learning/PatternMiner/PatternMinerSCM.cc
@@ -43,15 +43,123 @@ private:
     static void init_in_module(void*);
     void init(void);
 
-
-
     PatternMiner* patternMiner;
-
 
 public:
     PatternMinerSCM();
     void run_patternminer();
+
     string get_current_settings();
+
+    string get_Pattern_Max_Gram(){return "max_gram: " + patternMiner->get_Pattern_Max_Gram();}
+    string set_Pattern_Max_Gram(unsigned int _max_gram)
+    {
+        patternMiner->set_Pattern_Max_Gram(_max_gram);
+        return get_Pattern_Max_Gram();
+    }
+
+    string get_Enable_Interesting_Pattern(){return "enable_Interesting_Pattern: " + patternMiner->get_Enable_Interesting_Pattern();}
+    string set_Enable_Interesting_Pattern(bool _enable)
+    {
+        patternMiner->set_Enable_Interesting_Pattern(_enable) ;
+        return get_Enable_Interesting_Pattern();
+    }
+
+    string get_Frequency_threshold() {return "Frequency_threshold: " + patternMiner->get_Frequency_threshold();}
+    string set_Frequency_threshold(unsigned int _Frequency_threshold)
+    {
+        patternMiner->set_Frequency_threshold(_Frequency_threshold);
+        return get_Frequency_threshold();
+    }
+
+
+    string get_use_keyword_black_list(){return "use_keyword_black_list: " + patternMiner->get_use_keyword_black_list();}
+    string set_use_keyword_black_list(bool _use)
+    {
+        patternMiner->set_use_keyword_black_list(_use);
+        return get_use_keyword_black_list();
+    }
+
+    string get_use_keyword_white_list(){return "use_keyword_white_list: " + patternMiner->get_use_keyword_white_list();}
+    string set_use_keyword_white_list(bool _use)
+    {
+        patternMiner->set_use_keyword_white_list(_use);
+        return get_use_keyword_white_list();
+    }
+
+    string get_Ignore_Link_Types()
+    {
+        string result =  "Ignore_Link_Types:";
+        vector<Type> ignore_list = patternMiner->get_Ignore_Link_Types();
+        for (Type type : ignore_list)
+            result +=  " " + classserver().getTypeName(type);
+
+        return result;
+    }
+
+    string add_Ignore_Link_Type(string _typeStr)
+    {
+        Type atomType = classserver().getType(_typeStr);
+        if (atomType == NOTYPE)
+            return "Input type is wrong!";
+
+        string result = "";
+
+        if (patternMiner->add_Ignore_Link_Type(atomType))
+            result += "Added!\n";
+        else
+            result += "Input type already exists in the ingnore Link list!\n";
+
+        return result + get_Ignore_Link_Types();
+
+    }
+
+    string remove_Ignore_Link_Type(string _typeStr)
+    {
+        Type atomType = classserver().getType(_typeStr);
+        if (atomType == NOTYPE)
+            return "Input type is wrong!";
+
+        string result = "";
+        if (patternMiner->remove_Ignore_Link_Type(atomType))
+            result += "Removed!\n";
+        else
+            result += "Input type does not exist in the ingnore Link list!\n";
+
+        return result + get_Ignore_Link_Types();
+
+    }
+
+    string get_keyword_black_list()
+    {
+        string result =  "keyword_black_list:";
+        vector<string> black_list = patternMiner->get_keyword_black_list();
+        for (string word : black_list)
+            result +=  " " + word;
+
+        return result;
+    }
+
+    string add_keyword_to_black_list(string _keyword)
+    {
+        string result = "";
+        if (patternMiner->add_keyword_to_black_list(_keyword))
+            result += "Added!\n";
+        else
+            result +=  "Input keyword already exists in the black list!\n";
+        return result + get_keyword_black_list();
+    }
+
+    string remove_keyword_from_black_list(string _keyword)
+    {
+        string result = "";
+        if (patternMiner->remove_keyword_from_black_list(_keyword))
+            result += "Removed!\n";
+        else
+            result +=  "Input keyword does not exist in the black list!\n";
+        return result + get_keyword_black_list();
+    }
+
 };
 
 
