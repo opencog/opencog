09b216029c6948f9049db10cf6b2735305cfacaf
leungmanhin <manhinleung@gmail.com>
2017-04-26 14:32:45 +0800

    Cleanup

--- opencog/nlp/chatlang/ui.scm
+++ opencog/nlp/chatlang/ui.scm
@@ -2,11 +2,11 @@
              (opencog nlp)
              (opencog nlp chatlang)
              (opencog logger)
+             (srfi srfi-1)
              (ice-9 readline)
              (ice-9 regex))
 
 ; XXX TODO Remove me later
-(cog-logger-set-level! "debug")
 (cog-logger-set-level! "debug")
 (cog-logger-set-stdout! #t)
 
@@ -23,6 +23,13 @@
 ; words, lemmas, and concepts
 (define regex-choices "^[[a-zA-Z0-9 '~]+]")
 
+; The regex to extract a negation term e.g. !greet or !~food etc
+(define regex-negterm "^![~'a-zA-Z0-9]+\\b")
+
+; The regex to extract the negation a list of terms
+; e.g. ![hi hey ~others]
+(define regex-neglst "^![[<'~a-zA-Z0-9 ]+\\b]*>*")
+
 (define (symbol-contains? sym str)
   "Check if a symbol contains a specific string."
   (not (equal? #f (string-contains (symbol->string sym) str))))
@@ -31,7 +38,7 @@
   "Get the first term from the text."
   (define term-no-brackets (string-match "^[!'~a-zA-Z0-9]+\\b" txt))
   (if (equal? #f term-no-brackets)
-    (match:substring (string-match "^[![<'~a-zA-Z0-9 ]+\\b]*>*" txt))
+    (match:substring (string-match "^![[<'~a-zA-Z0-9 ]+\\b]*>*" txt))
     (match:substring term-no-brackets)))
 
 ; XXX TODO Remove me later
@@ -53,6 +60,7 @@
 ; Anywhere:
 ; Sentence Anchors: <
 ; Sentence Anchors: >
+; Variable: _
 
 ; TODO: Extract Part-of-Speech as well!
 (define (extract-1st-term txt)
@@ -95,14 +103,14 @@
         ; just return the whole term
         (else (get-first-term txt))))
 
-(define (extract-terms txt lst)
+(define* (extract-terms txt #:optional (lst '()))
   "Construct a list of terms by recursively extracting the
    terms one by one from the given text."
   (cog-logger-debug "Constructing term-list from: ~a" txt)
-  (let* ((t (extract-1st-term (string-trim-both txt)))
-         (newtxt (string-trim (string-drop txt (string-length t))))
-         (newlst (append lst (list t))))
-    (cog-logger-debug "Term extracted: ~a" t)
+  (let* ((term (extract-1st-term (string-trim-both txt)))
+         (newtxt (string-trim (string-drop txt (string-length term))))
+         (newlst (append lst (list term))))
+    (cog-logger-debug "Term extracted: ~a" term)
     (if (< 0 (string-length newtxt))
       (extract-terms newtxt newlst)
       newlst)))
@@ -134,40 +142,42 @@
   "Remove the first and last n chars from the string t."
   (substring t n (- (string-length t) n)))
 
-(define (gen-func func args wrap)
-  "Generate a scheme function call, in the form of a string,
-   that accepts the list of args as arguments.
-   The last argument is to indicate whether or not to
-   wrap each of the listed args by double-quotes."
-  (string-append
-    (fold (lambda (t s) (if wrap (string-append s " \"" t "\"")
-                                 (string-append s " " t)))
-          (string-append "(" func)
-          args)
-    ")"))
-
 (define (interpret-terms terms)
   "Interpret the terms one by one, and generate the actual function
    calls."
   (map
     (lambda (t)
-      (cond ((string-prefix? "'" t)
-             (if (equal? #f (string-match regex-proper-names t))
-               (gen-func "word" (list (substring t 1)) #t)
-               (gen-func "proper-names" (string-tokenize (subterm t 1)) #t)))
+      (cond ((and (string-prefix? "'" t)
+                  (not (equal? #f (string-match regex-proper-names t))))
+             (cons "proper-names" (string-tokenize (subterm t 1))))
+            ((string-prefix? "'" t)
+             (cons "word" (list (substring t 1))))
             ((string-prefix? "<<" t)
-             (gen-func "unordered-matching"
-               ;TODO TBC
-               (interpret-terms (extract-terms (subterm t 2) '())) #f))
+             (cons "unordered-matching"
+               (interpret-terms (extract-terms (subterm t 2)))))
+            ((string-prefix? "[" t)
+             (cons "or-choices"
+               (interpret-terms (extract-terms (subterm t 1)))))
+            ; TODO: negation-start, negation-end?
+;            ((and (string-prefix? "!" t)
+;                  (not (equal? #f (string-match regex-negterm t))))
+;             (cons "negation" (list (substring
+;               (match:substring (string-match regex-negterm t)) 1))))
+;            ((and (string-prefix? "!" t)
+;                  (not (equal? #f (string-match regex-neglst t))))
+;             (cons "negation" (interpret-terms (extract-terms
+;               (substring (match:substring (string-match regex-neglst t)) 1)))))
+            ((string-prefix? "~" t)
+             (cons "concept" (list (substring t 1))))
             ; Consider as lemma by default
-            (else (gen-func "lemma" (list t) #t))))
+            (else (cons "lemma" (list t)))))
     terms))
 
 (define (interpret-text txt)
   "Interpret the text in the pattern of the rule by firstly
    extracting the terms from the text and interpret them
    one by one later."
-  (let* ((terms (extract-terms (string-trim-both txt) '()))
+  (let* ((terms (extract-terms (string-trim-both txt)))
          (sorted-terms (rearrange-terms terms))
          (interp-terms (interpret-terms sorted-terms)))
     (cog-logger-debug "Total ~d terms were extracted: ~a" (length terms) terms)
@@ -206,233 +216,4 @@
          (display " (expression)")))
       (newline))
     action)
-)
-
-(define (s str) (quasiquote ,str))
-;(define (b str) (quasiquote ,str)) ; placeholder for behaviour
-
-(define (original-cr pattern action)
-    (define strBuilder "")
-    (define tmp "")
-    (define wordtype "")
-    (define x "")
-    (define flag_or-choice 0)
-    (define flag_proper-names 0)
-    (define flag_unordered-matches 0)
-    (define intCounter 0)
-    (define intCounter2 0)
-    (define pos_flag 1)
-    (define (my-interpret var)
-        ;;
-        ;; Treat each word separately
-        ;;
-        (cond
-            ; concept
-            ((string-match "^[~][[:alnum:]]" var) (set! wordtype "concept"))
-            ; word
-            ((string-match "^['][[:alnum:]]" var) (set! wordtype "word"))
-            ; main subject
-            ((string-match "[[:alnum:]]~mainsubject" var)
-             (begin
-                 (set! wordtype "main-subj")
-                 (if (string-match "~mainsubject" var)
-                     (set! var (regexp-substitute #f (string-match "~mainsubject" var) 'pre "" 'post)))))
-            ; main verb
-            ((string-match "[[:alnum:]]~mainverb" var)
-             (begin
-                 (set! wordtype "main-verb")
-                 (if (string-match "~mainverb" var)
-                     (set! var (regexp-substitute #f (string-match "~mainverb" var) 'pre "" 'post)))))
-            ; main object
-            ((string-match "[[:alnum:]]~mainobject" var)
-             (begin
-                 (set! wordtype "main-obj")
-                 (if (string-match "~mainobject" var)
-                     (set! var (regexp-substitute #f (string-match "~mainobject" var) 'pre "" 'post)))))
-            ; part of speech
-            ((string-match "^[[:alpha:]]+~[[:alpha:]]+$" var)
-             (begin
-                 (set! wordtype "pos")
-                 (set! var (regexp-substitute #f (string-match "[~]" var) 'pre "\" \"" 'post))))
-            ; wildcard
-            ((string-match "^([[:digit:]]+~[[:digit:]]+|[*])|[*]$" var)
-             (begin
-                 (set! wordtype "wildcard")
-                 (if (string-match "^[[:digit:]]+[*]$" var)
-                     (set! var (regexp-substitute #f (string-match "[*]" var) 'pre "" 'post)))))
-            ; start-with <
-            ((string-match "^[[:alnum:][:space:]]*[[:space:]]<[[:space:]][[:alnum:][:space:]]*$" var)
-             (begin
-                 (set! wordtype "start-with")
-                 ; Discard everything before the '<'
-                 (set! var (string-trim (regexp-substitute #f (string-match "^.*<" var) 'pre "" 'post)))
-                 (set! var (regexp-substitute #f (string-match "[[:space:]]*" var) 'pre "\" \"" 'post))))
-            ; XXX same as above?
-            ; start-with <
-            ((string-match ".*[[:space:]]*<[[:space:]]*.*" var)
-             (begin
-                 ;(set! var (string-trim (regexp-substitute #f (string-match "<" var) 'pre "" 'post))) ; discard everything before the '<'
-                 ;(set! var (regexp-substitute #f (string-match "[[:space:]]*" var) 'pre "" 'post))
-                 (set! wordtype "start-with")))
-            (else (set! wordtype "lemma")))
-
-        (string-append "(" wordtype " \"" var "\"" ") " )
-
-        ;(for-each (lambda (x) (set! result (string-append result x))))
-        ;result
-    )
-
-;;
-;; Process groups of words
-;;
-; XXX syntax error, to be removed?
-#!
-(define (my-interpret-group x name leftMatch rightMatch flag)
-    ((or (string-match leftMatch x) (equal? flag 1))
-        (if (string-match leftMatch tmp) (set! tmp (regexp-substitute #f (string-match leftMatch tmp) 'pre "" 'post)))
-            (if (string-match rightMatch tmp) (set! tmp (regexp-substitute #f (string-match rightMatch tmp) 'pre "" 'post)))
-                ;(display  intCounter2)
-                (if (equal? intCounter2 0)
-                    (begin
-                        (set! strBuilder (string-append strBuilder "(" name " "))
-                            (if (equal? tmp "") (display "") (set! strBuilder (string-append strBuilder "\""  tmp "\" ")))
-                            (set! intCounter2 1))
-                    (if (not (equal? tmp ""))  (set! strBuilder (string-append strBuilder  "\"" tmp "\" "))))
-            (if (string-match rightMatch x)
-                (begin
-                    (set! flag 0)
-                    (set! strBuilder (string-append strBuilder ") ")))
-                (set! flag 1)
-                ;;(set! intCounter 0)
-            )))
-!#
-
-    (set! pattern (string-trim pattern))
-    (if (string-match "^#" pattern)
-        (display "exit\n")
-        ; exit if there is a comment # at start of string
-        (begin
-            (set! strBuilder (string-append strBuilder "(chat-rule\n    '("))
-            ;; sentense level processing
-
-; XXX to be removed?
-#!
-(if (string-match "^.*[[:space:]]<[[:space:]].*$" pattern)
-    (begin ; start-with <
-        (set! tmp pattern)
-        (set! tmp (string-trim (regexp-substitute #f (string-match "^.*<" tmp) 'pre "" 'post))) ; discard everything before the '<'
-        (set! tmp (string-trim (regexp-substitute #f (string-match "[[:space:]]*" (string-trim tmp)) 'pre "" 'post)))
-        (set! strBuilder (string-append strBuilder "(start-with \"" tmp "\"" ") " ))
-        ;(display (string-append "\n\n" strBuilder "\n\n"))
-        ;(set! strBuilder (string-append strBuilder (my-interpret x))
-    ))
-!#
-
-            ; (set! strBuilder (string-append strBuilder (string-concatenate (map my-interpret (string-split pattern #\space)))))
-
-            ; (for-each (lambda (x) (set! strBuilder (string-append strBuilder (string-concatenate (map my-interpret x)))
-            (for-each
-                (lambda (x)
-                    (set! x (string-trim x))
-                    (set! tmp (string-trim (string-filter (lambda (c) (not (or (char=? #\[ c) (char=? #\] c)))) x)))
-                    (cond
-                        ;; or-choices
-                        ((or (string-match "\\[" x) (equal? flag_or-choice 1))
-                         (if (equal? intCounter 0)
-                            (begin
-                               (set! strBuilder (string-append strBuilder "(or-choices "))
-                               (if (not (equal? tmp ""))
-                                   (set! strBuilder (string-append strBuilder "\""  tmp "\" ")))
-                               (set! intCounter 1))
-                            (if (not (equal? tmp ""))
-                                (set! strBuilder (string-append strBuilder  "\"" tmp "\" "))))
-                         (if (string-match "\\]" x)
-                             (begin
-                                 (set! flag_or-choice 0)
-                                 (set! strBuilder (string-append strBuilder ") ")))
-                             (set! flag_or-choice 1)))
-
-                        ;; proper-names
-                        ((or (string-match "^''" x) (equal? flag_proper-names 1))
-                         ;(set! tmp (string-trim (string-filter (lambda (c) (not  (char=? #\' c))) x)))
-                         (if (string-match "''" tmp)
-                             (set! tmp (regexp-substitute #f (string-match "''" tmp) 'pre "" 'post)))
-                         (if (equal? intCounter 0)
-                            (begin
-                                (set! strBuilder (string-append strBuilder "(proper-names "))
-                                (if (equal? tmp "")
-                                    ; XXX
-                                    (display "")
-                                    (set! strBuilder (string-append strBuilder "\""  tmp "\" ")))
-                                (set! intCounter 1))
-                            (if (not (equal? tmp ""))
-                                (set! strBuilder (string-append strBuilder  "\"" tmp "\" "))))
-                         (if (string-match "['']$" x)
-                            (begin
-                                (set! flag_proper-names 0)
-                                (set! strBuilder (string-append strBuilder ") ")))
-                            (set! flag_proper-names 1)
-                            ;;(set! intCounter 0)
-                         ))
-
-                        ; (my-interpret-group x "proper-names" "''" "''" flag_proper-names) ;leftMatch rightMatch flag)
-
-                        ;; unordered matches
-                        ((or (string-match "<<" x) (equal? flag_unordered-matches 1))
-                         (if (string-match "<<" tmp)
-                             (set! tmp (regexp-substitute #f (string-match "<<" tmp) 'pre "" 'post)))
-                         (if (string-match ">>" tmp)
-                             (set! tmp (regexp-substitute #f (string-match ">>" tmp) 'pre "" 'post)))
-                         ;(display  intCounter2)
-                         (if (equal? intCounter2 0)
-                            (begin
-                                (set! strBuilder (string-append strBuilder "(unordered-matches "))
-                                (if (equal? tmp "")
-                                    ; XXX
-                                    (display "")
-                                    (set! strBuilder (string-append strBuilder "\""  tmp "\" ")))
-                                (set! intCounter2 1))
-                            (if (not (equal? tmp ""))
-                                (set! strBuilder (string-append strBuilder  "\"" tmp "\" "))))
-                         (if (string-match ">>" x)
-                             (begin
-                                 (set! flag_unordered-matches 0)
-                                 (set! strBuilder (string-append strBuilder ") ")))
-                             (set! flag_unordered-matches 1)
-                             ;;(set! intCounter 0)
-                         ))
-
-                        ;; Ignore everything after # because it's a comment
-                        ;( (string-match "#" x)
-                            ;; how to break out of a for-each?
-                        ;)
-
-                        ;; single word processing
-                        (else
-                            (if (equal? x "")
-                                ; XXX
-                                (display "")
-                                (set! strBuilder (string-append strBuilder (my-interpret x)))))
-                    )
-                )
-            (string-split pattern #\space))
-
-; XXX to be removed?
-; startwith alternate code
-;(if (string-match "^.*[[:space:]]<[[:space:]].*$" pattern) (begin ; start-with <
-;        (set! tmp pattern)
-;        (set! tmp (string-trim (regexp-substitute #f (string-match "^.*<" tmp) 'pre "" 'post))) ; discard everything before the '<'
-;        (set! tmp (string-trim (regexp-substitute #f (string-match "[[:space:]]*" (string-trim tmp)) 'pre "" 'post)))
-;        (set! strBuilder (string-append strBuilder "(start-with \"" tmp "\"" ") " ))
-;        ;(display (string-append "\n\n" strBuilder "\n\n"))
-;        ;(set! strBuilder (string-append strBuilder (my-interpret x))
-;))
-
-            (set! strBuilder (string-append strBuilder ")\n"))
-            (set! strBuilder (string-append strBuilder "    '(say \"" action "\"))" ))
-            (display strBuilder)
-            (newline)
-            ;(eval-string strBuilder)
-        )
-    )
 )
