35af8900b17b8a8bdc037a01f375f7781cd06ce3
Linas Vepstas <linasvepstas@gmail.com>
2017-05-02 21:36:59 -0500

    More restructuring of the main loops

--- opencog/nlp/learn/compute-mi.scm
+++ opencog/nlp/learn/compute-mi.scm
@@ -767,33 +767,21 @@
 ; RAM usage.
 ;
 
-(define (batch-all-pair-wildcard-counts lg_rel)
+(define (batch-all-pair-wildcard-counts ALL-WORDS
+	GET-PAIR SET-LEFT-TOTAL SET-RIGHT-TOTAL)
 	(begin
 		(start-trace "Start batched wildcard-counting\n")
 		(display "Start batched wildcard-counting\n")
-
-		; Make sure all words are in the atomspace
-		(fetch-all-words)
-		(trace-msg-num "In wildcard-count, num words="
-			(length (get-all-words)))
-		(format #t "In wildcard-count, num words=~A\n"
-			(length (get-all-words)))
-
-		; Make sure all word-pairs are in the atomspace.
-		(fetch-incoming-set lg_rel)
-		(display "Finished loading word-pairs\n")
 
 		; Compute the counts. par-for-each runs in parallel.
 		; (for-each
 		(par-for-each
 			(lambda (word)
 				(compute-pair-wildcard-counts word
-					get-any-pair-link
-					set-any-left-wildcard-count
-					set-any-right-wildcard-count)
+					GET-PAIR SET-LEFT-TOTAL SET-RIGHT-TOTAL)
 				(trace-msg-cnt "Wildcard-count did ")
 			)
-			(get-all-words)
+			ALL-WORDS
 		)
 		(trace-elapsed)
 		(trace-msg "Done with wild-card count\n")
@@ -1065,10 +1053,28 @@
 (define (batch-all-pair-mi
 	GET-LEFT-WILD GET-RIGHT-WILD SET-PAIR-TOTAL
 	lg_rel)
+
+	; Make sure all words are in the atomspace
+	(fetch-all-words)
 	(begin
+		(define all-the-words (get-all-words))
+
+		(trace-msg-num "In wildcard-count, num words="
+			(length all-the-words))
+		(format #t "In wildcard-count, num words=~A\n"
+			(length all-the-words))
+
+		; Make sure all word-pairs are in the atomspace.
+		(fetch-incoming-set lg_rel)
+		(display "Finished loading word-pairs\n")
+
 		; First, get the left and right wildcard counts.
 		(trace-msg "Going to batch-wildcard count\n")
-		(batch-all-pair-wildcard-counts lg_rel)
+		(batch-all-pair-wildcard-counts
+			all-the-words
+			get-any-pair-link
+			set-any-left-wildcard-count
+			set-any-right-wildcard-count)
 
 		; Now, get the grand-total
 		(trace-elapsed)
@@ -1093,7 +1099,7 @@
 				(compute-pair-mi right-word lg_rel)
 				(trace-msg-cnt "Done with pair MI cnt=")
 			)
-			(get-all-words)
+			all-the-words
 		)
 		(trace-msg "Finished with MI batch\n")
 		(display "Finished with MI batch\n")
