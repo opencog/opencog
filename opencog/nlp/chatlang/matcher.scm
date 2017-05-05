a37505bfab5be2040d8c53a34dc901e7e8a05e15
leungmanhin <manhinleung@gmail.com>
2017-05-05 14:18:34 +0800

    Ground the GlobNodes

--- opencog/nlp/chatlang/matcher.scm
+++ opencog/nlp/chatlang/matcher.scm
@@ -8,16 +8,50 @@
 
 (use-modules (opencog logger))
 
+(define globs '())
+(define (ground-globs pattern input)
+  "Use MapLink to ground the GlobNodes in the pattern and save them in 'globs'"
+  ; For example, if the pattern is:
+  ;   (ListLink (WordNode "A") (GlobNode "$x"))
+  ;
+  ; and the input is:
+  ;   (ListLink (WordNode "A") (WordNode "B"))
+  ;
+  ; Create an association list:
+  ;   (("$x" . (WordNode "B")))
+  ;
+  ; MapLink returns things like e.g.:
+  ; (SetLink
+  ;   (ListLink
+  ;     (ListLink ...)  ; GlobNode-1
+  ;     (ListLink ...)  ; GlobNode-2
+  ;     ...))
+  (define grds (cog-execute! (Map pattern (Set input))))
+  (for-each
+    (lambda (gn grd)
+      (set! globs (assoc-set! globs (cog-name gn) grd)))
+    (cog-filter 'GlobNode (cog-outgoing-set pattern))
+    ; TODO: There may be more than one possible groundings?
+    (cog-outgoing-set (gar grds))))
+
 (define-public (chat-find-rules sent-node)
   "The action selector. It first searches for the rules using DualLink,
    and then does the filtering by evaluating the context of the rules.
    Eventually returns a list of weighted rules that can satisfy the demand"
-  (let* ((lemma-list (get-sent-lemmas sent-node))
-         (no-constant (cog-chase-link 'InheritanceLink 'ListLink chatlang-no-constant))
-         (rules-matched (append (psi-get-dual-match lemma-list)
-                                (psi-get-exact-match lemma-list)
-                                (append-map psi-get-exact-match no-constant))))
-    (cog-logger-debug "For input:\n~aRules found:\n~a" lemma-list rules-matched)
+  (let* ((input-lemmas (get-sent-lemmas sent-node))
+         (no-constant (append-map psi-get-exact-match
+           (cog-chase-link 'InheritanceLink 'ListLink chatlang-no-constant)))
+         (dual-match (psi-get-dual-match input-lemmas))
+         (exact-match (psi-get-exact-match input-lemmas))
+         (rules-matched (append dual-match exact-match no-constant)))
+    (cog-logger-debug "For input:\n~aRules found:\n~a" input-lemmas rules-matched)
+
+    ; Clear any previous groundings before grounding the above GlobNodes
+    (set! globs '())
+    (if (not (null? dual-match)) (ground-globs dual-match input-lemmas))
+    (if (not (null? no-constant)) (ground-globs no-constant input-lemmas))
+    (cog-logger-debug "GlobNode groundings: ~a" globs)
+
     ; TODO: Pick the ones with the highest weight
     (List (append-map
       ; TODO: "psi-satisfiable?" doesn't work here (?)
