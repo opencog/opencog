d9aa4a7ab73ebb37717a3bce3890006c130ab40c
Linas Vepstas <linasvepstas@gmail.com>
2017-05-04 19:02:34 -0500

    Move the counter function over o the common side

--- opencog/nlp/learn/common.scm
+++ opencog/nlp/learn/common.scm
@@ -85,6 +85,37 @@
 )
 
 ; ---------------------------------------------------------------------
+
+(define (count-one-atom ATM)
+"
+  count-one-atom ATM -- increment the count by one on ATM, and
+  update the SQL database to hold that count.
+
+  This will also automatically fetch the previous count from
+  the SQL database, so that counting will work correctly, when
+  picking up from a previous point.
+
+  Warning: this is NOT SAFE for distributed processing! That is
+  because this does NOT grab the count from the database every time,
+  so if some other process updates the database, this will miss that
+  update.
+"
+	(define (incr-one atom)
+		; If the atom doesn't yet have a count TV attached to it,
+		; then its probably a freshly created atom. Go fetch it
+		; from SQL. Otherwise, assume that what we've got here,
+		; in the atomspace, is the current copy.  This works if
+		; there is only one process updating the counts.
+		(if (not (cog-ctv? (cog-tv atom)))
+			(fetch-atom atom)) ; get from SQL
+		(cog-inc-count! atom 1) ; increment
+	)
+	(begin
+		(incr-one ATM) ; increment the count on ATM
+		(store-atom ATM)) ; save to SQL
+)
+
+; ---------------------------------------------------------------------
 ; Compute log liklihood of having observed a given atom.
 ;
 ; The liklihood and its log-base-2 will be stored under the key
