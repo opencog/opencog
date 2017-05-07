42690daf6f42a59088d4452ceadf6f87fbd48c4b
Linas Vepstas <linasvepstas@gmail.com>
2017-05-07 12:02:23 -0500

    Start implementing similariy measures

--- opencog/nlp/learn/gram-sim.scm
+++ opencog/nlp/learn/gram-sim.scm
@@ -14,27 +14,41 @@
 ; sets have already been made, and are currently stored in the database.
 ;
 ; Note that similarity scores are symmetric, so exchanging left and
-; right give the same answer.
+; right give the same answer.  Thus, an UnorderedLink is best for
+; storing the pair. Specifically, it will be this:
+;
+;    SimilarityLink
+;        WordNode "this"
+;        WordNode "that"
+;
+; ---------------------------------------------------------------------
 
-(define cos-key (PredicateNode "*-Cosine Distance Key-*))
+(use-modules (srfi srfi-1))
+(use-modules (opencog) (opencog persist) (opencog persist-sql))
+
+(define cos-key (PredicateNode "*-Cosine Distance Key-*"))
 
 ; If the similarity is less than this, it is not saved.
 (define cutoff 0.05)
+
+; Define the atom at which the cosine similarity value will be stored.
+(define (sim-pair WORD-A WORD-B) (SimilarityLink WORD-A WORD-B))
 
 ; Compute and store the similarity between the WORD, and the other
 ; words in the word-list
 (define (batch-sim WORD WORD-LIST)
 
 	(define (store-sim WORD-A WORD-B SIM)
-		(define simpr
-			(EvaluationLink 
-(cog-set-value!
+		(store-atom
+			(cog-set-value!
+				(sim-pair WORD-A WORD-B) cos-key
+				(FloatValue SIM (get-angle SIM)))))
 
 	(for-each
 		(lambda (wrd)
-			(define sim (cset-vec-cosine (WORD wrd)))
-			(if (< cutoff sim)
-				(define dist (get-angle sim))
-			))
+			(define sim (cset-vec-cosine WORD wrd))
+			(if (< cutoff sim) (store-sim WORD wrd sim)))
 		WORD-LIST)
 )
+
+; ---------------------------------------------------------------------
