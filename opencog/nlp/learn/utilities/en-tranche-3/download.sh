b07257160c910ab331e0080a98d69624ed528fe9
Linas Vepstas <linasvepstas@gmail.com>
2017-05-29 15:25:25 -0500

    Updated processing scripts

--- opencog/nlp/learn/utilities/en-tranche-3/download.sh
+++ opencog/nlp/learn/utilities/en-tranche-3/download.sh
@@ -86,3 +86,260 @@
 ./process-gutenberg.sh http://www.gutenberg.org/files/54731/54731-0.txt
 ./process-gutenberg.sh http://www.gutenberg.org/files/54730/54730-0.txt
 
+
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54729/54729-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54728/54728-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54727/54727-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54726/54726-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54725/54725-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54724/54724-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54723/54723-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54722/54722-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54721/54721-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54720/54720-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54719/54719-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54718/54718-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54717/54717-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54716/54716-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54715/54715-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54714/54714-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54713/54713-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54712/54712-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54711/54711-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54710/54710-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54709/54709-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54708/54708-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54707/54707-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54706/54706-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54705/54705-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54704/54704-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54703/54703-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54702/54702-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54701/54701-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54700/54700-0.txt
+
+##############
+./process-gutenberg.sh http://www.gutenberg.org/files/54699/54699-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54698/54698-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54697/54697-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54696/54696-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54695/54695-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54694/54694-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54693/54693-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54692/54692-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54691/54691-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54690/54690-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54689/54689-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54688/54688-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54687/54687-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54686/54686-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54685/54685-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54684/54684-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54683/54683-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54682/54682-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54681/54681-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54680/54680-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54679/54679-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54678/54678-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54677/54677-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54676/54676-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54675/54675-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54674/54674-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54673/54673-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54672/54672-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54671/54671-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54670/54670-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54669/54669-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54668/54668-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54667/54667-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54666/54666-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54665/54665-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54664/54664-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54663/54663-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54662/54662-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54661/54661-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54660/54660-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54659/54659-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54658/54658-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54657/54657-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54656/54656-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54655/54655-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54654/54654-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54653/54653-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54652/54652-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54651/54651-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54650/54650-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54649/54649-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54648/54648-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54647/54647-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54646/54646-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54645/54645-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54644/54644-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54643/54643-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54642/54642-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54641/54641-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54640/54640-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54639/54639-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54638/54638-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54637/54637-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54636/54636-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54635/54635-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54634/54634-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54633/54633-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54632/54632-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54631/54631-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54630/54630-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54629/54629-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54628/54628-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54627/54627-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54626/54626-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54625/54625-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54624/54624-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54623/54623-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54622/54622-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54621/54621-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54620/54620-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54619/54619-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54618/54618-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54617/54617-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54616/54616-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54615/54615-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54614/54614-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54613/54613-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54612/54612-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54611/54611-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54610/54610-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54609/54609-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54608/54608-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54607/54607-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54606/54606-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54605/54605-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54604/54604-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54603/54603-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54602/54602-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54601/54601-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54600/54600-0.txt
+
+##############
+./process-gutenberg.sh http://www.gutenberg.org/files/54599/54599-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54598/54598-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54597/54597-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54596/54596-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54595/54595-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54594/54594-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54593/54593-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54592/54592-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54591/54591-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54590/54590-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54589/54589-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54588/54588-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54587/54587-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54586/54586-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54585/54585-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54584/54584-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54583/54583-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54582/54582-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54581/54581-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54580/54580-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54579/54579-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54578/54578-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54577/54577-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54576/54576-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54575/54575-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54574/54574-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54573/54573-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54572/54572-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54571/54571-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54570/54570-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54569/54569-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54568/54568-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54567/54567-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54566/54566-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54565/54565-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54564/54564-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54563/54563-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54562/54562-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54561/54561-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54560/54560-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54559/54559-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54558/54558-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54557/54557-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54556/54556-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54555/54555-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54554/54554-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54553/54553-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54552/54552-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54551/54551-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54550/54550-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54549/54549-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54548/54548-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54547/54547-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54546/54546-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54545/54545-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54544/54544-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54543/54543-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54542/54542-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54541/54541-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54540/54540-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54539/54539-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54538/54538-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54537/54537-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54536/54536-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54535/54535-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54534/54534-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54533/54533-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54532/54532-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54531/54531-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54530/54530-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54529/54529-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54528/54528-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54527/54527-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54526/54526-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54525/54525-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54524/54524-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54523/54523-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54522/54522-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54521/54521-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54520/54520-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54519/54519-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54518/54518-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54517/54517-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54516/54516-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54515/54515-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54514/54514-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54513/54513-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54512/54512-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54511/54511-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54510/54510-0.txt
+
+./process-gutenberg.sh http://www.gutenberg.org/files/54509/54509-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54508/54508-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54507/54507-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54506/54506-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54505/54505-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54504/54504-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54503/54503-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54502/54502-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54501/54501-0.txt
+./process-gutenberg.sh http://www.gutenberg.org/files/54500/54500-0.txt
+
--- opencog/nlp/learn/utilities/en-tranche-3/process-gutenberg.sh
+++ opencog/nlp/learn/utilities/en-tranche-3/process-gutenberg.sh
@@ -11,25 +11,46 @@ echo  this $filename has basename $base-
 cat $filename | sed -n '/START .* PROJECT GUTENBERG EBOOK/,$p' | \
 	sed -n '1,/END .* PROJECT GUTENBERG EBOOK/p' > foo
 
-grep -l 大 foo
-if [ $? -ne 1 ]; then
-   echo Found CJK characters, bailing
+grep -l "Language: German" $filename
+if [ $? -ne 0 ]; then
+   mv $filename ../novels-de
 	exit
 fi
 
-grep -l Anmerkungen foo
-if [ $? -ne 1 ]; then
-   echo Suspected German book, bailing
+grep -l "Language: French" $filename
+if [ $? -ne 0 ]; then
+   mv $filename ../novels-fr
 	exit
 fi
 
-grep -l purezza foo
-if [ $? -ne 1 ]; then
-   echo Suspected Italian book, bailing
+grep -l "Language: Italian" $filename
+if [ $? -ne 0 ]; then
+   mv $filename ../novels-it
+	exit
+fi
+
+grep -l "Language: Spanish" $filename
+if [ $? -ne 0 ]; then
+   mv $filename ../novels-es
+	exit
+fi
+
+grep -l "Language: Chinese" $filename
+if [ $? -ne 0 ]; then
+   mv $filename ../novels-cn
+	exit
+fi
+
+grep -l "Language: English" $filename
+if [ $? -ne 0 ]; then
+   echo Suspected non-ENglish, bailing
 	exit
 fi
 
 ./chapters.sh foo $base-
+
+# mv foo $filename
+
 mv $filename whole-books
 mv $base-* split-books
 rm foo
