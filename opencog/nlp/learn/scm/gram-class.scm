5ac22108b5eb63921cd021d5b0a9f8050ecd2d18
Linas Vepstas <linasvepstas@gmail.com>
2017-06-08 02:49:05 -0500

    Start work on grammatical classification.

---
+++ opencog/nlp/learn/scm/gram-class.scm
@@ -0,0 +1,20 @@
+;
+; gram-class.scm
+;
+; Classify words into grammatical categories, using the "thresholding
+; PCA" (Principal Component Analsysis) algo. 
+;
+; ---------------------------------------------------------------------
+; OVERVIEW
+; --------
+; The overall algo is described in the language diary.  The bulk of the
+; computation is performed in the (opencog analysis) toolkit.
+;
+; ---------------------------------------------------------------------
+
+(use-modules (opencog) (opencog analysis))
+
+
+(define (do-it)
+
+)
