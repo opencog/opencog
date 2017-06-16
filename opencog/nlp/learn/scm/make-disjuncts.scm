3211dc4a3c7972a9e6630297acc3e8ec90adf5a1
Linas Vepstas <linasvepstas@gmail.com>
2017-06-16 05:05:13 -0500

    Remove code that was moved to the atomspace repo

--- opencog/nlp/learn/scm/make-disjuncts.scm
+++ opencog/nlp/learn/scm/make-disjuncts.scm
@@ -23,6 +23,7 @@
 ; ---------------------------------------------------------------------
 
 (use-modules (srfi srfi-1))
+(use-modules (opencog sheaf))
 
 ; ---------------------------------------------------------------------
 ; Convert an integer into a string of letters. Useful for creating
@@ -42,113 +43,6 @@
 	(list->string (cons #\T (number->letters num)))
 )
 
-; ---------------------------------------------------------------------
-
-(define-public (make-pseudo-disjuncts MST-PARSE)
-"
-  make-pseudo-disjuncts - create 'decoded' disjuncts.
-
-  Given an MST parse of a sentence, return a list of 'decoded'
-  disjuncts for each word in the sentence.
-
-  It is the nature of MST parses that the links between the words
-  have no labels: the links are of the 'any' type. We'd like to
-  disover thier types, and we begin by creating pseudo-disjuncts.
-  These resemble ordinary disjuncts, except that the connectors
-  are replaced by the words that they connect to.
-
-  So, for example, given the MST parse
-     (mst-parse-text 'The game is played on a level playing field')
-  the word 'playing' might get this connector set:
-
-    (PseudoWordCset
-       (WordNode \"playing\")
-       (PseudoAnd
-          (PseudoConnector
-             (WordNode \"level\")
-             (LgConnDirNode \"-\"))
-          (PseudoConnector
-             (WordNode \"field\")
-             (LgConnDirNode \"+\"))))
-
-  Grammatically-speaking, this is not a good connector, but it does
-  show the general idea: that there was a link level<-->playing and
-  a link playing<-->field.
-"
-	; Discard links with bad MI values; anything less than
-	; -50 is bad. Heck, anything under minus ten...
-	(define good-links (filter
-		(lambda (mlink) (< -50 (mst-link-get-score mlink)))
-		MST-PARSE))
-
-	; Create a list of all of the words in the sentence.
-	(define seq-list (delete-duplicates!
-		(fold
-			(lambda (mlnk lst)
-				(cons (mst-link-get-left-numa mlnk)
-					(cons (mst-link-get-right-numa mlnk) lst)))
-			'()
-			good-links)))
-
-	; Return #t if word appears on the left side of mst-lnk
-	(define (is-on-left-side? wrd mlnk)
-		(equal? wrd (mst-link-get-left-atom mlnk)))
-	(define (is-on-right-side? wrd mlnk)
-		(equal? wrd (mst-link-get-right-atom mlnk)))
-
-	; Given a word, and the mst-parse linkset, create a shorter
-	; seq-list which holds only the words linked to the right.
-	(define (mk-right-seqlist seq mparse)
-		(define wrd (mst-numa-get-atom seq))
-		(map mst-link-get-right-numa
-			(filter
-				(lambda (mlnk) (is-on-left-side? wrd mlnk))
-				mparse)))
-
-	(define (mk-left-seqlist seq mparse)
-		(define wrd (mst-numa-get-atom seq))
-		(map mst-link-get-left-numa
-			(filter
-				(lambda (mlnk) (is-on-right-side? wrd mlnk))
-				mparse)))
-
-	; Sort a seq-list into ascending order
-	(define (sort-seqlist seq-list)
-		(sort seq-list
-			(lambda (sa sb)
-				(< (mst-numa-get-index sa) (mst-numa-get-index sb)))))
-
-	; Given a word, the the links, create a pseudo-disjunct
-	(define (mk-pseudo seq mlist)
-		(define lefts (sort-seqlist (mk-left-seqlist seq mlist)))
-		(define rights (sort-seqlist (mk-right-seqlist seq mlist)))
-
-		; Create a list of left-connectors
-		(define left-cnc
-			(map (lambda (sw)
-					(PseudoConnector
-						(mst-numa-get-atom sw)
-						(LgConnDirNode "-")))
-			lefts))
-
-		(define right-cnc
-			(map (lambda (sw)
-					(PseudoConnector
-						(mst-numa-get-atom sw)
-						(LgConnDirNode "+")))
-			rights))
-
-		; return the connector-set
-		(PseudoWordCset
-			(mst-numa-get-atom seq)
-			(PseudoAnd (append left-cnc right-cnc)))
-	)
-
-	(map
-		(lambda (seq) (mk-pseudo seq MST-PARSE))
-		seq-list)
-)
-
 ;  ---------------------------------------------------------------------
 
 (define-public (observe-mst plain-text)
@@ -162,7 +56,7 @@
 	; increments the count by one, and stores the result back
 	(for-each
 		(lambda (dj) (count-one-atom dj))
-		(make-pseudo-disjuncts (mst-parse-text plain-text))
+		(make-sections (mst-parse-text plain-text))
 	)
 )
 ;  ---------------------------------------------------------------------
