3e5eb76f8cb92996907bbe2aae3c82b623fd6bd4
Linas Vepstas <linasvepstas@gmail.com>
2017-06-20 13:35:54 -0500

    No types are in the modulues dir.
    Also, there are no embodiment types.

--- opencog/scm/atom-types.scm
+++ opencog/scm/atom-types.scm
@@ -1,14 +1,11 @@
 ;
 ; Opencog atom-types module
 ;
-
-; Some of the type definition libraries are located in
-; /usr/local/lib/opencog/modules
 (setenv "LTDL_LIBRARY_PATH"
     (if (getenv "LTDL_LIBRARY_PATH")
         (string-append (getenv "LTDL_LIBRARY_PATH")
-            ":/usr/local/lib/opencog:/usr/local/lib/opencog/modules")
-        "/usr/local/lib/opencog:/usr/local/lib/opencog/modules"))
+            ":/usr/local/lib/opencog")
+        "/usr/local/lib/opencog"))
 
 (define-module (opencog atom-types))
 ; Alternately, we could also have
@@ -18,14 +15,12 @@
 
 ; Load the C libraries that actually call the classserver to load
 ; the types.
+(load-extension "libattention-types" "attention_types_init")
 (load-extension "libnlp-types" "nlp_types_init")
 (load-extension "libspacetime-types" "spacetime_types_init")
-(load-extension "libattention-types" "attention_types_init")
-(load-extension "libembodiment-types" "embodiment_types_init")
 
 (add-to-load-path "/usr/local/share/opencog/scm")
 (use-modules (opencog))  ; needed for cog-type->int
+(load-from-path "attention/attention_types.scm")
 (load-from-path "nlp/types/nlp_types.scm")
 (load-from-path "spacetime/spacetime_types.scm")
-(load-from-path "attention/attention_types.scm")
-(load-from-path "embodiment/embodiment_types.scm")
