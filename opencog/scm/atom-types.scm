54cbe57d404b25e434a1a6ad9b92b553bc4e4c5d
Linas Vepstas <linasvepstas@gmail.com>
2017-06-20 14:45:28 -0500

    What the heck. Lets just have a attention module, already

--- opencog/scm/atom-types.scm
+++ opencog/scm/atom-types.scm
@@ -2,16 +2,11 @@
 ; Opencog atom-types module
 ;
 
-(use-modules (opencog))  ; needed for cog-type->int and LTDL
+(use-modules (opencog) (opencog nlp) (opencog attention))
 
 (define-module (opencog atom-types))
-; Alternately, we could also have
-; (define-module (opencog atomtypes spacetime-types))
-; and so on, but I don't see the point of that, at the moment...
 
-; Load the C libraries that call the classserver to load the types.
-(load-extension "libattention-types" "attention_types_init")
+; Load the C library that calls the classserver to load the types.
 (load-extension "libspacetime-types" "spacetime_types_init")
 
-(load "attention/attention_types.scm")
 (load "spacetime/spacetime_types.scm")
