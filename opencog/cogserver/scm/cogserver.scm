2bbf4b417d3eafde752aaec093313e48b23dc813
Linas Vepstas <linasvepstas@gmail.com>
2017-06-20 14:18:52 -0500

    Remove an un-needed search path

--- opencog/cogserver/scm/cogserver.scm
+++ opencog/cogserver/scm/cogserver.scm
@@ -3,7 +3,7 @@
 ;
 
 ; Old CentOS-based systems use lib64
-(define path "/usr/local/lib/opencog:/usr/local/lib/opencog/modules:/usr/local/lib64/opencog:/usr/local/lib64/opencog/modules")
+(define path "/usr/local/lib/opencog:/usr/local/lib64/opencog")
 
 (setenv "LTDL_LIBRARY_PATH"
     (if (getenv "LTDL_LIBRARY_PATH")
@@ -13,8 +13,6 @@
 (define-module (opencog cogserver))
 
 ; libguile-cogserver.so is located in /usr/local/lib/opencog
-; libnlp-types.so is in /usr/local/lib/opencog/modules
-
 (load-extension "libguile-cogserver" "opencog_cogserver_init")
 
 ;; Server falls over if the atom types are not loaded.
