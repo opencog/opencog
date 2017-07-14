2cb017bf481ac0373280f98c5d5aa61f568157f6
Linas Vepstas <linasvepstas@gmail.com>
2017-07-14 11:49:15 -0500

    Work on creating graphs

--- opencog/nlp/learn/learn-lang-diary/zh-images/zh-pairs.scm
+++ opencog/nlp/learn/learn-lang-diary/zh-images/zh-pairs.scm
@@ -11,11 +11,25 @@
 (define ala (make-any-link-api))
 
 ; All of the pairs!
-(define all-hanprs (ala 'all-pairs))
+(define all-hanprs (ala 'all-pairs))  ; 5922477
 
-(define (hanpr-mi HANPR) (pfrq 'pair-fmi HANPR))
+; Assign each word a score, using SCORE-FN, and then rank them by
+; score: i.e. sort them, with highest score first.
+(define (score-and-rank SCORE-FN WORD-LIST)
+	(sort
+		(map (lambda (wrd) (cons (SCORE-FN wrd) wrd)) WORD-LIST)
+		(lambda (a b) (> (car a) (car b)))))
+
+(define (hanpr-mi HANPR) (pfrq 'pair-fmi (gdr HANPR)))
 
 (define sorted-hanpr-mi (score-and-rank hanpr-mi all-hanprs))
+
+(define binned-hanpr-mi (bin-count-simple sorted-hanpr-mi 400))
+
+(let ((outport (open-file "/tmp/binned-hanpr-mi.dat" "w")))
+	(print-bincounts-tsv binned-hanpr-mi outport)
+	(close outport))
+
 
 
 ; binned-cset-mi and weighted-cset-mi
