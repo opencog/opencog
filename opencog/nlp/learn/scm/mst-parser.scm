51702a7bd466a24cbb4b476eb08f40ed258b96e4
Linas Vepstas <linasvepstas@gmail.com>
2017-07-14 22:20:52 -0500

    Merge two files into one

--- opencog/nlp/learn/scm/mst-parser.scm
+++ opencog/nlp/learn/scm/mst-parser.scm
@@ -29,6 +29,22 @@
 ; other additive quantity) associated with the edges. This tree is the
 ; MST tree.
 ;
+; After an sentence has been parsed with the MST parser, the links
+; between words in the parse can be interpreted as Link Grammar links.
+; There are two possible interpretations that can be given to these
+; links: they are either "ANY" links, that connect between any words,
+; or they are links capable of connecting ONLY those two words.
+; In the later case, the link-name can be thought of as the
+; concatenation of the two words it connects.
+;
+; In either case, one can work "backwards", and obtain the effective
+; disjunct on each word, that would have lead to the given MST parse.
+; For each word, this disjunct is just the collection of the other words
+; that it is connected to. It is the unit-distance section of a sheaf.
+;
+; All the hard work is done in the `sheaf` module. This is just a very
+; slim wrapper to parse the text, and update the number of times the
+; disjunct has been observed.
 ; ---------------------------------------------------------------------
 ;
 (use-modules (srfi srfi-1))
@@ -199,6 +215,33 @@
 	(mst-parse-atom-seq word-list scorer)
 )
 
+; ---------------------------------------------------------------------
+; Return #t if the section is bigger than what the current postgres
+; backend can store. Currently, the limit is atoms with at most 330
+; atoms in the outgoing set.
+;
+; This typically occurs when the MST parser is fed a long string of
+; punctuation, or a table of some kind, or other strings that are not
+; actual sentences.
+(define (is-oversize? SECTION)
+	(< 330 (cog-arity (gdr SECTION)))
+)
+
+(define-public (observe-mst plain-text)
+"
+  observe-mst -- update pseduo-disjunct counts by observing raw text.
+
+  This is the second part of the learning algo: simply count how
+  often pseudo-disjuncts show up.
+"
+	; The count-one-atom function fetches from the SQL database,
+	; increments the count by one, and stores the result back
+	(for-each
+		(lambda (dj) (if (not (is-oversize? dj)) (count-one-atom dj)))
+		(make-sections (mst-parse-text plain-text))
+	)
+)
+; ---------------------------------------------------------------------
 ; ---------------------------------------------------------------------
 ;
 ; (use-modules (opencog) (opencog persist) (opencog persist-sql))
