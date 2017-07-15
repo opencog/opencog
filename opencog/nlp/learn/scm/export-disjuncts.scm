3abd759ccd31fda26d9f14a5c593987d060d9791
Linas Vepstas <linasvepstas@gmail.com>
2017-07-15 10:53:08 -0500

    Continue with the file export

--- opencog/nlp/learn/scm/export-disjuncts.scm
+++ opencog/nlp/learn/scm/export-disjuncts.scm
@@ -24,8 +24,37 @@
 
 (use-modules (srfi srfi-1))
 (use-modules (dbi dbi))  ; The guile-dbi interface to SQLite3
+(use-modules (opencog))
 (use-modules (opencog matrix))
 (use-modules (opencog sheaf))
+
+; ---------------------------------------------------------------------
+; Return a caching version of AFUNC. Here, AFUNC is a function that
+; takes a single atom as an argument, and returns some object
+; associated with that atom.
+;
+; This returns a function that returns the same values that AFUNC would
+; return, for the same argument; but if a cached value is available,
+; then return just that.  In order for the cache to be valid, the AFUNC
+; must be side-effect-free.
+;
+(define (make-afunc-cache AFUNC)
+
+	; Define the local hash table we will use.
+	(define cache (make-hash-table))
+
+	; Guile needs help computing the hash of an atom.
+	(define (atom-hash ATOM SZ) (modulo (cog-handle ATOM) SZ))
+	(define (atom-assoc ATOM ALIST)
+		(find (lambda (pr) (equal? ATOM (car pr))) ALIST))
+
+	(lambda (ITEM)
+		(define val (hashx-ref atom-hash atom-assoc cache ITEM))
+		(if val val
+			(let ((fv (AFUNC ITEM)))
+				(hashx-set! atom-hash atom-assoc cache ITEM fv)
+				fv)))
+)
 
 ; ---------------------------------------------------------------------
 ; Convert an integer into a string of letters. Useful for creating
@@ -47,8 +76,20 @@
 
 ;  ---------------------------------------------------------------------
 ;
-; Given left and right strings, return the connector letters.
-(define (get-cnr-name LSTR RSTR)
+; Given a word-pair atom, return a synthetic link name
+; The link names are issued in serial order, first-come, first-served.
+;
+(define get-cnr-name
+	(let ((cnt 0))
+
+		; Notice that the lambda does not actually depend on the
+		; word-pair. It just issues a new string.  The function
+		; cache is what is able to detect and re-emit a previously
+		; issued link name.
+		(make-afunc-cache
+			(lambda (WORD-PAIR)
+				(set! cnt (+ cnt 1))
+				(number->tag cnt))))
 )
 
 ;  ---------------------------------------------------------------------
@@ -60,14 +101,14 @@
   cset-to-lg - SECTION should be a SectionLink
 "
 	; The string name of the germ of the section.
-	(define germ (cog-name (gar SECTION)))
+	(define germ (gar SECTION))
 	(define (connector-to-lg CONNECTOR)
-		(define cnr (cog-name (gar CONNECTOR)))
+		(define cnr (gar CONNECTOR))
 		(define dir (gdr CONNECTOR))
 
 		(if (equal? dir cnr-to-left)
-			(get-cnr-name cnr germ)
-			(get-cnr-name germ cnr)
+			(get-cnr-name (ListLink cnr germ))
+			(get-cnr-name (ListLink germ cnr))
 		)
 	)
 
