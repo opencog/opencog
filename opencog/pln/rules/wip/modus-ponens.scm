5b44736df30c2b462986d9fabcfb33b02d157b9f
Nil Geisweiller <ngeiswei@gmail.com>
2017-07-20 08:17:31 +0300

    Fix Modus Ponens to be Backward Chainer compatible

--- opencog/pln/rules/wip/modus-ponens.scm
+++ opencog/pln/rules/wip/modus-ponens.scm
@@ -19,22 +19,35 @@
 
 ;; Generate the corresponding modus ponens rule given its link-type.
 (define (gen-modus-ponens-rule link-type)
-    (BindLink
-        (VariableList
-            (VariableNode "$A")
-            (VariableNode "$B"))
-        (AndLink
-            (link-type
-                (VariableNode "$A")
-                (VariableNode "$B"))
-            (VariableNode "$A"))
-        (ExecutionOutputLink
-            (GroundedSchemaNode "scm: modus-ponens-formula")
-            (ListLink
-                (link-type
-                    (VariableNode "$A")
-                    (VariableNode "$B"))
-                (VariableNode "$B")))))
+  (let* ((A (Variable "$A"))
+         (B (Variable "$B"))
+         (AB (link-type
+               A
+               B)))
+  (Bind
+    ;; Variable declaration
+    (VariableList
+      A
+      B)
+    ;; Patterns
+    (And
+      ;; Preconditions
+      (Evaluation
+        (GroundedPredicate "scm: gt-zero-confidence")
+        A)
+      (Evaluation
+        (GroundedPredicate "scm: gt-zero-confidence")
+        AB)
+      ;; Pattern clauses
+      AB
+      A)
+    ;; Rewrite
+    (ExecutionOutputLink
+      (GroundedSchemaNode "scm: modus-ponens-formula")
+      (ListLink
+        B
+        AB
+        A)))))
 
 (define modus-ponens-inheritance-rule
   (gen-modus-ponens-rule InheritanceLink))
@@ -45,19 +58,20 @@
 (define modus-ponens-subset-rule
   (gen-modus-ponens-rule SubsetLink))
 
-(define (modus-ponens-formula AB B)
-    (let
-        ((sA (cog-stv-strength (gar AB)))
-         (cA (cog-stv-confidence (gar AB)))
-         (sAB (cog-stv-strength AB))
-         (cAB (cog-stv-confidence AB))
-         (snotAB 0.2)
-         (cnotAB 1))
-        (cog-set-tv!
-            B
-            (stv 
-                (precise-modus-ponens-strength-formula sA sAB snotAB) 
-                (min (min cAB cnotAB) cA)))))
+(define (modus-ponens-formula B AB A)
+  (let
+      ((sA (cog-stv-strength A))
+       (cA (cog-stv-confidence A))
+       (sAB (cog-stv-strength AB))
+       (cAB (cog-stv-confidence AB))
+       ;; Define another rule where these are moved into the premises
+       (snotAB 0.2)
+       (cnotAB 1))
+    (cog-set-tv!
+     B
+     (stv 
+      (precise-modus-ponens-strength-formula sA sAB snotAB) 
+      (min (min cAB cnotAB) cA)))))
 
 ;; Name the rules
 (define modus-ponens-inheritance-rule-name
