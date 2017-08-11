cd39e72546c63bc203635648235408d40811362b
leungmanhin <manhinleung@gmail.com>
2017-08-11 16:13:39 +0800

    Add functions for getting the nlp outputs

--- opencog/nlp/ghost/test.scm
+++ opencog/nlp/ghost/test.scm
@@ -25,3 +25,22 @@
   (display "=== Variables (words)\n") (display var-grd-words) (newline)
   (display "=== Variables (lemmas)\n") (display var-grd-lemmas) (newline)
   (display "=== User Variables\n") (display uvars) (newline))
+
+(define-public (ghost-get-curr-sent)
+  "Get the SentenceNode that is being processed currently."
+  (define sent (cog-chase-link 'StateLink 'SentenceNode ghost-anchor))
+  (if sent (car sent) '()))
+
+(define*-public (ghost-show-relation #:optional (SENT (ghost-get-curr-sent)))
+  "Get a subset of the RelEx outputs of a sentence that GHOST cares.
+   SENT is a SentenceNode, if not given, it will be the current input."
+  (define parses (sentence-get-parses SENT))
+  (define relex-outputs (append-map parse-get-relex-outputs parses))
+  (filter
+    (lambda (r)
+      (define type (cog-type r))
+      (or (equal? 'ParseLink type)
+          (equal? 'WordInstanceLink type)
+          (equal? 'ReferenceLink type)
+          (equal? 'LemmaLink type)))
+    relex-outputs))
