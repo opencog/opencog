4c502c3ec91e750aa86ef2d9219d97f58f7c3aa6
Amen Belayneh <amenbelayneh@gmail.com>
2017-09-19 19:42:04 +0800

    Add unit test for OpenPsiImplicator::check_satisfiability

---
+++ tests/openpsi/psi-implicator.scm
@@ -0,0 +1,34 @@
+(define (rule-1)
+  (Implication
+      (And
+        (InheritanceLink
+          (VariableNode "$H")
+          (ConceptNode "human"))
+        (EvaluationLink
+          (Predicate "eat")
+          (List
+            (VariableNode "$H")
+            (ConceptNode "Beso"))))
+     (InheritanceLink
+        (VariableNode "$H")
+        (ConceptNode "animal"))))
+
+(define (groundable-content-1)
+; Some data to populate the atomspace for grounding (rule-1)
+  (InheritanceLink
+    (ConceptNode "Abeba")
+    (ConceptNode "human"))
+  (EvaluationLink
+    (Predicate "eat")
+    (List
+      (ConceptNode "Abeba")
+      (ConceptNode "Beso")))
+  (InheritanceLink
+    (ConceptNode "Chala")
+    (ConceptNode "human"))
+  (EvaluationLink
+    (Predicate "eat")
+    (List
+      (ConceptNode "Chala")
+      (ConceptNode "Beso")))
+)
