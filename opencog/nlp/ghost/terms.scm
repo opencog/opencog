c4139e8898177940c9923419309a35410a6d7fac
leungmanhin <manhinleung@gmail.com>
2017-09-20 17:59:38 +0800

    Update context-function

--- opencog/nlp/ghost/terms.scm
+++ opencog/nlp/ghost/terms.scm
@@ -176,7 +176,9 @@
 (define (context-function NAME ARGS)
   "Occurrence of a function in the context of a rule.
    The DefinedPredicateNode named NAME should have already been defined."
-  (Put (DefinedPredicate NAME) (List ARGS)))
+  (if (equal? 1 (length ARGS))
+      (Put (DefinedPredicate NAME) ARGS)
+      (Put (DefinedPredicate NAME) (List ARGS))))
 
 (define (action-function NAME ARGS)
   "Occurrence of a function in the action of a rule.
