d3107a612848a3d809ab478b111caed43c277343
Nil Geisweiller <ngeiswei@gmail.com>
2017-11-08 10:24:58 +0200

    Add Mixture Model parameters

--- examples/pln/inference-control-learning/pln-rb.scm
+++ examples/pln/inference-control-learning/pln-rb.scm
@@ -85,3 +85,9 @@
 
 ;; Complexity penalty
 (ure-set-num-parameter pln-rbs "URE:BC:complexity-penalty" 1)
+
+;; Mixture Model compressiveness
+(ure-set-num-parameter pln-rbs "URE:BC:MM:compressiveness" 0.5)
+
+;; Mixture Model complexity penalty
+(ure-set-num-parameter pln-rbs "URE:BC:MM:complexity-penalty" 0.5)
