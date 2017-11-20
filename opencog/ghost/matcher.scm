bab1399bae266d8583cd3295e20c2a00dd05c3a3
leungmanhin <manhinleung@gmail.com>
2017-11-20 18:16:33 +0800

    Implement goal-driven action selection

--- opencog/ghost/matcher.scm
+++ opencog/ghost/matcher.scm
@@ -1,10 +1,13 @@
 ;; This is the GHOST action selector for finding and deciding
 ;; which action should be executed at a particular point in time.
 
-(define (rank-and-eval RULES)
+; Note: This is not being used at the moment
+(define (cs-eval RULES)
 "
+  A ChatScript-like way for selecting actions.
+
   Partition RULES into different categories based on their type,
-  and evaluate each of them. The satisfied one(s) will be returned.
+  and evaluate each of them. A satisfied one will be returned.
 "
   (define curr-topic (ghost-get-curr-topic))
   (define topic-rejoinders '())
@@ -92,6 +95,114 @@
         ; If we are here, there is no match
         (else (list))))
 
+(define (eval-and-select RULES)
+"
+  This is the goal-driven action selection.
+
+  Evaluate the candidates and see which of them, if any, satisfy the
+  current context, and then select one of them based on their weights.
+
+  The weight of an action will be calculated in this way:
+  Wa = 1/Na * sum(Wcagi)
+
+  Na = number of satisfied rules [i] that have the action [a]
+  Wcagi = Scag * Sc * Ig
+
+  Scag = Strength of the psi-rule (c ∧ a => g)
+  Sc = Satisfiability of the context of the psi-rule
+  Ig = Importance of the goal [g]
+"
+  ; Store the evaluation results for the contexts, so that the same context
+  ; won't be evaluated again, in the same psi-step
+  (define context-alist '())
+
+  ; Store the action counts [Na]
+  (define action-cnt-alist '())
+
+  ; Store the sum of action-weights [sum(Wcagi)]
+  (define sum-weight-alist '())
+
+  ; Store the final weight of an action [Wa]
+  (define action-weight-alist '())
+
+  ; For random number generation
+  (define total-weight 0)
+
+  ; For quickly find out which rule an given
+  ; action belongs to
+  ; To-be removed once action selector actually
+  ; returns an action instead of a rule (?)
+  (define action-rule-alist '())
+
+  ; Calculate the weight of the rule R
+  (define (calculate-rweight R)
+    (* (cog-stv-strength R)
+       (assoc-ref context-alist (psi-get-context R))
+       (cog-av-sti (psi-get-goal R))))
+
+  ; Calculate the weight of the action A
+  (define (calculate-aweight A)
+    (* (/ 1 (assoc-ref action-cnt-alist A))
+       (assoc-ref sum-weight-alist A)))
+
+  (for-each
+    (lambda (r)
+      (define rc (psi-get-context r))
+      (define ra (psi-get-action r))
+
+      ; Though an action may be in multiple psi-rule, but
+      ; it doesn't really matter here, just record one
+      ; of them
+      (set! action-rule-alist (assoc-set! action-rule-alist ra r))
+
+      (if (equal? (assoc-ref context-alist rc) #f)
+        (set! context-alist (assoc-set! context-alist rc
+          (cdadr (cog-tv->alist (psi-satisfiable? r))))))
+
+      (if (equal? (assoc-ref action-cnt-alist ra) #f)
+        (set! action-cnt-alist (assoc-set! action-cnt-alist ra 1))
+        (set! action-cnt-alist (assoc-set! action-cnt-alist ra
+          (+ (assoc-ref action-cnt-alist ra) 1))))
+
+      ; Ignore the action if its weight is zero
+      (let ((w (calculate-rweight r)))
+        (if (> w 0)
+          (if (equal? (assoc-ref sum-weight-alist ra) #f)
+            (set! sum-weight-alist (assoc-set! sum-weight-alist ra w))
+            (set! sum-weight-alist (assoc-set! sum-weight-alist ra
+              (+ (assoc-ref sum-weight-alist ra) w)))))))
+    RULES)
+
+  (for-each
+    (lambda (a)
+      (set! action-weight-alist
+        (assoc-set! action-weight-alist (car a) (calculate-aweight (car a))))
+      (set! total-weight (+ total-weight
+        (assoc-ref action-weight-alist (car a)))))
+    sum-weight-alist)
+
+  (cog-logger-debug ghost-logger "context-alist: ~a" context-alist)
+  (cog-logger-debug ghost-logger "action-cnt-alist: ~a" action-cnt-alist)
+  (cog-logger-debug ghost-logger "sum-weight-alist: ~a" sum-weight-alist)
+  (cog-logger-debug ghost-logger "action-weight-alist: ~a" action-weight-alist)
+
+  ; If there is only one action in the list, return that
+  ; Otherwise, pick one based on their weights
+  ; TODO: Return the actual action instead of a rule
+  (if (equal? (length action-weight-alist) 1)
+    (cdar action-rule-alist)
+    (let* ((accum-weight 0)
+           (cutoff (* total-weight (random:uniform (random-state-from-platform))))
+           (action-rtn
+             (find
+               (lambda (a)
+                 (set! accum-weight (+ accum-weight (cdr a)))
+                 (<= cutoff accum-weight))
+               action-weight-alist)))
+      (if (equal? #f action-rtn)
+        (list)
+        (assoc-ref action-rule-alist (car action-rtn))))))
+
 ; ----------
 (define-public (ghost-find-rules SENT)
 "
@@ -123,14 +234,18 @@
                  (list) (append exact-match no-const dual-match)))
          ; Evaluate the matched rules one by one and see which of them satisfy
          ; the current context
-         (rules-satisfied (rank-and-eval rules-candidates)))
+         ; One of them, if any, will be selected and executed
+         (selected (eval-and-select
+           (delete-duplicates (append exact-match no-const dual-match)))))
+
         (cog-logger-debug ghost-logger "For input:\n~a" input-lseq)
         (cog-logger-debug ghost-logger "Rules with no constant:\n~a" no-const)
         (cog-logger-debug ghost-logger "Exact match:\n~a" exact-match)
         (cog-logger-debug ghost-logger "Dual match:\n~a" dual-match)
-        (cog-logger-debug ghost-logger "Rule to-be-evaluated:\n~a" rules-candidates)
-        (cog-logger-debug ghost-logger "Rules satisfied:\n~a" rules-satisfied)
-        (List rules-satisfied)))
+        (cog-logger-debug ghost-logger "To-be-evaluated:\n~a" rules-candidates)
+        (cog-logger-debug ghost-logger "Selected:\n~a" selected)
+
+        (List selected)))
 
 (Define
   (DefinedSchema (ghost-prefix "Get Current Input"))
