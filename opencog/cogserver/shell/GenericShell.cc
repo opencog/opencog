e1d5fa72c71dabac989b9b0eb4700a2f733449cf
Linas Vepstas <linasvepstas@gmail.com>
2018-01-16 16:59:42 -0600

    Telnet on Debian stable works differently from that on Ubuntu.
    Basically, Debian stable does not send Ctrl-C's -- it just sends
    IAC DO TIMING-MARK instead ... which is bizarrely wrong, but whatever.

--- opencog/cogserver/shell/GenericShell.cc
+++ opencog/cogserver/shell/GenericShell.cc
@@ -33,13 +33,27 @@
 using namespace opencog;
 
 // Some random RFC 854 characters
-#define IAC 0xff  // Telnet Interpret As Command
-#define IP 0xf4   // Telnet IP Interrupt Process
-#define AO 0xf5   // Telnet AO Abort Output
-#define EL 0xf8   // Telnet EL Erase Line
+#define IAC  0xff // Telnet Interpret As Command
+#define TEOF 0xec // Telnet EOF
+#define SUSP 0xed // Telnet suspend
+#define ABRT 0xee // Telnet abort
+#define NOP  0xf1 // Telnet NOP no-op
+#define BRK  0xf3 // Telnet break
+#define IP   0xf4 // Telnet IP Interrupt Process
+#define AO   0xf5 // Telnet AO Abort Output
+#define AYT  0xf6 // Telnet AYT Are You There
+#define EC   0xf7 // Telnet EC Erase Character
+#define EL   0xf8 // Telnet EL Erase Line
+#define GA   0xf9 // Telnet GA Go ahead
 #define WILL 0xfb // Telnet WILL
-#define DO 0xfd   // Telnet DO
-#define TIMING_MARK 0x6 // Telnet RFC 860 timing mark
+#define WONT 0xfc // Telnet WONT
+#define DO   0xfd // Telnet DO
+#define DONT 0xfe // Telnet DONT
+
+#define RFC_ECHO          1  // Telnet RFC 857 ECHO option
+#define SUPPRESS_GO_AHEAD 3  // Telnet RFC 858 supporess go ahead
+#define TIMING_MARK       6  // Telnet RFC 860 timing mark
+#define LINEMODE          34 // Telnet RFC 1116 linemode
 
 // Some random ASCII control characters (unix semantics)
 #define EOT 0x4   // end    or ^D at keyboard.
@@ -276,6 +290,25 @@ void GenericShell::eval(const std::string &expr)
 	}
 }
 
+/**
+ * Handle user-generated interrupt (ctrl-C, etc)
+ */
+void GenericShell::user_interrupt()
+{
+	// Discard all pending, unevaluated junk in the queue.
+	// Failure to do so will typically result in confusing
+	// the shell user.
+	while (not evalque.is_empty()) evalque.pop();
+
+	// Must write the abort prompt, first, because
+	// telnet will silently ignore any bytes that
+	// come before it.
+	put_output(abort_prompt);
+	_evaluator->interrupt();
+	_evaluator->clear_pending();
+	finish_eval();
+}
+
 /* ============================================================== */
 /**
  * Handle special characters, evaluate the expression
@@ -310,28 +343,74 @@ void GenericShell::line_discipline(const std::string &expr)
 		if (IAC == c)
 		{
 			c = expr[i+1];
-			if ((IP == c) || (AO == c))
+			if (IP == c or AO == c or SUSP == c)
 			{
-				logger().debug("[GenericShell] got user-interrupt");
-				// Discard all pending, unevaluated junk in the queue.
-				// Failure to do so will typically result in confusing
-				// the shell user.
-				while (not evalque.is_empty()) evalque.pop();
-
-				// Must write the abort prompt, first, because
-				// telnet will silently ignore any bytes that
-				// come before it.
-				put_output(abort_prompt);
-				_evaluator->interrupt();
-				_evaluator->clear_pending();
-				finish_eval();
+				logger().debug("[GenericShell] got user-interrupt %d", c);
+				user_interrupt();
 				return;
 			}
 
 			// Erase line -- just ignore this line.
-			if (EL == c)
+			// Also other things we want to ignore, like break and abort.
+			if (EL == c or EC == c or BRK == c or ABRT == c or
+			    AYT == c or GA == c or NOP == c)
 			{
 				put_output(get_prompt());
+				return;
+			}
+
+			// End-of-file just like ctrl-D
+			if (TEOF == c)
+			{
+				logger().debug("[GenericShell] got end-of-file; exiting shell");
+				self_destruct = true;
+				evalque.cancel();
+				if (show_prompt)
+					put_output("Exiting the shell\n");
+				return;
+			}
+
+			if (DO == c)
+			{
+				c = expr[i+2];
+				// Some telnets, including on Debian Stable, send us
+				// IAC DO TIMING_MARK after a ctrl-C from the user.
+				// If we don't ack this, telnet will stall, discarding
+				// everything we send it (It thinks it is doing us a
+				// favor by discarding buffered up junk).
+				if (TIMING_MARK == c)
+				{
+					logger().debug("[GenericShell] timing mark (user-interrupt?)");
+					user_interrupt();
+					return;
+				}
+
+				// If telnet ever tries to go into character mode,
+				// it will send us SUPPRESS-GO-AHEAD and ECHO
+				if (SUPPRESS_GO_AHEAD == c)
+				{
+					unsigned char ok[] = {IAC, WILL, SUPPRESS_GO_AHEAD, 0};
+					put_output((const char *) ok);
+					return;
+				}
+				if (RFC_ECHO == c)
+				{
+					unsigned char ok[] = {IAC, WONT, RFC_ECHO, '\n', 0};
+					put_output((const char *) ok);
+					return;
+				}
+				logger().debug("[GenericShell] IAC WONT %d", c);
+				unsigned char ok[] = {IAC, WONT, c, '\n', 0};
+				put_output((const char *) ok);
+				return;
+			}
+
+			if (WILL == c)
+			{
+				// Just ignore these.  Typically, will be IAC WILL LINEMODE
+				// sent by the telnet client.
+				c = expr[i+2];
+				logger().debug("[GenericShell] ignoring IAC WILL %d", c);
 				return;
 			}
 		}
