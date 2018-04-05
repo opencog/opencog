d5bf0a25ebfe67f466c55bbd1df1dc7f0562e2a4
glicerico <suarezandres@gmail.com>
2018-04-06 02:11:30 +0800

    fixed comments on PR3022

--- opencog/nlp/learn/scm/mst-parser.scm
+++ opencog/nlp/learn/scm/mst-parser.scm
@@ -400,46 +400,6 @@
 	(close-port file-port)
 )
 
-(define-public (export-mi)
-"
-  Export the word-pairs fMI for
-  every possible pair in the corpus, to allow analysis
-  of the MI-based MST-parser
-
-  The format is:
-  word#1 word#2 pair-mi
-  word#5 word#3 pair-mi
-  ...
-"
-	(define pair-obj (make-clique-pair-api))
-	(define mi-source (add-pair-freq-api pair-obj))
-	(define scorer (make-score-fn mi-source 'pair-fmi))
-
-	(define file-port-fmi (open-file "mi-pairs.txt" "a"))
-
-	; Print an mi-pair
-	(define (print-mi EL)
-		(let ([l-node (gar (gdr EL))] [r-node (gdr (gdr EL))])
-			(if 
-				(and
-					(equal? 'WordNode (cog-type l-node))
-					(equal? 'WordNode (cog-type r-node)))
-				(display
-					(format #f "~a ~a ~a\n"
-						(cog-name l-node)
-						(cog-name r-node)
-						(scorer l-node r-node 1))
-					file-port-fmi))))
-
-	; Print MI-info
-	(for-each
-		(lambda (e) 
-			(print-mi e))
-		(cog-get-atoms 'EvaluationLink))
-
-	(close-port file-port-fmi)
-)
-
 ; ---------------------------------------------------------------------
 ; ---------------------------------------------------------------------
 ;
