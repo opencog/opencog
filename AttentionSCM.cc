9f5c1bc4b8676ad3dcb8597ff6a0b0389c0b867b
Linas Vepstas <linasvepstas@gmail.com>
2018-05-09 00:06:54 -0500

    Start actually implementing attantionSCM

--- AttentionSCM.cc
+++ AttentionSCM.cc
@@ -7,18 +7,22 @@
 
 #ifdef HAVE_GUILE
 
-#include <opencog/guile/SchemeModule.h>
+#include <opencog/guile/SchemePrimitive.h>
+#include "AttentionBank.h"
 
 namespace opencog {
 
-class AttentionSCM : public ModuleWrap
+class AttentionSCM
 {
 	protected:
-		virtual void init(void);
-		static std::vector<FunctionWrap*> _binders;
+		static void* init_in_guile(void*);
+		static void init_in_module(void*);
+		void init(void);
 	public:
 		AttentionSCM(void);
 		~AttentionSCM();
+
+		AttentionValuePtr get_av(const Handle&);
 };
 
 }
@@ -27,58 +31,46 @@ class AttentionSCM : public ModuleWrap
 #include <opencog/guile/SchemePrimitive.h>
 #include <opencog/guile/SchemeSmob.h>
 
-#include "AttentionBank.h"
-
 using namespace opencog;
 
 // ========================================================
 
-// XXX HACK ALERT This needs to be static, in order for python to
-// work correctly.  The problem is that python keeps creating and
-// destroying this class, but it expects things to stick around.
-// Oh well. I guess that's OK, since the definition is meant to be
-// for the lifetime of the process, anyway.
-std::vector<FunctionWrap*> AttentionSCM::_binders;
+AttentionSCM::AttentionSCM(void)
+{
+	static bool is_init = false;
+	if (is_init) return;
+	is_init = true;
+	scm_with_guile(init_in_guile, this);
+}
 
-AttentionSCM::AttentionSCM(void) :
-	ModuleWrap("opencog attention")
-{}
+
+void* AttentionSCM::init_in_guile(void* self)
+{
+	scm_c_define_module("opencog attention", init_in_module, self);
+	scm_c_use_module("opencog attention");
+	return NULL;
+}
+
+void AttentionSCM::init_in_module(void* data)
+{
+	AttentionSCM* self = (AttentionSCM*) data;
+	self->init();
+}
 
 /// This is called while (opencog attention) is the current module.
 /// Thus, all the definitions below happen in that module.
 void AttentionSCM::init(void)
 {
-	// Run implication, assuming that the first argument is a handle to a
-	// BindLink containing variables, a pattern and a rewrite rules.
-	// Returns the first N matches, assuming that N is the second argument.
-	_binders.push_back(new FunctionWrap(bindlink,
-	                   "cog-bind-first-n", "query"));
-
-	// Attentional Focus function
-	_binders.push_back(new FunctionWrap(af_bindlink,
-	                   "cog-bind-af", "query"));
-
-	// A bindlink that returns a TV
-	_binders.push_back(new FunctionWrap(do_satlink,
-	                   "cog-satisfy", "query"));
-
-	// Finds set of all variable groundings, assuming that the first
-	// argument is a handle to pattern. Returns the first N matches,
-	// assuming that N is the second argument.
-	_binders.push_back(new FunctionWrap(satisfying_set,
-	                   "cog-satisfying-set-first-n", "query"));
-
-	// Rule recognition.
-	_binders.push_back(new FunctionWrap(recognize,
-	                   "cog-recognize", "query"));
+	define_scheme_primitive("bcog-av", &AttentionSCM::get_av, this, "attention");
 }
 
 AttentionSCM::~AttentionSCM()
 {
-#if PYTHON_BUG_IS_FIXED
-	for (FunctionWrap* pw : _binders)
-		delete pw;
-#endif
+}
+
+AttentionValuePtr AttentionSCM::get_av(const Handle& h)
+{
+	return get_av(h);
 }
 
 
@@ -89,6 +81,5 @@ void opencog_attention_init(void);
 void opencog_attention_init(void)
 {
 	static AttentionSCM atty;
-	atty.module_init();
 }
 #endif // HAVE_GUILE
