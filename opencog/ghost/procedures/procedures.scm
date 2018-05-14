ef29af4bcac7ade6617e3ffd57577da6f1a9b18b
Amen Belayneh <amenbelayneh@gmail.com>
2018-05-14 18:09:09 +0800

    Add documentation

--- opencog/ghost/procedures/procedures.scm
+++ opencog/ghost/procedures/procedures.scm
@@ -133,6 +133,13 @@
 (define default-stimulus 150)
 
 (define (perceive-face face-id confidence)
+"
+  perceive-face FACE-ID CONFIDENCE
+
+  Returns the atom representing whether the face with id FACE-ID is being
+  seen, after setting its truth-value to (stv 1 CONFIDENCE) and increasing
+  its sti.
+"
   (let ((model (see-face face-id)))
     (cog-stimulate model default-stimulus)
     (cog-set-tv! model (stv 1 confidence))
@@ -140,6 +147,13 @@
 )
 
 (define (perceive-emotion face-id emotion-type confidence)
+"
+  perceive-emotion FACE-ID EMOTION-TYPE CONFIDENCE
+
+  Returns the atom representing whether the face with id FACE-ID is in an
+  emotional-state EMOTION-TYPE, after increasing its sti and setting its
+  truth-value to (stv 1 CONFIDENCE).
+"
   (let ((model (face-emotion face-id emotion-type)))
     (cog-stimulate model default-stimulus)
     (cog-set-tv! model (stv 1 confidence))
@@ -147,6 +161,11 @@
 )
 
 (define (perceive-word word)
+"
+  perceive-word WORD
+
+  Returns WORD after increasing its sti.
+"
   (define wn (Word word))
   (set-time-perceived! wn)
 
@@ -160,6 +179,18 @@
 )
 
 (define (perceive-face-talking face-id new-conf)
+"
+  perceive-face-talking FACE-ID NEW-CONF
+
+  Returns the atom representing that the face with id FACE-ID is talking,
+  after increasing its sti, setting its truth-value to (stv 1 NEW-CONF),
+  and recording the start/stop time of the face starting/stoping talking.
+
+  When NEW-CONF increases past 0.5 then the time is recorded as
+  the start time for the event of the person starting talking, and when
+  NEW-CONF decreases past 0.5 the time is recorded as the stop time for
+  event of the person stopped talking.
+"
   (let* ((model (face-talking face-id))
     (old-conf (tv-conf (cog-tv model))))
 
