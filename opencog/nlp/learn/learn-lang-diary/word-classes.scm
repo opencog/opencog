b038315630a0f456a1283899f01852d31115a861
Linas Vepstas <linasvepstas@gmail.com>
2018-06-05 08:18:10 -0500

    Load them all, before printing them!

--- opencog/nlp/learn/learn-lang-diary/word-classes.scm
+++ opencog/nlp/learn/learn-lang-diary/word-classes.scm
@@ -2,18 +2,22 @@
 ; grammatical class explorer
 ; Display stats and stuff, about the various word classes.
 ;
+; -------------------------------------------------------
 ;
-How many grammatical classes are there?
+; Load all grammatical classes from storage
+(define (fetch-all-gram-classes)
+	(load-atoms-of-type 'WordClassNode)
+	(for-each
+		(lambda (CLS) (fetch-incoming-by-type CLS 'MemberLink))
+		(cog-get-atoms 'WordClassNode)))
+
+; How many grammatical classes are there?
 (cog-count-atoms 'WordClassNode)
 
 ; Show them all
 (cog-get-atoms 'WordClassNode)
 
-; Show the members.
-(map (lambda (atom) (cog-incoming-by-type atom 'MemberLink))
-	(cog-get-atoms 'WordClassNode))
-
-; Print the members
+; Print the members of one class.
 (define (prt-members-of-class CLS)
 	(define membs (cog-incoming-by-type CLS 'MemberLink))
 	(format #t "Class <~A> has ~A members:\n"
@@ -22,7 +26,6 @@ How many grammatical classes are there?
 		(lambda (memb)
 			(format #t "      ~A\n" (cog-name (gar memb))))
 		membs))
-
 
 ; Print all classes and members
 (for-each prt-members-of-class (cog-get-atoms 'WordClassNode))
