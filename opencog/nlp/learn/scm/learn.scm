163fd6894b94f1e833be3edb0618d0906bd92cd4
Linas Vepstas <linasvepstas@gmail.com>
2018-06-15 16:22:53 -0500

    Add cross-connector code

--- opencog/nlp/learn/scm/learn.scm
+++ opencog/nlp/learn/scm/learn.scm
@@ -14,6 +14,7 @@
 (load "learn/batch-word-pair.scm")
 (load "learn/mst-parser.scm")
 (load "learn/pseudo-csets.scm")
+(load "learn/cross-connector.scm")
 (load "learn/word-cosines.scm")
 (load "learn/summary.scm")
 (load "learn/gram-class.scm")
