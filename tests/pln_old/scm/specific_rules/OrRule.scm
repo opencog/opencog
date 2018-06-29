74eecfbb8c79c97a4d29d5b4dac6b41ae7ca2c40
Nil Geisweiller <ngeiswei@gmail.com>
2018-06-29 08:47:57 +0300

    Remove old Python PLN

--- tests/pln_old/scm/specific_rules/OrRule.scm
+++
@@ -1,7 +0,0 @@
-(define a (ConceptNode "A" (stv 0.8 0.8)))
-(define b (ConceptNode "B" (stv 0.2 0.65)))
-(define or_ab (OrLink a b))
-
-(EvaluationLink (PredicateNode "query") (ListLink or_ab))
-(EvaluationLink (PredicateNode "rules") (ListLink (ConceptNode "OrCreationRule<2>")))
-
--- tests/pln_old/scm_disabled/specific_rules/NotCreationRule.scm
+++
@@ -1,7 +0,0 @@
-; test of NotRule
-(define A (ConceptNode "A" (stv 0.1 0.3)))
-(define NotA (NotLink A))
-
-(EvaluationLink (PredicateNode "query") (ListLink NotA))
-(EvaluationLink (PredicateNode "rules") (ListLink (ConceptNode "NotCreationRule")))
-
