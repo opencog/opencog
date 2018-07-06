0667aaf36acbf3427953aed87f48e6fb292dab7d
Amen Belayneh <amenbelayneh@gmail.com>
2018-07-06 20:18:08 +0800

    Finish defining the pln trail-3 ure rulebase

--- opencog/ghost/procedures/pln-trail-3.scm
+++ opencog/ghost/procedures/pln-trail-3.scm
@@ -1,3 +1,11 @@
+#!
+
+Input = Reptiles can breathe
+Input = Lizards are reptiles
+|-
+output = Lizards can breathe
+
+!#
 ; ----- For SuReal ----- ;
 (nlp-parse "cats can read")
 
@@ -98,6 +106,16 @@
 (define (configure-pln-rbs-3)
     (define rb (ConceptNode "r2l-pln-3"))
 
+    ; The deduction rule doesn't work when the truth-value are extremely
+    ; low. (load-gtwc) wasn't helpful
+    ;(pln-load-rules "term/deduction")
+    (pln-load-rules "wip/evaluation-to-member")
+    (pln-load-rules "wip/member-to-inheritance")
+    (pln-load-rules "wip/temp-deduction")
+    (pln-load-rules "wip/evaluation-to-member")
+    (pln-load-rules "wip/inheritance-to-member")
+    (pln-load-rules "wip/member-to-evaluation")
+
     ; NOTE: The number has no relevance in r2l-mode
     (ure-define-rbs rb 0)
     ; Not sure why
@@ -109,6 +127,18 @@
          r2l-post-processing-2-rule (stv 1 1))
     (ure-define-add-rule rb "r2l-post-processing-3-rule"
          r2l-post-processing-3-rule (stv 1 1))
+    (ure-define-add-rule rb "evaluation-to-member-1-rule"
+         evaluation-to-member-1-rule (stv 1 1))
+    (ure-define-add-rule rb "member-to-inheritance-rule"
+         member-to-inheritance-rule (stv 1 1))
+    ;(ure-define-add-rule rb "deduction-inheritance-rule"
+    ;    deduction-inheritance-rule (stv 1 1))
+    (ure-define-add-rule rb "temp-deduction-inheritance-rule"
+        temp-deduction-inheritance-rule (stv 1 1))
+    (ure-define-add-rule rb "inheritance-to-member-rule"
+        inheritance-to-member-rule (stv 1 1))
+    (ure-define-add-rule rb "member-to-evaluation-1-rule"
+         member-to-evaluation-1-rule (stv 1 1))
 
     ; Return the rule-base
     rb
