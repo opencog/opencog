d5897bc69d180239132de613a3326714ee398b2b
Linas Vepštas <linasvepstas@gmail.com>
2018-08-16 21:39:15 +0800

    Start working on a public vector summer

--- opencog/nlp/learn/scm/gram-class.scm
+++ opencog/nlp/learn/scm/gram-class.scm
@@ -369,11 +369,164 @@
 
 ; ---------------------------------------------------------------------
 
+(define-public (section-vec-sum SUM LLOBJ FRAC ZIPF WA WB)
+"
+  section-vec-sum SUM LLOBJ FRAC ZIPF WA WB - sum the vectors WA and
+  WB, by summing the counts on SectionLinks anchored by WA and WB.
+  Place the sums on Sections anchored by the atom SUM. This does NOT
+  alter the counts on WA and WB.
+
+  SUM can be any atom.
+  WA and WB can be any atoms identifying Sections; they are typically
+     WordNodes or possibly WordClassNodes.
+  FRAC should be a floating point number between zero and one,
+     indicating the fraction of a non-shared count to be used.
+     Setting this to 1.0 gives the sum of the union of supports;
+     setting this to 0.0 gives the sum of the intersection of supports.
+  ZIPF is the smallest observation count, below which counts
+     will not be divided up.
+  LLOBJ is used to access counts on pairs.  Pairs are SectionLinks,
+     that is, are (left-atom,right-atom) pairs wrapped in a SectionLink.
+     Typically, left-Atom is a WordNode, and right-atom is a disjunct.
+
+  The sum of the counts on Sections identified by WA and WB are
+  performed. That is for each matching section on WA and WB, the counts
+  are accessed, summed, and a corresponding Section is created on SUM,
+  with the count placed on that Section.
+
+  The counts are summed in full only if both counts are non-zero.
+  Otherwise, only a FRAC fraction of a single, unmatched count is
+  copied to the corresponding Section on SUM.
+
+  The counts on WA and WB are NOT altered! This means that the total
+  number of counts in the system is increased by this call.  This 
+  implies that there can be a subtle corrpution of frequencies, if the
+  SUM atom is not chosen wisely -- e.g. if the SUM is the same type as
+  WA and WB (and SUM is then stored to disk, or otherwise mixed in.)
+"
+	(define (bogus a b) (format #t "Its ~A and ~A\n" a b))
+	(define ptu (add-tuple-math LLOBJ bogus))
+
+	; set-count ATOM CNT - Set the raw observational count on ATOM.
+	(define (set-count ATOM CNT) (cog-set-tv! ATOM (cog-new-ctv 1 0 CNT)))
+
+	; Accumulated counts for the two.
+	(define accum-lcnt 0)
+	(define accum-rcnt 0)
+
+	; Merge two sections into one, placing the result on the word-class.
+	; Given, given a pair of sections, sum the counts from each, and
+	; then place that count on a corresponding section on the word-class.
+	; Store the updated section to the database.
+	;
+	; One or the other sections can be null. If both sections are not
+	; null, then both are assumed to have exactly the same disjunct.
+	;
+	; This works fine for merging two words, or for merging
+	; a word and a word-class.  It even works for merging
+	; two word-classes.
+	;
+	(define (merge-section-pair SECT-PAIR)
+		; The two word-sections to merge
+		(define lsec (first SECT-PAIR))
+		(define rsec (second SECT-PAIR))
+
+		; The counts on each, or zero.
+		(define lcnt (if (null? lsec) 0 (LLOBJ 'get-count lsec)))
+		(define rcnt (if (null? rsec) 0 (LLOBJ 'get-count rsec)))
+
+		; Return #t if sect is a Word section, not a word-class section.
+		(define (is-word-sect? sect)
+			(eq? 'WordNode (cog-type (cog-outgoing-atom sect 0))))
+
+		; If the other count is zero, take only a FRAC of the count.
+		; But only if we are merging in a word, not a word-class;
+		; we never want to shrink the support of a word-class, here.
+		(define wlc (if
+				(and (null? rsec) (is-word-sect? lsec) (< ZIPF lcnt))
+				(* FRAC lcnt) lcnt))
+		(define wrc (if
+				(and (null? lsec) (is-word-sect? rsec) (< ZIPF rcnt))
+				(* FRAC rcnt) rcnt))
+
+		; Sum them.
+		(define cnt (+ wlc wrc))
+
+		; Compute what's left on each.
+		(define lrem (- lcnt wlc))
+
+		; Update the count on the section.
+		; If the count is zero or less, delete the section.
+		(define (update-section-count SECT CNT)
+			(if (< 1.0e-10 CNT)
+				(begin (set-count SECT CNT) (store-atom SECT))
+				(begin (set-count SECT 0) (cog-delete SECT))))
+
+		; The cnt can be zero, if FRAC is zero.  Do nothing in this case.
+		(if (< 1.0e-10 cnt)
+			(let* (
+					; The disjunct. Both lsec and rsec have the same disjunct.
+					(seq (if (null? lsec) (cog-outgoing-atom rsec 1)
+							(cog-outgoing-atom lsec 1)))
+					; The merged word-class
+					(mrg (Section wrd-class seq))
+				)
+
+				; The summed counts
+				(set-count mrg cnt)
+				(store-atom mrg) ; save to the database.
+
+				; Now subtract the counts from the words.
+				; Left side is either a word or a word-class.
+				; If its a word-class, we've already updated
+				; the count.
+				(if (and (not (null? lsec)) (is-word-sect? lsec))
+					(update-section-count lsec (- lcnt wlc)))
+
+				; Right side is WB and is always a WordNode
+				(if (not (null? rsec))
+					(update-section-count rsec (- rcnt wrc)))
+			))
+
+		; Accumulate the counts, handy for tracking membership fraction
+		(set! accum-lcnt (+ accum-lcnt wlc))
+		(set! accum-rcnt (+ accum-rcnt wrc))
+	)
+
+	(for-each merge-section-pair (ptu 'right-stars (list WA WB)))
+
+	(if (eq? 'WordNode (cog-type WA))
+		(let ((ma (MemberLink WA wrd-class))
+				(mb (MemberLink WB wrd-class)))
+			; Track the number of word-observations moved from
+			; the words, the the class. This is how much the words
+			; contributed to the class.
+			(set-count ma accum-lcnt)
+			(set-count mb accum-rcnt)
+			; Put the two words into the new word-class.
+			(store-atom ma)
+			(store-atom mb))
+
+		; If WA is not a WordNode, assume its a WordClassNode.
+		; The process is similar, but slightly altered.
+		; We assume that WB is a WordNode, but perform no safety
+		; checking to verify this.
+		(let ((mb (MemberLink WB wrd-class)))
+			(set-count mb accum-rcnt)
+			; Add WB to the mrg-class (which is WA already)
+			(store-atom mb))
+	)
+	wrd-class
+)
+
+; ---------------------------------------------------------------------
+
 (define (merge-project LLOBJ FRAC ZIPF WA WB)
 "
-  merge-project FRAC WA WB - merge WA and WB into a grammatical class.
-  Return the merged class. This merges the Sections, this does not merge
-  the shapes (connectors).  See `cset-class.scm` for shape merging.
+  merge-project LLOBJ FRAC ZIPF WA WB - merge WA and WB into a grammatical
+  class.  Return the merged class. This merges the Sections, this does not
+  merge connectors, nor does it merge shapes.  See `cset-class.scm` for
+  connector merging.
 
   WA should be a WordNode or a WordClassNode.
   WB is expected to be a WordNode.
