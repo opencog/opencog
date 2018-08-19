b46ba5c28d33f007e302c7dc4a0d5d0c0787dd1b
glicerico <suarezandres@gmail.com>
2018-08-19 12:09:25 +0200

    modified counter to increment an atom n-times

--- opencog/nlp/learn/scm/common.scm
+++ opencog/nlp/learn/scm/common.scm
@@ -31,6 +31,18 @@
   count-one-atom ATM -- increment the count by one on ATM, and
   update the SQL database to hold that count.
 
+  calls count-one-atom-times with count argument equal to 1.
+"
+	(count-one-atom-times ATM 1) ; call incrementer with parameter 1
+)
+
+; ---------------------------------------------------------------------
+
+(define (count-one-atom-times ATM times)
+"
+  count-one-atom-times ATM times-- increment the count by times on ATM, and
+  update the SQL database to hold that count.
+
   This will also automatically fetch the previous count from
   the SQL database, so that counting will work correctly, when
   picking up from a previous point.
@@ -40,7 +52,7 @@
   so if some other process updates the database, this will miss that
   update.
 "
-	(define (incr-one atom)
+	(define (incr-times atom TIMES)
 		; If the atom doesn't yet have a count TV attached to it,
 		; then its probably a freshly created atom. Go fetch it
 		; from SQL. Otherwise, assume that what we've got here,
@@ -48,10 +60,10 @@
 		; there is only one process updating the counts.
 		(if (not (cog-ctv? (cog-tv atom)))
 			(fetch-atom atom)) ; get from SQL
-		(cog-inc-count! atom 1) ; increment
+		(cog-inc-count! atom TIMES) ; increment
 	)
 	(begin
-		(incr-one ATM) ; increment the count on ATM
+		(incr-times ATM times) ; increment the count on ATM
 		(store-atom ATM)) ; save to SQL
 )
 
