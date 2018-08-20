cb90681a464565ecb91c1a36666f0ce8961b36c5
glicerico <suarezandres@gmail.com>
2018-08-20 17:14:09 +0200

    reorganized ULL run files into run-ull

--- opencog/nlp/learn/run-ull/launch-cogserver.scm
+++ opencog/nlp/learn/run-ull/launch-cogserver.scm
@@ -1,5 +1,5 @@
 ;
-; observe-launch.scm
+; launch-cogserver.scm
 ;
 ; Run the cogserver, needed for the language-learning disjunct
 ; counting pipeline. Starts the cogserver, opens the database,
@@ -7,31 +7,33 @@
 ;
 (use-modules (opencog) (opencog persist) (opencog persist-sql))
 (use-modules (opencog cogserver))
-(use-modules (opencog nlp) (opencog nlp learn))
+(use-modules (opencog nlp) (opencog nlp ull-parser))
 (use-modules (system repl common))
 (use-modules (system repl server))
 
 (add-to-load-path ".")
 (load "utilities.scm")
 
-; Get the database connection details
-(define database-uri (get-connection-uri))
+; Get the mode, languague and database connection details
+(define cog-mode (get-mode))
 (define language (get-lang))
+(define database-uri (get-connection-uri))
 
-; set the prompt for the given language
+; set the prompt for the given language and mode
+;(if (condicion)
 (repl-default-option-set! 'prompt (string-append "scheme@(" 
-    language "-pairs)> "))
-
-; Get the database connection details
-(define database-uri (get-connection-uri))
-(define language (get-lang))
+    language "-" cog-mode ")> "))
+;(else statement))
 
 ; Start the cogserver with configs for the given language
-(start-cogserver (string-append "config/opencog-pairs-" language ".conf"))
+(start-cogserver (string-append "config/opencog-" cog-mode "-" language ".conf"))
 
 ; Open the database.
 (sql-open database-uri)
 (display "Opened database: ")
 (display database-uri)
 (display "\n")
+
+; Load up the words and word-pairs
+(display "Fetching all words from database. This may take a few minutes.\n")
 (fetch-all-words)
