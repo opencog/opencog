2bf899e6747940c1e7f63cb7a95e16b02f163bc5
glicerico <suarezandres@gmail.com>
2018-09-11 16:48:56 +0800

    fixed parameter passing

--- opencog/nlp/learn/run-poc/submit-one.pl
+++ opencog/nlp/learn/run-poc/submit-one.pl
@@ -6,16 +6,16 @@
 # cogserver located on host ARGV[0] and port ARGV[1].  The sentences
 # are read from standard input, and must be arranged with one sentence
 # per line. The are sent to the cogserver using ARGV[2] as the command.
-# For word-pair counting, ARGV[2] is "observe-text"
-# For disjunct counting, ARGV[2] is "observe-mst"
-# ARGV[3] and ARGV[4] are necessary arguments for parameter configuration
-# (see file process-one.sh to see them)
+# For word-pair counting, ARGV[2] is "observe-text-mode"
+# For disjunct counting, ARGV[2] is "observe-mst-mode"
+# ARGV[3] and ARGV[4] (and ARGV[5] for observe-mst-mode) are necessary
+# arguments for parameter configuration (see file process-one.sh to see them)
 #
 # Example usage:
-#    cat file | ./submit-one.pl localhost 17001 observe-text "lg" 24
+#    cat file | ./submit-one.pl localhost 17001 observe-text-mode "any" 24
 #
 
-die "Wrong number of args!" if ($#ARGV != 4);
+die "Wrong number of args!" if ($#ARGV < 4);
 
 # Verify that the host and port number are OK.
 `nc -z $ARGV[0] $ARGV[1]`;
@@ -31,7 +31,10 @@ while (<STDIN>)
 
 	# open(NC, "|nc localhost 17002") || die "nc failed: $!\n";
 	open NC, $netcat || die "nc failed: $!\n";
-	print NC "($ARGV[2] \"$_\" \"$ARGV[3]\" $ARGV[4])\n";
+	if ( $ARGV[2] eq "observe-text-mode" )
+		{ print NC "($ARGV[2] \"$_\" \"$ARGV[3]\" $ARGV[4])\n"; }
+	elsif ( $ARGV[2] eq "observe-mst-mode" )
+		{ print NC "($ARGV[2] \"$_\" \"$ARGV[3]\" $ARGV[4] $ARGV[5])\n"; }
 	my $elapsed = time() - $start_time;
 	print "submit-one (elapsed $elapsed): $_\n";
 }
