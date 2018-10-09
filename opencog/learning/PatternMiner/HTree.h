d859717a791883e089d3777797602d90a63dce78
Nil Geisweiller <ngeiswei@gmail.com>
2018-10-09 14:03:39 +0300

    Indent with tabs

--- opencog/learning/PatternMiner/HTree.h
+++ opencog/learning/PatternMiner/HTree.h
@@ -38,64 +38,64 @@ class HTreeNode;
 
 struct ExtendRelation // to store a super pattern of a pattern
 {
-    HTreeNode* extendedHTreeNode; // super pattern HTreeNode
-    Handle sharedLink; // link in the original pattern that connect to new extended Link
-    Handle newExtendedLink; // in super pattern (contains variables, not the instance link), without unifying
-    Handle extendedNode; // node that being extended in the original AtomSpace (the value node, not its variable name node)
-    bool isExtendedFromVar;
+	HTreeNode* extendedHTreeNode; // super pattern HTreeNode
+	Handle sharedLink; // link in the original pattern that connect to new extended Link
+	Handle newExtendedLink; // in super pattern (contains variables, not the instance link), without unifying
+	Handle extendedNode; // node that being extended in the original AtomSpace (the value node, not its variable name node)
+	bool isExtendedFromVar;
 };
 
 struct SuperRelation_b // to store a super pattern of the same gram of a pattern
 {
-    HTreeNode* superHTreeNode; // the super pattern HTreeNode
-    Handle constNode; // the const Node
+	HTreeNode* superHTreeNode; // the super pattern HTreeNode
+	Handle constNode; // the const Node
 };
 
 struct SubRelation_b // to store a sub pattern of the same gram of a pattern
 {
-    HTreeNode* subHTreeNode; // the sub pattern HTreeNode
-    Handle constNode; // the const Node
+	HTreeNode* subHTreeNode; // the sub pattern HTreeNode
+	Handle constNode; // the const Node
 };
 
 class HTreeNode
 {
 public:
-    HandleSeq pattern;
-    Handle quotedPatternLink; // only used when if_quote_output_pattern = true
-    HandleSeqSeq instances; // the corresponding instances of this pattern in the original AtomSpace, only be used by breadth first mining
-    std::set<HTreeNode*> parentLinks;
-    std::set<HTreeNode*> childLinks;
+	HandleSeq pattern;
+	Handle quotedPatternLink; // only used when if_quote_output_pattern = true
+	HandleSeqSeq instances; // the corresponding instances of this pattern in the original AtomSpace, only be used by breadth first mining
+	std::set<HTreeNode*> parentLinks;
+	std::set<HTreeNode*> childLinks;
 
-    // set<string> instancesUidStrings;// all uid in each instance HandleSeq in all instances, in the form of 5152_815_201584. to prevent the same instance being count multiple times
+	// set<string> instancesUidStrings;// all uid in each instance HandleSeq in all instances, in the form of 5152_815_201584. to prevent the same instance being count multiple times
 
-    std::vector<ExtendRelation> superPatternRelations; // store all the connections to its super patterns
+	std::vector<ExtendRelation> superPatternRelations; // store all the connections to its super patterns
 
-    std::vector<SuperRelation_b> superRelation_b_list; // store all the superRelation_b
+	std::vector<SuperRelation_b> superRelation_b_list; // store all the superRelation_b
 
-    std::map<Handle, std::vector<SubRelation_b>> SubRelation_b_map;// map<VariableNode, vector<SubRelation_b>>
+	std::map<Handle, std::vector<SubRelation_b>> SubRelation_b_map;// map<VariableNode, vector<SubRelation_b>>
 
-    unsigned int count; // Number of instances grounding this pattern
-    unsigned int var_num; // Number of the variables in this pattern
-    double interactionInformation;
-    double nI_Surprisingness;
-    double nII_Surprisingness;
-    double nII_Surprisingness_b; // calculated from the other same gram patterns
-    unsigned int max_b_subpattern_num;
+	unsigned int count; // Number of instances grounding this pattern
+	unsigned int var_num; // Number of the variables in this pattern
+	double interactionInformation;
+	double nI_Surprisingness;
+	double nII_Surprisingness;
+	double nII_Surprisingness_b; // calculated from the other same gram patterns
+	unsigned int max_b_subpattern_num;
 
-    std::string surprisingnessInfo; // the middle info record the surpringness calculating process for this pattern
+	std::string surprisingnessInfo; // the middle info record the surpringness calculating process for this pattern
 
-    HandleSeq sharedVarNodeList; // all the shared nodes in these links in the original AtomSpace, each handle is a shared node
+	HandleSeq sharedVarNodeList; // all the shared nodes in these links in the original AtomSpace, each handle is a shared node
 
-    std::string to_string(const std::string& indent=empty_string) const;
+	std::string to_string(const std::string& indent=empty_string) const;
 
-    HTreeNode()
-        : count(0),
-          var_num(0),
-          interactionInformation(0.0),
-          nI_Surprisingness(0.0),
-          nII_Surprisingness(0.0),
-          nII_Surprisingness_b(1.0),
-          max_b_subpattern_num(0) {}
+	HTreeNode()
+		: count(0),
+		  var_num(0),
+		  interactionInformation(0.0),
+		  nI_Surprisingness(0.0),
+		  nII_Surprisingness(0.0),
+		  nII_Surprisingness_b(1.0),
+		  max_b_subpattern_num(0) {}
 };
 
 } // ~namespace PatterMining
