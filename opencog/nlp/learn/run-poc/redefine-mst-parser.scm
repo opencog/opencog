5d1cdb3c8b3dfa5763d33749382429ca15136383
andres <suarezandres@gmail.com>
2019-02-17 23:58:26 +0800

    building file_MI parser

--- opencog/nlp/learn/run-poc/redefine-mst-parser.scm
+++ opencog/nlp/learn/run-poc/redefine-mst-parser.scm
@@ -6,6 +6,67 @@
 ; A list of word-pairs, together with the associated mutual information,
 ; is returned.
 ;
+(define-public (mst-parse-text-file plain-text mst-dist)
+"
+	Procedure to mst-parse sentences coming from an instance-pair weight file.
+	Pre-process the sentence to assign word ids, and also store the weights
+	corresponding to the instance-pairs in the atomspace. Finally flush
+	this newly created atoms to avoid RAM explosion.
+"
+	; Tokenize the given input into a list of words.
+	### assume sentence is tokenized, just separate by spaces
+	;(define (word-strs text-line)
+	;	(tokenize-text text-line)
+	;)
+
+	; Append the given id-str to the given word
+	(define (append-id word id-str)
+		(string-append word "_" id-str)
+	)
+
+	; Create a sequence of atoms from the sequence of strings.
+	; Also appends the word position to every word as instance marker
+	(define (word-list list-of-words)
+		(define cnt 0)
+		(map 
+			(lambda (str) 
+				(set! cnt (+ cnt 1))
+				(WordNode (append-id str (number->string cnt)))
+			)
+			list-of-words
+		)
+	)	
+
+	; define scoring function to look for weights in atoms
+	###(define scorer ())
+
+	; Check if blank line
+	(if (equal? plain-text "\n")
+		; Parse stored sentence and set new-sentence flag to true
+		(begin
+			(set! new-sent-flag #t)
+			(word-list (tokenize-text current-sentence))
+			(mst-parse-atom-seq word-list trunc-scorer)
+		)
+
+		; Check if it's the first line in a block (contains sentence to parse)
+		; If not, it's an instance-pair with its weight
+		(if new-sent-flag
+			; store current sent, set new-sent-flag to false, flush prev instance-pairs
+			(begin
+				###; delete old word-pairs
+				(define current-sentence plain-text)
+				(set! new-sent-flag #f)
+			)
+			; Create atom with instance-pair weight, tag instances with id
+			###
+		)
+	)
+
+
+)
+
+
 (define-public (mst-parse-text-mode plain-text cnt-mode mst-dist)
 
 	; Tokenize the sentence into a list of words.
@@ -18,8 +79,8 @@
 	(define pair-obj
 		(cond
 			((or (equal? cnt-mode "clique")
-			     (equal? cnt-mode "clique-dist"))
-			      	(make-clique-pair-api))
+				 (equal? cnt-mode "clique-dist"))
+					(make-clique-pair-api))
 			(else (make-any-link-api))))
 
 	(define mi-source (add-pair-freq-api pair-obj))
@@ -109,11 +170,11 @@
 (define-public (observe-mst-mode plain-text CNT-MODE MST-DIST EXPORT-MST)
 "
   observe-mst-mode -- update pseduo-disjunct counts by observing raw text.
-                      Build mst-parses using MI calculated beforehand.
-                      When MST-DIST is true, word-pair MI values are adjusted
-                      for distance.
-                      Obtained parses are exported to file if EXPORT-MST
-                      is true.
+					  Build mst-parses using MI calculated beforehand.
+					  When MST-DIST is true, word-pair MI values are adjusted
+					  for distance.
+					  Obtained parses are exported to file if EXPORT-MST
+					  is true.
 
   This is the second part of the learning algo: simply count how
   often pseudo-disjuncts show up.
