d220979dc0d043953fe0e7ab698ab4dad13ef688
andres <suarezandres@gmail.com>
2019-02-24 15:47:42 +0800

    removed my utility files

--- opencog/nlp/learn/run-poc/prepare_run.sh
+++
@@ -1,3 +0,0 @@
-rm beta-pages/* gamma-pages/*
-cp alpha-pages/$1 beta-pages/
-cp alpha-pages/$1 gamma-pages/
--- opencog/nlp/learn/run-poc/reset_database.sh
+++
@@ -1,5 +0,0 @@
-dropdb $1
-
-createdb $1
-
-cat /atomspace/opencog/persist/sql/multi-driver/atom.sql | psql $1
