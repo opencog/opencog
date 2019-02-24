d50c13fce7b96a4ac8a0933c1be160b41bc20e88
Linas Vepstas <linasvepstas@gmail.com>
2019-02-24 15:12:52 -0600

    Remove all files duplicted in opencog/learn

--- opencog/nlp/learn/run/pair-submit-en.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Batch word-pair counting script for English.
-# Loop over all the files in 'beta-pages', sentence-split them
-# and submit them for word-pair couting.
-#
-time find beta-pages -type f -exec ./pair-one.sh en {} localhost 17005 \;
--- opencog/nlp/learn/run/pair-submit-lt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Batch word-pair counting script for Lithuanian.
-# Loop over all the files in 'beta-pages', sentence-split them
-# and submit them for word-pair counting.
-#
-time find beta-pages -type f -exec ./pair-one.sh lt {} localhost 17002 \;
