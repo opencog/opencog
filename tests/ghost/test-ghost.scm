1eac8dbbcfc8161a7b350488fa447b296dbfd624
leungmanhin <manhinleung@gmail.com>
2019-03-04 17:37:32 +0800

    Test for concepts

--- tests/ghost/test-ghost.scm
+++ tests/ghost/test-ghost.scm
@@ -42,9 +42,9 @@
 (ghost-parse "r: (mr. and MRS. Smith) test literal - personal title")
 (ghost-parse "r: (9 a.m.) test literal - time 1")
 (ghost-parse "r: (5pm) test literal - time 2")
-(ghost-parse "r: (\"wanna eat\") test literal - phrase")
+(ghost-parse "r: (\"wanna drink\") test literal - phrase")
 
-(test-equal "GHOST literal" "" (get-result "orange"))
+(test-equal "GHOST literal" (string) (get-result "orange"))
 (test-equal "GHOST literal" "test literal" (get-result "oranges"))
 (test-equal "GHOST literal" "test literal - apostrophe" (get-result "let's"))
 (test-equal "GHOST literal" "test literal - capital letter" (get-result "I'm good"))
@@ -52,7 +52,20 @@
 (test-equal "GHOST literal" "test literal - time 1" (get-result "9 a.m."))
 (test-equal "GHOST literal" "test literal - time 2" (get-result "5pm"))
 (test-equal "GHOST literal" "test literal - time 2" (get-result "5 pm"))
-(test-equal "GHOST literal" "test literal - phrase" (get-result "wanna eat"))
+(test-equal "GHOST literal" "test literal - phrase" (get-result "wanna drink"))
+(test-equal "GHOST literal" (string) (get-result "wanna go drink"))
+
+; --- Concept --- ;
+(ghost-parse "concept: ~eat (eat ingests \"binge and purge\" ~swallow)")
+(ghost-parse "concept: ~swallow (gulp)")
+(ghost-parse "r: (~eat) test concept")
+
+(test-equal "GHOST concept" "test concept" (get-result "eat"))
+(test-equal "GHOST concept" "test concept" (get-result "eats"))
+(test-equal "GHOST concept" (string) (get-result "ingest"))
+(test-equal "GHOST concept" "test concept" (get-result "ingests"))
+(test-equal "GHOST concept" "test concept" (get-result "binge and purge"))
+(test-equal "GHOST concept" "test concept" (get-result "gulp"))
 
 ; End of the test
 (test-end ghost-utest)
