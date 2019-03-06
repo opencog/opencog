90a12d24178a945dd8761acc85907920db203812
andres <suarezandres@gmail.com>
2019-03-06 13:59:13 +0800

    remove files duplicated due to fork of singnet/learn

--- opencog/nlp/learn/run/pair-submit-pl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Batch word-pair counting script for Polish.
-# Loop over all the files in 'beta-pages', sentence-split them
-# and submit them for word-pair counting.
-#
-time find beta-pages -type f -exec ./pair-one.sh pl {} locahost 17004 \;
