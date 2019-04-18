b7b1de08776a2e9b3bdfc4cb9a2dcf0b98bfebf6
Nil Geisweiller <ngeiswei@gmail.com>
2019-04-18 08:29:39 +0300

    Ignore scope links

--- examples/learning/miner/sumo/mine-sumo.scm
+++ examples/learning/miner/sumo/mine-sumo.scm
@@ -5,18 +5,29 @@
 ;; (load "scm/Merge.scm")
 ;; (load "scm/Music.scm")
 ;; (load "scm/Geography.scm")
-(load "scm/Cars.scm")
+;; (load "scm/Cars.scm")
+(load "scm/Hotel.scm")
 
 ;; Set ure logger to debug
 (use-modules (opencog ure))
 (ure-logger-set-level! "debug")
 
+;; Set regular logger to debug
+(use-modules (opencog logger))
+(cog-logger-set-level! "debug")
+
+;; Construct corpus to mine. We select all root atoms except quanfiers
+;; (ForAll, Exists, ImplicationScope, etc) statements, as they usually
+;; represent rules rather than data.
+(define (scope? x) (cog-subtype? 'ScopeLink (cog-type x)))
+(define texts
+  (filter (lambda (x) (not (scope? x))) (cog-get-all-roots)))
+
 ;; Run pattern miner
 (use-modules (opencog miner))
-(define texts (get-all-roots))
 (define results (cog-mine texts
                           #:minsup 5
-                          #:maximum-iterations 1000
+                          #:maximum-iterations 10000
                           #:incremental-expansion #t
                           #:max-conjuncts 2
                           #:surprisingness 'nisurp))
