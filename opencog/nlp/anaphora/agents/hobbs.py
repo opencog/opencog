c832ac56ba4832f6745d8f7ba435a9f70c589d27
Linas Vepstas <linasvepstas@gmail.com>
2019-06-07 13:32:18 -0500

    And aonther bug-fix

--- opencog/nlp/anaphora/agents/hobbs.py
+++ opencog/nlp/anaphora/agents/hobbs.py
@@ -97,8 +97,7 @@ class BindLinkExecution():
         listOfLinks=self.response.out
         for link in listOfLinks:
             atom=(link.out)[1]
-            if atom.type==self.atomType:
-                rv.append(atom)
+            rv.append(atom)
         for link in listOfLinks:
             self.atomspace.remove(link)
         self.atomspace.remove(self.response)
