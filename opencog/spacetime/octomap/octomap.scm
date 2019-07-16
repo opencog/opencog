6b2a9af13b605d7ffda7df79e02518cad27a5b07
Linas Vepstas <linasvepstas@gmail.com>
2019-07-16 16:03:29 -0500

    Move spacetime code over to it's own directory

--- opencog/spacetime/octomap/octomap.scm
+++
@@ -1,8 +0,0 @@
-(define-module (opencog octomap))
-
-(use-modules (srfi srfi-1) (opencog) (opencog exec))
-(use-modules (opencog oc-config))
-
-; Load the C library that calls the nameserver to load the types.
-(load-extension (string-append opencog-ext-path-point-memory "liboctomap-types") "octomap_types_init")
-(load "spacetime/octomap/octomap_types.scm")
