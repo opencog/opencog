3a9bfc322031b0483f31f8b3481a35742202d024
Linas Vepstas <linasvepstas@gmail.com>
2019-12-10 16:09:15 -0600

    Kill off boost

--- opencog/cython/setup.py
+++ opencog/cython/setup.py
@@ -39,7 +39,6 @@ ext = Extension(
         "/opt/local/include" # For MacPorts
         ],
     libraries=["stdc++",
-        "boost_system-mt","boost_thread-mt", # boost dependencies
         # opencog libraries
         "atomspace",
         "util"
@@ -63,7 +62,6 @@ helper_ext = Extension(
         "/opt/local/include" # For MacPorts
         ],
     libraries=["stdc++",
-        "boost_system-mt","boost_thread-mt", # boost dependencies
         ],
     library_dirs=[
         "/opt/local/lib", # For MacPorts
