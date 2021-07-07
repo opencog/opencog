865d71bc5b03b3572313deda2ba75509e9a10599
Linas Vepstas <linasvepstas@gmail.com>
2021-07-07 14:40:14 -0500

    Move files to avoid file path collision

--- opencog/nlp/oc-types/NLPOCTypes.cc
+++ opencog/nlp/oc-types/NLPOCTypes.cc
@@ -1,14 +1,14 @@
 /**
- * NLPTypes.cc
+ * NLPOCTypes.cc
  *
  * Atom Types used during NLP processing.
  *
  * Copyright (c) 2009, 2014 Linas Vepstas <linasvepstas@gmail.com>
  */
 
-#include "opencog/nlp/types/atom_types.definitions"
+#include "opencog/nlp/oc-types/atom_types.definitions"
 
-#define INHERITANCE_FILE "opencog/nlp/types/atom_types.inheritance"
+#define INHERITANCE_FILE "opencog/nlp/oc-types/atom_types.inheritance"
 #define INITNAME nlp_oc_types_init
 
 #include <opencog/atoms/atom_types/atom_types.cc>
