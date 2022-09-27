6a899342eee0734553542cf4271224001c3dc2aa
Linas Vepstas <linasvepstas@gmail.com>
2022-09-27 17:14:30 -0500

    Attempt to fix issue #3680

--- opencog/nlp/relex2logic/relex2logic.scm
+++ opencog/nlp/relex2logic/relex2logic.scm
@@ -99,9 +99,9 @@
 
 	; "." in case the cogserver is started from in-source build directory.
 	(add-to-load-path ".")
-	(load "relex2logic/rule-utils.scm")  ; XXX
+	(load "relex2logic/rule-utils.scm")
 	(load "relex2logic/rule-helpers.scm")
-	(load "relex2logic/loader/load-rules.scm")  ; XXX
+	(load "relex2logic/load-rules.scm")
 	(load "relex2logic/loader/gen-r2l-en-rulebase.scm")
 
 	*unspecified*  ; no return value, avoids printing gunk.
